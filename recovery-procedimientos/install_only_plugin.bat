e31fd253bba5eae6af62a9403f372c5645ba3837
jorge.martin <jorge.martin@pfsgroup.es>
2015-05-11 17:36:47 +0200

    Modificacion de los pom, version unica

--- recovery-procedimientos/install_only_plugin.bat
+++
@@ -1,8 +0,0 @@
-move recovery-procedimientos-plugin\target\recovery-procedimientos-plugin-2.0-SNAPSHOT.jar C:\pfs\Tomcat-6.0\wtpwebapps\rec-web\WEB-INF\lib
-
-xcopy /s /y recovery-procedimientos-plugin\src\web\flows\*.* C:\pfs\Tomcat-6.0\wtpwebapps\rec-web\WEB-INF\flows
-xcopy /s /y recovery-procedimientos-plugin\src\web\jsp\*.* C:\pfs\Tomcat-6.0\wtpwebapps\rec-web\WEB-INF\jsp
-xcopy /s /y recovery-procedimientos-plugin\src\web\img\*.* C:\pfs\Tomcat-6.0\wtpwebapps\rec-web\img
-xcopy /s /y recovery-procedimientos-plugin\src\web\img\*.* C:\pfs\Tomcat-6.0\wtpwebapps\rec-web\img
-copy recovery-procedimientos-plugin\src\main\resources\optionalConfiguration\*.xml C:\pfs\Tomcat-6.0\wtpwebapps\rec-web\WEB-INF\classes\optionalConfiguration 
- 

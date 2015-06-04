d0264db655ccfbf819bf7684c6d2590aeb4f026a
Ignacio <ignacio.arcos@pfsgroup.es>
2015-06-04 18:47:10 +0200

    HR-940 Creación de despacho de supervisor de asunto y asignación de usuarios

--- sql/src/9.1/haya/DML_006_HAYA01_CarterizarAsuntosConSupAsunto.sql
+++ sql/src/9.1/haya/DML_006_HAYA01_CarterizarAsuntosConSupAsunto.sql
@@ -15,7 +15,6 @@
 --*/
 WHENEVER SQLERROR EXIT SQL.SQLCODE;
 SET SERVEROUTPUT ON;
-SET DEFINE OFF;
 DECLARE
     V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
     V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
@@ -24,31 +23,107 @@ DECLARE
     V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.   
     ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
     ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
-
+  	
+    ID_ASUNTO NUMBER(16);
+    ID_TGE NUMBER(16);
+    ID_USD NUMBER(16);
+    ID_USU NUMBER(16);
+    USERNAME VARCHAR2(10 CHAR);
+    TOTAL_INSERTS NUMBER(3); -- Variable para controlar el número de inserts realizados
+    
+    ASUNTOS_CURSOR SYS_REFCURSOR;
+    
 BEGIN
 	
-	V_MSQL := 'UPDATE '||V_ESQUEMA||'.DD_TPO_TIPO_PROCEDIMIENTO' ||
-	          ' SET FLAG_UNICO_BIEN = 1 ' ||
-	          ' WHERE DD_TPO_CODIGO = ''H036''';
-    DBMS_OUTPUT.PUT_LINE('[INFO] Actualizando flag de único bien del trámite de ejecución notarial.......');
-    DBMS_OUTPUT.PUT_LINE(V_MSQL);
-    EXECUTE IMMEDIATE V_MSQL;
-    DBMS_OUTPUT.PUT_LINE('[INFO] Flag de único bien del trámite actualizado.');
-    
-	V_MSQL := 'UPDATE '||V_ESQUEMA||'.TAP_TAREA_PROCEDIMIENTO' ||
-	          ' SET TAP_SCRIPT_VALIDACION = ''!comprobarBienAsociadoPrc() ? ''''<div align="justify" style="font-size: 8pt; font-family: Arial; margin-bottom:10px;">' ||
-	          '<p>&iexcl;Atenci&oacute;n! Para dar por terminada esta tarea, deber&aacute; registrar un &uacute;nico bien, para ello debe acceder a la pesta&ntilde;' ||
-	          'a Bienes.</p></div>'''': (comprobarExisteDocumentoADS() ? (comprobarExisteDocumentoBOP() ? null ' ||
-	          ': ''''Es necesario adjuntar el documento Bolet&iacute;n Oficial Propiedad'''' ) : ''''Es necesario adjuntar el documento Auto declarando subasta'''' ) ''' ||
-	          ' WHERE TAP_CODIGO = ''H036_registrarAnuncioSubasta'' ';
-    DBMS_OUTPUT.PUT_LINE('[INFO] Actualizando label de los campos.......');
-    DBMS_OUTPUT.PUT_LINE(V_MSQL);
-    EXECUTE IMMEDIATE V_MSQL;
-    DBMS_OUTPUT.PUT_LINE('[INFO] Label de los campos actualizada.');
-    
-    
-    COMMIT;
+	V_SQL := 'SELECT USD.USD_ID FROM '||V_ESQUEMA||'.USD_USUARIOS_DESPACHOS USD' || 
+			'INNER JOIN '||V_ESQUEMA||'.DES_DESPACHO_EXTERNO DES ON DES.DES_ID = USD.DES_ID AND DES.DES_CODIGO = ''DSUPASU'' ' ||
+			'INNER JOIN '||V_ESQUEMA_M||'.USU_USUARIOS USU ON USU.USU_ID = USD.USD_ID AND USU_USERNAME = ''SUPER_UCO'' ';
+	EXECUTE IMMEDIATE V_SQL INTO ID_USD;
+	
+	V_SQL := 'SELECT DD_TGE_ID FROM '||V_ESQUEMA_M||'.DD_TGE_TIPO_GESTOR WHERE DD_TGE_CODIGO = ''SUP'' ';
+	EXECUTE IMMEDIATE V_SQL INTO ID_TGE;
+	
+	OPEN ASUNTOS_CURSOR FOR 'SELECT a.ASU_ID ' ||
+							' FROM '||V_ESQUEMA||'.ASU_ASUNTOS a, '||V_ESQUEMA||'.GAA_GESTOR_ADICIONAL_ASUNTO gaa, '||
+							V_ESQUEMA||'.USD_USUARIOS_DESPACHOS usd '||
+							'WHERE gaa.ASU_ID = a.ASU_ID AND gaa.USD_ID = usd.USD_ID AND '||
+							'usd.USU_ID IN (SELECT USU_ID FROM '||V_ESQUEMA_M||'.USU_USUARIOS WHERE USU_USERNAME = ''LETRADO'')';
+ 	
+	TOTAL_INSERTS := 0;
+ 	LOOP
+ 		FETCH ASUNTOS_CURSOR INTO ID_ASUNTO;
+ 		EXIT WHEN ASUNTOS_CURSOR%NOTFOUND;
 
+ 	 		--Sólo si no tiene el usuario y tipo de gestor ya asignados lo doy de alta
+		V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.GAA_GESTOR_ADICIONAL_ASUNTO WHERE ASU_ID='||ID_ASUNTO||' AND DD_TGE_ID='||ID_TGE;
+        --DBMS_OUTPUT.PUT_LINE(V_SQL);
+        EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
+            
+        IF V_NUM_TABLAS < 1 THEN 
+        --Saco el insert a realizar por cada tipo de gestor que tengo que carterizar	
+			V_MSQL := 'INSERT INTO '||V_ESQUEMA||'.GAA_GESTOR_ADICIONAL_ASUNTO (GAA_ID, ASU_ID, USD_ID, DD_TGE_ID, VERSION, USUARIOCREAR, FECHACREAR, BORRADO)'
+			    		|| ' VALUES ('
+			    		
+						|| 'S_GAA_GESTOR_ADICIONAL_ASUNTO.NEXTVAL,'
+						|| ID_ASUNTO || ','
+						|| ID_USD || ','
+						|| ID_TGE || ','
+						|| '0,'
+						|| ' ''GESTOR'','
+						|| 'SYSDATE,'
+						|| '0'
+						|| ')';
+						
+			--DBMS_OUTPUT.PUT_LINE(V_MSQL);
+			EXECUTE IMMEDIATE V_MSQL;
+							
+			V_MSQL := 'INSERT INTO '||V_ESQUEMA||'.GAH_GESTOR_ADICIONAL_HISTORICO'
+						|| ' ('
+						|| ' GAH_ID, '
+						|| ' GAH_GESTOR_ID, '
+						|| ' GAH_ASU_ID, '
+						|| ' GAH_TIPO_GESTOR_ID, '
+						|| ' GAH_FECHA_DESDE, '
+						|| ' VERSION, '
+						|| ' USUARIOCREAR, '
+						|| ' FECHACREAR, '
+						|| ' BORRADO'
+						|| ' )'
+						|| ' VALUES '
+						|| ' ('
+						|| ' S_GAH_GESTOR_ADIC_HISTORICO.NEXTVAL,'
+						|| ID_USD || ','
+						|| ID_ASUNTO || ','
+						|| ID_TGE || ','
+						|| ' SYSDATE,'
+						|| ' 0,'
+						|| ' ''GESTOR'','
+						|| ' SYSDATE,'
+						|| ' 0'
+						|| ' )';
+						
+			--DBMS_OUTPUT.PUT_LINE(V_MSQL);
+			EXECUTE IMMEDIATE V_MSQL;							
+			
+			TOTAL_INSERTS := TOTAL_INSERTS + 1; 
+			
+			IF (TOTAL_INSERTS=100) THEN		
+				COMMIT;					
+				TOTAL_INSERTS := 0; 
+			END IF;
+						
+			--DBMS_OUTPUT.PUT_LINE('El usuario '||gestores.usu_username||' ha sido dado de alta como gestor para el asunto con asu_id='||asuntos.asu_id);				
+			END IF;
+	END LOOP;
+	
+	DBMS_OUTPUT.PUT_LINE('Usuarios gestores y supervisores de prueba ya asignados.');
+	
+	COMMIT;
+
+	DBMS_OUTPUT.PUT_LINE('El proceso ha finalizado correctamente.');
+						
+    CLOSE ASUNTOS_CURSOR;
+    
 EXCEPTION
      
     -- Opcional: Excepciones particulares que se quieran tratar
@@ -68,4 +143,3 @@ END;
 /
  
 EXIT;
-  	

320dc4d6060fd208d0041d20b3897c60b4cc586d
Alberto <alberto.ramirez@pfsgroup.es>
2015-06-08 12:30:53 +0200

    BKNIVDOS-1915

--- recovery-procedimientos-plugin/src/main/java/es/pfsgroup/concursal/convenio/ConvenioManager.java
+++ recovery-procedimientos-plugin/src/main/java/es/pfsgroup/concursal/convenio/ConvenioManager.java
@@ -5,8 +5,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.TimeZone;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;

e6875be5b421ea802a9e3ab248f314d94e7979c6
jorge.martin <jorge.martin@pfsgroup.es>
2015-06-30 18:26:42 +0200

    PRODUCTO-90 Modificacion de la estructura de paquetes a nviel de expediente judicial

--- recovery-precontencioso-plugin/src/main/java/es/pfsgroup/plugin/precontencioso/expedienteJudicial/dao/ExpedienteJudicialDao.java
+++ recovery-precontencioso-plugin/src/main/java/es/pfsgroup/plugin/precontencioso/expedienteJudicial/dao/ExpedienteJudicialDao.java
@@ -3,6 +3,6 @@ package es.pfsgroup.plugin.precontencioso.expedienteJudicial.dao;
 import es.capgemini.pfs.dao.AbstractDao;
 import es.pfsgroup.plugin.recovery.nuevoModeloBienes.model.NMBBien;
 
-public interface ProcedimientoJudicialDao extends AbstractDao<NMBBien, Long> {
+public interface ExpedienteJudicialDao extends AbstractDao<NMBBien, Long> {
 
 }
--- recovery-precontencioso-plugin/src/main/java/es/pfsgroup/plugin/precontencioso/expedienteJudicial/dao/impl/ExpedienteJudicialDaoImpl.java
+++ recovery-precontencioso-plugin/src/main/java/es/pfsgroup/plugin/precontencioso/expedienteJudicial/dao/impl/ExpedienteJudicialDaoImpl.java
@@ -1,9 +1,9 @@
 package es.pfsgroup.plugin.precontencioso.expedienteJudicial.dao.impl;
 
 import es.capgemini.pfs.dao.AbstractEntityDao;
-import es.pfsgroup.plugin.precontencioso.expedienteJudicial.dao.ProcedimientoJudicialDao;
+import es.pfsgroup.plugin.precontencioso.expedienteJudicial.dao.ExpedienteJudicialDao;
 import es.pfsgroup.plugin.recovery.nuevoModeloBienes.model.NMBBien;
 
-public class ProcedimientoJudicialDaoImpl extends AbstractEntityDao<NMBBien, Long> implements ProcedimientoJudicialDao {
+public class ExpedienteJudicialDaoImpl extends AbstractEntityDao<NMBBien, Long> implements ExpedienteJudicialDao {
 
 }

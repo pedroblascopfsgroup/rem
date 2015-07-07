c20f7d9c9a4c9d0aa0fc761b04bec933134e4e70
carlos.gil.gimeno <carlos.gil.gimeno@pfsgroup.es>
2015-07-07 10:39:14 +0200

    Resoluciones PL-SQL

--- recovery-procuradores-common/scripts/v1.0_haya/datos/configuracion/resoluciones/30_resoluciones_tramite_costas_entidad/001_solicitud_costas_entidad.sql
+++ recovery-procuradores-common/scripts/v1.0_haya/datos/configuracion/resoluciones/30_resoluciones_tramite_costas_entidad/001_solicitud_costas_entidad.sql
@@ -7,7 +7,7 @@
 -- Insertamos en la tabla dd_tr_tipos_resolucion el tipo de resolución
 INSERT INTO dd_tr_tipos_resolucion
             (dd_tr_id, dd_tr_codigo, dd_tr_descripcion, dd_tr_descripcion_larga, dd_tj_id, VERSION, usuariocrear, fechacrear, borrado, dd_tr_ayuda, bpm_dd_tac_id)
-   SELECT '351', 'R_TR_COS_ENT_SOL_COST', 'Translado de contrario instando solicitud de costas', 'Translado de contrario instando solicitud de costas', (SELECT dd_tj_id FROM dd_tj_tipo_juicio WHERE dd_tj_codigo = 'TCE')
+   SELECT '351', 'R_TR_COS_ENT_SOL_COST', 'Traslado de contrario instando solicitud de costas', 'Translado de contrario instando solicitud de costas', (SELECT dd_tj_id FROM dd_tj_tipo_juicio WHERE dd_tj_codigo = 'TCE')
           , '0','MOD_PROC', SYSDATE, '0', 'Ayuda de translado de contrario instando solicitud de costas.', (SELECT bpm_dd_tac_id FROM bpm_dd_tac_tipo_accion WHERE bpm_dd_tac_codigo = 'ADVANCE')
      FROM DUAL;
 

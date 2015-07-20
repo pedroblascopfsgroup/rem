0f42d4ca5aca1357c4b7849a1253e5f6ff0fa773
oscar <oscar.dorado@pfsgroup.es>
2015-07-20 10:22:51 +0200

    FASE-1444

---
+++ sql/9.1/bankia/DML_052_BANK01_FASE-1444.sql
@@ -0,0 +1,69 @@
+--/*
+--##########################################
+--## AUTOR=OSCAR DORADO
+--## FECHA_CREACION=20150719
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.1
+--## INCIDENCIA_LINK=FASE-1444
+--## PRODUCTO=NO
+--## Finalidad: DML
+--##           
+--## INSTRUCCIONES: Configurar las variables necesarias en el principio del DECLARE
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+
+--Para permitir la visualización de texto en un bloque PL/SQL utilizando DBMS_OUTPUT.PUT_LINE
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
+SET DEFINE OFF;
+
+DECLARE
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar     
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.   
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+	
+    V_TEXT1 VARCHAR2(2400 CHAR); -- Vble. auxiliar
+    V_ENTIDAD_ID NUMBER(16);
+
+BEGIN
+
+
+DBMS_OUTPUT.PUT_LINE('[INICIO]');
+
+--/**
+-- * Modificacion de tfi_tareas_form_items.tfi_valor_inicial para la tarea P408_lecturaAceptacionInstrucciones
+-- * Original: 		valores['P408_autoApertura'] == null ? '' : ( valores['P408_autoApertura']['fechaJunta'] )
+-- **/           
+execute immediate 'update '||V_ESQUEMA||'.tfi_tareas_form_items set tfi_valor_inicial = null, usuariomodificar = ''FASE-1444'', FECHAMODIFICAR = SYSDATE  where TFI_NOMBRE = ''fechaJunta'' AND tap_id = (select tap_id from '||V_ESQUEMA||'.tap_tarea_procedimiento where tap_codigo = ''P408_lecturaAceptacionInstrucciones'')';
+
+COMMIT;
+
+DBMS_OUTPUT.PUT_LINE('[FIN]');
+
+
+
+EXCEPTION
+     WHEN OTHERS THEN
+          err_num := SQLCODE;
+          err_msg := SQLERRM;
+
+          DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecuciÃ³n:'||TO_CHAR(err_num));
+          DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+          DBMS_OUTPUT.put_line(err_msg);
+
+          ROLLBACK;
+          RAISE;          
+
+END;
+
+/
+
+EXIT
+

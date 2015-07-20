cfa53da0c8118ca3e5cbacb9c2853b28ea79f84e
Alberto Campos <alberto.campos@pfsgroup.es>
2015-07-20 16:15:31 +0200

    [CMREC-373] Se comprueban los cambios en el trámite de Depósito.
    Former-commit-id: 2d4095bdec8a4db0d1c602988408368ac7108e23

--- sql/9.1/vip/DML_038_ENTITY01_ProcedimientoDeposito.sql
+++ sql/9.1/vip/DML_038_ENTITY01_ProcedimientoDeposito.sql
@@ -31,6 +31,7 @@ BEGIN
 	 * UPDATES
 	 * -------
 	 */
+	EXECUTE IMMEDIATE 'UPDATE '||V_ESQUEMA||'.DD_TPO_TIPO_PROCEDIMIENTO tpo SET tpo.DD_TPO_DESCRIPCION = ''T. de depósito - HCJ'', tpo.DD_TPO_XML_JBPM      = ''hcj_tramiteDeposito'' WHERE tpo.DD_TPO_CODIGO = ''H034''';
 	EXECUTE IMMEDIATE 'DELETE FROM  '||V_ESQUEMA||'.DD_PTP_PLAZOS_TAREAS_PLAZAS where tap_id = (SELECT tap_id from '||V_ESQUEMA||'.TAP_TAREA_PROCEDIMIENTO where tap_codigo = ''H034_ContactarDeudor'')';
     EXECUTE IMMEDIATE 'DELETE FROM  '||V_ESQUEMA||'.TFI_TAREAS_FORM_ITEMS where tap_id = (SELECT tap_id from '||V_ESQUEMA||'.TAP_TAREA_PROCEDIMIENTO where tap_codigo = ''H034_ContactarDeudor'')';
     EXECUTE IMMEDIATE 'DELETE FROM  '||V_ESQUEMA||'.TAP_TAREA_PROCEDIMIENTO where tap_codigo = ''H034_ContactarDeudor''';

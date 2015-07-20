94994ce5c9743709264f2f0da11d78cc28ab7b3f
Alberto Campos <alberto.campos@pfsgroup.es>
2015-07-20 13:06:02 +0200

    [CMREC-365] Se comprueban las modificaciones en el procedimiento Ordinario
    Former-commit-id: 8100242e8bdc728798963222b3a23eb5d372a3ef

--- sql/9.1/vip/DML_000_ENTITY01-NUEVOS_DOCUMENTOS.sql
+++ sql/9.1/vip/DML_000_ENTITY01-NUEVOS_DOCUMENTOS.sql
@@ -33,7 +33,8 @@ DECLARE
       ,T_TIPO_TFA('HRESOL', 'Resolución (Hipotecario)', 'Resolución (Hipotecario)', 'EJ') -- T. Hipotecario
       ,T_TIPO_TFA('PRVFND', 'Provisión de Fondos', 'Provisión de Fondos', 'TR') -- T. Provision de fondos
       ,T_TIPO_TFA('INFLIQ', 'Informe de liquidación', 'Informe de liquidación', 'TR') -- T. Calculo de deuda a fecha
-      T_TIPO_TFA('IFISCAL', 'Informe fiscal (T. Subasta)', 'Informe fiscal (T. Subasta)', 'AP') -- T. Calculo de deuda a fecha
+      ,T_TIPO_TFA('IFISCAL', 'Informe fiscal (T. Subasta)', 'Informe fiscal (T. Subasta)', 'AP') -- T. Calculo de deuda a fecha
+      ,T_TIPO_TFA('EDCSDE', 'Escrito de demanda completo + copia sellada de la demanda', 'Escrito de demanda completo + copia sellada de la demanda', 'DE') -- Trámites de Tipo de Actuación Declarativo 
     ); 
     V_TMP_TIPO_TFA T_TIPO_TFA;
     

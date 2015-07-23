d64574727e81739f4a2134ff1252b07481843605
josevi <josevicente.jimenez@pfsgroup.es>
2015-07-23 12:15:39 +0200

    CDD

--- sql/9.1/producto/DDL_045_BANK01_CREAR_DICCIONARIO_RES_VAL_CDD.sql
+++ sql/9.1/producto/DDL_045_BANK01_CREAR_DICCIONARIO_RES_VAL_CDD.sql
@@ -27,12 +27,13 @@ DECLARE
     V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
     V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
     V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
-    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.  
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.
+    V_NUM_SEQ  NUMBER(16); -- Vble. para validar la existencia de una secuencia.
     ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
     ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
 
     V_TEXT1 VARCHAR2(2400 CHAR); -- Vble. auxiliar
-	V_NUM_SEQ NUMBER(16);
+
 BEGIN
 
 SELECT COUNT(1) INTO V_NUM_TABLAS FROM all_tab_cols  

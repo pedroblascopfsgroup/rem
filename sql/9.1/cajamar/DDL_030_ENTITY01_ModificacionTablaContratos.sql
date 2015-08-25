3f47a164ff0c32d922ee2604a68136b04f1ec93a
Gustavo Mora <gustavo.mora@pfsgroup.es>
2015-08-25 12:54:02 +0200

    Cambio tipos de datos
    Former-commit-id: 80048150a4f07c6bf58f42251577f09c3705541d

--- sql/9.1/cajamar/DDL_030_ENTITY01_ModificacionTablaContratos.sql
+++ sql/9.1/cajamar/DDL_030_ENTITY01_ModificacionTablaContratos.sql
@@ -23,7 +23,8 @@ DECLARE
  TABLA VARCHAR(30) :='CNT_CONTRATOS';
  err_num NUMBER;
  err_msg VARCHAR2(2048); 
- V_MSQL VARCHAR2(8500 CHAR);
+ V_MSQL VARCHAR2(2500 CHAR);
+ V_MSQL2 VARCHAR2(2500 CHAR); 
 
 
 BEGIN 
@@ -31,44 +32,29 @@ BEGIN
 
   V_MSQL := 'ALTER TABLE '||V_ESQUEMA||'.'||TABLA||'  ADD
              (
-                CNT_COD_OFI_OP NUMBER(12)
-              , CNT_NUM_ESPEC NUMBER(15)
-              , CNT_POS_VIVA_NO_VENCIDA NUMBER(15,2)
-              , CNT_POS_VIVA_VENCIDA NUMBER(15,2)
-              , CNT_DEUDA_IRREGULAR NUMBER(15,2)
-              , CNT_INT_REMUNERATORIOS NUMBER(15,2)
-              , CNT_INT_MORATORIOS NUMBER(15,2)
-              , CNT_COMISIONES NUMBER(15,2)
-              , CNT_GASTOS NUMBER(15,2)
-              , CNT_IMPUESTOS NUMBER(15,2)
-              , CNT_ENTREGAS NUMBER(15,2)
-              , CNT_INTERESES_ENTREGAS NUMBER(15,2)
-              , CNT_CUOTAS_VENCIDAS_IMP NUMBER(3)
-              , CNT_PTE_DESEMBOLSO NUMBER(15,2)
-              , CNT_SALDO_EXCE NUMBER(15,2)
-              , CNT_LIMITE_DESC NUMBER(15,2)
-              , CNT_LTV_INI NUMBER(5,2)
-              , CNT_LTV_FIN NUMBER(5,2)
-              , CNT_SALDO_PASIVO NUMBER(15,2)
-              , SALDO_PASIVO_OTROS NUMBER(15,2)
-              , CNT_SALDO_DUDOSO NUMBER(15,2)
+                SALDO_PASIVO_OTROS NUMBER(15,2)
               , CNT_SALDO_MUY_DUDOSO NUMBER(15,2)
-              , CNT_PROVISION NUMBER(15,2)
-              , CNT_PORCEN_PROVISION NUMBER(5,2)
-              , CNT_RIESGO NUMBER(15,2)
-              , CNT_RIESGO_GARANT NUMBER(15,2)
-              , CNT_FECHA_INI_EPI_IRREG DATE
-              , CNT_FECHA_POS_VENCIDA DATE
-              , CNT_FECHA_DUDOSO DATE
-              , CNT_SCORING VARCHAR2(40)
-              , CNT_FECHA_RECIBO_DEV_ANT DATE
               , CNT_SALDO_VENC_NO_RECLAM NUMBER(15,2)
-              , CONTRATO_PADRE_NIVEL_1 NUMBER(22)
               , CONTRATO_PADRE_NIVEL_2 NUMBER(22)
             )';
 
+  V_MSQL2 := 'ALTER TABLE '||V_ESQUEMA||'.'||TABLA||'  MODIFY
+             (     
+                  CNT_CONTRATO               NUMBER(17)
+                , CNT_CUOTA_IMPORTE	     NUMBER(15,2)
+                , CNT_DISPUESTO              NUMBER(15,2)
+                , CNT_LIMITE_INI	     NUMBER (15,2)
+                , CNT_LIMITE_FIN	     NUMBER(15,2)
+                , CNT_SISTEMA_AMORTIZACION   VARCHAR2(32)
+                , CNT_INTERES_FIJO_VAR	     VARCHAR2(4)
+                , CNT_CCC_DOMICILIACION	     VARCHAR2(80)
+                , CNT_DOMICI_EXT	     VARCHAR2(4)
+                , CNT_CONTRATO_ANTERIOR	     NUMBER(17)
+                , CNT_COD_OFICINA	     NUMBER(12)
+              )';
      
 	 EXECUTE IMMEDIATE V_MSQL;
+	 EXECUTE IMMEDIATE V_MSQL2;	 
 
 DBMS_OUTPUT.PUT_LINE(''||TABLA||' Modificada');
 

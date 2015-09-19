45097da87f57a04dc2675361312b73eb62418f9e
rachel <raquel.moreno@pfsgroup.es>
2015-09-19 17:08:20 +0200

    PITERTUL. Comprobación de la existencia de WHENEVER SQLERROR... y prefijado de las funciones de la herramienta

--- sql/tool/scripts/DxL-scripts.sh
+++ sql/tool/scripts/DxL-scripts.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Ejecución de scripts de los esquemas de operacional con los usuarios propietarios: master y entidad.
+#
+
 if [ "$#" -ne 2 ]; then
     echo "Parametros: master_pass@sid #ENTITY#"
     exit

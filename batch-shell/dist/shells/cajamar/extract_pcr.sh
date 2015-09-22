139ad0a785b011bcc1591cfab6f027c25bee197a
rachel <raquel.moreno@pfsgroup.es>
2015-09-22 18:31:21 +0200

    Corrección de rutas en shells
    Former-commit-id: b8776377db62705e30113a7649d6b404ef655dad

--- batch-shell/dist/shells/cajamar/extract_pcr.sh
+++ batch-shell/dist/shells/cajamar/extract_pcr.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # Generado manualmente por PBO
  
-ENTIDAD=3058
-
-DIR_INPUT=/aplicaciones/recovecb/transferencia/aprov_troncal/
-export DIR_TXT=/aplicaciones/recovecb/batch-server/control/etl/input/
+DIR_INPUT=/recovery/transferencia/aprov_troncal/
+export DIR_TXT=/recovery/batch-server/control/etl/input/
 
 MAX_WAITING_MINUTES=10
 ficheros=PCR

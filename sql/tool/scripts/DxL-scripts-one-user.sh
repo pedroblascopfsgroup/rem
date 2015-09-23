a8c0e2654dc0bc434cc5bdd2a4fd48e58019c7d5
rachel <raquel.moreno@pfsgroup.es>
2015-09-23 22:54:02 +0200

    Empaquetado de scripts: corrección de variables previas a ejecución
    Former-commit-id: 62d2997477e9d1f548901a8054b61b418fe16202

--- sql/tool/scripts/DxL-scripts-one-user.sh
+++ sql/tool/scripts/DxL-scripts-one-user.sh
@@ -3,5 +3,4 @@ if [ "$#" -ne 1 ]; then
     echo "Parametros: pass@sid"
     exit
 fi
-export NLS_LANG=.AL32UTF8
 
--- sql/tool/scripts/DxL-scripts.sh
+++ sql/tool/scripts/DxL-scripts.sh
@@ -3,5 +3,4 @@ if [ "$#" -ne 2 ]; then
     echo "Parametros: master_pass@sid #ENTITY#"
     exit
 fi
-export NLS_LANG=.AL32UTF8
 

2d5ef89110f794346e4467788504ed9a0f8098a5
Bruno Anglés <bruno.angles@pfsgroup.es>
2015-10-01 21:14:50 +0200

    Creamos un wrapper para java.sql.Connection que nos sustituya los entity.schema que le lleguen a la hora de ejecutar SQLs

--- rec-common/src/main/java/es/capgemini/pfs/dsm/EntityDataSource.java
+++ rec-common/src/main/java/es/capgemini/pfs/dsm/EntityDataSource.java
@@ -88,13 +88,14 @@ public class EntityDataSource extends AbstractRoutingDataSource {
 		// Sobreescribimos el método para añadirle la gestión de los usuarios
 		// transaccionales
 		Connection cnx = super.getConnection();
+		TransactionalUsersConnectionWrapper cnwrap = new TransactionalUsersConnectionWrapper(cnx);
 
 		String transactionalUsers = appProperties
 				.getProperty(DevonPropertiesConstants.DatabaseConfig.USE_TRANSACTIONAL_USERS_KEY);
 
 		if (DevonPropertiesConstants.DatabaseConfig.USE_TRANSACTIONAL_USERS_VALUE_YES
 				.equals(transactionalUsers)) {
-			cambiaCurrentSchema(cnx);
+			cambiaCurrentSchema(cnwrap);
 		}
 		return cnx;
 	}
@@ -105,13 +106,14 @@ public class EntityDataSource extends AbstractRoutingDataSource {
 		// Sobreescribimos el método para añadirle la gestión de los usuarios
 		// transaccionales
 		Connection cnx = super.getConnection(username, password);
+		TransactionalUsersConnectionWrapper cnwrap = new TransactionalUsersConnectionWrapper(cnx);
 
 		String transactionalUsers = appProperties
 				.getProperty(DevonPropertiesConstants.DatabaseConfig.USE_TRANSACTIONAL_USERS_KEY);
 
 		if (DevonPropertiesConstants.DatabaseConfig.USE_TRANSACTIONAL_USERS_VALUE_YES
 				.equals(transactionalUsers)) {
-			cambiaCurrentSchema(cnx);
+			cambiaCurrentSchema(cnwrap);
 		}
 		return cnx;
 	}
@@ -126,7 +128,7 @@ public class EntityDataSource extends AbstractRoutingDataSource {
 	 * @throws RecoveryDSMConfigurationException
 	 *             Si falta por configurar cualquier cosa en devon.properties
 	 */
-	private void cambiaCurrentSchema(Connection cnx) throws SQLException {
+	private void cambiaCurrentSchema(TransactionalUsersConnectionWrapper cnx) throws SQLException {
 		Long dbId = (Long) determineCurrentLookupKey();
 		if (!DataSourceManager.NO_DATASOURCE_ID.equals(dbId)) {
 			Statement st = cnx.createStatement();
@@ -136,6 +138,7 @@ public class EntityDataSource extends AbstractRoutingDataSource {
 						.getProperty(DevonPropertiesConstants.DatabaseConfig.MASTER_SCHEMA_KEY);
 				String sql = "ALTER SESSION SET CURRENT_SCHEMA = "
 						+ masterSchema;
+				cnx.setCurrentSchema(masterSchema);
 				log.debug(sql);
 				st.execute(sql);
 
@@ -148,6 +151,7 @@ public class EntityDataSource extends AbstractRoutingDataSource {
 							mapping);
 					String sql = "ALTER SESSION SET CURRENT_SCHEMA = "
 							+ entitySchema;
+					cnx.setCurrentSchema(entitySchema);
 					log.debug(sql);
 					st.execute(sql);
 				} catch (RecoveryDSMConfigurationException ce) {

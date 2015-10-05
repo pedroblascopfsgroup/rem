8864b9ea60ff055643892216bd1a7ff9ce212eff
Francisco Gutierrez <francisco.gutierrez@pfsgroup.es>
2015-10-05 17:31:03 +0200

    actualización de 3 shells que no estaban en su versión más actualizada en el repositorio (II)

--- batch-shell/dist/shells/bankia/proc_convivencia_cierre_deudas.sh
+++ batch-shell/dist/shells/bankia/proc_convivencia_cierre_deudas.sh
@@ -51,26 +51,38 @@ END_OF_SESSION
 	    java -Xms512M -Xmx1536M -Dconfig.dir=$DIR_CONFIG -Dconfig.file.mask=$CFG_FILE  -Duser.country=ES -Duser.language=es -cp $CLASS2 $CLASEINICIO --context=Default "$@"
 	    	
 		if [ $? = 0 ] ; then
-			cat conv_ccdd_cuentas_demand* > conv_ccdd_cuentas_demand.dat
-                        cat conv_ccdd_cuentas_grnt_bien* > conv_ccdd_cuentas_grnt_bien.dat
-			cat conv_ccdd_cuentas_rel_proc* > conv_ccdd_cuentas_rel_proc.dat
-			cat conv_ccdd_datos_bienes_prc* > conv_ccdd_datos_bienes_prc.dat
-			cat conv_ccdd_instr_letr_pr_lot* > conv_ccdd_instr_letr_pr_lot.dat
-			cat conv_ccdd_lote_activ_proc* > conv_ccdd_lote_activ_proc.dat
-			cat conv_ccdd_per_bien_propuest* > conv_ccdd_per_bien_propuest.dat
-			cat conv_ccdd_pers_proc_acuerdo* > conv_ccdd_pers_proc_acuerdo.dat
-			cat conv_ccdd_procedimientos* > conv_ccdd_procedimientos.dat
-			cat conv_ccdd_res_comit_pr_lot* > conv_ccdd_res_comit_pr_lot.dat		
-			rm conv_ccdd_cuentas_demand_haya.dat
-			rm conv_ccdd_cuentas_grnt_bien_haya.dat
-			rm conv_ccdd_cuentas_rel_proc_haya.dat
-			rm conv_ccdd_datos_bienes_prc_haya.dat
-			rm conv_ccdd_instr_letr_pr_lot_haya.dat
-			rm conv_ccdd_lote_activ_proc_haya.dat
-			rm conv_ccdd_per_bien_propuest_haya.dat
-			rm conv_ccdd_pers_proc_acuerdo_haya.dat
-			rm conv_ccdd_procedimientos_haya.dat
-			rm conv_ccdd_res_comit_pr_lot_haya.dat	
+			cd $DIR_LOCAL
+			rm -f DEF_*
+			cat conv_ccdd_cuentas_demand* > DEF_conv_ccdd_cuentas_demand.dat
+                        cat conv_ccdd_cuentas_grnt_bien* > DEF_conv_ccdd_cuentas_grnt_bien.dat
+			cat conv_ccdd_cuentas_rel_proc* > DEF_conv_ccdd_cuentas_rel_proc.dat
+			cat conv_ccdd_datos_bienes_prc* > DEF_conv_ccdd_datos_bienes_prc.dat
+			cat conv_ccdd_instr_letr_pr_lot* > DEF_conv_ccdd_instr_letr_pr_lot.dat
+			cat conv_ccdd_lote_activ_proc* > DEF_conv_ccdd_lote_activ_proc.dat
+			cat conv_ccdd_per_bien_propuest* > DEF_conv_ccdd_per_bien_propuest.dat
+			cat conv_ccdd_pers_proc_acuerdo* > DEF_conv_ccdd_pers_proc_acuerdo.dat
+			cat conv_ccdd_procedimientos* > DEF_conv_ccdd_procedimientos.dat
+			cat conv_ccdd_res_comit_pr_lot* > DEF_conv_ccdd_res_comit_pr_lot.dat		
+			rm -f conv_ccdd_cuentas_demand_haya.dat
+			rm -f conv_ccdd_cuentas_grnt_bien_haya.dat
+			rm -f conv_ccdd_cuentas_rel_proc_haya.dat
+			rm -f conv_ccdd_datos_bienes_prc_haya.dat
+			rm -f conv_ccdd_instr_letr_pr_lot_haya.dat
+			rm -f conv_ccdd_lote_activ_proc_haya.dat
+			rm -f conv_ccdd_per_bien_propuest_haya.dat
+			rm -f conv_ccdd_pers_proc_acuerdo_haya.dat
+			rm -f conv_ccdd_procedimientos_haya.dat
+			rm -f conv_ccdd_res_comit_pr_lot_haya.dat	
+			mv DEF_conv_ccdd_cuentas_demand.dat conv_ccdd_cuentas_demand.dat
+                        mv DEF_conv_ccdd_cuentas_grnt_bien.dat conv_ccdd_cuentas_grnt_bien.dat
+			mv DEF_conv_ccdd_cuentas_rel_proc.dat conv_ccdd_cuentas_rel_proc.dat
+			mv DEF_conv_ccdd_datos_bienes_prc.dat conv_ccdd_datos_bienes_prc.dat
+			mv DEF_conv_ccdd_instr_letr_pr_lot.dat conv_ccdd_instr_letr_pr_lot.dat
+			mv DEF_conv_ccdd_lote_activ_proc.dat conv_ccdd_lote_activ_proc.dat
+			mv DEF_conv_ccdd_per_bien_propuest.dat conv_ccdd_per_bien_propuest.dat
+			mv DEF_conv_ccdd_pers_proc_acuerdo.dat conv_ccdd_pers_proc_acuerdo.dat
+			mv DEF_conv_ccdd_procedimientos.dat conv_ccdd_procedimientos.dat
+			mv DEF_conv_ccdd_res_comit_pr_lot.dat conv_ccdd_res_comit_pr_lot.dat 
 		else
 			echo "$(basename $0) Error en $filename: error en el ETL"
 			exit 1

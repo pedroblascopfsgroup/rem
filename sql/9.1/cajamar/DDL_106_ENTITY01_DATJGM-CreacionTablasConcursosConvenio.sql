b9c1de06330a3d00424dbd63c33ee44e2858f947
Gustavo Mora <gustavo.mora@pfsgroup.es>
2015-10-07 18:32:56 +0200

    DDLs tablas migracion
    Former-commit-id: 979df1215b589d42281f39ad66506c5f4052b3c3

---
+++ sql/9.1/cajamar/DDL_106_ENTITY01_DATJGM-CreacionTablasConcursosConvenio.sql
@@ -0,0 +1,96 @@
+--/*
+--##########################################
+--## AUTOR=JUAN GALLEGO MOLERO
+--## FECHA_CREACION=20150930
+--## ARTEFACTO=batch
+--## VERSION_ARTEFACTO=0.1
+--## INCIDENCIA_LINK=CMREC-868
+--## PRODUCTO=NO
+--## 
+--## Finalidad: Creación de tabla MIG_CONCURSOS_CONVENIOS
+--## INSTRUCCIONES:  Configurar las variables necesarias en el principio del DECLARE
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
+
+
+DECLARE
+
+ V_ESQUEMA VARCHAR2(25 CHAR):=   '#ESQUEMA#'; 			-- Configuracion Esquema3
+ V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; 		-- Configuracion Esquema Master
+ TABLA VARCHAR(30) :='MIG_CONCURSOS_CONVENIOS';
+ ITABLE_SPACE VARCHAR(25) :='#TABLESPACE_INDEX#';
+ err_num NUMBER;
+ err_msg VARCHAR2(2048); 
+ V_MSQL VARCHAR2(8500 CHAR);
+ V_EXISTE NUMBER (1);
+
+
+BEGIN 
+
+--Validamos si la tabla existe antes de crearla
+
+  SELECT COUNT(*) INTO V_EXISTE
+  FROM ALL_TABLES
+  WHERE TABLE_NAME = ''||TABLA;
+
+  
+  V_MSQL := 'CREATE TABLE '||V_ESQUEMA||'.'||TABLA||' 
+   (	
+	  ID_CONCURSO_CONVENIO      NUMBER(16) 	   NOT NULL, 
+	  CD_CONVENIO               NUMBER(16)	   NOT NULL,
+	  CD_CONCURSO 		    NUMBER(16)	   NOT NULL,
+	  ESTADO_CONVENIO     	    VARCHAR2(20)   NOT NULL,
+	  ORIGEN          	    VARCHAR2(20)   NOT NULL,
+	  TIPO                      VARCHAR2(20)   NOT NULL,
+	  NUMERO_PROPONENTES        NUMBER(3)      NOT NULL,
+	  TOTAL_MASA_PASIVA         NUMBER(14,2),
+	  PORCENTAJE_MASA_PASIVA    NUMBER(3,2) ,         
+	  ALTERNATIVA               VARCHAR2(2) ,
+	  PROPONENETES              VARCHAR2(1000),
+	  TERCEROS                  VARCHAR2(1000),
+	  ADHESION                  NUMBER(1),
+	  RESUMEN_PROPUESTA         VARCHAR2(1000),
+	  POSTURA                   VARCHAR2(20),
+	  DESCRIPCION_ADHESIONES    VARCHAR2(1000),
+	  CONSTRAINT "PK_ID_CONCURSO_CONVENIO" PRIMARY KEY ("ID_CONCURSO_CONVENIO") 
+	  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 NOCOMPRESS LOGGING
+	  TABLESPACE '||ITABLE_SPACE||'  ENABLE
+   ) SEGMENT CREATION DEFERRED 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING';
+ 
+ 
+  IF V_EXISTE = 0 THEN   
+     EXECUTE IMMEDIATE V_MSQL;
+     DBMS_OUTPUT.PUT_LINE(TABLA||' CREADA');
+  ELSE   
+     EXECUTE IMMEDIATE ('DROP TABLE '||V_ESQUEMA||'.'||TABLA );
+     DBMS_OUTPUT.PUT_LINE(TABLA||' BORRADA');
+     EXECUTE IMMEDIATE V_MSQL;
+     DBMS_OUTPUT.PUT_LINE(TABLA||' CREADA');     
+  END IF;   
+          
+--Fin crear tabla
+
+--Excepciones
+
+EXCEPTION
+WHEN OTHERS THEN  
+  err_num := SQLCODE;
+  err_msg := SQLERRM;
+
+  DBMS_OUTPUT.put_line('Error:'||TO_CHAR(err_num));
+  DBMS_OUTPUT.put_line(err_msg);
+  
+  ROLLBACK;
+  RAISE;
+END;
+/
+EXIT;   
+
+
+

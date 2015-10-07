7528db674c0ad24ff264ab41cd639af1c644d749
Alberto Campos <alberto.campos@pfsgroup.es>
2015-10-07 14:08:21 +0200

    [CMREC-891] Se comprueba que las cargas registrales estén rechazadas o canceladas.
    Former-commit-id: 299103ab862bd5139359cfe664d64fc6a79e4ca4

--- sql/9.1/cajamar/DML_208_ENTITY01_IncidenciasLitigios_00.sql
+++ sql/9.1/cajamar/DML_208_ENTITY01_IncidenciasLitigios_00.sql
@@ -29,6 +29,16 @@ BEGIN
 	EXECUTE IMMEDIATE 'UPDATE '||V_ESQUEMA ||'.DD_PTP_PLAZOS_TAREAS_PLAZAS SET DD_PTP_PLAZO_SCRIPT = ''valoresBPMPadre[''''H005_RegistrarInscripcionDelTitulo''''] != null ? damePlazo(dameFechaFinTareaProcedimientoPadre(''''H005_RegistrarInscripcionDelTitulo'''')) + 5*24*60*60*1000L : 10*24*60*60*1000L'' WHERE TAP_ID = (SELECT TAP_ID FROM '||V_ESQUEMA ||'.TAP_TAREA_PROCEDIMIENTO WHERE TAP_CODIGO = '''||V_TAREA||''')';
 	DBMS_OUTPUT.PUT_LINE('[FIN] LINK CMREC-883');
 	
+	DBMS_OUTPUT.PUT_LINE('[INICIO] LINK CMREC-891');
+	V_TAREA:='H008_RegInsCancelacionCargasReg';
+	EXECUTE IMMEDIATE 'UPDATE '||V_ESQUEMA ||'.TAP_TAREA_PROCEDIMIENTO SET TAP_SCRIPT_VALIDACION = ''!comprobarEstadoCargasCancelacion() ? ''''<div align="justify" style="font-size:8pt; font-family:Arial; margin-bottom:10px;">Todas las cargas registrales deben ser informadas en estado cancelada o rechazada</div>'''' : comprobarExisteDocumentoSCBCCR() ? null : ''''<div align="justify" style="font-size:8pt; font-family:Arial; margin-bottom:10px;">Debe adjuntar el Documento acreditativo de la cancelaci&oacute;n de cargas registrales.</div>'''''' WHERE TAP_CODIGO = '''||V_TAREA||'''';
+
+	V_TAREA:='H008_RegInsCancelacionCargasRegEco';
+	EXECUTE IMMEDIATE 'UPDATE '||V_ESQUEMA ||'.TAP_TAREA_PROCEDIMIENTO SET TAP_SCRIPT_VALIDACION = ''!comprobarEstadoCargasCancelacion() ? ''''<div align="justify" style="font-size:8pt; font-family:Arial; margin-bottom:10px;">Todas las cargas registrales deben ser informadas en estado cancelada o rechazada</div>'''' : comprobarExisteDocumentoSCBCCR() ? null : ''''<div align="justify" style="font-size:8pt; font-family:Arial; margin-bottom:10px;">Debe adjuntar el Documento acreditativo de la cancelaci&oacute;n de cargas registrales.</div>'''''' WHERE TAP_CODIGO = '''||V_TAREA||'''';
+	
+	DBMS_OUTPUT.PUT_LINE('[FIN] LINK CMREC-891');
+	
+	
 COMMIT;
  
 EXCEPTION

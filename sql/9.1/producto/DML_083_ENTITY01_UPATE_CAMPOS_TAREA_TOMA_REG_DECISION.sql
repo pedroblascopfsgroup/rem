62fe8422818303d2590375c8dc4f18a6e5710c96
manuel.mejias <manuel.mejias@pfsgroup.es>
2015-10-15 18:26:43 +0200

    Soluciona error formato texto
    Former-commit-id: 253011f75e228b855608456368fecce57c6c18b4

--- sql/9.1/producto/DML_083_ENTITY01_UPATE_CAMPOS_TAREA_TOMA_REG_DECISION.sql
+++ sql/9.1/producto/DML_083_ENTITY01_UPATE_CAMPOS_TAREA_TOMA_REG_DECISION.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=MANUEL_MEJIAS
---## FECHA_CREACION=20151015
+--## FECHA_CREACION=20151016
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.1.3-hy-master
 --## INCIDENCIA_LINK=PRODUCTO-180
@@ -58,7 +58,12 @@ BEGIN
 		V_SQL := 'UPDATE '||V_ESQUEMA||'.TFI_TAREAS_FORM_ITEMS SET TFI_LABEL = ''<div align="justify" style="font-size: 8pt; font-family: Arial; margin-bottom: 30px;"><p style="margin-bottom: 10px">Una vez en disposición de toda la documentación requerida por el expediente de prelitigio, a través de esta pantalla deberá indicar la fecha en que procede al envío de dicha documentación al letrado correspondiente.</p><p style="margin-bottom: 10px">En el campo observaciones consignar cualquier aspecto relevante que le interese que quede reflejado en este punto del procedimiento.</p><p style="margin-bottom: 10px">Una vez complete esta tarea se dará por terminada la preparación documental del expediente de prelitigio, dando así inicio a la fase judicial.</p></div>'' WHERE  TFI_NOMBRE = ''titulo'' AND TAP_ID =  ' || 
       		'(SELECT TAP_ID FROM TAP_TAREA_PROCEDIMIENTO WHERE TAP_CODIGO = ''PCO_EnviarExpedienteLetrado'')';
 
-		EXECUTE IMMEDIATE V_SQL;		
+		EXECUTE IMMEDIATE V_SQL;	
+		
+		V_SQL := 'UPDATE '||V_ESQUEMA||'.TFI_TAREAS_FORM_ITEMS SET TFI_LABEL = ''<div align="justify" style="font-size: 8pt; font-family: Arial; margin-bottom: 30px;"><p style="margin-bottom: 10px">En esta pantalla deberá consignar la fecha en la que recibe la documentación, en el campo “Documentación completa y correcta” deberá indicar si la documentación del expediente efectivamente es completa y correcta o no. En caso de no ser completa deberá indicar el problema detectado según sea por Documentos, Requerimientos, Liquidaciones o por requerir nueva documentación al cambiar el tipo de procedimiento a iniciar.</p><p style="margin-bottom: 10px">En el campo "Procedimiento propuesto por la entidad" se le indica el tipo de procedimiento propuesto por la entidad. En caso de estar de acuerdo con dicha propuesta de actuación, deberá consignar en el campo "Procedimiento a iniciar" el mismo tipo de procedimiento, en caso contrario, deberá seleccionar otro procedimiento según su criterio.</p><p style="margin-bottom: 10px">En el campo observaciones consignar cualquier aspecto relevante que le interesa quede reflejado en este punto.</p>Una vez rellene esta pantalla y en función de la información facilitada podrán darse las siguientes situaciones:</p><p style="margin-bottom: 10px"><li>En caso de haber encontrado un problema en la Documentación, liquidaciones o Requerimientos se iniciará la tarea “Revisar subsanación propuesta” a realizar por la entidad.</li><li>En caso de haber propuesto un cambio de procedimiento se iniciará la tarea “Validar cambio de procedimiento” a realizar por la entidad.</li><li>En caso de no haber encontrado error en la documentación y de haber seleccionado el mismo tipo de procedimiento que el comité se iniciará dicho procedimiento</li></p></div>'' WHERE  TFI_NOMBRE = ''titulo'' AND TAP_ID =  ' || 
+      		'(SELECT TAP_ID FROM TAP_TAREA_PROCEDIMIENTO WHERE TAP_CODIGO = ''PCO_RegistrarTomaDec'')';
+
+		EXECUTE IMMEDIATE V_SQL;
 		
 		
     	DBMS_OUTPUT.PUT_LINE('[FIN] '||V_ESQUEMA||'... ACTUALIZACION DE TABLA TFI_TAREAS_FORM_ITEMS ' );

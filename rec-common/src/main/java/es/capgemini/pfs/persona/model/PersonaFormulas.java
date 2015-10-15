a63baa47bcf4bfd78cee5b7b91483d7df6ab6ec1
Carlos Perez <carlos.perez@pfsgroup.es>
2015-10-15 18:02:54 +0200

    Serializar clase PersonasFormulas
    Former-commit-id: 420869e505c45f65bc23907c27d095ca21d6c880

--- rec-common/src/main/java/es/capgemini/pfs/persona/model/PersonaFormulas.java
+++ rec-common/src/main/java/es/capgemini/pfs/persona/model/PersonaFormulas.java
@@ -1,5 +1,7 @@
 package es.capgemini.pfs.persona.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -11,7 +13,9 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 @Entity
 @Table(name = "PER_PERSONAS_FORMULAS", schema = "${entity.schema}")
 @Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
-public class PersonaFormulas {
+public class PersonaFormulas implements Serializable {
+
+	private static final long serialVersionUID = -3335787426750719296L;
 
 	@Id
 	@Column(name = "PER_ID")

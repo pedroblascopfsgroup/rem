1c20faf81ca8462187f7e097d52b2bcca754bc47
Jorge Ros <jorge.ros@pfsgroup.es>
2015-10-16 14:34:16 +0200

    BKREC-943 Scripts create/drop exp_expedientes_index6 && script insert en FUN_FNCIONES -> MENU-LIST-EXP-RECOBRO-ALL-USERS

---
+++ sql/9.1/producto/DDL_074_BANK01_BKREC_943.sql
@@ -0,0 +1,51 @@
+--/*
+--##########################################
+--## AUTOR=JORGE ROS
+--## FECHA_CREACION=20151015
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.1.16-bk
+--## INCIDENCIA_LINK=BKREC-943
+--## PRODUCTO=SI
+--## Finalidad: DDL
+--##           
+--## INSTRUCCIONES: Configurar las variables necesarias en el principio del DECLARE
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+
+--Para permitir la visualización de texto en un bloque PL/SQL utilizando DBMS_OUTPUT.PUT_LINE
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+DECLARE
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA_ENTIDAD#'; -- Configuracion Esquemas
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_COLS NUMBER(16); -- Vble. para validar la existencia de una tabla.   
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+    CUENTA NUMBER(10);  -- Vble. auxiliar para ver si existe el indice a crear.
+BEGIN	
+  
+  -- CREACION DE INDICES
+  DBMS_OUTPUT.PUT_LINE('INICIO CREATE INDEX '|| V_ESQUEMA ||'.EXP_EXPEDIENTES_INDEX6...');
+  SELECT COUNT(*) INTO CUENTA FROM ALL_INDEXES WHERE INDEX_NAME = 'EXP_EXPEDIENTES_INDEX6' AND TABLE_OWNER=V_ESQUEMA AND TABLE_NAME='EXP_EXPEDIENTES';    
+  IF CUENTA=0 THEN
+	  EXECUTE IMMEDIATE 'CREATE INDEX '|| V_ESQUEMA ||'.EXP_EXPEDIENTES_INDEX6 ON '|| V_ESQUEMA ||'.EXP_EXPEDIENTES (LOWER(EXP_DESCRIPCION) ASC)';  
+  DBMS_OUTPUT.PUT_LINE('CREATE INDEX '|| V_ESQUEMA ||'.EXP_EXPEDIENTES_INDEX6...Creado');
+  END IF;
+	
+
+EXCEPTION
+  WHEN OTHERS THEN
+    ERR_NUM := SQLCODE;
+    ERR_MSG := SQLERRM;
+    DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(ERR_NUM));
+    DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+    DBMS_OUTPUT.put_line(ERR_MSG);
+    ROLLBACK;
+    RAISE;   
+END;
+/
+
+EXIT;

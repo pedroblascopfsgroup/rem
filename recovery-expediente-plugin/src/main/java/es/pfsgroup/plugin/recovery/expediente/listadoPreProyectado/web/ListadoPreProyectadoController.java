b951fc0de6f4d8cd317adda84d623b131cf5a73b
Carlos Perez <carlos.perez@pfsgroup.es>
2015-10-22 13:06:58 +0200

    Add controller
    Former-commit-id: 9aec2c9835cfeba824c967a50a18a4d1b2d79bee

--- recovery-expediente-plugin/src/main/java/es/pfsgroup/plugin/recovery/expediente/listadoPreProyectado/web/ListadoPreProyectadoController.java
+++ recovery-expediente-plugin/src/main/java/es/pfsgroup/plugin/recovery/expediente/listadoPreProyectado/web/ListadoPreProyectadoController.java
@@ -1,8 +1,20 @@
 package es.pfsgroup.plugin.recovery.expediente.listadoPreProyectado.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ListadoPreProyectadoController {
+
+	static final String BUSQUEDA_PREPROYECTADO = "plugin/expediente/listadoPreProyectado/busqueda/preProyectado";
+	
+	@SuppressWarnings("unchecked")
+	@RequestMapping
+	public String abrirBusqueda(ModelMap model) {
+
+//		model.put("id", id);
+		return BUSQUEDA_PREPROYECTADO;
+	}
 
 }

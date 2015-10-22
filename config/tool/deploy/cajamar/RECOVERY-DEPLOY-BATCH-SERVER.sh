bf2976b6e2d9b3150d1d1d5f0975864a1b742f52
rachel <raquel.moreno@pfsgroup.es>
2015-10-22 20:39:46 +0200

    DEPLOY Config de BD y permisos de batch
    Former-commit-id: 1c3ed832230bb2acd433d51507fb3c8b8ddad40c

--- config/tool/deploy/cajamar/RECOVERY-DEPLOY-BATCH-SERVER.sh
+++ config/tool/deploy/cajamar/RECOVERY-DEPLOY-BATCH-SERVER.sh
@@ -2,15 +2,15 @@
 
 LOCAL_PATH=`pwd`
 
-
 unzip zip/batch*.zip
 rm -rf /recovery/batch-server/programas/batch/*
 cp -r batch/* /recovery/batch-server/programas/batch/
-chmod -R g+rw /recovery/batch-server/programas/batch/*
+chmod -R a+rw /recovery/batch-server/programas/batch/*
+chmod a+x /recovery/batch-server/programas/batch/*.sh
 cd $LOCAL_PATH
 rm -f /recovery/batch-server/shells/*.sh
 cp scripts/shells/* /recovery/batch-server/shells/
-chmod ug+x /recovery/batch-server/shells/*.sh
+chmod a+x /recovery/batch-server/shells/*.sh
 cp etl/* /recovery/batch-server/programas/etl/
 cd /recovery/batch-server/programas/etl/
 for etl in `ls *.zip`
@@ -21,8 +21,8 @@ do
 done
 for directory in `ls -d */`
 do
-    chmod -f 775 ${directory}
-    chmod -f ug+x ${directory}/*.sh
+    chmod -f 777 ${directory}
+    chmod -f a+x ${directory}/*.sh
 done
 rm *.zip
 cd $LOCAL_PATH

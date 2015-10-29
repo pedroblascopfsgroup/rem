ef33648232b25118107a026ca2637c5103a57a77
rachel <raquel.moreno@pfsgroup.es>
2015-10-29 20:43:52 +0100

    DEPLOY Fichero de despliegue de ETLs y SHELLS

---
+++ config/tool/deploy/bankia/RECOVERY-DEPLOY-BATCH-SERVER.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+LOCAL_PATH=`pwd`
+
+cp scripts/shells/* /aplicaciones/recovecb/shells/
+chmod a+x /aplicaciones/recovecb/shells/*.sh
+cp etl/* /aplicaciones/recovecb/programas/etl/
+cd /aplicaciones/recovecb/programas/etl/
+for etl in `ls *.zip`
+do
+    ZIPNAME=`echo ${etl} | cut -d- -f1`
+    mv ${etl} "${ZIPNAME}.zip"
+    unzip -o ${ZIPNAME}.zip
+done
+for directory in `ls -d */`
+do
+    chmod -f 777 ${directory}
+    chmod -f a+x ${directory}/*.sh
+done
+rm *.zip
+cd $LOCAL_PATH

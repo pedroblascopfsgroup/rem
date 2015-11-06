abf11a62579827097713c9fc34181b00c163ab4b
Pedro Blasco Onsurbe <pedro.blasco@pfsgroup.es>
2015-11-06 13:39:19 +0100

    Nuevas versiones de scripts de ETLs Liquidaciones
    Former-commit-id: f5f2d3ee351725cdad913250dfd43ef899249829

--- sql/9.1/bankia/DDL_004_BANK01_RECIBOS.sql
+++ sql/9.1/bankia/DDL_004_BANK01_RECIBOS.sql
@@ -1,4 +1,4 @@
---/*
+/*
 --##########################################
 --## AUTOR=Alejandro I�igo
 --## FECHA_CREACION=20151030
@@ -9,7 +9,7 @@
 --##########################################
 --*/
 
---Para permitir la visualización de texto en un bloque PL/SQL utilizando DBMS_OUTPUT.PUT_LINE
+
 
 WHENEVER SQLERROR EXIT SQL.SQLCODE;
 SET SERVEROUTPUT ON;
@@ -17,29 +17,29 @@ SET DEFINE OFF;
 
 DECLARE
 
-    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar    
-    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
-    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
-    V_TS_INDEX VARCHAR2(25 CHAR):= '#TABLESPACE_INDEX#'; -- Configuracion Indice
-    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
-    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.  
-    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
-    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+    V_MSQL VARCHAR2(32000 CHAR); /* Sentencia a ejecutar    */
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; /* Configuracion Esquema*/
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; /* Configuracion Esquema Master*/
+    V_TS_INDEX VARCHAR2(25 CHAR):= '#TABLESPACE_INDEX#'; /* Configuracion Indice*/
+    V_SQL VARCHAR2(4000 CHAR); /* Vble. para consulta que valida la existencia de una tabla.*/
+    V_NUM_TABLAS NUMBER(16); /* Vble. para validar la existencia de una tabla.  */
+    ERR_NUM NUMBER(25);  /* Vble. auxiliar para registrar errores en el script.*/
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.*/
 
-    V_TEXT1 VARCHAR2(2400 CHAR); -- Vble. auxiliar
+    V_TEXT1 VARCHAR2(2400 CHAR); /* Vble. auxiliar*/
     
-    V_ESQUEMA_MIN VARCHAR2(25 CHAR):= '#ESQUEMA_MINI#'; -- Configuracion Esquema minirec
-    V_ESQUEMA_DWH VARCHAR2(25 CHAR):= '#ESQUEMA_DWH#'; -- Configuracion Esquema recovery_bankia_dwh
-    V_ESQUEMA_STG VARCHAR2(25 CHAR):= '#ESQUEMA_STG#'; -- Configuracion Esquema recovery_bankia_datastage
+    V_ESQUEMA_MIN VARCHAR2(25 CHAR):= '#ESQUEMA_MINIREC#'; /* Configuracion Esquema minirec*/
+    V_ESQUEMA_DWH VARCHAR2(25 CHAR):= '#ESQUEMA_DWH#'; /* Configuracion Esquema recovery_bankia_dwh*/
+    V_ESQUEMA_STG VARCHAR2(25 CHAR):= '#ESQUEMA_STG#'; /* Configuracion Esquema recovery_bankia_datastage*/
 	
 BEGIN
 
 
-    --  tabla RCB_RECIBOS_LIQ <-- ELIMINAR
+    /*  tabla RCB_RECIBOS_LIQ <-- ELIMINAR*/
 
     DBMS_OUTPUT.PUT_LINE('[START] DROP TABLE tabla RCB_RECIBOS_LIQ');
 
-    select count(1) into V_NUM_TABLAS from USER_tables where table_name = 'RCB_RECIBOS_LIQ';
+    select count(1) into V_NUM_TABLAS from ALL_TABLES where table_name = 'RCB_RECIBOS_LIQ';
     if V_NUM_TABLAS > 0 then
         EXECUTE IMMEDIATE ' DROP TABLE '|| V_ESQUEMA ||'.RCB_RECIBOS_LIQ CASCADE CONSTRAINTS';
             DBMS_OUTPUT.PUT_LINE('DROP TABLE '|| V_ESQUEMA ||'.RCB_RECIBOS_LIQ... Tabla borrada OK');
@@ -79,9 +79,7 @@ BEGIN
 		VERSION                   	INTEGER DEFAULT 0  NOT NULL,
 		USUARIOBORRAR             	VARCHAR2(50)         ,
 		FECHABORRAR               	TIMESTAMP(6)         ,
-		BORRADO                  	NUMBER(1) DEFAULT 0  NOT NULL,		
-	    --CONSTRAINT PK_RCB_PCO_LIQ_ID    PRIMARY KEY (RCB_PCO_LIQ_ID),
-		CONSTRAINT RCB_UNIQUE_LIQ_EXTERN_ID UNIQUE (RCB_XCOEMP,RCB_COPSER,RCB_IDPRIG,RCB_IDCOEC,RCB_FEVACM,RCB_FEPEAU,RCB_CLLIQ1)				)	 ' ; 
+		BORRADO                  	NUMBER(1) DEFAULT 0  NOT NULL   )	 ' ; 
 		
 		EXECUTE IMMEDIATE 'CREATE INDEX ' || V_ESQUEMA || '.IDX_RCB_PCO_LIQ_ID ON ' || V_ESQUEMA || '.RCB_RECIBOS_LIQ (RCB_PCO_LIQ_ID) ' ||
 		'  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645 ' ||

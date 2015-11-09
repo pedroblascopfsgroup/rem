9a4afd85b8f47c922832ffd8dd3b3a4809f21c17
rachel <raquel.moreno@pfsgroup.es>
2015-11-09 16:40:02 +0100

    MIGRACION Ajustes por cambio de parámetro de entrada
    Former-commit-id: 322dd7089842eb0b8e36b84af927352751da6d80

--- sql/9.1/cajamar/migracion/shells/CJM_migracion_a_recovery.sh
+++ sql/9.1/cajamar/migracion/shells/CJM_migracion_a_recovery.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$#" -ne 1 ]; then
-    echo "Parametros: <CM01_pass@host:puerto/ORACLE_SID>"
+    echo "Parametros: <CM01/CM01_pass@host:puerto/ORACLE_SID>"
     exit
 fi
 
@@ -10,7 +10,7 @@ echo "#####    INICIO MIGRACION_A_RECOVERY.sql"
 echo "########################################################"  
 echo "Inicio MIGRACION_A_RECOVERY_CM.sql"
 
-$ORACLE_HOME/bin/sqlplus CM01/"$1" @"$sql_dir"CJM_MIGRACION_A_RECOVERY.sql  
+$ORACLE_HOME/bin/sqlplus "$1" @"$sql_dir"CJM_MIGRACION_A_RECOVERY.sql  
 
 if [ $? != 0 ] ; then 
    echo -e "\n\n======>>> "Error en @"$sql_dir"CJM_MIGRACION_A_RECOVERY_CM.sql

4bf8f5335d31602d0eed77149ef4fc2573b7397f
jorge.martin <jorge.martin@pfsgroup.es>
2015-11-17 11:51:41 +0100

    Refactor if-else por diccionario
    Former-commit-id: 5e7affd8cccab68f455d23f292df08086371c0af

--- recovery-procedimientos-bpmHaya/src/main/java/es/pfsgroup/recovery/cajamar/gestorDocumental/dto/ConstantesGestorDocumental.java
+++ recovery-procedimientos-bpmHaya/src/main/java/es/pfsgroup/recovery/cajamar/gestorDocumental/dto/ConstantesGestorDocumental.java
@@ -1,5 +1,9 @@
 package es.pfsgroup.recovery.cajamar.gestorDocumental.dto;
 
+import java.util.HashMap;
+
+import es.capgemini.pfs.tareaNotificacion.model.DDTipoEntidad;
+
 public class ConstantesGestorDocumental {
 
 	public final static String ALTA_DOCUMENTO_OPERACION = "A";
@@ -12,4 +16,17 @@ public class ConstantesGestorDocumental {
 	public final static String GESTOR_DOCUMENTAL_TIPO_ASOCIACION_PERSONA = "PERS";
 	public final static String GESTOR_DOCUMENTAL_TIPO_ASOCIACION_PROCEDIMIENTO = "PROC";	
 
+	// Relaciona los tipos de entidad recovery con los tipos de asociacion de cajamar
+	public final static HashMap<String, String> tipoAsociacionPorEntidad;
+
+    static
+    {
+        tipoAsociacionPorEntidad = new HashMap<String, String>();
+        tipoAsociacionPorEntidad.put(DDTipoEntidad.CODIGO_ENTIDAD_EXPEDIENTE, GESTOR_DOCUMENTAL_TIPO_ASOCIACION_EXPEDIENTE);
+        tipoAsociacionPorEntidad.put(DDTipoEntidad.CODIGO_ENTIDAD_CLIENTE, GESTOR_DOCUMENTAL_TIPO_ASOCIACION_PERSONA);
+        tipoAsociacionPorEntidad.put(DDTipoEntidad.CODIGO_ENTIDAD_CONTRATO, GESTOR_DOCUMENTAL_TIPO_ASOCIACION_CONTRATO);
+        tipoAsociacionPorEntidad.put(DDTipoEntidad.CODIGO_ENTIDAD_ASUNTO, GESTOR_DOCUMENTAL_TIPO_ASOCIACION_ASUNTO);
+        tipoAsociacionPorEntidad.put(DDTipoEntidad.CODIGO_ENTIDAD_PROCEDIMIENTO, GESTOR_DOCUMENTAL_TIPO_ASOCIACION_PROCEDIMIENTO);
+        
+    }
 }

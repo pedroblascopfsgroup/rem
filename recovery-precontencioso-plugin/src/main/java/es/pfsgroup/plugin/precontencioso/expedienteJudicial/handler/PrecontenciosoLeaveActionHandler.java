a9c0a5e789b768ed90ee315b93499a67a551c9f2
Carlos Perez <carlos.perez@pfsgroup.es>
2015-11-19 17:57:29 +0100

    [CMREC-1231] Precontencioso. No pasa a estado finalizado
    Former-commit-id: 437519cd7f282afa89d652e4cfe55bb4052bea94

--- recovery-precontencioso-plugin/src/main/java/es/pfsgroup/plugin/precontencioso/expedienteJudicial/handler/PrecontenciosoLeaveActionHandler.java
+++ recovery-precontencioso-plugin/src/main/java/es/pfsgroup/plugin/precontencioso/expedienteJudicial/handler/PrecontenciosoLeaveActionHandler.java
@@ -151,6 +151,16 @@ public class PrecontenciosoLeaveActionHandler extends PROGenericLeaveActionHandl
 			if(DDSiNo.NO.equals(exp_correcto)) {
 				executor.execute("plugin.precontencioso.cambiarEstadoExpediete", prc.getId(), PrecontenciosoBPMConstants.PCO_PREPARACION);
 			}
+		} else if (PrecontenciosoBPMConstants.PCO_RevisarExpDigCONC.equals(tex.getTareaProcedimiento().getCodigo())) {
+			String docCompleta = DDSiNo.NO;
+			for(EXTTareaExternaValor valor : listado) {
+				if("docCompleta".equals(valor.getNombre())){
+					docCompleta = valor.getValor();
+				}
+			}
+			if (DDSiNo.SI.equals(docCompleta)) {
+				executor.execute("plugin.precontencioso.cambiarEstadoExpediete", prc.getId(), PrecontenciosoBPMConstants.PCO_FINALIZADO);
+			}
 		}	
 	}
 

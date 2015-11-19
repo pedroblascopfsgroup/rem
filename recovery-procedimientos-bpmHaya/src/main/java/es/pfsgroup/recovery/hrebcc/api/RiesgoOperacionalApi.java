3bb91379ff9bfa01caad4409fb9da46a738444e2
Javier Ruiz <javier.ruiz@pfsgroup.es>
2015-11-19 16:52:31 +0100

    [PRODUCTO-463] Controller para el riesgo operacional
    Former-commit-id: 185b6f1806e50553cda1fec593be25457f8f9b2b

--- recovery-procedimientos-bpmHaya/src/main/java/es/pfsgroup/recovery/hrebcc/api/RiesgoOperacionalApi.java
+++ recovery-procedimientos-bpmHaya/src/main/java/es/pfsgroup/recovery/hrebcc/api/RiesgoOperacionalApi.java
@@ -1,8 +1,10 @@
 package es.pfsgroup.recovery.hrebcc.api;
 
 import es.pfsgroup.recovery.hrebcc.dto.ActualizarRiesgoOperacionalDto;
+import es.pfsgroup.recovery.hrebcc.model.DDRiesgoOperacional;
 
 public interface RiesgoOperacionalApi {
 
 	public void ActualizarRiesgoOperacional(ActualizarRiesgoOperacionalDto dto);
+	public DDRiesgoOperacional ObtenerRiesgoOperacionalContrato(Long cntId);
 }
--- recovery-procedimientos-bpmHaya/src/main/java/es/pfsgroup/recovery/hrebcc/manager/RiesgoOperacionalManager.java
+++ recovery-procedimientos-bpmHaya/src/main/java/es/pfsgroup/recovery/hrebcc/manager/RiesgoOperacionalManager.java
@@ -1,16 +1,22 @@
 package es.pfsgroup.recovery.hrebcc.manager;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import sun.reflect.generics.visitor.Reifier;
 import es.capgemini.pfs.contrato.dao.EXTContratoDao;
 import es.capgemini.pfs.contrato.model.EXTContrato;
 import es.pfsgroup.commons.utils.Checks;
+import es.pfsgroup.commons.utils.dao.abm.GenericABMDao;
+import es.pfsgroup.commons.utils.dao.abm.GenericABMDao.FilterType;
 import es.pfsgroup.plugin.recovery.coreextension.utils.api.UtilDiccionarioApi;
 import es.pfsgroup.recovery.hrebcc.api.RiesgoOperacionalApi;
 import es.pfsgroup.recovery.hrebcc.dto.ActualizarRiesgoOperacionalDto;
+import es.pfsgroup.recovery.hrebcc.model.CntRiesgoOperacional;
 import es.pfsgroup.recovery.hrebcc.model.DDRiesgoOperacional;
 import es.pfsgroup.recovery.integration.bpm.DiccionarioDeCodigos;
 
+@Service
 public class RiesgoOperacionalManager implements RiesgoOperacionalApi {
 
 	@Autowired
@@ -19,9 +25,12 @@ public class RiesgoOperacionalManager implements RiesgoOperacionalApi {
 	@Autowired
 	UtilDiccionarioApi utilDiccionario;
 	
+	@Autowired
+	GenericABMDao genericDao;
+	
 	@Override
 	public void ActualizarRiesgoOperacional(ActualizarRiesgoOperacionalDto dto) {
-		if (!Checks.esNulo(dto.getIdContrato())) && !Checks.esNulo(dto.getCodRiesgoOperacional())) {
+		if (!Checks.esNulo(dto.getIdContrato()) && !Checks.esNulo(dto.getCodRiesgoOperacional())) {
 			//Obtenemos el contrato enviado
 			EXTContrato contrato = (EXTContrato)contratoDao.get(dto.getIdContrato());
 			
@@ -30,10 +39,39 @@ public class RiesgoOperacionalManager implements RiesgoOperacionalApi {
 			
 			//Si tenemos ambos objetos actualizamos el contrato
 			if (!Checks.esNulo(contrato) && !Checks.esNulo(riesgoOperacional)) {
-				contrato.setRiesgoOperacional(riesgoOperacional);
-				contratoDao.saveOrUpdate(contrato);
+				//Primero obtenemos la relación del contrato con algún riesgo operacional
+				CntRiesgoOperacional cntRiesgo = genericDao.get(CntRiesgoOperacional.class, genericDao.createFilter(FilterType.EQUALS, "contrato.id", contrato.getId())
+															,genericDao.createFilter(FilterType.EQUALS, "borrado", false));
+				if (!Checks.esNulo(cntRiesgo)) {
+					//Si ya tiene uno lo borramos
+					genericDao.deleteById(CntRiesgoOperacional.class, cntRiesgo.getId());
+				}
+				
+				//Ahora creamos un nuevo registro
+				cntRiesgo = new CntRiesgoOperacional();
+				cntRiesgo.setContrato(contrato);
+				cntRiesgo.setRiesgoOperacional(riesgoOperacional);
+				
+				genericDao.save(CntRiesgoOperacional.class, cntRiesgo);
 			}
 		}
 	}
-
+	
+	@Override
+	public DDRiesgoOperacional ObtenerRiesgoOperacionalContrato(Long cntId) {
+		DDRiesgoOperacional resultado = null;
+		
+		if (!Checks.esNulo(cntId)) {
+			//Obtenemos el riesgo operacional del contrato
+			CntRiesgoOperacional cntRiesgo = genericDao.get(CntRiesgoOperacional.class, genericDao.createFilter(FilterType.EQUALS, "contrato.id", cntId)
+					,genericDao.createFilter(FilterType.EQUALS, "borrado", false));
+			
+			if (!Checks.esNulo(cntRiesgo)) {
+				//Si tiene un riesgo se devuelve
+				resultado = cntRiesgo.getRiesgoOperacional();
+			}
+		}
+		
+		return resultado;
+	}
 }

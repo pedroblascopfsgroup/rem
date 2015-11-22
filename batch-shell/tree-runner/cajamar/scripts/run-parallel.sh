e5a98ef67de654bda76b1c394e45d0554b86d255
rachel <raquel.moreno@pfsgroup.es>
2015-11-22 17:28:19 +0100

    Lanzador del árbol de CAJAMAR
    Former-commit-id: 887561f6ad5b470905ff700469999493177ba289

---
+++ batch-shell/tree-runner/cajamar/scripts/run-parallel.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR_SHELLS=/recovery/batch-server/shells
+
+BASE_DIR=`dirname $0`
+while read line; do
+    SHELL=`echo $line | sed -e 's/ //g'`
+    source $DIR_SHELLS/setBatchEnv.sh
+    echo "## Running: "$line
+    $DIR_SHELLS/$line > $BASE_DIR/../log/$SHELL.log
+    if [ $? != 0 ]; then
+        echo "## Failed: " $line
+        exit 1
+    fi
+done < ./lists/$1

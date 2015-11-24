0e219923857a319aea9dda00c8cc146eb048244c
Gustavo Mora <gustavo.mora@pfsgroup.es>
2015-11-24 19:20:12 +0100

    migracion HAYA-CAJAMAR
    Former-commit-id: 4fa030d2bb2975d46f6a5a086342d492a4cc5f47

---
+++ sql/9.1/haya/bcc/migracion/shells/CJM_migracion_a_tabla_intermedia.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo "Parametros: <CM01/CM01_pass@host:puerto/ORACLE_SID>"
+    exit
+fi
+
+sql_dir="sql/"
+
+echo "INICIO DEL SCRIPT MIGRACION_A_TABLA_INTERMEDIA $0" 
+echo "########################################################"  
+echo "#####    INICIO MIGRACION_A_TABLA_INTERMEDIA.sql"  
+echo "########################################################"  
+echo "Migración en curso...."
+
+$ORACLE_HOME/bin/sqlplus "$1" @"$sql_dir"SP_MIG_A_TABLA_INTERMEDIA.sql  
+if [ $? != 0 ] ; then
+   echo -e "\n\n======>>> "Error en @"$sql_dir"SP_MIG_A_TABLA_INTERMEDIA.sql
+   exit 1
+fi
+
+echo "Fin MIGRACION_A_TABLA_INTERMEDIA.sql. Revise el fichero de log" 
+exit 0

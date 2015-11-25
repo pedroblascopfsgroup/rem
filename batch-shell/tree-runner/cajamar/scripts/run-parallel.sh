f562cf174ab67bc904da5502b514b7f47c333d00
rachel <raquel.moreno@pfsgroup.es>
2015-11-25 14:39:45 +0100

    Lanzador del árbol de CAJAMAR. Añadidos extras de personas y contratos, así como generación de ficheros de error
    Former-commit-id: 8845bc3438e980bb60c41a7a52cff6eb6199a844

--- batch-shell/tree-runner/cajamar/scripts/run-parallel.sh
+++ batch-shell/tree-runner/cajamar/scripts/run-parallel.sh
@@ -7,9 +7,11 @@ while read line; do
     SHELL=`echo $line | sed -e 's/ //g'`
     source $DIR_SHELLS/setBatchEnv.sh
     echo "## Running: "$line
-    $DIR_SHELLS/$line > $BASE_DIR/../log/$SHELL.log
+    $DIR_SHELLS/$line > $BASE_DIR/../log/$SHELL.log 2> $BASE_DIR/../log/$SHELL.error
     if [ $? != 0 ]; then
         echo "## Failed: " $line
         exit 1
+    else
+        echo "## Success: " $line
     fi
 done < ./lists/$1

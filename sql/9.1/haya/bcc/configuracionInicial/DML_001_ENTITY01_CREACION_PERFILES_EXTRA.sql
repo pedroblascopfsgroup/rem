981f1a54f08e9018b14463c49f17f2bcdf74645f
Gonzalo Estellés <gonzalo.estelles@pfsgroup.es>
2015-11-30 14:22:37 +0100

    [CMREC-1390] Quitados los la función para cierre de deuda a todos los perfiles
    Former-commit-id: 8ef8a8fcdc83f991b3472724cc054488b6a9e91a

--- sql/9.1/haya/bcc/configuracionInicial/DML_001_ENTITY01_CREACION_PERFILES_EXTRA.sql
+++ sql/9.1/haya/bcc/configuracionInicial/DML_001_ENTITY01_CREACION_PERFILES_EXTRA.sql
@@ -3,3 +3,8 @@ INSERT INTO HAYA02.FUN_PEF (FP_ID,PEF_ID,FUN_ID,USUARIOCREAR,FECHACREAR) values 
 
 --despachos procuradores
 INSERT INTO HAYA02.des_despacho_externo des (des_id,des_despacho,dd_tde_id,zon_id,usuariocrear,fechacrear)  VALUES(HAYA02.s_des_despacho_externo.nextval,'Despacho Procuradores',(SELECT dd_tde_id FROM hayamaster.dd_tde_tipo_despacho WHERE dd_tde_codigo = '2'),(SELECT MAX(zon_id) FROM HAYA02.zon_zonificacion WHERE zon_cod = '01'),'JSV',sysdate);
+
+-- Limpiamos la función ENVIO_CIERRE_DEUDA de los perfiles
+delete
+from Fun_Pef 
+where fun_id in (select fun_id from HAYAMASTER.Fun_Funciones where fun_descripcion='ENVIO_CIERRE_DEUDA');

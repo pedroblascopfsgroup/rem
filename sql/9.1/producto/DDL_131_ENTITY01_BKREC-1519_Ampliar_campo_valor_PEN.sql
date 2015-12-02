bd5f96c015ac5fd57ac4abef3df89600327778af
oscar <oscar.dorado@pfsgroup.es>
2015-12-02 09:39:58 +0100

    BKREC-1519 Ampliar campo valor de tabla PEN
    Former-commit-id: 6c33dd71d2a4087df9b94e66d8387bb265be9c99

---
+++ sql/9.1/producto/DDL_131_ENTITY01_BKREC-1519_Ampliar_campo_valor_PEN.sql
@@ -0,0 +1,70 @@
+--/*
+--##########################################
+--## AUTOR=ALBERTO CAMPOS
+--## FECHA_CREACION=20151110
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.1.17-bk
+--## INCIDENCIA_LINK=BKREC-1051
+--## PRODUCTO=SI
+--## Finalidad: DDL
+--##           
+--## INSTRUCCIONES: Configurar las variables necesarias en el principio del DECLARE
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
+SET DEFINE OFF;
+
+
+DECLARE
+
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar    
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.  
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+
+    V_TABLA VARCHAR2(50 CHAR) := 'PEN_PARAM_ENTIDAD';
+
+BEGIN
+	    
+    DBMS_OUTPUT.PUT_LINE('******** PEN_PARAM_ENTIDAD - Añadir campo PEN_USOS *******');
+    
+    
+    V_SQL := 'SELECT COUNT(1) FROM all_tab_columns WHERE TABLE_NAME = '''||V_TABLA||''' and owner = '''||V_ESQUEMA||''' and column_name = ''PEN_USOS''';
+    EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
+    -- Si existe el campo lo indicamos sino lo creamos
+    IF V_NUM_TABLAS = 1 THEN
+        DBMS_OUTPUT.PUT_LINE('[INFO] ' || V_ESQUEMA || '.'||V_TABLA||'... El campo ya existe en la tabla');
+    ELSE
+        V_MSQL := 'alter table '||V_ESQUEMA||'.'||V_TABLA||' add(PEN_USOS VARCHAR2(4000))';        
+        DBMS_OUTPUT.PUT_LINE(V_MSQL);
+        EXECUTE IMMEDIATE V_MSQL; 
+
+        V_MSQL := 'COMMENT ON COLUMN '||V_ESQUEMA||'.PEN_PARAM_ENTIDAD.PEN_USOS IS ''Documentacion de uso del parametro. Syntaxis de valores.'' ';
+        DBMS_OUTPUT.PUT_LINE(V_MSQL);
+        EXECUTE IMMEDIATE V_MSQL; 
+
+        DBMS_OUTPUT.PUT_LINE('[INFO] ' || V_ESQUEMA || '.'||V_TABLA||'... Añadido el campo PEN_USOS');
+    END IF;
+    
+	
+	DBMS_OUTPUT.PUT_LINE('[INFO] Fin script.');
+
+    COMMIT;
+    
+EXCEPTION
+  WHEN OTHERS THEN
+    ERR_NUM := SQLCODE;
+    ERR_MSG := SQLERRM;
+    DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(ERR_NUM));
+    DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+    DBMS_OUTPUT.put_line(ERR_MSG);
+    ROLLBACK;
+    RAISE;   
+END;
+/
+EXIT;

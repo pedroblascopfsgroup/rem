c28a7ee28083e97cc2ff4b1bf3a25dc20a79411d
Gustavo Mora <gustavo.mora@pfsgroup.es>
2015-12-11 12:56:05 +0100

    asignacion GUID HAYA
    Former-commit-id: 7a1d53c650a7cad3c64925e1d372a070de408dd1

--- sql/9.1/haya/bcc/migracion/sql/CJM_asigna_SYSGUID_HRE.sql
+++ sql/9.1/haya/bcc/migracion/sql/CJM_asigna_SYSGUID_HRE.sql
@@ -116,11 +116,26 @@ BEGIN
      COMMIT;   
   
 
---  EXECUTE IMMEDIATE('TRUNCATE TABLE '||V_ESQUEMA||'.TMP_GUID_PRB_PRC_BIE_CODIGO');
+  EXECUTE IMMEDIATE('TRUNCATE TABLE '||V_ESQUEMA||'.TMP_HAYA_PRC_SYSGUID');
 
---  V_SQL:= null;
+  V_SQL:= null;  
   
--- TABLA INTERMEDIA TMP_GUID_PRB_PRC_BIE_CODIGO CON CODIGO_BIEN
+-- TABLA INTERMEDIA CON CODIGO_BIEN
+ V_SQL:= 'INSERT INTO '||V_ESQUEMA||'.TMP_HAYA_PRC_SYSGUID (PRC_ID, ASU_ID, ASU_ID_EXTERNO, DD_TPO_CODIGO, BIE_CODIGO_INTERNO)
+          SELECT DISTINCT PRC.PRC_ID, ASU.ASU_ID, ASU.ASU_ID_EXTERNO, TPO.DD_TPO_CODIGO, XX.BIE_CODIGO_INTERNO
+          FROM '||V_ESQUEMA||'.PRC_PROCEDIMIENTOS PRC
+             INNER JOIN '||V_ESQUEMA||'.ASU_ASUNTOS ASU ON PRC.ASU_ID = ASU.ASU_ID 
+             INNER JOIN '||V_ESQUEMA||'.DD_TPO_TIPO_PROCEDIMIENTO TPO ON PRC.DD_TPO_ID = TPO.DD_TPO_ID
+           LEFT OUTER JOIN 
+           ( SELECT TT.* FROM (
+             SELECT PRB.PRC_ID, BB.BIE_CODIGO_INTERNO, RANK ()  OVER (PARTITION BY PRB.PRC_ID ORDER BY BB.BIE_CODIGO_INTERNO) AS RANKING
+             FROM '||V_ESQUEMA||'.PRB_PRC_BIE PRB 
+               INNER JOIN '||V_ESQUEMA||'.BIE_BIEN BB ON BB.BIE_ID = PRB.BIE_ID
+               )TT WHERE TT.RANKING = 1
+            ) XX ON XX.PRC_ID = PRC.PRC_ID  
+         WHERE PRC.USUARIOCREAR = ''MIGRAHAYA02''
+           AND ASU.USUARIOCREAR = ''MIGRAHAYA02''';
+
 --  V_SQL:= 'INSERT INTO TMP_GUID_PRB_PRC_BIE_CODIGO 
 --     (  PRB_ID , PRC_ID , BIE_ID , USUARIOCREAR , BIE_CODIGO_INTERNO
 --     )
@@ -134,21 +149,19 @@ BEGIN
 --                 , RANK() OVER (PARTITION BY prb.PRC_ID ORDER BY bie.BIE_CODIGO_INTERNO) AS RANKING     
 --            FROM
 --                 '||V_ESQUEMA||'.PRB_PRC_BIE prb
---               , (SELECT *
---                    FROM (SELECT BIE_ID, BIE_CODIGO_INTERNO, RANK() OVER (PARTITION BY BIE_CODIGO_INTERNO ORDER BY BIE_ID) AS R1 FROM '||V_ESQUEMA||'.BIE_BIEN  )
---                   WHERE R1 = 1 ) bie
+--               ,  ||V_ESQUEMA||'.BIE_BIEN  bie
 --            WHERE 
 --                   prb.BIE_ID = bie.BIE_ID               
 --               AND prb.USUARIOCREAR = ''MIGRAHAYA02'' ) WHERE RANKING = 1'  ;
 --
 --
---   EXECUTE IMMEDIATE V_SQL;
---  
---   COMMIT;
---
---
---   HAYA02.UTILES.ANALIZA_TABLA('HAYA02','TMP_GUID_PRB_PRC_BIE_CODIGO');   
---   DBMS_OUTPUT.PUT_LINE(V_ESQUEMA||'.UTILES.ANALIZA_TABLA( '''||V_ESQUEMA||''',''TMP_GUID_PRB_PRC_BIE_CODIGO'' )');   
+   EXECUTE IMMEDIATE V_SQL;
+  
+   COMMIT;
+
+
+   HAYA02.UTILES.ANALIZA_TABLA('HAYA02','TMP_HAYA_PRC_SYSGUID');   
+   DBMS_OUTPUT.PUT_LINE(V_ESQUEMA||'.UTILES.ANALIZA_TABLA( '''||V_ESQUEMA||''',''TMP_HAYA_PRC_SYSGUID'' )');   
 
    
 -- PRC_PROCEDIMIENTOS
@@ -161,16 +174,12 @@ BEGIN
      V_SQL:= 'INSERT INTO '||V_ESQUEMA||'.TMP_GUID_PRC_PROCS_HRE (SYS_GUID, PRC_ID)
                            SELECT tmp.GUID_SYSGUID, prchre.PRC_ID  
                           FROM '||V_ESQUEMA||'.TMP_GUID_HRE_2 tmp
-                             , '||V_ESQUEMA||'.ASU_ASUNTOS asuhre
-                             , '||V_ESQUEMA||'.DD_TPO_TIPO_PROCEDIMIENTO dd
                              , '||V_ESQUEMA||'.PRC_PROCEDIMIENTOS prchre                             
---                             , '||V_ESQUEMA||'.TMP_GUID_PRB_PRC_BIE_CODIGO prb
-                         WHERE prchre.ASU_ID              = asuhre.ASU_ID
-                           AND prchre.DD_TPO_ID           = dd.DD_TPO_ID 
---                           AND prchre.PRC_ID              = prb.PRC_ID (+)
---                           AND NVL(tmp.GUID_BIE_CODIGO_INTERNO,0) = NVL(prb.BIE_CODIGO_INTERNO,0)
-                           AND tmp.GUID_ASU_ID_EXTERNO    = asuhre.ASU_ID_EXTERNO 
-                           AND tmp.GUID_DD_TPO_CODIGO     = dd.DD_TPO_CODIGO
+                             , '||V_ESQUEMA||'.TMP_HAYA_PRC_SYSGUID haya
+                         WHERE  prchre.PRC_ID             = haya.PRC_ID      
+                           AND NVL(tmp.GUID_BIE_CODIGO_INTERNO,''-123'') = NVL(haya.BIE_CODIGO_INTERNO,''-123'')
+                           AND tmp.GUID_ASU_ID_EXTERNO    = haya.ASU_ID_EXTERNO 
+                           AND tmp.GUID_DD_TPO_CODIGO     = haya.DD_TPO_CODIGO
                            AND tmp.GUID_DES               = ''PRC_PROCEDIMIENTOS''
                            AND prchre.USUARIOCREAR = ''MIGRAHAYA02''';
 
@@ -241,36 +250,80 @@ BEGIN
 
    EXECUTE IMMEDIATE('TRUNCATE TABLE '||V_ESQUEMA||'.TMP_GUID_PRB_HRE'); 
 
-   V_SQL:= 'insert into '||V_ESQUEMA||'.TMP_GUID_PRB_HRE (SYS_GUID, prb_id)               
-                 select tmp.SYS_GUID, prbhre.prb_id
-                          FROM (select * from (select GUID_SYSGUID as sys_guid
-                                           , GUID_ASU_ID_EXTERNO
-                                           , GUID_DD_TPO_CODIGO
-                                           , GUID_BIE_CODIGO_INTERNO
-                                           , GUID_DES
-                                              ,rank() over (partition by   GUID_ASU_ID_EXTERNO
-                                                                         , GUID_DD_TPO_CODIGO
-                                                                          , GUID_BIE_CODIGO_INTERNO
-                                                             order by GUID_SYSGUID) as ranking
-                                      from '||V_ESQUEMA||'.TMP_GUID_HRE_2 ) where ranking = 1) tmp
-                             , '||V_ESQUEMA||'.ASU_ASUNTOS asuhre
-                             , '||V_ESQUEMA||'.DD_TPO_TIPO_PROCEDIMIENTO dd
-                             , '||V_ESQUEMA||'.PRC_PROCEDIMIENTOS prchre
---                           , '||V_ESQUEMA||'.BIE_BIEN           biehre
-                             , '||V_ESQUEMA||'.PRB_PRC_BIE prbhre
-                            , (SELECT BIE_ID, BIE_CODIGO_INTERNO, RANK() OVER (PARTITION BY BIE_CODIGO_INTERNO ORDER BY BIE_ID) AS RANKING2 FROM '||V_ESQUEMA||'.BIE_BIEN ) biehre
-                         WHERE prchre.ASU_ID              = asuhre.ASU_ID
-                           AND prchre.DD_TPO_ID           = dd.DD_TPO_ID 
-                           AND tmp.GUID_ASU_ID_EXTERNO    = asuhre.ASU_ID_EXTERNO 
-                           AND tmp.GUID_DD_TPO_CODIGO     = dd.DD_TPO_CODIGO
-                           AND tmp.GUID_DES               = ''PRB_PRC_BIE''
-                           AND tmp.GUID_BIE_CODIGO_INTERNO = biehre.BIE_CODIGO_INTERNO
-                           AND biehre.BIE_ID               = prbhre.BIE_ID
-                           AND prchre.PRC_ID               = prbhre.PRC_ID 
-                           AND prbhre.usuariocrear = ''MIGRAHAYA02''
-                           and biehre.ranking2 = 1 '
-                           ;
-   
+-- versión sin tener en cuenta duplicados
+--   V_SQL:= 'insert into '||V_ESQUEMA||'.TMP_GUID_PRB_HRE (SYS_GUID, prb_id)               
+--                 select tmp.SYS_GUID, prbhre.prb_id
+--                          FROM (select * from (select GUID_SYSGUID as sys_guid
+--                                           , GUID_ASU_ID_EXTERNO
+--                                           , GUID_DD_TPO_CODIGO
+--                                           , GUID_BIE_CODIGO_INTERNO
+--                                           , GUID_DES
+--                                              ,rank() over (partition by   GUID_ASU_ID_EXTERNO
+--                                                                         , GUID_DD_TPO_CODIGO
+--                                                                          , GUID_BIE_CODIGO_INTERNO
+--                                                             order by GUID_SYSGUID) as ranking
+--                                      from '||V_ESQUEMA||'.TMP_GUID_HRE_2 ) where ranking = 1) tmp
+--                             , '||V_ESQUEMA||'.ASU_ASUNTOS asuhre
+--                             , '||V_ESQUEMA||'.DD_TPO_TIPO_PROCEDIMIENTO dd
+--                             , '||V_ESQUEMA||'.PRC_PROCEDIMIENTOS prchre
+----                           , '||V_ESQUEMA||'.BIE_BIEN           biehre
+--                             , '||V_ESQUEMA||'.PRB_PRC_BIE prbhre
+--                            , (SELECT BIE_ID, BIE_CODIGO_INTERNO, RANK() OVER (PARTITION BY BIE_CODIGO_INTERNO ORDER BY BIE_ID) AS RANKING2 FROM '||V_ESQUEMA||'.BIE_BIEN ) biehre
+--                         WHERE prchre.ASU_ID              = asuhre.ASU_ID
+--                           AND prchre.DD_TPO_ID           = dd.DD_TPO_ID 
+--                           AND tmp.GUID_ASU_ID_EXTERNO    = asuhre.ASU_ID_EXTERNO 
+--                           AND tmp.GUID_DD_TPO_CODIGO     = dd.DD_TPO_CODIGO
+--                           AND tmp.GUID_DES               = ''PRB_PRC_BIE''
+--                           AND tmp.GUID_BIE_CODIGO_INTERNO = biehre.BIE_CODIGO_INTERNO
+--                           AND biehre.BIE_ID               = prbhre.BIE_ID
+--                           AND prchre.PRC_ID               = prbhre.PRC_ID 
+--                           AND prbhre.usuariocrear = ''MIGRAHAYA02''
+--                           and biehre.ranking2 = 1 '
+--                           ;
+
+   V_SQL:= 'insert into '||V_ESQUEMA||'.TMP_GUID_PRB_HRE (SYS_GUID, prb_id)                              
+            SELECT DISTINCT tmp.GUID_SYSGUID as SYS_GUID, haya2.prb_id
+            FROM 
+               (
+                  select * from 
+                   ( 
+                      select GUID_SYSGUID 
+                           , GUID_ASU_ID_EXTERNO
+                           , GUID_DD_TPO_CODIGO
+                           , GUID_BIE_CODIGO_INTERNO
+                           , GUID_DES
+                           , RANK() over (partition by GUID_ASU_ID_EXTERNO
+                                                     , GUID_DD_TPO_CODIGO
+                                                     , GUID_BIE_CODIGO_INTERNO
+                                          order by GUID_SYSGUID) as ranking
+                        from '||V_ESQUEMA||'.TMP_GUID_HRE_2 
+                        where guid_des = ''PRB_PRC_BIE''
+                    ) 
+               ) tmp
+            ,  ( select haya.prc_id
+                       , haya.asu_id
+                       , haya.asu_id_externo
+                       , haya.dd_tpo_codigo
+                       , biehre.bie_codigo_interno
+                       , prbhre.prb_id      
+                       , biehre.bie_id
+                       , rank() over (partition by   haya.ASU_ID_EXTERNO
+                                                   , haya.DD_TPO_CODIGO
+                                                   , biehre.BIE_CODIGO_INTERNO
+                                       order by prbhre.PRB_ID) as ranking2             
+                     from          '||V_ESQUEMA||'.TMP_HAYA_PRC_SYSGUID haya
+                                 , '||V_ESQUEMA||'.BIE_BIEN        biehre
+                                 , '||V_ESQUEMA||'.PRB_PRC_BIE     prbhre
+                    where biehre.BIE_ID       = prbhre.BIE_ID
+                      and haya.PRC_ID         = prbhre.PRC_ID 
+                      and prbhre.usuariocrear = ''MIGRAHAYA02'' 
+                      ) haya2
+                     WHERE tmp.GUID_ASU_ID_EXTERNO     = haya2.ASU_ID_EXTERNO 
+                       AND tmp.GUID_DD_TPO_CODIGO      = haya2.DD_TPO_CODIGO
+                       AND tmp.GUID_BIE_CODIGO_INTERNO = haya2.BIE_CODIGO_INTERNO
+                       AND tmp.ranking                = haya2.ranking2';
+                       
+                           
    EXECUTE IMMEDIATE V_SQL;                           
    COMMIT;
     
@@ -323,27 +376,20 @@ BEGIN
     EXECUTE IMMEDIATE('TRUNCATE TABLE '||V_ESQUEMA||'.TMP_GUID_TAR_TAREAS_HRE'); 
       
    
-     V_SQL:= 'insert into TMP_GUID_TAR_TAREAS_HRE (SYS_GUID, TAR_ID)
+     V_SQL:= 'insert into '||V_ESQUEMA||'.TMP_GUID_TAR_TAREAS_HRE (SYS_GUID, TAR_ID)
                          SELECT bcc.GUID_SYSGUID, tarhre.TAR_ID
-                          FROM '||V_ESQUEMA||'.TMP_GUID_HRE_2 bcc
-                             , '||V_ESQUEMA||'.ASU_ASUNTOS asuhre
-                             , '||V_ESQUEMA||'.PRC_PROCEDIMIENTOS prchre                             
-                             , '||V_ESQUEMA||'.DD_TPO_TIPO_PROCEDIMIENTO dd                             
---                             , '||V_ESQUEMA||'.TMP_GUID_PRB_PRC_BIE_CODIGO prb
+                          FROM '||V_ESQUEMA||'.TMP_GUID_HRE_2 bcc                            
+                             , '||V_ESQUEMA||'.TMP_HAYA_PRC_SYSGUID haya
                              , '||V_ESQUEMA||'.TEX_TAREA_EXTERNA tex
                              , '||V_ESQUEMA||'.TAP_TAREA_PROCEDIMIENTO tap  
                              , '||V_ESQUEMA||'.TAR_TAREAS_NOTIFICACIONES tarhre
-                         WHERE prchre.ASU_ID     = asuhre.ASU_ID
-                           AND prchre.DD_TPO_ID  = dd.DD_TPO_ID 
-  --                         AND prchre.PRC_ID     = prb.PRC_ID (+)
-                           AND tarhre.ASU_ID     = prchre.ASU_ID  
-                           AND tarhre.PRC_ID     = prchre.PRC_ID  
+                         WHERE NVL(bcc.GUID_BIE_CODIGO_INTERNO,''-123'') = NVL(haya.BIE_CODIGO_INTERNO,''-123'')                           
+                           AND tarhre.PRC_ID     = haya.PRC_ID  
                            AND tarhre.TAR_ID     = tex.TAR_ID
                            AND tex.TAP_ID        = tap.TAP_ID
-                           AND bcc.GUID_ASU_ID_EXTERNO = asuhre.ASU_ID_EXTERNO 
-                           AND bcc.GUID_DD_TPO_CODIGO  = dd.DD_TPO_CODIGO
+                           AND bcc.GUID_ASU_ID_EXTERNO = haya.ASU_ID_EXTERNO 
+                           AND bcc.GUID_DD_TPO_CODIGO  = haya.DD_TPO_CODIGO
                            AND bcc.GUID_TAP_CODIGO     = tap.TAP_CODIGO
-    --                       AND NVL(bcc.GUID_BIE_CODIGO_INTERNO,0) = NVL(prb.BIE_CODIGO_INTERNO,0)                                                      
                            AND bcc.GUID_DES            = ''TAR_TAREAS_NOTIFICACIONES''
                            AND tarhre.USUARIOCREAR = ''MIGRAHAYA02'''
                            ;   
@@ -429,19 +475,14 @@ BEGIN
      V_SQL:= 'insert into '||V_ESQUEMA||'.TMP_GUID_SUB_HRE (SYS_GUID, SUB_ID)
                         SELECT tmp.GUID_SYSGUID AS SYS_GUID, subhre.SUB_ID       
                           FROM '||V_ESQUEMA||'.TMP_GUID_HRE_2 tmp
-                             , '||V_ESQUEMA||'.ASU_ASUNTOS asuhre
-                             , '||V_ESQUEMA||'.PRC_PROCEDIMIENTOS prchre                             
-                             , '||V_ESQUEMA||'.DD_TPO_TIPO_PROCEDIMIENTO dd                             
---                             , '||V_ESQUEMA||'.TMP_GUID_PRB_PRC_BIE_CODIGO prb
+                             , '||V_ESQUEMA||'.TMP_HAYA_PRC_SYSGUID haya                        
                              , '||V_ESQUEMA||'.SUB_SUBASTA subhre
-                         WHERE prchre.ASU_ID     = asuhre.ASU_ID
-                           AND prchre.DD_TPO_ID  = dd.DD_TPO_ID 
---                           AND prchre.PRC_ID     = prb.PRC_ID (+)
-                           AND subhre.ASU_ID     = prchre.ASU_ID  
-                           AND subhre.PRC_ID     = prchre.PRC_ID  
-                           AND tmp.GUID_ASU_ID_EXTERNO = asuhre.ASU_ID_EXTERNO 
-                           AND tmp.GUID_DD_TPO_CODIGO  = dd.DD_TPO_CODIGO
- --                          AND NVL(tmp.GUID_BIE_CODIGO_INTERNO,0) = NVL(prb.BIE_CODIGO_INTERNO,0)
+                         WHERE 
+                               subhre.ASU_ID     = haya.ASU_ID  
+                           AND subhre.PRC_ID     = haya.PRC_ID  
+                           AND NVL(tmp.GUID_BIE_CODIGO_INTERNO,''-123'') = NVL(haya.BIE_CODIGO_INTERNO,''-123'')                                   
+                           AND tmp.GUID_ASU_ID_EXTERNO = haya.ASU_ID_EXTERNO 
+                           AND tmp.GUID_DD_TPO_CODIGO  = haya.DD_TPO_CODIGO
                            AND tmp.GUID_DES            = ''SUB_SUBASTA''
                            AND subhre.USUARIOCREAR = ''MIGRAHAYA02''';    
                                 

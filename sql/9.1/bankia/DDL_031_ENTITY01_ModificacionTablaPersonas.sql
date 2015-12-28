ce00735284b5f263e9cd84662629b1343b9df8b9
josevicente.jimenez <josevicente.jimenez@pfsgroup.es>
2015-12-28 18:58:18 +0100

    MERGE_PRECON Correcciones scripts para hacerlos RELANZABLES
    Former-commit-id: 8079cdae7f52b0e38cbfa959478c27c95678bc77

--- sql/9.1/bankia/DDL_031_ENTITY01_ModificacionTablaPersonas.sql
+++ sql/9.1/bankia/DDL_031_ENTITY01_ModificacionTablaPersonas.sql
@@ -1,5 +1,5 @@
 --##########################################
---## AUTOR=GUSTAVO MORA NAVARRO
+--## AUTOR=GUSTAVO MORA NAVARRO / JOSEVI
 --## FECHA_CREACION=20150820
 --## ARTEFACTO=batch
 --## VERSION_ARTEFACTO=9.3
@@ -20,29 +20,128 @@ SET SERVEROUTPUT ON;
 
 DECLARE
 
- V_ESQUEMA VARCHAR(25) := 'BANK01';
+ V_ESQUEMA VARCHAR(25) := '#ESQUEMA#';
  TABLA VARCHAR(30) :='PER_PERSONAS';
+ COLUMNA VARCHAR(50) :='';
+ TIPO VARCHAR(50) :='';
+ V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.   
  err_num NUMBER;
  err_msg VARCHAR2(2048); 
- V_MSQL VARCHAR2(8500 CHAR);
- V_MSQL2 VARCHAR2(8500 CHAR);
+ V_MSQL VARCHAR2(2500 CHAR);
+ V_MSQL2 VARCHAR2(2500 CHAR); 
 
 
 BEGIN 
   
 
-  V_MSQL := 'ALTER TABLE '||V_ESQUEMA||'.'||TABLA||'  ADD
-             (
-                 PER_COD_ESTADO_CICLO_VIDA VARCHAR2(50 CHAR)
-               , PER_SCORING VARCHAR2(10 CHAR)
-               , PER_SERV_NOMINA_PENSION VARCHAR2(1 CHAR)
-               , PER_ULTIMA_ACTUACION VARCHAR2(1024 CHAR)
-               , PER_SITUACION_CONCURSAL VARCHAR2(1 CHAR)
-            )';
+--  V_MSQL := 'ALTER TABLE '||V_ESQUEMA||'.'||TABLA||'  ADD
+--             (
+--                 PER_COD_ESTADO_CICLO_VIDA VARCHAR2(50 CHAR)
+--               , PER_SCORING VARCHAR2(10 CHAR)
+--               , PER_SERV_NOMINA_PENSION VARCHAR2(1 CHAR)
+--               , PER_ULTIMA_ACTUACION VARCHAR2(1024 CHAR)
+--               , PER_SITUACION_CONCURSAL VARCHAR2(1 CHAR)
+--            )';
 
 
-     
-  EXECUTE IMMEDIATE V_MSQL;
+  COLUMNA := 'PER_COD_ESTADO_CICLO_VIDA';
+  TIPO := 'VARCHAR2(50 CHAR)';
+  V_MSQL := 'select count(1) from all_tab_cols where column_name = '''||COLUMNA||'''  and owner = '''||V_ESQUEMA||''' ';
+  EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;
+
+  IF V_NUM_TABLAS = 0 THEN            
+
+    V_MSQL2 := 'ALTER TABLE '||V_ESQUEMA||'.'||TABLA||'  ADD
+               ( '||COLUMNA||' '||TIPO||' )';
+       
+    EXECUTE IMMEDIATE V_MSQL2;
+    DBMS_OUTPUT.put_line('[[INFO]] Agregada nueva columna '||TABLA||'.'||COLUMNA||' - OK');
+
+  ELSE        
+    DBMS_OUTPUT.put_line('[[INFO]] Ya existe el la columna '||COLUMNA||'. NO SE REALIZAN CAMBIOS. ');
+  END IF ;
+
+
+
+
+  COLUMNA := 'PER_SCORING';
+  TIPO := 'VARCHAR2(10 CHAR)';
+  V_MSQL := 'select count(1) from all_tab_cols where column_name = '''||COLUMNA||'''  and owner = '''||V_ESQUEMA||''' ';
+  EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;
+
+  IF V_NUM_TABLAS = 0 THEN            
+
+    V_MSQL2 := 'ALTER TABLE '||V_ESQUEMA||'.'||TABLA||'  ADD
+               ( '||COLUMNA||' '||TIPO||' )';
+       
+    EXECUTE IMMEDIATE V_MSQL2;
+    DBMS_OUTPUT.put_line('[[INFO]] Agregada nueva columna '||TABLA||'.'||COLUMNA||' - OK');
+
+  ELSE        
+    DBMS_OUTPUT.put_line('[[INFO]] Ya existe el la columna '||COLUMNA||'. NO SE REALIZAN CAMBIOS. ');
+  END IF ;
+
+
+
+
+  COLUMNA := 'PER_SERV_NOMINA_PENSION';
+  TIPO := 'VARCHAR2(1 CHAR)';
+  V_MSQL := 'select count(1) from all_tab_cols where column_name = '''||COLUMNA||'''  and owner = '''||V_ESQUEMA||''' ';
+  EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;
+
+  IF V_NUM_TABLAS = 0 THEN            
+
+    V_MSQL2 := 'ALTER TABLE '||V_ESQUEMA||'.'||TABLA||'  ADD
+               ( '||COLUMNA||' '||TIPO||' )';
+       
+    EXECUTE IMMEDIATE V_MSQL2;
+    DBMS_OUTPUT.put_line('[[INFO]] Agregada nueva columna '||TABLA||'.'||COLUMNA||' - OK');
+
+  ELSE        
+    DBMS_OUTPUT.put_line('[[INFO]] Ya existe el la columna '||COLUMNA||'. NO SE REALIZAN CAMBIOS. ');
+  END IF ;
+
+
+
+
+  COLUMNA := 'PER_ULTIMA_ACTUACION';
+  TIPO := 'VARCHAR2(1024 CHAR)';
+  V_MSQL := 'select count(1) from all_tab_cols where column_name = '''||COLUMNA||'''  and owner = '''||V_ESQUEMA||''' ';
+  EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;
+
+  IF V_NUM_TABLAS = 0 THEN            
+
+    V_MSQL2 := 'ALTER TABLE '||V_ESQUEMA||'.'||TABLA||'  ADD
+               ( '||COLUMNA||' '||TIPO||' )';
+       
+    EXECUTE IMMEDIATE V_MSQL2;
+    DBMS_OUTPUT.put_line('[[INFO]] Agregada nueva columna '||TABLA||'.'||COLUMNA||' - OK');
+
+  ELSE        
+    DBMS_OUTPUT.put_line('[[INFO]] Ya existe el la columna '||COLUMNA||'. NO SE REALIZAN CAMBIOS. ');
+  END IF ;
+
+
+
+
+  COLUMNA := 'PER_SITUACION_CONCURSAL';
+  TIPO := 'VARCHAR2(1 CHAR)';
+  V_MSQL := 'select count(1) from all_tab_cols where column_name = '''||COLUMNA||'''  and owner = '''||V_ESQUEMA||''' ';
+  EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;
+
+  IF V_NUM_TABLAS = 0 THEN            
+
+    V_MSQL2 := 'ALTER TABLE '||V_ESQUEMA||'.'||TABLA||'  ADD
+               ( '||COLUMNA||' '||TIPO||' )';
+       
+    EXECUTE IMMEDIATE V_MSQL2;
+    DBMS_OUTPUT.put_line('[[INFO]] Agregada nueva columna '||TABLA||'.'||COLUMNA||' - OK');
+
+  ELSE        
+    DBMS_OUTPUT.put_line('[[INFO]] Ya existe el la columna '||COLUMNA||'. NO SE REALIZAN CAMBIOS. ');
+  END IF ;
+
+
 
 DBMS_OUTPUT.PUT_LINE(''||TABLA||' Modificada');
 
--- sql/9.1/bankia/DDL_032_ENTITY01_ModificacionTablaRelaciones.sql
+++ sql/9.1/bankia/DDL_032_ENTITY01_ModificacionTablaRelaciones.sql
@@ -1,5 +1,5 @@
 --##########################################
---## AUTOR=GUSTAVO MORA NAVARRO
+--## AUTOR=GUSTAVO MORA NAVARRO / JOSEVI
 --## FECHA_CREACION=20150820
 --## ARTEFACTO=batch
 --## VERSION_ARTEFACTO=9.3
@@ -20,26 +20,86 @@ SET SERVEROUTPUT ON;
 
 DECLARE
 
- V_ESQUEMA VARCHAR(25) := 'BANK01';
+ V_ESQUEMA VARCHAR(25) := '#ESQUEMA#';
  TABLA VARCHAR(30) :='CPE_CONTRATOS_PERSONAS';
+ COLUMNA VARCHAR(50) :='';
+ TIPO VARCHAR(50) :='';
+ V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.   
  err_num NUMBER;
  err_msg VARCHAR2(2048); 
  V_MSQL VARCHAR2(2500 CHAR);
- V_MSQL2 VARCHAR2(2500 CHAR);
+ V_MSQL2 VARCHAR2(2500 CHAR); 
 
 
 BEGIN 
   
 
-  V_MSQL := 'ALTER TABLE '||V_ESQUEMA||'.'||TABLA||'  ADD
-             (
-                CNT_PER_EDO_CICLO_VIDA_REL VARCHAR2(40)
-              , CNT_PER_FEC_CLO_VIDA_REL DATE
-              , CNT_PER_FICHERO_CARGA VARCHAR2(200)
-            )';
+--  V_MSQL := 'ALTER TABLE '||V_ESQUEMA||'.'||TABLA||'  ADD
+--             (
+--                CNT_PER_EDO_CICLO_VIDA_REL VARCHAR2(40)
+--              , CNT_PER_FEC_CLO_VIDA_REL DATE
+--              , CNT_PER_FICHERO_CARGA VARCHAR2(200)
+--            )';
 
-     
-  EXECUTE IMMEDIATE V_MSQL;
+
+  COLUMNA := 'CNT_PER_EDO_CICLO_VIDA_REL';
+  TIPO := 'VARCHAR2(40)';
+  V_MSQL := 'select count(1) from all_tab_cols where column_name = '''||COLUMNA||'''  and owner = '''||V_ESQUEMA||''' ';
+  EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;
+
+  IF V_NUM_TABLAS = 0 THEN            
+
+    V_MSQL2 := 'ALTER TABLE '||V_ESQUEMA||'.'||TABLA||'  ADD
+               ( '||COLUMNA||' '||TIPO||' )';
+       
+    EXECUTE IMMEDIATE V_MSQL2;
+    DBMS_OUTPUT.put_line('[INFO] Agregada nueva columna '||TABLA||'.'||COLUMNA||' - OK');
+
+  ELSE        
+    DBMS_OUTPUT.put_line('[INFO] Ya existe el la columna '||COLUMNA||'. NO SE REALIZAN CAMBIOS. ');
+  END IF ;
+
+
+
+
+  COLUMNA := 'CNT_PER_FEC_CLO_VIDA_REL';
+  TIPO := 'DATE';
+  V_MSQL := 'select count(1) from all_tab_cols where column_name = '''||COLUMNA||'''  and owner = '''||V_ESQUEMA||''' ';
+  EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;
+
+  IF V_NUM_TABLAS = 0 THEN            
+
+    V_MSQL2 := 'ALTER TABLE '||V_ESQUEMA||'.'||TABLA||'  ADD
+               ( '||COLUMNA||' '||TIPO||' )';
+       
+    EXECUTE IMMEDIATE V_MSQL2;
+    DBMS_OUTPUT.put_line('[INFO] Agregada nueva columna '||TABLA||'.'||COLUMNA||' - OK');
+
+  ELSE        
+    DBMS_OUTPUT.put_line('[INFO] Ya existe el la columna '||COLUMNA||'. NO SE REALIZAN CAMBIOS. ');
+  END IF ;
+
+
+
+
+  COLUMNA := 'CNT_PER_FICHERO_CARGA';
+  TIPO := 'VARCHAR2(200)';
+  V_MSQL := 'select count(1) from all_tab_cols where column_name = '''||COLUMNA||'''  and owner = '''||V_ESQUEMA||''' ';
+  EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;
+
+  IF V_NUM_TABLAS = 0 THEN            
+
+    V_MSQL2 := 'ALTER TABLE '||V_ESQUEMA||'.'||TABLA||'  ADD
+               ( '||COLUMNA||' '||TIPO||' )';
+       
+    EXECUTE IMMEDIATE V_MSQL2;
+    DBMS_OUTPUT.put_line('[INFO] Agregada nueva columna '||TABLA||'.'||COLUMNA||' - OK');
+
+  ELSE        
+    DBMS_OUTPUT.put_line('[INFO] Ya existe el la columna '||COLUMNA||'. NO SE REALIZAN CAMBIOS. ');
+  END IF ;
+
+
 
 DBMS_OUTPUT.PUT_LINE(''||TABLA||' Modificada');
 

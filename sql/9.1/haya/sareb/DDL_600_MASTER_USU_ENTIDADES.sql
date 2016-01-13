9622c5bca903316a2abb1fa04a233564a2425eab
Ignacio <ignacio.arcos@pfsgroup.es>
2016-01-13 16:57:59 +0100

    HR-1675 añadir grants para multientidad
    Former-commit-id: 138ce674b717ac60814c989b32b9f501aa33c951

---
+++ sql/9.1/haya/sareb/DDL_600_MASTER_USU_ENTIDADES.sql
@@ -0,0 +1,64 @@
+--/*
+--##########################################
+--## AUTOR=Nacho Arcos
+--## FECHA_CREACION=20160113
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.1.12-bk
+--## INCIDENCIA_LINK=HR-1675
+--## PRODUCTO=SI
+--## Finalidad: DDL
+--##           
+--## INSTRUCCIONES: Configurar las variables necesarias en el principio del DECLARE
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+
+--Para permitir la visualización de texto en un bloque PL/SQL utilizando DBMS_OUTPUT.PUT_LINE
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF;
+
+
+DECLARE
+
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar    
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.  
+    V_NUM_SEQ NUMBER(16); -- Vble. para validar la existencia de una secuencia.  
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+
+    V_TABLA VARCHAR2(50 CHAR) := 'USU_ENTIDADES';
+
+BEGIN
+	
+	-- ejecutamos grants --
+    V_MSQL := 'GRANT ALL ON '||V_ESQUEMA_M||'.USU_ENTIDADES TO '||V_ESQUEMA; 			
+	EXECUTE IMMEDIATE V_MSQL;
+	DBMS_OUTPUT.PUT_LINE('[INFO] Grant ejecutado correctamente.');
+	
+COMMIT;
+
+
+EXCEPTION
+     WHEN OTHERS THEN 
+         DBMS_OUTPUT.PUT_LINE('KO!');
+          err_num := SQLCODE;
+          err_msg := SQLERRM;
+
+          DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(err_num));
+          DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+          DBMS_OUTPUT.put_line(err_msg);
+
+          ROLLBACK;
+          RAISE;          
+
+END;
+
+/
+
+EXIT	

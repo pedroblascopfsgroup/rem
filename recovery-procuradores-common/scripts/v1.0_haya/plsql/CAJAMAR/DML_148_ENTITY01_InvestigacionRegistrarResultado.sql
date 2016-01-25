e14440f5ca07915ba44172da2cdf8ddab36efd73
oscar <oscar.dorado@pfsgroup.es>
2016-01-25 17:01:26 +0100

    sql - T. de investigación judicial
    Former-commit-id: e3d3aa5a8fe1adf1b2bd155182a63cdbf79bdbc7

--- recovery-procuradores-common/scripts/v1.0_haya/plsql/CAJAMAR/DML_148_ENTITY01_InvestigacionRegistrarResultado.sql
+++ recovery-procuradores-common/scripts/v1.0_haya/plsql/CAJAMAR/DML_148_ENTITY01_InvestigacionRegistrarResultado.sql
@@ -1,11 +1,11 @@
 --/*
 --##########################################
---## AUTOR=DANIEL GUTIERREZ
---## FECHA_CREACION=20150707
+--## AUTOR=OSCAR DORADO
+--## FECHA_CREACION=20160118
 --## ARTEFACTO=online
---## VERSION_ARTEFACTO=9.1.0-X
---## INCIDENCIA_LINK=MITCDD-2068
---## PRODUCTO=SI
+--## VERSION_ARTEFACTO=9.1
+--## INCIDENCIA_LINK=PRODUCTO-585
+--## PRODUCTO=NO
 --##
 --## Finalidad: Realiza las inserciones de la resolución Registrar resultado de investigación para la tarea H044_RegistrarResultadoInvestigacion.
 --## INSTRUCCIONES: 
@@ -43,7 +43,7 @@ DECLARE
     */
     V_TR_ID VARCHAR2(16 CHAR):= 			'348';
     V_TR_CODIGO VARCHAR2(25 CHAR):= 		'R_REG_RES_INV';
-    V_TR_DESCRIPCION  VARCHAR2(100 CHAR):=	'Averiguación patrimonial';
+    V_TR_DESCRIPCION  VARCHAR2(100 CHAR):=	'Documento recibido de cada organismo';
     V_TJ_CODIGO VARCHAR2(20 CHAR):=			'INJ';
     V_TAC_CODIGO VARCHAR2(20 CHAR):=		'ADVANCE'; -- ADVANCE, INFO, etc.
     

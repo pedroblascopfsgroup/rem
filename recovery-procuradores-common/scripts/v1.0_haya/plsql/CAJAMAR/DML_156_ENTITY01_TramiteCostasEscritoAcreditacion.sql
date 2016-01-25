d4892705d8664a47ee3e1727fc85e8b233e7303b
oscar <oscar.dorado@pfsgroup.es>
2016-01-25 14:02:23 +0100

    sql - T. de costas contra entidad
    Former-commit-id: 6ab3d4116f0115919dcd236e7c05b0b7596a6b49

--- recovery-procuradores-common/scripts/v1.0_haya/plsql/CAJAMAR/DML_156_ENTITY01_TramiteCostasEscritoAcreditacion.sql
+++ recovery-procuradores-common/scripts/v1.0_haya/plsql/CAJAMAR/DML_156_ENTITY01_TramiteCostasEscritoAcreditacion.sql
@@ -1,11 +1,11 @@
 --/*
 --##########################################
---## AUTOR=CARLOS GIL
---## FECHA_CREACION=20150623
+--## AUTOR=OSCAR DORADO
+--## FECHA_CREACION=20160118
 --## ARTEFACTO=online
---## VERSION_ARTEFACTO=9.1.0-X
---## INCIDENCIA_LINK=MITCDD-2068
---## PRODUCTO=SI
+--## VERSION_ARTEFACTO=9.1
+--## INCIDENCIA_LINK=PRODUCTO-585
+--## PRODUCTO=NO
 --##
 --## Finalidad: Realiza las inserciones de la resolución Escrito sellado acreditando consignacion para la tarea H032_RegistrarPago.
 --## INSTRUCCIONES: 
@@ -43,7 +43,7 @@ DECLARE
     */
     V_TR_ID VARCHAR2(16 CHAR):= 			'356';
     V_TR_CODIGO VARCHAR2(25 CHAR):= 		'R_TR_COS_ENT_ACRE_CON';
-    V_TR_DESCRIPCION  VARCHAR2(100 CHAR):=	'Escrito sellado acreditando consignacion';
+    V_TR_DESCRIPCION  VARCHAR2(100 CHAR):=	'Presentación justificante de pago';
     V_TJ_CODIGO VARCHAR2(20 CHAR):=			'TCE';
     V_TAC_CODIGO VARCHAR2(20 CHAR):=		'ADVANCE'; -- ADVANCE, INFO, etc.
     

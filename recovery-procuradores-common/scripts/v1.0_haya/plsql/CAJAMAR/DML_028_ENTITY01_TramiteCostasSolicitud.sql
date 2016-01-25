cf28000a652961b4c44fba5bb7f136eca2402125
oscar <oscar.dorado@pfsgroup.es>
2016-01-25 13:55:54 +0100

    sql - T. de costas
    Former-commit-id: f9ca6b0ce0335db344509fec912901696deef0f2

--- recovery-procuradores-common/scripts/v1.0_haya/plsql/CAJAMAR/DML_028_ENTITY01_TramiteCostasSolicitud.sql
+++ recovery-procuradores-common/scripts/v1.0_haya/plsql/CAJAMAR/DML_028_ENTITY01_TramiteCostasSolicitud.sql
@@ -1,11 +1,11 @@
 --/*
 --##########################################
---## AUTOR=CARLOS GIL
---## FECHA_CREACION=20150623
+--## AUTOR=OSCAR DORADO
+--## FECHA_CREACION=20160118
 --## ARTEFACTO=online
---## VERSION_ARTEFACTO=9.1.0-X
---## INCIDENCIA_LINK=MITCDD-2068
---## PRODUCTO=SI
+--## VERSION_ARTEFACTO=9.1
+--## INCIDENCIA_LINK=PRODUCTO-585
+--## PRODUCTO=NO
 --##
 --## Finalidad: Realiza las inserciones de la resolución Escrito sellado solicitando tasacion de costas y liquidacion de intereses para la tarea H007_SolicitudTasacion.
 --## INSTRUCCIONES: 
@@ -43,7 +43,7 @@ DECLARE
     */
     V_TR_ID VARCHAR2(16 CHAR):= 			'227';
     V_TR_CODIGO VARCHAR2(25 CHAR):= 		'R_TR_SOL_TAS_COST';
-    V_TR_DESCRIPCION  VARCHAR2(100 CHAR):=	'Escrito sellado solicitando tasacion de costas y liquidacion de intereses';
+    V_TR_DESCRIPCION  VARCHAR2(100 CHAR):=	'Solicitud costas letrado';
     V_TJ_CODIGO VARCHAR2(20 CHAR):=			'TCS';
     V_TAC_CODIGO VARCHAR2(20 CHAR):=		'ADVANCE'; -- ADVANCE, INFO, etc.
     
--- recovery-procuradores-common/scripts/v1.0_haya/plsql/CAJAMAR/DML_029_ENTITY01_TramiteCostasTasacionCostas.sql
+++ recovery-procuradores-common/scripts/v1.0_haya/plsql/CAJAMAR/DML_029_ENTITY01_TramiteCostasTasacionCostas.sql
@@ -1,11 +1,11 @@
 --/*
 --##########################################
---## AUTOR=CARLOS GIL
---## FECHA_CREACION=20150623
+--## AUTOR=OSCAR DORADO
+--## FECHA_CREACION=20160118
 --## ARTEFACTO=online
---## VERSION_ARTEFACTO=9.1.0-X
---## INCIDENCIA_LINK=MITCDD-2068
---## PRODUCTO=SI
+--## VERSION_ARTEFACTO=9.1
+--## INCIDENCIA_LINK=PRODUCTO-585
+--## PRODUCTO=NO
 --##
 --## Finalidad: Realiza las inserciones de la resolución Tasación de costas del juzgado para la tarea H007_TasacionCostas.
 --## INSTRUCCIONES: 
@@ -43,7 +43,7 @@ DECLARE
     */
     V_TR_ID VARCHAR2(16 CHAR):= 			'228';
     V_TR_CODIGO VARCHAR2(25 CHAR):= 		'R_TAS_COST_JUZG';
-    V_TR_DESCRIPCION  VARCHAR2(100 CHAR):=	'Tasación de costas del juzgado';
+    V_TR_DESCRIPCION  VARCHAR2(100 CHAR):=	'Tasación del juzgado';
     V_TJ_CODIGO VARCHAR2(20 CHAR):=			'TCS';
     V_TAC_CODIGO VARCHAR2(20 CHAR):=		'ADVANCE'; -- ADVANCE, INFO, etc.
     

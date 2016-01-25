9964cb6b09770055174a49a62b3bb89bb43e09d8
Alberto Soler <alberto.soler@pfsgroup.es>
2016-01-25 11:58:04 +0100

    T. de moratoria de posesión
    Former-commit-id: d1b7dd0d43585f12cc172114318e5ef7675406c3

--- recovery-procuradores-common/scripts/v1.0_haya/plsql/CAJAMAR/DML_053_ENTITY01_MoratoriaSolicitud.sql
+++ recovery-procuradores-common/scripts/v1.0_haya/plsql/CAJAMAR/DML_053_ENTITY01_MoratoriaSolicitud.sql
@@ -1,11 +1,11 @@
 --/*
 --##########################################
---## AUTOR=CARLOS GIL
---## FECHA_CREACION=20150623
+--## AUTOR=ALBERTO SOLER
+--## FECHA_CREACION=20150125
 --## ARTEFACTO=online
---## VERSION_ARTEFACTO=9.1.0-X
---## INCIDENCIA_LINK=MITCDD-2068
---## PRODUCTO=SI
+--## VERSION_ARTEFACTO=9.1
+--## INCIDENCIA_LINK=PRODUCTO-585
+--## PRODUCTO=NO
 --##
 --## Finalidad: Realiza las inserciones de la resolución Registrar solicitud de moratoria para la tarea H011_RegistrarSolicitudMoratoria.
 --## INSTRUCCIONES: 
@@ -43,7 +43,7 @@ DECLARE
     */
     V_TR_ID VARCHAR2(16 CHAR):= 			'252';
     V_TR_CODIGO VARCHAR2(25 CHAR):= 		'R_REG_SOL_MRT';
-    V_TR_DESCRIPCION  VARCHAR2(100 CHAR):=	'Registrar solicitud de moratoria';
+    V_TR_DESCRIPCION  VARCHAR2(100 CHAR):=	'Solcitud de moratoria posesión por contrario';
     V_TJ_CODIGO VARCHAR2(20 CHAR):=			'MRL';
     V_TAC_CODIGO VARCHAR2(20 CHAR):=		'ADVANCE'; -- ADVANCE, INFO, etc.
     

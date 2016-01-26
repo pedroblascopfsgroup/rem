a141ed4c91f99dd27740e52c6c75e3abbe76a506
oscar <oscar.dorado@pfsgroup.es>
2016-01-26 12:41:07 +0100

    sql - Trámite de certificación de cargas
    Former-commit-id: 33640dd67c41a9cf69f97083712ecd56314966f5

--- recovery-procuradores-common/scripts/v1.0_haya/plsql/CAJAMAR/DML_088_ENTITY01_TramiteCargasSolicitarCertificadoCargas.sql
+++ recovery-procuradores-common/scripts/v1.0_haya/plsql/CAJAMAR/DML_088_ENTITY01_TramiteCargasSolicitarCertificadoCargas.sql
@@ -1,11 +1,11 @@
 --/*
 --##########################################
---## AUTOR=CARLOS GIL
---## FECHA_CREACION=20150623
+--## AUTOR=OSCAR DORADO
+--## FECHA_CREACION=20160118
 --## ARTEFACTO=online
---## VERSION_ARTEFACTO=9.1.0-X
---## INCIDENCIA_LINK=MITCDD-2068
---## PRODUCTO=SI
+--## VERSION_ARTEFACTO=9.1
+--## INCIDENCIA_LINK=PRODUCTO-585
+--## PRODUCTO=NO
 --##
 --## Finalidad: Realiza las inserciones de la resolución Certificación de dominio de cargas para la tarea H030_SolicitudCertificacion.
 --## INSTRUCCIONES: 
@@ -43,7 +43,7 @@ DECLARE
     */
     V_TR_ID VARCHAR2(16 CHAR):= 			'287';
     V_TR_CODIGO VARCHAR2(25 CHAR):= 		'R_CER_DOM_CAR';
-    V_TR_DESCRIPCION  VARCHAR2(100 CHAR):=	'Certificación de dominio de cargas';
+    V_TR_DESCRIPCION  VARCHAR2(100 CHAR):=	'Escrito solicitando certificación de cargas';
     V_TJ_CODIGO VARCHAR2(20 CHAR):=			'CCR';
     V_TAC_CODIGO VARCHAR2(20 CHAR):=		'ADVANCE'; -- ADVANCE, INFO, etc.
     

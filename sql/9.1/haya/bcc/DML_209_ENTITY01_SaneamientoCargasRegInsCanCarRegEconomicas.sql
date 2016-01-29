e5440195b42baeba485e755f4324aa289fa3558d
jorge.martin <jorge.martin@pfsgroup.es>
2016-01-29 09:52:16 +0100

    PRODUCTO-585 fix sql codigo + de 25 chars - Saneamiento de cargas
    Former-commit-id: 09c1f7d062d51af2b2b357a63fd78d8ce8bda8f0

--- sql/9.1/haya/bcc/DML_209_ENTITY01_SaneamientoCargasRegInsCanCarRegEconomicas.sql
+++ sql/9.1/haya/bcc/DML_209_ENTITY01_SaneamientoCargasRegInsCanCarRegEconomicas.sql
@@ -42,12 +42,12 @@ DECLARE
     ## En el input introducirá la cadena 'Input ' seguida del valor definido en la variable: V_TR_DESCRIPCION.
     */
     V_TR_ID VARCHAR2(16 CHAR):= 			'429';
-    V_TR_CODIGO VARCHAR2(25 CHAR):= 		'R_REG_INS_CAN_CAR_ECO_REG_ECO';
+    V_TR_CODIGO VARCHAR2(25 CHAR):= 		'R_REG_INS_CC_ECO_REG_ECO';
     V_TR_DESCRIPCION  VARCHAR2(100 CHAR):=	'Inscripción cancelación de cargas económicas';
     V_TJ_CODIGO VARCHAR2(20 CHAR):=			'SCR';
     V_TAC_CODIGO VARCHAR2(20 CHAR):=		'ADVANCE'; -- ADVANCE, INFO, etc.
     
-    V_TIN_CODIGO VARCHAR2(50 CHAR):=		'I_REG_INS_CAN_CAR_ECO_REG_ECO';
+    V_TIN_CODIGO VARCHAR2(50 CHAR):=		'I_REG_INS_CC_ECO_REG_ECO';
     
     V_TPO_CODIGO VARCHAR2(20 CHAR):=		'H008';
     V_NODO VARCHAR2(50 CHAR):=				'H008_RegInsCancelacionCargasEconomicas';

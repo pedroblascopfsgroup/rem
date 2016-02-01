6bd3edf1fd4125c8fdd7d922333b8fe411011add
Pedro Sebastián <pedro.sebastian@pfsgroup.es>
2016-02-01 17:56:04 +0100

    añado carga informacional cajamar
    Former-commit-id: f62a70c587e6bd3370edc528edaeb3ab12755f00

---
+++ sql/9.1/cajamar/informacional/procedures/DWH/TRUNCAR_DIM_BIEN.sql
@@ -0,0 +1,57 @@
+create or replace PROCEDURE TRUNCAR_DIM_BIEN (error OUT VARCHAR2) AS
+-- ===============================================================================================
+-- Autor: Gonzalo Martín, PFS Group
+-- Fecha creación: Febrero 2014
+-- Responsable ultima modificacion: Jaime Sánchez-Cuenca, PFS Group
+-- Fecha ultima modificacion: 02/12/2015
+-- Motivos del cambio: CMREC - 1220 : Desarrollo - Informes específicos CM - Bienes y Subastas
+-- Cliente: Recovery BI CAJAMAR
+--
+-- Descripción: Procedimiento almancenado que trunca las tablas de la dimensión Asunto.
+-- ===============================================================================================
+
+V_NOMBRE VARCHAR2(50) := 'TRUNCAR_DIM_BIEN';
+V_SQL VARCHAR2(16000);
+  
+BEGIN
+
+  --Log_Proceso
+  execute immediate 'BEGIN INSERTAR_LOG_PROCESO(:NOMBRE_PROCESO, :DESCRIPCION, :TAB); END;' USING IN V_NOMBRE, 'Empieza ' || V_NOMBRE, 2;
+  
+
+  V_SQL :=  'BEGIN OPERACION_DDL.DDL_TABLE(''TRUNCATE'', ''D_BIE_ENTIDAD_ADJUDICATARIA'', '''', :O_ERROR_STATUS); END;';
+ execute immediate V_SQL USING OUT error;
+  V_SQL :=  'BEGIN OPERACION_DDL.DDL_TABLE(''TRUNCATE'', ''D_BIE_CODIGO_ACTIVO'', '''', :O_ERROR_STATUS); END;';
+ execute immediate V_SQL USING OUT error;
+  V_SQL :=  'BEGIN OPERACION_DDL.DDL_TABLE(''TRUNCATE'', ''D_BIE_ADJ_CESION_REM'', '''', :O_ERROR_STATUS); END;';
+ execute immediate V_SQL USING OUT error;
+  V_SQL :=  'BEGIN OPERACION_DDL.DDL_TABLE(''TRUNCATE'', ''D_BIE_ADJUDICADO'', '''', :O_ERROR_STATUS); END;';
+ execute immediate V_SQL USING OUT error;
+  V_SQL :=  'BEGIN OPERACION_DDL.DDL_TABLE(''TRUNCATE'', ''D_BIE_POBLACION'', '''', :O_ERROR_STATUS); END;';
+ execute immediate V_SQL USING OUT error;
+  V_SQL :=  'BEGIN OPERACION_DDL.DDL_TABLE(''TRUNCATE'', ''D_BIE_SUBTIPO_BIEN'', '''', :O_ERROR_STATUS); END;';
+ execute immediate V_SQL USING OUT error;
+  V_SQL :=  'BEGIN OPERACION_DDL.DDL_TABLE(''TRUNCATE'', ''D_BIE_TIPO_BIEN'', '''', :O_ERROR_STATUS); END;';  
+ execute immediate V_SQL USING OUT error;
+  V_SQL :=  'BEGIN OPERACION_DDL.DDL_TABLE(''TRUNCATE'', ''D_BIE'', '''', :O_ERROR_STATUS); END;';
+ execute immediate V_SQL USING OUT error;
+  V_SQL :=  'BEGIN OPERACION_DDL.DDL_TABLE(''TRUNCATE'', ''D_BIE_FASE_ACTUAL_DETALLE'', '''', :O_ERROR_STATUS); END;';
+ execute immediate V_SQL USING OUT error;
+  V_SQL :=  'BEGIN OPERACION_DDL.DDL_TABLE(''TRUNCATE'', ''D_BIE_DESC_LANZAMIENTOS'', '''', :O_ERROR_STATUS); END;';
+ execute immediate V_SQL USING OUT error;
+  V_SQL :=  'BEGIN OPERACION_DDL.DDL_TABLE(''TRUNCATE'', ''D_BIE_PRIMER_TITULAR'', '''', :O_ERROR_STATUS); END;';
+ execute immediate V_SQL USING OUT error;
+  V_SQL :=  'BEGIN OPERACION_DDL.DDL_TABLE(''TRUNCATE'', ''D_BIE_NUM_OPERACION'', '''', :O_ERROR_STATUS); END;';
+ execute immediate V_SQL USING OUT error;
+  V_SQL :=  'BEGIN OPERACION_DDL.DDL_TABLE(''TRUNCATE'', ''D_BIE_ZONA'', '''', :O_ERROR_STATUS); END;';
+ execute immediate V_SQL USING OUT error;
+  V_SQL :=  'BEGIN OPERACION_DDL.DDL_TABLE(''TRUNCATE'', ''D_BIE_OFICINA'', '''', :O_ERROR_STATUS); END;';
+ execute immediate V_SQL USING OUT error;
+  V_SQL :=  'BEGIN OPERACION_DDL.DDL_TABLE(''TRUNCATE'', ''D_BIE_ENTIDAD'', '''', :O_ERROR_STATUS); END;';
+ execute immediate V_SQL USING OUT error;
+
+  commit;
+  --Log_Proceso
+  execute immediate 'BEGIN INSERTAR_Log_Proceso(:NOMBRE_PROCESO, :DESCRIPCION, :TAB); END;' USING IN V_NOMBRE, 'Termina ' || V_NOMBRE, 2;
+  
+END TRUNCAR_DIM_BIEN;

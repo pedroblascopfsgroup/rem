280f32f1ff3cd73ec4f4879e422d3d59c88e43f7
Pedro Sebastián <pedro.sebastian@pfsgroup.es>
2016-02-02 12:21:07 +0100

    corrijo script de etl
    Former-commit-id: 615034f21dfb29cf3f1b7fd16d3120d41eace7e9

--- batch-shell/dist/shells/cajamar/carga_bi.sh
+++ batch-shell/dist/shells/cajamar/carga_bi.sh
@@ -1,4 +1,31 @@
-#!/bin/sh
-cd `dirname $0`
- ROOT_PATH=`pwd`
- java -Xms512M -Xmx1536M -Dconfig.dir=/recovery/batch-server/programas/etl/config -Dconfig.file.mask=config.ini -Duser.country=ES -Duser.languaje=es -cp $ROOT_PATH/../lib/talendcsv.jar:$ROOT_PATH/../lib/talend-oracle-timestamptz.jar:$ROOT_PATH/../lib/dom4j-1.6.1.jar:$ROOT_PATH/../lib/ojdbc6.jar:$ROOT_PATH/../lib/tns.jar:$ROOT_PATH/../lib/jakarta-oro-2.0.8.jar:$ROOT_PATH/../lib/rec-encrypt-1.1.0-SNAPSHOT-jar-with-dependencies.jar:$ROOT_PATH/../lib/talend_file_enhanced_20070724.jar:$ROOT_PATH:$ROOT_PATH/../lib/systemRoutines.jar::$ROOT_PATH/../lib/userRoutines.jar::.:$ROOT_PATH/carga_bi_2_11.jar:$ROOT_PATH/loadspsdwh_0_7.jar:$ROOT_PATH/loaddatastaging_0_1.jar:$ROOT_PATH/loaddatawarehouse_0_6.jar:$ROOT_PATH/updatestatisticsdstg_0_1.jar:$ROOT_PATH/loadspsdstg_0_7.jar:$ROOT_PATH/testconnection_0_1.jar: cargar_bi.carga_bi_2_11.carga_bi --context=Default "$@" 
+#!/bin/bash
+# Generado automaticamente a las mié jul 23 13:32:51 CEST 2014
+ 
+
+
+filename=$(basename $0)
+nameETL="${filename%.*}"
+
+export DIR_ETL=$DIR_BASE_ETL/$nameETL
+export DIR_CONFIG=$DIR_BASE_ETL/config/
+export CFG_FILE=config.ini
+export MAINSH="$nameETL"_run.sh
+
+cd "$DIR_ETL" &> /dev/null
+if [ $? -ne 0 ] ; then
+   echo "$(basename $0) Error en $filename: directorio inexistente $DIR_ETL"
+   exit 1
+fi
+
+if [ -f $MAINSH ]; then
+    CLASS="$(cat $MAINSH | grep "^ java" | cut -f10 -d" ")"
+    CLASS2=`echo $CLASS | sed -e 's/$ROOT_PATH/./g'`
+    CLASEINICIO="$(cat $MAINSH | grep "^ java" | cut -f11 -d" ")"
+    java -Xms512M -Xmx1536M -Dconfig.dir=$DIR_CONFIG -Dconfig.file.mask=$CFG_FILE -Duser.country=ES -Duser.language=es -cp $CLASS2 $CLASEINICIO --context=Default "$@"
+    exit $?
+else
+    echo "$(basename $0) Error en $filename: no se ha encontrado  $MAINSH"
+    exit 1
+fi
+
+

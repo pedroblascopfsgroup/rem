1f05dd4bf982751f05d12dac65e1cbd4efe08360
rachel <raquel.moreno@pfsgroup.es>
2016-02-04 12:27:28 +0100

    SHELLs Correccion APR_ANA_MAN
    Former-commit-id: 09efc8ea5d117638f46a8251372532c9ed16f317

--- batch-shell/dist/shells/cajamar/apr_ana_man.sh
+++ batch-shell/dist/shells/cajamar/apr_ana_man.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-# Generado automaticamente a las mié jul 23 13:32:51 CEST 2014
- 
-WAIT_FOR_JOBS=analizePCRProduccionJob,procesoMantenimientoJob
-
-#CMD=(java -jar batch-shell.jar - - - =${ENTIDAD} "${WAIT_FOR_JOBS}")
-#"${CMD[@]}"
-#exit $?
-
-
 exit 0

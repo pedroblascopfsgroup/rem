24f7d4ba1c0746d8ca38dfa93ed8767eeac44100
rachel <raquel.moreno@pfsgroup.es>
2016-02-05 21:22:07 +0100

    SHELLs Corrección de formato
    Former-commit-id: e76fe1418456468bc574f72802f1fec908cb020f

--- batch-shell/dist/shells/cajamar/refresco_mstr.sh
+++ batch-shell/dist/shells/cajamar/refresco_mstr.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Generado manualmente
  
 cd /recovery/i-server/var/opt/MicroStrategy/bin
 ./mstrcmdmgr  -n "Recovery_BI" -u "administrator" -p "1pfsgroup" -f  "/recovery/batch-server/shells/update_cache.scp" -o "/recovery/batch-server/shells/update_cache.out"

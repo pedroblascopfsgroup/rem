82b80db8915ea9a5caa636048c069d7b5eb65de3
Gustavo Mora <gustavo.mora@pfsgroup.es>
2016-02-08 11:34:37 +0100

    parametrizacion adjudicados
    Former-commit-id: a05c0838abc8f249caf0447753bc6f5ec27799f7

--- sql/9.1/cajamar/migracion/CJM_lanza_migracion.sh
+++ sql/9.1/cajamar/migracion/CJM_lanza_migracion.sh
@@ -214,25 +214,27 @@ if [ $? != 0 ] ; then
 fi
 echo "[OK] ""$sh_dir""CMREC_1671_Gestion_asunto_por_marca_haya.sh ejecutado correctamente"   
 
-
-echo "[INFO] Comienza ejecución de: ""$sh_dir""CJM_MIGRACION_EXPEDIENTES_NOTIFICACIONES.sh"                               
-./"$sh_dir"CJM_MIGRACION_EXPEDIENTES_NOTIFICACIONES.sh "$1"   
-if [ $? != 0 ] ; then 
-  echo -e "\n\n======>>> [ERROR] en "$sh_dir"CJM_MIGRACION_EXPEDIENTES_NOTIFICACIONES.sh"
-  echo -e "\n\n======>>> [ERROR] en CJM_lanza_migracion.sh"          
-  exit 1
-fi
-echo "[OK] ""$sh_dir""CJM_MIGRACION_EXPEDIENTES_NOTIFICACIONES.sh ejecutado correctamente" 
-
-
-echo "[INFO] Comienza ejecución de: ""$sh_dir""CMREC_1510_MIGRACION_PROPUESTAS_FONDOS_PROPIOS.sh"                               
-./"$sh_dir"CMREC_1510_MIGRACION_PROPUESTAS_FONDOS_PROPIOS.sh "$1"   
-if [ $? != 0 ] ; then 
-  echo -e "\n\n======>>> [ERROR] en "$sh_dir"CMREC_1510_MIGRACION_PROPUESTAS_FONDOS_PROPIOS.sh"
-  echo -e "\n\n======>>> [ERROR] en CJM_lanza_migracion.sh"          
-  exit 1
-fi
-echo "[OK] ""$sh_dir""CMREC_1510_MIGRACION_PROPUESTAS_FONDOS_PROPIOS.sh ejecutado correctamente" 
+###########################
+## PENDIENTES DE CORRECCION 
+###########################
+#echo "[INFO] Comienza ejecución de: ""$sh_dir""CJM_MIGRACION_EXPEDIENTES_NOTIFICACIONES.sh"                               
+#./"$sh_dir"CJM_MIGRACION_EXPEDIENTES_NOTIFICACIONES.sh "$1"   
+#if [ $? != 0 ] ; then 
+#  echo -e "\n\n======>>> [ERROR] en "$sh_dir"CJM_MIGRACION_EXPEDIENTES_NOTIFICACIONES.sh"
+#  echo -e "\n\n======>>> [ERROR] en CJM_lanza_migracion.sh"          
+#  exit 1
+#fi
+#echo "[OK] ""$sh_dir""CJM_MIGRACION_EXPEDIENTES_NOTIFICACIONES.sh ejecutado correctamente" 
+#
+#
+#echo "[INFO] Comienza ejecución de: ""$sh_dir""CMREC_1510_MIGRACION_PROPUESTAS_FONDOS_PROPIOS.sh"                               
+#./"$sh_dir"CMREC_1510_MIGRACION_PROPUESTAS_FONDOS_PROPIOS.sh "$1"   
+#if [ $? != 0 ] ; then 
+#  echo -e "\n\n======>>> [ERROR] en "$sh_dir"CMREC_1510_MIGRACION_PROPUESTAS_FONDOS_PROPIOS.sh"
+#  echo -e "\n\n======>>> [ERROR] en CJM_lanza_migracion.sh"          
+#  exit 1
+#fi
+#echo "[OK] ""$sh_dir""CMREC_1510_MIGRACION_PROPUESTAS_FONDOS_PROPIOS.sh ejecutado correctamente" 
 
 
 echo "[INFO] Comienza ejecución de: ""$sh_dir""apr_main_observaciones.sh"                      

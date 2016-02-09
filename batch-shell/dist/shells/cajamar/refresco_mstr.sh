1c98c469515ac63a207a601f88dd646a67739a12
rachel <raquel.moreno@pfsgroup.es>
2016-02-09 14:57:19 +0100

    SHELLs Corrección de ruta de log
    Former-commit-id: 334072e50cbd1612dc36eb5d18b06e2e08e8b0fa

--- batch-shell/dist/shells/cajamar/refresco_mstr.sh
+++ batch-shell/dist/shells/cajamar/refresco_mstr.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
  
 cd /recovery/i-server/var/opt/MicroStrategy/bin
-./mstrcmdmgr  -n "Recovery_BI" -u "administrator" -p "1pfsgroup" -f  "/recovery/batch-server/shells/update_cache.scp" -o "/recovery/batch-server/shells/update_cache.out"
+./mstrcmdmgr  -n "Recovery_BI" -u "administrator" -p "1pfsgroup" -f  "/recovery/batch-server/shells/update_cache.scp" -o "/recovery/transferencia/control/etl/output/Logs/update_cache.out"

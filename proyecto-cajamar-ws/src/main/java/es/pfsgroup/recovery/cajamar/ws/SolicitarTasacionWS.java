e78bbe13ada18cdda7bb587dba6058290b37fafa
Alberto Campos <alberto.campos@pfsgroup.es>
2016-02-09 10:00:23 +0100

    [CMREC-1420] Pruebas del WS de alta de tasación
    Former-commit-id: f060f039cf472d7b486df5ea3f84cbad2a690391

--- proyecto-cajamar-ws/src/main/java/es/pfsgroup/recovery/cajamar/ws/SolicitarTasacionWS.java
+++ proyecto-cajamar-ws/src/main/java/es/pfsgroup/recovery/cajamar/ws/SolicitarTasacionWS.java
@@ -208,6 +208,7 @@ public class SolicitarTasacionWS extends BaseWS implements SolicitarTasacionWSAp
 		
 		// Se selecciona la persona que tiene una mayor participación. En caso de igualdad, el primer registro
 		Long nPersona = null;
+		String codEntidad = "";
 		if(bien.getPersonas() != null) {
 			
 			float participacion = -1;
@@ -215,14 +216,18 @@ public class SolicitarTasacionWS extends BaseWS implements SolicitarTasacionWSAp
 				if(personaBien.getParticipacion() != null && personaBien.getParticipacion() > participacion) {
 					nPersona = personaBien.getPersona().getCodClienteEntidad();
 					participacion = personaBien.getParticipacion();
+					codEntidad = personaBien.getPersona().getCodigoEntidad();
 				}
 			}			
 		}
-		
-		
+				
 		String sPersona = "";
 		if(nPersona != null) {
 			sPersona = nPersona.toString();
+			
+			if(sPersona.startsWith(codEntidad)) {
+				sPersona = StringUtils.substringAfter(sPersona, codEntidad);
+			}
 		}
 		
 		logger.info("NPERSONA: " + StringUtils.substring(sPersona, 0, 10));

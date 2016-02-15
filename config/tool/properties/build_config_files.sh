96bdfd69e85538575818a20b7f79cf0cca359484
rachel <raquel.moreno@pfsgroup.es>
2016-02-15 02:10:54 +0100

    CONFIG Adaptación de properties de batch de BK
    Former-commit-id: 542ff011c9f64ab7efa1809218b1b2cc514c9bf6

--- config/tool/properties/build_config_files.sh
+++ config/tool/properties/build_config_files.sh
@@ -70,6 +70,7 @@ while read line; do
     if [[ $line =~ ^.+=.+$ ]] ; then
         KEY=`echo $line | cut -d= -f1`
         VALUE=${!KEY//\//\\/}
+        VALUE=${VALUE//:/\\\\:}
         sed -e "s/${KEY}/${VALUE}/g" -i config/tmp/batch/${cliente}/devon.properties
         sed -e "s/${KEY}/${VALUE}/g" -i config/tmp/web/${cliente}/devon.properties
     fi 

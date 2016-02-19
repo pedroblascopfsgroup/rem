6e6da36ecff44ab60a935b1f6d628dc00888cb6e
Ignacio <ignacio.arcos@pfsgroup.es>
2016-02-19 13:58:45 +0100

    PRODUCTO-711 Corregido las tareas para procurador
    Former-commit-id: c04ff63ae9abdc1aa893e57b9b67b236d3d2156d

--- proyecto-cajamar/src/main/java/es/pfsgroup/procedimientos/handlers/CostasProcuradorHandler.java
+++ proyecto-cajamar/src/main/java/es/pfsgroup/procedimientos/handlers/CostasProcuradorHandler.java
@@ -2,9 +2,11 @@ package es.pfsgroup.procedimientos.handlers;
 
 
 import org.jbpm.graph.exe.ExecutionContext;
+
 import es.capgemini.pfs.asunto.model.Procedimiento;
 import es.pfsgroup.commons.utils.Checks;
 import es.pfsgroup.procedimientos.PROBaseActionHandler;
+import es.pfsgroup.recovery.ext.impl.asunto.model.EXTAsunto;
 
 public class CostasProcuradorHandler extends PROBaseActionHandler {
 
@@ -16,10 +18,15 @@ public class CostasProcuradorHandler extends PROBaseActionHandler {
 	@Override
 	public void run(ExecutionContext executionContext) throws Exception {
 		Procedimiento prc = getProcedimiento(executionContext);
-		if(!Checks.esNulo(prc.getAsunto().getProcurador())){
-			executionContext.getToken().signal("avanzaConProcu");
-		}else{
-			executionContext.getToken().signal("avanza");
+		if(!Checks.esNulo(prc.getAsunto())){
+			if(prc.getAsunto() instanceof EXTAsunto){
+				EXTAsunto extAsunto = (EXTAsunto) prc.getAsunto();
+				if(!Checks.esNulo(extAsunto.getProcurador())){		
+					executionContext.getToken().signal("avanzaConProcu");
+				}else{
+					executionContext.getToken().signal("avanza");
+				}
+			}
 		}
 	}
 

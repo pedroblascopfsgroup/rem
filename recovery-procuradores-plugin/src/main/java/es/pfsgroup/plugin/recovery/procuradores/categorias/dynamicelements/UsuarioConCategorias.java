5c2614c2cee9ecd4994a2bf90e168338c7b5305d
jorge.martin <jorge.martin@pfsgroup.es>
2016-02-23 18:51:28 +0100

    PRODUCTO-726 Ocultar Subcategorías
    Former-commit-id: 80db5f0d86b4fe7aaed70d1b7bf7bbe4ec4e20fc

--- recovery-procuradores-plugin/src/main/java/es/pfsgroup/plugin/recovery/procuradores/categorias/dynamicelements/UsuarioConCategorias.java
+++ recovery-procuradores-plugin/src/main/java/es/pfsgroup/plugin/recovery/procuradores/categorias/dynamicelements/UsuarioConCategorias.java
@@ -1,30 +1,40 @@
 package es.pfsgroup.plugin.recovery.procuradores.categorias.dynamicelements;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
+import es.capgemini.devon.pagination.Page;
 import es.capgemini.pfs.core.api.usuario.UsuarioApi;
-import es.capgemini.pfs.users.domain.Usuario;
 import es.pfsgroup.plugin.recovery.procuradores.categorias.api.CategoriaApi;
-import es.pfsgroup.plugin.recovery.procuradores.categorias.model.Categoria;
+import es.pfsgroup.plugin.recovery.procuradores.categorias.dto.CategoriaDto;
+import es.pfsgroup.plugin.recovery.procuradores.configuracion.api.ConfiguracionDespachoExternoApi;
 import es.pfsgroup.plugins.web.menu.DynamicElementLogic;
 
 public class UsuarioConCategorias implements DynamicElementLogic {
 
 	@Autowired
 	private UsuarioApi usuarioApi;
-	
+
 	@Autowired
 	private CategoriaApi categoriaApi;
-	
+
+	@Autowired
+	private ConfiguracionDespachoExternoApi configuracionDespachoExternoApi;
+
 	@Override
 	public Boolean validar() { 
-		
-		Usuario u = usuarioApi.getUsuarioLogado();
-		List<Categoria> lista = categoriaApi.getListaTotalCategorias(u.getId());
-		
-		return (lista != null && lista.size() > 0);
+		CategoriaDto dto = new CategoriaDto();
+		dto.setLimit(1);
+		dto.setStart(0);
+		dto.setIdcategorizacion(Long.valueOf(-1));
+
+		Long categorizacionActivaDelUser = configuracionDespachoExternoApi.activoCategorizacion();
+		if(categorizacionActivaDelUser != null){
+			dto.setIdcategorizacion(categorizacionActivaDelUser);
+		}
+
+		Page pageCategorias = categoriaApi.getListaCategorias(dto);
+
+		return (pageCategorias != null && pageCategorias.getResults() != null &&  pageCategorias.getResults().size() > 0);
 	}
 
 }

1b76c774a0f0b7da5ab25ad05dc205d41661ba05
alejandro <alejandro.inigo@pfsgroup.es>
2016-02-24 09:44:17 +0100

    permisos tabla zona_jerarquia to dts
    Former-commit-id: 8c6a4d53c75597c9d03767c4ab7bccb27a011232

---
+++ sql/9.1/bankia/DDL_036_BANK01_LANZAR_ZONA_JERARQUIA.sql
@@ -0,0 +1,49 @@
+/*
+--##########################################
+--## AUTOR=Alejandro I�igo
+--## FECHA_CREACION=20160223
+--## ARTEFACTO=ETL
+--## VERSION_ARTEFACTO=1.00
+--## INCIDENCIA_LINK=BKREC-1718
+--## PRODUCTO=NO
+--##########################################
+--*/
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
+SET DEFINE OFF;
+
+DECLARE
+
+    V_MSQL VARCHAR2(32000 CHAR); /* Sentencia a ejecutar    */
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; /* Configuracion Esquema*/
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; /* Configuracion Esquema Master*/
+    V_TS_INDEX VARCHAR2(25 CHAR):= '#TABLESPACE_INDEX#'; /* Configuracion Indice*/
+    V_SQL VARCHAR2(4000 CHAR); /* Vble. para consulta que valida la existencia de una tabla.*/
+    V_NUM_TABLAS NUMBER(16); /* Vble. para validar la existencia de una tabla.  */
+    ERR_NUM NUMBER(25);  /* Vble. auxiliar para registrar errores en el script.*/
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.*/
+
+    V_TEXT1 VARCHAR2(2400 CHAR); /* Vble. auxiliar*/
+    
+    V_ESQUEMA_MIN VARCHAR2(25 CHAR):= '#ESQUEMA_MINIREC#'; /* Configuracion Esquema minirec*/
+    V_ESQUEMA_DWH VARCHAR2(25 CHAR):= '#ESQUEMA_DWH#'; /* Configuracion Esquema recovery_bankia_dwh*/
+    V_ESQUEMA_STG VARCHAR2(25 CHAR):= '#ESQUEMA_STG#'; /* Configuracion Esquema recovery_bankia_datastage*/
+	
+BEGIN
+
+    /*  tabla APR_AUX_JERARQUIAS_RECHAZOS <-- ELIMINAR */
+
+  DBMS_OUTPUT.PUT_LINE('[START] TABLA ZONA_JERARQUIA OK');
+
+  EXECUTE IMMEDIATE 'GRANT SELECT ON ' || V_ESQUEMA || '.ZONA_JERARQUIA TO RECOVERY_BANKIA_DATASTAGE';      
+  
+  DBMS_OUTPUT.PUT_LINE('[FIN] TABLA ZONA_JERARQUIA OK');
+
+EXCEPTION
+     WHEN OTHERS THEN
+       DBMS_OUTPUT.PUT_LINE('[ERROR] Se ha producido un error en la ejecución: '|| TO_CHAR(SQLCODE) || ' ' || SQLERRM );
+    RAISE;
+END;
+/
+exit

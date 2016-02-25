1373cdea6e13bf5fa81a627758c23077de19b73b
daniel <daniel.esteve@pfsgroup.es>
2016-02-25 17:55:11 +0100

    corregido error sintaxis DML_136
    Former-commit-id: 3dde9df34143e08c637439410f1ebda4c795eb0f

--- sql/9.1/bankia/bankia_solo_produccion/DML_136_BANK01_Update_RCF_AGE_AGENCIAS.sql
+++ sql/9.1/bankia/bankia_solo_produccion/DML_136_BANK01_Update_RCF_AGE_AGENCIAS.sql
@@ -19,7 +19,7 @@ SET SERVEROUTPUT ON;
 SET DEFINE OFF;
 
 DECLARE
-    V_ESQUEMA VARCHAR2(25 CHAR):= 'BANK01'; -- Configuracion Esquema
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
     V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
     seq_count number(3); -- Vble. para validar la existencia de las Secuencias.
     table_count number(3); -- Vble. para validar la existencia de las Tablas.

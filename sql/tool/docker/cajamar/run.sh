9458fd548a03f0c6ee9f532a1caf834059875086
rachel <raquel.moreno@pfsgroup.es>
2016-02-26 13:37:31 +0100

    FIX Bug de Oracle (CI)
    Former-commit-id: cad1f13a9dd1d2365fe070ed5ae6703d4c4247e3

--- sql/tool/docker/cajamar/run.sh
+++ sql/tool/docker/cajamar/run.sh
@@ -219,7 +219,9 @@ function package_sql () {
         cp $PREVIOUS_SCRIPTS_DIR/DML_009_MASTER_CARGA_TODAS_FUNCIONES.sql $ws_package_dir/
         cp $PREVIOUS_SCRIPTS_DIR/DML_010_ENTITY_CARGA_TODOS_PERFILES.sql $ws_package_dir/
 
-        sed -e 's/NUEVO_DD_SCL_ID/NUEVO_DD_SCL_ID, ROWNUM/g' -i $ws_package_dir/**/**/scripts/DML_382_ENTITY01_DATJPB_MergeSCE_SCL_en_PER_PERSONAS.sql
+        # FIX: Bug de la versión que tenemos de Oracle. Problema con MERGE
+        # ORA-00600: internal error code, arguments: [kkmupsViewDestFro_4], [76107],[75624], [], [], [], [], [], [], [], [], []
+        sed -e 's/NUEVO_DD_SCL_ID/NUEVO_DD_SCL_ID, ROWNUM/g' -i $ws_package_dir/**/**/**/scripts/DML_382_ENTITY01_DATJPB_MergeSCE_SCL_en_PER_PERSONAS.sql
 
 		chmod -R go+w $ws_package_dir
 		for sh in $(find $ws_package_dir -name '*.sh'); do

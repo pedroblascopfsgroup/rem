a2e43383b6dc1b72e220ecbf4b5a8e9227e0e23e
Pedro Blasco Onsurbe <pedro.blasco@pfsgroup.es>
2016-03-02 12:07:09 +0100

    GC-1178 Correcciones a errores en despliegue en PRE-Bankia
    Former-commit-id: 5fc0337f42f9d79a6219f1eeb66e34145b90b536

--- sql/9.2/producto/DDL_001_ENTITY01_CreacionPLSQLExpedSaldoIrregular.sql
+++ sql/9.2/producto/DDL_001_ENTITY01_CreacionPLSQLExpedSaldoIrregular.sql
@@ -29,20 +29,27 @@ CREATE OR REPLACE PROCEDURE EXP_SALDO_IRREGULAR AUTHID CURRENT_USER IS
     ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
 
 	V_SQL_INSERT VARCHAR2(4000 CHAR)  := 'INSERT INTO #ESQUEMA#.TAR_TAREAS_NOTIFICACIONES (TAR_ID, EXP_ID, DD_EST_ID, DD_EIN_ID, DD_STA_ID, TAR_CODIGO, TAR_TAREA, TAR_DESCRIPCION, TAR_FECHA_INI, TAR_EMISOR, USUARIOCREAR, FECHACREAR) ' ||
-		' VALUES (#ESQUEMA.S_TAR_TAREAS_NOTIFICACIONES.NEXTVAL, :1, :2, :3, :4, :5, :6, :7, SYSDATE, :8, :9, SYSDATE) ';
+		' VALUES (#ESQUEMA#.S_TAR_TAREAS_NOTIFICACIONES.NEXTVAL, :1, :2, :3, :4, :5, :6, :7, SYSDATE, :8, :9, SYSDATE) ';
 
 	CURSOR C_CONSULTA IS
-		select exp.exp_id EXP_ID
-		from exp_expedientes exp inner join
-		cex_contratos_expediente cex on cex.exp_id = exp.exp_id inner join
-		mov_movimientos mov on mov.cnt_id = cex.cnt_id and mov.MOV_FECHA_EXTRACCION = (select max(mov_fecha_extraccion) from mov_movimientos)
-		where
-		mov.MOV_FECHA_EXTRACCION > exp.fechacrear and 
-		mov.mov_pos_viva_vencida > 0 and
-		(select mov_pos_viva_vencida 
-		from mov_movimientos movi 
-		where movi.cnt_id = cex.cnt_id and
-		movi.MOV_FECHA_EXTRACCION < mov.MOV_FECHA_EXTRACCION) = 0;
+		select exp.exp_id
+			from #ESQUEMA#.exp_expedientes exp
+			inner join #ESQUEMA#.cex_contratos_expediente cex
+			on cex.exp_id = exp.exp_id
+			inner join #ESQUEMA#.mov_movimientos mov
+			on mov.cnt_id                = cex.cnt_id
+			and mov.MOV_FECHA_EXTRACCION =
+			  (select max(mov_fecha_extraccion)
+			  from #ESQUEMA#.mov_movimientos
+			  )
+			where mov.MOV_FECHA_EXTRACCION > exp.fechacrear
+			and mov.mov_pos_viva_vencida   > 0
+			and nvl(
+			  (select min(mov_pos_viva_vencida)
+			  from #ESQUEMA#.mov_movimientos movi
+			  where movi.cnt_id             = cex.cnt_id
+			  and movi.MOV_FECHA_EXTRACCION < mov.MOV_FECHA_EXTRACCION
+			  ),1)                          = 0;
 	V_CONS C_CONSULTA%ROWTYPE;
 
 	V_DD_EST_ID NUMBER(16,0);

acfdf36ea264cb273d4c33f03da2ab1b3d8db090
Enrique Jimenez <enrique.jimenez@pfsgroup.es>
2016-03-03 10:49:23 +0100

    Incluimos los cambios/parches de Transiciones Automáticas y Correcciones Asuntos Sin Nombre en HAYA-BCC.
    Former-commit-id: 4fcbbc8e9f5a1a4d7a1be2ad956e9ab7ce107abf

---
+++ sql/9.1/haya/bcc/migracion/shells/CMREC-2339_Correccion_asuntos_sin_nombre_HAYA02.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo "Parametros: <CM01_pass@host:puerto/ORACLE_SID>"
+    exit
+fi
+
+
+sql_dir="sql/"
+
+echo "INICIO DEL SCRIPT Correccion_asuntos_sin_nombre_HAYA02 $0"
+echo "########################################################"
+echo "#####    INICIO CMREC-2339_Correccion_asuntos_sin_nombre_HAYA02.sql"
+echo "########################################################"
+echo "Inicio CMREC-2339_Correccion_asuntos_sin_nombre_HAYA02.sql"
+
+$ORACLE_HOME/bin/sqlplus "$1" @"$sql_dir"CMREC-2339_Correccion_asuntos_sin_nombre_HAYA02.sql
+
+if [ $? != 0 ] ; then
+   echo -e "\n\n======>>> "Error en @"$sql_dir"CMREC-2339_Correccion_asuntos_sin_nombre_HAYA02.sql
+   exit 1
+fi
+
+
+echo "Fin CMREC-2339_Correccion_asuntos_sin_nombre_HAYA02. Revise el fichero de log"
+exit 0
---
+++ sql/9.1/haya/bcc/migracion/sql/CMREC-2339_Correccion_asuntos_sin_nombre_HAYA02.sql
@@ -0,0 +1,77 @@
+--Corrección nombres de asunto con demandados
+MERGE INTO HAYA02.ASU_ASUNTOS ASU2 USING 
+(SELECT * FROM (
+WITH MIG_NOM_ASUNTO AS (
+  SELECT CD_PROCEDIMIENTO, PER_NOM50 AS PER_NOM50, PER_DOC_ID AS PER_DOC_ID
+  FROM
+      (
+          select distinct pdem.cd_procedimiento, per.per_doc_id, per.per_nom50, tin.DD_TIN_CODIGO, CPE_ORDEN
+              , rank() over (partition by pdem.cd_procedimiento order by tin.DD_TIN_CODIGO, CPE_ORDEN ASC) RANKING
+           from  HAYA02.mig_procedimientos_demandados pdem 
+           inner join HAYA02.MIG_PROCEDIMIENTOS_OPERACIONES mpo on mpo.CD_PROCEDIMIENTO = pdem.CD_PROCEDIMIENTO
+           inner join HAYA02.CNT_CONTRATOS CNT ON CNT.CNT_CONTRATO = mpo.NUMERO_CONTRATO   
+           inner join  HAYA02.CPE_CONTRATOS_PERSONAS cpe ON cnt.cnt_id = cpe.cnt_id 
+          -- inner join  HAYA02.per_personas per on per.per_cod_cliente_entidad = pdem.CODIGO_PERSONA
+           inner join  HAYA02.per_personas per on per.per_id = cpe.per_id
+           inner join  HAYA02.DD_TIN_TIPO_INTERVENCION tin ON cpe.dd_tin_id = tin.dd_tin_id 
+          where tin.DD_TIN_TITULAR = 1 -- AND CPE.CNT_ID = 6746736
+            -- and mpo.NUMERO_CONTRATO = 34907200064946
+  )
+  WHERE RANKING = 1   
+)
+SELECT       PCAB.CD_PROCEDIMIENTO
+          ,  substr(lpad(pop.numero_contrato,16,'0') || ' | ' || per_doc_id || ' ' || per_nom50,1,50) AS NOMBRE_ASUNTO
+          ,  PCAB.ENTIDAD_PROPIETARIA
+          ,  PCAB.GESTION_PLATAFORMA
+          ,  ASU.ASU_ID
+FROM HAYA02.mig_procedimientos_cabecera pcab 
+    INNER JOIN HAYA02.mig_procedimientos_operaciones pop on pcab.cd_procedimiento = pop.cd_procedimiento 
+    INNER JOIN HAYA02.ASU_ASUNTOS ASU ON ASU.asu_id_externo = pop.CD_PROCEDIMIENTO 
+    INNER JOIN MIG_NOM_ASUNTO MNA ON MNA.CD_PROCEDIMIENTO = pop.cd_procedimiento   
+) ) TMP 
+ON (ASU2.ASU_ID = TMP.ASU_ID)
+WHEN MATCHED THEN 
+UPDATE SET ASU2.ASU_NOMBRE = TMP.NOMBRE_ASUNTO;
+          
+COMMIT;          
+
+
+
+
+MERGE INTO HAYA02.ASU_ASUNTOS ASU2 USING 
+(SELECT * FROM (
+WITH MIG_NOM_ASUNTO AS (
+  SELECT CD_PROCEDIMIENTO, PER_NOM50 AS PER_NOM50, PER_DOC_ID AS PER_DOC_ID
+  FROM
+      (
+          select distinct mpo.cd_procedimiento, per.per_doc_id, per.per_nom50, tin.DD_TIN_CODIGO, CPE_ORDEN
+              , rank() over (partition by mpo.cd_procedimiento order by tin.DD_TIN_CODIGO, CPE_ORDEN ASC) RANKING
+           from  HAYA02.MIG_PROCEDIMIENTOS_OPERACIONES mpo --on mpo.CD_PROCEDIMIENTO = pdem.CD_PROCEDIMIENTO
+           inner join HAYA02.CNT_CONTRATOS CNT ON CNT.CNT_CONTRATO = mpo.NUMERO_CONTRATO   
+           inner join  HAYA02.CPE_CONTRATOS_PERSONAS cpe ON cnt.cnt_id = cpe.cnt_id 
+           inner join  HAYA02.per_personas per on per.per_id = cpe.per_id
+           inner join  HAYA02.DD_TIN_TIPO_INTERVENCION tin ON cpe.dd_tin_id = tin.dd_tin_id 
+          where tin.DD_TIN_TITULAR = 1          
+  )
+  WHERE RANKING = 1   
+)
+SELECT       PCAB.CD_PROCEDIMIENTO
+          ,  substr(lpad(pop.numero_contrato,16,'0') || ' | ' || per_doc_id || ' ' || per_nom50,1,50) AS NOMBRE_ASUNTO
+          ,  PCAB.ENTIDAD_PROPIETARIA
+          ,  PCAB.GESTION_PLATAFORMA
+          ,  ASU.ASU_ID
+FROM HAYA02.mig_procedimientos_cabecera pcab 
+    INNER JOIN HAYA02.mig_procedimientos_operaciones pop on pcab.cd_procedimiento = pop.cd_procedimiento 
+    INNER JOIN HAYA02.ASU_ASUNTOS ASU ON ASU.asu_id_externo = pop.CD_PROCEDIMIENTO 
+    INNER JOIN MIG_NOM_ASUNTO MNA ON MNA.CD_PROCEDIMIENTO = pop.cd_procedimiento   
+    WHERE ASU.ASU_NOMBRE =  ' |  '        
+) ) TMP 
+ON (ASU2.ASU_ID = TMP.ASU_ID)
+WHEN MATCHED THEN 
+UPDATE SET ASU2.ASU_NOMBRE = TMP.NOMBRE_ASUNTO;
+          
+COMMIT;                  
+          
+          
+          
+ 

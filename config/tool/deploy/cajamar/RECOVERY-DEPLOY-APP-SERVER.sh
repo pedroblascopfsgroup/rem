1e02fc674e9ac80911433eb29d389d7cca6acede
rachel <raquel.moreno@pfsgroup.es>
2016-03-18 00:11:18 +0100

    CONFIG Update
    Former-commit-id: 40af82184dfe95b2a3aecf72b10ef957be2673ae

--- config/tool/deploy/cajamar/RECOVERY-DEPLOY-APP-SERVER.sh
+++ config/tool/deploy/cajamar/RECOVERY-DEPLOY-APP-SERVER.sh
@@ -4,7 +4,7 @@ if [ "$#" -lt 1 ]; then
     echo ""
     echo "Uso: " $0 " <entorno>"
     echo ""
-    echo "   <entorno>: desa, pre, pro01, pro02"
+    echo "   <entorno>: desa, pre, pro"
     echo "" 
     exit 1
 fi
@@ -17,5 +17,24 @@ fi
 
 LOCAL_PATH=`pwd`
 
+echo "Copiando fichero de configuración ..."
 cp config/$1/devon.properties /recovery/app-server/
+
+echo "Creando directorio para salida de burofax ..."
+mkdir -p /recovery/app-server/output/burofax
+chmod -R og+rwx /recovery/app-server/output
+
+echo "Copiando plantillas ..."
+mkdir -p /recovery/app-server/plantillas
+cp -r plantillas/* /recovery/app-server/plantillas/
+chmod -R og+rwx /recovery/app-server/plantillas
+
+echo "Copiando ficheros WAR ..."
+if [ -f war/pfs.war ]; then
+    cp war/pfs.war /wlapps/applications/PFS/
+fi
+if [ -f war/MicroStrategy.war ]; then
+    cp war/MicroStrategy.war /wlapps/applications/MS/
+fi
+
 cd $LOCAL_PATH

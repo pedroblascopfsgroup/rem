f42ee312ae030a65cc83d99004714a0a88c4d1a3
Enrique Jimenez <enrique.jimenez@pfsgroup.es>
2016-03-18 10:30:38 +0100

    Incluimos cambios requeridos en la CMREC-2799
    Former-commit-id: 8c61b1e049776118bd9eca1d105b832b5dbcaebb

--- sql/9.2/producto/DDL_001_ENTITY01_CreacionPLSQLExpedSaldoIrregular.sql
+++ sql/9.2/producto/DDL_001_ENTITY01_CreacionPLSQLExpedSaldoIrregular.sql
@@ -11,6 +11,8 @@
 --## INSTRUCCIONES: 
 --## VERSIONES:
 --##        0.1 Version inicial
+--## 	    0.2 EJD:> Cambio sobre la query C_CONSULTA que incluye " max(mov_pos_viva_vencida) ", también retorno resultado.
+--## 	    0.3 EJD:> Cambio sobre la query C_CONSULTA que incluye DD_EST_ID, que será el que se informe en la Tarea Notificación.
 --##########################################
 --*/
 
@@ -18,7 +20,7 @@ WHENEVER SQLERROR EXIT SQL.SQLCODE;
 SET SERVEROUTPUT ON;
 SET DEFINE OFF;
 
-CREATE OR REPLACE PROCEDURE EXP_SALDO_IRREGULAR AUTHID CURRENT_USER IS
+CREATE OR REPLACE PROCEDURE EXP_SALDO_IRREGULAR (RESULT_EXE OUT VARCHAR2 ) AUTHID CURRENT_USER IS
 
     V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar     
     V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
@@ -32,14 +34,14 @@ CREATE OR REPLACE PROCEDURE EXP_SALDO_IRREGULAR AUTHID CURRENT_USER IS
 		' VALUES (#ESQUEMA#.S_TAR_TAREAS_NOTIFICACIONES.NEXTVAL, :1, :2, :3, :4, :5, :6, :7, SYSDATE, :8, :9, SYSDATE) ';
 
 	CURSOR C_CONSULTA IS
-		select exp.exp_id EXP_ID
+		select exp.exp_id EXP_ID, exp.DD_EST_ID DD_EST_ID
 		from #ESQUEMA#.exp_expedientes exp inner join
 		#ESQUEMA#.cex_contratos_expediente cex on cex.exp_id = exp.exp_id inner join
 		#ESQUEMA#.mov_movimientos mov on mov.cnt_id = cex.cnt_id and mov.MOV_FECHA_EXTRACCION = (select max(mov_fecha_extraccion) from #ESQUEMA#.mov_movimientos)
 		where
 		mov.MOV_FECHA_EXTRACCION > exp.fechacrear and 
 		mov.mov_pos_viva_vencida > 0 and
-		(select mov_pos_viva_vencida 
+		(select max(movi.mov_pos_viva_vencida) 
 		from #ESQUEMA#.mov_movimientos movi 
 		where movi.cnt_id = cex.cnt_id and
 		movi.MOV_FECHA_EXTRACCION < mov.MOV_FECHA_EXTRACCION) = 0;
@@ -67,10 +69,11 @@ BEGIN
         values
           ('||V_ESQUEMA_M||'.s_dd_sta_subtipo_tarea_base.nextval, 3, ''1000'', ''Notificación Nuevo contrato impagado en expediente'', ''Notificación Nuevo contrato impagado en expediente'', sysdate, ''SAG'',1)';
         EXECUTE IMMEDIATE V_SQL ; 
+	RESULT_EXE := 'Insercción en dd_sta_subtipo_tarea_base Codigo 1000, realizada';
    END IF;
  
-   V_SQL := ' SELECT DD_EST_ID FROM ' || V_ESQUEMA_M || '.DD_EST_ESTADOS_ITINERARIOS WHERE DD_EST_CODIGO=''CMER'' ';
-   EXECUTE IMMEDIATE V_SQL INTO V_DD_EST_ID;
+   -- V_SQL := ' SELECT DD_EST_ID FROM ' || V_ESQUEMA_M || '.DD_EST_ESTADOS_ITINERARIOS WHERE DD_EST_CODIGO=''CMER'' ';
+   -- EXECUTE IMMEDIATE V_SQL INTO V_DD_EST_ID;
    V_SQL := ' SELECT DD_EIN_ID FROM ' || V_ESQUEMA_M || '.DD_EIN_ENTIDAD_INFORMACION WHERE DD_EIN_DESCRIPCION=''Expediente'' ';
    EXECUTE IMMEDIATE V_SQL INTO V_DD_EIN_ID;
    V_SQL := ' SELECT DD_STA_ID FROM ' || V_ESQUEMA_M || '.DD_STA_SUBTIPO_TAREA_BASE WHERE DD_STA_CODIGO=''1000'' ';
@@ -81,11 +84,12 @@ BEGIN
    LOOP
     	FETCH C_CONSULTA INTO V_CONS;
 		EXIT WHEN C_CONSULTA%NOTFOUND;
-		EXECUTE IMMEDIATE V_SQL_INSERT USING V_CONS.EXP_ID, V_DD_EST_ID, V_DD_EIN_ID, V_DD_STA_ID, V_TAR_CODIGO, V_TAR_TAREA, V_TAR_DESCRIPCION, V_TAR_EMISOR, V_USUARIOCREAR;
-		DBMS_OUTPUT.PUT_LINE('CREADA NOTIFICACION PARA EXPEDIENTE: ' || V_CONS.EXP_ID);
+		EXECUTE IMMEDIATE V_SQL_INSERT USING V_CONS.EXP_ID, V_CONS.DD_EST_ID, V_DD_EIN_ID, V_DD_STA_ID, V_TAR_CODIGO, V_TAR_TAREA, V_TAR_DESCRIPCION, V_TAR_EMISOR, V_USUARIOCREAR;
+		DBMS_OUTPUT.PUT_LINE('CREADA NOTIFICACION PARA EXPEDIENTE: ' || V_CONS.EXP_ID||' En estado: '||V_CONS.DD_EST_ID);
     END LOOP;
 
     COMMIT;
+    RESULT_EXE := RESULT_EXE||'| Insercción en TAR_TAREAS_NOTIFICACIONES de las notificaciones, realizadas';
 
     DBMS_OUTPUT.PUT_LINE('[FIN] '||V_ESQUEMA||' -----.... ' );
     
@@ -96,6 +100,7 @@ EXCEPTION
           DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(ERR_NUM));
           DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
           DBMS_OUTPUT.put_line(ERR_MSG);
+	  RESULT_EXE := 'Error:'||TO_CHAR(ERR_NUM)||'['||ERR_MSG||']';
           ROLLBACK;
           RAISE;   
 END;

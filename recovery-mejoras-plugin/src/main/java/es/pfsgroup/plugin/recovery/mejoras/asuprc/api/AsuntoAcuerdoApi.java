07beb2348451a043f38e85113b06641291990e2c
Alberto Soler <alberto.soler@pfsgroup.es>
2016-03-21 10:36:13 +0100

    [PRODUCTO-888] Creación de nuevo api para el tipo asunto acuerdo
    Former-commit-id: b07545cff3bfc03b6f33535b085df281c2af3571

---
+++ recovery-mejoras-plugin/src/main/java/es/pfsgroup/plugin/recovery/mejoras/asuprc/api/AsuntoAcuerdoApi.java
@@ -0,0 +1,10 @@
+package es.pfsgroup.plugin.recovery.mejoras.asuprc.api;
+
+
+public interface AsuntoAcuerdoApi {
+	
+	String BO_CORE_ASUNTO_PUEDER_VER_TAB_ACUERDOS = "core.asunto.puedeVerTabAcuerdos";
+	String BO_ASUNTO_ORIGEN= "AsuntoAcuerdoManager.asuntoOrigen";
+	String BO_EXPEDIENTE_ORIGEN= "AsuntoAcuerdoManager.expedienteOrigen";
+
+}

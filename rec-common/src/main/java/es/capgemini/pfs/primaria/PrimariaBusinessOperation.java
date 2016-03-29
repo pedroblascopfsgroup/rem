586ca63db39c933b4b4fd726c379cffa2b3a54f9
joseluis.gauxachs <joseluis.gauxachs@pfsgroup.es>
2016-03-29 17:07:14 +0200

    CMREC-2752 Modificaciones en las fechas de la pestaña cirbe
    Former-commit-id: 883113b8d9a33475b11665f5eb2858fd5fdcb1cc

--- rec-common/src/main/java/es/capgemini/pfs/primaria/PrimariaBusinessOperation.java
+++ rec-common/src/main/java/es/capgemini/pfs/primaria/PrimariaBusinessOperation.java
@@ -229,6 +229,7 @@ public final class PrimariaBusinessOperation {
      ** CirbeManager.
      ****************************************************************************/
     public static final String BO_CIRBE_MGR_GET_FECHAS_EXTRAC_PERSONA = "cirbeManager.getFechasExtraccionPersona";
+    public static final String BO_CIRBE_MGR_GET_FECHAS_ACTUALIZACION_PERSONA = "cirbeManager.getFechasActualizacionPersona";
     public static final String BO_CIRBE_MGR_GET_FECHAS_EXTRAC_PERSONA_DESDE = "cirbeManager.getFechasExtraccionPersonaDesde";
     public static final String BO_CIRBE_MGR_GET_FECHAS_EXTRAC_PERSONA_HASTA = "cirbeManager.getFechasExtraccionPersonaHasta";
     public static final String BO_CIRBE_MGR_GET_CIRBE_DATA = "cirbeManager.getCirbeData";

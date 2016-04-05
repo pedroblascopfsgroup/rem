2c173bd5336b313ac7eb72e712ff3126c8988833
jorge.martin <jorge.martin@pfsgroup.es>
2016-04-05 17:56:55 +0200

    PRODUCTO-932 - Lanzar el trámite de Provisión de fondos por el procurador
    Former-commit-id: 0f49ac4ffdf8b95817a33ea8fdf8d317ee9fdac5

--- recovery-procuradores-plugin/src/main/java/es/pfsgroup/plugin/recovery/procuradores/procedimientos/ProcedimientoProcuradorController.java
+++ recovery-procuradores-plugin/src/main/java/es/pfsgroup/plugin/recovery/procuradores/procedimientos/ProcedimientoProcuradorController.java
@@ -1,5 +1,6 @@
 package es.pfsgroup.plugin.recovery.procuradores.procedimientos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,15 +9,25 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import es.capgemini.pfs.asunto.ProcedimientoManager;
+import es.capgemini.pfs.asunto.model.DDTipoActuacion;
 import es.capgemini.pfs.asunto.model.DDTipoReclamacion;
 import es.capgemini.pfs.asunto.model.Procedimiento;
 import es.capgemini.pfs.core.api.procedimiento.ProcedimientoApi;
+import es.capgemini.pfs.decisionProcedimiento.DecisionProcedimientoManager;
+import es.capgemini.pfs.decisionProcedimiento.dto.DtoDecisionProcedimiento;
+import es.capgemini.pfs.decisionProcedimiento.model.DDEstadoDecision;
+import es.capgemini.pfs.decisionProcedimiento.model.DecisionProcedimiento;
 import es.capgemini.pfs.despachoExterno.model.DDTipoDespachoExterno;
 import es.capgemini.pfs.despachoExterno.model.DespachoExterno;
 import es.capgemini.pfs.despachoExterno.model.GestorDespacho;
+import es.capgemini.pfs.persona.model.Persona;
+import es.capgemini.pfs.procedimientoDerivado.dto.DtoProcedimientoDerivado;
 import es.capgemini.pfs.users.domain.Usuario;
 import es.pfsgroup.commons.utils.Checks;
 import es.pfsgroup.commons.utils.api.ApiProxyFactory;
+import es.pfsgroup.plugin.recovery.mejoras.decisionProcedimiento.MEJDecisionProcedimientoManager;
+import es.pfsgroup.plugin.recovery.mejoras.decisionProcedimiento.dto.MEJDtoDecisionProcedimiento;
 import es.pfsgroup.plugin.recovery.procuradores.categorias.model.Categoria;
 import es.pfsgroup.plugin.recovery.procuradores.configuracion.api.ConfiguracionDespachoExternoApi;
 import es.pfsgroup.plugin.recovery.procuradores.procurador.api.ProcuradorApi;
@@ -38,7 +49,13 @@ public class ProcedimientoProcuradorController {
 	
 	@Autowired
 	private ConfiguracionDespachoExternoApi configuracionDespachoExternoApi;
-	
+
+	@Autowired
+	private ProcedimientoManager procedimientoApi;
+
+	@Autowired
+	private MEJDecisionProcedimientoManager mejDecisionProcedimientoManager;
+
 	public static final String VISTA_EDIT_PROCEDIMIENTO_PROCURADOR = "plugin/procuradores/procurador/editaCabeceraProcedimiento";
 	
 	/**
@@ -104,6 +121,50 @@ public class ProcedimientoProcuradorController {
 		
 		return false;
 	}
-	
 
+	/**
+	 * PRODUCTO-932 - Lanzar el trámite de Provisión de fondos por el procurador
+	 * 
+	 * @param idProcedimiento
+	 * @return
+	 */
+	@RequestMapping
+	public String lanzarTramiteProvisionFondos(Long idProcedimiento) {
+		Procedimiento procedimiento = procedimientoApi.getProcedimiento(idProcedimiento);
+
+		DtoProcedimientoDerivado procedimientoDervidadoDto = new DtoProcedimientoDerivado();
+		procedimientoDervidadoDto.setId(procedimiento.getId());
+		procedimientoDervidadoDto.setProcedimientoPadre(procedimiento.getId());
+		procedimientoDervidadoDto.setTipoReclamacion(procedimiento.getTipoReclamacion().getCodigo());
+		procedimientoDervidadoDto.setTipoActuacion("TR");
+		procedimientoDervidadoDto.setTipoProcedimiento("HC103");
+		procedimientoDervidadoDto.setPorcentajeRecuperacion(procedimiento.getPorcentajeRecuperacion());
+		procedimientoDervidadoDto.setPlazoRecuperacion(procedimiento.getPlazoRecuperacion());
+		procedimientoDervidadoDto.setSaldoRecuperacion(procedimiento.getSaldoRecuperacion());
+
+		List<Long> personasList = new ArrayList<Long>();
+		for (Persona persona : procedimiento.getPersonasAfectadas()) {
+			personasList.add(persona.getId());
+		}
+
+		if (personasList.size() > 0) {
+			Long[] idPersonas = new Long[personasList.size()];
+			idPersonas = personasList.toArray(idPersonas);
+			procedimientoDervidadoDto.setPersonas(idPersonas);
+		}
+
+		List<DtoProcedimientoDerivado> procedimientosDerivadosList = new ArrayList<DtoProcedimientoDerivado>();
+		procedimientosDerivadosList.add(procedimientoDervidadoDto);
+
+		MEJDtoDecisionProcedimiento decisionProcedimientoDto = new MEJDtoDecisionProcedimiento();
+		decisionProcedimientoDto.setIdProcedimiento(idProcedimiento);
+		decisionProcedimientoDto.setStrEstadoDecision(DDEstadoDecision.ESTADO_ACEPTADO);	
+		decisionProcedimientoDto.setProcedimientosDerivados(procedimientosDerivadosList);
+		decisionProcedimientoDto.setDecisionProcedimiento(new DecisionProcedimiento());
+		
+		mejDecisionProcedimientoManager.aceptarPropuesta(decisionProcedimientoDto);
+
+		return "default";
+	}
+	
 }

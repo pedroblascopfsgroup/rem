2ec6f67e617e51bf01fd7a2c87a67f4e84c2ac17
josemanuel.perez <josemanuel.perez@pfsgroup.es>
2016-04-11 13:56:31 +0200

    info ejecución
    Former-commit-id: 9394ed3e2dfbeff066a1b0eaf83dc93e5903c11f

--- config/tool/deploy/haya/sareb/RECOVERY-DEPLOY-BATCH-SERVER.sh
+++ config/tool/deploy/haya/sareb/RECOVERY-DEPLOY-BATCH-SERVER.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-#if [ "$#" -lt 1 ]; then
-#    echo ""
-#    echo "Uso: " $0 " <entorno>"
-#    echo ""
-#    echo "   <entorno>: val2, pre, pro"
-#    echo "" 
-#    exit 1
-#fi
+if [ "$#" -lt 1 ]; then
+    echo ""
+    echo "Uso: " $0 " <entorno>"
+    echo ""
+    echo "   <entorno>: val2, pre, pro"
+    echo "" 
+    exit 1
+fi
 
-#if [[ ! -f config/$1/devon.properties ]] ; then
-#    echo ""
-#    echo "ERROR: no existe el fichero config/$1/devon.properties"
-#    exit 1
-#fi
+if [[ ! -f config/$1/devon.properties ]] ; then
+    echo ""
+    echo "ERROR: no existe el fichero config/$1/devon.properties"
+    exit 1
+fi
 
 LOCAL_PATH=`pwd`
 DIR_BASE=/recovery/haya

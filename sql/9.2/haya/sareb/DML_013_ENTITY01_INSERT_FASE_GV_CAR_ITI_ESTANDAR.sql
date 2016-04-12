309ff80c9fab5bcb17ab98fa3cb253c2623fe9a2
Kevin Fernandez <kevin.fernandez@pfsgroup.es>
2016-04-12 16:54:29 +0200

    PRODUCTO-796, corregido un fallo de comprobacion de variables
    Former-commit-id: e05626e9271caeed2b844e9df5533b3e832c5bb6

--- sql/9.2/haya/sareb/DML_013_ENTITY01_INSERT_FASE_GV_CAR_ITI_ESTANDAR.sql
+++ sql/9.2/haya/sareb/DML_013_ENTITY01_INSERT_FASE_GV_CAR_ITI_ESTANDAR.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=Kevin Fernández
---## FECHA_CREACION=20160412
+--## FECHA_CREACION=20160408
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.2.0-hy-rc01
 --## INCIDENCIA_LINK=PRODUCTO-796
@@ -43,84 +43,96 @@ DECLARE
 BEGIN
 
 	-- Encontrar los ids para DD_TIT_ID y DD_AEX_ID
-	V_MSQL := 'SELECT DD_TIT_ID FROM '||V_ESQUEMA_M||'.DD_TIT_TIPO_ITINERARIOS WHERE DD_TIT_CODIGO = ''DEU''';
-	EXECUTE IMMEDIATE V_MSQL INTO V_DD_TIT_ID;
-	
-	V_MSQL := 'SELECT DD_AEX_ID FROM '||V_ESQUEMA_M||'.DD_AEX_AMBITOS_EXPEDIENTE WHERE DD_AEX_CODIGO = ''PPGRA''';
-	EXECUTE IMMEDIATE V_MSQL INTO V_DD_AEX_ID;
-		
-		
-	-- Comprobar si existe el itinerario de tipo Gestión de deuda con nombre Estándar
-	V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.ITI_ITINERARIOS WHERE ITI_NOMBRE = ''Estándar'' AND borrado = 0 AND DD_TIT_ID = '||V_DD_TIT_ID||'';
+	V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA_M||'.DD_TIT_TIPO_ITINERARIOS WHERE DD_TIT_CODIGO = ''DEU'' AND borrado = 0';
 	EXECUTE IMMEDIATE V_SQL INTO V_NUM;
-	
 	IF V_NUM > 0 THEN
-		V_SQL := 'SELECT ITI_ID FROM '||V_ESQUEMA||'.ITI_ITINERARIOS WHERE ITI_NOMBRE = ''Estándar'' AND borrado = 0 AND DD_TIT_ID = '||V_DD_TIT_ID||' AND ROWNUM = 1';
-		EXECUTE IMMEDIATE V_SQL INTO V_ITI;
-
-		-- Comprobar si existe el perfil de gestión para los nuevos estados del itinerario
-		V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.PEF_PERFILES WHERE PEF_CODIGO = ''GESTDEUDA''';
+		V_MSQL := 'SELECT DD_TIT_ID FROM '||V_ESQUEMA_M||'.DD_TIT_TIPO_ITINERARIOS WHERE DD_TIT_CODIGO = ''DEU'' AND borrado = 0';
+		EXECUTE IMMEDIATE V_MSQL INTO V_DD_TIT_ID;
+		
+		V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA_M||'.DD_AEX_AMBITOS_EXPEDIENTE WHERE DD_AEX_CODIGO = ''PPGRA'' AND borrado = 0';
 		EXECUTE IMMEDIATE V_SQL INTO V_NUM;
 		IF V_NUM > 0 THEN
-			V_SQL := 'SELECT PEF_ID FROM '||V_ESQUEMA||'.PEF_PERFILES WHERE PEF_CODIGO = ''GESTDEUDA''';
-			EXECUTE IMMEDIATE V_SQL INTO V_PEF_GESTOR;
+			V_MSQL := 'SELECT DD_AEX_ID FROM '||V_ESQUEMA_M||'.DD_AEX_AMBITOS_EXPEDIENTE WHERE DD_AEX_CODIGO = ''PPGRA'' AND borrado = 0';
+			EXECUTE IMMEDIATE V_MSQL INTO V_DD_AEX_ID;
 			
-			-- Ahora creamos los registros para enlazar los estados a este nuevo itinerario
-			-- CAR Carencia
-			V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA_M||'.DD_EST_ESTADOS_ITINERARIOS WHERE DD_EST_CODIGO=''CAR'' AND DD_EIN_ID = 1 AND borrado = 0';
+			-- Comprobar si existe el itinerario de tipo Gestión de deuda con nombre Estándar
+			V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.ITI_ITINERARIOS WHERE ITI_NOMBRE = ''Estándar'' AND borrado = 0';
 			EXECUTE IMMEDIATE V_SQL INTO V_NUM;
-			IF V_NUM > 0 THEN
-				V_SQL := 'SELECT DD_EST_ID FROM '||V_ESQUEMA_M||'.DD_EST_ESTADOS_ITINERARIOS WHERE DD_EST_CODIGO=''CAR'' AND DD_EIN_ID = 1 AND borrado = 0';
-				EXECUTE IMMEDIATE V_SQL INTO V_DD_EST;
-				V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.EST_ESTADOS WHERE ITI_ID = '||V_ITI||' AND DD_EST_ID = '||V_DD_EST||' AND BORRADO = 0';
-				EXECUTE IMMEDIATE V_SQL INTO V_NUM;
-				IF V_NUM = 0 THEN		
-					V_SQL := 'SELECT '||V_ESQUEMA||'.S_EST_ESTADOS.NEXTVAL FROM DUAL';
-					EXECUTE IMMEDIATE V_SQL INTO V_EST_CAR;
-					V_MSQL := 'INSERT INTO '||V_ESQUEMA||'.EST_ESTADOS (EST_ID, PEF_ID_GESTOR, PEF_ID_SUPERVISOR, ITI_ID, DD_EST_ID, EST_TELECOBRO, EST_PLAZO, VERSION, USUARIOCREAR, FECHACREAR, BORRADO) 
-								VALUES ('||V_EST_CAR||', '||V_PEF_GESTOR||', '||V_PEF_GESTOR||', '||V_ITI||', '||V_DD_EST||', 0, 0, 0, ''DML'', SYSDATE, 0)';
-					EXECUTE IMMEDIATE V_MSQL;
-					DBMS_OUTPUT.PUT_LINE('[INFO] Insertado estado Carencia al itinerario: Estándar.');
-				ELSE
-					V_SQL := 'SELECT EST_ID FROM '||V_ESQUEMA||'.EST_ESTADOS WHERE ITI_ID = '||V_ITI||' AND DD_EST_ID = '||V_DD_EST||' AND BORRADO = 0 AND ROWNUM = 1';
-					EXECUTE IMMEDIATE V_SQL INTO V_EST_CAR;
-					DBMS_OUTPUT.PUT_LINE('[INFO] Ya estaba insertado el estado Carencia al itinerario: Estándar con id, EST_ID: '||V_EST_CAR);
-				END IF;
-			ELSE
-				DBMS_OUTPUT.PUT_LINE('[INFO] NO se ha encontrado el estado itinerario Carencia(CAR). No se puede asociar la fase al itinerario..');
-			END IF;
 			
-			-- GV Gestión de vencidos
-			V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA_M||'.DD_EST_ESTADOS_ITINERARIOS WHERE DD_EST_CODIGO=''GV'' AND DD_EIN_ID = 1 AND borrado = 0';
-			EXECUTE IMMEDIATE V_SQL INTO V_NUM;
 			IF V_NUM > 0 THEN
-				V_SQL := 'SELECT DD_EST_ID FROM '||V_ESQUEMA_M||'.DD_EST_ESTADOS_ITINERARIOS WHERE DD_EST_CODIGO=''GV'' AND DD_EIN_ID = 1  AND borrado = 0';
-				EXECUTE IMMEDIATE V_SQL INTO V_DD_EST;
-				V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.EST_ESTADOS WHERE ITI_ID = '||V_ITI||' AND DD_EST_ID = '||V_DD_EST||' AND BORRADO = 0';
+				V_SQL := 'SELECT ITI_ID FROM '||V_ESQUEMA||'.ITI_ITINERARIOS WHERE ITI_NOMBRE = ''Estándar'' AND DD_TIT_ID = '||V_DD_TIT_ID||' AND borrado = 0 AND ROWNUM = 1';
+				EXECUTE IMMEDIATE V_SQL INTO V_ITI;
+		
+				-- Comprobar si existe el perfil de gestión para los nuevos estados del itinerario
+				V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.PEF_PERFILES WHERE PEF_CODIGO = ''GESTDEUDA'' AND borrado = 0';
 				EXECUTE IMMEDIATE V_SQL INTO V_NUM;
-				IF V_NUM = 0 THEN		
-					V_SQL := 'SELECT '||V_ESQUEMA||'.S_EST_ESTADOS.NEXTVAL FROM DUAL';
-					EXECUTE IMMEDIATE V_SQL INTO V_EST_GV;
-					V_MSQL := 'INSERT INTO '||V_ESQUEMA||'.EST_ESTADOS (EST_ID, PEF_ID_GESTOR, PEF_ID_SUPERVISOR, ITI_ID, DD_EST_ID, EST_TELECOBRO, EST_PLAZO, VERSION, USUARIOCREAR, FECHACREAR, BORRADO) 
-								VALUES ('||V_EST_GV||', '||V_PEF_GESTOR||', '||V_PEF_GESTOR||', '||V_ITI||', '||V_DD_EST||', 0, 0, 0, ''DML'', SYSDATE, 0)';
-					EXECUTE IMMEDIATE V_MSQL;
-					DBMS_OUTPUT.PUT_LINE('[INFO] Insertado estado Gestión de vencidos al itinerario: Estándar.');
-				ELSE
-					V_SQL := 'SELECT EST_ID FROM '||V_ESQUEMA||'.EST_ESTADOS WHERE ITI_ID = '||V_ITI||' AND DD_EST_ID = '||V_DD_EST||' AND BORRADO = 0 AND ROWNUM = 1';
-					EXECUTE IMMEDIATE V_SQL INTO V_EST_GV;
-					DBMS_OUTPUT.PUT_LINE('[INFO] Ya estaba insertado el estado Gestión de vencidos al itinerario: Estándar con id, EST_ID: '||V_EST_GV);
+				IF V_NUM > 0 THEN
+					V_SQL := 'SELECT PEF_ID FROM '||V_ESQUEMA||'.PEF_PERFILES WHERE PEF_CODIGO = ''GESTDEUDA'' AND borrado = 0';
+					EXECUTE IMMEDIATE V_SQL INTO V_PEF_GESTOR;
+					
+					-- Ahora creamos los registros para enlazar los estados a este nuevo itinerario
+					-- CAR Carencia
+					V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA_M||'.DD_EST_ESTADOS_ITINERARIOS WHERE DD_EST_CODIGO=''CAR'' AND DD_EIN_ID = 1 AND borrado = 0';
+					EXECUTE IMMEDIATE V_SQL INTO V_NUM;
+					IF V_NUM > 0 THEN
+						V_SQL := 'SELECT DD_EST_ID FROM '||V_ESQUEMA_M||'.DD_EST_ESTADOS_ITINERARIOS WHERE DD_EST_CODIGO=''CAR'' AND DD_EIN_ID = 1 AND borrado = 0';
+						EXECUTE IMMEDIATE V_SQL INTO V_DD_EST;
+						V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.EST_ESTADOS WHERE ITI_ID = '||V_ITI||' AND DD_EST_ID = '||V_DD_EST||' AND borrado = 0';
+						EXECUTE IMMEDIATE V_SQL INTO V_NUM;
+						IF V_NUM = 0 THEN		
+							V_SQL := 'SELECT '||V_ESQUEMA||'.S_EST_ESTADOS.NEXTVAL FROM DUAL';
+							EXECUTE IMMEDIATE V_SQL INTO V_EST_CAR;
+							V_MSQL := 'INSERT INTO '||V_ESQUEMA||'.EST_ESTADOS (EST_ID, PEF_ID_GESTOR, PEF_ID_SUPERVISOR, ITI_ID, DD_EST_ID, EST_TELECOBRO, EST_PLAZO, VERSION, USUARIOCREAR, FECHACREAR, BORRADO) 
+										VALUES ('||V_EST_CAR||', '||V_PEF_GESTOR||', '||V_PEF_GESTOR||', '||V_ITI||', '||V_DD_EST||', 0, 0, 0, ''DML'', SYSDATE, 0)';
+							EXECUTE IMMEDIATE V_MSQL;
+							DBMS_OUTPUT.PUT_LINE('[INFO] Insertado estado Carencia al itinerario: Estándar.');
+						ELSE
+							V_SQL := 'SELECT EST_ID FROM '||V_ESQUEMA||'.EST_ESTADOS WHERE ITI_ID = '||V_ITI||' AND DD_EST_ID = '||V_DD_EST||' AND borrado = 0 AND ROWNUM = 1';
+							EXECUTE IMMEDIATE V_SQL INTO V_EST_CAR;
+							DBMS_OUTPUT.PUT_LINE('[INFO] Ya estaba insertado el estado Carencia al itinerario: Estándar con id, EST_ID: '||V_EST_CAR);
+						END IF;
+					ELSE
+						DBMS_OUTPUT.PUT_LINE('[ERROR] NO se ha encontrado el estado itinerario Carencia(CAR). No se ha registrado el estado de carencia en el nuevo itinerario.');
+					END IF;
+					
+					-- GV Gestión de vencidos
+					V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA_M||'.DD_EST_ESTADOS_ITINERARIOS WHERE DD_EST_CODIGO=''GV'' AND DD_EIN_ID = 1 AND borrado = 0';
+					EXECUTE IMMEDIATE V_SQL INTO V_NUM;
+					IF V_NUM > 0 THEN
+						V_SQL := 'SELECT DD_EST_ID FROM '||V_ESQUEMA_M||'.DD_EST_ESTADOS_ITINERARIOS WHERE DD_EST_CODIGO=''GV'' AND DD_EIN_ID = 1 AND borrado = 0';
+						EXECUTE IMMEDIATE V_SQL INTO V_DD_EST;
+						V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.EST_ESTADOS WHERE ITI_ID = '||V_ITI||' AND DD_EST_ID = '||V_DD_EST||' AND borrado = 0';
+						EXECUTE IMMEDIATE V_SQL INTO V_NUM;
+						IF V_NUM = 0 THEN		
+							V_SQL := 'SELECT '||V_ESQUEMA||'.S_EST_ESTADOS.NEXTVAL FROM DUAL';
+							EXECUTE IMMEDIATE V_SQL INTO V_EST_GV;
+							V_MSQL := 'INSERT INTO '||V_ESQUEMA||'.EST_ESTADOS (EST_ID, PEF_ID_GESTOR, PEF_ID_SUPERVISOR, ITI_ID, DD_EST_ID, EST_TELECOBRO, EST_PLAZO, VERSION, USUARIOCREAR, FECHACREAR, BORRADO) 
+										VALUES ('||V_EST_GV||', '||V_PEF_GESTOR||', '||V_PEF_GESTOR||', '||V_ITI||', '||V_DD_EST||', 0, 0, 0, ''DML'', SYSDATE, 0)';
+							EXECUTE IMMEDIATE V_MSQL;
+							DBMS_OUTPUT.PUT_LINE('[INFO] Insertado estado Gestión de vencidos al itinerario: Estándar.');
+						ELSE
+							V_SQL := 'SELECT EST_ID FROM '||V_ESQUEMA||'.EST_ESTADOS WHERE ITI_ID = '||V_ITI||' AND DD_EST_ID = '||V_DD_EST||' AND borrado = 0 AND ROWNUM = 1';
+							EXECUTE IMMEDIATE V_SQL INTO V_EST_GV;
+							DBMS_OUTPUT.PUT_LINE('[INFO] Ya estaba insertado el estado Gestión de vencidos al itinerario: Estándar con id, EST_ID: '||V_EST_GV);
+						END IF;
+					ELSE
+						DBMS_OUTPUT.PUT_LINE('[ERROR] NO se ha encontrado el estado itinerario Gestion de Vencidos(GV). No se ha registrado el estado gestion de vencidos en el nuevo itinerario.');
+					END IF;
+					
+				ELSE		
+					DBMS_OUTPUT.PUT_LINE('[ERROR] NO existe el perfil gestor deuda(GESTDEUDA). No se puede continuar..');
 				END IF;
+		
 			ELSE
-				DBMS_OUTPUT.PUT_LINE('[INFO] NO se ha encontrado el estado itinerario Gestion de Vencidos(GV). No se puede asociar la fase al itinerario..');
+				DBMS_OUTPUT.PUT_LINE('[ERROR] NO existe un itinerario con nombre Estándar. No se puede continuar..');
 			END IF;
-			
-		ELSE		
-			DBMS_OUTPUT.PUT_LINE('[INFO] NO existe el perfil gestor deuda(GESTDEUDA). No se puede continuar..');
+		ELSE
+			DBMS_OUTPUT.PUT_LINE('[ERROR] NO se ha encontrado el ambito de expediente necesario. No se puede continuar..');
 		END IF;
-
 	ELSE
-		DBMS_OUTPUT.PUT_LINE('[INFO] NO existe un itinerario con nombre Estándar. No se puede continuar..');
-	END IF;		
+		DBMS_OUTPUT.PUT_LINE('[ERROR] NO se ha encontrado el tipo de itinerario de deuda. No se puede continuar..');
+	END IF;
+	
 
     COMMIT;
 	

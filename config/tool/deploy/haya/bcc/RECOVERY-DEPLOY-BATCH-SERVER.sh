2ad58f5bd440111f018633f222ef538d7c71f82c
rachel <raquel.moreno@pfsgroup.es>
2016-04-13 19:18:22 +0200

    CONFIG Shell para despliegue completo de HAYA-BCC
    Former-commit-id: 691260b2063e2528d9673a9c0c64e0b45c414420

--- config/tool/deploy/haya/bcc/RECOVERY-DEPLOY-BATCH-SERVER.sh
+++ config/tool/deploy/haya/bcc/RECOVERY-DEPLOY-BATCH-SERVER.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
-#if [ "$#" -lt 1 ]; then
-#    echo ""
-#    echo "Uso: " $0 " <entorno>"
-#    echo ""
-#    echo "   <entorno>: desa, pre, pro01, pro02"
-#    echo "" 
-#    exit 1
-#fi
-#
-#if [[ ! -f config/$1/devon.properties ]] ; then
-#    echo ""
-#    echo "ERROR: no existe el fichero config/$1/devon.properties"
-#    exit 1
-#fi
+if [ "$#" -lt 1 ]; then
+    echo ""
+    echo "Uso: " $0 " <entorno>"
+    echo ""
+    echo "   <entorno>: val01, pre, pro"
+    echo "" 
+    exit 1
+fi
+
+if [[ ! -f config/$1/devon.properties ]] ; then
+    echo ""
+    echo "ERROR: no existe el fichero config/$1/devon.properties"
+    exit 1
+fi
 
 LOCAL_PATH=`pwd`
-BASE_DIR=/recovery/haya/batch-server/bcc
+BASE_DIR=/recovery/haya/batch-server
 
-#cp config/$1/devon.properties $BASE_DIR/
-#cp config/$1/config.ini $BASE_DIR/programas/etl/config/
-#unzip zip/batch*.zip
-#rm -rf $BASE_DIR/programas/batch/*
-#cp -r batch/* $BASE_DIR/programas/batch/
-#chmod -R a+rwx $BASE_DIR/programas/batch/*
-#cd $LOCAL_PATH
-rm -f $BASE_DIR/shells/*.sh
-cp scripts/shells/* $BASE_DIR/shells/
-#sed -e 's/ENTORNO/$1/g' -i $BASE_DIR/shells/unzip-messages-to-queue.sh
-#sed -e 's/ENTORNO/$1/g' -i $BASE_DIR/shells/zip-messages-from-queue.sh
-chmod a+rx $BASE_DIR/shells/*.sh
-rm -rf $BASE_DIR/programas/etl/apr_*
-rm -rf $BASE_DIR/programas/etl/APR_*
-cp etl/* $BASE_DIR/programas/etl/
-cd $BASE_DIR/programas/etl/
+cp config/$1/devon.properties $BASE_DIR/
+cp config/$1/config.ini $BASE_DIR/bcc/programas/etl/config/
+unzip zip/batch*.zip
+rm -rf $BASE_DIR/batch/*
+cp -r batch/* $BASE_DIR/batch/
+chmod -R a+rwx $BASE_DIR/batch/*
+cd $LOCAL_PATH
+rm -f $BASE_DIR/bcc/shells/*.sh
+cp scripts/shells/* $BASE_DIR/bcc/shells/
+sed -e 's/ENTORNO/$1/g' -i $BASE_DIR/shells/unzip-messages-to-queue.sh
+sed -e 's/ENTORNO/$1/g' -i $BASE_DIR/shells/zip-messages-from-queue.sh
+chmod a+rx $BASE_DIR/bcc/shells/*.sh
+rm -rf $BASE_DIR/bcc/programas/etl/apr_*
+rm -rf $BASE_DIR/bcc/programas/etl/APR_*
+cp etl/* $BASE_DIR/bcc/programas/etl/
+cd $BASE_DIR/bcc/programas/etl/
 for etl in `ls *.zip`
 do
     ZIPNAME=`echo ${etl} | cut -d- -f1`
@@ -45,5 +45,5 @@ do
     chmod -fR a+rwx ${directory}
 done
 rm *.zip
-#cd $LOCAL_PATH
-#cp scripts/batch/*.sh $BASE_DIR/programas/
+cd $LOCAL_PATH
+cp scripts/batch/*.sh $BASE_DIR/

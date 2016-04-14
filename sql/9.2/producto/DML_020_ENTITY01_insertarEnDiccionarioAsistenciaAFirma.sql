1c69468e92d9b819988b2e3fe062a8ab4881806c
Oscar Dorado <oscar.dorado@pfsgroup.es>
2016-04-14 10:16:32 +0200

    PRODUCTO-1093 Fichero adjuntos obligatorios - inserción en el diccionario
    Former-commit-id: a7efe97c3a60286a09654432c2f20435bb963949

--- sql/9.2/producto/DML_020_ENTITY01_insertarEnDiccionarioAsistenciaAFirma.sql
+++ sql/9.2/producto/DML_020_ENTITY01_insertarEnDiccionarioAsistenciaAFirma.sql
@@ -29,6 +29,7 @@ DECLARE
    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
 
+<<<<<<< HEAD:sql/9.2/producto/DML_020_ENTITY01_insertarEnDiccionarioAsistenciaAFirma.sql
    V_DDNAME VARCHAR2(50 CHAR):= 'DD_AAF_ASISTENCIA_FIRMA';
    V_SEQNAME VARCHAR2(50 CHAR):= 'S_DD_AAF_ASISTENCIA_FIRMA';
    V_PREFIJO VARCHAR2(50 CHAR) := 'DD_AAF_';
@@ -42,22 +43,81 @@ DECLARE
     ' WHEN MATCHED THEN ' ||
     ' UPDATE SET tabla.' || V_PREFIJO || 'DESCRIPCION=actual.descripcion, tabla.' || V_PREFIJO || 'DESCRIPCION_LARGA=actual.descripcion_larga,BORRADO=0'  ;
          
+=======
+   V_DDNAME VARCHAR2(50 CHAR):= 'DD_TFA_FICHERO_ADJUNTO';
+   V_SEQNAME VARCHAR2(50 CHAR):= 'S_DD_TFA_FICHERO_ADJUNTO';
+   V_PREFIJO VARCHAR2(50 CHAR) := 'DD_TFA_';
+   V_ENTIDAD_ID NUMBER(16);
+  
+   V_MSQL VARCHAR(5000);
+   
+>>>>>>> eb5149b... PRODUCTO-1093 Fichero adjuntos obligatorios - inserción en el diccionario:sql/9.2/producto/DML_023_ENTITY01_insertarEnDiccionarioTFA.sql
     --Valores a insertar
     TYPE T_TIPO IS TABLE OF VARCHAR2(4000 CHAR);
     TYPE T_ARRAY IS TABLE OF T_TIPO;
     V_TIPO T_ARRAY := T_ARRAY(
+<<<<<<< HEAD:sql/9.2/producto/DML_020_ENTITY01_insertarEnDiccionarioAsistenciaAFirma.sql
       T_TIPO('GESTOR', 'Gestor', 'Gestor')
      ,T_TIPO('GESTORIA', 'Gestoria', 'Gestoria')
+=======
+      T_TIPO('ESSU', 'Escrito de solicitud de la subasta','AP')
+     ,T_TIPO('DCSU', 'Decreto de convocatoria de subasta','AP')
+     ,T_TIPO('DO', 'Diligencia de ordenación','AP')
+     ,T_TIPO('ESUSU', 'Edicto subsanado de subasta','AP')
+     ,T_TIPO('PTSU', 'Pago tasa subasta','AP')
+     ,T_TIPO('PPSBOE', 'Pantallazo de publicación de la subasta en BOE.ES','AP')
+     ,T_TIPO('TCION', 'Tasación','AP')
+     ,T_TIPO('ESADJ', 'Escrito de solicitud de adjudicación','AP')
+     
+>>>>>>> eb5149b... PRODUCTO-1093 Fichero adjuntos obligatorios - inserción en el diccionario:sql/9.2/producto/DML_023_ENTITY01_insertarEnDiccionarioTFA.sql
     ); 
     V_TMP_TIPO T_TIPO;
+       
+    BEGIN
 
-    V_ENTIDAD_ID NUMBER(16);
-    
-    V_OBTENER_ID VARCHAR2(2000 CHAR) := 'SELECT ' || V_ESQUEMA || '.' || V_SEQNAME || '.NEXTVAL FROM DUAL';
-    V_SELECT_ID  VARCHAR2(2000 CHAR) := 'SELECT COUNT(*) FROM ' || V_ESQUEMA || '.' || V_DDNAME || ' WHERE ' || V_PREFIJO || 'ID=:1';
+   FOR I IN V_TIPO.FIRST .. V_TIPO.LAST
+   LOOP
+        V_TMP_TIPO := V_TIPO(I);
+
+        EXECUTE IMMEDIATE('SELECT COUNT(1) FROM '||V_ESQUEMA||'.'||V_DDNAME||' WHERE DD_TFA_CODIGO = '''||V_TMP_TIPO(1)||'''') INTO V_NUM_REGS;
+ 
+     if V_NUM_REGS = 0 then    
+
+           V_MSQL := 'SELECT '||V_ESQUEMA||'.'||V_SEQNAME||'.NEXTVAL FROM DUAL';
+           EXECUTE IMMEDIATE V_MSQL INTO V_ENTIDAD_ID;
+
+            DBMS_OUTPUT.PUT_LINE('Producto nuevo en '||V_DDNAME||' - INSERT: '||V_TMP_TIPO(1));  
+
+           V_MSQL := 'INSERT INTO '||V_ESQUEMA||'.'||V_DDNAME||'
+                        (DD_TFA_ID, DD_TFA_CODIGO, DD_TFA_DESCRIPCION, DD_TFA_DESCRIPCION_LARGA, VERSION, USUARIOCREAR, FECHACREAR, BORRADO, DD_TAC_ID)
+               VALUES 
+                        ('||  V_ENTIDAD_ID || ','''||V_TMP_TIPO(1)||''','''||V_TMP_TIPO(2)||''','''||V_TMP_TIPO(2)||''',0,''PRODUCTO-1093'',SYSDATE,0, (SELECT DD_TAC_ID FROM '||V_ESQUEMA||'.DD_TAC_TIPO_ACTUACION WHERE DD_TAC_CODIGO = '''||V_TMP_TIPO(3)||'''))';
+         
+          EXECUTE IMMEDIATE V_MSQL;
+                      
+      else 
+
+            DBMS_OUTPUT.PUT_LINE('ACTUALIZANDO: '||V_TMP_TIPO(1)||','||TRIM(V_TMP_TIPO(2)));
+ 
+            V_MSQL := 'UPDATE '||V_ESQUEMA||'.'||V_DDNAME||'
+            SET 
+                DD_TFA_DESCRIPCION = '''||V_TMP_TIPO(2)||''', 
+                DD_TFA_DESCRIPCION_LARGA = '''||V_TMP_TIPO(2)||''', 
+                USUARIOMODIFICAR = ''PRODUCTO-1093'', 
+                FECHAMODIFICAR = SYSDATE,
+                DD_TAC_ID = (SELECT DD_TAC_ID FROM '||V_ESQUEMA||'.DD_TAC_TIPO_ACTUACION WHERE DD_TAC_CODIGO = '''||V_TMP_TIPO(3)||''')
+            WHERE DD_TFA_CODIGO = '''||V_TMP_TIPO(1)||'''';   
+            
+           EXECUTE IMMEDIATE V_MSQL;
+            
+
+       end if;    
+   
+   END LOOP; 
+   COMMIT;
   
-BEGIN
   
+<<<<<<< HEAD:sql/9.2/producto/DML_020_ENTITY01_insertarEnDiccionarioAsistenciaAFirma.sql
   -- LOOP Insertando valores en la tabla del diccionario
   DBMS_OUTPUT.PUT_LINE('[INICIO] '||V_ESQUEMA||'.' || V_DDNAME ||'... Empezando a insertar datos en el diccionario');
   FOR I IN V_TIPO.FIRST .. V_TIPO.LAST
@@ -78,6 +138,11 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('INSERTANDO: ' || V_ENTIDAD_ID || ', '''||V_TMP_TIPO(1)||''','''||TRIM(V_TMP_TIPO(2))||'''');
   END LOOP;
   COMMIT;
+=======
+   -- DBMS_OUTPUT.PUT_LINE('[INFO] SE HAN INSERTADO '||V_COUNT||' PRODUCTOS NUEVOS.');
+    DBMS_OUTPUT.PUT_LINE('[INFO] EJECUCION TERMINADA CON EXITO.');
+    
+>>>>>>> eb5149b... PRODUCTO-1093 Fichero adjuntos obligatorios - inserción en el diccionario:sql/9.2/producto/DML_023_ENTITY01_insertarEnDiccionarioTFA.sql
   DBMS_OUTPUT.PUT_LINE('[FIN] '||V_ESQUEMA||'.' || V_DDNAME ||'... Datos del diccionario insertado');
 
 EXCEPTION

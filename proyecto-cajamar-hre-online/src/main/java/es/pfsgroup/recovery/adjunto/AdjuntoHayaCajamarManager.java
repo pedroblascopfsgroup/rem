4f497c9be07f85ee1f26805b4ee23bd1a53ad61b
manuel.mejias <manuel.mejias@pfsgroup.es>
2016-04-18 10:41:02 +0200

    para que siga funcionando igualmente si desactivan el gestor
    Former-commit-id: c3bc9d041195a1aa6c45ed2b36527257b8ae1bec

--- proyecto-cajamar-hre-online/src/main/java/es/pfsgroup/recovery/adjunto/AdjuntoHayaCajamarManager.java
+++ proyecto-cajamar-hre-online/src/main/java/es/pfsgroup/recovery/adjunto/AdjuntoHayaCajamarManager.java
@@ -42,17 +42,20 @@ public class AdjuntoHayaCajamarManager extends AdjuntoManager  implements Adjunt
 	@Resource
     Properties appProperties;
 	
+	private String ENTIDAD_HAYA = "HAYA";
 	private String ENTIDAD_CAJAMAR = "CAJAMAR";
     private final static String GESTOR_DOCUMENTAL_WS_ACTIVADO = "gestor.documental.cajamar.ws.activado"; 
+    private final static String GESTOR_DOCUMENTAL_HAYA_WS_ACTIVADO = "gestor.documental.haya.ws.activado";
 
 	@Override
 	@Transactional(readOnly = false)
 	public List<? extends EXTAdjuntoDto> getAdjuntosConBorrado(Long id) {
 		if(esEntidadCajamar()){
 			return (List<? extends EXTAdjuntoDto>) adjuntoCajamarManager.getAdjuntosCntConBorrado(id);
-		}else{
+		}else if(esEntidadHaya()){
 			return adjuntoHayaManager.getAdjuntosConBorrado(id);
 		}
+		return super.getAdjuntosConBorrado(id);
 	}
 
 	@Override
@@ -60,86 +63,94 @@ public class AdjuntoHayaCajamarManager extends AdjuntoManager  implements Adjunt
 	public List<? extends AdjuntoDto> getAdjuntosConBorradoByPrcId(Long prcId) {
 		if(esEntidadCajamar()){
 			return adjuntoCajamarManager.getAdjuntosConBorradoByPrcId(prcId);
-		}else{
+		}else if(esEntidadHaya()){
 			return adjuntoHayaManager.getAdjuntosConBorradoByPrcId(prcId);
 		}
+		return super.getAdjuntosConBorradoByPrcId(prcId);
 	}
 	
 	@Override
 	public List<ExtAdjuntoGenericoDto> getAdjuntosContratosAsu(Long id) {
 		if(esEntidadCajamar()){
 			return adjuntoCajamarManager.getAdjuntosContratosAsu(id);
-		}else{
+		}else if(esEntidadHaya()){
 			return adjuntoHayaManager.getAdjuntosContratosAsu(id);
 		}
+		return super.getAdjuntosContratosAsu(id);
 	}
 
 	@Override
 	public List<ExtAdjuntoGenericoDto> getAdjuntosPersonaAsu(Long id) {
 		if(esEntidadCajamar()){
 			return adjuntoCajamarManager.getAdjuntosPersonaAsu(id);
-		}else{
+		}else if(esEntidadHaya()){
 			return adjuntoHayaManager.getAdjuntosPersonaAsu(id);
 		}
+		return super.getAdjuntosPersonaAsu(id);
 	}
 
 	@Override
 	public List<ExtAdjuntoGenericoDto> getAdjuntosExpedienteAsu(Long id) {
 		if(esEntidadCajamar()){
 			return adjuntoCajamarManager.getAdjuntosExpedienteAsu(id);
-		}else{
+		}else if(esEntidadHaya()){
 			return super.getAdjuntosExpedienteAsu(id);
 //			return adjuntoHayaManager.getAdjuntosExpedienteAsu(id);
 		}
+		return super.getAdjuntosExpedienteAsu(id);
 	}
 
 	@Override
 	public List<? extends AdjuntoDto> getAdjuntosConBorradoExp(Long id) {
 		if(esEntidadCajamar()){
 			return adjuntoCajamarManager.getAdjuntosConBorradoExp(id);
-		}else{
+		}else if(esEntidadHaya()){
 			return super.getAdjuntosConBorradoExp(id);
 //			return adjuntoHayaManager.getAdjuntosConBorradoExp(id);
 		}
-		
+		return super.getAdjuntosConBorradoExp(id);
 	}
 
 	@Override
 	public List<ExtAdjuntoGenericoDto> getAdjuntosPersonasExp(Long id) {
 		if(esEntidadCajamar()){
 			return adjuntoCajamarManager.getAdjuntosPersonasExp(id);
-		}else{
+		}else if(esEntidadHaya()){
 			return super.getAdjuntosPersonasExp(id);
 //			return adjuntoHayaManager.getAdjuntosPersonasExp(id);
 		}
+		return super.getAdjuntosPersonasExp(id);
 	}
 
 	@Override
 	public List<ExtAdjuntoGenericoDto> getAdjuntosContratoExp(Long id) {
 		if(esEntidadCajamar()){
 			return adjuntoCajamarManager.getAdjuntosContratoExp(id);
-		}else{
+		}else if(esEntidadHaya()){
 			return super.getAdjuntosContratoExp(id);
 //			return adjuntoHayaManager.getAdjuntosContratoExp(id);
 		}
+		return super.getAdjuntosContratoExp(id);
 	}
 
 	@Override
 	public List<? extends AdjuntoDto> getAdjuntosCntConBorrado(Long id) {
 		if(esEntidadCajamar()){
 			return adjuntoCajamarManager.getAdjuntosCntConBorrado(id);
-		}else{
+		}else if(esEntidadHaya()){
 			return adjuntoHayaManager.getAdjuntosCntConBorrado(id);
 		}
+		return super.getAdjuntosCntConBorrado(id);
 	}
 
 	@Override
 	public List<? extends AdjuntoDto> getAdjuntosPersonaConBorrado(Long id) {
 		if(esEntidadCajamar()){
 			return adjuntoCajamarManager.getAdjuntosPersonaConBorrado(id);
-		}else{
+		}else if(esEntidadHaya()){
 			return adjuntoHayaManager.getAdjuntosPersonaConBorrado(id);
 		}
+		return super.getAdjuntosPersonaConBorrado(id);
 	}
 
 	@Override
@@ -147,9 +158,10 @@ public class AdjuntoHayaCajamarManager extends AdjuntoManager  implements Adjunt
 		if(!Checks.esNulo(uploadForm) && !Checks.esNulo(uploadForm.getParameter("id"))){
 			if(esEntidadCajamar()){
 				return adjuntoCajamarManager.upload(uploadForm);
-			}else{
+			}else if(esEntidadHaya()){
 				return adjuntoHayaManager.upload(uploadForm);
 			}
+			return super.upload(uploadForm);
 		}else{
 			return null;
 		}
@@ -160,9 +172,10 @@ public class AdjuntoHayaCajamarManager extends AdjuntoManager  implements Adjunt
 		if(!Checks.esNulo(uploadForm) && !Checks.esNulo(uploadForm.getParameter("id"))){
 			if(esEntidadCajamar()){
 				return adjuntoCajamarManager.uploadPersona(uploadForm);
-			}else{
+			}else if(esEntidadHaya()){
 				return adjuntoHayaManager.uploadPersona(uploadForm);
 			}
+			return super.uploadPersona(uploadForm);
 		}else{
 			return null;
 		}
@@ -173,10 +186,11 @@ public class AdjuntoHayaCajamarManager extends AdjuntoManager  implements Adjunt
 		if(!Checks.esNulo(uploadForm) && !Checks.esNulo(uploadForm.getParameter("id"))){
 			if(esEntidadCajamar()){
 				return adjuntoCajamarManager.uploadExpediente(uploadForm);
-			}else{
+			}else if(esEntidadHaya()){
 				return super.uploadExpediente(uploadForm);
 //				return adjuntoHayaManager.uploadExpediente(uploadForm);
 			}
+			return super.uploadExpediente(uploadForm);
 		}else{
 			return null;
 		}
@@ -187,9 +201,10 @@ public class AdjuntoHayaCajamarManager extends AdjuntoManager  implements Adjunt
 		if(!Checks.esNulo(uploadForm) && !Checks.esNulo(uploadForm.getParameter("id"))){
 			if(esEntidadCajamar()){
 				return adjuntoCajamarManager.uploadContrato(uploadForm);
-			}else{
+			}else if(esEntidadHaya()){
 				return adjuntoHayaManager.uploadContrato(uploadForm);
 			}
+			return super.uploadContrato(uploadForm);
 		}else{
 			return null;
 		}
@@ -199,37 +214,41 @@ public class AdjuntoHayaCajamarManager extends AdjuntoManager  implements Adjunt
 	public FileItem bajarAdjuntoAsunto(Long asuntoId, String adjuntoId, String nombre, String extension) {
 		if(esEntidadCajamar()){
 			return adjuntoCajamarManager.bajarAdjuntoAsunto(asuntoId, adjuntoId, nombre, extension);
-		}else{
+		}else if(esEntidadHaya()){
 			return adjuntoHayaManager.bajarAdjunto(adjuntoId);
 		}
+		return super.bajarAdjuntoAsunto(asuntoId, adjuntoId, nombre, extension);
 	}
 	
 	@Override
 	public FileItem bajarAdjuntoExpediente(String adjuntoId, String nombre, String extension) {
 		if(esEntidadCajamar()){
 			return adjuntoCajamarManager.bajarAdjuntoExpediente(adjuntoId, nombre, extension);
-		}else{
+		}else if(esEntidadHaya()){
 			return super.bajarAdjuntoExpediente(adjuntoId, nombre, extension);
 //			return adjuntoHayaManager.bajarAdjunto(adjuntoId);
 		}
+		return super.bajarAdjuntoExpediente(adjuntoId, nombre, extension);
 	}
 
 	@Override
 	public FileItem bajarAdjuntoContrato(String adjuntoId, String nombre, String extension) {
 		if(esEntidadCajamar()){
 			return adjuntoCajamarManager.bajarAdjuntoContrato(adjuntoId, nombre, extension);
-		}else{
+		}else if(esEntidadHaya()){
 			return adjuntoHayaManager.bajarAdjunto(adjuntoId);
 		}
+		return super.bajarAdjuntoContrato(adjuntoId, nombre, extension);
 	}
 
 	@Override
 	public FileItem bajarAdjuntoPersona(String adjuntoId, String nombre, String extension) {
 		if(esEntidadCajamar()){
 			return adjuntoCajamarManager.bajarAdjuntoPersona(adjuntoId, nombre, extension);
-		}else{
+		}else if(esEntidadHaya()){
 			return adjuntoHayaManager.bajarAdjunto(adjuntoId);
 		}
+		return super.bajarAdjuntoPersona(adjuntoId, nombre, extension);
 	}
 
 	private boolean esEntidadCajamar(){
@@ -240,4 +259,13 @@ public class AdjuntoHayaCajamarManager extends AdjuntoManager  implements Adjunt
 			return false;	
 		}
 	}	
+	
+	private boolean esEntidadHaya(){
+		Usuario usuario = proxyFactory.proxy(UsuarioApi.class).getUsuarioLogado();
+		if(ENTIDAD_HAYA.equals(usuario.getEntidad().getDescripcion()) && Boolean.parseBoolean(appProperties.getProperty(GESTOR_DOCUMENTAL_HAYA_WS_ACTIVADO))){
+			return true;
+		}else{
+			return false;	
+		}
+	}
 }

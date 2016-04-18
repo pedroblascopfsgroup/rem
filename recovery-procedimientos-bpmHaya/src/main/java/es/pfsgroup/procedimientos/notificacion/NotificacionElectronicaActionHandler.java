5110739fd9aa8717b2cb06f3648975cfe2497695
Alberto Soler <alberto.soler@pfsgroup.es>
2016-04-18 18:55:06 +0200

    modificación handler trámite notificación
    Former-commit-id: 6ee60efceac614e29de79c81ac4be281e5101ae4

--- recovery-procedimientos-bpmHaya/src/main/java/es/pfsgroup/procedimientos/notificacion/NotificacionElectronicaActionHandler.java
+++ recovery-procedimientos-bpmHaya/src/main/java/es/pfsgroup/procedimientos/notificacion/NotificacionElectronicaActionHandler.java
@@ -2,11 +2,8 @@ package es.pfsgroup.procedimientos.notificacion;
 
 
 import org.jbpm.graph.exe.ExecutionContext;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import es.capgemini.pfs.asunto.EXTAsuntoManager;
 import es.capgemini.pfs.asunto.model.Procedimiento;
-import es.pfsgroup.plugin.recovery.coreextension.api.CoreProjectContext;
 import es.pfsgroup.procedimientos.PROBaseActionHandler;
 
 public class NotificacionElectronicaActionHandler extends PROBaseActionHandler {
@@ -15,12 +12,6 @@ public class NotificacionElectronicaActionHandler extends PROBaseActionHandler {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	@Autowired
-	private EXTAsuntoManager extAsuntoManager;
-	
-	@Autowired
-	private CoreProjectContext coreProjectContext;
 
 	@Override
 	public void run(ExecutionContext executionContext) throws Exception {

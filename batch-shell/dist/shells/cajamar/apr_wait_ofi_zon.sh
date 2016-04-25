9293ee7b95016ee60df5b1e796c7056ba80594de
rachel <raquel.moreno@pfsgroup.es>
2016-04-26 00:00:56 +0200

    SHELLs Limpieza en precontencioso
    Former-commit-id: 3bac8dc6dddc0269486d74477dbe19d289559b48

--- batch-shell/dist/shells/cajamar/apr_wait_ofi_zon.sh
+++ batch-shell/dist/shells/cajamar/apr_wait_ofi_zon.sh
@@ -3,8 +3,8 @@
 
 ficheros=OFICINAS,ZONAS
 
-if [ -z ${DIR_DESTINO} ]; then
-    echo "$(basename $0) Error: DIR_DESTINO no definido. Compruebe invocación previa a setBatchEnv.sh"
+if [[ -z ${DIR_DESTINO} ]] || [[ ! -d ${DIR_DESTINO} ]]; then
+    echo "$(basename $0) Error: DIR_DESTINO no definido o no es un directorio. Compruebe invocación previa a setBatchEnv.sh"
     exit 1
 fi
 rm -f $DIR_DESTINO*

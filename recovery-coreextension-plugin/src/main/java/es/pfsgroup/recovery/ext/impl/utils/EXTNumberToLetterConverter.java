0718df1fc9730ed8d1f90dd28563b024d891bafa
Carlos Martos <carlos.martos@pfsgroup.es>
2016-04-25 12:24:23 +0200

    [BKREC-2346] Correccion en cifras en la generacion de liquidaciones
    Former-commit-id: 272958ff5cabfcd6e2d12a8128bc6fe7351448fe

--- recovery-coreextension-plugin/src/main/java/es/pfsgroup/recovery/ext/impl/utils/EXTNumberToLetterConverter.java
+++ recovery-coreextension-plugin/src/main/java/es/pfsgroup/recovery/ext/impl/utils/EXTNumberToLetterConverter.java
@@ -13,10 +13,10 @@ import java.text.DecimalFormat;
 public class EXTNumberToLetterConverter {
 	private static final String[] UNIDADES = { "", "UN ", "DOS ", "TRES ",
         "CUATRO ", "CINCO ", "SEIS ", "SIETE ", "OCHO ", "NUEVE ", "DIEZ ",
-        "ONCE ", "DOCE ", "TRECE ", "CATORCE ", "QUINCE ", "DIECISEIS",
-        "DIECISIETE", "DIECIOCHO", "DIECINUEVE", "VEINTE" };
+        "ONCE ", "DOCE ", "TRECE ", "CATORCE ", "QUINCE ", "DIECISEIS ",
+        "DIECISIETE ", "DIECIOCHO ", "DIECINUEVE ", "VEINTE " };
 
-private static final String[] DECENAS = { "VENTI", "TREINTA ", "CUARENTA ",
+private static final String[] DECENAS = { "VEINTI", "TREINTA ", "CUARENTA ",
         "CINCUENTA ", "SESENTA ", "SETENTA ", "OCHENTA ", "NOVENTA ",
         "CIEN " };
 

89b714ebdb02dee134ed59669d1e0e8d4d6e2d83
Gustavo Mora <gustavo.mora@pfsgroup.es>
2016-04-27 18:14:23 +0200

    Carterizacion HRE letrados faltantes
    Former-commit-id: 6f68bf6aca6476b43a0ced3ff7c5d39a2ebbd789

--- sql/9.1/haya/bcc/migracion/CJM_lanza_migracion_MIM.sh
+++ sql/9.1/haya/bcc/migracion/CJM_lanza_migracion_MIM.sh
@@ -43,7 +43,16 @@ if [ $? != 0 ] ; then
     echo -e "\n\n======>>> [ERROR] en CJM_lanza_migracion.sh"
     exit 1
 fi
-echo "[OK] ""$sh_dir""CJM_backup_ficheros_migracion_MIM.sh ejecutado correctamente"               
+echo "[OK] ""$sh_dir""CJM_backup_ficheros_migracion_MIM.sh ejecutado correctamente"
+
+echo "[INFO] Comienza ejecución de: ""$sh_dir""HR-2391_Carterizacion_LETRADOS_y_PROCURADORES.sh"                  
+./"$sh_dir"HR-2391_Carterizacion_LETRADOS_y_PROCURADORES.sh "$2"
+if [ $? != 0 ] ; then
+    echo -e "\n\n======>>> [ERROR] en "$sh_dir"CJM_backup_ficheros_migracion.sh"
+    echo -e "\n\n======>>> [ERROR] en CJM_lanza_migracion.sh"
+    exit 1
+fi
+echo "[OK] ""$sh_dir""HR-2391_Carterizacion_LETRADOS_y_PROCURADORES.sh ejecutado correctamente"               
 
   
 

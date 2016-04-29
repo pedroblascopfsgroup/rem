4e06260ec8901f0c202c9385eb4ea2bbd55e30f7
María Villanueva <maria.villanueva@pfsgroup.es>
2016-04-29 13:43:54 +0200

    Se quita el left join de subastas con lotes para evitar que se carguen subastas con los lotes nulos
    Former-commit-id: 70cff73adbc94300647fb3fde2c1e6964748179b

--- sql/9.1/cajamar/informacional/ddl/DDL_000_DWH_CARGAR_H_SUBASTA.sql
+++ sql/9.1/cajamar/informacional/ddl/DDL_000_DWH_CARGAR_H_SUBASTA.sql
@@ -1,3 +1,21 @@
+--/*
+--##########################################
+--## AUTOR=María V.
+--## FECHA_CREACION=20160429
+--## ARTEFACTO=batch
+--## VERSION_ARTEFACTO=0.1
+--## INCIDENCIA_LINK=CMREC-2387
+--## PRODUCTO=NO
+--## 
+--## Finalidad: Se quita el left join de subastas con lotes para evitar que se carguen subastas con los lotes nulos
+--## INSTRUCCIONES:  Configurar las variables necesarias en el principio del DECLARE
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
 create or replace PROCEDURE CARGAR_H_SUBASTA (DATE_START IN DATE, DATE_END IN DATE, O_ERROR_STATUS OUT VARCHAR2) AS
 -- ===============================================================================================
 -- Autor: Jaime Sánchez-Cuenca Bellido, PFS Group
@@ -757,3 +775,5 @@ EXCEPTION
 end;
 
 END CARGAR_H_SUBASTA;
+/
+EXIT

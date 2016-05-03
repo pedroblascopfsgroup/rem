8761b2146a632013ae952185f263fe31069362bb
Ruben Rovira <ruben.rovira@pfsgroup.es>
2016-05-03 18:46:25 +0200

    DDl tablas salidas burofax
    Former-commit-id: 658bfe477888beee7baf4417aa5cbbc67a04b3fd

---
+++ sql/9.1/cajamar/DDL_407_ENTITY01_TMP_PCO_GEN_BUROFAX.sql
@@ -0,0 +1,128 @@
+--/*
+--##########################################
+--## AUTOR=RUBEN ROVIRA
+--## FECHA_CREACION=20160503
+--## ARTEFACTO=batch
+--## VERSION_ARTEFACTO=9.1
+--## INCIDENCIA_LINK=CMREC-3273
+--## PRODUCTO=NO
+--## Finalidad: DDL
+--##           
+--## INSTRUCCIONES: Configurar las variables necesarias en el principio del DECLARE
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
+SET DEFINE OFF;
+
+DECLARE
+
+    V_MSQL         VARCHAR2(32000 CHAR); -- Sentencia a ejecutar    
+    V_ESQUEMA      VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+    V_ESQUEMA_M    VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+    V_SQL          VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS   NUMBER(16); -- Vble. para validar la existencia de una tabla.  
+    ERR_NUM        NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG        VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+
+    V_TEXT1        VARCHAR2(2400 CHAR); -- Vble. auxiliar
+    
+
+BEGIN
+    
+    ----------------------------
+    --   TMP_PCO_GEN_BUROFAX   --
+    ----------------------------   
+    
+    --** Comprobamos si existe la tabla   
+    V_SQL := 'SELECT COUNT(1) FROM ALL_TABLES WHERE TABLE_NAME = ''TMP_PCO_GEN_BUROFAX'' and owner = '''||v_esquema||'''';
+    EXECUTE IMMEDIATE v_sql INTO v_num_tablas;
+    -- Si existe la borramos
+    IF V_NUM_TABLAS = 1 THEN 
+            V_MSQL := 'DROP TABLE '||v_esquema||'.TMP_PCO_GEN_BUROFAX CASCADE CONSTRAINTS';
+            EXECUTE IMMEDIATE V_MSQL;
+            DBMS_OUTPUT.PUT_LINE('[INFO] '||v_esquema||'.TMP_PCO_GEN_BUROFAX... Tabla borrada');  
+    END IF;
+    DBMS_OUTPUT.PUT_LINE('[INFO] '||v_esquema||'.TMP_PCO_GEN_BUROFAX... Comprobaciones previas FIN');
+    
+   
+    --** Creamos la tabla
+    V_MSQL := '  CREATE TABLE '||v_esquema||'."TMP_PCO_GEN_BUROFAX" 
+   (	"BUROFAX_ID" NUMBER(16,0),  
+	"CONTRATO" VARCHAR2(50 CHAR) NOT NULL ENABLE, 
+	"FECHA_SOLICITUD" TIMESTAMP (6), 
+	"FECHA_ENVIO" TIMESTAMP (6), 
+	"NOMBRE" VARCHAR2(100 CHAR), 
+	"APELLIDO1" VARCHAR2(100 CHAR), 
+	"APELLIDO2" VARCHAR2(100 CHAR), 
+	"TELEFONO_MOVIL_DEST" VARCHAR2(20 CHAR), 
+	"TELEFONO_FIJO_DEST" VARCHAR2(20 CHAR), 
+	"EMAIL" CHAR(320 CHAR), 
+	"DOCUMENTO" VARCHAR2(20 CHAR), 
+	"TIPO_DOCUMENTO" NUMBER(16,0), 
+	"CALLE" VARCHAR2(100 CHAR) NOT NULL ENABLE, 
+	"NUMERO" VARCHAR2(10 CHAR), 
+	"TIPO_DIRECCION" NUMBER(16,0), 
+	"PORTAL" VARCHAR2(15 CHAR), 
+	"PUERTA" VARCHAR2(10 CHAR), 
+	"POBLACION" VARCHAR2(100 CHAR), 
+	"CODIGO_POSTAL" VARCHAR2(20 CHAR), 
+	"PAIS" VARCHAR2(100 CHAR), 
+	"COMENTARIO_DIRECCION" CHAR(500 CHAR), 
+	"PROVINCIA" VARCHAR2(100 CHAR), 
+	"LOCALIDAD_OFICINA" NUMBER(10,0), 
+	"NUMERO_CUENTA_CCC" VARCHAR2(80 CHAR), 
+	"IBAN" VARCHAR2(24 CHAR), 
+	"FECHA_FORMALIZACION" DATE, 
+	"IMPORTE_POLIZA" NUMBER(15,2) NOT NULL ENABLE, 
+	"TITULAR" VARCHAR2(1200 CHAR), 
+	"AVALISTA" VARCHAR2(1200 CHAR), 
+	"INTERES_DEMORA" NUMBER, 
+	"DIRECCION_OFICINA" VARCHAR2(100 CHAR), 
+	"APLICATIVO_ORIGEN" NUMBER(16,0) NOT NULL ENABLE, 
+	"GARANTIA" CHAR(30 CHAR), 
+	"COTITULAR" VARCHAR2(1200 CHAR), 
+	"FECHA_CERTIFICACION" TIMESTAMP (6), 
+	"CAPITAL_CERTIFICADO" NUMBER(16,2), 
+	"INTERESES_CERTIFICADOS" NUMBER(16,2), 
+	"DEMORA_CERTIFICADA" NUMBER(16,2), 
+	"TOTAL_CERTIFICADO" NUMBER(16,2), 
+	"NOTARIO_POLIZA" VARCHAR2(50 CHAR), 
+	"NOMBRE_ENTIDAD" VARCHAR2(50 CHAR), 
+	"DIRECCION_ENTIDAD" VARCHAR2(50 CHAR), 
+	"TELEFONO_ENTIDAD" VARCHAR2(50 CHAR), 
+	"LOCALIDAD_ENTIDAD" VARCHAR2(50 CHAR), 
+	"VENCIMIENTO_ANTICIPADO" VARCHAR2(50 CHAR), 
+	"RELACION_PER_CUE" VARCHAR2(20 CHAR), 
+	"IMPORTE_INTERESES" NUMBER, 
+	"IMPORTE_COMISIONES" NUMBER, 
+	"ESTADO" VARCHAR2(20 CHAR), 
+	"DESCRIPCION_ERROR" VARCHAR2(100 CHAR), 
+	"EMISOR" VARCHAR2(50 CHAR), 
+	"CENTRO_COSTE" VARCHAR2(50 CHAR), 
+	"VERSION" NUMBER, 
+	"USUARIOCREAR" VARCHAR2(50 CHAR), 
+	"FECHACREAR" TIMESTAMP(6),
+	"USUARIOBORRAR" VARCHAR2(50 CHAR), 
+	"FECHABORRAR" TIMESTAMP(6),
+	"BORRADO" NUMBER (1,0)
+   )';
+		
+    EXECUTE IMMEDIATE V_MSQL;
+    DBMS_OUTPUT.PUT_LINE('[INFO] '||v_esquema||'.TMP_PCO_GEN_BUROFAX... Tabla creada');
+    
+    
+EXCEPTION
+  WHEN OTHERS THEN
+    ERR_NUM := SQLCODE;
+    ERR_MSG := SQLERRM;
+    DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(ERR_NUM));
+    DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+    DBMS_OUTPUT.put_line(ERR_MSG);
+    ROLLBACK;
+    RAISE;   
+END;
+/
+EXIT;

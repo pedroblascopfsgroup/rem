6416d63e0f0f174bb3680d8c72cf2a5211ad0ff3
carlos.gil.gimeno <carlos.gil.gimeno@pfsgroup.es>
2016-05-04 13:45:30 +0200

    PRODUCTO-1323, crear tabla CAL_CALCULO_LIQUIDACION
    Former-commit-id: d470dbd020a0b9728197c8ac684a0858fcfd5902

--- sql/9.2/producto/DDL_365_ENTITY01_CREATE_DD_ECA_ESTADO_CALCULO.sql
+++ sql/9.2/producto/DDL_365_ENTITY01_CREATE_DD_ECA_ESTADO_CALCULO.sql
@@ -80,11 +80,11 @@ DECLARE
 					  DD_ECA_DESCRIPCION        VARCHAR2(50 CHAR)   NOT NULL,
 					  DD_ECA_DESCRIPCION_LARGA  VARCHAR2(200 CHAR)  NOT NULL,
 					  VERSION                   INTEGER             DEFAULT 0                     NOT NULL,
-					  USUARIOCREAR              VARCHAR2(10 CHAR)   NOT NULL,
+					  USUARIOCREAR              VARCHAR2(50 CHAR)   NOT NULL,
 					  FECHACREAR                TIMESTAMP(6)        NOT NULL,
-					  USUARIOMODIFICAR          VARCHAR2(10 CHAR),
+					  USUARIOMODIFICAR          VARCHAR2(50 CHAR),
 					  FECHAMODIFICAR            TIMESTAMP(6),
-					  USUARIOBORRAR             VARCHAR2(10 CHAR),
+					  USUARIOBORRAR             VARCHAR2(50 CHAR),
 					  FECHABORRAR               TIMESTAMP(6),
 					  BORRADO                   NUMBER(1)           DEFAULT 0                     NOT NULL
 					)';

991ed083cfe3b441ae4d8a693f020ffdc9a9dafb
Ignacio <ignacio.arcos@pfsgroup.es>
2016-05-04 13:57:27 +0200

    PRODUCTO-1265 Se ha corregido la obtención de los datos de gestor y supervisor de la cabecera del asunto y la parametrización de los mismos por cliente y tipo de asunto.
    Former-commit-id: 958096e425b6006d08d7d4118a9af81a95928232

--- rec-common/src/main/java/es/capgemini/pfs/expediente/ExpedienteManager.java
+++ rec-common/src/main/java/es/capgemini/pfs/expediente/ExpedienteManager.java
@@ -899,8 +899,8 @@ public class ExpedienteManager implements ExpedienteBPMConstants, ExpedienteMana
         
         //Seteamos la oficina
         if(DDTipoExpediente.TIPO_EXPEDIENTE_GESTION_DEUDA.equals(expediente.getTipoExpediente()) || DDTipoExpediente.TIPO_EXPEDIENTE_SEGUIMIENTO.equals(expediente.getTipoExpediente())){
-        	if(!Checks.esNulo(persona) && !Checks.esNulo(persona.getOficinaCliente())){
-        		expediente.setOficina(persona.getOficinaCliente());
+        	if(!Checks.esNulo(persona) && !Checks.esNulo(persona.getOficinaGestora())){
+        		expediente.setOficina(persona.getOficinaGestora());
         	}
         }
         if(Checks.esNulo(expediente.getOficina())){     	

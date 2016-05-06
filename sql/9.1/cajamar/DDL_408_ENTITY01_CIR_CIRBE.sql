29f2b87e6b55a75fb472cae1f5f69bc2119182a5
Ruben Rovira <ruben.rovira@pfsgroup.es>
2016-05-06 14:34:38 +0200

    DDL que desactiva indice cirbe
    Former-commit-id: 332f1a97188f2849b5d7dbdf16cefb400e505dac

---
+++ sql/9.1/cajamar/DDL_408_ENTITY01_CIR_CIRBE.sql
@@ -0,0 +1,59 @@
+--/*
+--##########################################
+--## AUTOR=RUBEN ROVIRA
+--## FECHA_CREACION=20160506
+--## ARTEFACTO=batch
+--## VERSION_ARTEFACTO=9.1
+--## INCIDENCIA_LINK=CMREC-3273
+--## PRODUCTO=NO
+--## Finalidad: DDL
+--##           
+--## INSTRUCCIONES: Configurar las variables necesarias en el principio del DECLARE
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
+SET DEFINE OFF;
+
+DECLARE
+
+    V_MSQL         VARCHAR2(32000 CHAR); -- Sentencia a ejecutar    
+    V_ESQUEMA      VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+    V_ESQUEMA_M    VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+    V_SQL          VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS   NUMBER(16); -- Vble. para validar la existencia de una tabla.  
+    ERR_NUM        NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG        VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+
+    V_TEXT1        VARCHAR2(2400 CHAR); -- Vble. auxiliar
+    
+
+BEGIN
+    
+    ----------------------------
+    --   cir_cirbe   --
+    ----------------------------   
+    
+
+   
+    --** Creamos la tabla
+    V_MSQL := 'ALTER TABLE '||v_esquema||'.cir_cirbe DISABLE CONSTRAINT FK_CIR_CIR';
+		
+    EXECUTE IMMEDIATE V_MSQL;
+    DBMS_OUTPUT.PUT_LINE('[INFO] '||v_esquema||'.cir_cirbe... Tabla alterada');
+    
+    
+EXCEPTION
+  WHEN OTHERS THEN
+    ERR_NUM := SQLCODE;
+    ERR_MSG := SQLERRM;
+    DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(ERR_NUM));
+    DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+    DBMS_OUTPUT.put_line(ERR_MSG);
+    ROLLBACK;
+    RAISE;   
+END;
+/
+EXIT;

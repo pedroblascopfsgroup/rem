ea0e46680a9ee39434699fd1efa1f7e94b4cb4a7
Alberto Barrantes <alberto.barrantes@pfsgroup.es>
2016-05-31 15:55:10 +0200

    [CMREC-3012] Certificado de Saldo: Datos incorrectos en el texto
    Former-commit-id: 729dbf29906289294d22018926379080d71c46f6

--- proyecto-cajamar-hre-online/src/main/java/es/pfsgroup/plugin/precontencioso/liquidacion/generar/DatosPlantillaCertificadoSaldo.java
+++ proyecto-cajamar-hre-online/src/main/java/es/pfsgroup/plugin/precontencioso/liquidacion/generar/DatosPlantillaCertificadoSaldo.java
@@ -266,7 +266,8 @@ public class DatosPlantillaCertificadoSaldo extends DatosGenerarDocumentoCajamar
 
 	private String obtenerTipoInteresDemora(Contrato contrato,	String campo) {
 		
-		String resultado = noDisponible(campo);
+		//String resultado = noDisponible(campo);
+		String resultado = "0";
 		if (!Checks.esNulo(contrato.getInteresesDemora())) {
 			resultado = contrato.getInteresesDemora(); 
 		}

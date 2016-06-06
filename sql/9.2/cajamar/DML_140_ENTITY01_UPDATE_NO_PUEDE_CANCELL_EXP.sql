9a532a52c120349537c3a88b900e0d80a0a2f426
carlos.gil.gimeno <carlos.gil.gimeno@pfsgroup.es>
2016-06-06 14:08:48 +0200

    PRODUCTO-1673, configuración para la cancelación de expedientes
    Former-commit-id: 4a56ab5935953f67f2234b0f93ca5d7e3f4de741

---
+++ sql/9.2/cajamar/DML_140_ENTITY01_UPDATE_NO_PUEDE_CANCELL_EXP.sql
@@ -0,0 +1,54 @@
+--/*
+--##########################################
+--## AUTOR=Carlos Gil Gimeno
+--## FECHA_CREACION=20160606
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.2.6
+--## INCIDENCIA_LINK=PRODUCTO-1673
+--## PRODUCTO=NO
+--##
+--## Finalidad: Script para inicializar el campo EST_PUEDE_CANCEL de EST_ESTADOS = 0 en Cajamar
+--## INSTRUCCIONES: Relanzable
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF; 
+DECLARE
+	V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar    
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquemas
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+
+    V_TAREA VARCHAR(50 CHAR);
+    
+BEGIN	
+
+	DBMS_OUTPUT.PUT_LINE('[INICIO] ACTUALIZACION EST_ESTADOS');
+	
+	EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
+	
+	V_MSQL := 'UPDATE '||V_ESQUEMA||'.EST_ESTADOS SET EST_PUEDE_CANCEL = 0';
+	DBMS_OUTPUT.PUT_LINE(V_MSQL);
+	EXECUTE IMMEDIATE V_MSQL;
+	DBMS_OUTPUT.PUT_LINE('[FIN] EST_ESTADOS  actualizados');
+	
+	COMMIT;
+   
+EXCEPTION
+     WHEN OTHERS THEN
+          ERR_NUM := SQLCODE;
+          ERR_MSG := SQLERRM;
+          DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(ERR_NUM));
+          DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+          DBMS_OUTPUT.put_line(ERR_MSG);
+          ROLLBACK;
+          RAISE;   
+END;
+/
+EXIT;

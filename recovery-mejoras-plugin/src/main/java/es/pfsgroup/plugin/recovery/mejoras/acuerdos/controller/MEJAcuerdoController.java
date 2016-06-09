d594a120a6af3827fb065202695fea108ae128d8
Ignacio <ignacio.arcos@pfsgroup.es>
2016-06-09 11:29:19 +0200

    PRODUCTO-1271 Coregido el formato de las fechas de los campos dinámicos de los térmimos en Juniper (reverted from commit f7bc28bf2ee4ecf68409590374fb948998598b92 [formerly 034eede654e3b298fadbdb63a926fb6b137438de])
    Former-commit-id: 8172662857e17b5000a0859b7015c8b138c5f123

--- recovery-mejoras-plugin/src/main/java/es/pfsgroup/plugin/recovery/mejoras/acuerdos/controller/MEJAcuerdoController.java
+++ recovery-mejoras-plugin/src/main/java/es/pfsgroup/plugin/recovery/mejoras/acuerdos/controller/MEJAcuerdoController.java
@@ -54,7 +54,6 @@ import es.pfsgroup.commons.utils.dao.abm.GenericABMDao.OrderType;
 import es.pfsgroup.commons.utils.dao.abm.Order;
 import es.pfsgroup.plugin.recovery.coreextension.utils.api.UtilDiccionarioApi;
 import es.pfsgroup.plugin.recovery.mejoras.acuerdos.MEJAcuerdoApi;
-import es.pfsgroup.plugin.recovery.mejoras.acuerdos.manager.CamposTerminoTipoacuerdoDTO;
 import es.pfsgroup.plugin.recovery.nuevoModeloBienes.model.NMBContratoBien;
 import es.pfsgroup.recovery.ext.impl.acuerdo.model.ACDAcuerdoDerivaciones;
 import es.pfsgroup.recovery.ext.impl.acuerdo.model.EXTAcuerdo;
@@ -422,11 +421,8 @@ public class MEJAcuerdoController {
 	@SuppressWarnings("unchecked")
 	@RequestMapping
 	public String getCamposDinamicosTerminosPorTipoAcuerdo(ModelMap model, Long idTipoAcuerdo){
-		List<CamposTerminoTipoacuerdoDTO> listaCampos =  new ArrayList<CamposTerminoTipoacuerdoDTO>();
-		for(CamposTerminoTipoAcuerdo campos:terminoOperacionesManager.getCamposOperacionesPorTipoAcuerdo(idTipoAcuerdo)){
-			listaCampos.add(new CamposTerminoTipoacuerdoDTO(campos));
-		}
-		model.put("operacionesPorTipo", listaCampos );
+		
+		model.put("operacionesPorTipo", terminoOperacionesManager.getCamposOperacionesPorTipoAcuerdo(idTipoAcuerdo) );
 		return JSON_LIST_CAMPOS_DINAMICOS_TERMINOS_POR_TIPO_ACUERDO;
 	}
 	

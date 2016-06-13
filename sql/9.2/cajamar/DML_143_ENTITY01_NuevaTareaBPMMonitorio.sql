d3433db6107134e44f3a9b9d0d10e01dfd513c77
Carlos Martos <carlos.martos@pfsgroup.es>
2016-06-13 18:30:25 +0200

    [PRODUCTO-1975] Correcciones tareas y bpm Monitorio y Cambiario
    Former-commit-id: e48bc2528c94dc2753259710328c1626c66edf8d

--- sql/9.2/cajamar/DML_143_ENTITY01_NuevaTareaBPMMonitorio.sql
+++ sql/9.2/cajamar/DML_143_ENTITY01_NuevaTareaBPMMonitorio.sql
@@ -1,7 +1,7 @@
 /*
 --##########################################
 --## AUTOR=Carlos Martos
---## FECHA_CREACION=20160602
+--## FECHA_CREACION=20160614
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.2
 --## INCIDENCIA_LINK=PRODUCTO-1975
@@ -38,7 +38,7 @@ DECLARE
     TYPE T_ARRAY_TAP IS TABLE OF T_TIPO_TAP;
     V_TIPO_TAP T_ARRAY_TAP := T_ARRAY_TAP(
         T_TIPO_TAP('H022','H022_BPMAnticipoFondos',null,null,null,null,'P460','0','Se inicia el trámite de anticipo de fondos y pago de suplidos','0','PRODUCTO-1975','0',null,null,null,'1','EXTTareaProcedimiento','3',null,null,null,null,null)
-       ,T_TIPO_TAP('H022','H022_BPMTramiteNotificacionPersonal',null,null,null,null,'HC108','0','Se inicia el trámite de notificación personal','0','PRODUCTO-1975','0',null,null,null,'1','EXTTareaProcedimiento','3',null,null,null,null,null)
+       ,T_TIPO_TAP('H022','DEL_H022_BPMTramiteNotificacionPersonal',null,null,null,null,'HC108','0','Se inicia el trámite de notificación personal','0','PRODUCTO-1975','0',null,null,null,'1','EXTTareaProcedimiento','3',null,null,null,null,null)
     ); 
     V_TMP_TIPO_TAP T_TIPO_TAP;
 

2de2634387c53465577f26c56a08573b78f4f1fe
Carlos Martos <carlos.martos@pfsgroup.es>
2016-06-24 10:37:06 +0200

    [RECOVERY-16] Modificaciones BPM para Bankia y finalizado de tareas obsoletas
    Former-commit-id: 9ab917648e44d5478a2231c0cfc8b9b92917874f

---
+++ sql/9.2/bankia/DML_101_ENTITY01_UpdateTFITGestionLlaves.sql
@@ -0,0 +1,82 @@
+/*
+--##########################################
+--## AUTOR=Carlos Martos
+--## FECHA_CREACION=20160624
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.2
+--## INCIDENCIA_LINK=RECOVERY-16
+--## PRODUCTO=NO
+--##
+--## Finalidad: BPM - Realiza el update de tfi de una tarea del mismo tramite
+--## INSTRUCCIONES:  Ejecutar y definir las variables
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON
+SET DEFINE OFF
+set timing ON
+set linesize 2000
+
+DECLARE
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar   
+    V_MSQL_1 VARCHAR2(32000 CHAR); -- Sentencia a ejecutar  
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquemas
+    V_ESQUEMA_MASTER VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquemas
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.   
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+
+    VAR_TABLENAME VARCHAR2(50 CHAR); -- Nombre de la tabla a crear
+    
+    --Insertando valores en TFI_TAREAS_FORM_ITEMS
+    TYPE T_TIPO_TFI IS TABLE OF VARCHAR2(5000);
+    TYPE T_ARRAY_TFI IS TABLE OF T_TIPO_TFI;
+    V_TIPO_TFI T_ARRAY_TFI := T_ARRAY_TFI(
+
+        T_TIPO_TFI('P417_RegistrarEnvioLlaves','0','<div align="justify" style="font-size: 8pt; font-family: Arial; margin-bottom: 30px;"><p style="margin-bottom: 10px">Para dar por completada esta tarea deber&aacute; de consignar los campos “Nombre del primer depositario” y “Fecha env&iacute;o de llaves letrado”.</p><p style="margin-bottom: 10px">En el campo observaciones consignar cualquier aspecto relevante que le interesa quede reflejado en ese punto del procedimiento.</p></div>','RECOVERY-16')
+
+        ); 
+    V_TMP_TIPO_TFI T_TIPO_TFI;
+    
+BEGIN   
+    
+    -- LOOP Insertando valores en TFI_TAREAS_FORM_ITEMS
+    VAR_TABLENAME := 'TFI_TAREAS_FORM_ITEMS';
+    DBMS_OUTPUT.PUT_LINE('[INICIO] '||V_ESQUEMA||'.' || VAR_TABLENAME || '... Empezando a insertar Campos de tareas');
+    FOR I IN V_TIPO_TFI.FIRST .. V_TIPO_TFI.LAST
+      LOOP
+        V_TMP_TIPO_TFI := V_TIPO_TFI(I);
+        V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.TFI_TAREAS_FORM_ITEMS WHERE TAP_ID = (SELECT TAP_ID FROM ' || V_ESQUEMA || '.TAP_TAREA_PROCEDIMIENTO WHERE TAP_CODIGO = '''||TRIM(V_TMP_TIPO_TFI(1))||''') and TFI_ORDEN = '||TRIM(V_TMP_TIPO_TFI(2));
+        EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
+        IF V_NUM_TABLAS > 0 THEN        
+        
+          V_SQL := 'UPDATE '||V_ESQUEMA||'.'||VAR_TABLENAME||' SET TFI_LABEL=''' || REPLACE(TRIM(V_TMP_TIPO_TFI(3)),'''','''''') || ''',' ||
+          ' USUARIOMODIFICAR=''' || REPLACE(TRIM(V_TMP_TIPO_TFI(4)),'''','''''') || ''',' ||
+          ' FECHAMODIFICAR=sysdate ' ||
+          ' WHERE TAP_ID = (SELECT TAP_ID FROM ' || V_ESQUEMA || '.TAP_TAREA_PROCEDIMIENTO WHERE TAP_CODIGO = '''||TRIM(V_TMP_TIPO_TFI(1))||''') and TFI_ORDEN = '||TRIM(V_TMP_TIPO_TFI(2));
+        --DBMS_OUTPUT.PUT_LINE(V_SQL);
+        EXECUTE IMMEDIATE V_SQL;  
+          DBMS_OUTPUT.PUT_LINE('[INFO] ' || V_ESQUEMA || '.TFI_TAREAS_FORM_ITEMS... Actualizado el item '''|| TRIM(V_TMP_TIPO_TFI(1)) ||''' and TFI_ORDEN = '||TRIM(V_TMP_TIPO_TFI(2))||' ');
+        ELSE 
+          DBMS_OUTPUT.PUT_LINE('No existe la tarea a actualizar'); 
+        END IF;
+      END LOOP;
+    COMMIT;
+    DBMS_OUTPUT.PUT_LINE('[FIN] '||V_ESQUEMA||'.' || VAR_TABLENAME || '... Campos');
+    
+EXCEPTION
+     WHEN OTHERS THEN
+          ERR_NUM := SQLCODE;
+          ERR_MSG := SQLERRM;
+          DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(ERR_NUM));
+          DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+          DBMS_OUTPUT.put_line(ERR_MSG);
+          ROLLBACK;
+          RAISE;   
+END;
+/
+
+EXIT;

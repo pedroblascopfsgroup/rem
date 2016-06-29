839350084d070bcf56458a52e157301ff1bf7a62
Pedro Blasco Onsurbe <pedro.blasco@pfsgroup.es>
2016-06-29 18:32:39 +0200

    RECOVERY-107 Correccion al problema de ordenacion especial para los apoderados de liquidaciones PCO
    Former-commit-id: 70b517e7be6230639faea285a4f2347ac42df72b

--- recovery-coreextension-plugin/src/main/java/es/pfsgroup/plugin/recovery/coreextension/dao/impl/EXTGestoresDaoImpl.java
+++ recovery-coreextension-plugin/src/main/java/es/pfsgroup/plugin/recovery/coreextension/dao/impl/EXTGestoresDaoImpl.java
@@ -222,30 +222,35 @@ public class EXTGestoresDaoImpl extends AbstractEntityDao<Usuario, Long> impleme
 	
 	private Comparator gestoresComparatorEspecial = new Comparator() {
 
+		private final String CONECTOR_Y = " Y ";
+		
 		@Override
 		public int compare(Object gd1, Object gd2) {
 			Usuario u1=((GestorDespacho)gd1).getUsuario();
 			Usuario u2=((GestorDespacho)gd2).getUsuario();
-			String nombre1 = obtenerNombreCompuesto(u1);
-			String nombre2 = obtenerNombreCompuesto(u2);
-			boolean empiezaConY1 = u1.getApellido2().startsWith("Y ");
-			boolean empiezaConY2 = u2.getApellido2().startsWith("Y ");
-			if ( (empiezaConY1 && empiezaConY2) ||
+			String nombre1 = u1.getApellidoNombre().toUpperCase();
+			String nombre2 = u2.getApellidoNombre().toUpperCase();
+			boolean esNulo1 = ( u1.getApellido2() == null);
+			boolean esNulo2 = ( u2.getApellido2() == null);
+			boolean empiezaConY1 = (esNulo1 ? false : u1.getApellido2().startsWith(CONECTOR_Y));
+			boolean empiezaConY2 = (esNulo2 ? false : u2.getApellido2().startsWith(CONECTOR_Y));
+			if ( (esNulo1 && esNulo2) ||
+					(empiezaConY1 && empiezaConY2) ||
 					(!empiezaConY1 && !empiezaConY2)) {
 				return nombre1.compareTo(nombre2);
+			} else if (esNulo1 && empiezaConY2) {
+				return -1;
+			} else if (empiezaConY1 && esNulo2) {
+				return 1;
 			} else if (empiezaConY1 && !empiezaConY2) {
 				return 1;
 			} else if (!empiezaConY1 && empiezaConY2) {
 				return -1;
-			}
+			} 
 			return 0;
 		}
 		
 	};
-	
-	private String obtenerNombreCompuesto(Usuario usuario) {
-		return (usuario.getNombre() == null ? "" : usuario.getNombre()) + " " + (usuario.getApellido1() == null ? "" : usuario.getApellido1()) + " " + (usuario.getApellido2() == null ? "" : usuario.getApellido2()).trim();
-	}
 	
 	@Override
 	public Page getListUsuariosPaginatedDataOrdenacionEspecial(UsuarioDto usuarioDto) {

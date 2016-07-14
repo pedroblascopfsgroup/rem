0a34305d9479705f8e302637be0d2a5401662149
Gonzalo Estellés <gonzalo.estelles@pfsgroup.es>
2016-07-14 16:21:43 +0200

    ARQ-924 Eliminado plugin de mvn_rem.sh (online) por no ser necesario
    Former-commit-id: bf823477837279f36509348c17431335e7760dbf

--- mvn_rem.sh
+++ mvn_rem.sh
@@ -1,1 +1,1 @@
-mvn -Prem -Dmaven.test.skip=true -pl proyecto-rem-online,recovery-webservice,paradise-framework,recovery-gestordocumentalREM-plugin,rem-web $*
+mvn -Prem -Dmaven.test.skip=true -pl recovery-webservice,paradise-framework,recovery-gestordocumentalREM-plugin,rem-web $*

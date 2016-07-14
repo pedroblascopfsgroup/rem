fd4143e3ec3b971168888938d792daeec5e32b5b
Bruno Anglés <bruno.angles@pfsgroup.es>
2016-07-14 16:15:45 +0200

    HREOS-673 Dockerizar el secha build que no ofusca el código
    Former-commit-id: 97c363daf690efb53c6e88a37b7be1edca2b8f9a

--- proyecto-rem-online/sencha-build.sh
+++ proyecto-rem-online/sencha-build.sh
@@ -28,33 +28,10 @@ echo ""
 echo "Borrando 'output'"
 rm -Rf $OUTPUT/*
 
-######## Construcción y arranque de imágenes y contenedores
-echo "Validando instalación de la plataforma"
 
-if [ "x$(docker images | grep rem/sencha-cmd)" != "x" ]; then
-	echo  "rem/sencha-cmd: Se ha encontrado la imagen requerida"
-else
-	echo "Falta algunas imágenes de la platarforma. Se van a reconstruir"
-	if [ "x$(docker images | grep rem/base)" != "x" ]; then
-		echo "rem/base: Se ha encontrado la imágen"
-	else
-		echo "Generando imágen rem/base"
-		sleep 1
-		docker build --no-cache=true -t rem/base $CURRENT/../dockerize/rem/rem-base/.
-		[ $? -ne 0 ] && exit 1
-		echo -e "\n\n"
-	fi
-	echo "Generando imágen rem/sencha-cmd"
-	sleep 1
-	docker build --no-cache=true -t rem/sencha-cmd $CURRENT/../dockerize/rem/rem-sencha-cmd/.
-	[ $? -ne 0 ] && exit 1
-	echo -e "\n\n"
-fi
-
-echo "Continuando"
-sleep 1
-# Descomentar para construir con el build de sencha
-docker run --rm -i -v $(pwd)/sencha-app:/input -v $(pwd)/src/web/js/plugin/rem/:/output rem/sencha-cmd sencha-build.sh $(id -u) $(id -g)
+docker build -t rem/base $CURRENT/../dockerize/rem/rem-base/. \
+	&& docker build -t rem/sencha-cmd $CURRENT/../dockerize/rem/rem-sencha-cmd/. \
+	&& docker run --rm -i -v $(pwd)/sencha-app:/input -v $(pwd)/src/web/js/plugin/rem/:/output rem/sencha-cmd sencha-build.sh $(id -u) $(id -g) $*
 
 
 if [[ ! -f $(pwd)/src/web/js/plugin/rem/index.jsp ]]; then

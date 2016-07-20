282a3448edd54cbc8dc0c6589953111521e3ea4c
Daniel Gutiérrez <daniel.gutierrez@pfsgroup.es>
2016-07-20 10:20:07 +0200

    HREOS-677: Esqueleto de ventana de publicaciones
    Former-commit-id: 3903b45698b5401fcf382ad2aa2ad076deef8ecd

--- proyecto-rem-online/sencha-app/classic/src/view/publicacion/PublicacionMain.js
+++ proyecto-rem-online/sencha-app/classic/src/view/publicacion/PublicacionMain.js
@@ -1,8 +1,31 @@
 Ext.define('HreRem.view.publicacion.PublicacionMain', {
-    extend		: 'Ext.container.Container',
+    extend		: 'Ext.tab.Panel',
+    cls			: 'tabpanel-base',
     xtype		: 'publicacionmain',
     reference	: 'publicacionMain',
-    layout		: 'fit'
+    layout		: 'fit',
+    requires	: ['HreRem.view.publicacion.PublicacionController', 'HreRem.view.publicacion.activos.ActivosPublicacionMain',
+            	   'HreRem.view.publicacion.configuracion.ConfiguracionPublicacionMain', 'HreRem.view.publicacion.PublicacionModel'],
+    controller: 'publicaciones',
+    viewModel: {
+        type: 'publicaciones'
+    },
+    
+    initComponent: function () {
+        
+        var me = this;
+        
+        me.items = [
+		    {	
+				xtype: 'activospublicacionmain', reference: 'activosPublicacionMain'
+			},
+			{	
+				xtype: 'configuracionpublicacionmain', reference	: 'configuracionPublicacionMain'				
+			}
+        ];
+        
+        me.callParent(); 
 
+        
+    }
 });
-

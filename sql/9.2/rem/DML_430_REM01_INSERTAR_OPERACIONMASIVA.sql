cddc6a5408377bbc7f181e9ac3845c741c4eb319
Daniel Gutiérrez <daniel.gutierrez@pfsgroup.es>
2016-08-25 09:31:26 +0200

    Modificación de script
    Former-commit-id: 8f0456d8453c6b207795210eac923164a8b7a3d6

--- sql/9.2/rem/DML_430_REM01_INSERTAR_OPERACIONMASIVA.sql
+++ sql/9.2/rem/DML_430_REM01_INSERTAR_OPERACIONMASIVA.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=DANIEL GUTIÉRREZ
---## FECHA_CREACION=20160817
+--## FECHA_CREACION=20160825
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.1
 --## INCIDENCIA_LINK=0
@@ -42,7 +42,7 @@ DECLARE
 	  T_FUNCION('ADAC', 'Desocultar publicación activo', 'Desocultar publicación activo', 'ACTUALIZAR_DESOCULTARACTIVO', 'nD*'),
 	  T_FUNCION('AOPR', 'Ocultar precio publicación', 'Ocultar precio publicación', 'ACTUALIZAR_OCULTARPRECIO', 'nD*'),
 	  T_FUNCION('ADPR', 'Desocultar precio publicación', 'Desocultar precio publicación', 'ACTUALIZAR_DESOCULTARPRECIO', 'nD*'),
-	  T_FUNCION('APUB', 'Despublicar', 'Despublicar', 'ACTUALIZAR_DESPUBLICAR', 'nD*'),
+	  T_FUNCION('ADPU', 'Despublicar', 'Despublicar', 'ACTUALIZAR_DESPUBLICAR', 'nD*'),
 	  T_FUNCION('AEME', 'Autorizar edición a mediador', 'Autorizar edición a mediador', 'ACTUALIZAR_AUTORIZAREDICION', 'nD*')
     ); 
     V_TMP_FUNCION T_FUNCION;

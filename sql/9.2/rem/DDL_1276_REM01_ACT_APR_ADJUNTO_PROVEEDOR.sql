59a26eeff491c6e867b1eb56e362ab87c334beb5
Daniel Gutiérrez <daniel.gutierrez@pfsgroup.es>
2016-08-31 13:58:00 +0200

    HREOS-807: Nueva tabla de adjuntos del proveedor
    Former-commit-id: e4dae99289709c9ed51adca6e82904ee99e4c435

---
+++ sql/9.2/rem/DDL_1276_REM01_ACT_APR_ADJUNTO_PROVEEDOR.sql
@@ -0,0 +1,254 @@
+--/*
+--##########################################
+--## AUTOR=DANIEL GUTIERREZ
+--## FECHA_CREACION=20160831
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.1
+--## INCIDENCIA_LINK=0
+--## PRODUCTO=NO
+--## Finalidad: Tabla para gestionar los adjuntos del proveedor
+--##           
+--## INSTRUCCIONES: Configurar las variables necesarias en el principio del DECLARE
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+
+--Para permitir la visualización de texto en un bloque PL/SQL utilizando DBMS_OUTPUT.PUT_LINE
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF;
+
+
+DECLARE
+
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar    
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+    V_TABLESPACE_IDX VARCHAR2(25 CHAR):= '#TABLESPACE_INDEX#'; -- Configuracion Tablespace de Indices
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.  
+    V_NUM_SEQ NUMBER(16); -- Vble. para validar la existencia de una secuencia.  
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+
+    V_TEXT1 VARCHAR2(2400 CHAR); -- Vble. auxiliar
+    V_TEXT_TABLA VARCHAR2(2400 CHAR) := 'ACT_APR_ADJUNTO_PROVEEDOR'; -- Vble. auxiliar para almacenar el nombre de la tabla de ref.
+    V_COMMENT_TABLE VARCHAR2(500 CHAR):= 'Tabla para gestionar los adjuntos de un proveedor.'; -- Vble. para los comentarios de las tablas
+
+BEGIN
+
+
+	DBMS_OUTPUT.PUT_LINE('********' ||V_TEXT_TABLA|| '********'); 
+	DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.'||V_TEXT_TABLA||'... Comprobaciones previas');
+	
+	
+	-- Verificar si la tabla ya existe
+	V_MSQL := 'SELECT COUNT(1) FROM ALL_TABLES WHERE TABLE_NAME = '''||V_TEXT_TABLA||''' and owner = '''||V_ESQUEMA||'''';
+	EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;	
+	IF V_NUM_TABLAS = 1 THEN
+		DBMS_OUTPUT.PUT_LINE('[INFO] ' || V_ESQUEMA || '.'||V_TEXT_TABLA||'... Ya existe. Se borrará.');
+		EXECUTE IMMEDIATE 'DROP TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' CASCADE CONSTRAINTS';
+		
+	END IF;
+
+	-- Comprobamos si existe la secuencia
+	V_SQL := 'SELECT COUNT(1) FROM ALL_SEQUENCES WHERE SEQUENCE_NAME = ''S_'||V_TEXT_TABLA||''' and SEQUENCE_OWNER = '''||V_ESQUEMA||'''';
+	EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS; 
+	IF V_NUM_TABLAS = 1 THEN
+		DBMS_OUTPUT.PUT_LINE('[INFO] '|| V_ESQUEMA ||'.S_'||V_TEXT_TABLA||'... Ya existe. Se borrará.');  
+		EXECUTE IMMEDIATE 'DROP SEQUENCE '||V_ESQUEMA||'.S_'||V_TEXT_TABLA||'';
+		
+	END IF; 
+	
+	
+	-- Creamos la tabla
+	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA|| '.'||V_TEXT_TABLA||'...');
+	V_MSQL := 'CREATE TABLE ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'
+	(
+		APR_ID		           		NUMBER(16,0)			NOT NULL,
+		PVE_ID						NUMBER(16,0)			NOT NULL,
+		DD_TPD_ID					NUMBER(16,0)			NOT NULL,
+		ADJ_ID						NUMBER(16,0),
+		APR_NOMBRE					VARCHAR2(255 CHAR),
+		APR_CONTENT_TYPE			VARHCAR2(100 CHAR),
+		APR_LENGTH					NUMBER(16,0),
+		APR_DESCRIPCION				VARCHAR2(1024 CHAR),
+		APR_FECHA_DOCUMENTO			DATE,
+		DD_EDC_ID					NUMBER(16,0),
+		VERSION 					NUMBER(38,0) 				DEFAULT 0 NOT NULL ENABLE, 
+		USUARIOCREAR 				VARCHAR2(50 CHAR) 			NOT NULL ENABLE, 
+		FECHACREAR 					TIMESTAMP (6) 				NOT NULL ENABLE, 
+		USUARIOMODIFICAR 			VARCHAR2(50 CHAR), 
+		FECHAMODIFICAR 				TIMESTAMP (6), 
+		USUARIOBORRAR 				VARCHAR2(50 CHAR), 
+		FECHABORRAR 				TIMESTAMP (6), 
+		BORRADO 					NUMBER(1,0) 				DEFAULT 0 NOT NULL ENABLE
+	)
+	LOGGING 
+	NOCOMPRESS 
+	NOCACHE
+	NOPARALLEL
+	NOMONITORING
+	';
+	EXECUTE IMMEDIATE V_MSQL;
+	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'... Tabla creada.');
+	
+
+	-- Creamos indice	
+	V_MSQL := 'CREATE UNIQUE INDEX '||V_ESQUEMA||'.'||V_TEXT_TABLA||'_PK ON '||V_ESQUEMA|| '.'||V_TEXT_TABLA||'(APR_ID) TABLESPACE '||V_TABLESPACE_IDX;		
+	EXECUTE IMMEDIATE V_MSQL;
+	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'_PK... Indice creado.');
+	
+	
+	-- Creamos primary key
+	V_MSQL := 'ALTER TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' ADD (CONSTRAINT '||V_TEXT_TABLA||'_PK PRIMARY KEY (APR_ID) USING INDEX)';
+	EXECUTE IMMEDIATE V_MSQL;
+	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'_PK... PK creada.');
+	
+	
+	-- Creamos sequence
+	V_MSQL := 'CREATE SEQUENCE '||V_ESQUEMA||'.S_'||V_TEXT_TABLA||'';		
+	EXECUTE IMMEDIATE V_MSQL;		
+	DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.S_'||V_TEXT_TABLA||'... Secuencia creada');
+
+	
+	-- Creamos comentario	
+	V_MSQL := 'COMMENT ON TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' IS '''||V_COMMENT_TABLE||'''';		
+	EXECUTE IMMEDIATE V_MSQL;
+	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'... Comentario creado.');
+	
+	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'... OK');
+
+	--Comprobamos si existe foreign key FK_PVE_ID
+	V_MSQL := 'SELECT COUNT(1) FROM ALL_CONSTRAINTS WHERE CONSTRAINT_NAME= ''FK_PVE_ID'' and TABLE_NAME='''||V_TEXT_TABLA||''' and owner = '''||V_ESQUEMA||'''';
+	EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;
+	
+	IF V_NUM_TABLAS = 1 THEN
+		DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.PVE_ID... Ya existe. No hacemos nada.');		
+	ELSE
+	-- Creamos foreign key FK_PVE_ID
+		V_MSQL := 'ALTER TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' ADD (CONSTRAINT FK_PVE_ID FOREIGN KEY (PVE_ID) REFERENCES '||V_ESQUEMA||'.ACT_PVE_PROVEEDOR (PVE_ID) ON DELETE SET NULL)';
+		EXECUTE IMMEDIATE V_MSQL;
+		DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.FK_PVE_ID... Foreign key creada.');
+	END IF;
+	
+	--Comprobamos si existe foreign key FK_DD_TPD_ID
+	V_MSQL := 'SELECT COUNT(1) FROM ALL_CONSTRAINTS WHERE CONSTRAINT_NAME= ''FK_DD_TPD_ID'' and TABLE_NAME='''||V_TEXT_TABLA||''' and owner = '''||V_ESQUEMA||'''';
+	EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;
+	
+	IF V_NUM_TABLAS = 1 THEN
+		DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.DD_TPD_ID... Ya existe. No hacemos nada.');		
+	ELSE
+	-- Creamos foreign key FK_DD_TPD_ID
+		V_MSQL := 'ALTER TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' ADD (CONSTRAINT FK_DD_TPD_ID FOREIGN KEY (DD_TPD_ID) REFERENCES '||V_ESQUEMA||'.DD_TPD_TIPO_DOCUMENTO (DD_TPD_ID) ON DELETE SET NULL)';
+		EXECUTE IMMEDIATE V_MSQL;
+		DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.FK_DD_TPD_ID... Foreign key creada.');
+	END IF;
+	
+	--Comprobamos si existe foreign key FK_ADJ_ID
+	V_MSQL := 'SELECT COUNT(1) FROM ALL_CONSTRAINTS WHERE CONSTRAINT_NAME= ''FK_ADJ_ID'' and TABLE_NAME='''||V_TEXT_TABLA||''' and owner = '''||V_ESQUEMA||'''';
+	EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;
+	
+	IF V_NUM_TABLAS = 1 THEN
+		DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.ADJ_ID... Ya existe. No hacemos nada.');		
+	ELSE
+	-- Creamos foreign key FK_ADJ_ID
+		V_MSQL := 'ALTER TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' ADD (CONSTRAINT FK_ADJ_ID FOREIGN KEY (ADJ_ID) REFERENCES '||V_ESQUEMA||'.ADJ_ADJUNTOS (ADJ_ID) ON DELETE SET NULL)';
+		EXECUTE IMMEDIATE V_MSQL;
+		DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.FK_ADJ_ID... Foreign key creada.');
+	END IF;
+	
+	--Comprobamos si existe foreign key FK_DD_EDP_ID
+	V_MSQL := 'SELECT COUNT(1) FROM ALL_CONSTRAINTS WHERE CONSTRAINT_NAME= ''FK_DD_EDP_ID'' and TABLE_NAME='''||V_TEXT_TABLA||''' and owner = '''||V_ESQUEMA||'''';
+	EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;
+	
+	IF V_NUM_TABLAS = 1 THEN
+		DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.DD_EDP_ID... Ya existe. No hacemos nada.');		
+	ELSE
+	-- Creamos foreign key FK_DD_EPD_ID
+		V_MSQL := 'ALTER TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' ADD (CONSTRAINT FK_DD_EDP_ID FOREIGN KEY (DD_EDP_ID) REFERENCES '||V_ESQUEMA||'.DD_EDP_ESTADO_DOC_PROVEEDOR (DD_EDP_ID) ON DELETE SET NULL)';
+		EXECUTE IMMEDIATE V_MSQL;
+		DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.FK_DD_EDP_ID... Foreign key creada.');
+	END IF;
+	
+		
+		V_TEXT1 := 'Código identificador único de adjunto proveedor.';
+		EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.APR_ID IS '''||V_TEXT1||'''  ';
+		
+		V_TEXT1 := 'Código identificador único del proveedor.';
+		EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.PVE_ID IS '''||V_TEXT1||'''  ';
+		
+		V_TEXT1 := 'Tipo de documento.';
+		EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.DD_TPD_ID IS '''||V_TEXT1||'''  ';
+		
+		V_TEXT1 := 'Localizador/clave/ruta del documento.';
+		EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.ADJ_ID IS '''||V_TEXT1||'''  ';
+		
+		V_TEXT1 := 'Nombre descriptivo del documento.';
+		EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.APR_NOMBRE IS '''||V_TEXT1||'''  ';
+		
+		V_TEXT1 := 'Tipo de contenido del documento.';
+		EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.APR_CONTENT_TYPE IS '''||V_TEXT1||'''  ';
+		
+		V_TEXT1 := 'Tamaño en bytes del documento.';
+		EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.APR_LENGTH IS '''||V_TEXT1||'''  ';
+		
+		V_TEXT1 := 'Descripción breve del documento.';
+		EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.APR_DESCRIPCION IS '''||V_TEXT1||'''  ';
+		
+		V_TEXT1 := 'Fecha de subida del documento.';
+		EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.APR_FECHA_DOCUMENTO IS '''||V_TEXT1||'''  ';
+		
+		V_TEXT1 := 'Estado del documento.';
+		EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.DD_EDC_ID IS '''||V_TEXT1||'''  ';
+		
+		V_TEXT1 := 'Indica la version del registro.';
+		EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.VERSION IS '''||V_TEXT1||'''  ';
+		
+		V_TEXT1 := 'Indica el usuario que creo el registro.';
+		EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.USUARIOCREAR IS '''||V_TEXT1||'''  ';
+		
+		V_TEXT1 := 'Indica la fecha en la que se creo el registro.';
+		EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.FECHACREAR IS '''||V_TEXT1||'''  ';
+		
+		V_TEXT1 := 'Indica el usuario que modificó el registro.';
+		EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.USUARIOMODIFICAR IS '''||V_TEXT1||'''  ';
+		
+		V_TEXT1 := 'Indica la fecha en la que se modificó el registro.';
+		EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.FECHAMODIFICAR IS '''||V_TEXT1||'''  ';
+		
+		V_TEXT1 := 'Indica el usuario que borró el registro.';
+		EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.USUARIOBORRAR IS '''||V_TEXT1||'''  ';
+		
+		V_TEXT1 := 'Indica la fecha en la que se borró el registro.';
+		EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.FECHABORRAR IS '''||V_TEXT1||'''  ';
+		
+		V_TEXT1 := 'Indicador de borrado.';
+		EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.BORRADO IS '''||V_TEXT1||'''  ';
+		
+		
+		
+		
+	COMMIT;
+
+
+EXCEPTION
+     WHEN OTHERS THEN 
+         DBMS_OUTPUT.PUT_LINE('KO!');
+          err_num := SQLCODE;
+          err_msg := SQLERRM;
+
+          DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(err_num));
+          DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+          DBMS_OUTPUT.put_line(err_msg);
+
+          ROLLBACK;
+          RAISE;          
+
+END;
+
+/
+
+EXIT

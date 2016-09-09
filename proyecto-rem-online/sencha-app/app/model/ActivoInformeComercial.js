b41d1ba985ec0d6bb2f42a4f0b525fbc04efbb0e
Kevin Fernandez <kevin.fernandez@pfsgroup.es>
2016-09-09 11:50:14 +0200

    HREOS-799, correccion en fechas valores economicos
    Former-commit-id: f323dd3f4268f6c0aadbba5129c1fc990bd19822

--- proyecto-rem-online/sencha-app/app/model/ActivoInformeComercial.js
+++ proyecto-rem-online/sencha-app/app/model/ActivoInformeComercial.js
@@ -176,11 +176,13 @@ Ext.define('HreRem.model.ActivoInformeComercial', {
     		},
     		{
     			name:'fechaEstimacionVenta',
-    			type:'date'
+    			type:'date',
+    			dateFormat: 'c'
     		},
      		{
     			name:'fechaEstimacionRenta',
-    			type:'date'
+    			type:'date',
+    			dateFormat: 'c'
     		},
     		{
     			name: 'inferiorMunicipioCodigo'

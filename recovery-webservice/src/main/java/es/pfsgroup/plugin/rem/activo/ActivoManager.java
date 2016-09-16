bbe6e0d9e6e7c81d168342c428d16514f528f9e1
josemanuel.villel <josemanuel.villel@pfsgroup.es>
2016-09-16 14:13:43 +0200

    Bugs Version 2.0.0-160923
    Former-commit-id: ee8afbaafbbdfd907439916421f42a55f7249682

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/activo/ActivoManager.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/activo/ActivoManager.java
@@ -86,6 +86,7 @@ import es.pfsgroup.plugin.rem.model.Visita;
 import es.pfsgroup.plugin.rem.model.dd.DDEstadoOferta;
 import es.pfsgroup.plugin.rem.model.dd.DDEstadoPublicacion;
 import es.pfsgroup.plugin.rem.model.dd.DDEstadosExpedienteComercial;
+import es.pfsgroup.plugin.rem.model.dd.DDEstadosVisitaOferta;
 import es.pfsgroup.plugin.rem.model.dd.DDSituacionComercial;
 import es.pfsgroup.plugin.rem.model.dd.DDSubtipoTrabajo;
 import es.pfsgroup.plugin.rem.model.dd.DDTipoDocumentoActivo;
@@ -329,9 +330,15 @@ public class ActivoManager extends BusinessOperationOverrider<ActivoApi> impleme
 					}
 					
 					if(!listaVisitasCliente.isEmpty()) {
-						oferta.setVisita(listaVisitasCliente.get(0));			
-						genericDao.save(Oferta.class, oferta);
+						oferta.setVisita(listaVisitasCliente.get(0));
+						DDEstadosVisitaOferta estadoVisitaOferta = (DDEstadosVisitaOferta) utilDiccionarioApi.dameValorDiccionarioByCod(DDEstadosVisitaOferta.class, DDEstadosVisitaOferta.ESTADO_VISITA_OFERTA_REALIZADA);
+						oferta.setEstadoVisitaOferta(estadoVisitaOferta);
+					} else {
+						DDEstadosVisitaOferta estadoVisitaOferta = (DDEstadosVisitaOferta) utilDiccionarioApi.dameValorDiccionarioByCod(DDEstadosVisitaOferta.class, DDEstadosVisitaOferta.ESTADO_VISITA_OFERTA_PENDIENTE);
+						oferta.setEstadoVisitaOferta(estadoVisitaOferta);
 					}
+					
+					genericDao.save(Oferta.class, oferta);
 
 				}
 			}

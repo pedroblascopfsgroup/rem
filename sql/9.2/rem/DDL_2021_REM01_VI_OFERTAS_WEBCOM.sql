7c3cf2748ca77cbab5b18ab0abe1a51281478ad4
Anahuac <anahuac.devicente@pfsgroup.es>
2016-09-21 20:14:16 +0200

    HREOS-870 - Crear vista y tabla historica ofertas webcom
    Former-commit-id: 27f23b62eff166bba379aba1cb46941de5ae7e40

---
+++ sql/9.2/rem/DDL_2021_REM01_VI_OFERTAS_WEBCOM.sql
@@ -0,0 +1,169 @@
+--/*
+--##########################################
+--## AUTOR=ANAHUAC DE VICENTE
+--## FECHA_CREACION=20160919
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.2
+--## INCIDENCIA_LINK=0
+--## PRODUCTO=NO
+--## Finalidad: Tabla para almacentar el historico de las ofertas enviadas a webcom
+--##           
+--## INSTRUCCIONES: Configurar las variables necesarias en el principio del DECLARE
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+
+--Para permitir la visualización de texto en un bloque PL/SQL utilizando DBMS_OUTPUT.PUT_LINE
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+
+DECLARE
+    seq_count number(3); -- Vble. para validar la existencia de las Secuencias.
+    table_count number(3); -- Vble. para validar la existencia de las Tablas.
+    v_column_count number(3); -- Vble. para validar la existencia de las Columnas.    
+    v_constraint_count number(3); -- Vble. para validar la existencia de las Constraints.
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla. 
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    err_num NUMBER; -- N?mero de errores
+    err_msg VARCHAR2(2048); -- Mensaje de error
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquemas
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquemas
+    V_TABLESPACE_IDX VARCHAR2(25 CHAR):= '#TABLESPACE_INDEX#'; -- Configuracion Tablespace de Indices
+    V_TEXT_VISTA VARCHAR2(2400 CHAR) := 'VI_OFERTAS_WEBCOM'; -- Vble. auxiliar para almacenar el nombre de la tabla de ref.
+    V_TEXT_TABLA VARCHAR2(2400 CHAR) := 'OWH_OFERTAS_WEBCOM_HIST'; -- Vble. auxiliar para almacenar el nombre de la tabla de ref.
+    V_MSQL VARCHAR2(4000 CHAR); 
+	V_COMMENT_TABLE VARCHAR2(500 CHAR):= 'Tabla para almacentar el historico de las ofertas enviadas a webcom.'; -- Vble. para los comentarios de las tablas
+
+    CUENTA NUMBER;
+    
+BEGIN
+	
+
+	DBMS_OUTPUT.PUT_LINE('********' ||V_TEXT_VISTA|| '********'); 
+	DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.'||V_TEXT_VISTA||'... Comprobaciones previas');
+		
+	DBMS_OUTPUT.PUT_LINE('[INFO] Verificamos si existe vista '||V_ESQUEMA||'.'||V_TEXT_VISTA||'..');	
+	SELECT COUNT(*) INTO CUENTA FROM ALL_OBJECTS WHERE OBJECT_NAME = V_TEXT_VISTA AND OWNER = V_ESQUEMA AND OBJECT_TYPE = 'MATERIALIZED VIEW';
+	IF CUENTA>0 THEN
+		DBMS_OUTPUT.PUT_LINE('Vista materializada: '|| V_ESQUEMA ||'.'|| V_TEXT_VISTA ||' existe, se procede a eliminarla..');
+		EXECUTE IMMEDIATE 'DROP MATERIALIZED VIEW ' || V_ESQUEMA || '.'|| V_TEXT_VISTA ||'';  
+		DBMS_OUTPUT.PUT_LINE('Vista materializada borrada OK');
+	END IF;
+	
+	DBMS_OUTPUT.PUT_LINE('[INFO] Verificamos si existe vista materializada '||V_ESQUEMA||'.'||V_TEXT_VISTA||'..');
+	SELECT COUNT(*) INTO CUENTA FROM ALL_OBJECTS WHERE OBJECT_NAME = V_TEXT_VISTA AND OWNER = V_ESQUEMA AND OBJECT_TYPE = 'VIEW';  
+	IF CUENTA>0 THEN
+		DBMS_OUTPUT.PUT_LINE('Vista: '|| V_ESQUEMA ||'.'|| V_TEXT_VISTA ||' existe, se procede a eliminarla..');
+		EXECUTE IMMEDIATE 'DROP VIEW ' || V_ESQUEMA || '.'|| V_TEXT_VISTA ||'';  
+		DBMS_OUTPUT.PUT_LINE('Vista borrada OK');
+	END IF;
+  
+  
+  	DBMS_OUTPUT.PUT_LINE('********' ||V_TEXT_TABLA|| '********'); 
+	DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.'||V_TEXT_TABLA||'... Comprobaciones previas');
+  
+	DBMS_OUTPUT.PUT_LINE('[INFO] Verificamos si existe tabla '||V_ESQUEMA||'.'||V_TEXT_TABLA||'..');
+	V_MSQL := 'SELECT COUNT(1) FROM ALL_TABLES WHERE TABLE_NAME = '''||V_TEXT_TABLA||''' and owner = '''||V_ESQUEMA||'''';
+	EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;	
+	IF V_NUM_TABLAS = 1 THEN
+		DBMS_OUTPUT.PUT_LINE('[INFO] ' || V_ESQUEMA || '.'||V_TEXT_TABLA||'... Ya existe. Se borrará.');
+		EXECUTE IMMEDIATE 'DROP TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' CASCADE CONSTRAINTS';		
+	END IF;
+
+	DBMS_OUTPUT.PUT_LINE('[INFO] Verificamos si existe secuencia '||V_ESQUEMA||'.S_'||V_TEXT_TABLA||'..');
+	V_SQL := 'SELECT COUNT(1) FROM ALL_SEQUENCES WHERE SEQUENCE_NAME = ''S_'||V_TEXT_TABLA||''' and SEQUENCE_OWNER = '''||V_ESQUEMA||'''';
+	EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS; 
+	IF V_NUM_TABLAS = 1 THEN
+		DBMS_OUTPUT.PUT_LINE('[INFO] '|| V_ESQUEMA ||'.S_'||V_TEXT_TABLA||'... Ya existe. Se borrará.');  
+		EXECUTE IMMEDIATE 'DROP SEQUENCE '||V_ESQUEMA||'.S_'||V_TEXT_TABLA||'';		
+	END IF; 
+
+   -- Creamos vista materializada
+	DBMS_OUTPUT.PUT_LINE('[INFO] Crear nueva vista materializada : '|| V_ESQUEMA ||'.'|| V_TEXT_VISTA ||'..');
+  	EXECUTE IMMEDIATE 'CREATE MATERIALIZED VIEW ' || V_ESQUEMA || '.'|| V_TEXT_VISTA ||' 
+	AS
+		SELECT 
+		CAST(OFR.OFR_WEBCOM_ID AS NUMBER(16,0)) 												AS ID_OFERTA_WEBCOM,
+		CAST(OFR.OFR_NUM_OFERTA AS NUMBER(16,0)) 												AS ID_OFERTA_REM,
+	    CASE WHEN (OFR.AGR_ID IS NOT NULL)
+			THEN CAST((SELECT ACT.ACT_NUM_ACTIVO 
+        		FROM '||V_ESQUEMA||'.ACT_AGR_AGRUPACION AGR, '||V_ESQUEMA||'.ACT_ACTIVO ACT 
+        		WHERE AGR.AGR_ID = OFR.AGR_ID
+        		AND ACT.ACT_ID = AGR.AGR_ACT_PRINCIPAL) AS NUMBER(16,0)) 
+			ELSE CAST((SELECT ACT.ACT_NUM_ACTIVO 
+		        FROM '||V_ESQUEMA||'.ACT_OFR AOF, '||V_ESQUEMA||'.ACT_ACTIVO ACT 
+		        WHERE AOF.OFR_ID = OFR.OFR_ID 
+		        AND ACT.ACT_ID = AOF.ACT_ID)  AS NUMBER(16,0))
+		END 																					ID_ACTIVO_HAYA,  
+		CAST(DDEOF.DD_EOF_CODIGO AS VARCHAR2(5 CHAR)) 											AS COD_ESTADO_OFERTA,
+		CAST(DDEEC.DD_EEC_CODIGO AS VARCHAR2(5 CHAR)) 											AS COD_ESTADO_EXPEDIENTE,
+		CASE WHEN (DDEEC.DD_EEC_CODIGO = ''08'')
+		      THEN CAST(''1'' AS NUMBER(1,0))
+		      ELSE CAST(''0'' AS NUMBER(1,0))
+	    END VENDIDO, 
+	    CASE WHEN (OFR.FECHAMODIFICAR IS NOT NULL)
+			THEN CAST(TO_CHAR(OFR.FECHAMODIFICAR,''YYYY-MM-DD'') ||
+				''T'' ||TO_CHAR(OFR.FECHACREAR,''HH24:MI:SS'') AS VARCHAR2(50 CHAR))
+			ELSE CAST(TO_CHAR(OFR.FECHACREAR,''YYYY-MM-DD'') ||
+				''T'' ||TO_CHAR(OFR.FECHACREAR,''HH24:MI:SS'') AS VARCHAR2(50 CHAR))
+		END 																					FECHA_ACCION,
+		CASE WHEN (OFR.USUARIOMODIFICAR IS NOT NULL)
+			THEN CAST((SELECT USU.USU_ID FROM '||V_ESQUEMA_M||'.USU_USUARIOS USU
+				WHERE USU.USU_USERNAME = OFR.USUARIOMODIFICAR) AS NUMBER(16,0))
+			ELSE CAST((SELECT USU.USU_ID FROM '||V_ESQUEMA_M||'.USU_USUARIOS USU
+				WHERE USU.USU_USERNAME = OFR.USUARIOCREAR) AS NUMBER(16,0))
+		END 																			   		ID_USUARIO_REM_ACCION
+		FROM OFR_OFERTAS OFR
+		LEFT JOIN '||V_ESQUEMA||'.ECO_EXPEDIENTE_COMERCIAL ECO ON ECO.OFR_ID = OFR.OFR_ID
+		LEFT JOIN '||V_ESQUEMA||'.DD_EEC_EST_EXP_COMERCIAL DDEEC ON DDEEC.DD_EEC_ID = ECO.DD_EEC_ID
+		LEFT JOIN '||V_ESQUEMA||'.DD_EOF_ESTADOS_OFERTA DDEOF ON DDEOF.DD_EOF_ID = OFR.DD_EOF_ID
+		WHERE ACT.USUARIOMODIFICAR IS NOT NULL AND ACT.USUARIOMODIFICAR != ''REST-USER''';
+		
+ 		DBMS_OUTPUT.PUT_LINE('[INFO] Vista materializada : '|| V_ESQUEMA ||'.'|| V_TEXT_VISTA ||'... creada');
+
+		
+		
+		-- Creamos tabla
+		DBMS_OUTPUT.PUT_LINE('[INFO] Crear nueva tabla : '|| V_ESQUEMA ||'.'|| V_TEXT_TABLA ||' a partir de la vista materializada ');
+		EXECUTE IMMEDIATE 'CREATE TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' AS  (SELECT * FROM '||V_ESQUEMA||'.'||V_TEXT_VISTA||')';	
+		DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'... Tabla creada.');	
+
+		-- Creamos indice	
+		V_MSQL := 'CREATE UNIQUE INDEX '||V_ESQUEMA||'.'||V_TEXT_TABLA||'_IDX ON '||V_ESQUEMA|| '.'||V_TEXT_TABLA||'(ID_OFERTA_WEBCOM) TABLESPACE '||V_TABLESPACE_IDX;		
+		EXECUTE IMMEDIATE V_MSQL;
+		DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'_IDX... Indice creado.');	
+	
+		-- Creamos primary key
+		V_MSQL := 'ALTER TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' ADD (CONSTRAINT '||V_TEXT_TABLA||'_PK PRIMARY KEY (ID_OFERTA_WEBCOM) USING INDEX)';
+		EXECUTE IMMEDIATE V_MSQL;
+		DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'_PK... PK creada.');	
+	
+		-- Creamos comentario	
+		V_MSQL := 'COMMENT ON TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' IS '''||V_COMMENT_TABLE||'''';		
+		EXECUTE IMMEDIATE V_MSQL;
+		DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'... Comentario creado.');	
+	
+	
+		DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'... OK');
+	COMMIT;
+
+
+EXCEPTION
+     WHEN OTHERS THEN 
+         DBMS_OUTPUT.PUT_LINE('KO!');
+          err_num := SQLCODE;
+          err_msg := SQLERRM;
+
+          DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(err_num));
+          DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+          DBMS_OUTPUT.put_line(err_msg);
+
+          ROLLBACK;
+          RAISE;          
+
+END;
+/
+
+EXIT;

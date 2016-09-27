afb95867fb6d445b2b85c0862b2e0ab5de4213a5
Anahuac <anahuac.devicente@pfsgroup.es>
2016-09-27 18:23:33 +0200

    HREOS-832 - Modificación respuestas para que devuelva los id's para los errores generales
    Former-commit-id: bdfb1c0d82bdce4ca67a5224610ef4674768fc71

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/rest/api/RestApi.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/rest/api/RestApi.java
@@ -20,7 +20,7 @@ public interface RestApi {
 	public static final String REST_MSG_BROKER_NOT_EXIST = "BROKER_NOT_EXIST";
 	public static final String REST_MSG_INVALID_SIGNATURE = "INVALID_SIGNATURE";
 	public static final String REST_MSG_REPETEAD_REQUEST = "REPETEAD_REQUEST";
-	public static final String REST_MSG_REQUEST_WITHOUT_DATA = "REQUEST_WITHOUT_DATA";
+	public static final String REST_MSG_MISSING_REQUIRED_FIELDS = "MISSING_REQUIRED_FIELDS";
 	public static final String REST_MSG_INVALID_WORKINGCODE = "INVALID_WORKINGCODE";
 	public static final String REST_LOGGED_USER_USERNAME = "USER";
 	public static final Object REST_LOGGED_USER_EMPTY_PASSWORD = "";

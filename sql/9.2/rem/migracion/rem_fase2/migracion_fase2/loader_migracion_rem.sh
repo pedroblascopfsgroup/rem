a1aae8d030020f5277b02876fdf17ada20a18f3b
Manuel Rodriguez Sajardo <manuel.rodriguez@pfsgroup.es>
2016-09-28 13:12:00 +0200

    [HREOS-855] DDLs y CTLs Fase 2
    Former-commit-id: e0aead452cbc53e71dd7e188b2c28e5776bdd953

--- sql/9.2/rem/migracion/rem_fase2/migracion_fase2/loader_migracion_rem.sh
+++ sql/9.2/rem/migracion/rem_fase2/migracion_fase2/loader_migracion_rem.sh
@@ -20,46 +20,52 @@ export NLS_LANG=SPANISH_SPAIN.WE8ISO8859P1
 echo "Se ha establecido la variable de entorno NLS_LANG=SPANISH_SPAIN.WE8ISO8859P1"
 echo "Comienzo de la migración."
 
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/CLIENTES_COMERCIALES.ctl      		log=./CTLs_DATs/logs/CLIENTES_COMERCIALES.log
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/VISITAS.ctl              				log=./CTLs_DATs/logs/VISITAS.log
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/OFERTAS.ctl      						log=./CTLs_DATs/logs/OFERTAS.log
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/OFERTAS_ACTIVO.ctl        			log=./CTLs_DATs/logs/OFERTAS_ACTIVO.log
-
-
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/CLIENTES_COMERCIALES.ctl				log=./CTLs_DATs/logs/CLIENTES_COMERCIALES.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/VISITAS.ctl							log=./CTLs_DATs/logs/VISITAS.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/OFERTAS.ctl							log=./CTLs_DATs/logs/OFERTAS.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/OFERTAS_ACTIVO.ctl					log=./CTLs_DATs/logs/OFERTAS_ACTIVO.log
 $ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/CONDICIONANTE_OFERTA_ACEPTADA.ctl 	log=./CTLs_DATs/logs/CONDICIONANTE_OFERTA_ACEPTADA.log
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/RESERVAS.ctl  						log=./CTLs_DATs/logs/RESERVAS.log
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/ENTREGAS_RESERVAS.ctl    				log=./CTLs_DATs/logs/ENTREGAS_RESERVAS.log
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/TITULARES_ADICIONALES_OFERTA.ctl      log=./CTLs_DATs/logs/TITULARES_ADICIONALES_OFERTA.log
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/COMPRADORES.ctl       			    log=./CTLs_DATs/logs/COMPRADORES.log
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/COMPRADOR_EXPEDIENTE.ctl            	log=./CTLs_DATs/logs/COMPRADOR_EXPEDIENTE.log
-
-
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/COMPARECIENTES_VENDEDOR.ctl           log=./CTLs_DATs/logs/COMPARECIENTES_VENDEDOR.log
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/COMISIONES_GASTOS.ctl          		log=./CTLs_DATs/logs/COMISIONES_GASTOS.log
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/OFERTAS_OBSERVACIONES.ctl      		log=./CTLs_DATs/logs/OFERTAS_OBSERVACIONES.log
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/FORMALIZACIONES.ctl      				log=./CTLs_DATs/logs/FORMALIZACIONES.log
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/POSICIONAMIENTOS.ctl      			log=./CTLs_DATs/logs/POSICIONAMIENTOS.log
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/SUBSANACIONES.ctl      				log=./CTLs_DATs/logs/SUBSANACIONES.log
-
-
-
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/RESERVAS.ctl							log=./CTLs_DATs/logs/RESERVAS.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/ENTREGAS_RESERVAS.ctl					log=./CTLs_DATs/logs/ENTREGAS_RESERVAS.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/TITULARES_ADICIONALES_OFERTA.ctl		log=./CTLs_DATs/logs/TITULARES_ADICIONALES_OFERTA.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/COMPRADORES.ctl						log=./CTLs_DATs/logs/COMPRADORES.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/COMPRADOR_EXPEDIENTE.ctl				log=./CTLs_DATs/logs/COMPRADOR_EXPEDIENTE.log
+#COMITE_OFERTA_ACEPTADA
+#COMITES
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/COMPARECIENTES_VENDEDOR.ctl			log=./CTLs_DATs/logs/COMPARECIENTES_VENDEDOR.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/COMISIONES_GASTOS.ctl					log=./CTLs_DATs/logs/COMISIONES_GASTOS.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/OFERTAS_OBSERVACIONES.ctl				log=./CTLs_DATs/logs/OFERTAS_OBSERVACIONES.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/FORMALIZACIONES.ctl					log=./CTLs_DATs/logs/FORMALIZACIONES.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/POSICIONAMIENTOS.ctl					log=./CTLs_DATs/logs/POSICIONAMIENTOS.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/SUBSANACIONES.ctl						log=./CTLs_DATs/logs/SUBSANACIONES.log
+#Información comercial
+#Histórico estados informes comerciales
+#Datos de publicación 
 $ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/HIST_ESTADOS_PUBLICACIONES.ctl 		log=./CTLs_DATs/logs/HIST_ESTADOS_PUBLICACIONES.log
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/COND_ESPEC_ACTIVOS_PUBLICADOS.ctl     log=./CTLs_DATs/logs/COND_ESPEC_ACTIVOS_PUBLICADOS.log
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/ACTIVO_PRECIOS.ctl               		log=./CTLs_DATs/logs/ACTIVO_PRECIOS.log
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/HIST_ACTIVOS_PRECIOS.ctl   			log=./CTLs_DATs/logs/HIST_ACTIVOS_PRECIOS.log
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/PROPUESTAS_PRECIOS.ctl         		log=./CTLs_DATs/logs/PROPUESTAS_PRECIOS.log
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/ACTIVO_PROPUESTAS.ctl   				log=./CTLs_DATs/logs/ACTIVO_PROPUESTAS.log
-
-
-
-
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/PROVEEDORES.ctl                    	log=./CTLs_DATs/logs/PROVEEDORES.log
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/PROVEEDORES_DIRECCIONES.ctl        	log=./CTLs_DATs/logs/PROVEEDORES_DIRECCIONES.log
-$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/PROVEEDOR_CONTACTO.ctl        		log=./CTLs_DATs/logs/PROVEEDOR_CONTACTO.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/COND_ESPEC_ACTIVOS_PUBLICADOS.ctl		log=./CTLs_DATs/logs/COND_ESPEC_ACTIVOS_PUBLICADOS.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/ACTIVO_PRECIOS.ctl					log=./CTLs_DATs/logs/ACTIVO_PRECIOS.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/HIST_ACTIVOS_PRECIOS.ctl				log=./CTLs_DATs/logs/HIST_ACTIVOS_PRECIOS.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/PROPUESTAS_PRECIOS.ctl				log=./CTLs_DATs/logs/PROPUESTAS_PRECIOS.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/ACTIVO_PROPUESTAS.ctl					log=./CTLs_DATs/logs/ACTIVO_PROPUESTAS.log
+#Campañas -activo
+#Campañas
+#Fotos
+#Clientes
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/PROVEEDORES.ctl						log=./CTLs_DATs/logs/PROVEEDORES.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/PROVEEDORES_DIRECCIONES.ctl			log=./CTLs_DATs/logs/PROVEEDORES_DIRECCIONES.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/PROVEEDOR_CONTACTO.ctl				log=./CTLs_DATs/logs/PROVEEDOR_CONTACTO.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/GASTOS_PROVEEDORES.ctl				log=./CTLs_DATs/logs/GASTOS_PROVEEDORES.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/GASTOS_PROVEEDORES_ACTIVOS.ctl		log=./CTLs_DATs/logs/GASTOS_PROVEEDORES_ACTIVOS.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/GASTOS_PROVEEDORES _TRABAJOS.ctl		log=./CTLs_DATs/logs/GASTOS_PROVEEDORES _TRABAJOS.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/DETALLE_ECONOMICO_GASTOS.ctl			log=./CTLs_DATs/logs/DETALLE_ECONOMICO_GASTOS.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/GESTION_GASTOS.ctl					log=./CTLs_DATs/logs/GESTION_GASTOS.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/GASTOS_IMPUGNACIONES.ctl				log=./CTLs_DATs/logs/GASTOS_IMPUGNACIONES.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/GASTOS_INFORMACION_CONTABILIDAD.ctl	log=./CTLs_DATs/logs/GASTOS_INFORMACION_CONTABILIDAD.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/ACTIVOS.ctl							log=./CTLs_DATs/logs/ACTIVOS.log
+$ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/PERIMETRO_ACTIVOS.ctl					log=./CTLs_DATs/logs/PERIMETRO_ACTIVOS.log
 
 #$ORACLE_HOME/bin/sqlplus REM01/$1 @aux/Mig_estadisticas.sql > ./CTLs_DATs/logs/$0.log
 #if [ $? != 0 ] ; then echo -e "\n\n======>>> "Error en @aux/Mig_estadisticas.sql >> ./CTLs_DATs/logs/$0.log ; exit 1 ; fi
-
 
 
 echo "Fin de la carga de ficheros en Tablas MIG_. Revise directorio de logs y el directorio /bad."

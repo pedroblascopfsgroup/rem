4596fe028b7fbc3ac23101a051857da42452bd82
Luis Caballero <luis.caballero@pfsgroup.es>
2016-10-03 17:24:17 +0200

    HREOS-915 Pestaña Impugnación esqueleto
    Former-commit-id: 38e0eb34d9d36eed05601ef70290844a981b1294

---
+++ proyecto-rem-online/sencha-app/classic/src/view/gastos/ImpugnacionGasto.js
@@ -0,0 +1,89 @@
+Ext.define('HreRem.view.gastos.ImpugnacionGasto', {
+    extend: 'HreRem.view.common.FormBase',
+    xtype: 'impugnaciongasto',    
+    cls	: 'panel-base shadow-panel',
+    collapsed: false,
+    disableValidation: true,
+    reference: 'gestiongastoref',
+    scrollable	: 'y',
+//	recordName: "impugnacion",
+//	
+//	recordClass: "HreRem.model.ImpugnacionGasto",
+//    
+//    requires: ['HreRem.model.ImpugnacionGasto'],
+//    
+//    listeners: {
+//		boxready:'cargarTabData'
+//	},
+//    
+    initComponent: function () {
+
+        var me = this;
+        me.setTitle(HreRem.i18n('title.gasto.impugnacion'));
+        var items= [
+       
+	    	{   
+				xtype:'fieldsettable',
+//				collapsible: false,
+				items :
+					[
+						{
+							xtype:'datefieldbase',
+							formatter: 'date("d/m/Y")',
+							reference: 'fechaTopeImpugnacion',
+							fieldLabel: HreRem.i18n('fieldlabel.gasto.impugnacion.fecha.tope.impugnacion'),
+							bind: '{impugnacion.fechaTopeImpugnacion}',
+							maxValue: null
+						},
+						{
+							xtype:'datefieldbase',
+							formatter: 'date("d/m/Y")',
+							reference: 'fechaPresentacionImpugnacion',
+							fieldLabel: HreRem.i18n('fieldlabel.gasto.impugnacion.fecha.presentacion.impugnacion'),
+							bind: '{impugnacion.fechaPresentacionImpugnacion}',
+							maxValue: null
+						},
+						{
+							xtype:'datefieldbase',
+							formatter: 'date("d/m/Y")',
+							reference: 'fechaResolucionImpugnacion',
+							fieldLabel: HreRem.i18n('fieldlabel.gasto.impugnacion.fecha.resolucion.impugnacion'),
+							bind: '{impugnacion.fechaResolucionImpugnacion}',
+							maxValue: null
+						},
+						{ 
+							xtype:'comboboxfieldbase',
+							fieldLabel:  HreRem.i18n('fieldlabel.gasto.impugnacion.resultado'),
+							readOnly:true,
+							bind: {
+								store: '{comboResultadoImpugnacion}',
+								value: '{impugnacion.resultado}'
+							}
+						},
+						{ 
+		                	xtype: 'textareafieldbase',
+//		                	labelWidth: 200,
+//		                	rowspan: 4,
+//		                	height: 160,
+//		                	labelAlign: 'top',
+		                	fieldLabel: HreRem.i18n('fieldlabel.gasto.impugnacion.observaciones'),
+		                	bind:		'{impugnacion.observaciones}'
+		                }
+					]
+			}
+
+           
+    	];
+    
+	    me.addPlugin({ptype: 'lazyitems', items: items });
+	    
+	    me.callParent(); 
+    },
+    
+    funcionRecargar: function() {
+    	var me = this; 
+		me.recargar = false;		
+		me.lookupController().cargarTabData(me);
+    	
+    }
+});

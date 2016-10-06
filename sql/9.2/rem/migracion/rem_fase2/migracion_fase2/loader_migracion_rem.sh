2472f5f55e035d90362acdfe1c77f84429eafb8e
Manuel Rodriguez Sajardo <manuel.rodriguez@pfsgroup.es>
2016-10-06 16:47:29 +0200

    [HREOS-855] REM F2
    Former-commit-id: 3ad8a9505d23be04de27e448b7e181eb403c02d0

--- sql/9.2/rem/migracion/rem_fase2/migracion_fase2/loader_migracion_rem.sh
+++ sql/9.2/rem/migracion/rem_fase2/migracion_fase2/loader_migracion_rem.sh
@@ -26,13 +26,11 @@ $ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/OFERTAS.ctl							log=./CT
 $ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/OFERTAS_ACTIVO.ctl					log=./CTLs_DATs/logs/OFERTAS_ACTIVO.log
 $ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/CONDICIONANTE_OFERTA_ACEPTADA.ctl 	log=./CTLs_DATs/logs/CONDICIONANTE_OFERTA_ACEPTADA.log
 $ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/RESERVAS.ctl							log=./CTLs_DATs/logs/RESERVAS.log
-# ELIMINADA # $ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/ENTREGAS_RESERVAS.ctl					log=./CTLs_DATs/logs/ENTREGAS_RESERVAS.log
 $ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/TITULARES_ADICIONALES_OFERTA.ctl		log=./CTLs_DATs/logs/TITULARES_ADICIONALES_OFERTA.log
 $ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/COMPRADORES.ctl						log=./CTLs_DATs/logs/COMPRADORES.log
 $ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/COMPRADOR_EXPEDIENTE.ctl				log=./CTLs_DATs/logs/COMPRADOR_EXPEDIENTE.log
 #COMITE_OFERTA_ACEPTADA
 #COMITES
-# ELIMINADA # $ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/COMPARECIENTES_VENDEDOR.ctl			log=./CTLs_DATs/logs/COMPARECIENTES_VENDEDOR.log
 $ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/COMISIONES_GASTOS.ctl					log=./CTLs_DATs/logs/COMISIONES_GASTOS.log
 $ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/OFERTAS_OBSERVACIONES.ctl				log=./CTLs_DATs/logs/OFERTAS_OBSERVACIONES.log
 $ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/FORMALIZACIONES.ctl					log=./CTLs_DATs/logs/FORMALIZACIONES.log
@@ -43,7 +41,6 @@ $ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/SUBSANACIONES.ctl						log
 #Datos de publicación 
 $ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/HIST_ESTADOS_PUBLICACIONES.ctl 		log=./CTLs_DATs/logs/HIST_ESTADOS_PUBLICACIONES.log
 $ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/COND_ESPEC_ACTIVOS_PUBLICADOS.ctl		log=./CTLs_DATs/logs/COND_ESPEC_ACTIVOS_PUBLICADOS.log
-# ELIMINADA #  $ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/ACTIVO_PRECIOS.ctl					log=./CTLs_DATs/logs/ACTIVO_PRECIOS.log
 $ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/HIST_ACTIVOS_PRECIOS.ctl				log=./CTLs_DATs/logs/HIST_ACTIVOS_PRECIOS.log
 $ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/PROPUESTAS_PRECIOS.ctl				log=./CTLs_DATs/logs/PROPUESTAS_PRECIOS.log
 $ORACLE_HOME/bin/sqlldr REM01/$1  control=./CTLs_DATs/ACTIVO_PROPUESTAS.ctl					log=./CTLs_DATs/logs/ACTIVO_PROPUESTAS.log

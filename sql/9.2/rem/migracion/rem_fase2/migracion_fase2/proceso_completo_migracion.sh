795a67577ab8930692bf4ad10cb9c436167e9ec6
Manuel Rodriguez Sajardo <manuel.rodriguez@pfsgroup.es>
2016-10-11 09:31:13 +0200

    [HREOS-855] REM F2
    Former-commit-id: ec791bb7dfa338a8349bd5e00e571d36b8902e9e

--- sql/9.2/rem/migracion/rem_fase2/migracion_fase2/proceso_completo_migracion.sh
+++ sql/9.2/rem/migracion/rem_fase2/migracion_fase2/proceso_completo_migracion.sh
@@ -16,20 +16,14 @@ echo
 echo
 echo
 
-# [1/4] Lanzando script ./000_Preparacion_migracion.sh
 
 ./000_Preparacion_migracion.sh
 
-# [2/4] Lanzando script ./001_Creacion_de_tablas.sh...
 
 ./001_Creacion_de_tablas.sh $1
 
-# [3/4] Lanzando script ./002_Carga_de_tablas_de_migracion.sh...
 
-./003_Comprobacion_de_diccionarios.sh $1
+./002_Carga_de_tablas_de_migracion.sh $1
 
-# [4/4] Lanzando script ./003_Migrar.sh...
 
-./004_Migrar.sh $1
-
-echo "[INFO] Proceso de migración finalizado."
+./003_Migrar.sh $1

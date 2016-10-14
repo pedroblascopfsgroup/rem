b3b32c9e285dddbb0d1273b5bf5f20ca0ddf878f
Kevin Fernandez <kevin.fernandez@pfsgroup.es>
2016-10-14 10:35:40 +0200

    HREOS-926, inicio
    Former-commit-id: ebd4cc20b8228ed9a0ca381ebd53b72b379a8f78

--- proyecto-rem-online/sencha-app/classic/src/view/publicacion/configuracion/ConfiguracionPublicacionMain.js
+++ proyecto-rem-online/sencha-app/classic/src/view/publicacion/configuracion/ConfiguracionPublicacionMain.js
@@ -1,28 +1,23 @@
 Ext.define('HreRem.view.publicacion.configuracion.ConfiguracionPublicacionMain', {
 	extend		: 'Ext.panel.Panel',
     xtype		: 'configuracionpublicacionmain',
-    requires	: /* Componentes que van dentro: Búsqueda y grid */[],
+    requires	: [],
     layout: {
         type: 'vbox',
         align: 'stretch'
     },
 
     initComponent: function () {
-        
+
         var me = this;
-        
+
         me.setTitle(HreRem.i18n('title.publicaciones.configuracion'));
-        
+
         me.items = [
-                    /* Aquí metemos los xtype que tenemos que definir: Búsqueda y Grid con stock */
-        
+        	
         ];
         
-        me.callParent(); 
-
-        
+        me.callParent();
     }
-
-
 });
 

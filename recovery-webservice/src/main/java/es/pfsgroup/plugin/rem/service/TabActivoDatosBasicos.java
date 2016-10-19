9f6c71d9a9bc2657f94860bcc9a8fdbb66e2ddf2
Jorge Ros <jorge.ros@pfsgroup.es>
2016-10-19 19:18:39 +0200

    HREOS-1026 Correccion en TabActivoDatosBasicos
    Former-commit-id: 19c4aff385ea11fba229d957a224e581368518ba

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/service/TabActivoDatosBasicos.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/service/TabActivoDatosBasicos.java
@@ -190,7 +190,7 @@ public class TabActivoDatosBasicos implements TabActivoService {
 		if(activo.getAgrupaciones().size() > 0){
 			Boolean pertenceAgrupacionRestringida= false;
 			for(ActivoAgrupacionActivo agrupaciones: activo.getAgrupaciones()){
-				if(agrupaciones.getAgrupacion().getTipoAgrupacion().getCodigo().equals(DDTipoAgrupacion.AGRUPACION_RESTRINGIDA)){
+				if(DDTipoAgrupacion.AGRUPACION_RESTRINGIDA.equals(agrupaciones.getAgrupacion().getTipoAgrupacion().getCodigo())){
 					pertenceAgrupacionRestringida= true;
 					break;
 				}

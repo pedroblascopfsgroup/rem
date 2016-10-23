913d1a2c25b8ad801d08ccdcc1268268517782ab
sergio hernandez <sergio.hernandez@pfsgroup.es>
2016-10-23 20:29:27 +0200

    Sergio
    Former-commit-id: 1b7cf65261e60b4bacb71d007186423b28356956

--- sql/9.2/rem/migracion/fase1/DML_001_REM01_HAYA01_DD_CRA_CARTERA.sql
+++ sql/9.2/rem/migracion/fase1/DML_001_REM01_HAYA01_DD_CRA_CARTERA.sql
@@ -28,7 +28,7 @@ DECLARE
     V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.   
     ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
     ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
-	
+        
     V_TEXT1 VARCHAR2(2400 CHAR); -- Vble. auxiliar
     V_ENTIDAD_ID NUMBER(16);
     V_ID NUMBER(16);
@@ -37,18 +37,19 @@ DECLARE
     TYPE T_TIPO_DATA IS TABLE OF VARCHAR2(150);
     TYPE T_ARRAY_DATA IS TABLE OF T_TIPO_DATA;
     V_TIPO_DATA T_ARRAY_DATA := T_ARRAY_DATA(
-    	T_TIPO_DATA('03'	,''	,'Bankia'						,'Bankia'),
-    	T_TIPO_DATA('04'	,''	,'Cartera Terceros'						,'Cartera Terceros')
+        T_TIPO_DATA('03'        ,''     ,'Bankia'                                               ,'Bankia'),
+        T_TIPO_DATA('04'        ,''     ,'Otras carteras'                                               ,'Otras carteras')
+        T_TIPO_DATA('05'        ,''     ,'Sin definir'                                               ,'Sin definir')
     ); 
 
 
     V_TMP_TIPO_DATA T_TIPO_DATA;
     
-BEGIN	
-	
-	DBMS_OUTPUT.PUT_LINE('[INICIO] ');
+BEGIN   
+        
+        DBMS_OUTPUT.PUT_LINE('[INICIO] ');
 
-	 
+         
     -- LOOP para insertar los valores en DD_CRA_CARTERA -----------------------------------------------------------------
     DBMS_OUTPUT.PUT_LINE('[INFO]: INSERCION EN DD_CRA_CARTERA] ');
     FOR I IN V_TIPO_DATA.FIRST .. V_TIPO_DATA.LAST
@@ -61,15 +62,15 @@ BEGIN
         EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
         
         --Si existe lo modificamos
-        IF V_NUM_TABLAS > 0 THEN				
+        IF V_NUM_TABLAS > 0 THEN                                
           
           DBMS_OUTPUT.PUT_LINE('[INFO]: MODIFICAMOS EL REGISTRO '''|| TRIM(V_TMP_TIPO_DATA(1)) ||'''');
-       	  V_MSQL := 'UPDATE '|| V_ESQUEMA ||'.DD_CRA_CARTERA '||
+          V_MSQL := 'UPDATE '|| V_ESQUEMA ||'.DD_CRA_CARTERA '||
                     'SET DD_CRA_CIF = '''||TRIM(V_TMP_TIPO_DATA(2))||''''|| 
-					', DD_CRA_DESCRIPCION = '''||TRIM(V_TMP_TIPO_DATA(3))||''''|| 
-					', DD_CRA_DESCRIPCION_LARGA = '''||TRIM(V_TMP_TIPO_DATA(4))||''''||
-					', USUARIOMODIFICAR = ''MIGRAREM01BNK'' , FECHAMODIFICAR = SYSDATE '||
-					'WHERE DD_CRA_CODIGO = '''||TRIM(V_TMP_TIPO_DATA(1))||'''';
+                                        ', DD_CRA_DESCRIPCION = '''||TRIM(V_TMP_TIPO_DATA(3))||''''|| 
+                                        ', DD_CRA_DESCRIPCION_LARGA = '''||TRIM(V_TMP_TIPO_DATA(4))||''''||
+                                        ', USUARIOMODIFICAR = ''MIGRAREM01BNK'' , FECHAMODIFICAR = SYSDATE '||
+                                        'WHERE DD_CRA_CODIGO = '''||TRIM(V_TMP_TIPO_DATA(1))||'''';
           EXECUTE IMMEDIATE V_MSQL;
           DBMS_OUTPUT.PUT_LINE('[INFO]: REGISTRO MODIFICADO CORRECTAMENTE');
           
@@ -78,7 +79,7 @@ BEGIN
        
           DBMS_OUTPUT.PUT_LINE('[INFO]: INSERTAMOS EL REGISTRO '''|| TRIM(V_TMP_TIPO_DATA(1)) ||'''');   
           V_MSQL := 'SELECT '|| V_ESQUEMA ||'.S_DD_CRA_CARTERA.NEXTVAL FROM DUAL';
-          EXECUTE IMMEDIATE V_MSQL INTO V_ID;	
+          EXECUTE IMMEDIATE V_MSQL INTO V_ID;   
           V_MSQL := 'INSERT INTO '|| V_ESQUEMA ||'.DD_CRA_CARTERA (' ||
                       'DD_CRA_ID, DD_CRA_CODIGO, DD_CRA_CIF, DD_CRA_DESCRIPCION, DD_CRA_DESCRIPCION_LARGA, VERSION, USUARIOCREAR, FECHACREAR, BORRADO) ' ||
                       'SELECT '|| V_ID || ','''||V_TMP_TIPO_DATA(1)||''' ,'''||V_TMP_TIPO_DATA(2)||''','''||TRIM(V_TMP_TIPO_DATA(3))||''','''||TRIM(V_TMP_TIPO_DATA(4))||''', 0, ''MIGRAREM01BNK'',SYSDATE,0 FROM DUAL';

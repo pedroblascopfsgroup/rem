754f419824e698c67c6a578721e5edc1c46a6f9f
Jorge Ros <jorge.ros@pfsgroup.es>
2016-11-10 16:32:08 +0100

    HREOS-1130 Precios: Modificacion tramite y carga masiva propuesta precios
    Former-commit-id: 347b13fae3de6a83867fe7c1b7aa5d119370a0c1

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/activo/ActivoManager.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/activo/ActivoManager.java
@@ -106,6 +106,7 @@ import es.pfsgroup.plugin.rem.model.GastosExpediente;
 import es.pfsgroup.plugin.rem.model.Oferta;
 import es.pfsgroup.plugin.rem.model.PerimetroActivo;
 import es.pfsgroup.plugin.rem.model.PropuestaActivosVinculados;
+import es.pfsgroup.plugin.rem.model.PropuestaPrecio;
 import es.pfsgroup.plugin.rem.model.Reserva;
 import es.pfsgroup.plugin.rem.model.TareaActivo;
 import es.pfsgroup.plugin.rem.model.TitularesAdicionalesOferta;
@@ -119,6 +120,7 @@ import es.pfsgroup.plugin.rem.model.Visita;
 import es.pfsgroup.plugin.rem.model.dd.DDAccionGastos;
 import es.pfsgroup.plugin.rem.model.dd.DDCartera;
 import es.pfsgroup.plugin.rem.model.dd.DDEstadoOferta;
+import es.pfsgroup.plugin.rem.model.dd.DDEstadoPropuestaPrecio;
 import es.pfsgroup.plugin.rem.model.dd.DDEstadoPublicacion;
 import es.pfsgroup.plugin.rem.model.dd.DDEstadosExpedienteComercial;
 import es.pfsgroup.plugin.rem.model.dd.DDEstadosVisitaOferta;
@@ -2791,5 +2793,20 @@ public class ActivoManager extends BusinessOperationOverrider<ActivoApi> impleme
 		}
 
 		return dtoMov;
+	}
+	
+	@Override
+	@Transactional(readOnly = false)
+	public void actualizarFechaYEstadoCargaPropuesta(Long idPropuesta) {
+		Filter filtro = genericDao.createFilter(FilterType.EQUALS, "id", idPropuesta);
+		PropuestaPrecio propuesta = genericDao.get(PropuestaPrecio.class, filtro);
+		
+		propuesta.setFechaCarga(new Date());
+		
+		DDEstadoPropuestaPrecio estado = (DDEstadoPropuestaPrecio) utilDiccionarioApi
+				.dameValorDiccionarioByCod(DDEstadoPropuestaPrecio.class, DDEstadoPropuestaPrecio.ESTADO_CARGADA);
+		propuesta.setEstado(estado);
+		
+		genericDao.update(PropuestaPrecio.class, propuesta);
 	}
 }

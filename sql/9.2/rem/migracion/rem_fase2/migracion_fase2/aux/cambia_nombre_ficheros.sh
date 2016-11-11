1f903b503ebf872140a995b290e899ca95681792
Pablo Meseguer Benlloch <pablo.meseguer@pfsgroup.es>
2016-11-11 14:45:27 +0100

    [HREOS-855] Proceso migracion FASE 2
    Former-commit-id: 7a5ab3dc7b5612c88a7db154f212c96937a98c6a

--- sql/9.2/rem/migracion/rem_fase2/migracion_fase2/aux/cambia_nombre_ficheros.sh
+++ sql/9.2/rem/migracion/rem_fase2/migracion_fase2/aux/cambia_nombre_ficheros.sh
@@ -9,7 +9,7 @@ mv Fich_Agr_Agrupaciones.dat 											AGRUPACIONES.dat
 mv Fich_Clientes_Comerciales.dat 										CLIENTES_COMERCIALES.dat
 mv Fich_Historico_Publicacion.dat 										HIST_ESTADOS_PUBLICACIONES.dat
 mv Fich_Ofertas.dat 													OFERTAS.dat
-mv Fich_Ofertas_Alquiler.dat 											ACTIVOS_DATOS_ALQUILER.dat
+mv Fich_Ofertas_Alquiler.dat 											ACTIVOS_DATOS_ALQUILERES.dat
 mv Fich_Perimetro_Activo.dat 											PERIMETRO_ACTIVOS.dat
 mv Fich_Propietarios.dat 												PROPIETARIOS.dat
 mv Fich_Propuestas_Precio.dat 											PROPUESTAS_PRECIOS.dat

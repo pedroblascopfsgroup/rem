61ed01e096e5f4a5e04fdde74a3464d130ede077
Jose Vicente Jimenez <josevicente.jimenez@pfsgroup.es>
2016-11-18 14:01:49 +0100

    HREOS-1158 Correccion errores script
    Former-commit-id: 58140a8ef9ab5210785e6a9b6c5d91d4cb325944

--- sql/9.2/rem/DDL_2089_REM01_ALTER_ACT_PVE_PROVEEDOR_EVAL_PROP.sql
+++ sql/9.2/rem/DDL_2089_REM01_ALTER_ACT_PVE_PROVEEDOR_EVAL_PROP.sql
@@ -36,7 +36,7 @@ DECLARE
  
     V_TEXT1 VARCHAR2(2400 CHAR); -- Vble. auxiliar 
     V_TEXT_TABLA VARCHAR2(2400 CHAR) := 'ACT_PVE_PROVEEDOR'; -- Vble. auxiliar para almacenar el nombre de la tabla de ref.
-	V_CREAR_FK VARCHAR2(2 CHAR) := 'NO'; -- [SI, NO] Vble. para indicar al script si debe o no crear tambien las relaciones Foreign Keys.
+	V_CREAR_FK VARCHAR2(2 CHAR) := 'SI'; -- [SI, NO] Vble. para indicar al script si debe o no crear tambien las relaciones Foreign Keys.
 
 
     /* -- ARRAY CON NUEVAS COLUMNAS */

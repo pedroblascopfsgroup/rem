0e337502e1096c75a957330a4a708d775519b54f
Carlos Pons <carlos.pons@pfsgroup.es>
2016-11-23 17:11:16 +0100

    Generar PDF formalización de venta (HREOS-1085).
    Former-commit-id: 6655ad4360a793ec4abfac288619c32440c7d657

---
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/model/DtoComprador.java
@@ -0,0 +1,56 @@
+package es.pfsgroup.plugin.rem.model;
+
+public class DtoComprador {
+
+	private String nombreComprador;
+	private String direccionComprador;
+	private String dniComprador;
+	private String tlfComprador;
+	private String rBienesComprador;
+	private String eCivilComprador;
+	private String porcentajeComprador;
+	
+	public String getNombreComprador() {
+		return nombreComprador;
+	}
+	public void setNombreComprador(String nombreComprador) {
+		this.nombreComprador = nombreComprador;
+	}
+	public String getDireccionComprador() {
+		return direccionComprador;
+	}
+	public void setDireccionComprador(String direccionComprador) {
+		this.direccionComprador = direccionComprador;
+	}
+	public String getDniComprador() {
+		return dniComprador;
+	}
+	public void setDniComprador(String dniComprador) {
+		this.dniComprador = dniComprador;
+	}
+	public String getTlfComprador() {
+		return tlfComprador;
+	}
+	public void setTlfComprador(String tlfComprador) {
+		this.tlfComprador = tlfComprador;
+	}
+	public String getrBienesComprador() {
+		return rBienesComprador;
+	}
+	public void setrBienesComprador(String rBienesComprador) {
+		this.rBienesComprador = rBienesComprador;
+	}
+	public String geteCivilComprador() {
+		return eCivilComprador;
+	}
+	public void seteCivilComprador(String eCivilComprador) {
+		this.eCivilComprador = eCivilComprador;
+	}
+	public String getPorcentajeComprador() {
+		return porcentajeComprador;
+	}
+	public void setPorcentajeComprador(String porcentajeComprador) {
+		this.porcentajeComprador = porcentajeComprador;
+	}
+	
+}

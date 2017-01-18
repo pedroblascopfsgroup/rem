a46b69f4aea6ca02e4bd62dd1f06b634618e84ce
Kevin Fernandez <kevin.fernandez@pfsgroup.es>
2017-01-18 12:56:52 +0100

    HREOS-1402, publicacion de agrupaciones
    Former-commit-id: 4e4961fa2d5837fda11551304253efb4aabae6ed

--- proyecto-rem-online/sencha-app/classic/src/view/agrupaciones/detalle/ActivosAgrupacion.js
+++ proyecto-rem-online/sencha-app/classic/src/view/agrupaciones/detalle/ActivosAgrupacion.js
@@ -1,26 +1,24 @@
 Ext.define('HreRem.view.agrupaciones.detalle.ActivosAgrupacion', {
-    extend: 'HreRem.view.common.FormBase',
-    xtype: 'activosagrupacion',
-    reference: 'activosagrupacion',
-    layout: 'fit',
+    extend		: 'HreRem.view.common.FormBase',
+    xtype		: 'activosagrupacion',
+    reference	: 'activosagrupacion',
+    layout		: 'fit',
     scrollable	: 'y',
+    requires	: ['HreRem.view.common.GridBaseEditableRow', 'HreRem.model.ActivoAgrupacion',
+    	'HreRem.view.agrupaciones.detalle.AgrupacionDetalleModel', 'HreRem.view.agrupaciones.detalle.ActivosAgrupacionList'],
 
-    requires: ['HreRem.view.common.GridBaseEditableRow', 'HreRem.model.ActivoAgrupacion', 'HreRem.view.agrupaciones.detalle.AgrupacionDetalleModel'],
+	initComponent: function () {
+        var me = this; 
 
-    initComponent: function () {
+        me.setTitle(HreRem.i18n('title.activos'));
 
-     	var me = this;    	
-
-     	me.setTitle(HreRem.i18n('title.activos'));
-
-	    var items = [
-	    	{
-				xtype		:'fieldsettable',
+        me.items = [
+			{
+				xtype		:'fieldset',
 				collapsible	: false,
 				defaultType	: 'textfieldbase',
 				scrollable	: 'y',
-				items 		:
-					[
+				items 		: [
 						{
 			            	xtype: 'button',
 			            	reference: 'btnExportarActivosLoteComercial',
@@ -29,222 +27,25 @@ Ext.define('HreRem.view.agrupaciones.detalle.ActivosAgrupacion', {
 			            	bind : {
 								hidden: '{!esAgrupacionLoteComercial}'
 							},
-							colspan: 3,
 							margin: '15 0 0 15'
 			            },
 				    	{
-							title: 'Listado de Activos',
-						    xtype: 'gridBaseEditableRow',
-						    idPrincipal: 'agrupacionficha.id',
-						    reference: 'listadoactivosagrupacion',
-							cls	: 'panel-base shadow-panel',
-							topBar: true,
-							editOnSelect: false,
-							colspan: 3,
-							bind: {
-								store: '{storeActivos}',
-								topBar: '{agrupacionficha.esEditable}',
-								editOnSelect: '{agrupacionficha.esEditable}'
-							},
-							colspan: 2,
-							listeners: {
-								rowdblclick: 'onActivosAgrupacionListDobleClick',
-								boxready: function() {
-									//Si la agrupacion tiene oferta vivas, no se puede agregar/quitar activos
-									var desactivarBotones = !this.lookupController().getViewModel().get('agrupacionficha').getData().existenOfertasVivas;
-									this.down('#removeButton').setVisible(desactivarBotones);
-									this.down('#addButton').setVisible(desactivarBotones);
-								}
-						    },
-						    
-							secFunToEdit: 'EDITAR_AGRUPACION',
-							
-							secButtons: {
-								secFunPermToEnable : 'EDITAR_AGRUPACION'
-							},
-				    
-							
-							columns: [
-							    {
-							        xtype: 'actioncolumn',
-							        reference: 'activoPrincipal',
-							        bind: {
-							        	hidden: '{!esAgrupacionRestringida}'
-							        },
-							        width: 30,
-							        text: HreRem.i18n('header.principal'),
-									hideable: false,
-									items: [
-									        	{
-										            getClass: function(v, meta, rec) {
-										                if (rec.get('activoPrincipal') != 1) {
-										                	this.items[0].handler = 'onMarcarPrincipalClick';
-										                    return 'fa fa-check';
-										                } else {
-								            				this.items[0].handler = 'onMarcarPrincipalClick';
-										                    return 'fa fa-check green-color';
-										                }
-										            }
-									        	}
-									 ]
-					    		},  		
-							    {
-						            dataIndex: 'numActivo',
-						            text: HreRem.i18n('header.numero.activo.haya'),
-						            flex: 0.5,
-									editor: {xtype:'textfield'}
-			
-						        },
-						        {   
-					            	text	 : HreRem.i18n('header.fecha.alta'),
-					                dataIndex: 'fechaInclusion',
-							        formatter: 'date("d/m/Y")',
-							        width: 130 
-							    },
-						        {
-						            dataIndex: 'tipoActivoDescripcion',
-						            text: HreRem.i18n('header.tipo'),
-						            flex: 1
-						        },
-						        {
-						            dataIndex: 'subtipoActivo',
-						            text: HreRem.i18n('header.subtipo'),
-						            flex: 0.5,
-						            renderer: function (value) {
-						            	return Ext.isEmpty(value) ? "" : value.descripcion;
-						            }
-						        },
-						        {
-						            dataIndex: 'subdivision',
-						            text: HreRem.i18n('header.subdivision'),
-						            hideable: false,
-						            bind: {
-							        	hidden: '{!esAgrupacionObraNuevaOrAsistida}'
-							        },
-						            flex: 0.5
-						        },
-						        {
-						            dataIndex: 'direccion',
-						            text: HreRem.i18n('header.direccion'),
-						            hideable: false,
-						            bind: {
-							        	hidden: '{esAgrupacionObraNuevaOrAsistida}'
-							        },
-						            flex: 1
-						        },
-						        {
-						            dataIndex: 'numFinca',
-						            text: HreRem.i18n('header.finca.registral'),
-						            hideable: false,
-						            bind: {
-							        	hidden: '{!esAgrupacionObraNuevaOrAsistida}'
-							        },
-						            flex: 0.5
-						        },
-						        {
-						            dataIndex: 'puerta',
-						            text: HreRem.i18n('header.puerta'),
-						            hideable: false,
-						            bind: {
-							        	hidden: '{!esAgrupacionObraNuevaOrAsistida}'
-							        },
-						            flex: 0.5
-						        },
-						        {
-						            dataIndex: 'publicado',
-						            text: HreRem.i18n('header.publicado'),
-						            flex: 0.5,
-						            renderer: Utils.rendererBooleanToSiNo
-						        },
-						        {
-						            dataIndex: 'situacionComercial',
-						            text: HreRem.i18n('header.disponibilidad.comercial'),
-						            flex: 1
-						        },
-						        {
-						            dataIndex: 'importeMinimoAutorizado',
-						            text: HreRem.i18n('header.valor.web'),
-						            flex: 1,
-						            renderer: function(value) {
-						        		return Ext.util.Format.currency(value);
-						        	},
-						            sortable: false		            
-						        },
-						        {
-						            dataIndex: 'importeAprobadoVenta',
-						            text: HreRem.i18n('header.valor.aprobado.venta'),
-						            flex: 1,
-						            renderer: function(value) {
-						        		return Ext.util.Format.currency(value);
-						        	},
-						            sortable: false		            
-						        },
-						        {
-						            dataIndex: 'importeDescuentoPublicado',
-						            text: HreRem.i18n('header.valor.descuento.publicado'),
-						            flex: 1,
-						            renderer: function(value) {
-						        		return Ext.util.Format.currency(value);
-						        	},
-						            sortable: false		            
-						        },
-						        {
-						            dataIndex: 'superficieConstruida',
-						            hideable: false,
-						            bind: {
-							        	hidden: '{esAgrupacionObraNuevaOrAsistida}'
-							        },
-						            text: HreRem.i18n('header.superficie.construida'),
-						            flex: 1,
-						            renderer: Ext.util.Format.numberRenderer('0,000.00'),
-						            sortable: false      
-						        }
-						         	        
-						    ],
-						    
-						    saveSuccessFn: function() {
-						    	if (Ext.isFunction(me.lookupController().refrescarAgrupacion)) {
-						    		me.lookupController().onClickBotonRefrescar();
-						    	}
-						    },
-						    
-						    deleteSuccessFn: function() {
-						    	if (Ext.isFunction(me.lookupController().refrescarAgrupacion)) {
-						    		me.lookupController().onClickBotonRefrescar();
-						    	}
-						    },
-						    
-							deleteFailureFn: function() {
-						    	if (Ext.isFunction(me.lookupController().refrescarAgrupacion)) {
-						    		me.lookupController().onClickBotonRefrescar();
-						    	}
-						    },
-						    
-						    dockedItems : [
-						        {
-						            xtype: 'pagingtoolbar',
-						            dock: 'bottom',
-						            displayInfo: true,
-						            bind: {
-						                store: '{storeActivos}'
-						            }
-						        }
-						    ]
+						    xtype: 'activosagrupacionlist',
+						    reference: 'listadoactivosagrupacion'
 						}
 			    ]
-	    	}
+			}
 	    ];
 
-		me.addPlugin({ptype: 'lazyitems', items: items });
 		me.callParent();
-  },
+	},
 
-  funcionRecargar: function() {
-	var me = this; 
-	me.recargar = false;
-	var listadoActivosAgrupacion = me.down("[reference=listadoactivosagrupacion]");
-
-	// FIXME ¿¿Deberiamos cargar la primera página??
-	listadoActivosAgrupacion.getStore().loadPage(1);
-  }
+	funcionRecargar: function() {
+		var me = this; 
+		me.recargar = false;
+		var listadoActivosAgrupacion = me.down("[reference=listadoactivosagrupacion]");
+	
+		// FIXME ¿¿Deberiamos cargar la primera página??
+		listadoActivosAgrupacion.getStore().loadPage(1);
+	}
 });
---
+++ proyecto-rem-online/sencha-app/classic/src/view/agrupaciones/detalle/ActivosAgrupacionList.js
@@ -0,0 +1,533 @@
+Ext.define('HreRem.view.agrupaciones.detalle.ActivosAgrupacionList', {
+	extend		: 'HreRem.view.common.GridBase',
+    xtype		: 'activosagrupacionlist',
+    topBar		: false,
+    cls			: 'panel-base shadow-panel',
+    allowDeselect: true,
+	allowBlank	: true,
+    idPrincipal	: 'agrupacionficha.id',
+    bind		: {
+		store: '{storeActivos}'
+	},
+	requires	: ['HreRem.view.common.CheckBoxModelBase', 'HreRem.ux.plugin.PagingSelectionPersistence'],
+	secFunToEdit: 'EDITAR_AGRUPACION',
+	secButtons	: {
+		secFunPermToEnable: 'EDITAR_AGRUPACION'
+	},
+
+	listeners	: {
+		rowdblclick: 'onActivosAgrupacionListDobleClick',
+		rowclick: function(grid, record, tr, rowIndex, event, eOpts) {
+			var me = this;
+
+			if(Ext.isEmpty(grid.getSelection())){
+				me.submenuItemsDisabled(true);
+			} else {
+				me.submenuItemsDisabled(false);
+			}
+		},
+		boxready: function() {
+			var me = this;
+			//Si la agrupacion tiene oferta vivas, no se puede agregar/quitar activos
+			var desactivarBotones = !me.lookupController().getViewModel().get('agrupacionficha').getData().existenOfertasVivas;
+			me.visibilityAddButton(desactivarBotones);
+			me.visibilityRemoveButton(desactivarBotones);
+		},
+		edit: function(editor, context, eOpts) {
+			var me = this;
+			me.editFuncion(editor, context);
+	    },
+	    beforeedit: function(editor) {
+	    	var me = this;
+        	//Si ya estamos editando o no estamos creando un registro nuevo ni se permite la edición
+        	if (editor.editing || (!editor.isNew && !me.editOnSelect)) {
+        		return false;
+        	}
+	    },
+	    canceledit: function(editor){
+	    	var me = this;
+			me.disableAddButton(false);
+			me.disablePagingToolBar(false);
+        	me.getSelectionModel().deselectAll();
+        	if(editor.isNew) {
+        		me.getStore().remove(me.getStore().getAt(me.editPosition));
+        		editor.isNew = false;
+        	}
+	    },
+    	afterbind: function(grid) {
+    		var me = this;
+			if (me.loadAfterBind) {
+				grid.getStore().load();
+			}
+		}
+    },
+
+    initComponent: function () {
+        var me = this;
+
+        // Plugin edicion.
+        me.rowEditing = new Ext.grid.plugin.RowEditing({
+			pluginId: 'rowEditingPlugin',
+			// Puesto para cancelar el save con el onEnterKey
+			config: {
+	            buttonFocus: ''
+	        },  
+	        onEnterKey: function (){},
+			saveBtnText  : HreRem.i18n('btn.saveBtnText'),
+			cancelBtnText: HreRem.i18n('btn.cancelBtnText'),
+			errorsText: HreRem.i18n('btn.errorsText'),
+			dirtyText: HreRem.i18n('btn.dirtyText'),
+            clicksToMoveEditor: 1,
+       	 	autoCancel: false,
+       	 	errorSummary: false
+        });
+
+        // Plugin checkboxes.
+        me.pagingSelectPersist = new HreRem.ux.plugin.PagingSelectionPersistence({
+        	pluginId: 'pagingselectpersist'
+        });
+
+        // Preparar plugins.
+        var addRowPluginFunction = function() {
+			Ext.apply(me, {
+				plugins: [me.rowEditing,  me.pagingSelectPersist],
+				editable: true
+				});	
+				
+		};
+
+		// Aplicar plugins segun permisos.
+		if(Ext.isEmpty(me.rowPluginSecurity) && Ext.isEmpty(me.secFunToEdit)) {		
+			addRowPluginFunction();
+		} else if (!Ext.isEmpty(me.secFunToEdit)) {
+			$AU.confirmFunToFunctionExecution(addRowPluginFunction, me.secFunToEdit);
+		} else {
+			$AU.confirmRolesToFunctionExecution(addRowPluginFunction, me.rowPluginSecurity);
+		}
+
+        // Submenu del Grid.
+        me.menu = Ext.create("Ext.menu.Menu", {
+		    cls: 'menu-favoritos',
+		    plain: true,
+		    floating: true,
+	    	items: [
+	    		{
+	    			text: HreRem.i18n('grid.submenu.msg.publicar.agrupacion'),
+	    			handler: 'onClickPublicarAgrupacionSubmenuGrid'
+	    		},
+	    		{
+	    			text: HreRem.i18n('grid.submenu.msg.publicar.activos.seleccionados'),
+	    			handler: 'onClickPublicarActivosSeleccionadosSubmenuGrid',
+	    			reference: 'submenugridpublicaractivosbtn',
+	    			disabled: true
+	    		},
+	    		{
+	    			text: HreRem.i18n('grid.menu.msg.publicar.subdivisiones.activos.seleccionados'),
+	    			handler: 'onClickPublicarSubdivisionesActivosSeleccioandosSubmenuGrid',
+	    			reference: 'submenugridpublicarsubdivisionesbtn',
+	    			disabled: true
+	    		}
+	    	]
+	    	
+	    });
+
+        // Botones de la barra del grid.
+        var configAddBtn = {iconCls:'x-fa fa-plus', itemId:'addButton', handler: 'onAddClick', scope: this};
+		var configRemoveBtn = {iconCls:'x-fa fa-minus', itemId:'removeButton', handler: 'onDeleteClick', scope: this, disabled: true};
+		var configGridMenu = {iconCls:'x-fa fa-bars', cls:'boton-cabecera', itemId:'menuGridBtn', arrowVisible: false, menuAlign: 'tr-br', menu: me.menu};
+		var separador = {xtype: 'tbfill'};
+
+		// Se configura manualmente la Top-Bar con los botones especificados anteriormente.
+		me.tbar = {
+    		xtype: 'toolbar',
+    		dock: 'top',
+    		items: [configAddBtn, configRemoveBtn, separador, configGridMenu]
+		};
+
+        me.columns= [
+        	{
+		        xtype: 'actioncolumn',
+		        reference: 'activoPrincipal',
+		        bind: {
+		        	hidden: '{!esAgrupacionRestringida}'
+		        },
+		        width: 30,
+		        text: HreRem.i18n('header.principal'),
+				hideable: false,
+				items: [
+				        	{
+					            getClass: function(v, meta, rec) {
+					                if (rec.get('activoPrincipal') != 1) {
+					                	this.items[0].handler = 'onMarcarPrincipalClick';
+					                    return 'fa fa-check';
+					                } else {
+			            				this.items[0].handler = 'onMarcarPrincipalClick';
+					                    return 'fa fa-check green-color';
+					                }
+					            }
+				        	}
+				 ]
+    		},  		
+		    {
+	            dataIndex: 'numActivo',
+	            text: HreRem.i18n('header.numero.activo.haya'),
+	            flex: 0.6,
+				editor: {xtype:'textfield'}
+
+	        },
+	        {   
+            	text	 : HreRem.i18n('header.fecha.alta'),
+                dataIndex: 'fechaInclusion',
+		        formatter: 'date("d/m/Y")',
+		        flex: 0.7,
+		        width: 130 
+		    },
+	        {
+	            dataIndex: 'tipoActivoDescripcion',
+	            text: HreRem.i18n('header.tipo'),
+	            flex: 1
+	        },
+	        {
+	            dataIndex: 'subtipoActivo',
+	            text: HreRem.i18n('header.subtipo'),
+	            flex: 0.5,
+	            renderer: function (value) {
+	            	return Ext.isEmpty(value) ? "" : value.descripcion;
+	            }
+	        },
+	        {
+	            dataIndex: 'subdivision',
+	            text: HreRem.i18n('header.subdivision'),
+	            hideable: false,
+	            bind: {
+		        	hidden: '{!esAgrupacionObraNuevaOrAsistida}'
+		        },
+	            flex: 0.5
+	        },
+	        {
+	            dataIndex: 'direccion',
+	            text: HreRem.i18n('header.direccion'),
+	            hideable: false,
+	            bind: {
+		        	hidden: '{esAgrupacionObraNuevaOrAsistida}'
+		        },
+	            flex: 1
+	        },
+	        {
+	            dataIndex: 'numFinca',
+	            text: HreRem.i18n('header.finca.registral'),
+	            hideable: false,
+	            bind: {
+		        	hidden: '{!esAgrupacionObraNuevaOrAsistida}'
+		        },
+	            flex: 0.5
+	        },
+	        {
+	            dataIndex: 'puerta',
+	            text: HreRem.i18n('header.puerta'),
+	            hideable: false,
+	            bind: {
+		        	hidden: '{!esAgrupacionObraNuevaOrAsistida}'
+		        },
+	            flex: 0.5
+	        },
+	        {
+	            dataIndex: 'publicado',
+	            text: HreRem.i18n('header.publicado'),
+	            flex: 0.5,
+	            renderer: Utils.rendererBooleanToSiNo
+	        },
+	        {
+	            dataIndex: 'situacionComercial',
+	            text: HreRem.i18n('header.disponibilidad.comercial'),
+	            flex: 1
+	        },
+	        {
+	            dataIndex: 'importeMinimoAutorizado',
+	            text: HreRem.i18n('header.valor.web'),
+	            flex: 1,
+	            renderer: function(value) {
+	        		return Ext.util.Format.currency(value);
+	        	},
+	            sortable: false		            
+	        },
+	        {
+	            dataIndex: 'importeAprobadoVenta',
+	            text: HreRem.i18n('header.valor.aprobado.venta'),
+	            flex: 1,
+	            renderer: function(value) {
+	        		return Ext.util.Format.currency(value);
+	        	},
+	            sortable: false		            
+	        },
+	        {
+	            dataIndex: 'importeDescuentoPublicado',
+	            text: HreRem.i18n('header.valor.descuento.publicado'),
+	            flex: 1,
+	            renderer: function(value) {
+	        		return Ext.util.Format.currency(value);
+	        	},
+	            sortable: false		            
+	        },
+	        {
+	            dataIndex: 'superficieConstruida',
+	            hideable: false,
+	            bind: {
+		        	hidden: '{esAgrupacionObraNuevaOrAsistida}'
+		        },
+	            text: HreRem.i18n('header.superficie.construida'),
+	            flex: 1,
+	            renderer: Ext.util.Format.numberRenderer('0,000.00'),
+	            sortable: false      
+	        }
+        ];
+
+        me.saveSuccessFn = function() {
+	    	if (Ext.isFunction(me.lookupController().refrescarAgrupacion)) {
+	    		me.lookupController().onClickBotonRefrescar();
+	    	}
+	    };
+
+	    me.deleteSuccessFn = function() {
+	    	if (Ext.isFunction(me.lookupController().refrescarAgrupacion)) {
+	    		me.lookupController().onClickBotonRefrescar();
+	    	}
+	    };
+
+	    me.deleteFailureFn = function() {
+	    	if (Ext.isFunction(me.lookupController().refrescarAgrupacion)) {
+	    		me.lookupController().onClickBotonRefrescar();
+	    	}
+	    };
+
+        me.dockedItems = [
+        	{
+	            xtype: 'pagingtoolbar',
+	            dock: 'bottom',
+	            displayInfo: true,
+	            bind: {
+	                store: '{storeActivos}'
+	            },
+	            items:[
+	            	{
+	            		xtype: 'tbfill'
+	            	},
+	                {
+	                	xtype: 'displayfieldbase',
+	                	itemId: 'displaySelection',
+	                	fieldStyle: 'color:#0c364b; padding-top: 4px'
+	                }
+	            ]
+	        }
+		];
+
+        me.selModel = Ext.create('HreRem.view.common.CheckBoxModelBase');
+
+    	me.callParent();
+
+    	me.getSelectionModel().on({
+        	'selectionchange': function(sm,record,e) {
+        		var displaySelection = me.down('displayfield[itemId=displaySelection]');
+    			var persistedSelection = me.getPersistedSelection();
+    			var disabled = Ext.isEmpty(persistedSelection);
+
+    			if(disabled) {
+    				displaySelection.setValue("No seleccionados");
+    				me.disableRemoveButton(true);
+    			} else if (persistedSelection.length > 1) {
+    				displaySelection.setValue(persistedSelection.length +  " elementos seleccionados");
+    				me.disableRemoveButton(true); // Solo permitir eliminar un único elemento a la vez.
+    			} else {
+    				displaySelection.setValue("1 elemento seleccionado"); 
+    			}
+        	},
+
+        	'selectall': function(sm) {
+        		me.pagingSelectPersist.selectAll();
+        		me.submenuItemsDisabled(false);
+        	},
+
+        	'deselectall': function(sm) {
+        		me.pagingSelectPersist.deselectAll();
+        		me.submenuItemsDisabled(true);
+        	}
+        });
+    },
+
+    getActivoIDPersistedSelection: function() {
+    	var me = this;
+    	var arraySelection = me.getPlugin('pagingselectpersist').getPersistedSelection();
+		var activoSelection = [];
+
+		Ext.Array.each(arraySelection, function(rec) {
+			activoSelection.push(rec.getData().activoId);
+        });
+
+    	return activoSelection;
+    },
+
+    deselectAll: function() {
+    	var me = this;
+    	return me.pagingSelectPersist.deselectAll();     		
+    },
+
+    visibilityAddButton: function(visibility) {
+    	var me = this;
+
+    	if (!Ext.isEmpty(me.down('#addButton'))) {
+    		me.down('#addButton').setVisible(visibility);  		
+    	}
+    },
+
+    visibilityRemoveButton: function(visibility) {
+    	var me = this;
+
+    	if (!Ext.isEmpty(me.down('#removeButton'))) {
+    		me.down('#removeButton').setVisible(visibility);  		
+    	}
+    },
+
+    onAddClick: function(btn){
+    	
+		var me = this;
+		var rec = Ext.create(me.getStore().config.model);
+		me.getStore().sorters.clear();
+		me.editPosition = 0;
+        me.getStore().insert(me.editPosition, rec);
+        me.rowEditing.isNew = true;
+        me.rowEditing.startEdit(me.editPosition, 0);
+        me.disableAddButton(true);
+        me.disablePagingToolBar(true);
+        me.disableRemoveButton(true);
+
+    },
+
+    onDeleteClick: function(btn){
+    	var me = this;
+
+        Ext.Msg.show({
+			   title: HreRem.i18n('title.confirmar.eliminacion'),
+			   msg: HreRem.i18n('msg.desea.eliminar'),
+			   buttons: Ext.MessageBox.YESNO,
+			   fn: function(buttonId) {
+			        if (buttonId == 'yes') {
+			        	me.mask(HreRem.i18n("msg.mask.espere"));
+			    		me.rowEditing.cancelEdit();
+			            var sm = me.getSelectionModel();
+			            sm.getSelection()[0].erase({
+			            	success: function (a, operation, c) {
+                                me.fireEvent("infoToast", HreRem.i18n("msg.operacion.ok"));
+								me.unmask();
+								me.deleteSuccessFn();
+                            },
+                            
+                            failure: function (a, operation) {
+                            	var data = {};
+                            	try {
+                            		data = Ext.decode(operation._response.responseText);
+                            	}
+                            	catch (e){ };
+                            	if (!Ext.isEmpty(data.msg)) {
+                            		me.fireEvent("errorToast", data.msg);
+                            	} else {
+                            		me.fireEvent("errorToast", HreRem.i18n("msg.operacion.ko"));
+                            	}
+								me.unmask();
+								me.deleteFailureFn()
+                            }
+                        });
+
+			            if (me.getStore().getCount() > 0) {
+			                sm.select(0);
+			            }
+			        }
+			   }
+		});
+    },
+
+    disablePagingToolBar: function(disabled) {
+    	var me = this,
+    	paginToolBar = me.down('pagingtoolbar');
+
+    	if(!Ext.isEmpty(paginToolBar)) {
+    		paginToolBar.setDisabled(disabled);
+    	}
+    },
+
+    disableAddButton: function(disabled) {
+    	var me = this;
+
+    	if (!Ext.isEmpty(me.down('#addButton'))) {
+    		me.down('#addButton').setDisabled(disabled);    		
+    	}
+    },
+
+    disableRemoveButton: function(disabled) {
+    	var me = this;
+
+    	if (!Ext.isEmpty(me.down('#removeButton')) && !me.disabledDeleteBtn) {
+    		me.down('#removeButton').setDisabled(disabled);    		
+    	}
+    },
+
+    submenuItemsDisabled: function(disabled) {
+    	var me = this;
+
+		var menuItemPublicarActivos = me.up('agrupacionesdetallemain').lookupReference('submenugridpublicaractivosbtn');
+		var menuItemPublicarSubdivisionesActivos = me.up('agrupacionesdetallemain').lookupReference('submenugridpublicarsubdivisionesbtn');
+
+		if(Ext.isDefined(menuItemPublicarActivos) && Ext.isDefined(menuItemPublicarSubdivisionesActivos)) {
+			if(disabled){
+				menuItemPublicarActivos.setDisabled(true);
+				menuItemPublicarSubdivisionesActivos.setDisabled(true);
+			} else {
+				menuItemPublicarActivos.setDisabled(false);
+				menuItemPublicarSubdivisionesActivos.setDisabled(false);
+			}
+		}
+    },
+
+    isValidRecord: function(record) {    	
+    	return true;    	
+    },
+
+    editFuncion: function(editor, context){
+   		var me= this;
+		me.mask(HreRem.i18n("msg.mask.espere"));
+
+			if (me.isValidRecord(context.record)) {				
+        		context.record.save({
+                    params: {
+                        idEntidad: Ext.isEmpty(me.idPrincipal) ? "" : this.up('{viewModel}').getViewModel().get(me.idPrincipal)
+                    },
+
+                    success: function (a, operation, c) {
+                        context.store.load();
+                        me.unmask();
+                        me.fireEvent("infoToast", HreRem.i18n("msg.operacion.ok"));																			
+						me.saveSuccessFn();											
+						
+                    },
+
+					failure: function (a, operation) {
+                    	context.store.load();
+                    	try {
+                    		var response = Ext.JSON.decode(operation.getResponse().responseText)
+                    		
+                    	}catch(err) {}
+                    	
+                    	if(!Ext.isEmpty(response) && !Ext.isEmpty(response.msg)) {
+                    		me.fireEvent("errorToast", response.msg);
+                    	} else {
+                    		me.fireEvent("errorToast", HreRem.i18n("msg.operacion.ko"));
+                    	}                        	
+						me.unmask();
+                    }
+                });	                            
+        		me.disableAddButton(false);
+        		me.disablePagingToolBar(false);
+        		me.getSelectionModel().deselectAll();
+        		editor.isNew = false;
+			}
+   }
+});

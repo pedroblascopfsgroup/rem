974762bb317c9925d436d414bcc331410882d7fa
Jorge Ros <jorge.ros@pfsgroup.es>
2017-02-06 17:48:39 +0100

    HREOS-1421 Tanteo y Retracto de la Generalitat
    Former-commit-id: 4c3805c1d0a71e9942f2e9d78de6a40298f40c93

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/condiciontanteo/CondicionTanteoTipologia.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/condiciontanteo/CondicionTanteoTipologia.java
@@ -4,16 +4,24 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 
+import es.pfsgroup.commons.utils.Checks;
 import es.pfsgroup.plugin.rem.model.Activo;
+import es.pfsgroup.plugin.rem.model.dd.DDTipoActivo;
 
 @Service("condicionTanteoTipologia")
 public class CondicionTanteoTipologia implements CondicionTanteoApi {
-	//TODO: Tipologia = Vivienda
 	
 	protected static final Log logger = LogFactory.getLog(CondicionTanteoTipologia.class);
 	
+	/**
+	 * Comprueba que el tipo de activo sea Vivienda
+	 */
 	@Override
 	public Boolean checkCondicion(Activo activo){
+		
+		if(!Checks.esNulo(activo.getTipoActivo()) && DDTipoActivo.COD_VIVIENDA.equals(activo.getTipoActivo().getCodigo()))
+			return true;
+		
 		return false;
 	}
 }

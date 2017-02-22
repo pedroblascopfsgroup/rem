97ce1a08bc55a0ebe1650c5ca305bd7c07e03c55
Gustavo Mora <gustavo.mora@pfsgroup.es>
2017-02-22 18:21:00 +0100

    HREOS-1613: se incluye el schema en el sql
    Former-commit-id: 9876f47bc56d667ec6a365f268c43e7c4184cabb

--- sql/9.2/rem/migracion/rem_fase2/migracion_fase2/post_migracion/DML_3000_REM_SPOOL_GEN_FICHERO_PARES_COMERCIAL.sql
+++ sql/9.2/rem/migracion/rem_fase2/migracion_fase2/post_migracion/DML_3000_REM_SPOOL_GEN_FICHERO_PARES_COMERCIAL.sql
@@ -4,7 +4,7 @@ SPOOL ../../output_uvem/PARES_OFERTAS_UVEM_REM.txt
   from dual
   union ALL
   select lpad(ofr_num_oferta,16,0) ||'	'||  lpad(ofr_num_oferta,16,0) 
-  from ofr_ofertas;
+  from REM01.ofr_ofertas;
 
 SPOOL OFF;
 set echo on heading on feedback on termout on;

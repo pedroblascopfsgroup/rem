fd7412d70336f3ada6340f3d2472eca31655eb18
josemanuel.villel <josemanuel.villel@pfsgroup.es>
2017-02-28 10:51:07 +0100

    HREOS-1657: Validación ofertas vivas en baja de agrupación
    Former-commit-id: 7e45174a98ae07a0142d3ad24134908a68d77230

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/adapter/AgrupacionAdapter.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/adapter/AgrupacionAdapter.java
@@ -169,6 +169,8 @@ public class AgrupacionAdapter {
 	public static final String PUBLICACION_ACTIVOS_AGRUPACION_ERROR_MSG = "No ha sido posible publicar. Algún activo no tiene las condiciones necesarias";
 	public static final String PUBLICACION_MOTIVO_MSG = "Publicado desde agrupación";
 	public static final String PUBLICACION_AGRUPACION_BAJA_ERROR_MSG = "No ha sido posible publicar. La agrupación está dada de baja";
+	public static final String AGRUPACION_BAJA_ERROR_OFERTAS_VIVAS = "No ha sido posible dar de baja la agrupación. Existen ofertas vivas";
+	
     
 	public DtoAgrupaciones getAgrupacionById(Long id){
 
@@ -1292,6 +1294,17 @@ public class AgrupacionAdapter {
 	public boolean saveAgrupacion(DtoAgrupaciones dto, Long id) {
 		
 		ActivoAgrupacion agrupacion = activoAgrupacionApi.get(id);
+		
+		//Primero comprobamos si estamos dandola de baja y se cumplen todos los requisitos para poder hacerlo
+		
+		if(!Checks.esNulo(dto.getFechaBaja())) {
+			String error = validarBajaAgrupacion(agrupacion);
+			
+			if(!Checks.esNulo(error)) {
+				throw new JsonViewerException(error);
+			}
+		}		
+		
 		// SI ES TIPO OBRA NUEVA
 		if (agrupacion.getTipoAgrupacion().getCodigo().equals(DDTipoAgrupacion.AGRUPACION_OBRA_NUEVA)) {
 			
@@ -1425,6 +1438,17 @@ public class AgrupacionAdapter {
 	}
 
 	
+	private String validarBajaAgrupacion(ActivoAgrupacion agrupacion) {
+		
+		String error = null;
+		
+		if(existenOfertasActivasEnAgrupacion(agrupacion.getId())) {			
+			error = AGRUPACION_BAJA_ERROR_OFERTAS_VIVAS;
+		}
+
+		return error;
+	}
+
 	public List<ActivoFoto> getFotosActivosAgrupacionById(Long id) {
 		
 		return activoAgrupacionApi.getFotosActivosAgrupacionById(id);

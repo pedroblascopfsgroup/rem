4e22061ca3af95022ea8000745dfd32ee1f453de
Daniel <daniel.albert@pfsgroup.es>
2017-03-30 12:05:24 +0200

    HREOS-1860 DDL

---
+++ sql/9.2/rem/DDL_99900104_REM01_ACTIVOS_COMERCIAL_BK.sql
@@ -0,0 +1,132 @@
+--/*
+--##########################################
+--## AUTOR=DAP
+--## FECHA_CREACION=20170330
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=X.X
+--## INCIDENCIA_LINK=HREOS-1860
+--## PRODUCTO=NO
+--## Finalidad: Tablas para la interfaz REM-UVEM de COMERCIAL BK
+--##           
+--## INSTRUCCIONES: Configurar las variables necesarias en el principio del DECLARE
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+--Para permitir la visualización de texto en un bloque PL/SQL utilizando DBMS_OUTPUT.PUT_LINE
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF;
+
+DECLARE
+
+  V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar    
+  V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+  V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+  V_TABLESPACE_IDX VARCHAR2(25 CHAR):= '#TABLESPACE_INDEX#'; -- Configuracion Tablespace de Indices
+  V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+  V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.  
+  V_NUM_SEQ NUMBER(16); -- Vble. para validar la existencia de una secuencia.  
+  ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+  ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+  V_TEXT_TABLA VARCHAR2(50 CHAR); -- Vble. auxiliar para nombre de tabla.
+  V_TEXT1 VARCHAR2(2400 CHAR); -- Vble. auxiliar.
+  V_COMMENT_TABLE VARCHAR2(500 CHAR); -- Vble. para los comentarios de las tablas
+
+BEGIN
+
+  --EPF_ESTADOS_PROV_FOND
+	V_TEXT_TABLA := 'ECB_ESTADOS_COMERCIAL_BK';
+  V_COMMENT_TABLE := 'Tabla histórica para guardar los estados (enviado/no enviado) de las ofertas comerciales a Bankia';
+	DBMS_OUTPUT.PUT_LINE('******** ' ||V_TEXT_TABLA|| ' ********'); 
+	DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.'||V_TEXT_TABLA||'... Comprobaciones previas');
+
+	-- Verificar si la tabla ya existe
+	V_MSQL := 'SELECT COUNT(1) FROM ALL_TABLES WHERE TABLE_NAME = '''||V_TEXT_TABLA||''' AND OWNER = '''||V_ESQUEMA||'''';
+	EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;
+  
+	IF V_NUM_TABLAS = 1 THEN
+		DBMS_OUTPUT.PUT_LINE('[INFO] ' || V_ESQUEMA || '.'||V_TEXT_TABLA||'... Ya existe. Se borrará.');
+		EXECUTE IMMEDIATE 'DROP TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' CASCADE CONSTRAINTS';
+	END IF;
+
+	-- Comprobamos si existe la secuencia
+	V_SQL := 'SELECT COUNT(1) FROM ALL_SEQUENCES WHERE SEQUENCE_NAME = ''S_'||V_TEXT_TABLA||''' AND SEQUENCE_OWNER = '''||V_ESQUEMA||'''';
+	EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
+  
+	IF V_NUM_TABLAS = 1 THEN
+		DBMS_OUTPUT.PUT_LINE('[INFO] '|| V_ESQUEMA ||'.S_'||V_TEXT_TABLA||'... Ya existe. Se borrará.');  
+		EXECUTE IMMEDIATE 'DROP SEQUENCE '||V_ESQUEMA||'.S_'||V_TEXT_TABLA||'';
+	END IF; 
+	
+	-- Creamos la tabla
+	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA|| '.'||V_TEXT_TABLA||'...');
+	V_MSQL := 'CREATE TABLE ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'
+    (
+        ECB_ID           	NUMBER (16,0),
+        OFR_ID            	NUMBER (16,0),
+        ECB_FECHA_ENVIO   	TIMESTAMP (6),
+        ECB_ENVIADO       	NUMBER (1,0) DEFAULT 0 NOT NULL ENABLE,
+        VERSION 			NUMBER (38,0) DEFAULT 0 NOT NULL ENABLE, 
+        USUARIOCREAR 		VARCHAR2(50 CHAR) NOT NULL ENABLE, 
+        FECHACREAR 			TIMESTAMP (6) NOT NULL ENABLE, 
+        USUARIOMODIFICAR  	VARCHAR2(50 CHAR), 
+        FECHAMODIFICAR 		TIMESTAMP (6), 
+        USUARIOBORRAR 		VARCHAR2(50 CHAR), 
+        FECHABORRAR 		TIMESTAMP (6), 
+        BORRADO 			NUMBER(1,0) DEFAULT 0 NOT NULL ENABLE
+    )';
+	EXECUTE IMMEDIATE V_MSQL;
+	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'... Tabla creada.');	
+
+	-- Creamos índice	
+	V_MSQL := 'CREATE UNIQUE INDEX '||V_ESQUEMA||'.'||V_TEXT_TABLA||'_PK ON '||V_ESQUEMA|| '.'||V_TEXT_TABLA||'(ECB_ID) TABLESPACE '||V_TABLESPACE_IDX;
+	EXECUTE IMMEDIATE V_MSQL;
+	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'_PK... Índice creado.');	
+	
+	-- Creamos primary key
+	V_MSQL := 'ALTER TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' ADD (CONSTRAINT '||V_TEXT_TABLA||'_PK PRIMARY KEY (ECB_ID) USING INDEX)';
+	EXECUTE IMMEDIATE V_MSQL;
+	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'_PK... PK creada.');
+  
+  -- Creamos foreign key
+	V_MSQL := 'ALTER TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' ADD CONSTRAINT '||V_TEXT_TABLA||'_FK FOREIGN KEY (OFR_ID) REFERENCES '||V_ESQUEMA||'.OFR_OFERTAS (OFR_ID)';
+  EXECUTE IMMEDIATE V_MSQL;
+	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'_FK... FK creada.');
+
+  -- Creamos sequence
+	V_MSQL := 'CREATE SEQUENCE '||V_ESQUEMA||'.S_'||V_TEXT_TABLA||'';		
+	EXECUTE IMMEDIATE V_MSQL;		
+	DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.S_'||V_TEXT_TABLA||'... Secuencia creada');
+		
+	-- Creamos comentario	
+	V_MSQL := 'COMMENT ON TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' IS '''||V_COMMENT_TABLE||'''';		
+	EXECUTE IMMEDIATE V_MSQL;
+	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'... Comentario TABLA creado.');
+	
+	-- Creamos comentarios sobre las columnas
+	EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.ECB_ID IS ''Código identificador único de los estados de las ofertas comerciales''';
+	EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.OFR_ID IS ''Código identificador único de la oferta''';
+	EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.ECB_FECHA_ENVIO IS ''Fecha de envío de la oferta comercial''';
+	EXECUTE IMMEDIATE 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.ECB_ENVIADO IS ''Flag que indica si la oferta comercial está enviada''';
+	
+	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'... Comentarios de las columnas creados.');
+		
+	DBMS_OUTPUT.PUT_LINE('[FIN] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'... OK');
+	
+	COMMIT;
+
+EXCEPTION
+  WHEN OTHERS THEN 
+    DBMS_OUTPUT.PUT_LINE('KO!');
+    err_num := SQLCODE;
+    err_msg := SQLERRM;
+    DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(err_num));
+    DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+    DBMS_OUTPUT.put_line(err_msg);
+    ROLLBACK;
+    RAISE;          
+
+END;
+/
+EXIT

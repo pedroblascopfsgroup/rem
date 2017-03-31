0eb5223e5efe15463a15beb54dff2d707de1e577
Daniel <daniel.albert@pfsgroup.es>
2017-03-31 13:50:07 +0200

    HREOS-1840 shells modificados

--- batch-shell/dist/shells/rem/apr_wait_centros2_entidad.sh
+++ batch-shell/dist/shells/rem/apr_wait_centros2_entidad.sh
@@ -1,36 +1,41 @@
 #!/bin/bash
  
-ficheros=URCENTRO2
+fichero=URCENTRO2
 
 if [[ -z ${DIR_DESTINO} ]] || [[ ! -d ${DIR_DESTINO} ]]; then
     echo "$(basename $0) Error: DIR_DESTINO no definido o no es un directorio. Compruebe invocación previa a setBatchEnv.sh"
     exit 1
 fi
-rm -f ${DIR_DESTINO}URCENTRO2*
+rm -f ${DIR_DESTINO}$fichero*
 
+#mascara='_'$1
+#extensionSem=".sem"
+#extensionZip=".dat"
 extensionTxt=".txt"
 
 OIFS=$IFS
 IFS=','
-arrayFicheros=$ficheros
+arrayfichero=$fichero
 
 #Calculo de hora limite
 hora_limite=`date --date="$MAX_WAITING_MINUTES minutes" +%Y%m%d%H%M%S`
 hora_actual=`date +%Y%m%d%H%M%S`
 echo "Hora actual: $hora_actual - Hora limite: $hora_limite"
 
-for fichero in $arrayFicheros
+for fichero in $arrayfichero
 do
+#    ficheroem=$DIR_INPUT_AUX$fichero$mascara$extensionSem
+#    ficheroZip=$DIR_INPUT_AUX$fichero$mascara$extensionZip
      ficheroTxt=$DIR_INPUT_AUX$fichero$extensionTxt
 
     echo "$ficheroTxt"
-    if [[ "$#" -gt 1 ]] && [[ "$2" -eq "-ftp" ]]; then
+    if [[ "$#" -gt 1 ]]; then
         ./ftp/ftp_get_uvem_files.sh $1 $fichero
     fi
         while [[ "$hora_actual" -lt "$hora_limite" ]] && [[ ! -e $ficheroTxt ]]; do
             sleep 10
             hora_actual=`date +%Y%m%d%H%M%S`
-        if [[ "$#" -gt 1 ]] && [[ "$2" -eq "-ftp" ]]; then
+        if [[ "$#" -gt 1 ]]; then
             ./ftp/ftp_get_uvem_files.sh $1 $fichero
         fi
         done
@@ -38,18 +43,20 @@ done
 
 if [ "$hora_actual" -ge "$hora_limite" ]
 then
-   echo "$(basename $0) Error: Tiempo límite alcanzado: ficheros $ficheros no encontrados"
+   echo "$(basename $0) Error: Tiempo límite alcanzado: fichero $fichero no encontrados"
    exit 1
 else
-   for fichero in $arrayFicheros
+   for fichero in $arrayfichero
    do
+#            ficheroem=$DIR_INPUT_AUX$fichero$mascara$extensionSem
+#            ficheroZip=$DIR_INPUT_AUX$fichero$mascara$extensionZip
             ficheroTxt=$DIR_INPUT_AUX$fichero$extensionTxt
 
             sed -i 's/ //g' $ficheroTxt
             mv $ficheroTxt $DIR_DESTINO
 
    done
-   echo "$(basename $0) Ficheros encontrados"
+   echo "$(basename $0) fichero encontrados"
    exit 0
 fi
 

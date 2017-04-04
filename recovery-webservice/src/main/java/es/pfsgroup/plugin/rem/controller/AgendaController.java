6312aa661474e7b3acb14d12ed1bdfe2e1a3f55e
Ramon Llinares <ramon.llinares@pfsgroup.es>
2017-04-04 16:49:44 +0200

    HREOS-1888: Error al obtener el total de avisos y alertas

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/controller/AgendaController.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/controller/AgendaController.java
@@ -34,72 +34,88 @@ import es.pfsgroup.recovery.ext.factory.dao.dto.DtoResultadoBusquedaTareasBuzone
 
 @Controller
 public class AgendaController extends TareaController {
-	
+
 	@Autowired
 	private AgendaAdapter adapter;
-	
+
 	@Autowired
 	private ActivoEstadoPublicacionApi activoEstadoPublicacionApi;
-	
+
 	@Resource
 	Properties appProperties;
-	
+
 	@Autowired
 	ExcelReportGeneratorApi excelReportGeneratorApi;
-	
-	
-	 /*******************************************************
+
+	/*******************************************************
 	 * NOTA FASE II : Se refactoriza en ParadiseJsonController.java
-	 * *******************************************************/
-	/*@InitBinder
-	protected void initBinder(HttpServletRequest request,  ServletRequestDataBinder binder) throws Exception{
-        
-	    JsonWriterConfiguratorTemplateRegistry registry = JsonWriterConfiguratorTemplateRegistry.load(request);             
-	    registry.registerConfiguratorTemplate(new SojoJsonWriterConfiguratorTemplate(){
-	                
-	        	 	@Override
-	                public SojoConfig getJsonConfig() {
-	                    SojoConfig config= new SojoConfig();
-                        config.setIgnoreNullValues(true);
-                        return config;
-	        	 	}
-	         }
-	   );
+	 *******************************************************/
+	/*
+	 * @InitBinder protected void initBinder(HttpServletRequest request,
+	 * ServletRequestDataBinder binder) throws Exception{
+	 * 
+	 * JsonWriterConfiguratorTemplateRegistry registry =
+	 * JsonWriterConfiguratorTemplateRegistry.load(request);
+	 * registry.registerConfiguratorTemplate(new
+	 * SojoJsonWriterConfiguratorTemplate(){
+	 * 
+	 * @Override public SojoConfig getJsonConfig() { SojoConfig config= new
+	 * SojoConfig(); config.setIgnoreNullValues(true); return config; } } );
+	 * 
+	 * 
+	 * SimpleDateFormat dateFormat = new
+	 * SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"); dateFormat.setLenient(false);
+	 * dateFormat.setTimeZone(TimeZone.getDefault());
+	 * binder.registerCustomEditor(Date.class, new
+	 * ParadiseCustomDateEditor(dateFormat, true));
+	 * 
+	 * binder.registerCustomEditor(boolean.class, new
+	 * CustomBooleanEditor("true", "false", true));
+	 * binder.registerCustomEditor(Boolean.class, new
+	 * CustomBooleanEditor("true", "false", true));
+	 * binder.registerCustomEditor(String.class, new
+	 * StringTrimmerEditor(false)); NumberFormat f =
+	 * NumberFormat.getInstance(Locale.ENGLISH); f.setGroupingUsed(false);
+	 * f.setMaximumFractionDigits(2); f.setMinimumFractionDigits(2);
+	 * binder.registerCustomEditor(double.class, new
+	 * CustomNumberEditor(Double.class, f, true));
+	 * binder.registerCustomEditor(Double.class, new
+	 * CustomNumberEditor(Double.class, f, true));
+	 * 
+	 * 
+	 * /*binder.registerCustomEditor(Float.class, new
+	 * CustomNumberEditor(Float.class, true));
+	 * binder.registerCustomEditor(Long.class, new
+	 * CustomNumberEditor(Long.class, true));
+	 * binder.registerCustomEditor(Integer.class, new
+	 * CustomNumberEditor(Integer.class, true));
+	 * 
+	 * 
+	 * 
+	 * }
+	 */
 
-
-	    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-        dateFormat.setLenient(false);
-        dateFormat.setTimeZone(TimeZone.getDefault());
-        binder.registerCustomEditor(Date.class, new ParadiseCustomDateEditor(dateFormat, true));
-        
-        binder.registerCustomEditor(boolean.class, new CustomBooleanEditor("true", "false", true));
-        binder.registerCustomEditor(Boolean.class, new CustomBooleanEditor("true", "false", true));
-        binder.registerCustomEditor(String.class, new StringTrimmerEditor(false));
-        NumberFormat f = NumberFormat.getInstance(Locale.ENGLISH);
-    	f.setGroupingUsed(false);
-    	f.setMaximumFractionDigits(2);
-        f.setMinimumFractionDigits(2);
-        binder.registerCustomEditor(double.class, new CustomNumberEditor(Double.class, f, true));
-        binder.registerCustomEditor(Double.class, new CustomNumberEditor(Double.class, f, true));
-       
-        
-        /*binder.registerCustomEditor(Float.class, new CustomNumberEditor(Float.class, true));
-        binder.registerCustomEditor(Long.class, new CustomNumberEditor(Long.class, true));
-        binder.registerCustomEditor(Integer.class, new CustomNumberEditor(Integer.class, true));
-
-        
-        
-	}*/
-
+	@SuppressWarnings("unchecked")
 	@RequestMapping(method = RequestMethod.POST)
 	public ModelAndView getListTareas(DtoTareaFilter dtoTareaFiltro, ModelMap model) {
-		Page p = adapter.getListTareas(dtoTareaFiltro);		
+		Page p = adapter.getListTareas(dtoTareaFiltro);
 		model.put("data", p.getResults());
-		model.put("totalCount", adapter.tareasPendientes());
-		
+		// avisos
+		if (dtoTareaFiltro.getCodigoTipoTarea() != null && dtoTareaFiltro.getCodigoTipoTarea().equals("3")) {
+			model.put("totalCount", adapter.avisosPendientes());
+		} else if (dtoTareaFiltro.getCodigoTipoTarea() != null && dtoTareaFiltro.getCodigoTipoTarea().equals("1")
+				&& dtoTareaFiltro.getEsAlerta() != null && dtoTareaFiltro.getEsAlerta()) {
+			// alerta
+			model.put("totalCount", adapter.alertasPendientes());
+		} else {
+			// tarea
+			model.put("totalCount", adapter.tareasPendientes());
+		}
+
 		return createModelAndViewJson(model);
 
 	}
+
 	@RequestMapping(method = RequestMethod.POST)
 	public ModelAndView getFormularioTarea(Long idTarea, ModelMap model) {
 
@@ -112,13 +128,13 @@ public class AgendaController extends TareaController {
 		model.put("data", adapter.getValidacionPrevia(idTarea));
 		return createModelAndViewJson(model);
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
 	public ModelAndView getIdActivoTarea(Long idTarea, ModelMap model) {
 		model.put("idActivoTarea", adapter.getIdActivoTarea(idTarea));
 		return createModelAndViewJson(model);
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
 	public ModelAndView getIdTrabajoTarea(Long idTarea, ModelMap model) {
 		model.put("idTrabajoTarea", adapter.getIdTrabajoTarea(idTarea));
@@ -130,166 +146,169 @@ public class AgendaController extends TareaController {
 		model.put("idExpediente", adapter.getIdExpediente(idTarea));
 		model.put("numExpediente", adapter.getNumExpediente(idTarea));
 		return createModelAndViewJson(model);
-	}	
+	}
 
 	@RequestMapping(method = RequestMethod.POST)
 	public ModelAndView getCodigoTramiteTarea(Long idTarea, ModelMap model) {
 		model.put("codigoTramite", adapter.getCodigoTramiteTarea(idTarea));
 		return createModelAndViewJson(model);
 	}
-	
 
 	@RequestMapping(method = RequestMethod.POST)
 	public ModelAndView detalleTarea(Long idTarea, String subtipoTarea) {
-	
+
 		return createModelAndViewJson(new ModelMap("data", adapter.abreTarea(idTarea, subtipoTarea)));
-		
+
 	}
 
-    @RequestMapping(method = RequestMethod.POST)
-    public ModelAndView save(WebRequest request, ModelMap model) {
-                            
-            boolean success = false;
-			try {
-				success = adapter.save(request.getParameterMap());
-				
-			} catch (InvalidDataAccessResourceUsageException e) {
-				// Mensajes concretos para este tipo de excepcion
-				model.put("errorValidacionGuardado", getMensajeInvalidDataAccessExcepcion(e));
-				
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				model.put("errorValidacionGuardado", e.getMessage());
-			}
-    
-            model.put("success", success);
-    
-            return createModelAndViewJson(model);
-    }
-    
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView save(WebRequest request, ModelMap model) {
+
+		boolean success = false;
+		try {
+			success = adapter.save(request.getParameterMap());
+
+		} catch (InvalidDataAccessResourceUsageException e) {
+			// Mensajes concretos para este tipo de excepcion
+			model.put("errorValidacionGuardado", getMensajeInvalidDataAccessExcepcion(e));
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			model.put("errorValidacionGuardado", e.getMessage());
+		}
+
+		model.put("success", success);
+
+		return createModelAndViewJson(model);
+	}
+
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView tareasPendientes(WebRequest request, ModelMap model){
+	public ModelAndView tareasPendientes(WebRequest request, ModelMap model) {
 		boolean success = true;
 		model.put("contador", adapter.tareasPendientes());
-		model.put("success",success);
+		model.put("success", success);
 		return createModelAndViewJson(model);
 	}
-	
-//	@RequestMapping(method = RequestMethod.GET)
-//	public ModelAndView notificacionesPendientes(WebRequest request, ModelMap model){
-//		boolean success = true;
-//		model.put("contador", adapter.notificacionesPendientes());
-//		model.put("success",success);
-//		return createModelAndViewJson(model);
-//	}
-	
-	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView alertasPendientes(WebRequest request, ModelMap model){
-		boolean success = true;
-		model.put("contador", adapter.alertasPendientes());
-		model.put("success",success);
-		return createModelAndViewJson(model);	
-	}
-	
-	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView avisosPendientes(WebRequest request, ModelMap model){
-		boolean success = true;
-		model.put("contador", adapter.avisosPendientes());
-		model.put("success",success);
-		return createModelAndViewJson(model);	
-	}
-	
+
+	// @RequestMapping(method = RequestMethod.GET)
+	// public ModelAndView notificacionesPendientes(WebRequest request, ModelMap
+	// model){
+	// boolean success = true;
+	// model.put("contador", adapter.notificacionesPendientes());
+	// model.put("success",success);
+	// return createModelAndViewJson(model);
+	// }
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView getComboNombreTarea(Long idTipoTramite,WebDto webDto, ModelMap model){
-		
-		model.put("data", adapter.getComboNombreTarea(idTipoTramite));
-		
+	public ModelAndView alertasPendientes(WebRequest request, ModelMap model) {
+		boolean success = true;
+		model.put("contador", adapter.alertasPendientes());
+		model.put("success", success);
 		return createModelAndViewJson(model);
-		
-	}	
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView avisosPendientes(WebRequest request, ModelMap model) {
+		boolean success = true;
+		model.put("contador", adapter.avisosPendientes());
+		model.put("success", success);
+		return createModelAndViewJson(model);
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView getComboNombreTarea(Long idTipoTramite, WebDto webDto, ModelMap model) {
+
+		model.put("data", adapter.getComboNombreTarea(idTipoTramite));
+
+		return createModelAndViewJson(model);
+
+	}
+
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView getAdvertenciaTarea(@RequestParam Long idTarea, ModelMap model){
-		
-		//TODO: Aquí se generan los distintos textos de avisos para la tarea
+	public ModelAndView getAdvertenciaTarea(@RequestParam Long idTarea, ModelMap model) {
+
+		// TODO: Aquí se generan los distintos textos de avisos para la tarea
 		String advertencia = adapter.getAdvertenciaTarea(idTarea);
 		String codigoTramite = adapter.getCodigoTramiteTarea(idTarea);
 		String codigoTareaProcedimiento = adapter.getCodigoTareaProcedimiento(idTarea);
-		
+
 		model.put("codigoTramite", codigoTramite);
 		model.put("codigoTareaProcedimiento", codigoTareaProcedimiento);
 		model.put("advertenciaExisteTrabajo", advertencia);
 		model.put("success", true);
-		
+
 		return createModelAndViewJson(model);
-		
+
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
-	public void generateExcel(DtoTareaFilter dtoTareaFilter, HttpServletRequest request, HttpServletResponse response) throws Exception {
+	public void generateExcel(DtoTareaFilter dtoTareaFilter, HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
 
 		dtoTareaFilter.setStart(excelReportGeneratorApi.getStart());
 		dtoTareaFilter.setLimit(excelReportGeneratorApi.getLimit());
-		
+
 		@SuppressWarnings("unchecked")
-		List<DtoResultadoBusquedaTareasBuzones> listaTareas = (List<DtoResultadoBusquedaTareasBuzones>) adapter.getListTareas(dtoTareaFilter).getResults();
-		
+		List<DtoResultadoBusquedaTareasBuzones> listaTareas = (List<DtoResultadoBusquedaTareasBuzones>) adapter
+				.getListTareas(dtoTareaFilter).getResults();
+
 		ExcelReport report = new TareaExcelReport(listaTareas);
-		
+
 		excelReportGeneratorApi.generateAndSend(report, response);
 
 	}
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView getTiposProcedimientoAgenda(ModelMap model){
+	public ModelAndView getTiposProcedimientoAgenda(ModelMap model) {
 		model.put("data", adapter.getTiposProcedimientoAgenda());
-		
+
 		return createModelAndViewJson(model);
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView generarAutoprorroga(DtoSolicitarProrrogaTarea dtoSolicitarProrroga, ModelMap model){
+	public ModelAndView generarAutoprorroga(DtoSolicitarProrrogaTarea dtoSolicitarProrroga, ModelMap model) {
 		model.put("success", adapter.generarAutoprorroga(dtoSolicitarProrroga));
-		//model.put("success", true);
-		
+		// model.put("success", true);
+
 		return createModelAndViewJson(model);
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView saltoCierreEconomico(Long idTareaExterna, ModelMap model){
+	public ModelAndView saltoCierreEconomico(Long idTareaExterna, ModelMap model) {
 		model.put("success", adapter.saltoCierreEconomico(idTareaExterna));
-		
+
 		return createModelAndViewJson(model);
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView saltoResolucionExpediente(Long idTareaExterna, ModelMap model){
+	public ModelAndView saltoResolucionExpediente(Long idTareaExterna, ModelMap model) {
 		model.put("success", adapter.saltoResolucionExpediente(idTareaExterna));
-		
+
 		return createModelAndViewJson(model);
 	}
-	
-	private String getMensajeInvalidDataAccessExcepcion(InvalidDataAccessResourceUsageException e){
-		
-		// En este tipo de excepción se pueden esconder errores del procedure de BBDD de publicacion ACTIVO_PUBLICACION_AUTO
-		// Hay que mostrar un mensaje de error concreto para uno de los errores (no cumplir condiciones para publicar)
+
+	private String getMensajeInvalidDataAccessExcepcion(InvalidDataAccessResourceUsageException e) {
+
+		// En este tipo de excepción se pueden esconder errores del procedure de
+		// BBDD de publicacion ACTIVO_PUBLICACION_AUTO
+		// Hay que mostrar un mensaje de error concreto para uno de los errores
+		// (no cumplir condiciones para publicar)
 		Throwable causa = e.getCause();
 		String mensajeError = null;
 		int contador = 0;
-		while (!Checks.esNulo(causa) && Checks.esNulo(mensajeError) && contador < 100){
-			if(causa.getMessage().contains("ACTIVO_PUBLICACION_AUTO") && causa.getMessage().contains("ORA-06510")){
-				mensajeError = "El activo no cumple todas las condiciones para ser publicado. Revise OK de precios o las reglas de publicación."; 
+		while (!Checks.esNulo(causa) && Checks.esNulo(mensajeError) && contador < 100) {
+			if (causa.getMessage().contains("ACTIVO_PUBLICACION_AUTO") && causa.getMessage().contains("ORA-06510")) {
+				mensajeError = "El activo no cumple todas las condiciones para ser publicado. Revise OK de precios o las reglas de publicación.";
 			}
 			causa = causa.getCause();
 			contador++;
 		}
 
 		// Para el resto de errores, se muestra un mensaje generico
-		if(Checks.esNulo(mensajeError))
+		if (Checks.esNulo(mensajeError))
 			mensajeError = "Se ha producido un error con la base de datos al avanzar la tarea. Inténtelo más tarde.";
 
 		return mensajeError;
 	}
-	
-	
+
 }

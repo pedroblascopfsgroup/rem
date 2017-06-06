d96ed55d5dbc15805f8c4d446db6c0c844b6321c
Daniel <daniel.albert@pfsgroup.es>
2017-06-06 18:30:21 +0200

    HREOS-2040 Modificacione SP

--- sql/9.2/rem/migracion_carterizada/SP/compila_SP.sh
+++ sql/9.2/rem/migracion_carterizada/SP/compila_SP.sh
@@ -5,25 +5,80 @@ if [ "$#" -ne 1 ]; then
 fi
 
 fichero="SP/SPs.list"
-echo " "
 echo "INICIO DEL SCRIPT $0"
 
 while read line
 do
-	if [ -f "SP/""$line".sql ] ; then
+	if [ -f SP/$line.sql ] ; then
 		echo "########################################################"
-		echo "#####    INICIO $line"
+		echo "#####    INICIO" $line
 		echo "########################################################"
-		echo " "
-		$ORACLE_HOME/bin/sqlplus "$1" @"SP/""$line" > ./Logs/compile_"$line".log
-		if [ $? != 0 ] ; then 
-		   echo -e "\n\n======>>> "Error en @"$line"
-		   exit 1
-		fi
+		$ORACLE_HOME/bin/sqlplus $1 @"SP/""$line".sql > Logs/compile_$line.log
 
-		echo "Fin $line"
-		echo " "
+		if [ $? != 0 ] ; then 
+		   	echo -e "\n\n======>>> "Error en @$line
+		   	exit 1
+		else
+			echo Fin $line
+			echo
+		fi
 	fi
 done < "$fichero"
+
+#if [ -f SP/SP_VALIDACION_DICCIONARIOS.sql ] ; then	
+#	echo "########################################################"	
+#	echo "#####    INICIO" SP_VALIDACION_DICCIONARIOS	
+#	echo "########################################################"	
+#	$ORACLE_HOME/bin/sqlplus $1 @SP/SP_VALIDACION_DICCIONARIOS.sql > Logs/compile_SP_VALIDACION_DICCIONARIOS.log
+#	if [ $? != 0 ] ; then 
+#	   	echo -e "\n\n======>>> "Error en @SP_VALIDACION_DICCIONARIOS
+#	   	exit 1
+#	else
+#		echo Fin SP_VALIDACION_DICCIONARIOS
+#		echo
+#	fi
+#fi
+#
+#if [ -f SP/SP_VALIDACION_DUPLICADOS.sql ] ; then	
+#	echo "########################################################"	
+#	echo "#####    INICIO" SP_VALIDACION_DUPLICADOS	
+#	echo "########################################################"	
+#	$ORACLE_HOME/bin/sqlplus $1 @SP/SP_VALIDACION_DUPLICADOS.sql > Logs/compile_SP_VALIDACION_DUPLICADOS.log
+#	if [ $? != 0 ] ; then 
+#	   	echo -e "\n\n======>>> "Error en @SP_VALIDACION_DUPLICADOS
+#	   	exit 1
+#	else
+#		echo Fin SP_VALIDACION_DUPLICADOS
+#		echo
+#	fi
+#fi
+#
+#if [ -f SP/SP_VALIDACION_FUNCIONALES.sql ] ; then	
+#	echo "########################################################"	
+#	echo "#####    INICIO" SP_VALIDACION_FUNCIONALES	
+#	echo "########################################################"	
+#	$ORACLE_HOME/bin/sqlplus $1 @SP/SP_VALIDACION_FUNCIONALES.sql > Logs/compile_SP_VALIDACION_FUNCIONALES.log
+#	if [ $? != 0 ] ; then 
+#	   	echo -e "\n\n======>>> "Error en @SP_VALIDACION_FUNCIONALES
+#	   	exit 1
+#	else
+#		echo Fin SP_VALIDACION_FUNCIONALES
+#		echo
+#	fi
+#fi
+#
+#if [ -f SP/SP_VALIDACION_DEPENDENCIAS.sql ] ; then	
+#	echo "########################################################"	
+#	echo "#####    INICIO" SP_VALIDACION_DEPENDENCIAS	
+#	echo "########################################################"	
+#	$ORACLE_HOME/bin/sqlplus $1 @SP/SP_VALIDACION_DEPENDENCIAS.sql > Logs/compile_SP_VALIDACION_DEPENDENCIAS.log
+#	if [ $? != 0 ] ; then 
+#	   	echo -e "\n\n======>>> "Error en @SP_VALIDACION_DEPENDENCIAS
+#	   	exit 1
+#	else
+#		echo Fin SP_VALIDACION_DEPENDENCIAS
+#		echo
+#	fi
+#fi
 
 exit 0

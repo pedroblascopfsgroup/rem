79d113a808465778c1bd829889ba0e3190c1721b
Luis Caballero <luis.caballero@pfsgroup.es>
2017-07-13 16:39:09 +0200

    HREOS-2421- Módulo comercial: problemas con filtro Tipo gestor

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/adapter/ActivoAdapter.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/adapter/ActivoAdapter.java
@@ -1296,13 +1296,37 @@ public class ActivoAdapter {
 		return listaUsuariosDto;
 	}
 	
+	public List<DtoUsuario> getComboUsuariosGrupos(long idTipoGestor) {
+		List<DespachoExterno> listDespachoExterno = coreextensionApi.getListAllDespachos(idTipoGestor, false);
+		List<DtoUsuario> listaUsuariosDto = new ArrayList<DtoUsuario>();
 
+		if (!Checks.estaVacio(listDespachoExterno)) {
+			DespachoExterno despachoExterno = listDespachoExterno.get(0);
+			List<Usuario> listaUsuarios = coreextensionApi.getListAllUsuariosData(despachoExterno.getId(), false);
+
+			try {
+				for (Usuario usuario : listaUsuarios) {
+					DtoUsuario dtoUsuario = new DtoUsuario();
+					BeanUtils.copyProperties(dtoUsuario, usuario);
+					if(usuario.getUsuarioGrupo()){
+						listaUsuariosDto.add(dtoUsuario);
+					}
+				}
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return listaUsuariosDto;
+	}
 
 
 	public List<DtoUsuario> getComboUsuariosGestoria() {
 		
 		EXTDDTipoGestor tipoGestorGestoria = genericDao.get(EXTDDTipoGestor.class, genericDao.createFilter(FilterType.EQUALS, "codigo", GestorActivoApi.CODIGO_GESTORIA_FORMALIZACION));
-		return getComboUsuarios(tipoGestorGestoria.getId());
+		return getComboUsuariosGrupos(tipoGestorGestoria.getId());
 	}
 	
 	

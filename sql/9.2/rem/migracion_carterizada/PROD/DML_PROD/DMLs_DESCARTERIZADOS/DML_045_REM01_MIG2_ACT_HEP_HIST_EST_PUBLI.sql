ad6d7cc012c4be67ecf874f040ced6982d6fcfb2
Daniel <daniel.albert@pfsgroup.es>
2017-07-28 15:31:10 +0200

    HREOS-2366 Cosas

--- sql/9.2/rem/migracion_carterizada/PROD/DML_PROD/DMLs_DESCARTERIZADOS/DML_045_REM01_MIG2_ACT_HEP_HIST_EST_PUBLI.sql
+++ sql/9.2/rem/migracion_carterizada/PROD/DML_PROD/DMLs_DESCARTERIZADOS/DML_045_REM01_MIG2_ACT_HEP_HIST_EST_PUBLI.sql
@@ -64,21 +64,54 @@ BEGIN
             '''||V_USUARIO||'''                                    AS USUARIOCREAR,
             SYSDATE                                                     AS FECHACREAR,
             0                                                           AS BORRADO
-          FROM '||V_ESQUEMA||'.'||V_TABLA_MIG||' MIG2
-          INNER JOIN '||V_ESQUEMA||'.ACT_ACTIVO ACT ON ACT.ACT_NUM_ACTIVO = MIG2.HEP_ACT_NUMERO_ACTIVO AND ACT.BORRADO = 0
-          LEFT JOIN '||V_ESQUEMA||'.DD_POR_PORTAL POR ON POR.DD_POR_CODIGO = LPAD(MIG2.HEP_COD_PORTAL,2,0) AND POR.BORRADO = 0
-          LEFT JOIN '||V_ESQUEMA||'.DD_TPU_TIPO_PUBLICACION TPU ON TPU.DD_TPU_CODIGO = LPAD(MIG2.HEP_COD_TIPO_PUBLICACION,2,0) AND TPU.BORRADO = 0
-          LEFT JOIN '||V_ESQUEMA||'.DD_EPU_ESTADO_PUBLICACION EPU ON EPU.DD_EPU_CODIGO = LPAD(MIG2.HEP_COD_ESTADO_PUBLI,2,0) AND EPU.BORRADO = 0
-		  WHERE MIG2.VALIDACION = 0
+          FROM REM01.MIG2_ACT_HEP_HIST_EST_PUBLI MIG2
+          INNER JOIN REM01.ACT_ACTIVO ACT ON ACT.ACT_NUM_ACTIVO = MIG2.HEP_ACT_NUMERO_ACTIVO AND ACT.BORRADO = 0
+          LEFT JOIN REM01.DD_POR_PORTAL POR ON POR.DD_POR_CODIGO = LPAD(MIG2.HEP_COD_PORTAL,2,0) AND POR.BORRADO = 0
+          LEFT JOIN REM01.DD_TPU_TIPO_PUBLICACION TPU ON TPU.DD_TPU_CODIGO = LPAD(MIG2.HEP_COD_TIPO_PUBLICACION,2,0) AND TPU.BORRADO = 0
+          LEFT JOIN REM01.DD_EPU_ESTADO_PUBLICACION EPU ON EPU.DD_EPU_CODIGO = LPAD(MIG2.HEP_COD_ESTADO_PUBLI,2,0) AND EPU.BORRADO = 0
+          WHERE MIG2.VALIDACION = 0
       '
       ;
 
       DBMS_OUTPUT.PUT_LINE('[INFO] - '||to_char(sysdate,'HH24:MI:SS')||'  '||V_ESQUEMA||'.'||V_TABLA||' cargada. '||SQL%ROWCOUNT||' Filas.');
       
-      COMMIT;
       EXECUTE IMMEDIATE V_SENTENCIA;
       
-      
+      V_SENTENCIA := '
+          INSERT INTO '||V_ESQUEMA||'.'||V_TABLA||' HEP (
+              HEP_ID
+              ,ACT_ID
+              ,HEP_FECHA_DESDE
+              ,DD_POR_ID
+              ,DD_TPU_ID
+              ,DD_EPU_ID
+              ,VERSION
+              ,USUARIOCREAR
+              ,FECHACREAR
+              ,BORRADO
+          )
+          SELECT
+            '||V_ESQUEMA||'.S_ACT_HEP_HIST_EST_PUBLICACION.NEXTVAL      AS HEP_ID,
+            ACT.ACT_ID                                                  AS ACT_ID,
+            MIG2.HEP_FECHA_HASTA                                        AS HEP_FECHA_DESDE,
+            POR.DD_POR_ID                                               AS DD_POR_ID,
+            TPU.DD_TPU_ID                                               AS DD_TPU_ID,
+            EPU.DD_EPU_ID                                               AS DD_EPU_ID,
+            0                                                           AS VERSION,
+            '''||V_USUARIO||'''                                         AS USUARIOCREAR,
+            SYSDATE                                                     AS FECHACREAR,
+            0                                                           AS BORRADO
+          FROM REM01.ACT_ACTIVO ACT
+          JOIN REM01.DD_EPU_ESTADO_PUBLICACION EPU ON EPU.DD_EPU_ID = T1.DD_EPU_ID AND EPU.BORRADO = 0 AND EPU.DD_EPU_CODIGO = ''05''
+          JOIN REM01.MIG2_TMP_ULT_HIST_PUBLI MIG2 ON MIG2.HEP_ACT_NUMERO_ACTIVO = ACT.ACT_NUM_ACTIVO AND MIG2.RN = 1 AND MIG2.HEP_COD_ESTADO_PUBLI <> ''05''
+          LEFT JOIN REM01.DD_POR_PORTAL POR ON POR.DD_POR_CODIGO = LPAD(MIG2.HEP_COD_PORTAL,2,0) AND POR.BORRADO = 0
+          LEFT JOIN REM01.DD_TPU_TIPO_PUBLICACION TPU ON TPU.DD_TPU_CODIGO = LPAD(MIG2.HEP_COD_TIPO_PUBLICACION,2,0) AND TPU.BORRADO = 0
+          WHERE ACT.BORRADO = 0';
+
+      DBMS_OUTPUT.PUT_LINE('[INFO] - '||to_char(sysdate,'HH24:MI:SS')||'  '||V_ESQUEMA||'.'||V_TABLA||' cargada con los registros que no vienen en migración (HISTORICOS DESPUBLICADOS). '||SQL%ROWCOUNT||' Filas.');
+
+      COMMIT;
+
       V_SENTENCIA := 'BEGIN '||V_ESQUEMA||'.OPERACION_DDL.DDL_TABLE(''ANALYZE'','''||V_TABLA||''',''10''); END;';
       EXECUTE IMMEDIATE V_SENTENCIA;
    

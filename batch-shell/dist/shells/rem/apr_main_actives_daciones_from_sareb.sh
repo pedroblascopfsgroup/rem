c595675a9a74c917f8e583551592f3808af07d4f
miguel.sanchez <miguel.sanchez@pfsgroup.es>
2017-11-15 12:14:48 +0100

    GC-4297 - modificacion de shell  apr_main_actives_daciones_from_sareb

--- batch-shell/dist/shells/rem/apr_main_actives_daciones_from_sareb.sh
+++ batch-shell/dist/shells/rem/apr_main_actives_daciones_from_sareb.sh
@@ -18,9 +18,9 @@ if [ $? -ne 0 ] ; then
 fi
 
 if [ -f $MAINSH ]; then
-    CLASS="$(cat $MAINSH | grep "^ java" | cut -f10 -d" ")"
+    CLASS="$(cat $MAINSH | grep "^ java" | cut -f8 -d" ")"
     CLASS2=`echo $CLASS | sed -e 's/$ROOT_PATH/./g'`
-    CLASEINICIO="$(cat $MAINSH | grep "^ java" | cut -f11 -d" ")"
+    CLASEINICIO="$(cat $MAINSH | grep "^ java" | cut -f9 -d" ")"
     java -Xms512M -Xmx1536M -Dconfig.dir=$DIR_CONFIG -Dconfig.file.mask=$CFG_FILE -Duser.country=ES -Duser.language=es -cp $CLASS2 $CLASEINICIO --context=Default "$@"
     exit $?
 else

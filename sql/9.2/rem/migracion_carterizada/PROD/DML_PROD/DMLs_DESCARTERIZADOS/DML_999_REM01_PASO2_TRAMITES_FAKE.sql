93f796a581983e01ddacd711572e4195a455f69e
Daniel <daniel.albert@pfsgroup.es>
2017-11-17 10:28:08 +0100

    HREOS-3175 Mod

--- sql/9.2/rem/migracion_carterizada/PROD/DML_PROD/DMLs_DESCARTERIZADOS/DML_999_REM01_PASO2_TRAMITES_FAKE.sql
+++ sql/9.2/rem/migracion_carterizada/PROD/DML_PROD/DMLs_DESCARTERIZADOS/DML_999_REM01_PASO2_TRAMITES_FAKE.sql
@@ -169,7 +169,7 @@ BEGIN
       V_UPDATE := V_UPDATE + SQL%ROWCOUNT;
       DBMS_OUTPUT.PUT_LINE('[INFO] - '||to_char(sysdate,'HH24:MI:SS')||'  '||V_ESQUEMA||'.'||V_TABLA||' actualizada (Gestor comercial). '||V_UPDATE||' Filas.');
 
-      V_UPDATE := 0
+      V_UPDATE := 0;
       EXECUTE IMMEDIATE 'MERGE INTO REM01.MIG2_TRA_TRAMITES_FAKE T1
         USING (
             SELECT DISTINCT MIG.OFR_ID, GEE.USU_ID, TGE.DD_TGE_CODIGO
@@ -196,7 +196,7 @@ BEGIN
       V_UPDATE := V_UPDATE + SQL%ROWCOUNT;
       DBMS_OUTPUT.PUT_LINE('[INFO] - '||to_char(sysdate,'HH24:MI:SS')||'  '||V_ESQUEMA||'.'||V_TABLA||' actualizada (Supervisor comercial). '||V_UPDATE||' Filas.');
 
-      V_UPDATE := 0
+      V_UPDATE := 0;
       EXECUTE IMMEDIATE 'MERGE INTO REM01.MIG2_TRA_TRAMITES_FAKE T1
         USING (
             SELECT DISTINCT MIG.OFR_ID, GEE.USU_ID, TGE.DD_TGE_CODIGO
@@ -223,7 +223,7 @@ BEGIN
       V_UPDATE := V_UPDATE + SQL%ROWCOUNT;
       DBMS_OUTPUT.PUT_LINE('[INFO] - '||to_char(sysdate,'HH24:MI:SS')||'  '||V_ESQUEMA||'.'||V_TABLA||' actualizada (Gestor formalización). '||V_UPDATE||' Filas.');
 
-      V_UPDATE := 0
+      V_UPDATE := 0;
       EXECUTE IMMEDIATE 'MERGE INTO REM01.MIG2_TRA_TRAMITES_FAKE T1
         USING (
             SELECT DISTINCT MIG.OFR_ID, GEE.USU_ID

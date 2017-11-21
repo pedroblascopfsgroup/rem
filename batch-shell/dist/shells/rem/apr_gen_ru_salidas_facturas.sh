d19475844a479330a93afdc73e8eeccbdb1e884c
miguel.sanchez <miguel.sanchez@pfsgroup.es>
2017-11-21 17:44:30 +0100

    HREOS-3166 - modificacion de shells

--- batch-shell/dist/shells/rem/apr_gen_ru_salidas_facturas.sh
+++ batch-shell/dist/shells/rem/apr_gen_ru_salidas_facturas.sh
@@ -22,25 +22,24 @@ if [ -f $MAINSH ]; then
     CLASS2=`echo $CLASS | sed -e 's/$ROOT_PATH/./g'`
     CLASEINICIO="$(cat $MAINSH | grep "^ java" | cut -f11 -d" ")"
     java -Xms512M -Xmx1536M -Dconfig.dir=$DIR_CONFIG -Dconfig.file.mask=$CFG_FILE -Duser.country=ES -Duser.language=es -cp $CLASS2 $CLASEINICIO --context=Default "$@"
-    exit $?
     
     
 if [ $? = 0 ]; then
-
+echo "Inicio de transferencia ficheros RUFACTUCP.txt/RUFACTUSP.txt"
    lftp -c "open -u rm02,R@95pr12 sftp://10.126.128.130; ls /$1"
    if [ $? -ne 0 ]; then
       lftp -c "open -u rm02,R@95pr12 sftp://10.126.128.130; mkdir /$1"
    fi
 
   lftp -u rm02,R@95pr12 sftp://10.126.128.130 <<EOF
-  cd /home/input/
+  cd /$1/
   mput $DIR_SALIDA/RUFACTUCP.txt
   mput $DIR_SALIDA/RUFACTUSP.txt 
   bye
 EOF
 
   exit 0    
-    
+fi    
 else
     echo "$(basename $0) Error en $filename: no se ha encontrado  $MAINSH"
     exit 1

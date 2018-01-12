16c5c7675e95e378a1625af874becb2fcb3136c5
vicente.oron <vicente.oron@pfsgroup.es>
2018-01-12 13:53:39 +0100

    GC-4759 inclusión de nuevos shells de ftp

---
+++ batch-shell/dist/shells/rem/ftp/ftp_put_aspro_rem_files.sh
@@ -0,0 +1,11 @@
+lftp -c "open -u rm02,R@95pr12 sftp://192.168.126.2; ls /$1"
+if [ $? -ne 0 ]; then
+    lftp -c "open -u rm02,R@95pr12 sftp://192.168.126.2; mkdir /$1"
+fi
+lftp -u rm02,R@95pr12 sftp://192.168.126.2 <<EOF
+
+cd /$1/
+mput $DIR_SALIDA/$2.*
+
+bye
+EOF

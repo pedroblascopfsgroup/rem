ed6de2d2af003bd180efe1926084b405ef4b74d6
simeon.nikolaev <simeon.nikolaev@pfsgroup.es>
2018-03-01 13:23:35 +0100

    REMVIP-168 - cambio fecha

--- sql/9.2/rem/solo_produccion/DML_999900258_REM01_REPOSICIONAMIENTO_TRAMITE.sql
+++ sql/9.2/rem/solo_produccion/DML_999900258_REM01_REPOSICIONAMIENTO_TRAMITE.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=SIMEON SHOPOV 
---## FECHA_CREACION=20180301
+--## FECHA_CREACION=20180228
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.2
 --## INCIDENCIA_LINK=REMVIP-168

4461ff5c85aa636f79dd24ed44a65ec2f0207a08
Simeon Nikolaev Shopov <simeon.nikolaev@pfsgroup.es>
2018-03-29 17:22:22 +0200

    REMVIP-384 - daciones

--- sql/9.2/haya/sareb/solo_produccion/DDL_002_ENTITY01_CREATE_TABLE_TMP_APROV_DACIONES.sql
+++ sql/9.2/haya/sareb/solo_produccion/DDL_002_ENTITY01_CREATE_TABLE_TMP_APROV_DACIONES.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=SIMEON SHOPOV
---## FECHA_CREACION=20180326
+--## FECHA_CREACION=20180329
 --## ARTEFACTO=batch
 --## VERSION_ARTEFACTO=9.2
 --## INCIDENCIA_LINK=REMVIP-384

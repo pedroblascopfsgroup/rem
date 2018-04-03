c5005a265d58ee6a4947579d568549cd2a25d10e
Luis Caballero <luis.caballero@pfsgroup.es>
2018-04-03 13:21:06 +0200

    [REMVIP-375] Carga masiva, igual que validación

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/api/impl/AbstractMSVActualizador.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/api/impl/AbstractMSVActualizador.java
@@ -25,9 +25,13 @@ import es.pfsgroup.commons.utils.Checks;
 import es.pfsgroup.commons.utils.api.ApiProxyFactory;
 import es.pfsgroup.framework.paradise.bulkUpload.adapter.ProcessAdapter;
 import es.pfsgroup.framework.paradise.bulkUpload.api.ExcelManagerApi;
+import es.pfsgroup.framework.paradise.bulkUpload.dao.MSVFicheroDao;
+import es.pfsgroup.framework.paradise.bulkUpload.dto.MSVExcelFileItemDto;
 import es.pfsgroup.framework.paradise.bulkUpload.liberators.MSVLiberator;
+import es.pfsgroup.framework.paradise.bulkUpload.model.ExcelFileBean;
 import es.pfsgroup.framework.paradise.bulkUpload.model.MSVDDOperacionMasiva;
 import es.pfsgroup.framework.paradise.bulkUpload.model.MSVDocumentoMasivo;
+import es.pfsgroup.framework.paradise.bulkUpload.utils.MSVExcelParser;
 import es.pfsgroup.framework.paradise.bulkUpload.utils.impl.MSVHojaExcel;
 import es.pfsgroup.framework.paradise.utils.JsonViewerException;
 
@@ -40,6 +44,12 @@ abstract public class AbstractMSVActualizador implements MSVLiberator {
 		
 	@Autowired
 	ProcessAdapter processAdapter;
+	
+	@Autowired
+	private MSVFicheroDao ficheroDao;
+	
+	@Autowired
+	private MSVExcelParser excelParser;
 	
 	@Resource(name = "entityTransactionManager")
     private PlatformTransactionManager transactionManager;
@@ -92,9 +102,16 @@ abstract public class AbstractMSVActualizador implements MSVLiberator {
 					processAdapter.addFilaProcesada(file.getProcesoMasivo().getId(), false);					
 				}
 				
-				if(!mapaErrores.isEmpty() && mapaErrores.get("KO").contains(fila)){
-					FileItem fileItemBBDD = file.getErroresFicheroProcesar();
-					FileItem fileItemErrores= exc.insertarErroresExcel(fileItemBBDD,mapaErrores,0,0,fila);
+				
+			}
+			if(!mapaErrores.isEmpty()){
+				MSVDocumentoMasivo archivo = ficheroDao.findByIdProceso(file.getProcesoMasivo().getId());
+				ExcelFileBean excelFile = new ExcelFileBean();
+				if (!Checks.esNulo(archivo)) {
+					excelFile.setFileItem(archivo.getContenidoFichero());
+					exc = excelParser.getExcel(archivo.getContenidoFichero().getFile());
+					String nomFicheroErrores = exc.crearExcelErroresMejorado(mapaErrores);
+					FileItem fileItemErrores = new FileItem(new File(nomFicheroErrores));
 					processAdapter.setExcelErroresProcesado(file, fileItemErrores);
 				}
 			}

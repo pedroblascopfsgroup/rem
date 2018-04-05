4c26d1cb0269744d30221cac6202d405244e30d5
Daniel Algaba <daniel.algaba@pfsgroup.es>
2018-04-05 14:01:57 +0200

    HREOS-3995 Ajustes Mapeo

--- sql/9.2/rem/DML_999900190_REM01_INSERT_TMP_MAPEO_PUB_VENTA.sql
+++ sql/9.2/rem/DML_999900190_REM01_INSERT_TMP_MAPEO_PUB_VENTA.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=DANIEL ALGABA
---## FECHA_CREACION=20180321
+--## FECHA_CREACION=20180405
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.2
 --## INCIDENCIA_LINK=HREOS-3890
@@ -42,7 +42,7 @@ DECLARE
         T_TIPO_DATA('04', '03', 1, 0, 1, 0, NULL, NULL, NULL),
         T_TIPO_DATA('05', '03', 1, 1, 0, 0, NULL, NULL, NULL),
         T_TIPO_DATA('06', '03', 1, 1, 0, 0, NULL, '12', 'Migración'),
-        T_TIPO_DATA('07', '03', 1, 0, 1, 0, '02', NULL, NULL)
+        T_TIPO_DATA('07', '03', 1, 1, 1, 0, '02', NULL, NULL)
 		); 
     V_TMP_TIPO_DATA T_TIPO_DATA;
     

3720dce3855de87e31ee8097ad7bc75e56d2b514
Simeon Nikolaev Shopov <simeon.nikolaev@pfsgroup.es>
2018-04-16 12:08:58 +0200

    REMVIP-505 fecha fix

--- sql/9.2/rem/solo_produccion/DML_999900260_REM01_MODIFICACION_ID_HAYA.sql
+++ sql/9.2/rem/solo_produccion/DML_999900260_REM01_MODIFICACION_ID_HAYA.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=SIMEON SHOPOV
---## FECHA_CREACION=20180416
+--## FECHA_CREACION=20180415
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.2
 --## INCIDENCIA_LINK=REMVIP-505

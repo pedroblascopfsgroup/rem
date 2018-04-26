170d8f7361b46bfb824ea49763f896691fd0f456
Simeon Nikolaev Shopov <simeon.nikolaev@pfsgroup.es>
2018-04-26 19:09:16 +0200

    REMVIP-505 fix daciones

--- sql/9.2/haya/sareb/solo_produccion/DDL_002_ENTITY01_CREATE_TABLE_TMP_APROV_DACIONES.sql
+++ sql/9.2/haya/sareb/solo_produccion/DDL_002_ENTITY01_CREATE_TABLE_TMP_APROV_DACIONES.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=SIMEON SHOPOV
---## FECHA_CREACION=20180426
+--## FECHA_CREACION=20180427
 --## ARTEFACTO=batch
 --## VERSION_ARTEFACTO=9.2
 --## INCIDENCIA_LINK=REMVIP-505

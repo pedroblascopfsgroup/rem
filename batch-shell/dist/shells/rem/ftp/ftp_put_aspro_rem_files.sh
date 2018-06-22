0b70420fd4385bc581658c986b1af77326e4162d
Gustavo Mora <gustavo.mora@pfsgroup.es>
2018-06-22 16:02:24 +0200

    REMVIP-1075 Modificamos los shells para que contemplen el usuario pfs y la ruta.

--- batch-shell/dist/shells/rem/ftp/ftp_put_aspro_rem_files.sh
+++ batch-shell/dist/shells/rem/ftp/ftp_put_aspro_rem_files.sh
@@ -1,11 +1,7 @@
-lftp -c "open -u rm02,R@95pr12 sftp://192.168.126.7; ls /$1"
-if [ $? -ne 0 ]; then
-    lftp -c "open -u rm02,R@95pr12 sftp://192.168.126.7; mkdir /$1"
-fi
-lftp -u rm02,R@95pr12 sftp://192.168.126.7 <<EOF
+lftp -c "open -u pfs,SwQdLRyFE8A5 sftp://192.168.126.7; ls /Archivos/REM/HayaToPFS/$1"
 
-cd /$1/
-mput $DIR_SALIDA/$2.*
-
+lftp -u pfs,SwQdLRyFE8A5 sftp://192.168.126.7 <<EOF
+cd /Archivos/REM/HayaToPFS/$1
+mput $DIR_SALIDA/$1*$2.*
 bye
 EOF

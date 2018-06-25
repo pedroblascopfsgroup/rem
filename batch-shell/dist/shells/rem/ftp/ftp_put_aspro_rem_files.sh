d07f233aac13810dbabb6a765cba0845ea6a6a6b
Gustavo Mora <gustavo.mora@pfsgroup.es>
2018-06-25 13:46:51 +0200

    REMVIP-1075: tratamiento creación directorio fechas y acceso

--- batch-shell/dist/shells/rem/ftp/ftp_put_aspro_rem_files.sh
+++ batch-shell/dist/shells/rem/ftp/ftp_put_aspro_rem_files.sh
@@ -9,6 +9,6 @@ lftp -c "open -u pfs,SwQdLRyFE8A5 sftp://192.168.126.7; ls /Archivos/REM/PFStoHa
 
 lftp -u pfs,SwQdLRyFE8A5 sftp://192.168.126.7 <<EOF
 cd /Archivos/REM/PFStoHaya/$1
-mput $DIR_SALIDA/$1*$2.*
+mput $DIR_SALIDA*$2.*
 bye
 EOF

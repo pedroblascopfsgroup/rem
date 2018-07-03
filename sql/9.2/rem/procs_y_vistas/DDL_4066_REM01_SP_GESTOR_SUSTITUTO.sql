740f0023059d2ae0b18ede42dda5b323ad64fbe7
Guillermo Llido <guillermo.llido@pfsgroup.es>
2018-07-03 13:57:37 +0200

    [REMVIP-1038] Corregir campo por el que buscar el usuario

--- sql/9.2/rem/procs_y_vistas/DDL_4066_REM01_SP_GESTOR_SUSTITUTO.sql
+++ sql/9.2/rem/procs_y_vistas/DDL_4066_REM01_SP_GESTOR_SUSTITUTO.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=Guillermo Llidó
---## FECHA_CREACION=20180702
+--## FECHA_CREACION=20180703
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.2
 --## INCIDENCIA_LINK=REMVIP-1038
@@ -20,8 +20,8 @@ SET DEFINE OFF;
 CREATE OR REPLACE PROCEDURE #ESQUEMA#.SP_GESTOR_SUSTITUTO
         (     
 		  OPERACION VARCHAR2
-        , V_USU_ID_ORI IN NUMBER
-        , V_USU_ID_SUS IN NUMBER
+        , V_USU_ID_ORI IN VARCHAR2
+        , V_USU_ID_SUS IN VARCHAR2
         , V_FECHA_INICIO IN VARCHAR2
         , V_FECHA_FIN IN VARCHAR2
         , PL_OUTPUT OUT VARCHAR2
@@ -40,164 +40,193 @@ CREATE OR REPLACE PROCEDURE #ESQUEMA#.SP_GESTOR_SUSTITUTO
 
 BEGIN
     IF (OPERACION IS NOT NULL )AND (V_USU_ID_ORI IS NOT NULL) AND (V_FECHA_INICIO IS NOT NULL) THEN
+	
+		EXECUTE IMMEDIATE 'SELECT COUNT(1) FROM '||V_ESQUEMA_M||'.USU_USUARIOS WHERE UPPER(USU_USERNAME) = UPPER('''||V_USU_ID_ORI||''')' INTO V_AUX;
+		
+		IF V_AUX = 1 THEN 
+		
+		   CASE
 
-       CASE
-
-        WHEN OPERACION = 'ALTA' THEN
-
-			EXECUTE IMMEDIATE 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.SGS_GESTOR_SUSTITUTO
-									WHERE USU_ID_ORI = '||V_USU_ID_ORI||'
-									AND (FECHA_FIN > '''||V_FECHA_FIN||''' OR FECHA_FIN IS NULL)
-									AND FECHA_INICIO < '''||V_FECHA_INICIO||'''
-									AND BORRADO = 0' INTO V_AUX;
-                                    
-			IF V_USU_ID_SUS IS NOT NULL THEN
-            
-                IF V_AUX = 0 THEN
-    
-                   V_SQL := 'INSERT INTO '||V_ESQUEMA||'.SGS_GESTOR_SUSTITUTO
-                                (     SGS_ID
-                                    , USU_ID_ORI
-                                    , USU_ID_SUS
-                                    , FECHA_INICIO
-                                    , FECHA_FIN
-                                    , USUARIOCREAR
-                                    , FECHACREAR
-                                    , USUARIOMODIFICAR
-                                    , FECHAMODIFICAR
-                                    , USUARIOBORRAR
-                                    , FECHABORRAR
-                                    )
-                                VALUES ( '||V_ESQUEMA||'.S_SGS_GESTOR_SUSTITUTO.NEXTVAL
-                                        ,'||V_USU_ID_ORI||'
-                                        ,'||V_USU_ID_SUS||'
-                                        ,TO_DATE('''||V_FECHA_INICIO||''',''DD/MM/YYYY'')
-                                        ,TO_DATE('''||V_FECHA_FIN||''',''DD/MM/YYYY'')
-                                        ,'''||USUARIO||'''
-                                        ,SYSDATE
-                                        ,NULL
-                                        ,NULL
-                                        ,NULL
-                                        ,NULL
-                                        )';
-    
-                    EXECUTE IMMEDIATE V_SQL;
-    
-                    PL_OUTPUT := '[INFO] El nuevo registro se ha dado de alta correctamente ' || CHR(10) ;
-    
-                  ELSE
-    
-                    PL_OUTPUT := PL_OUTPUT || '[ERROR] Ya existe un registro con ese rango de fechas' || CHR(10) ;
-    
-                  END IF;
-              
-            ELSE
-            
-                 PL_OUTPUT := PL_OUTPUT || '[ERROR] No se ha informado de la variable V_USU_ID_SUS' || CHR(10) ;
-                 
-            END IF;
-
-        WHEN OPERACION = 'MOD' THEN
-
-            EXECUTE IMMEDIATE 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.SGS_GESTOR_SUSTITUTO
-									WHERE USU_ID_ORI = '||V_USU_ID_ORI||'
-									AND FECHA_FIN >= '''||V_FECHA_FIN||'''
-									AND FECHA_INICIO <= '''||V_FECHA_INICIO||'''
-									AND BORRADO = 0' INTO V_AUX;
-
-			IF V_AUX = 0 THEN
+			WHEN OPERACION = 'ALTA' THEN
 
 				EXECUTE IMMEDIATE 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.SGS_GESTOR_SUSTITUTO
-										WHERE USU_ID_ORI = '||V_USU_ID_ORI||'
-										AND FECHA_INICIO = '''||V_FECHA_INICIO||'''
+										WHERE USU_ID_ORI = (SELECT USU_ID FROM '||V_ESQUEMA_M||'.USU_USUARIOS WHERE UPPER(USU_USERNAME) = UPPER('''||V_USU_ID_ORI||'''))
+										AND (FECHA_FIN > '''||V_FECHA_FIN||''' OR FECHA_FIN IS NULL)
+										AND FECHA_INICIO < '''||V_FECHA_INICIO||'''
+										AND BORRADO = 0' INTO V_AUX;
+										
+				IF V_USU_ID_SUS IS NOT NULL THEN
+				
+					IF V_AUX = 0 THEN
+					
+						EXECUTE IMMEDIATE 'SELECT COUNT(1) FROM '||V_ESQUEMA_M||'.USU_USUARIOS WHERE UPPER(USU_USERNAME) = UPPER('''||V_USU_ID_SUS||''')' INTO V_AUX;
+		
+						IF V_AUX = 1 THEN 
+		
+						   V_SQL := 'INSERT INTO '||V_ESQUEMA||'.SGS_GESTOR_SUSTITUTO
+										(     SGS_ID
+											, USU_ID_ORI
+											, USU_ID_SUS
+											, FECHA_INICIO
+											, FECHA_FIN
+											, USUARIOCREAR
+											, FECHACREAR
+											, USUARIOMODIFICAR
+											, FECHAMODIFICAR
+											, USUARIOBORRAR
+											, FECHABORRAR
+											)
+										VALUES ( '||V_ESQUEMA||'.S_SGS_GESTOR_SUSTITUTO.NEXTVAL
+												,(SELECT USU_ID FROM '||V_ESQUEMA_M||'.USU_USUARIOS WHERE UPPER(USU_USERNAME) = UPPER('''||V_USU_ID_ORI||'''))
+												,(SELECT USU_ID FROM '||V_ESQUEMA_M||'.USU_USUARIOS WHERE UPPER(USU_USERNAME) = UPPER('''||V_USU_ID_SUS||'''))
+												,TO_DATE('''||V_FECHA_INICIO||''',''DD/MM/YYYY'')
+												,TO_DATE('''||V_FECHA_FIN||''',''DD/MM/YYYY'')
+												,'''||USUARIO||'''
+												,SYSDATE
+												,NULL
+												,NULL
+												,NULL
+												,NULL
+												)';
+		
+							EXECUTE IMMEDIATE V_SQL;
+		
+							PL_OUTPUT := '[INFO] El nuevo registro se ha dado de alta correctamente ' || CHR(10) ;
+						
+						ELSE
+						
+							PL_OUTPUT := PL_OUTPUT || '[ERROR] No existe un usuario sustituto con ese nombre de usuario ' || CHR(10) ;
+						
+						END IF;
+						
+					ELSE
+		
+						PL_OUTPUT := PL_OUTPUT || '[ERROR] Ya existe un registro con ese rango de fechas ' || CHR(10) ;
+		
+					END IF;
+				
+				ELSE
+				
+					 PL_OUTPUT := PL_OUTPUT || '[ERROR] No se ha informado de la variable V_USU_ID_SUS' || CHR(10) ;
+					 
+				END IF;
+
+			WHEN OPERACION = 'MOD' THEN
+
+				EXECUTE IMMEDIATE 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.SGS_GESTOR_SUSTITUTO
+										WHERE USU_ID_ORI = (SELECT USU_ID FROM '||V_ESQUEMA_M||'.USU_USUARIOS WHERE UPPER(USU_USERNAME) = UPPER('''||V_USU_ID_ORI||'''))
+										AND FECHA_FIN >= '''||V_FECHA_FIN||'''
+										AND FECHA_INICIO <= '''||V_FECHA_INICIO||'''
 										AND BORRADO = 0' INTO V_AUX;
 
-				IF V_AUX > 0 THEN
+				IF V_AUX = 0 THEN
 
-					V_SQL := 'UPDATE '||V_ESQUEMA||'.SGS_GESTOR_SUSTITUTO SET
-								  FECHA_FIN = TO_DATE('''||V_FECHA_FIN||''',''DD/MM/YYYY'')
-								, USUARIOMODIFICAR = '''||USUARIO||'''
-								, FECHAMODIFICAR = SYSDATE
-                                WHERE USU_ID_ORI = '||V_USU_ID_ORI||'
-								  AND FECHA_INICIO = '''||V_FECHA_INICIO||'''
-								  AND BORRADO = 0';
-                            
-					EXECUTE IMMEDIATE V_SQL;
+					EXECUTE IMMEDIATE 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.SGS_GESTOR_SUSTITUTO
+											WHERE USU_ID_ORI = (SELECT USU_ID FROM '||V_ESQUEMA_M||'.USU_USUARIOS WHERE UPPER(USU_USERNAME) = UPPER('''||V_USU_ID_ORI||'''))
+											AND FECHA_INICIO = '''||V_FECHA_INICIO||'''
+											AND BORRADO = 0' INTO V_AUX;
 
-					PL_OUTPUT := PL_OUTPUT || '[INFO] El nuevo registro se ha modificado correctamente ' || CHR(10) ;
+					IF V_AUX > 0 THEN
+
+						V_SQL := 'UPDATE '||V_ESQUEMA||'.SGS_GESTOR_SUSTITUTO SET
+									  FECHA_FIN = TO_DATE('''||V_FECHA_FIN||''',''DD/MM/YYYY'')
+									, USUARIOMODIFICAR = '''||USUARIO||'''
+									, FECHAMODIFICAR = SYSDATE
+									WHERE USU_ID_ORI = (SELECT USU_ID FROM '||V_ESQUEMA_M||'.USU_USUARIOS WHERE UPPER(USU_USERNAME) = UPPER('''||V_USU_ID_ORI||'''))
+									  AND FECHA_INICIO = '''||V_FECHA_INICIO||'''
+									  AND BORRADO = 0';
+								
+						EXECUTE IMMEDIATE V_SQL;
+
+						PL_OUTPUT := PL_OUTPUT || '[INFO] El nuevo registro se ha modificado correctamente ' || CHR(10) ;
+
+					ELSE
+
+						PL_OUTPUT := PL_OUTPUT || '[ERROR] No existe un registro con esos datos' || CHR(10) ;
+
+					END IF;
 
 				ELSE
 
-					PL_OUTPUT := PL_OUTPUT || '[ERROR] No existe un registro con esos datos' || CHR(10) ;
+					PL_OUTPUT := PL_OUTPUT || '[ERROR] Ya existe un registro dentro del rango de fechas TO_DATE('''||V_FECHA_INICIO||''',''DD/MM/YYYY'') y TO_DATE('''||V_FECHA_FIN||''',''DD/MM/YYYY'') para el Usuario '||V_USU_ID_ORI||' y Sustituto  '||V_USU_ID_SUS||' ' || CHR(10) ;
 
 				END IF;
 
+			WHEN OPERACION = 'BAJA' THEN
+				
+				IF V_USU_ID_SUS IS NOT NULL THEN
+				
+					EXECUTE IMMEDIATE 'SELECT COUNT(1) FROM '||V_ESQUEMA_M||'.USU_USUARIOS WHERE UPPER(USU_USERNAME) = UPPER('''||V_USU_ID_SUS||''')' INTO V_AUX;
+		
+						IF V_AUX = 1 THEN 
+
+						EXECUTE IMMEDIATE 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.SGS_GESTOR_SUSTITUTO
+											WHERE USU_ID_ORI = (SELECT USU_ID FROM '||V_ESQUEMA_M||'.USU_USUARIOS WHERE USU_USERNAME = UPPER('''||V_USU_ID_ORI||'''))
+											AND USU_ID_SUS = (SELECT USU_ID FROM '||V_ESQUEMA_M||'.USU_USUARIOS WHERE USU_USERNAME = UPPER('''||V_USU_ID_SUS||'''))
+											AND BORRADO = 0' INTO V_AUX;
+							IF V_AUX > 0 THEN
+		
+								IF V_FECHA_FIN IS NOT NULL THEN
+		
+									V_SQL := 'UPDATE '||V_ESQUEMA||'.SGS_GESTOR_SUSTITUTO SET
+													FECHA_FIN = TO_DATE('''||V_FECHA_FIN||''',''DD/MM/YYYY'')
+													, USUARIOBORRAR = '''||USUARIO||'''
+													, FECHABORRAR = SYSDATE
+													, BORRADO = 1
+											  WHERE USU_ID_ORI = (SELECT USU_ID FROM '||V_ESQUEMA_M||'.USU_USUARIOS WHERE USU_USERNAME = UPPER('''||V_USU_ID_ORI||'''))
+													AND USU_ID_SUS = (SELECT USU_ID FROM '||V_ESQUEMA_M||'.USU_USUARIOS WHERE USU_USERNAME = UPPER('''||V_USU_ID_SUS||'''))
+													AND FECHA_INICIO = '''||V_FECHA_INICIO||'''
+													AND BORRADO = 0 ';
+								
+									EXECUTE IMMEDIATE V_SQL;
+		
+									PL_OUTPUT := PL_OUTPUT || '[INFO] El registro se ha borrado correctamente ' || CHR(10) ;
+		
+								ELSE
+		
+									V_SQL := 'UPDATE '||V_ESQUEMA||'.SGS_GESTOR_SUSTITUTO SET
+													  USUARIOBORRAR = '''||USUARIO||'''
+													, FECHABORRAR = SYSDATE
+													, BORRADO = 1
+											  WHERE USU_ID_ORI = (SELECT USU_ID FROM '||V_ESQUEMA_M||'.USU_USUARIOS WHERE USU_USERNAME = UPPER('''||V_USU_ID_ORI||'''))
+													AND USU_ID_SUS = (SELECT USU_ID FROM '||V_ESQUEMA_M||'.USU_USUARIOS WHERE USU_USERNAME = UPPER('''||V_USU_ID_SUS||'''))
+													AND FECHA_INICIO = '''||V_FECHA_INICIO||'''
+													AND BORRADO = 0 ';
+							
+									EXECUTE IMMEDIATE V_SQL;
+		
+									PL_OUTPUT := PL_OUTPUT || '[INFO] El registro se ha borrado correctamente ' || CHR(10);
+							
+								END IF;
+		
+							ELSE
+				
+								PL_OUTPUT := PL_OUTPUT || '[ERROR] No existe un registro con esos datos ' || CHR(10);
+				
+							END IF;
+					
+						ELSE
+						
+							PL_OUTPUT := PL_OUTPUT || '[ERROR] No existe un usuario sustituto con ese nombre de usuario ' || CHR(10) ;
+
+						END IF;
+				
+					ELSE
+					
+						PL_OUTPUT := PL_OUTPUT || '[ERROR] No se ha informado de la variable V_USU_ID_SUS ' || CHR(10) ;
+					
+					END IF;
+
 			ELSE
+				PL_OUTPUT := PL_OUTPUT || '[ERROR] No se ha reconocido el código de la operación a ejecutar ' || CHR(10);
 
-				PL_OUTPUT := PL_OUTPUT || '[ERROR] Ya existe un registro dentro del rango de fechas TO_DATE('''||V_FECHA_INICIO||''',''DD/MM/YYYY'') y TO_DATE('''||V_FECHA_FIN||''',''DD/MM/YYYY'') para el Usuario '||V_USU_ID_ORI||' y Sustituto  '||V_USU_ID_SUS||' ' || CHR(10) ;
-
-			END IF;
-
-        WHEN OPERACION = 'BAJA' THEN
-            
-            IF V_USU_ID_SUS IS NOT NULL THEN
-
-                EXECUTE IMMEDIATE 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.SGS_GESTOR_SUSTITUTO
-                                        WHERE USU_ID_ORI = '||V_USU_ID_ORI||'
-                                        AND USU_ID_SUS = '||V_USU_ID_SUS||'
-                                        AND FECHA_INICIO = '''||V_FECHA_INICIO||'''
-                                        AND BORRADO = 0' INTO V_AUX;
-                IF V_AUX > 0 THEN
-    
-                    IF V_FECHA_FIN IS NOT NULL THEN
-    
-                        V_SQL := 'UPDATE '||V_ESQUEMA||'.SGS_GESTOR_SUSTITUTO SET
-                                        FECHA_FIN = TO_DATE('''||V_FECHA_FIN||''',''DD/MM/YYYY'')
-                                        , USUARIOBORRAR = '''||USUARIO||'''
-                                        , FECHABORRAR = SYSDATE
-                                        , BORRADO = 1
-                                  WHERE USU_ID_ORI = '||V_USU_ID_ORI||'
-                                        AND USU_ID_SUS = '||V_USU_ID_SUS||'
-                                        AND FECHA_INICIO = '''||V_FECHA_INICIO||'''
-                                        AND BORRADO = 0 ';
-                            
-                        EXECUTE IMMEDIATE V_SQL;
-    
-                        PL_OUTPUT := PL_OUTPUT || '[INFO] El registro se ha borrado correctamente ' || CHR(10) ;
-    
-                    ELSE
-    
-                        V_SQL := 'UPDATE '||V_ESQUEMA||'.SGS_GESTOR_SUSTITUTO SET
-                                          USUARIOBORRAR = '''||USUARIO||'''
-                                        , FECHABORRAR = SYSDATE
-                                        , BORRADO = 1
-                                  WHERE USU_ID_ORI = '||V_USU_ID_ORI||'
-                                        AND USU_ID_SUS = '||V_USU_ID_SUS||'
-                                        AND FECHA_INICIO = '''||V_FECHA_INICIO||'''
-                                        AND BORRADO = 0 ';
-                        
-                        EXECUTE IMMEDIATE V_SQL;
-    
-                        PL_OUTPUT := PL_OUTPUT || '[INFO] El registro se ha borrado correctamente ' || CHR(10);
-                        
-                    END IF;
-    
-                ELSE
-    
-                    PL_OUTPUT := PL_OUTPUT || '[ERROR] No existe un registro con esos datos ' || CHR(10);
-    
-                END IF;
-                
-            ELSE
-            
-                PL_OUTPUT := PL_OUTPUT || '[ERROR] No se ha informado de la variable V_USU_ID_SUS' || CHR(10) ;
-
-            END IF;
-
-        ELSE
-            PL_OUTPUT := PL_OUTPUT || '[ERROR] No se ha reconocido el código de la operación a ejecutar ' || CHR(10);
-
-        END CASE;
+			END CASE;
+			
+		ELSE 
+		
+			PL_OUTPUT := PL_OUTPUT || '[ERROR] El usuario '||V_USU_ID_ORI||' no existe ' || CHR(10);
+		
+		END IF;
 
     ELSE
 

181ebc30ba9fc2ba3daf070f5cf07b84d0fbac19
Pablo Meseguer Benlloch <pablo.meseguer@pfsgroup.es>
2018-07-12 17:35:58 +0200

    [HREOS-4317] Modificar SP SP_EXT_ACTUALIZA_TASACION

--- sql/9.2/rem/procs_y_vistas/DDL_4070_REM01_SP_EXT_ACTUALIZA_TASACION.sql
+++ sql/9.2/rem/procs_y_vistas/DDL_4070_REM01_SP_EXT_ACTUALIZA_TASACION.sql
@@ -11,6 +11,7 @@
 --## INSTRUCCIONES:
 --## VERSIONES:
 --##        0.1 Versión inicial
+--##        0.2 Se añaden datos de entrada del SP CARGA_TASACIONES
 --##########################################
 --*/
 WHENEVER SQLERROR EXIT SQL.SQLCODE;
@@ -18,33 +19,50 @@ SET SERVEROUTPUT ON;
 SET DEFINE OFF;
 
 create or replace PROCEDURE       SP_EXT_ACTUALIZA_TASACION (
-          ID_ACTIVO_HAYA       IN  #ESQUEMA#.ACT_ACTIVO.ACT_NUM_ACTIVO%TYPE
-       ,  COD_EXPEDIENTE 	   IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_EXPEDIENTE_EXTERNO%TYPE
-       ,  IMPORTE_TAS_TOTAL    IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_IMPORTE_TAS_FIN%TYPE
-       ,  VALOR_MERCADO 	   IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_VALOR_MERCADO%TYPE
-       ,  VALOR_SUELO 		   IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_IMPORTE_VAL_SOLAR%TYPE
-       ,  VALOR_SEGURO 		   IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_VALOR_SEGURO%TYPE
-       ,  VALOR_LIQUIDATIVO    IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_VALOR_LIQUIDATIVO%TYPE
-       ,  VALOR_OBRA_TERMINADA IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_IMPORTE_VALOR_TER%TYPE
-       ,  PORCENTAJE_OBRA_EJEC IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_PORCENTAJE_OBRA%TYPE
-       ,  PORCENTAJE_AJUSTE	   IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_PORCENTAJE_AJUSTE%TYPE
-       ,  TASACION_MODIFICADA  IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_TASACION_MODIFICADA%TYPE
-       ,  CIF_TASADORA 		   IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_CIF_TASADOR%TYPE
-       ,  NOMBRE_TASADORA 	   IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_NOMBRE_TASADOR%TYPE
-       ,  FECHA_TASACION 	   IN  VARCHAR2
-       ,  TECNICO_TASADOR 	   IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_TECNICO_TASADOR%TYPE
-       ,  TIPO_TASACION  	   IN  #ESQUEMA#.DD_TTS_TIPO_TASACION.DD_TTS_CODIGO%TYPE
-       ,  FECHA_CADUCIDAD      IN  VARCHAR2
-       ,  TASACION_ACTIVA      IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_TASACION_ACTIVA%TYPE
-       ,  FECHA_VIGENCIA_INI   IN  VARCHAR2
-       ,  FECHA_VIGENCIA_FIN   IN  VARCHAR2
-       ,  CONDICIONANTE        IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_CONDICIONANTE%TYPE
-       ,  CONDICIONANTE_OBS    IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_CONDICIONANTE_OBS%TYPE
-       ,  ORDEN_ECO            IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_ORDEN_ECO%TYPE
-       ,  ORDEN_ECO_OBS        IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_ORDEN_ECO_OBS%TYPE
-       ,  ADVERTENCIAS 		   IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_ADVERTENCIAS%TYPE
-       ,  ADVERTENCIAS_OBS 	   IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_ADVERTENCIAS_OBS%TYPE
-       ,  PORCENTAJE_PARTICIPA IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_PORCENTAJE_PARTICIPACION%TYPE
+          ID_ACTIVO_HAYA       			IN  #ESQUEMA#.ACT_ACTIVO.ACT_NUM_ACTIVO%TYPE
+       ,  COD_EXPEDIENTE 	   			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_EXPEDIENTE_EXTERNO%TYPE
+       ,  IMPORTE_TAS_TOTAL    			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_IMPORTE_TAS_FIN%TYPE
+       ,  VALOR_MERCADO 	   			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_VALOR_MERCADO%TYPE
+       ,  VALOR_SUELO 		   			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_IMPORTE_VAL_SOLAR%TYPE
+       ,  VALOR_SEGURO 		   			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_VALOR_SEGURO%TYPE
+       ,  VALOR_LIQUIDATIVO    			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_VALOR_LIQUIDATIVO%TYPE
+       ,  VALOR_OBRA_TERMINADA 			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_IMPORTE_VALOR_TER%TYPE
+       ,  PORCENTAJE_OBRA_EJEC 			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_PORCENTAJE_OBRA%TYPE
+       ,  PORCENTAJE_AJUSTE	   			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_PORCENTAJE_AJUSTE%TYPE
+       ,  TASACION_MODIFICADA  			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_TASACION_MODIFICADA%TYPE
+       ,  CIF_TASADORA 		   			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_CIF_TASADOR%TYPE
+       ,  NOMBRE_TASADORA 	   			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_NOMBRE_TASADOR%TYPE
+       ,  FECHA_TASACION 	   			IN  VARCHAR2
+       ,  TECNICO_TASADOR 	   			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_TECNICO_TASADOR%TYPE
+       ,  TIPO_TASACION  	   			IN  #ESQUEMA#.DD_TTS_TIPO_TASACION.DD_TTS_CODIGO%TYPE
+       ,  FECHA_CADUCIDAD      			IN  VARCHAR2
+       ,  TASACION_ACTIVA      			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_TASACION_ACTIVA%TYPE
+       ,  FECHA_VIGENCIA_INI   			IN  VARCHAR2
+       ,  FECHA_VIGENCIA_FIN   			IN  VARCHAR2
+       ,  CONDICIONANTE        			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_CONDICIONANTE%TYPE
+       ,  CONDICIONANTE_OBS    			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_CONDICIONANTE_OBS%TYPE
+       ,  ORDEN_ECO            			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_ORDEN_ECO%TYPE
+       ,  ORDEN_ECO_OBS        			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_ORDEN_ECO_OBS%TYPE
+       ,  ADVERTENCIAS 		   			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_ADVERTENCIAS%TYPE
+       ,  ADVERTENCIAS_OBS 	   			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_ADVERTENCIAS_OBS%TYPE
+       ,  PORCENTAJE_PARTICIPA 			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_PORCENTAJE_PARTICIPACION%TYPE
+       ,  OBSERVACIONES 	   			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_OBSERVACIONES%TYPE
+       ,  IMPORTE_VAL_LEGAL_FINCA 		IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_IMPORTE_VAL_LEGAL_FINCA%TYPE
+       ,  ID_TEXTO_ASOCIADO 			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_ID_TEXTO_ASOCIADO%TYPE
+       ,  COSTE_REPOSICION 				IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_COSTE_REPOSICION%TYPE
+       ,  COSTE_UNI_REPO_NETO 			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_COSTE_UNI_REPO_NETO%TYPE
+       ,  COSTE_CONST_DEPRE 			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_COSTE_CONST_DEPRE%TYPE
+       ,  INDICE_TOTAL_DEPRE 			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_INDICE_TOTAL_DEPRE%TYPE
+       ,  INDICE_DEPRE_FUNCIONAL 		IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_INDICE_DEPRE_FUNCIONAL%TYPE
+       ,  INDICE_DEPRE_FISICA 			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_INDICE_DEPRE_FISICA%TYPE
+       ,  COSTE_CONST_CONST 			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_COSTE_CONST_CONST%TYPE
+       ,  VALOR_REPER_SUELO_CONST 		IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_VALOR_REPER_SUELO_CONST%TYPE
+       ,  COEF_POND_VALOR_ANYADIDO 		IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_COEF_POND_VALOR_ANYADIDO%TYPE
+       ,  COEF_MERCADO_ESTADO 			IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_COEF_MERCADO_ESTADO%TYPE
+       ,  COSTE_REPO_NETO_FINALIZADO 	IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_COSTE_REPO_NETO_FINALIZADO%TYPE
+       ,  COSTE_REPO_NETO_ACTUAL 		IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_COSTE_REPO_NETO_ACTUAL%TYPE
+       ,  CODIGO_FIRMA 					IN  #ESQUEMA#.ACT_TAS_TASACION.TAS_CODIGO_FIRMA%TYPE
+       ,  FECHA_RECEPCION_TASACION 		IN  VARCHAR2
        ,  COD_RETORNO    	   OUT NUMBER
 ) AS
 
@@ -99,7 +117,24 @@ V_TIPO_DATA T_ARRAY_DATA := T_ARRAY_DATA(
 	T_TIPO_DATA(''''||ORDEN_ECO_OBS||'''','TAS_ORDEN_ECO_OBS'),
 	T_TIPO_DATA(''''||ADVERTENCIAS||'''','TAS_ADVERTENCIAS'),
 	T_TIPO_DATA(''''||ADVERTENCIAS_OBS||'''','TAS_ADVERTENCIAS_OBS'),
-	T_TIPO_DATA(''''||PORCENTAJE_PARTICIPA||'''','TAS_PORCENTAJE_PARTICIPACION')
+	T_TIPO_DATA(''''||PORCENTAJE_PARTICIPA||'''','TAS_PORCENTAJE_PARTICIPACION'),
+	T_TIPO_DATA(''''||OBSERVACIONES||'''','TAS_OBSERVACIONES'),
+	T_TIPO_DATA(''''||IMPORTE_VAL_LEGAL_FINCA||'''','TAS_IMPORTE_VAL_LEGAL_FINCA'),
+	T_TIPO_DATA(''''||ID_TEXTO_ASOCIADO||'''','TAS_ID_TEXTO_ASOCIADO'),
+	T_TIPO_DATA(''''||COSTE_REPOSICION||'''','TAS_COSTE_REPOSICION'),
+	T_TIPO_DATA(''''||COSTE_UNI_REPO_NETO||'''','TAS_COSTE_UNI_REPO_NETO'),
+	T_TIPO_DATA(''''||COSTE_CONST_DEPRE||'''','TAS_COSTE_CONST_DEPRE'),
+	T_TIPO_DATA(''''||INDICE_TOTAL_DEPRE||'''','TAS_INDICE_TOTAL_DEPRE'),
+	T_TIPO_DATA(''''||INDICE_DEPRE_FUNCIONAL||'''','TAS_INDICE_DEPRE_FUNCIONAL'),
+	T_TIPO_DATA(''''||INDICE_DEPRE_FISICA||'''','TAS_INDICE_DEPRE_FISICA'),
+	T_TIPO_DATA(''''||COSTE_CONST_CONST||'''','TAS_COSTE_CONST_CONST'),
+	T_TIPO_DATA(''''||VALOR_REPER_SUELO_CONST||'''','TAS_VALOR_REPER_SUELO_CONST'),
+	T_TIPO_DATA(''''||COEF_POND_VALOR_ANYADIDO||'''','TAS_COEF_POND_VALOR_ANYADIDO'),
+	T_TIPO_DATA(''''||COEF_MERCADO_ESTADO||'''','TAS_COEF_MERCADO_ESTADO'),
+	T_TIPO_DATA(''''||COSTE_REPO_NETO_FINALIZADO||'''','TAS_COSTE_REPO_NETO_FINALIZADO'),
+	T_TIPO_DATA(''''||COSTE_REPO_NETO_ACTUAL||'''','TAS_COSTE_REPO_NETO_ACTUAL'),
+	T_TIPO_DATA(''''||CODIGO_FIRMA||'''','TAS_CODIGO_FIRMA'),
+	T_TIPO_DATA(''''||FECHA_RECEPCION_TASACION||'''','TAS_FECHA_RECEPCION_TASACION')
 
 );
 V_TMP_TIPO_DATA T_TIPO_DATA;
@@ -223,10 +258,10 @@ BEGIN
                                    ''TAS_ID'',
                                    '||TAS_ID||',
                                    '''||V_TMP_TIPO_DATA(2)||''',
-                                   '''||VALOR||''',
+                                   SUBSTR('''||VALOR||''', 1, 30),
                                    CASE WHEN '''||V_TMP_TIPO_DATA(2)||''' = ''DD_TTS_ID'' THEN ''''||(SELECT TTS.DD_TTS_ID FROM '||V_ESQUEMA||'.DD_TTS_TIPO_TASACION TTS WHERE TTS.DD_TTS_CODIGO = '||V_TMP_TIPO_DATA(1)||')
                                         WHEN '''||V_TMP_TIPO_DATA(2)||''' LIKE ''%FECHA%'' THEN ''''||TO_DATE('||V_TMP_TIPO_DATA(1)||',''yyyymmdd'')
-                                        ELSE '||V_TMP_TIPO_DATA(1)||' END
+                                        ELSE SUBSTR('||V_TMP_TIPO_DATA(1)||',1 ,30) END
                                    FROM DUAL
                             ';
                 EXECUTE IMMEDIATE V_MSQL;
@@ -340,9 +375,26 @@ BEGIN
                               ,  TAS_ORDEN_ECO_OBS
                               ,  TAS_ADVERTENCIAS
                               ,  TAS_ADVERTENCIAS_OBS
-                              ,  TAS_PORCENTAJE_PARTICIPACION         
+                              ,  TAS_PORCENTAJE_PARTICIPACION
+                              ,  TAS_OBSERVACIONES
+                              ,  TAS_IMPORTE_VAL_LEGAL_FINCA
+                              ,  TAS_ID_TEXTO_ASOCIADO
+                              ,  TAS_COSTE_REPOSICION
+                              ,  TAS_COSTE_UNI_REPO_NETO
+                              ,  TAS_COSTE_CONST_DEPRE
+                              ,  TAS_INDICE_TOTAL_DEPRE
+                              ,  TAS_INDICE_DEPRE_FUNCIONAL
+                              ,  TAS_INDICE_DEPRE_FISICA
+                              ,  TAS_COSTE_CONST_CONST
+                              ,  TAS_VALOR_REPER_SUELO_CONST
+                              ,  TAS_COEF_POND_VALOR_ANYADIDO
+                              ,  TAS_COEF_MERCADO_ESTADO
+                              ,  TAS_COSTE_REPO_NETO_FINALIZADO
+                              ,  TAS_COSTE_REPO_NETO_ACTUAL
+                              ,  TAS_CODIGO_FIRMA
+                              ,  TAS_FECHA_RECEPCION_TASACION
                             )
-                            SELECT '||ACT_ID||',             
+                            SELECT '||ACT_ID||',
                                    '||TAS_ID||',
                                    '||BIE_VAL_ID||',
                                    '''||V_NOMBRESP||''',
@@ -371,7 +423,24 @@ BEGIN
                                    '''||ORDEN_ECO_OBS||''',
                                    '''||ADVERTENCIAS||''',
                                    '''||ADVERTENCIAS_OBS||''',
-                                   '''||PORCENTAJE_PARTICIPA||'''
+                                   '''||PORCENTAJE_PARTICIPA||''',                                   
+                                   '''||OBSERVACIONES||''',          
+                                   '''||IMPORTE_VAL_LEGAL_FINCA||''',    
+                                   '''||ID_TEXTO_ASOCIADO||''',          
+                                   '''||COSTE_REPOSICION||''',          
+                                   '''||COSTE_UNI_REPO_NETO||''',        
+                                   '''||COSTE_CONST_DEPRE||''',          
+                                   '''||INDICE_TOTAL_DEPRE||''',         
+                                   '''||INDICE_DEPRE_FUNCIONAL||''',     
+                                   '''||INDICE_DEPRE_FISICA||''',        
+                                   '''||COSTE_CONST_CONST||''',          
+                                   '''||VALOR_REPER_SUELO_CONST||''',    
+                                   '''||COEF_POND_VALOR_ANYADIDO||''',   
+                                   '''||COEF_MERCADO_ESTADO||''',        
+                                   '''||COSTE_REPO_NETO_FINALIZADO||''', 
+                                   '''||COSTE_REPO_NETO_ACTUAL||''',     
+                                   '''||CODIGO_FIRMA||''',          
+                                   TO_DATE('''||FECHA_RECEPCION_TASACION||''' ,''yyyymmdd'')  
                             FROM DUAL
                 ';
                 EXECUTE IMMEDIATE V_MSQL;
@@ -401,11 +470,11 @@ BEGIN
                                        ''ACT_TAS_TASACION'',
                                        ''TAS_ID'',
                                        '||TAS_ID||',
-                                       '''||V_TMP_TIPO_DATA(2)||''',
+                                       SUBSTR('''||V_TMP_TIPO_DATA(2)||''', 1, 30),
                                        NULL,
                                        CASE WHEN '''||V_TMP_TIPO_DATA(2)||''' = ''DD_TTS_ID'' THEN ''''||(SELECT TTS.DD_TTS_ID FROM '||V_ESQUEMA||'.DD_TTS_TIPO_TASACION TTS WHERE TTS.DD_TTS_CODIGO = '||V_TMP_TIPO_DATA(1)||')
                                             WHEN '''||V_TMP_TIPO_DATA(2)||''' LIKE ''%FECHA%'' THEN ''''||TO_DATE('||V_TMP_TIPO_DATA(1)||',''yyyymmdd'')
-                                            ELSE '||V_TMP_TIPO_DATA(1)||' END
+                                            ELSE SUBSTR('||V_TMP_TIPO_DATA(1)||', 1, 30) END
                                        FROM DUAL
                                 ';
                     EXECUTE IMMEDIATE V_MSQL;

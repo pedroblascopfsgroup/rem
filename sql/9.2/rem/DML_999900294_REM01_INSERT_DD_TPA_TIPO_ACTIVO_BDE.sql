5fd27dd4a492dcdee1d14f8e2952d15eb3ef9079
Guillermo Llido <guillermo.llido@pfsgroup.es>
2018-07-24 13:28:36 +0200

    [REMVIP-1373] BdE & Promoción final

--- sql/9.2/rem/DML_999900294_REM01_INSERT_DD_TPA_TIPO_ACTIVO_BDE.sql
+++ sql/9.2/rem/DML_999900294_REM01_INSERT_DD_TPA_TIPO_ACTIVO_BDE.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=Guillermo Llidó Parra
---## FECHA_CREACION=20180719
+--## FECHA_CREACION=20180724
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.2
 --## INCIDENCIA_LINK=REMVIP-1373

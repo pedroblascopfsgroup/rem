281bd8c4e079d43c225136d182384e1dd7da09a8
Guillermo Llido <guillermo.llido@pfsgroup.es>
2018-08-01 13:32:01 +0200

    [REMVIP-1408] Nuevo proceso ETL mail Stock Cooper

--- sql/9.2/rem/procs_y_vistas/DDL_4074_REM01_SP_REPORT_STOCK_COOPER.sql
+++ sql/9.2/rem/procs_y_vistas/DDL_4074_REM01_SP_REPORT_STOCK_COOPER.sql
@@ -29,7 +29,7 @@ CREATE OR REPLACE PROCEDURE #ESQUEMA#.SP_REPORT_STOCK_COOPER(SP_OUTPUT OUT VARCH
     V_FROM 			VARCHAR2(100 CHAR) := 'noreply.rem@pfsgroup.es';
     V_TO 			VARCHAR2(100 CHAR) := 'luis.morales@haya.es, jhernandezbe@haya.es, jespejo@haya.es';
     V_BODY 			VARCHAR2(500 CHAR) := 'Se adjunta el informe diario del stock de COOPER.';
-    V_CC 			VARCHAR2(500 CHAR) := 'lgomezc@haya.es, dgutierrez@haya.es, guillermo.llido@pfsgroup.es';
+    V_CC 			VARCHAR2(500 CHAR) := 'lgomezc@haya.es, dgutierrez@haya.es, guillermo.llido@pfsgroup.es, daniel.albert@pfsgroup.es';
     V_ASUNTO 		VARCHAR2(250 CHAR) := 'Informe REM del Stock de COOPER ';
     V_ADJUNTO 		VARCHAR2(250 CHAR) := 'Informe_stock_cartera_COOPER.xlsx';
     

d082bd02386e32b0457f9462b41aceb89aa44edb
Ivan Rubio <ivan.rubio@pfsgroup.es>
2018-08-06 12:29:58 +0200

    HREOS-4377 Corrección script para subida a integración correctamente

--- sql/9.2/rem/DML_99990348_REM01_TRAMITE_SANCIONOFERTA_COMERCIAL_ALQUILER.sql
+++ sql/9.2/rem/DML_99990348_REM01_TRAMITE_SANCIONOFERTA_COMERCIAL_ALQUILER.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=Ivan Rubio
---## FECHA_CREACION=20180727
+--## FECHA_CREACION=20180806
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.2
 --## INCIDENCIA_LINK=HREOS-4311

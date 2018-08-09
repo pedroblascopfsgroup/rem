ed176b09c1923dd4e05b877dc6bed1133f9e82c5
Ivan Rubio <ivan.rubio@pfsgroup.es>
2018-08-09 13:31:04 +0200

    HREOS-4377 Corrección errores tarea elevar a sanción.

--- proyecto-rem-online/sencha-app/classic/src/view/agenda/TareaGenerica.js
+++ proyecto-rem-online/sencha-app/classic/src/view/agenda/TareaGenerica.js
@@ -1779,18 +1779,8 @@ Ext.define('HreRem.view.agenda.TareaGenerica', {
 		me.deshabilitarCampo(me.down('[name=comite]'));
 		me.deshabilitarCampo(me.down('[name=fechaElevacion]'));
 		
-		if(!Ext.ClassManager.isCreated('comiteAlquiler')){
-			Ext.define('comiteAlquiler', {
-			     extend: 'Ext.data.Model',
-			     fields: [
-			         {name: 'codigo', type: 'string'},
-			         {name: 'descripcion',  type: 'string'}
-			     ]
-			 });
-		}
-
 		var storeComite = Ext.create('Ext.data.Store', {
-		     model: 'comiteAlquiler',
+		     model:Ext.create('HreRem.model.ComiteAlquilerModel'),
 		     proxy: {
 		         type: 'uxproxy',
 		         remoteUrl: 'generic/getComitesAlquilerByCartera',

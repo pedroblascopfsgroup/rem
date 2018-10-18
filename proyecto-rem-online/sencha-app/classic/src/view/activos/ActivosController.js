114132675763a561d086a139ebabb7e33060bc5c
Pier <pier.gotta@pfsgroup.es>
2018-10-18 11:35:39 +0200

    REMVIP-2281 Limitacion exportacion excel de activos

--- proyecto-rem-online/sencha-app/classic/src/view/activos/ActivosController.js
+++ proyecto-rem-online/sencha-app/classic/src/view/activos/ActivosController.js
@@ -56,29 +56,34 @@ Ext.define('HreRem.view.activos.ActivosController', {
 		
     	var me = this,
 		config = {};
+    	var count = me.getViewModel().data.activos.totalCount;
 		
-		
-		
-		var initialData = {};
-
-		var searchForm = btn.up('formBase');
-		if (searchForm.isValid()) {
-			var params = Ext.apply(initialData, searchForm ? searchForm.getValues() : {});
+		if(count < CONST.EXPORTADOR['LIMITE']){
 			
-			Ext.Object.each(params, function(key, val) {
-				if (Ext.isEmpty(val)) {
-					delete params[key];
-				}
-			});
-        }
-		
-		config.params = params;
-		config.url= $AC.getRemoteUrl("activo/generateExcel");
-		//config.params = {};
-		//config.params.idProcess = this.getView().down('grid').selection.data.id;
-		
-		me.fireEvent("downloadFile", config);		
-    	
+			var initialData = {};
+
+			var searchForm = btn.up('formBase');
+			if (searchForm.isValid()) {
+				var params = Ext.apply(initialData, searchForm ? searchForm.getValues() : {});
+				
+				Ext.Object.each(params, function(key, val) {
+					if (Ext.isEmpty(val)) {
+						delete params[key];
+					}
+				});
+	        }
+			
+			config.params = params;
+			config.url= $AC.getRemoteUrl("activo/generateExcel");
+			//config.params = {};
+			//config.params.idProcess = this.getView().down('grid').selection.data.id;
+			
+			me.fireEvent("downloadFile", config);	
+			
+		} else{
+        	me.fireEvent("errorToast", HreRem.i18n("msg.operacion.ko.exportar.activos"));
+		}
+	
     	
 		},
 		    

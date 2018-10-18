92572ae6e024b97d08893e3ae761f48acb2527b2
Ramon Llinares <ramon.llinares@pfsgroup.es>
2018-10-18 17:22:41 +0200

    Revert "REMVIP-2267 Reasignacion de gestores de mantenimiento"
    This reverts commit b73317f1c81ff7afb0754552a86959ff6bd734ec.

--- sql/9.2/rem/procs_y_vistas/DDL_133_REM01_VI_GESTORES_ACTIVO.sql
+++ sql/9.2/rem/procs_y_vistas/DDL_133_REM01_VI_GESTORES_ACTIVO.sql
@@ -89,13 +89,13 @@ SELECT act.act_id, TO_NUMBER(cra.dd_cra_codigo), NULL dd_eac_codigo, NULL dd_tcr
            where act.borrado = 0
            UNION ALL
 /*Gestor del activo*/
-SELECT act.act_id, TO_NUMBER (dd_cra.dd_cra_codigo) dd_cra_codigo, TO_NUMBER (COALESCE (dist4.cod_estado_activo, dist3.cod_estado_activo, dist2.cod_estado_activo, dist1.cod_estado_activo, dist0.cod_estado_activo)) dd_eac_codigo,
+SELECT act.act_id, TO_NUMBER (dd_cra.dd_cra_codigo) dd_cra_codigo, TO_NUMBER (COALESCE (dist3.cod_estado_activo, dist2.cod_estado_activo, dist1.cod_estado_activo, dist0.cod_estado_activo)) dd_eac_codigo,
     NULL dd_tcr_codigo, dd_prov.dd_prv_codigo, 
-       COALESCE (dist4.cod_municipio, dist3.cod_municipio,dist2.cod_municipio,dist1.cod_municipio,dist0.cod_municipio) cod_municipio, 
-       COALESCE (dist4.cod_postal, dist3.cod_postal, dist2.cod_postal, dist1.cod_postal, dist0.cod_postal) cod_postal,
-       COALESCE (dist4.tipo_gestor, dist3.tipo_gestor, dist2.tipo_gestor, dist1.tipo_gestor, dist0.tipo_gestor) AS tipo_gestor, 
-       COALESCE (dist4.username, dist3.username, dist2.username, dist1.username, dist0.username) username,
-       COALESCE (dist4.nombre_usuario, dist3.nombre_usuario, dist2.nombre_usuario, dist1.nombre_usuario, dist0.nombre_usuario) nombre
+       COALESCE (dist3.cod_municipio,dist2.cod_municipio,dist1.cod_municipio,dist0.cod_municipio) cod_municipio, 
+       COALESCE (dist3.cod_postal, dist2.cod_postal, dist1.cod_postal, dist0.cod_postal) cod_postal,
+       COALESCE (dist3.tipo_gestor, dist2.tipo_gestor, dist1.tipo_gestor, dist0.tipo_gestor) AS tipo_gestor, 
+       COALESCE (dist3.username, dist2.username, dist1.username, dist0.username) username,
+       COALESCE (dist3.nombre_usuario, dist2.nombre_usuario, dist1.nombre_usuario, dist0.nombre_usuario) nombre
   FROM '||V_ESQUEMA||'.act_activo act JOIN '||V_ESQUEMA||'.act_loc_localizacion aloc ON act.act_id = aloc.act_id
        JOIN '||V_ESQUEMA||'.bie_localizacion loc ON loc.bie_loc_id = aloc.bie_loc_id
        JOIN '||V_ESQUEMA_M||'.dd_loc_localidad dd_loc ON loc.dd_loc_id = dd_loc.dd_loc_id
@@ -119,7 +119,7 @@ SELECT act.act_id, TO_NUMBER (dd_cra.dd_cra_codigo) dd_cra_codigo, TO_NUMBER (CO
            AND dist1.tipo_gestor = ''GACT''
           )
        left JOIN '||V_ESQUEMA||'.act_ges_dist_gestores dist2
-       ON (dist2.cod_estado_activo IS NULL
+       ON (dd_eac.dd_eac_codigo = dist2.cod_estado_activo
            AND dist2.cod_cartera = dd_cra.dd_cra_codigo
            AND dd_prov.dd_prv_codigo = dist2.cod_provincia
            AND dist2.cod_municipio = dd_loc.dd_loc_codigo
@@ -131,16 +131,8 @@ SELECT act.act_id, TO_NUMBER (dd_cra.dd_cra_codigo) dd_cra_codigo, TO_NUMBER (CO
            AND dist3.cod_cartera = dd_cra.dd_cra_codigo
            AND dd_prov.dd_prv_codigo = dist3.cod_provincia
            AND dist3.cod_municipio = dd_loc.dd_loc_codigo
-           AND dist3.cod_postal IS NULL
+           AND dist3.cod_postal  = loc.BIE_LOC_COD_POST
            AND dist3.tipo_gestor = ''GACT''
-          )
-       left JOIN '||V_ESQUEMA||'.act_ges_dist_gestores dist4
-       ON (dd_eac.dd_eac_codigo = dist4.cod_estado_activo
-           AND dist4.cod_cartera = dd_cra.dd_cra_codigo
-           AND dd_prov.dd_prv_codigo = dist4.cod_provincia
-           AND dist4.cod_municipio = dd_loc.dd_loc_codigo
-           AND dist4.cod_postal  = loc.BIE_LOC_COD_POST
-           AND dist4.tipo_gestor = ''GACT''
           )   
           where act.borrado = 0                 
            UNION ALL

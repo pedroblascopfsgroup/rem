62a44000f406442ed664a8b17766a0bedf9764d0
Javier Pons <javier.pons@pfsgroup.es>
2018-11-12 12:42:48 +0100

    REMVIP-2386:Propagacion activos
    Conflicts:
    	recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/activo/ActivoPropagacionFieldTabMap.java

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/activo/ActivoPropagacionFieldTabMap.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/activo/ActivoPropagacionFieldTabMap.java
@@ -35,7 +35,6 @@ public class ActivoPropagacionFieldTabMap {
 	    			"tipoActivoCodigo",
 	    			"subtipoActivoCodigo",
 	    			"estadoActivoCodigo",
-
 	    			// direccion
 	    			"tipoUsoDestinoCodigo",
 	    			"longitud",

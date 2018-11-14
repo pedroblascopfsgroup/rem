5843580c70a1143312d1efb37eda23d56b7f4deb
Marco Muñoz <marco.munoz@pfsgroup.es>
2018-11-14 13:52:02 +0100

    [HREOS-4758] Script para mergear estados del activo. Parte 2.3.

--- sql/9.2/rem/solo_produccion/DML_999900390_REM01_HREOS-4758_MERGES_PERIMETROS_Y_PUBLICACION_PARTE_2.sql
+++ sql/9.2/rem/solo_produccion/DML_999900390_REM01_HREOS-4758_MERGES_PERIMETROS_Y_PUBLICACION_PARTE_2.sql
@@ -40,13 +40,15 @@ DBMS_OUTPUT.PUT_LINE('[INICIO] Comienza el proceso...');
 
 --1º: Pasamos todos los activos con expediente "Vendido" a situación comercial igual a ------> "Vendido".
 ----------------------------------------------------------------------------------------------------------------
-MERGE INTO REM01.ACT_ACTIVO T1
+EXECUTE IMMEDIATE 'MERGE INTO REM01.ACT_ACTIVO T1
 USING (
-        SELECT DISTINCT ACT.ACT_ID AS ACT_ID
+        SELECT  DISTINCT ACT.ACT_ID AS ACT_ID
         FROM REM01.ACT_ACTIVO                           ACT
-        JOIN REM01.ACT_OFR                              AOF
+        JOIN ACT_PAC_PERIMETRO_ACTIVO                   PAC
+        ON PAC.ACT_ID = ACT.ACT_ID
+        LEFT JOIN REM01.ACT_OFR                         AOF
         ON AOF.ACT_ID = ACT.ACT_ID
-        JOIN REM01.OFR_OFERTAS                          OFR
+        LEFT JOIN REM01.OFR_OFERTAS                     OFR
         ON OFR.OFR_ID = AOF.OFR_ID
         LEFT JOIN REM01.ECO_EXPEDIENTE_COMERCIAL        ECO
         ON ECO.OFR_ID = OFR.OFR_ID
@@ -62,14 +64,16 @@ USING (
         ON SCM.DD_SCM_ID = ACT.DD_SCM_ID
         LEFT JOIN REM01.DD_TCO_TIPO_COMERCIALIZACION    TCO
         ON TCO.DD_TCO_ID = ACT.DD_TCO_ID
-        WHERE SCM.DD_SCM_CODIGO IN ('01')
-          AND EEC.DD_EEC_CODIGO IN ('08')
+        WHERE SCM.DD_SCM_CODIGO IN (''01'')
+          AND PAC.PAC_INCLUIDO = 1
+          AND PAC.PAC_CHECK_COMERCIALIZAR = 1
+          AND EEC.DD_EEC_CODIGO IN (''08'')
 ) T2
 ON (T1.ACT_ID = T2.ACT_ID)
 WHEN MATCHED THEN UPDATE SET
-    T1.DD_SCM_ID = (SELECT DD_SCM_ID FROM REM01.DD_SCM_SITUACION_COMERCIAL WHERE DD_SCM_CODIGO IN ('05')),
-    T1.USUARIOMODIFICAR = 'HREOS-4758',
-    T1.FECHAMODIFICAR = SYSDATE
+    T1.DD_SCM_ID = (SELECT DD_SCM_ID FROM REM01.DD_SCM_SITUACION_COMERCIAL WHERE DD_SCM_CODIGO IN (''05'')),
+    T1.USUARIOMODIFICAR = ''HREOS-4758'',
+    T1.FECHAMODIFICAR = SYSDATE'
 ;
 DBMS_OUTPUT.PUT_LINE('[INFO] Se pasan '||SQL%ROWCOUNT||' activos a Vendidos.'); 
 
@@ -83,15 +87,19 @@ IF TABLE_COUNT > 0 THEN
     EXECUTE IMMEDIATE 'DROP TABLE '||V_ESQUEMA_1||'.'||V_TABLA||'';  
 END IF;
 
+
 EXECUTE IMMEDIATE '
 		CREATE TABLE '||V_ESQUEMA_1||'.'||V_TABLA||'
 		AS (
-                SELECT DISTINCT ACT.ACT_ID AS ACT_ID, ACT.ACT_NUM_ACTIVO AS ACT_NUM_ASCTIVO, SCM.DD_SCM_DESCRIPCION AS SITUACION_COMERCIAL, TCO.DD_TCO_DESCRIPCION AS DESTINO_COMERCIAL, OFR.OFR_ID, 
-                                EOF.DD_EOF_DESCRIPCION AS ESTADO_OFERTA, ECO.ECO_ID,EEC.DD_EEC_DESCRIPCION AS ESTADO_EXPEDIENTE, RES.RES_ID,ERE.DD_ERE_DESCRIPCION AS ESTADO_RESERVA
+                SELECT DISTINCT ACT.ACT_ID AS ACT_ID, ACT.ACT_NUM_ACTIVO AS ACT_NUM_ACTIVO, SCM.DD_SCM_DESCRIPCION AS SITUACION_COMERCIAL, TCO.DD_TCO_DESCRIPCION AS DESTINO_COMERCIAL, OFR.OFR_ID, 
+                                EOF.DD_EOF_DESCRIPCION AS ESTADO_OFERTA, ECO.ECO_ID,EEC.DD_EEC_DESCRIPCION AS ESTADO_EXPEDIENTE, RES.RES_ID,ERE.DD_ERE_DESCRIPCION AS ESTADO_RESERVA,
+                                ERE.DD_ERE_CODIGO, EEC.DD_EEC_CODIGO, EOF.DD_EOF_CODIGO, SCM.DD_SCM_CODIGO, TCO.DD_TCO_CODIGO
                 FROM REM01.ACT_ACTIVO                           ACT
-                JOIN REM01.ACT_OFR                              AOF
+                JOIN ACT_PAC_PERIMETRO_ACTIVO                   PAC
+                ON PAC.ACT_ID = ACT.ACT_ID
+                LEFT JOIN REM01.ACT_OFR                         AOF
                 ON AOF.ACT_ID = ACT.ACT_ID
-                JOIN REM01.OFR_OFERTAS                          OFR
+                LEFT JOIN REM01.OFR_OFERTAS                     OFR
                 ON OFR.OFR_ID = AOF.OFR_ID
                 LEFT JOIN REM01.ECO_EXPEDIENTE_COMERCIAL        ECO
                 ON ECO.OFR_ID = OFR.OFR_ID
@@ -108,9 +116,12 @@ EXECUTE IMMEDIATE '
                 LEFT JOIN REM01.DD_TCO_TIPO_COMERCIALIZACION    TCO
                 ON TCO.DD_TCO_ID = ACT.DD_TCO_ID
                 WHERE SCM.DD_SCM_CODIGO IN (''01'')
+                  AND PAC.PAC_INCLUIDO = 1
+                  AND PAC.PAC_CHECK_COMERCIALIZAR = 1
         )
 ';
-DBMS_OUTPUT.PUT_LINE('[INFO] Tabla '||V_ESQUEMA_1||'.'||V_TABLA||' creada correctamente.');  
+DBMS_OUTPUT.PUT_LINE('[INFO] Tabla '||V_ESQUEMA_1||'.'||V_TABLA||' creada correctamente. Activos no comercializables con el check comercializar activado.'); 
+
 
 SELECT COUNT(DISTINCT OWNER) INTO OWNER_COUNT FROM ALL_TABLES WHERE OWNER IN (''''||V_ESQUEMA_2||'''');
 
@@ -133,41 +144,24 @@ IF (OWNER_COUNT > 0) AND (V_ESQUEMA_4 != V_ESQUEMA_1) THEN
 	DBMS_OUTPUT.PUT_LINE('[INFO] Permisos sobre la tabla '||V_ESQUEMA_1||'.'||V_TABLA||' otorgados al esquema '||V_ESQUEMA_4||''); 
 END IF;
 
-
+COMMIT;
 
 --3º: TODOS LOS ACTIVOS DE LA TABLA AUXILIAR CUYO DESTINO COMERCIAL SEA "Alquiler" ------> "Disponible para alquiler".
 ----------------------------------------------------------------------------------------------------------------
-MERGE INTO REM01.ACT_ACTIVO T1
+EXECUTE IMMEDIATE 'MERGE INTO REM01.ACT_ACTIVO T1
 USING (
-            SELECT DISTINCT ACT.ACT_ID AS ACT_ID
-            FROM AUX_MMC_NO_COMERCIALIZABLES                AUX
-            JOIN REM01.ACT_ACTIVO                           ACT
-            ON AUX.ACT_ID = ACT.ACT_ID
-            JOIN REM01.ACT_OFR                              AOF
-            ON AOF.ACT_ID = ACT.ACT_ID
-            JOIN REM01.OFR_OFERTAS                          OFR
-            ON OFR.OFR_ID = AOF.OFR_ID
-            LEFT JOIN REM01.ECO_EXPEDIENTE_COMERCIAL        ECO
-            ON ECO.OFR_ID = OFR.OFR_ID
-            LEFT JOIN REM01.RES_RESERVAS                    RES
-            ON RES.ECO_ID = ECO.ECO_ID
-            LEFT JOIN REM01.DD_ERE_ESTADOS_RESERVA          ERE
-            ON ERE.DD_ERE_ID = RES.DD_ERE_ID
-            LEFT JOIN REM01.DD_EEC_EST_EXP_COMERCIAL        EEC
-            ON EEC.DD_EEC_ID = ECO.DD_EEC_ID
-            LEFT JOIN REM01.DD_EOF_ESTADOS_OFERTA           EOF
-            ON EOF.DD_EOF_ID = OFR.DD_EOF_ID
-            LEFT JOIN  REM01.DD_SCM_SITUACION_COMERCIAL     SCM
-            ON SCM.DD_SCM_ID = ACT.DD_SCM_ID
-            LEFT JOIN REM01.DD_TCO_TIPO_COMERCIALIZACION    TCO
-            ON TCO.DD_TCO_ID = ACT.DD_TCO_ID
-            WHERE TCO.DD_TCO_CODIGO IN ('03')
+            SELECT DISTINCT AUX.ACT_ID AS ACT_ID,
+                            AUX.DESTINO_COMERCIAL
+            FROM REM01.AUX_MMC_NO_COMERCIALIZABLES                AUX
+            JOIN REM01.ACT_ACTIVO                                 ACT
+              ON ACT.ACT_ID = AUX.ACT_ID
+            WHERE AUX.DD_TCO_CODIGO IN (''03'')
 ) T2
 ON (T1.ACT_ID = T2.ACT_ID)
 WHEN MATCHED THEN UPDATE SET
-       T1.DD_SCM_ID = (SELECT DD_SCM_ID FROM REM01.DD_SCM_SITUACION_COMERCIAL WHERE DD_SCM_CODIGO IN ('07')),
-       T1.USUARIOMODIFICAR = 'HREOS-4758',
-       T1.FECHAMODIFICAR = SYSDATE
+       T1.DD_SCM_ID = (SELECT DD_SCM_ID FROM REM01.DD_SCM_SITUACION_COMERCIAL WHERE DD_SCM_CODIGO IN (''07'')),
+       T1.USUARIOMODIFICAR = ''HREOS-4758'',
+       T1.FECHAMODIFICAR = SYSDATE'
 ;
 DBMS_OUTPUT.PUT_LINE('[INFO] Se pasan '||SQL%ROWCOUNT||' activos a Disponible para alquiler.');
 
@@ -175,153 +169,87 @@ DBMS_OUTPUT.PUT_LINE('[INFO] Se pasan '||SQL%ROWCOUNT||' activos a Disponible pa
 
 --4º: TODOS LOS ACTIVOS DE LA TABLA AUXILIAR CUYO DESTINO COMERCIAL SEA "Alquiler y venta" ------> "Disponible para venta y alquiler".
 ----------------------------------------------------------------------------------------------------------------
-MERGE INTO REM01.ACT_ACTIVO T1
+EXECUTE IMMEDIATE 'MERGE INTO REM01.ACT_ACTIVO T1
 USING (
-            SELECT DISTINCT ACT.ACT_ID AS ACT_ID
-            FROM AUX_MMC_NO_COMERCIALIZABLES                AUX
-            JOIN REM01.ACT_ACTIVO                           ACT
-            ON AUX.ACT_ID = ACT.ACT_ID
-            JOIN REM01.ACT_OFR                              AOF
-            ON AOF.ACT_ID = ACT.ACT_ID
-            JOIN REM01.OFR_OFERTAS                          OFR
-            ON OFR.OFR_ID = AOF.OFR_ID
-            LEFT JOIN REM01.ECO_EXPEDIENTE_COMERCIAL        ECO
-            ON ECO.OFR_ID = OFR.OFR_ID
-            LEFT JOIN REM01.RES_RESERVAS                    RES
-            ON RES.ECO_ID = ECO.ECO_ID
-            LEFT JOIN REM01.DD_ERE_ESTADOS_RESERVA          ERE
-            ON ERE.DD_ERE_ID = RES.DD_ERE_ID
-            LEFT JOIN REM01.DD_EEC_EST_EXP_COMERCIAL        EEC
-            ON EEC.DD_EEC_ID = ECO.DD_EEC_ID
-            LEFT JOIN REM01.DD_EOF_ESTADOS_OFERTA           EOF
-            ON EOF.DD_EOF_ID = OFR.DD_EOF_ID
-            LEFT JOIN  REM01.DD_SCM_SITUACION_COMERCIAL     SCM
-            ON SCM.DD_SCM_ID = ACT.DD_SCM_ID
-            LEFT JOIN REM01.DD_TCO_TIPO_COMERCIALIZACION    TCO
-            ON TCO.DD_TCO_ID = ACT.DD_TCO_ID
-            WHERE TCO.DD_TCO_CODIGO IN ('02')
+            SELECT DISTINCT AUX.ACT_ID AS ACT_ID,
+                            AUX.DESTINO_COMERCIAL
+            FROM REM01.AUX_MMC_NO_COMERCIALIZABLES                AUX
+            JOIN REM01.ACT_ACTIVO                                 ACT
+              ON ACT.ACT_ID = AUX.ACT_ID
+            WHERE AUX.DD_TCO_CODIGO IN (''02'')
 ) T2
 ON (T1.ACT_ID = T2.ACT_ID)
 WHEN MATCHED THEN UPDATE SET
-       T1.DD_SCM_ID = (SELECT DD_SCM_ID FROM REM01.DD_SCM_SITUACION_COMERCIAL WHERE DD_SCM_CODIGO IN ('08')),
-       T1.USUARIOMODIFICAR = 'HREOS-4758',
-       T1.FECHAMODIFICAR = SYSDATE
+       T1.DD_SCM_ID = (SELECT DD_SCM_ID FROM REM01.DD_SCM_SITUACION_COMERCIAL WHERE DD_SCM_CODIGO IN (''08'')),
+       T1.USUARIOMODIFICAR = ''HREOS-4758'',
+       T1.FECHAMODIFICAR = SYSDATE'
 ;
 DBMS_OUTPUT.PUT_LINE('[INFO] Se pasan '||SQL%ROWCOUNT||' activos a Disponible para venta y alquiler.');
 
 
 --5º: TODOS LOS ACTIVOS DE LA TABLA AUXILIAR CUYO DESTINO COMERCIAL SEA "Venta" ------> "Disponible para la venta".
 ----------------------------------------------------------------------------------------------------------------
-MERGE INTO REM01.ACT_ACTIVO T1
+EXECUTE IMMEDIATE 'MERGE INTO REM01.ACT_ACTIVO T1
 USING (
-            SELECT DISTINCT ACT.ACT_ID AS ACT_ID
-            FROM AUX_MMC_NO_COMERCIALIZABLES                AUX
-            JOIN REM01.ACT_ACTIVO                           ACT
-            ON AUX.ACT_ID = ACT.ACT_ID
-            JOIN REM01.ACT_OFR                              AOF
-            ON AOF.ACT_ID = ACT.ACT_ID
-            JOIN REM01.OFR_OFERTAS                          OFR
-            ON OFR.OFR_ID = AOF.OFR_ID
-            LEFT JOIN REM01.ECO_EXPEDIENTE_COMERCIAL        ECO
-            ON ECO.OFR_ID = OFR.OFR_ID
-            LEFT JOIN REM01.RES_RESERVAS                    RES
-            ON RES.ECO_ID = ECO.ECO_ID
-            LEFT JOIN REM01.DD_ERE_ESTADOS_RESERVA          ERE
-            ON ERE.DD_ERE_ID = RES.DD_ERE_ID
-            LEFT JOIN REM01.DD_EEC_EST_EXP_COMERCIAL        EEC
-            ON EEC.DD_EEC_ID = ECO.DD_EEC_ID
-            LEFT JOIN REM01.DD_EOF_ESTADOS_OFERTA           EOF
-            ON EOF.DD_EOF_ID = OFR.DD_EOF_ID
-            LEFT JOIN  REM01.DD_SCM_SITUACION_COMERCIAL     SCM
-            ON SCM.DD_SCM_ID = ACT.DD_SCM_ID
-            LEFT JOIN REM01.DD_TCO_TIPO_COMERCIALIZACION    TCO
-            ON TCO.DD_TCO_ID = ACT.DD_TCO_ID
-            WHERE TCO.DD_TCO_CODIGO IN ('01')
+            SELECT DISTINCT AUX.ACT_ID AS ACT_ID,
+                            AUX.DESTINO_COMERCIAL
+            FROM REM01.AUX_MMC_NO_COMERCIALIZABLES                AUX
+            JOIN REM01.ACT_ACTIVO                                 ACT
+              ON ACT.ACT_ID = AUX.ACT_ID
+            WHERE AUX.DD_TCO_CODIGO IN (''01'')
 ) T2
 ON (T1.ACT_ID = T2.ACT_ID)
 WHEN MATCHED THEN UPDATE SET
-       T1.DD_SCM_ID = (SELECT DD_SCM_ID FROM REM01.DD_SCM_SITUACION_COMERCIAL WHERE DD_SCM_CODIGO IN ('02')),
-       T1.USUARIOMODIFICAR = 'HREOS-4758',
-       T1.FECHAMODIFICAR = SYSDATE
+       T1.DD_SCM_ID = (SELECT DD_SCM_ID FROM REM01.DD_SCM_SITUACION_COMERCIAL WHERE DD_SCM_CODIGO IN (''02'')),
+       T1.USUARIOMODIFICAR = ''HREOS-4758'',
+       T1.FECHAMODIFICAR = SYSDATE'
 ;
 DBMS_OUTPUT.PUT_LINE('[INFO] Se pasan '||SQL%ROWCOUNT||' activos a Disponible para la venta.');
 
 
 --6º: TODOS LOS ACTIVOS DE LA TABLA AUXILIAR CON OFERTA TRAMITADA ------> "Disponible para la venta con oferta".
 ----------------------------------------------------------------------------------------------------------------
-MERGE INTO REM01.ACT_ACTIVO T1
+EXECUTE IMMEDIATE 'MERGE INTO REM01.ACT_ACTIVO T1
 USING (
-            SELECT DISTINCT  ACT.ACT_ID AS ACT_ID
-            FROM AUX_MMC_NO_COMERCIALIZABLES                AUX
-            JOIN REM01.ACT_ACTIVO                           ACT
-            ON AUX.ACT_ID = ACT.ACT_ID
-            JOIN REM01.ACT_OFR                              AOF
-            ON AOF.ACT_ID = ACT.ACT_ID
-            JOIN REM01.OFR_OFERTAS                          OFR
-            ON OFR.OFR_ID = AOF.OFR_ID
-            LEFT JOIN REM01.ECO_EXPEDIENTE_COMERCIAL        ECO
-            ON ECO.OFR_ID = OFR.OFR_ID
-            LEFT JOIN REM01.RES_RESERVAS                    RES
-            ON RES.ECO_ID = ECO.ECO_ID
-            LEFT JOIN REM01.DD_ERE_ESTADOS_RESERVA          ERE
-            ON ERE.DD_ERE_ID = RES.DD_ERE_ID
-            LEFT JOIN REM01.DD_EEC_EST_EXP_COMERCIAL        EEC
-            ON EEC.DD_EEC_ID = ECO.DD_EEC_ID
-            LEFT JOIN REM01.DD_EOF_ESTADOS_OFERTA           EOF
-            ON EOF.DD_EOF_ID = OFR.DD_EOF_ID
-            LEFT JOIN  REM01.DD_SCM_SITUACION_COMERCIAL     SCM
-            ON SCM.DD_SCM_ID = ACT.DD_SCM_ID
-            LEFT JOIN REM01.DD_TCO_TIPO_COMERCIALIZACION    TCO
-            ON TCO.DD_TCO_ID = ACT.DD_TCO_ID
-            WHERE EOF.DD_EOF_CODIGO IN ('01')
+            SELECT DISTINCT AUX.ACT_ID AS ACT_ID
+            FROM REM01.AUX_MMC_NO_COMERCIALIZABLES                AUX
+            JOIN REM01.ACT_ACTIVO                                 ACT
+              ON ACT.ACT_ID = AUX.ACT_ID
+            WHERE AUX.DD_TCO_CODIGO NOT IN (''03'')
+              AND AUX.DD_EOF_CODIGO IN (''01'')
 ) T2
 ON (T1.ACT_ID = T2.ACT_ID)
 WHEN MATCHED THEN UPDATE SET
-       T1.DD_SCM_ID = (SELECT DD_SCM_ID FROM REM01.DD_SCM_SITUACION_COMERCIAL WHERE DD_SCM_CODIGO IN ('03')),
-       T1.USUARIOMODIFICAR = 'HREOS-4758',
-       T1.FECHAMODIFICAR = SYSDATE
+       T1.DD_SCM_ID = (SELECT DD_SCM_ID FROM REM01.DD_SCM_SITUACION_COMERCIAL WHERE DD_SCM_CODIGO IN (''03'')),
+       T1.USUARIOMODIFICAR = ''HREOS-4758'',
+       T1.FECHAMODIFICAR = SYSDATE'
 ;
 DBMS_OUTPUT.PUT_LINE('[INFO] Se pasan '||SQL%ROWCOUNT||' activos a Disponible para la venta con oferta.');
 
 
 --7º: TODOS LOS ACTIVOS DE LA TABLA AUXILIAR CON RESERVA FIRMADA ------> "Disponible para la venta con reserva".
 ----------------------------------------------------------------------------------------------------------------
-MERGE INTO REM01.ACT_ACTIVO T1
+EXECUTE IMMEDIATE 'MERGE INTO REM01.ACT_ACTIVO T1
 USING (
-            SELECT DISTINCT  ACT.ACT_ID AS ACT_ID
-            FROM AUX_MMC_NO_COMERCIALIZABLES                AUX
-            JOIN REM01.ACT_ACTIVO                           ACT
-            ON AUX.ACT_ID = ACT.ACT_ID
-            JOIN REM01.ACT_OFR                              AOF
-            ON AOF.ACT_ID = ACT.ACT_ID
-            JOIN REM01.OFR_OFERTAS                          OFR
-            ON OFR.OFR_ID = AOF.OFR_ID
-            LEFT JOIN REM01.ECO_EXPEDIENTE_COMERCIAL        ECO
-            ON ECO.OFR_ID = OFR.OFR_ID
-            LEFT JOIN REM01.RES_RESERVAS                    RES
-            ON RES.ECO_ID = ECO.ECO_ID
-            LEFT JOIN REM01.DD_ERE_ESTADOS_RESERVA          ERE
-            ON ERE.DD_ERE_ID = RES.DD_ERE_ID
-            LEFT JOIN REM01.DD_EEC_EST_EXP_COMERCIAL        EEC
-            ON EEC.DD_EEC_ID = ECO.DD_EEC_ID
-            LEFT JOIN REM01.DD_EOF_ESTADOS_OFERTA           EOF
-            ON EOF.DD_EOF_ID = OFR.DD_EOF_ID
-            LEFT JOIN  REM01.DD_SCM_SITUACION_COMERCIAL     SCM
-            ON SCM.DD_SCM_ID = ACT.DD_SCM_ID
-            LEFT JOIN REM01.DD_TCO_TIPO_COMERCIALIZACION    TCO
-            ON TCO.DD_TCO_ID = ACT.DD_TCO_ID
-            WHERE ERE.DD_ERE_CODIGO IN ('02')
+            SELECT DISTINCT AUX.ACT_ID AS ACT_ID
+            FROM REM01.AUX_MMC_NO_COMERCIALIZABLES                AUX
+            JOIN REM01.ACT_ACTIVO                                 ACT
+              ON ACT.ACT_ID = AUX.ACT_ID
+            WHERE AUX.DD_TCO_CODIGO NOT IN (''03'')
+              AND AUX.DD_ERE_CODIGO IN (''02'')
 ) T2
 ON (T1.ACT_ID = T2.ACT_ID)
 WHEN MATCHED THEN UPDATE SET
-       T1.DD_SCM_ID = (SELECT DD_SCM_ID FROM REM01.DD_SCM_SITUACION_COMERCIAL WHERE DD_SCM_CODIGO IN ('04')),
-       T1.USUARIOMODIFICAR = 'HREOS-4758',
-       T1.FECHAMODIFICAR = SYSDATE
+       T1.DD_SCM_ID = (SELECT DD_SCM_ID FROM REM01.DD_SCM_SITUACION_COMERCIAL WHERE DD_SCM_CODIGO IN (''04'')),
+       T1.USUARIOMODIFICAR = ''HREOS-4758'',
+       T1.FECHAMODIFICAR = SYSDATE'
 ;
 DBMS_OUTPUT.PUT_LINE('[INFO] Se pasan '||SQL%ROWCOUNT||' activos a Disponible para la venta con reserva.');
 
 
 DBMS_OUTPUT.PUT_LINE('[FIN] Fin del proceso.');
+
+COMMIT;
 
 EXCEPTION
 

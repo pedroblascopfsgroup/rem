5468a5f6a16dc7d9d358a2ba43e720a1e87911ae
hector gomez <hector.gomez@pfsgroup.es>
2018-11-19 10:26:07 +0100

    HREOS-4706 Campos Propagables

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/activo/ActivoPropagacionFieldTabMap.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/activo/ActivoPropagacionFieldTabMap.java
@@ -513,7 +513,7 @@ public class ActivoPropagacionFieldTabMap {
     				"contactoTel",
     				"visita",
     				"burofax",
-    				"situacion",
+    				"situacionCodigo",
     				"situacionId",
     				"fechaEnvioCarta",
     				"situacionDescripcion"

810d3599ef7eb3bd4bfad07bdb23f7cbbd6761c9
Pier <pier.gotta@pfsgroup.es>
2018-11-22 18:07:08 +0100

    REMVIP-2623 Modificacion ACT_APU_ACTIVO_PUBLICACION

---
+++ sql/9.2/rem/solo_produccion/DML_999900418_REM01_REMVIP_2557_UPDATE_ACT_APU_ACTIVO_PUBLICACION.sql
@@ -0,0 +1,106 @@
+--/*
+--##########################################
+--## AUTOR=PIER GOTTA
+--## FECHA_CREACION=20181122
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.2
+--## INCIDENCIA_LINK=REMVIP-2623
+--## PRODUCTO=NO
+--##
+--## Finalidad: Update de APU
+--## INSTRUCCIONES:
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF; 
+DECLARE
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar    
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquemas
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.
+    V_NUM_FILAS NUMBER(16); -- Vble. para validar la existencia de un registro.
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+    V_USR VARCHAR2(30 CHAR) := 'REMVIP-2623'; -- USUARIOCREAR/USUARIOMODIFICAR.
+    V_ECO_ID NUMBER(16); 
+    
+    ACT_NUM_ACTIVO NUMBER(16);
+    V_COUNT_UPDATE NUMBER(16):= 0; -- Vble. para contar updates
+    
+    TYPE T_JBV IS TABLE OF VARCHAR2(32000);
+    TYPE T_ARRAY_JBV IS TABLE OF T_JBV; 
+	
+	V_JBV T_ARRAY_JBV := T_ARRAY_JBV(
+		T_JBV(6079973)
+		,T_JBV(6040568)
+		,T_JBV(6038991)
+		,T_JBV(6032091)
+		,T_JBV(6031888)
+		
+	); 
+	V_TMP_JBV T_JBV;
+    
+BEGIN	
+
+	FOR I IN V_JBV.FIRST .. V_JBV.LAST
+	
+	LOOP
+ 
+	V_TMP_JBV := V_JBV(I);
+	
+  	ACT_NUM_ACTIVO := TRIM(V_TMP_JBV(1));
+	
+	DBMS_OUTPUT.PUT_LINE('[INICIO] ACTUALIZACION COLUMNA ACT_NUM_ACTIVO');
+	
+	V_SQL := 'SELECT COUNT(*) FROM '||V_ESQUEMA||'.ACT_ACTIVO WHERE ACT_NUM_ACTIVO = '||ACT_NUM_ACTIVO;
+	
+	EXECUTE IMMEDIATE V_SQL INTO V_NUM_FILAS;
+	
+	IF V_NUM_FILAS = 1 THEN
+	
+		V_MSQL := 'UPDATE '||V_ESQUEMA||'.ACT_APU_ACTIVO_PUBLICACION SET
+				   DD_EPV_ID = 3, 
+				   DD_EPA_ID = 3,
+				   APU_CHECK_OCULTAR_V = 0,
+				   APU_CHECK_OCULTAR_A = 0,
+				   DD_MTO_V_ID = null,
+				   DD_MTO_A_ID = null,
+				   USUARIOMODIFICAR = '''||V_USR||''',
+				   FECHAMODIFICAR = SYSDATE
+				   WHERE ACT_ID = (SELECT ACT_ID FROM '||V_ESQUEMA||'.ACT_ACTIVO WHERE ACT_NUM_ACTIVO = '||ACT_NUM_ACTIVO||')';
+	
+		EXECUTE IMMEDIATE V_MSQL;
+    
+		DBMS_OUTPUT.PUT_LINE('[FIN] REGISTRO CON ACT_NUM_ACTIVO: '||ACT_NUM_ACTIVO||' ACTUALIZADO');
+		
+		V_COUNT_UPDATE := V_COUNT_UPDATE + 1;
+		
+	ELSE
+		
+		DBMS_OUTPUT.PUT_LINE('[FIN] REGISTRO NO EXISTE');
+		
+	END IF;
+	
+	END LOOP;
+		
+	COMMIT;
+	
+	DBMS_OUTPUT.PUT_LINE('[INFO] Se han borrado en total '||V_COUNT_UPDATE||' registros');
+ 
+EXCEPTION
+     WHEN OTHERS THEN
+          ERR_NUM := SQLCODE;
+          ERR_MSG := SQLERRM;
+          DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(ERR_NUM));
+          DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+          DBMS_OUTPUT.put_line(ERR_MSG);
+          ROLLBACK;
+          RAISE;   
+END;
+/
+EXIT;

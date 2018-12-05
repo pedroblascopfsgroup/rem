3aa70d3928478662189f1dfdb316cf95c302489c
Carlos Molins <carlos.molins@pfsgroup.es>
2018-12-05 17:12:39 +0100

    REMVIP-2730 Cálculo en comités Liberbank

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/oferta/OfertaManager.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/oferta/OfertaManager.java
@@ -2778,6 +2778,7 @@ public class OfertaManager extends BusinessOperationOverrider<OfertaApi> impleme
 			List<ActivoAgrupacionActivo> activos = agrupacion.getActivos();
 			Double sumaTasaciones = 0.0;
 			Double sumaPreciosMinimosAutorizados = 0.0;
+			Double sumaPreciosDescuentoPublicado = 0.0;
 			
 			List<VTasacionCalculoLBK> vista = activoApi.getVistaTasacion(agrupacion.getId());
 
@@ -2785,20 +2786,23 @@ public class OfertaManager extends BusinessOperationOverrider<OfertaApi> impleme
 				Double importeTasacion = reg.getImporteTasacion();
 				Double precioAprobadoVenta = 0.0;	
 				Double precioMinimoAutorizado = 0.0;
+				Double precioDescuentoPublicado = 0.0;
 				Boolean esPrecioAprobadoVenta = false;
 
 				if(DDTipoPrecio.CODIGO_TPC_APROBADO_VENTA.equals(reg.getCodigoTipoPrecio())) {
 					precioAprobadoVenta = (!Checks.esNulo(reg.getImporteTipoPrecio())) ? reg.getImporteTipoPrecio() : 0.0;
 					esPrecioAprobadoVenta = true;
-				}else {
+				}else if(DDTipoPrecio.CODIGO_TPC_MIN_AUTORIZADO.equals(reg.getCodigoTipoPrecio())){
 					precioMinimoAutorizado = (!Checks.esNulo(reg.getImporteTipoPrecio())) ? reg.getImporteTipoPrecio() : 0.0;
+				} else if(DDTipoPrecio.CODIGO_TPC_DESC_APROBADO.equals(reg.getCodigoTipoPrecio())) {
+					precioDescuentoPublicado = (!Checks.esNulo(reg.getImporteTipoPrecio())) ? reg.getImporteTipoPrecio() : 0.0;
 				}
 
 				if(esPrecioAprobadoVenta) {
 					sumaTasaciones += (!Checks.esNulo(importeTasacion)) ? importeTasacion : precioAprobadoVenta;
 				}
 				sumaPreciosMinimosAutorizados += precioMinimoAutorizado;
-
+				sumaPreciosDescuentoPublicado += precioDescuentoPublicado;
 			}
 			
 			Integer tipoResidencial = 0;
@@ -2830,8 +2834,10 @@ public class OfertaManager extends BusinessOperationOverrider<OfertaApi> impleme
 				}
 			}
 			
-			if((!Checks.esNulo(sumaTasaciones) && sumaTasaciones < importeUmbral) 
-					&& (!Checks.esNulo(importeOferta) && !Checks.esNulo(sumaPreciosMinimosAutorizados) && importeOferta >= sumaPreciosMinimosAutorizados)) {
+			if(((!Checks.esNulo(sumaTasaciones) && sumaTasaciones < importeUmbral) 
+					&& (!Checks.esNulo(importeOferta) && !Checks.esNulo(sumaPreciosMinimosAutorizados) && importeOferta >= sumaPreciosMinimosAutorizados))
+			|| ((!Checks.esNulo(sumaTasaciones) && sumaTasaciones < importeUmbral) 
+					&& (!Checks.esNulo(importeOferta) && !Checks.esNulo(sumaPreciosDescuentoPublicado) && importeOferta >= sumaPreciosDescuentoPublicado))) {
 				Filter filterComite = genericDao.createFilter(FilterType.EQUALS, "codigo", DDComiteSancion.CODIGO_HAYA_LIBERBANK);
 				DDComiteSancion comiteSancion = genericDao.get(DDComiteSancion.class, filterComite);
 				

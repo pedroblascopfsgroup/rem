52798c48c7819ade76dff6f3964abe61da7b34ba
Carlos Ruiz <carlos.ruiz@pfsgroup.es>
2018-12-13 16:59:41 +0100

    HREOS-4937 - Correccion errores del previo commit

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/model/ClienteCompradorGDPR.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/model/ClienteCompradorGDPR.java
@@ -4,15 +4,23 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Embedded;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import javax.persistence.Version;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.Where;
 
 import es.capgemini.pfs.auditoria.Auditable;
 import es.capgemini.pfs.auditoria.model.Auditoria;
@@ -28,6 +36,11 @@ import es.capgemini.pfs.persona.model.DDTipoDocumento;
  * @author Carlos Ruiz Beltrán
  *
  */
+@Entity
+@Table(name = "CCG_CLIENTE_COM_GDPR", schema = "${entity.schema}")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Where(clause = Auditoria.UNDELETED_RESTICTION)
+@Inheritance(strategy=InheritanceType.JOINED)
 public class ClienteCompradorGDPR implements Serializable, Auditable{
 
 	private static final long serialVersionUID = 1L;

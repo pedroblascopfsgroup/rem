1350535787936e0eed505399fbcf82d6779e5184
Adrían Daniel Casiean <adrian.casiean@pfsgroup.es>
2018-12-18 18:01:45 +0100

    HREOS-5090 Punto 93

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/adapter/AgrupacionAdapter.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/adapter/AgrupacionAdapter.java
@@ -1,7 +1,6 @@
 package es.pfsgroup.plugin.rem.adapter;
 
 import java.lang.reflect.InvocationTargetException;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -13,8 +12,6 @@ import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.lang.BooleanUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.hibernate.annotations.Check;
-import org.apache.log4j.lf5.viewer.LogTableColumnFormatException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -63,6 +60,7 @@ import es.pfsgroup.plugin.rem.api.ExpedienteComercialApi;
 import es.pfsgroup.plugin.rem.api.OfertaApi;
 import es.pfsgroup.plugin.rem.api.ProveedoresApi;
 import es.pfsgroup.plugin.rem.api.TrabajoApi;
+import es.pfsgroup.plugin.rem.gestor.dao.GestorActivoDao;
 import es.pfsgroup.plugin.rem.jbpm.handler.notificator.impl.NotificatorServiceSancionOfertaAceptacionYRechazo;
 import es.pfsgroup.plugin.rem.model.Activo;
 import es.pfsgroup.plugin.rem.model.ActivoAgrupacion;
@@ -78,8 +76,8 @@ import es.pfsgroup.plugin.rem.model.ActivoOferta;
 import es.pfsgroup.plugin.rem.model.ActivoPatrimonio;
 import es.pfsgroup.plugin.rem.model.ActivoPropietario;
 import es.pfsgroup.plugin.rem.model.ActivoProveedor;
-import es.pfsgroup.plugin.rem.model.ActivoPublicacion;
 import es.pfsgroup.plugin.rem.model.ActivoProyecto;
+import es.pfsgroup.plugin.rem.model.ActivoPublicacion;
 import es.pfsgroup.plugin.rem.model.ActivoRestringida;
 import es.pfsgroup.plugin.rem.model.ActivoTrabajo;
 import es.pfsgroup.plugin.rem.model.AgrupacionesVigencias;
@@ -96,6 +94,8 @@ import es.pfsgroup.plugin.rem.model.DtoOfertaActivo;
 import es.pfsgroup.plugin.rem.model.DtoOfertasFilter;
 import es.pfsgroup.plugin.rem.model.DtoUsuario;
 import es.pfsgroup.plugin.rem.model.ExpedienteComercial;
+import es.pfsgroup.plugin.rem.model.GestorActivo;
+import es.pfsgroup.plugin.rem.model.GestorActivoHistorico;
 import es.pfsgroup.plugin.rem.model.Oferta;
 import es.pfsgroup.plugin.rem.model.PerimetroActivo;
 import es.pfsgroup.plugin.rem.model.Trabajo;
@@ -240,6 +240,9 @@ public class AgrupacionAdapter {
 
 	@Autowired
 	private ActivoPublicacionDao activoPublicacionDao;
+	
+	@Autowired
+	private GestorActivoDao gestorActivoDao;
 
 	@Autowired
 	private ActivoHistoricoPatrimonioDao activoHistoricoPatrimonioDao;
@@ -263,6 +266,7 @@ public class AgrupacionAdapter {
 	private static final String TIPO_COMERCIAL_ALQUILER = "Alquiler";
 	private static final String TIPO_GESTOR_COMERCIAL_VENTA = "GCOM";
 	private static final String TIPO_GESTOR_COMERCIAL_ALQUILER = "GESTCOMALQ";
+	private static final String TIPO_SUPERVISOR_COMERCIAL_VENTA = "SCOM";
 
 	public static final String SPLIT_VALUE = ";s;";
 
@@ -2356,7 +2360,7 @@ public class AgrupacionAdapter {
 									activoPublicacion = new ActivoPublicacion();
 									activoPublicacion.setActivo(activoAgrupacionActivo.getActivo());
 								}
-
+								String destino_comercial = activoPublicacion.getTipoComercializacion().getCodigo();
 								activoPublicacion.setTipoComercializacion(tipoComercializacion);
 
 
@@ -2399,7 +2403,47 @@ public class AgrupacionAdapter {
 								}
 								activoPatrimonioDao.saveOrUpdate(activoPatrimonio);
 								activoPublicacionDao.saveOrUpdate(activoPublicacion);
-
+								
+								//HREOS-5090 Punto 93
+								//Asignar Gestores al cambiar el destino comercial de la agrupación si pasamos de 'alquiler y venta' a 'alquiler'
+								if(DDTipoComercializacion.CODIGO_ALQUILER_VENTA.equals(destino_comercial) && DDTipoComercializacion.CODIGO_SOLO_ALQUILER.equals(tipoComercializacion.getCodigo())) {
+									
+									Filter filtro2 = genericDao.createFilter(FilterType.EQUALS, "activo", activoAgrupacionActivo.getActivo());
+									List<GestorActivo> gestores_activo  = (List<GestorActivo>) genericDao.getList(GestorActivo.class, filtro2);
+									for(GestorActivo gest : gestores_activo) {
+										if(TIPO_GESTOR_COMERCIAL_VENTA.equals(gest.getTipoGestor().getCodigo())) {
+											//borramos el gestor del activo
+											gestorActivoDao.delete(gest);
+											//y agregamos el gestor al historico del activo
+											Filter filt= genericDao.createFilter(FilterType.EQUALS, "activo", activoAgrupacionActivo.getActivo());
+											Filter filt1 = genericDao.createFilter(FilterType.EQUALS, "tipoGestor.codigo", TIPO_GESTOR_COMERCIAL_VENTA);
+											Filter filt3 = genericDao.createFilter(FilterType.NULL, "fechaHasta");
+											GestorActivoHistorico gah_g=genericDao.get(GestorActivoHistorico.class,filt,filt1,filt3);
+											if(!Checks.esNulo(gah_g)) {
+													gah_g.setFechaHasta(new Date());
+													genericDao.update(GestorActivoHistorico.class, gah_g); 
+											}
+											
+										}else if(TIPO_SUPERVISOR_COMERCIAL_VENTA.equals(gest.getTipoGestor().getCodigo())) {
+											//borramos el gestor del activo
+											gestorActivoDao.delete(gest);
+											//y agregamos el gestor al historico del activo
+											Filter filt= genericDao.createFilter(FilterType.EQUALS, "activo", activoAgrupacionActivo.getActivo());
+											Filter filt2 = genericDao.createFilter(FilterType.EQUALS, "tipoGestor.codigo", TIPO_SUPERVISOR_COMERCIAL_VENTA);
+											Filter filt3 = genericDao.createFilter(FilterType.NULL, "fechaHasta");
+											
+											GestorActivoHistorico gah_s=genericDao.get(GestorActivoHistorico.class,filt,filt2,filt3);
+											if(!Checks.esNulo(gah_s)) {
+													gah_s.setFechaHasta(new Date());
+													genericDao.update(GestorActivoHistorico.class, gah_s); 
+											}
+										}
+											
+										
+									}
+									
+								}
+								
 							}
 						}
 					}

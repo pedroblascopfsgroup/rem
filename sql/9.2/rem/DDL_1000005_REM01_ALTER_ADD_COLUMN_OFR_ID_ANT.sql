e42966ee8c2f2119edb2d4c8e0138a8c7645c353
Elisa <elisa.occhipinti@pfsgroup.es>
2019-01-03 09:13:21 +0100

    HREOS-5053 - Corrección error DDL_1000005_REM01_ALTER_ADD_COLUMN_OFR_ID_ANT

--- sql/9.2/rem/DDL_1000005_REM01_ALTER_ADD_COLUMN_OFR_ID_ANT.sql
+++ sql/9.2/rem/DDL_1000005_REM01_ALTER_ADD_COLUMN_OFR_ID_ANT.sql
@@ -3,7 +3,7 @@
 --## AUTOR=ELISA OCCHIPINTI
 --## FECHA_CREACION=20181226
 --## ARTEFACTO=web
---## VERSION_ARTEFACTO=web ???
+--## VERSION_ARTEFACTO=web 
 --## INCIDENCIA_LINK=HREOS-5053
 --## PRODUCTO=NO
 --## Finalidad: Añadir columna OFR_ID_ANT en la tabla ACT_OFG_OFERTA_GENCAT
@@ -13,6 +13,7 @@
 --##        0.1 Versión inicial
 --##########################################
 --*/
+
 WHENEVER SQLERROR EXIT SQL.SQLCODE;
 SET SERVEROUTPUT ON;
 SET DEFINE OFF;
@@ -22,23 +23,23 @@ DECLARE
     V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar    
     V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
     V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
-    V_TS_INDEX VARCHAR2(25 CHAR):= '#TABLESPACE_INDEX#'; -- Configuracion Indice
     V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
     V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.  
     ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
     ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
     
-    V_COLUMNA VARCHAR2(50 CHAR):= 'OFR_ID_ANT'; -- Vble. para almacenar el nombre de la columna.
+    V_COLUMNA VARCHAR2(30 CHAR):= 'OFR_ID_ANT'; -- Vble. para almacenar el nombre de la columna.
     V_TABLA VARCHAR2(50 CHAR):= 'ACT_OFG_OFERTA_GENCAT'; -- Vble. para almacenar el nombre de la tabla.
-    V_NUM_COLS NUMBER(16); /* Vble. para validar la existencia de una columna  */
+    V_NUM_COLS NUMBER(16); --Vble. para validar la existencia de una columna  
+   
     BEGIN
-     
-    V_SQL := 'SELECT COUNT(1) FROM ALL_TAB_COLUMNS WHERE COLUMN_NAME='|| V_COLUMNA ||' AND TABLE_NAME=''' || V_TABLA || ''' AND OWNER='''|| V_ESQUEMA ||'''';   
+    
+    V_SQL := 'SELECT COUNT(1) FROM ALL_TAB_COLUMNS WHERE COLUMN_NAME= '''|| V_COLUMNA ||''' AND TABLE_NAME= '''|| V_TABLA ||''' AND OWNER='''|| V_ESQUEMA ||''''; 
     EXECUTE IMMEDIATE V_SQL INTO V_NUM_COLS;
      
     -- Si existe la columna no hacemos nada
     IF V_NUM_COLS = 0 THEN 
-        EXECUTE IMMEDIATE 'ALTER TABLE '||V_ESQUEMA ||'.'|| V_TABLA ||' ADD '|| V_COLUMNA ||' NUMBER(16)';
+        EXECUTE IMMEDIATE 'ALTER TABLE '||V_ESQUEMA ||'.'|| V_TABLA ||' ADD ('|| V_COLUMNA ||' NUMBER(16))';
     ELSE
        DBMS_OUTPUT.PUT_LINE('YA EXISTE EL COLUMNA');
     END IF;   
@@ -51,7 +52,7 @@ EXCEPTION
     DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
     DBMS_OUTPUT.put_line(ERR_MSG);
     ROLLBACK;
-    RAISE;   
+    RAISE;  
 END;
 /
 EXIT;

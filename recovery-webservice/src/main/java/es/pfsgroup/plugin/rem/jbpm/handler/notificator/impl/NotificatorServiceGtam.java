a0269faecf69a5759a0dee8d864cfd3685a03671
Ramon Llinares <ramon.llinares@pfsgroup.es>
2019-01-07 17:57:59 +0100

    gtam

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/jbpm/handler/notificator/impl/NotificatorServiceGtam.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/jbpm/handler/notificator/impl/NotificatorServiceGtam.java
@@ -6,25 +6,32 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import es.capgemini.devon.dto.WebDto;
+import es.capgemini.devon.pagination.Page;
 import es.capgemini.pfs.procesosJudiciales.model.TareaExternaValor;
 import es.capgemini.pfs.users.domain.Usuario;
 import es.pfsgroup.commons.utils.Checks;
 import es.pfsgroup.commons.utils.dao.abm.GenericABMDao;
 import es.pfsgroup.commons.utils.dao.abm.GenericABMDao.FilterType;
+import es.pfsgroup.framework.paradise.utils.DtoPage;
 import es.pfsgroup.plugin.rem.adapter.GenericAdapter;
 import es.pfsgroup.plugin.rem.api.ActivoTramiteApi;
+import es.pfsgroup.plugin.rem.api.ExpedienteComercialApi;
 import es.pfsgroup.plugin.rem.expedienteComercial.dao.ExpedienteComercialDao;
 import es.pfsgroup.plugin.rem.jbpm.handler.notificator.AbstractNotificatorService;
 import es.pfsgroup.plugin.rem.jbpm.handler.notificator.NotificatorService;
 import es.pfsgroup.plugin.rem.model.ActivoTramite;
+import es.pfsgroup.plugin.rem.model.CondicionanteExpediente;
 import es.pfsgroup.plugin.rem.model.DtoSendNotificator;
 import es.pfsgroup.plugin.rem.model.ExpedienteComercial;
 import es.pfsgroup.plugin.rem.model.Oferta;
 import es.pfsgroup.plugin.rem.model.Trabajo;
+import es.pfsgroup.plugin.rem.model.VBusquedaCompradoresExpediente;
+import es.pfsgroup.plugin.rem.model.VListadoActivosExpediente;
 import es.pfsgroup.plugin.rem.model.dd.DDCartera;
 
 @Component
-public class NotificatorServiceGtam extends AbstractNotificatorService implements NotificatorService{
+public class NotificatorServiceGtam extends AbstractNotificatorService implements NotificatorService {
 
 	private static final String CODIGO_T013_DEFINICION_OFERTA = "T013_DefinicionOferta";
 	private static final String NPLREO_SUPPORT = "NPLREOSupport";
@@ -41,7 +48,10 @@ public class NotificatorServiceGtam extends AbstractNotificatorService implement
 
 	@Autowired
 	private ExpedienteComercialDao expedienteComercialDao;
-	
+
+	@Autowired
+	private ExpedienteComercialApi expedienteComercialApi;
+
 	List<TareaExternaValor> valores = null;
 
 	@Override
@@ -51,10 +61,10 @@ public class NotificatorServiceGtam extends AbstractNotificatorService implement
 
 	@Override
 	public String[] getCodigoTarea() {
-		return new String[] { CODIGO_T013_DEFINICION_OFERTA};
+		return new String[] { CODIGO_T013_DEFINICION_OFERTA };
 	}
-	
-	
+
+	@SuppressWarnings("unchecked")
 	@Override
 	public void notificator(ActivoTramite tramite) {
 		ExpedienteComercial expediente = getExpedienteComercial(tramite);
@@ -66,9 +76,54 @@ public class NotificatorServiceGtam extends AbstractNotificatorService implement
 					genericDao.createFilter(FilterType.EQUALS, "username", NPLREO_SUPPORT));
 			Usuario mailTracker = genericDao.get(Usuario.class,
 					genericDao.createFilter(FilterType.EQUALS, "username", MAILTRACKER_SUPPORT));
-			String titulo = "La Oferta #numoferta ha sido aceptada";
-			String contenido = "<p> Le informamos que la citada propuesta ha sido CONTRAOFERTADA por un importe de #importeContraoferta.</p>"
-					+ "<p> Quedamos a su disposición para cualquier consulta o aclaración.</p>"
+			String titulo = "La Oferta #numoferta ha sido TRAMITADA";
+			String contenido = "<p> Le informamos que la citada propuesta ha sido TRAMITADA.</p>";
+			
+			
+			DtoPage dto = expedienteComercialApi.getActivosExpedienteVista(expediente.getId());
+			
+			if(dto != null && dto.results.size() > 0){
+				contenido += "<p><b>Activos de la oferta</b></p>";
+				List<VListadoActivosExpediente> activos = (List<VListadoActivosExpediente>)dto.results;
+				for (VListadoActivosExpediente activoOferta : activos) {
+					contenido += "<p>Activo: " + activoOferta.getNumActivo()
+							+ ", Tipo : " + activoOferta.getTipoActivo() 
+							+ ", Municipio : " + activoOferta.getMunicipio()
+							+ ", Direcci&oacute;n : " + activoOferta.getDireccion()
+							+ ", &Iacute;mporte participaci&oacute;n : " + activoOferta.getImporteParticipacion()+ "&#8364"
+							+ ", Precio m&iacute;nimo autorizado : " + activoOferta.getPrecioMinimo()+ "&#8364"
+							+"</p>";
+				}
+			}
+
+			WebDto dtopage = new WebDto();
+			dtopage.setLimit(100);
+			 Page pagina = expedienteComercialApi.getCompradoresByExpediente(expediente.getId(), dtopage);
+			 List<VBusquedaCompradoresExpediente> compradores = (List<VBusquedaCompradoresExpediente>) pagina.getResults();
+			 
+			 if(compradores != null && compradores.size() > 0){
+				 contenido += "<p><b>Compradores</b></p>";
+				 for (VBusquedaCompradoresExpediente comprador : compradores) {
+					 contenido += "<p>Nombre o raz&oacute;n social: " + comprador.getNombreComprador()
+						+ ", Documento : " + comprador.getNumDocumentoComprador() 
+						+ ", % compra : " + comprador.getPorcentajeCompra()
+						+"</p>";
+					 
+				 }
+			 }
+			
+
+			CondicionanteExpediente condiciones = expediente.getCondicionante();
+
+			if (condiciones != null) {
+				if (condiciones.getTipoImpuesto() != null && condiciones.getTipoAplicable() != null) {
+					contenido += "<p><b>Condiciones fiscales</b></p>";
+					contenido += "<p>Tipo de impuesto: " + condiciones.getTipoImpuesto().getDescripcion()
+							+ ", Tipo aplicable: " + condiciones.getTipoAplicable().toString() +"</p>";
+				}
+			}
+
+			contenido += "<p> Quedamos a su disposición para cualquier consulta o aclaración.</p>"
 					+ "<p> Saludos cordiales.</p>" + "<p> Fdo: #gestorTarea </p>" + "<p> Email: #mailGestorTarea </p>";
 
 			DtoSendNotificator dtoSendNotificator = this.rellenaDtoSendNotificator(tramite);
@@ -130,9 +185,7 @@ public class NotificatorServiceGtam extends AbstractNotificatorService implement
 	public void notificatorFinTareaConValores(ActivoTramite tramite, List<TareaExternaValor> valores) {
 		this.valores = valores;
 		this.notificator(tramite);
-		
-	}
 
-	
+	}
 
 }

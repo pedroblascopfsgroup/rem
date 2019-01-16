b3c8a21d23194649949ee87a907f54afef3dc5e1
Ramon Llinares <ramon.llinares@pfsgroup.es>
2019-01-16 12:12:31 +0100

    REMVIP-3046: Activos no se abren en REM

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/tareasactivo/TareaActivoManager.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/tareasactivo/TareaActivoManager.java
@@ -444,6 +444,11 @@ public class TareaActivoManager implements TareaActivoApi {
 			}
 		} return "";
 	}
+
+	@Override
+	public List<TareaActivo> getTareasActivo(Long idActivo, String codigoTipoTramite) {
+		return tareaActivoDao.getTareasActivoPorIdActivoAndTramite(idActivo, codigoTipoTramite);
+	}
 }
 
 

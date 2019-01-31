caff91030c2b78fb8f3f2ed6bae2fd504ced343d
Alberto Soler <alberto.soler@pfsgroup.es>
2019-01-31 15:58:35 +0100

    [HREOS-5365] Modificado comportamiendo disclaimers expediente

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/expedienteComercial/avisos/ExpedienteAvisoPreBloqueadoGencat.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/expedienteComercial/avisos/ExpedienteAvisoPreBloqueadoGencat.java
@@ -5,14 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import es.capgemini.pfs.asunto.model.DDEstadoProcedimiento;
 import es.capgemini.pfs.users.domain.Usuario;
 import es.pfsgroup.commons.utils.Checks;
 import es.pfsgroup.commons.utils.dao.abm.GenericABMDao;
 import es.pfsgroup.commons.utils.dao.abm.GenericABMDao.FilterType;
 import es.pfsgroup.plugin.rem.activo.dao.ActivoDao;
+import es.pfsgroup.plugin.rem.api.ActivoTramiteApi;
 import es.pfsgroup.plugin.rem.api.ExpedienteAvisadorApi;
 import es.pfsgroup.plugin.rem.model.Activo;
 import es.pfsgroup.plugin.rem.model.ActivoOferta;
+import es.pfsgroup.plugin.rem.model.ActivoTramite;
 import es.pfsgroup.plugin.rem.model.DtoAviso;
 import es.pfsgroup.plugin.rem.model.ExpedienteComercial;
 import es.pfsgroup.plugin.rem.model.Reserva;
@@ -27,26 +30,45 @@ public class ExpedienteAvisoPreBloqueadoGencat implements ExpedienteAvisadorApi{
 	@Autowired
 	private ActivoDao activoDao;
 	
+	@Autowired
+	private ActivoTramiteApi activoTramiteApi;
+	
 	@Override
 	public DtoAviso getAviso(ExpedienteComercial expediente, Usuario usuarioLogado) {
 		DtoAviso dtoAviso = new DtoAviso();
+		Boolean expBloqueado = false;
 		
 		if(!Checks.esNulo(expediente) && !Checks.esNulo(expediente.getOferta())){
 			List<ActivoOferta> actOfrList = expediente.getOferta().getActivosOferta();
 			Reserva reserva = genericDao.get(Reserva.class, genericDao.createFilter(FilterType.EQUALS, "expediente.id", expediente.getId()));
 			for (ActivoOferta actOfr : actOfrList){
 				Activo activo = actOfr.getPrimaryKey().getActivo();	
-				if (!Checks.esNulo(reserva)){
-					if (activoDao.isActivoAfectoGENCAT(activo.getId()) && DDEstadosExpedienteComercial.RESERVADO.equals(expediente.getEstado().getCodigo())) {
-						dtoAviso.setId(String.valueOf(expediente.getId()));
-						dtoAviso.setDescripcion("Expediente pre-bloqueado por GENCAT");
-						break;
+				List<ActivoTramite> actTraList = genericDao.getList(ActivoTramite.class, genericDao.createFilter(FilterType.EQUALS, "activo.id", activo.getId()));
+				if(!Checks.estaVacio(actTraList)){
+					for (ActivoTramite activoTramite : actTraList) {
+						if(ActivoTramiteApi.CODIGO_TRAMITE_COMUNICACION_GENCAT.equals(activoTramite.getTipoTramite().getCodigo())){
+							if(!Checks.esNulo(activoTramite.getEstadoTramite()) && DDEstadoProcedimiento.ESTADO_PROCEDIMIENTO_CERRADO.equals(activoTramite.getEstadoTramite().getCodigo())){
+								expBloqueado = true;
+							}else{
+								expBloqueado = false; 
+								break;
+							}
+						}
 					}
-				}else {
-					if (activoDao.isActivoAfectoGENCAT(activo.getId()) && DDEstadosExpedienteComercial.APROBADO.equals(expediente.getEstado().getCodigo())) {
-						dtoAviso.setId(String.valueOf(expediente.getId()));
-						dtoAviso.setDescripcion("Expediente pre-bloqueado por GENCAT");
-						break;
+					if(!expBloqueado){
+						if (!Checks.esNulo(reserva)){
+							if (activoDao.isActivoAfectoGENCAT(activo.getId()) && DDEstadosExpedienteComercial.RESERVADO.equals(expediente.getEstado().getCodigo())) {
+								dtoAviso.setId(String.valueOf(expediente.getId()));
+								dtoAviso.setDescripcion("Expediente pre-bloqueado por GENCAT");
+								break;
+							}
+						}else {
+							if (activoDao.isActivoAfectoGENCAT(activo.getId()) && DDEstadosExpedienteComercial.APROBADO.equals(expediente.getEstado().getCodigo())) {
+								dtoAviso.setId(String.valueOf(expediente.getId()));
+								dtoAviso.setDescripcion("Expediente pre-bloqueado por GENCAT");
+								break;
+							}
+						}
 					}
 				}
 			}

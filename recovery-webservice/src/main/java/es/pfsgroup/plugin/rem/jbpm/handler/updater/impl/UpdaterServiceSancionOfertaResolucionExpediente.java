70f17c00eb21e534467f37755f889e1a57992bf4
Lara Pablo <lara.pablo@pfsgroup.es>
2019-02-07 16:01:25 +0100

    HREOS-5414    func-rem-GENCAT     Arreglos en el rechazo del tramite gencat

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/jbpm/handler/updater/impl/UpdaterServiceSancionOfertaResolucionExpediente.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/jbpm/handler/updater/impl/UpdaterServiceSancionOfertaResolucionExpediente.java
@@ -271,7 +271,7 @@ public class UpdaterServiceSancionOfertaResolucionExpediente implements UpdaterS
 									DDEstadoComunicacionGencat estado = genericDao.get(DDEstadoComunicacionGencat.class, filtro);
 									comunicacionGencat.setEstadoComunicacion(estado);
 									comunicacionGencat.setFechaAnulacion(new Date());
-									if(!Checks.esNulo(estado) && DDEstadoComunicacionGencat.COD_RECHAZADO.equals(estado)) {
+									if(!Checks.esNulo(estado) && DDEstadoComunicacionGencat.COD_RECHAZADO.equals(estado.getCodigo())) {
 										comunicacionGencat.setComunicadoAnulacionAGencat(true);
 									}
 									

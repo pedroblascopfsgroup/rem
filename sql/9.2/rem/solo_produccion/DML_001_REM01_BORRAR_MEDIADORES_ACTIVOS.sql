b973187ebb77a36e5901aeb5d43dfb50c60bd1b9
Guillermo Llido <guillermo.llido@pfsgroup.es>
2019-02-07 09:16:16 +0100

    REMVIP-3300 Quitar Mediador / Colaborador / API erróneo de activos

--- sql/9.2/rem/solo_produccion/DML_001_REM01_BORRAR_MEDIADORES_ACTIVOS.sql
+++ sql/9.2/rem/solo_produccion/DML_001_REM01_BORRAR_MEDIADORES_ACTIVOS.sql
@@ -1661,7 +1661,7 @@ BEGIN
             
             IF V_COUNT = 1 THEN
 				V_MSQL:='UPDATE '||V_ESQUEMA||'.ACT_ICM_INF_COMER_HIST_MEDI SET ICM_FECHA_HASTA = SYSDATE , fechamodificar = SYSDATE,usuariomodificar = '''||V_MODIFICAR||'''
-                    WHERE ACT_ID = (SELECT ACT_ID from '||V_ESQUEMA||'.ACT_ACTIVO where act_num_activo = '''||TRIM(V_TMP_TIPO_DATA(1))||''') AND
+                    WHERE ACT_ID = (SELECT ACT_ID from '||V_ESQUEMA||'.ACT_ACTIVO where act_num_activo = '''||TRIM(V_TMP_TIPO_DATA(1))||''') 
                     AND ICM_FECHA_HASTA IS NULL';
                 EXECUTE IMMEDIATE V_MSQL ;
             ELSE

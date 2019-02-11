c50c223e38a06c037ff3997fa09d9c986f719b05
Remus Ovidiu Viorel <ovidiu.remus@pfsgroup.es>
2019-02-11 15:29:14 +0100

    REMVIP-3211- Guarda salto de linea en BDD

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/activo/ActivoManager.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/activo/ActivoManager.java
@@ -3732,7 +3732,7 @@ public class ActivoManager extends BusinessOperationOverrider<ActivoApi> impleme
 		try {
 			beanUtilNotNull.copyProperty(activo, "fechaVentaExterna", dto.getFechaVenta());
 			beanUtilNotNull.copyProperty(activo, "importeVentaExterna", dto.getImporteVenta());
-			beanUtilNotNull.copyProperty(activo, "observacionesVentaExterna", dto.getObservaciones());
+			beanUtilNotNull.copyProperty(activo, "observacionesVentaExterna", dto.getObservaciones().replaceAll("(\n|\r)", " "));
 			dto.setVentaExterna(Checks.esNulo(activo.getFechaVentaExterna()));
 
 			// Si se ha introducido valores en fecha o importe de venta, se
@@ -3775,7 +3775,6 @@ public class ActivoManager extends BusinessOperationOverrider<ActivoApi> impleme
 			return false;
 		}
 
-		activo.setObservacionesVentaExterna(dto.getObservaciones());
 		activo.setEstaEnPuja(dto.getPuja());
 		activoDao.save(activo);
 

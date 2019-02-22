5d3824e4c05cb0e5819b74de3ab56c302256bdd8
Lara Pablo <lara.pablo@pfsgroup.es>
2019-02-22 13:59:43 +0100

    HREOS-5592    func-rem-GENCAT-sinVisitas-PruebasASM  Arreglos a la hora de lanzar tramite gencat y en el historico de gencat

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/gencat/GencatManager.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/gencat/GencatManager.java
@@ -572,13 +572,21 @@ public class GencatManager extends  BusinessOperationOverrider<GencatApi> implem
 				
 				HistoricoOfertaGencat ofertaGencat = listaOfertasGencat.get(i);
 				Oferta oferta = ofertaGencat.getOferta();
-				
-				dtoOfertasAsociadasActivo.setFechaPreBloqueo(hComunicacionGencat.getFechaPreBloqueo());
-				dtoOfertasAsociadasActivo.setNumOferta(oferta.getNumOferta());
-				dtoOfertasAsociadasActivo.setImporteOferta(ofertaGencat.getImporte());
-				dtoOfertasAsociadasActivo.setTipoComprador(ofertaGencat.getTiposPersona().getDescripcion());
-				dtoOfertasAsociadasActivo.setSituacionOcupacional(ofertaGencat.getSituacionPosesoria().getDescripcion());
-				
+				if(!Checks.esNulo(ofertaGencat)) {
+					dtoOfertasAsociadasActivo.setImporteOferta(ofertaGencat.getImporte());
+					if(!Checks.esNulo(ofertaGencat.getTiposPersona())){
+						dtoOfertasAsociadasActivo.setTipoComprador(ofertaGencat.getTiposPersona().getDescripcion());
+					}
+					if(!Checks.esNulo(ofertaGencat.getSituacionPosesoria())){
+						dtoOfertasAsociadasActivo.setSituacionOcupacional(ofertaGencat.getSituacionPosesoria().getDescripcion());
+					}
+				}
+				if(!Checks.esNulo(oferta)) {
+					dtoOfertasAsociadasActivo.setNumOferta(oferta.getNumOferta());
+				}
+				if(!Checks.esNulo(hComunicacionGencat)) {
+					dtoOfertasAsociadasActivo.setFechaPreBloqueo(hComunicacionGencat.getFechaPreBloqueo());
+				}
 				listaDtoOfertasAsociadasActivo.add(dtoOfertasAsociadasActivo);
 			}
 		}

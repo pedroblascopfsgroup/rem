8f86b9d4c53b97df84eda2ad5913c8c1b845a3f0
Pier <pier.gotta@pfsgroup.es>
2019-02-25 15:47:43 +0100

    REMVIP-3322 Modificacion registros de publicacion

---
+++ sql/9.2/rem/solo_produccion/DML_55_REM01_UPDATE_ACTIVOS_VENTA_HISTORICO_PUBLICACIONES.sql
@@ -0,0 +1,88 @@
+--/*
+--##########################################
+--## AUTOR=PIER GOTTA 
+--## FECHA_CREACION=20190225
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.2
+--## INCIDENCIA_LINK=REMVIP-3322
+--## PRODUCTO=NO
+--##
+--## Finalidad: Insertar un registro de alquiler para los activos DD_TCO_CODIGO = 02
+--## INSTRUCCIONES:
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
+SET DEFINE OFF;
+
+
+DECLARE
+    V_SQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar         
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+    --V_COUNT NUMBER(16); -- Vble. para contar.
+    --V_COUNT_INSERT NUMBER(16):= 0; -- Vble. para contar inserts
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+	V_USUARIO VARCHAR2(32 CHAR):= 'REMVIP-3322';
+    
+    
+ BEGIN
+
+    	V_SQL := 'MERGE INTO '||V_ESQUEMA||'.ACT_AHP_HIST_PUBLICACION T1 USING(
+			SELECT * FROM (
+			    WITH TODO AS (
+			    SELECT AHP.AHP_ID, ACT.ACT_ID, ACT.ACT_NUM_ACTIVO, AHP.AHP_FECHA_INI_ALQUILER,  AHP.AHP_FECHA_INI_VENTA,
+				    AHP.AHP_FECHA_FIN_ALQUILER, AHP.AHP_FECHA_FIN_VENTA, AHP.DD_TCO_ID
+			    FROM '||V_ESQUEMA||'.ACT_ACTIVO ACT
+			    JOIN '||V_ESQUEMA||'.ACT_AHP_HIST_PUBLICACION AHP ON AHP.ACT_ID = ACT.ACT_ID AND AHP.BORRADO = 0
+			    WHERE ACT.BORRADO = 0
+			    )
+			    SELECT AHP_ID, ACT_NUM_ACTIVO,AHP_FECHA_INI_ALQUILER, AHP_FECHA_INI_VENTA, AHP_FECHA_FIN_ALQUILER, AHP_FECHA_FIN_VENTA,DD_TCO_ID,
+				CASE 
+				    WHEN (AHP_FECHA_INI_ALQUILER IS NOT NULL OR AHP_FECHA_FIN_ALQUILER IS NOT NULL) AND (AHP_FECHA_INI_VENTA IS NULL OR AHP_FECHA_FIN_VENTA IS NULL)
+				    THEN ''SOLO ALQUILER''
+				    WHEN (AHP_FECHA_INI_VENTA IS NOT NULL OR AHP_FECHA_FIN_VENTA IS NOT NULL) AND (AHP_FECHA_INI_ALQUILER IS NULL OR AHP_FECHA_FIN_ALQUILER IS NULL)
+				    THEN ''SOLO VENTA''
+				ELSE
+				    ''ALQUILER VENTA''
+				END AS SOLO
+			    FROM TODO
+			)WHERE SOLO = ''ALQUILER VENTA'' AND (TRUNC(AHP_FECHA_INI_VENTA) <> TRUNC(AHP_FECHA_INI_ALQUILER)) OR (TRUNC(AHP_FECHA_FIN_ALQUILER) <> TRUNC(AHP_FECHA_FIN_VENTA))) T2
+			ON (T1.AHP_ID = T2.AHP_ID)
+			WHEN MATCHED THEN 
+			UPDATE SET
+			T1.DD_TPU_A_ID = NULL
+			,T1.DD_EPA_ID = NULL
+			,T1.DD_MTO_A_ID = NULL
+			,T1.DD_TCO_ID = 1
+			,T1.AHP_CHECK_PUBLICAR_A = 0
+			,T1.AHP_CHECK_OCULTAR_A = 0
+			,T1.AHP_CHECK_OCULTAR_PRECIO_A = 0
+			,T1.AHP_CHECK_PUB_SIN_PRECIO_A = 0
+			,T1.AHP_FECHA_INI_ALQUILER = NULL
+			,T1.AHP_FECHA_FIN_ALQUILER = NULL
+			,T1.USUARIOMODIFICAR = ''REMVIP-3322''
+			,T1.FECHAMODIFICAR = SYSDATE';
+    				
+      EXECUTE IMMEDIATE V_SQL;
+      
+	  DBMS_OUTPUT.PUT_LINE('Se han creado los registros en la histórica correctamente');
+      
+ COMMIT;
+ 
+EXCEPTION
+     WHEN OTHERS THEN
+          ERR_NUM := SQLCODE;
+          ERR_MSG := SQLERRM;
+          DBMS_OUTPUT.PUT_LINE('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(ERR_NUM));
+          DBMS_OUTPUT.PUT_LINE('-----------------------------------------------------------'); 
+          DBMS_OUTPUT.PUT_LINE(ERR_MSG);
+          ROLLBACK;
+          RAISE;   
+END;
+/
+EXIT;

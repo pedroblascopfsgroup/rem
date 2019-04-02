96bfc4f179cf6fde981fedd29019d5377a6cb259
Ramon Llinares <ramon.llinares@pfsgroup.es>
2019-04-02 09:46:00 +0200

    error validación documento de aprobación

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/jbpm/ValidateJbpmManager.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/jbpm/ValidateJbpmManager.java
@@ -56,8 +56,7 @@ public class ValidateJbpmManager implements ValidateJbpmApi {
 		if (!trabajoApi.checkReservaNecesariaNotNull(tareaExterna)) return FALTA_MARCAR_RESERVA_NECESARIA;
 		if (trabajoApi.checkBankia(tareaExterna) || trabajoApi.checkLiberbank(tareaExterna)
 				|| trabajoApi.checkGiants(tareaExterna))
-			return null;
-		return activoTramiteApi.existeAdjuntoUGValidacion(tareaExterna, DDTipoDocumentoActivo.CODIGO_PROPUESTA_PRECIOS_SANCIONADA,"E");
+			return null;return activoTramiteApi.existeAdjuntoUGValidacion(tareaExterna, "23","E");
 	}	
 	
 	@Override

a1e327ce7df2d45b64ad6e5449b2281cf574d403
sbelenguer <sbelenguer@pfsgroup.es>
2019-04-16 11:09:42 +0200

    Correción script borrado gestores

--- sql/9.3/rem/solo_produccion/DML_005_REM01_Borrar_gestores.sql
+++ sql/9.3/rem/solo_produccion/DML_005_REM01_Borrar_gestores.sql
@@ -1,13 +1,13 @@
 --/*
 --##########################################
---## AUTOR=JOSE MANUEL PÉREZ BARBERÁ
---## FECHA_CREACION=20170705
+--## AUTOR=sERGIO bELENGUER gADEA
+--## FECHA_CREACION=20190416
 --## ARTEFACTO=batch
---## VERSION_ARTEFACTO=9.2.17
---## INCIDENCIA_LINK=RECOVERY-4693
+--## VERSION_ARTEFACTO=2.8
+--## INCIDENCIA_LINK=hreos-6080
 --## PRODUCTO=NO
 --## 
---## Finalidad: Soluciona incidencia RECOVERY-4693
+--## Finalidad: Borrar gestores asignados por SP
 --## INSTRUCCIONES: Configurar las variables necesarias en el principio del DECLARE
 --## VERSIONES:
 --##        0.1 Versión inicial
@@ -21,8 +21,8 @@ SET DEFINE OFF;
 DECLARE
 
     V_MSQL         VARCHAR2(32000 CHAR); -- Sentencia a ejecutar    
-    V_ESQUEMA      VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
-    V_ESQUEMA_M    VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+    V_ESQUEMA      VARCHAR2(25 CHAR):= 'REM01'; -- Configuracion Esquema
+    V_ESQUEMA_M    VARCHAR2(25 CHAR):= 'REMMASTER'; -- Configuracion Esquema Master
     V_SQL          VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
     V_NUM_TABLAS   NUMBER(16); -- Vble. para validar la existencia de una tabla.  
     ERR_NUM        NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
@@ -55,8 +55,7 @@ BEGIN
 	-- Borramos los gestores en GAC_GESTOR_ADD_ACTIVO
 	loop
 		delete from #ESQUEMA#.GAC_GESTOR_ADD_ACTIVO t1
-		where exists (select 1 from act_activo act
-			join GAC_GESTOR_ADD_ACTIVO gac on gac.act_id=act.act_id
+		where exists (select 1 from GAC_GESTOR_ADD_ACTIVO gac
 			join GEE_GESTOR_ENTIDAD gee on gee.gee_id=gac.gee_id
 			join REMMASTER.DD_TGE_TIPO_GESTOR tge on tge.dd_tge_id=gee.dd_tge_id
 			where gee.USUARIOCREAR='SP_AGA_V4' and t1.gee_id=gee.gee_id)
@@ -73,9 +72,7 @@ BEGIN
 	-- Borramos los gestores en GEE_GESTOR_ENTIDAD
 	loop
 		delete from #ESQUEMA#.GEE_GESTOR_ENTIDAD t1
-		where exists (select 1 from act_activo act
-			join GAC_GESTOR_ADD_ACTIVO gac on gac.act_id=act.act_id
-			join GEE_GESTOR_ENTIDAD gee on gee.gee_id=gac.gee_id
+		where exists (select 1 from GEE_GESTOR_ENTIDAD gee
 			join REMMASTER.DD_TGE_TIPO_GESTOR tge on tge.dd_tge_id=gee.dd_tge_id
 			where gee.USUARIOCREAR='SP_AGA_V4' and t1.gee_id=gee.gee_id)
 		and rownum <=5000;
@@ -91,11 +88,10 @@ BEGIN
 	-- Borramos los gestores en GAH_GESTOR_ACTIVO_HISTORICO
 	loop
 		delete from #ESQUEMA#.GAH_GESTOR_ACTIVO_HISTORICO t1
-		where exists (select 1 from act_activo act
-			join GAH_GESTOR_ACTIVO_HISTORICO gac on gac.act_id=act.act_id
-			join GEH_GESTOR_ENTIDAD_HIST gee on gee.gee_id=gac.gee_id
+		where exists (select 1 from GAH_GESTOR_ACTIVO_HISTORICO gac
+			join GEH_GESTOR_ENTIDAD_HIST gee on gee.geh_id=gac.geh_id
 			join REMMASTER.DD_TGE_TIPO_GESTOR tge on tge.dd_tge_id=gee.dd_tge_id
-			where gee.USUARIOCREAR='SP_AGA_V4' and t1.gee_id=gee.gee_id)
+			where gee.USUARIOCREAR='SP_AGA_V4' and t1.geh_id=gee.geh_id)
 		and rownum <=5000;
 		
 		V_NUM_TABLAS := sql%rowcount;
@@ -109,9 +105,7 @@ BEGIN
 	-- Borramos los gestores en GEH_GESTOR_ENTIDAD_HIST
 	loop
 		delete from #ESQUEMA#.GEH_GESTOR_ENTIDAD_HIST t1
-		where exists (select 1 from act_activo act
-			join GAH_GESTOR_ACTIVO_HISTORICO gac on gac.act_id=act.act_id
-			join GEH_GESTOR_ENTIDAD_HIST gee on gee.gee_id=gac.gee_id
+		where exists (select 1 from GEH_GESTOR_ENTIDAD_HIST gee
 			join REMMASTER.DD_TGE_TIPO_GESTOR tge on tge.dd_tge_id=gee.dd_tge_id
 			where gee.USUARIOCREAR='SP_AGA_V4' and t1.gee_id=gee.gee_id)
 		and rownum <=5000;

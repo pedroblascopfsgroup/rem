dddd63f75747620ae814df409cd34026db6ba61a
Rasul <rasul.abdulaev@pfsgroup.es>
2019-04-18 15:24:15 +0200

    REMVIP-3683 - Añadir cambio al ws de envío de comisiones a WebCom

---
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/api/services/webcom/dto/ActivoComisionDto.java
@@ -0,0 +1,17 @@
+package es.pfsgroup.plugin.rem.api.services.webcom.dto;
+
+import es.pfsgroup.plugin.rem.api.services.webcom.dto.datatype.LongDataType;
+
+public class ActivoComisionDto {
+	
+	private LongDataType idActivoHaya;
+
+	public LongDataType getIdActivoHaya() {
+		return idActivoHaya;
+	}
+
+	public void setIdActivoHaya(LongDataType idActivoHaya) {
+		this.idActivoHaya = idActivoHaya;
+	}
+
+}

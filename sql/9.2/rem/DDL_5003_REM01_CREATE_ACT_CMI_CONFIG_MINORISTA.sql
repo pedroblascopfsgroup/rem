01c57fb90b6c8640d0f767ec39db02b01bc430dc
mariamLliso <mariam.lliso@pfsgroup.es>
2019-04-23 09:24:25 +0200

    HREOS-6202 Creación DD_EGE_EQUIPO_GESTION y ACT_CMI_CONFIG_MINORISTA

---
+++ sql/9.2/rem/DDL_5003_REM01_CREATE_ACT_CMI_CONFIG_MINORISTA.sql
@@ -0,0 +1,182 @@
+--/*
+--##########################################
+--## AUTOR=MARIAM LLISO
+--## FECHA_CREACION=20190423
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.1
+--## INCIDENCIA_LINK=HREOS-6202
+--## PRODUCTO=NO
+--## 
+--##           
+--## INSTRUCCIONES: Configurar las variables necesarias en el principio del DECLARE
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+
+--Para permitir la visualización de texto en un bloque PL/SQL utilizando DBMS_OUTPUT.PUT_LINE
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF;
+
+
+DECLARE
+
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar    
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+    V_TABLESPACE_IDX VARCHAR2(25 CHAR):= '#TABLESPACE_INDEX#'; -- Configuracion Tablespace de Indices
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.  
+    V_NUM_SEQ NUMBER(16); -- Vble. para validar la existencia de una secuencia.  
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+
+    V_TEXT1 VARCHAR2(2400 CHAR); -- Vble. auxiliar
+    V_TEXT_TABLA VARCHAR2(2400 CHAR) := 'ACT_CMI_CONFIG_MINORISTA'; -- Vble. auxiliar para almacenar el nombre de la tabla de ref.
+    V_COMMENT_TABLE VARCHAR2(500 CHAR):= 'Tabla configuración minoristas'; -- Vble. para los comentarios de las tablas
+    V_CREAR_FK VARCHAR2(2 CHAR) := 'SI'; -- [SI, NO] Vble. para indicar al script si debe o no crear tambien las relaciones Foreign Keys.
+    
+    
+    /* ARRAY CON NUEVAS FOREIGN KEYS */
+    TYPE T_FK IS TABLE OF VARCHAR2(4000);
+    TYPE T_ARRAY_FK IS TABLE OF T_FK;
+    V_FK T_ARRAY_FK := T_ARRAY_FK(
+    			--NOMBRE FK 					CAMPO FK 			TABLA DESTINO FK 					CAMPO DESTINO FK
+    	T_FK(	'FK_CONFIGMINOR_DDCARTERA',		'DD_CRA_ID',		V_ESQUEMA||'.DD_CRA_CARTERA',	    'DD_CRA_ID'			),
+    	T_FK(	'FK_CONFIGMINOR_DDTIPOACT',		'DD_TPA_ID',		V_ESQUEMA||'.DD_TPA_TIPO_ACTIVO',   'DD_TPA_ID'			)
+    );
+    V_T_FK T_FK;
+    
+    
+BEGIN
+	
+
+	DBMS_OUTPUT.PUT_LINE('********' ||V_TEXT_TABLA|| '********'); 
+	DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.'||V_TEXT_TABLA||'... Comprobaciones previas');
+	
+	
+	-- Verificar si la tabla ya existe
+	V_MSQL := 'SELECT COUNT(1) FROM ALL_TABLES WHERE TABLE_NAME = '''||V_TEXT_TABLA||''' and owner = '''||V_ESQUEMA||'''';
+	EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;	
+	IF V_NUM_TABLAS = 1 THEN
+		DBMS_OUTPUT.PUT_LINE('[INFO] ' || V_ESQUEMA || '.'||V_TEXT_TABLA||'... Ya existe. Se borrará.');
+		EXECUTE IMMEDIATE 'DROP TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' CASCADE CONSTRAINTS';
+	END IF;
+	
+	-- Comprobamos si existe la secuencia
+	V_SQL := 'SELECT COUNT(1) FROM ALL_SEQUENCES WHERE SEQUENCE_NAME = ''S_'||V_TEXT_TABLA||''' and SEQUENCE_OWNER = '''||V_ESQUEMA||'''';
+	EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS; 
+	IF V_NUM_TABLAS = 1 THEN
+		DBMS_OUTPUT.PUT_LINE('[INFO] '|| V_ESQUEMA ||'.S_'||V_TEXT_TABLA||'... Ya existe. Se borrará.');  
+		EXECUTE IMMEDIATE 'DROP SEQUENCE '||V_ESQUEMA||'.S_'||V_TEXT_TABLA||'';		
+	END IF; 	
+	
+	-- Creamos la tabla
+	DBMS_OUTPUT.PUT_LINE('[INFO] Creando '||V_ESQUEMA||'.'||V_TEXT_TABLA||'...');
+	V_MSQL := 'CREATE TABLE ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'
+	(
+			CMI_ID									NUMBER(16,0)                NOT NULL,
+			DD_CRA_ID	        					NUMBER(16,0)	          	NOT NULL,
+			DD_TPA_ID			       				NUMBER(16,0)				NOT NULL,
+			CMI_PRECIO_PUBLI_MAX	  				NUMBER(16,0),
+			CMI_ULT_TASACION_MAX					NUMBER(16,0),
+			VERSION 								NUMBER(38,0) 				DEFAULT 0 NOT NULL ENABLE, 
+			USUARIOCREAR 							VARCHAR2(50 CHAR) 			NOT NULL ENABLE, 
+			FECHACREAR 								TIMESTAMP (6) 				NOT NULL ENABLE, 
+			USUARIOMODIFICAR 						VARCHAR2(50 CHAR), 
+			FECHAMODIFICAR 							TIMESTAMP (6), 
+			USUARIOBORRAR 							VARCHAR2(50 CHAR), 
+			FECHABORRAR 							TIMESTAMP (6), 
+			BORRADO 								NUMBER(1,0) 				DEFAULT 0 NOT NULL ENABLE			
+	)';
+
+	EXECUTE IMMEDIATE V_MSQL;
+	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'... Tabla creada.');	
+
+	-- Creamos indice	
+	V_MSQL := 'CREATE UNIQUE INDEX '||V_ESQUEMA||'.'||V_TEXT_TABLA||'_PK ON '||V_ESQUEMA|| '.'||V_TEXT_TABLA||'(CMI_ID) TABLESPACE '||V_TABLESPACE_IDX;		
+	EXECUTE IMMEDIATE V_MSQL;
+	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'_PK... Indice creado.');	
+	
+	-- Creamos primary key
+	V_MSQL := 'ALTER TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' ADD (CONSTRAINT '||V_TEXT_TABLA||'_PK PRIMARY KEY (CMI_ID) USING INDEX)';
+	EXECUTE IMMEDIATE V_MSQL;
+	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'_PK... PK creada.');	
+	
+	-- Creamos sequence
+	V_MSQL := 'CREATE SEQUENCE '||V_ESQUEMA||'.S_'||V_TEXT_TABLA||'';		
+	EXECUTE IMMEDIATE V_MSQL;		
+	DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.S_'||V_TEXT_TABLA||'... Secuencia creada');
+	
+	-- Creamos comentario	
+	V_MSQL := 'COMMENT ON TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' IS '''||V_COMMENT_TABLE||'''';		
+	EXECUTE IMMEDIATE V_MSQL;
+	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'... Comentario creado.');	
+	
+	
+	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||' creada correctamente.');
+
+	COMMIT;
+	DBMS_OUTPUT.PUT_LINE('[INFO] ---------------------------------------------------------');
+	DBMS_OUTPUT.PUT_LINE('[INFO] Creando FKs sobre ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'... ');
+
+	-- Solo si esta activo el indicador de creacion FK, el script creara tambien las FK
+	IF V_CREAR_FK = 'SI' THEN
+
+		-- Bucle que CREA las FK de las nuevas columnas
+		FOR I IN V_FK.FIRST .. V_FK.LAST
+		LOOP
+
+			V_T_FK := V_FK(I);	
+
+			-- Verificar si la FK ya existe. Si ya existe la FK, no se hace nada.
+			V_MSQL := 'select count(1) from all_constraints where OWNER = '''||V_ESQUEMA||''' and table_name = '''||V_TEXT_TABLA||''' and constraint_name = '''||V_T_FK(1)||'''';
+			EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;	
+			IF V_NUM_TABLAS = 0 THEN
+				--No existe la FK y la creamos
+				DBMS_OUTPUT.PUT_LINE('[INFO] Cambios en ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'['||V_T_FK(1)||'] -------------------------------------------');
+				V_MSQL := '
+					ALTER TABLE '||V_TEXT_TABLA||'
+					ADD CONSTRAINT '||V_T_FK(1)||' FOREIGN KEY
+					(
+					  '||V_T_FK(2)||'
+					)
+					REFERENCES '||V_T_FK(3)||'
+					(
+					  '||V_T_FK(4)||' 
+					)
+					ON DELETE SET NULL ENABLE
+				';
+
+				EXECUTE IMMEDIATE V_MSQL;
+				--DBMS_OUTPUT.PUT_LINE('[3] '||V_MSQL);
+				DBMS_OUTPUT.PUT_LINE('[INFO] ... '||V_T_FK(1)||' creada en tabla: FK en columna '||V_T_FK(2)||' hacia '||V_T_FK(3)||'.'||V_T_FK(4)||'... OK');
+
+			END IF;
+
+		END LOOP;
+
+	END IF;
+
+	COMMIT;
+
+EXCEPTION
+     WHEN OTHERS THEN 
+         DBMS_OUTPUT.PUT_LINE('KO!');
+          err_num := SQLCODE;
+          err_msg := SQLERRM;
+
+          DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(err_num));
+          DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+          DBMS_OUTPUT.put_line(err_msg);
+
+          ROLLBACK;
+          RAISE;          
+
+END;
+
+/
+
+EXIT

2bb33750a4b5d0d6ebeaaea07d2be0a02a75f145
Daniel Algaba <daniel.algaba@pfsgroup.es>
2019-05-24 09:41:52 +0200

    HREOS-6453 Superusuario (Bloque 2): Carga masiva actualizar pestaña Formalización Parte II - Correción Tipo Financiación

--- paradise-framework/src/main/java/es/pfsgroup/framework/paradise/bulkUpload/api/impl/ParticularValidatorManager.java
+++ paradise-framework/src/main/java/es/pfsgroup/framework/paradise/bulkUpload/api/impl/ParticularValidatorManager.java
@@ -1,8 +1,6 @@
 package es.pfsgroup.framework.paradise.bulkUpload.api.impl;
 
 import java.math.BigDecimal;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -10,7 +8,6 @@ import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
-import org.codehaus.groovy.syntax.Numbers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -3090,33 +3087,7 @@ public class ParticularValidatorManager implements ParticularValidatorApi {
 				+"		FROM dual");
 		
 		return !"1".equals(resultado);
-	}
-
-	public Boolean existeExpedienteFormalizacion(String numExpediente){
-		if(Checks.esNulo(numExpediente) || !StringUtils.isNumeric(numExpediente))
-			return false;
-		String resultado = rawDao.getExecuteSQL("SELECT COUNT(*) "
-				+ "		 FROM FOR_FORMALIZACION FORM WHERE"
-				+ "		 	FORM.FOR_NUMEXPEDIENTE ='"+numExpediente+"' "
-				+ "		 	AND FORM.BORRADO = 0 AND FORM.FOR_NUMEXPEDIENTE IS NOT NULL");
-		return "1".equals(resultado);
-	}
-	
-	@Override
-	public Boolean existeExpedienteFormalizacionBankia(String numExpediente){
-		if(Checks.esNulo(numExpediente) || !StringUtils.isNumeric(numExpediente))
-			return false;
-		String resultado = rawDao.getExecuteSQL("SELECT COUNT(*) FROM DD_ETF_ENTIDAD_FINANCIERA DDETF" 
-				+"                JOIN COE_CONDICIONANTES_EXPEDIENTE ON DDETF.DD_ETF_ID = COE_CONDICIONANTES_EXPEDIENTE.DD_ETF_ID" 
-				+"                JOIN ECO_EXPEDIENTE_COMERCIAL ON ECO_EXPEDIENTE_COMERCIAL.ECO_ID = COE_CONDICIONANTES_EXPEDIENTE.ECO_ID" 
-				+"                JOIN FOR_FORMALIZACION ON FOR_FORMALIZACION.ECO_ID = ECO_EXPEDIENTE_COMERCIAL.ECO_ID" 
-				+"                WHERE DDETF.DD_ETF_CODIGO = '01'" 
-				+"				  AND FOR_NUMEXPEDIENTE ='"+numExpediente+"' "
-				+"                AND FOR_FORMALIZACION.FOR_NUMEXPEDIENTE IS NOT null"
-				+"		AND DDETF.BORRADO = 0");
-		return "1".equals(resultado);
-	}
-	
+	}	
 
 	@Override
 	public Boolean existeEntidadFinanciera(String entidadFinanciera){
@@ -3134,9 +3105,9 @@ public class ParticularValidatorManager implements ParticularValidatorApi {
 		if(Checks.esNulo(tipoFinanciacion) || !StringUtils.isNumeric(tipoFinanciacion))
 			return false;
 		String resultado = rawDao.getExecuteSQL("SELECT COUNT (1) "
-				+ "FROM DD_ESF_ESTADOS_FINANCIACION DDESF WHERE "
-				+ "DDESF.DD_ESF_CODIGO = '"+tipoFinanciacion+"' "
-				+" AND DDESF.BORRADO = 0");
+				+ "FROM DD_TRC_TIPO_RIESGO_CLASE DDTRC WHERE "
+				+ "DDTRC.DD_TRC_CODIGO = '"+tipoFinanciacion+"' "
+				+" AND DDTRC.BORRADO = 0");
 		return !"0".equals(resultado);
 	}
 	

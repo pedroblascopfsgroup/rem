196869afb34eb06a01a619f7a79701e90e1fb19a
Guillermo Llido <guillermo.llido@pfsgroup.es>
2019-05-29 12:51:10 +0200

    [REMVIP-4336] Arreglar el filtro de homeserve

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/trabajo/TrabajoManager.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/trabajo/TrabajoManager.java
@@ -1328,8 +1328,9 @@ public class TrabajoManager extends BusinessOperationOverrider<TrabajoApi> imple
 					Usuario usuProveedorBankiaSareb = genericDao.get(Usuario.class, filtroUsuProveedorBankiaSareb);
 					if(!Checks.esNulo(usuProveedorBankiaSareb)) {
 						Filter filtro = genericDao.createFilter(FilterType.EQUALS, "usuario",usuProveedorBankiaSareb);
-						Filter filtro2 = genericDao.createFilter(FilterType.EQUALS, "proveedor.tipoProveedor.codigo", DDTipoProveedor.COD_CERTIFICADORA);
-						List<ActivoProveedorContacto> listaPVC = genericDao.getList(ActivoProveedorContacto.class,filtro,filtro2);
+						Filter filtro2 = genericDao.createFilter(FilterType.NULL, "fechaBaja");
+						List<ActivoProveedorContacto> listaPVC = genericDao.getList(ActivoProveedorContacto.class,
+								filtro,filtro2);
 						if(!Checks.estaVacio(listaPVC)){
 							trabajo.setProveedorContacto(listaPVC.get(0));
 							trabajo = genericDao.save(Trabajo.class, trabajo);
@@ -1341,8 +1342,20 @@ public class TrabajoManager extends BusinessOperationOverrider<TrabajoApi> imple
 					Usuario usuProveedor = genericDao.get(Usuario.class, filtroUsuProveedor);
 					if (!Checks.esNulo(usuProveedor)) {
 						Filter filtro = genericDao.createFilter(FilterType.EQUALS, "usuario", usuProveedor);
-						Filter filtro2 = genericDao.createFilter(FilterType.EQUALS, "proveedor.tipoProveedor.codigo",
-								DDTipoProveedor.COD_CERTIFICADORA);
+						Filter filtro2 = genericDao.createFilter(FilterType.NULL, "proveedor.fechaBaja");
+						List<ActivoProveedorContacto> listaPVC = genericDao.getList(ActivoProveedorContacto.class,
+								filtro, filtro2);
+						if (!Checks.estaVacio(listaPVC)) {
+							trabajo.setProveedorContacto(listaPVC.get(0));
+							trabajo = genericDao.save(Trabajo.class, trabajo);
+						}
+					}
+				} else if (this.checkLiberbank(trabajo)) {
+					Filter filtroUsuProveedor = genericDao.createFilter(FilterType.EQUALS ,"username", remUtils.obtenerUsuarioPorDefecto(GestorActivoApi.USU_PROVEEDOR_AESCTECTONICA));
+					Usuario usuProveedor = genericDao.get(Usuario.class, filtroUsuProveedor);
+					if (!Checks.esNulo(usuProveedor)) {
+						Filter filtro = genericDao.createFilter(FilterType.EQUALS, "usuario", usuProveedor);
+						Filter filtro2 = genericDao.createFilter(FilterType.NULL, "fechaBaja");
 						List<ActivoProveedorContacto> listaPVC = genericDao.getList(ActivoProveedorContacto.class,
 								filtro, filtro2);
 						if (!Checks.estaVacio(listaPVC)) {
@@ -1351,21 +1364,6 @@ public class TrabajoManager extends BusinessOperationOverrider<TrabajoApi> imple
 						}
 					}
 				}
-
-			} else if (this.checkLiberbank(trabajo)) {
-					Filter filtroUsuProveedor = genericDao.createFilter(FilterType.EQUALS ,"username", remUtils.obtenerUsuarioPorDefecto(GestorActivoApi.USU_PROVEEDOR_AESCTECTONICA));
-					Usuario usuProveedor = genericDao.get(Usuario.class, filtroUsuProveedor);
-					if (!Checks.esNulo(usuProveedor)) {
-						Filter filtro = genericDao.createFilter(FilterType.EQUALS, "usuario", usuProveedor);
-						Filter filtro2 = genericDao.createFilter(FilterType.EQUALS, "proveedor.tipoProveedor.codigo",
-								DDTipoProveedor.COD_CERTIFICADORA);
-						List<ActivoProveedorContacto> listaPVC = genericDao.getList(ActivoProveedorContacto.class,
-								filtro, filtro2);
-						if (!Checks.estaVacio(listaPVC)) {
-							trabajo.setProveedorContacto(listaPVC.get(0));
-							trabajo = genericDao.save(Trabajo.class, trabajo);
-						}
-					}
 				
 			} else if (trabajo.getSubtipoTrabajo().getCodigo().equals(DDSubtipoTrabajo.CODIGO_CEDULA_HABITABILIDAD)) {
 				tipoTramite = tipoProcedimientoManager

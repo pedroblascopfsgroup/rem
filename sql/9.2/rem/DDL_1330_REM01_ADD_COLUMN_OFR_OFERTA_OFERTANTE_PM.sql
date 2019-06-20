d84cb94bdd525cb9b7ff6fe3b5fc86ef761745ca
Sergio Gimenez <sergio.gimenez@pfsgroup.es>
2019-06-20 15:28:43 +0200

    HREOS-6725 Trámite Comercial Apple - [RUFU-002] - Procesado Web Tarea Respuesta ofertante PM

--- sql/9.2/rem/DDL_1330_REM01_ADD_COLUMN_OFR_OFERTA_OFERTANTE_PM.sql
+++ sql/9.2/rem/DDL_1330_REM01_ADD_COLUMN_OFR_OFERTA_OFERTANTE_PM.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=Sergio Giménez Mota
---## FECHA_CREACION=20190612
+--## FECHA_CREACION=20190620
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.2
 --## INCIDENCIA_LINK=HREOS-6725

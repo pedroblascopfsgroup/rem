13e7328545db1416a88a5a20c39194fdf401a609
Remus Ovidiu Viorel <ovidiu.remus@pfsgroup.es>
2019-06-21 15:05:29 +0200

    REMVIP-4619 - Actualizar fecha ingreso cheque

---
+++ sql/9.2/rem/solo_produccion/DML_9163_REM01_UPDATE_FECHA_INGRESO_CHEQUE_REMVIP_4619.sql
@@ -0,0 +1,97 @@
+--/*
+--##########################################
+--## AUTOR=Viorel Remus Ovidiu
+--## FECHA_CREACION=20190621
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.2
+--## INCIDENCIA_LINK=REMVIP-4619
+--## PRODUCTO=NO
+--##
+--## Finalidad:  
+--## INSTRUCCIONES:
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
+SET DEFINE OFF;
+
+
+DECLARE
+    V_SQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar         
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+    V_TABLA VARCHAR2(25 CHAR):= 'ECO_EXPEDIENTE_COMERCIAL';
+    V_COUNT NUMBER(16); -- Vble. para contar.
+    V_COUNT_INSERT NUMBER(16):= 0; -- Vble. para contar inserts
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+    V_USUARIO VARCHAR2(32 CHAR):= 'REMVIP-4619';
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.
+    V_NUM_FILAS_1 NUMBER(16); -- Vble. para validar la existencia de un registro.
+    
+    ACT_NUM_ACTIVO NUMBER(16);
+    ACT_RECOVERY_ID_MALO NUMBER(16);
+    ACT_RECOVERY_ID_BUENO NUMBER(16);
+    
+    TYPE T_JBV IS TABLE OF VARCHAR2(32000);
+ 	TYPE T_ARRAY_JBV IS TABLE OF T_JBV; 
+
+ 	V_JBV T_ARRAY_JBV := T_ARRAY_JBV(
+		T_JBV('164877','06/06/19'),
+		T_JBV('164864','06/06/19'),
+		T_JBV('140578','06/06/19')
+	 	);   
+    V_TMP_JBV T_JBV;
+    
+    
+ BEGIN
+ 
+ FOR I IN V_JBV.FIRST .. V_JBV.LAST
+ LOOP
+    V_TMP_JBV := V_JBV(I);
+
+	V_SQL := 'SELECT COUNT(*) FROM '||V_ESQUEMA||'.ECO_EXPEDIENTE_COMERCIAL WHERE ECO_NUM_EXPEDIENTE = '||V_TMP_JBV(1)||'';
+	
+	EXECUTE IMMEDIATE V_SQL INTO V_NUM_FILAS_1;
+	
+	IF V_NUM_FILAS_1 = 1 THEN
+	
+	V_SQL := 'UPDATE '||V_ESQUEMA||'.'||V_TABLA||' SET 
+					ECO_FECHA_CONT_PROPIETARIO = TO_DATE('''||V_TMP_JBV(2)||''',''DD/MM/RR''),
+					USUARIOMODIFICAR = '''||V_USUARIO||''',
+					FECHAMODIFICAR = SYSDATE 
+				WHERE ECO_NUM_EXPEDIENTE = '||V_TMP_JBV(1)||'';	  
+	
+	  EXECUTE IMMEDIATE V_SQL;
+	  
+	  DBMS_OUTPUT.PUT_LINE('[INFO] Se ha actualizado el expediente '||V_TMP_JBV(1)||'');
+	  
+	  V_COUNT_INSERT := V_COUNT_INSERT + 1;
+
+	ELSE
+		
+		DBMS_OUTPUT.PUT_LINE('[INFO] REGISTRO NO EXISTE');
+		
+	END IF;
+  
+  END LOOP;
+  DBMS_OUTPUT.PUT_LINE('[INFO] Se han actualizado en total '||V_COUNT_INSERT||' registros en la tabla '||V_TABLA);
+  
+ COMMIT;
+ 
+EXCEPTION
+     WHEN OTHERS THEN
+          ERR_NUM := SQLCODE;
+          ERR_MSG := SQLERRM;
+          DBMS_OUTPUT.PUT_LINE('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(ERR_NUM));
+          DBMS_OUTPUT.PUT_LINE('-----------------------------------------------------------'); 
+          DBMS_OUTPUT.PUT_LINE(ERR_MSG);
+          ROLLBACK;
+          RAISE;   
+END;
+/
+EXIT;
+

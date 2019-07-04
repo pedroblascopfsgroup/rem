74f26beb9ddffaff06d0d457ae6cac4f0d229afe
adrian molina <adrian.molina@pfsgroup.es>
2019-07-04 12:19:31 +0200

    REMVIP-4186 - No se puede asignar proveedor

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/trabajo/TrabajoManager.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/trabajo/TrabajoManager.java
@@ -813,7 +813,8 @@ public class TrabajoManager extends BusinessOperationOverrider<TrabajoApi> imple
 				isFirstLoop = false;
 			}
 
-			if (DDTipoTrabajo.CODIGO_OBTENCION_DOCUMENTAL.equals(trabajo.getTipoTrabajo().getCodigo()))
+			if (DDTipoTrabajo.CODIGO_OBTENCION_DOCUMENTAL.equals(trabajo.getTipoTrabajo().getCodigo())
+					|| DDSubtipoTrabajo.CODIGO_AT_VERIFICACION_AVERIAS.equals(trabajo.getSubtipoTrabajo().getCodigo()))
 				trabajo.setEsTarificado(true);
 
 			if (activosAgrupacionTrabajo.size() > 0) {
@@ -988,7 +989,8 @@ public class TrabajoManager extends BusinessOperationOverrider<TrabajoApi> imple
 						trabajo.setFechaAprobacion(new Date());
 					}
 
-					if (DDTipoTrabajo.CODIGO_OBTENCION_DOCUMENTAL.equals(trabajo.getTipoTrabajo().getCodigo())) {
+					if (DDTipoTrabajo.CODIGO_OBTENCION_DOCUMENTAL.equals(trabajo.getTipoTrabajo().getCodigo()) 
+							|| DDSubtipoTrabajo.CODIGO_AT_VERIFICACION_AVERIAS.equals(trabajo.getSubtipoTrabajo().getCodigo())) {
 						trabajo.setEsTarificado(true);
 					}
 				}
@@ -1104,7 +1106,8 @@ public class TrabajoManager extends BusinessOperationOverrider<TrabajoApi> imple
 			ActivoTrabajo activoTrabajo = createActivoTrabajo(activo, trabajo, dtoTrabajo.getParticipacion());
 			trabajo.getActivosTrabajo().add(activoTrabajo);
 
-			if (DDTipoTrabajo.CODIGO_OBTENCION_DOCUMENTAL.equals(trabajo.getTipoTrabajo().getCodigo()))
+			if (DDTipoTrabajo.CODIGO_OBTENCION_DOCUMENTAL.equals(trabajo.getTipoTrabajo().getCodigo())
+					|| DDSubtipoTrabajo.CODIGO_AT_VERIFICACION_AVERIAS.equals(trabajo.getSubtipoTrabajo().getCodigo()))
 				trabajo.setEsTarificado(true);
 
 			// El gestor de activo se salta tareas de estos trámites y por tanto

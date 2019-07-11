839c4583783ea540bb04e52eb030700b354d90e2
Vicente Martinez <vicente.martinez@pfsgroup.es>
2019-07-11 15:07:18 +0200

    HREOS-6925 - Mapeo Subtipos Documento Expediente

---
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/model/dd/DDSubtipoDocumentoProveedor.java
@@ -0,0 +1,134 @@
+package es.pfsgroup.plugin.rem.model.dd;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.Where;
+
+import es.capgemini.pfs.auditoria.Auditable;
+import es.capgemini.pfs.auditoria.model.Auditoria;
+import es.capgemini.pfs.diccionarios.Dictionary;
+
+/**
+ * Modelo que gestiona el diccionario de carteras.
+ * 
+ * @author Benjamín Guerrero
+ *
+ */
+@Entity
+@Table(name = "DD_SDP_SUBTIPO_DOC_PROVEEDOR", schema = "${entity.schema}")
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+@Where(clause=Auditoria.UNDELETED_RESTICTION)
+public class DDSubtipoDocumentoProveedor implements Auditable, Dictionary {
+
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "DD_SDP_ID")
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "DDSubtipoDocumentoProveedorGenerator")
+	@SequenceGenerator(name = "DDSubtipoDocumentoProveedorGenerator", sequenceName = "S_DD_SDP_SUBTIPO_DOC_PROVEEDOR")
+	private Long id;
+	
+	@ManyToOne
+    @JoinColumn(name = "DD_TDP_ID")
+    private DDTipoDocumentoProveedor tipoDocumentoProveedor; 
+	 
+	@Column(name = "DD_SDP_CODIGO")   
+	private String codigo;
+	 
+	@Column(name = "DD_SDP_DESCRIPCION")   
+	private String descripcion;
+	    
+	@Column(name = "DD_SDP_DESCRIPCION_LARGA")   
+	private String descripcionLarga;
+	
+	@Column(name = "DD_SDP_MATRICULA_GD")   
+	private String matricula;	
+	    
+	@Version   
+	private Long version;
+
+	@Embedded
+	private Auditoria auditoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public DDTipoDocumentoProveedor getTipoDocumentoProveedor() {
+		return tipoDocumentoProveedor;
+	}
+
+	public void setTipoDocumentoProveedor(DDTipoDocumentoProveedor tipoDocumentoProveedor) {
+		this.tipoDocumentoProveedor = tipoDocumentoProveedor;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getDescripcionLarga() {
+		return descripcionLarga;
+	}
+
+	public void setDescripcionLarga(String descripcionLarga) {
+		this.descripcionLarga = descripcionLarga;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public Long getVersion() {
+		return version;
+	}
+
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+
+	public Auditoria getAuditoria() {
+		return auditoria;
+	}
+
+	public void setAuditoria(Auditoria auditoria) {
+		this.auditoria = auditoria;
+	}
+
+}

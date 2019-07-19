1005ba91c383e73bc659e12a7e528e9a51ef1ddf
Guillermo Llido <guillermo.llido@pfsgroup.es>
2019-07-19 12:52:12 +0200

    [REMVIP-4813] ITREM-9377 Correo de aprobación con gestor sustituto

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/usuarioRem/UsuarioRemApiImpl.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/usuarioRem/UsuarioRemApiImpl.java
@@ -10,15 +10,30 @@ import es.capgemini.devon.beans.Service;
 import es.capgemini.pfs.users.domain.Usuario;
 import es.pfsgroup.commons.utils.Checks;
 import es.pfsgroup.commons.utils.dao.abm.GenericABMDao;
+import es.pfsgroup.commons.utils.dao.abm.GenericABMDao.Filter;
 import es.pfsgroup.commons.utils.dao.abm.GenericABMDao.FilterType;
+import es.pfsgroup.plugin.rem.adapter.RemUtils;
+import es.pfsgroup.plugin.rem.api.GestorActivoApi;
+import es.pfsgroup.plugin.rem.api.GestorExpedienteComercialApi;
+import es.pfsgroup.plugin.rem.model.Activo;
+import es.pfsgroup.plugin.rem.model.ExpedienteComercial;
 import es.pfsgroup.plugin.rem.model.GestorSustituto;
 
 @Service("usuarioRemApiImpl")
-public class UsuarioRemApiImpl implements UsuarioRemApi{
+public class UsuarioRemApiImpl implements UsuarioRemApi {
 
 	@Autowired
 	private GenericABMDao genericDao;
 	
+	@Autowired
+	private GestorActivoApi gestorActivoManager;
+	
+	@Autowired
+	private RemUtils remUtils;
+	
+	@Autowired
+	private GestorExpedienteComercialApi gestorExpedienteComercialApi;
+
 	public List<String> getGestorSustitutoUsuario(Usuario usuario) {
 
 		List<String> mailsPara = new ArrayList<String>();
@@ -29,11 +44,14 @@ public class UsuarioRemApiImpl implements UsuarioRemApi{
 			if (!Checks.esNulo(sustitutos)) {
 				for (GestorSustituto gestorSustituto : sustitutos) {
 					if (!Checks.esNulo(gestorSustituto)) {
-						if ((gestorSustituto.getFechaFin() == null || gestorSustituto.getFechaFin().after(new Date()) || gestorSustituto.getFechaFin().equals(new Date()))
-								&& (gestorSustituto.getFechaInicio().before(new Date()) || gestorSustituto.getFechaInicio().equals(new Date()))
+						if ((gestorSustituto.getFechaFin() == null || gestorSustituto.getFechaFin().after(new Date())
+								|| gestorSustituto.getFechaFin().equals(new Date()))
+								&& (gestorSustituto.getFechaInicio().before(new Date())
+										|| gestorSustituto.getFechaInicio().equals(new Date()))
 								&& !gestorSustituto.getAuditoria().isBorrado()) {
 
-							if (!Checks.esNulo(gestorSustituto.getUsuarioGestorSustituto()) || !Checks.esNulo(gestorSustituto.getUsuarioGestorSustituto().getEmail())) {
+							if (!Checks.esNulo(gestorSustituto.getUsuarioGestorSustituto())
+									|| !Checks.esNulo(gestorSustituto.getUsuarioGestorSustituto().getEmail())) {
 								mailsPara.add(gestorSustituto.getUsuarioGestorSustituto().getEmail());
 							}
 						}
@@ -44,9 +62,9 @@ public class UsuarioRemApiImpl implements UsuarioRemApi{
 
 		return mailsPara;
 	}
-	
+
 	public List<String> getApellidoNombreSustituto(Usuario usuario) {
-		
+
 		List<String> apellidoNombreSus = new ArrayList<String>();
 
 		List<GestorSustituto> sustitutos = genericDao.getList(GestorSustituto.class,
@@ -55,11 +73,14 @@ public class UsuarioRemApiImpl implements UsuarioRemApi{
 			if (!Checks.esNulo(sustitutos)) {
 				for (GestorSustituto gestorSustituto : sustitutos) {
 					if (!Checks.esNulo(gestorSustituto)) {
-						if ((gestorSustituto.getFechaFin() == null || gestorSustituto.getFechaFin().after(new Date()) || gestorSustituto.getFechaFin().equals(new Date()))
-								&& (gestorSustituto.getFechaInicio().before(new Date()) || gestorSustituto.getFechaInicio().equals(new Date()))
+						if ((gestorSustituto.getFechaFin() == null || gestorSustituto.getFechaFin().after(new Date())
+								|| gestorSustituto.getFechaFin().equals(new Date()))
+								&& (gestorSustituto.getFechaInicio().before(new Date())
+										|| gestorSustituto.getFechaInicio().equals(new Date()))
 								&& !gestorSustituto.getAuditoria().isBorrado()) {
 
-							if (!Checks.esNulo(gestorSustituto.getUsuarioGestorSustituto()) || !Checks.esNulo(gestorSustituto.getUsuarioGestorSustituto().getEmail())) {
+							if (!Checks.esNulo(gestorSustituto.getUsuarioGestorSustituto())
+									|| !Checks.esNulo(gestorSustituto.getUsuarioGestorSustituto().getEmail())) {
 								apellidoNombreSus.add(gestorSustituto.getUsuarioGestorSustituto().getApellidoNombre());
 							}
 						}
@@ -67,9 +88,61 @@ public class UsuarioRemApiImpl implements UsuarioRemApi{
 				}
 			}
 		}
-		
+
 		return apellidoNombreSus;
-		
+
+	}
+	
+	public void rellenaListaCorreos(ExpedienteComercial expediente, String codigoTipo, List<String> mailsPara, List<String> mailsCC,
+			boolean addDirector) {
+		Usuario gestorExpediente = gestorExpedienteComercialApi.getGestorByExpedienteComercialYTipo(expediente,codigoTipo);
+		rellenaListaCorreos(gestorExpediente, codigoTipo, mailsPara, mailsCC, addDirector);
 	}
 
+	public void rellenaListaCorreos(Activo activo, String codigoTipo, List<String> mailsPara, List<String> mailsCC,
+			boolean addDirector) {
+		Usuario gestorActivo = gestorActivoManager.getGestorByActivoYTipo(activo, codigoTipo);
+		rellenaListaCorreos(gestorActivo, codigoTipo, mailsPara, mailsCC, addDirector);
+	}
+	
+	public void rellenaListaCorreos(Usuario usuarioGestor, String codigoTipo, List<String> mailsPara, List<String> mailsCC,
+			boolean addDirector) {
+		List<String> mailsConsulta = getGestorSustitutoUsuario(usuarioGestor);
+		if (!addDirector) {
+			if (!Checks.estaVacio(mailsConsulta)) {
+				mailsPara.addAll(mailsConsulta);
+				if (usuarioGestor.getEmail() != null) {
+					mailsCC.add(usuarioGestor.getEmail());
+				}
+			} else {
+				if (usuarioGestor.getEmail() != null) {
+					mailsPara.add(usuarioGestor.getEmail());
+				}
+			}
+		} else {
+			Usuario directorEquipo = gestorActivoManager.getDirectorEquipoByGestor(usuarioGestor);
+			if (directorEquipo != null) {
+				if (!Checks.estaVacio(mailsConsulta)) {
+					mailsPara.addAll(mailsConsulta);
+					if (directorEquipo.getEmail() != null) {
+						mailsCC.add(directorEquipo.getEmail());
+					}
+				} else {
+					if (usuarioGestor.getEmail() != null) {
+						mailsPara.add(directorEquipo.getEmail());
+					}
+				}
+			}
+		}
+		
+	}
+	
+	public void rellenaListaCorreosPorDefecto(String codigoTipo, List<String> mailsPara){
+		Filter filtroUsuarioPorDefecto = null;
+		filtroUsuarioPorDefecto = genericDao.createFilter(FilterType.EQUALS ,"username", remUtils.obtenerUsuarioPorDefecto(codigoTipo));
+		Usuario usuPorDefecto = genericDao.get(Usuario.class, filtroUsuarioPorDefecto);
+		if (!Checks.esNulo(usuPorDefecto) && !Checks.esNulo(usuPorDefecto.getEmail())) {
+			mailsPara.add(usuPorDefecto.getEmail());
+		}
+	}
 }

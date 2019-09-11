8f881e02915ae8ba499ca4248c43ecc571c8e819
JinLi Hu <jin.li@pfsgroup.es>
2019-09-11 10:37:26 +0200

    HREOS-7598, Cuando no calcula el comite no deja avanzar la Tarea

--- proyecto-rem-online/sencha-app/classic/src/view/agenda/TareaGenerica.js
+++ proyecto-rem-online/sencha-app/classic/src/view/agenda/TareaGenerica.js
@@ -1228,14 +1228,34 @@ Ext.define('HreRem.view.agenda.TareaGenerica', {
 	    				 if(ResofertaPrincipal == "true"){
 	    						
 	    						me.ocultarCampo(comite);
+	    						me.campoNoObligatorio(comite);
 	    						me.desocultarCampo(comitePropuesto);
+	    						me.campoNoObligatorio(comitePropuesto);
 	    						me.desocultarCampo(importeTotalOfertaAgrupada);
 	    						me.bloquearCampo(me.down('[name=importeTotalOfertaAgrupada]'));
 	    						me.desocultarCampo(huecoVenta);  	
 	    				 }
 	    			}
 	    	});	
-	    
+	    	
+	    	var url = $AC.getRemoteUrl('agenda/isOfertaIndividual');
+	    	Ext.Ajax.request({
+	    			url:url,
+	    			params: {idExpediente : idExp},
+	    			success: function(response,opts){
+	    				 var ResofertaIndividual = Ext.JSON.decode(response.responseText).ofertaIndividual;
+	    				 if(ResofertaIndividual == "true"){
+	    					 	me.ocultarCampo(comite);
+	    					 	me.campoNoObligatorio(comite);
+	    						me.desocultarCampo(comitePropuesto);
+	    						me.campoNoObligatorio(comitePropuesto);
+	    						me.desocultarCampo(importeTotalOfertaAgrupada);
+	    						me.bloquearCampo(me.down('[name=importeTotalOfertaAgrupada]'));
+	    						me.desocultarCampo(huecoVenta);
+	    				 }
+	    			}
+	    	});
+	    	
 	    	var url = $AC.getRemoteUrl('agenda/isOfertaDependiente');
 	    	Ext.Ajax.request({
 	    			url:url,

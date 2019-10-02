9bdaf57b49c95366a3f56b99d1fe00a93224ed44
Alejandro Valverde <alejandro.valverde@pfsgroup.es>
2019-10-02 14:13:57 +0200

    Revert "HREOS-7494 REM-DIVARIAN: Diferenciar entre cartera ARROW y REMAINING - Modelo de datos - Cambio de cartera a subcartera"
    This reverts commit 8f422cbb5c7fc3d9c5518f8865492188c1d34f56.

--- sql/9.2/rem/DML_016_REM01_DD_CRA_CARTERA.sql
+++ sql/9.2/rem/DML_016_REM01_DD_CRA_CARTERA.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=Daniel Algaba
---## FECHA_CREACION=20190906
+--## FECHA_CREACION=20190902
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.2
 --## INCIDENCIA_LINK=HREOS-7494
@@ -37,7 +37,7 @@ DECLARE
     TYPE T_TIPO_DATA IS TABLE OF VARCHAR2(150);
     TYPE T_ARRAY_DATA IS TABLE OF T_TIPO_DATA;
     V_TIPO_DATA T_ARRAY_DATA := T_ARRAY_DATA(
-        T_TIPO_DATA('07','150', 'Divarian', 'Divarian')
+        T_TIPO_DATA('16', 'Divarian', 'Divarian')
     ); 
 
 
@@ -49,44 +49,26 @@ BEGIN
 
          
     -- LOOP para insertar los valores en DD_CRA_CARTERA -----------------------------------------------------------------
-    DBMS_OUTPUT.PUT_LINE('[INFO]: INSERCION EN DD_SCR_SUBCARTERA ');
+    DBMS_OUTPUT.PUT_LINE('[INFO]: INSERCION EN DD_CRA_CARTERA ');
     FOR I IN V_TIPO_DATA.FIRST .. V_TIPO_DATA.LAST
       LOOP
       
         V_TMP_TIPO_DATA := V_TIPO_DATA(I);
     
         --Comprobamos el dato a insertar
-        V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.DD_CRA_CARTERA WHERE DD_CRA_CODIGO = ''16''';
+        V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.DD_CRA_CARTERA WHERE DD_CRA_CODIGO = '''||TRIM(V_TMP_TIPO_DATA(1))||'''';
+        DBMS_OUTPUT.PUT_LINE(V_SQL);
         EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
         
         --Si existe lo modificamos
         IF V_NUM_TABLAS > 0 THEN                                
           
-          V_MSQL := 'UPDATE '||V_ESQUEMA||'.DD_CRA_CARTERA 
-                      SET USUARIOBORRAR = ''HREOS-7494'' 
-                      , FECHABORRAR = SYSDATE
-                      , BORRADO = 1
-                      WHERE DD_CRA_CODIGO = ''16''';
-          EXECUTE IMMEDIATE V_MSQL;
-
-        END IF;
-
-        --Comprobamos el dato a insertar
-        V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.DD_SCR_SUBCARTERA WHERE DD_SCR_CODIGO = '''||TRIM(V_TMP_TIPO_DATA(2))||'''';
-        DBMS_OUTPUT.PUT_LINE(V_SQL);
-        EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
-        
-        --Si existe lo modificamos
-        IF V_NUM_TABLAS > 0 THEN    
-
-          DBMS_OUTPUT.PUT_LINE('[INFO]: MODIFICAMOS EL REGISTRO '''|| TRIM(V_TMP_TIPO_DATA(2)) ||'''');
-          V_MSQL := 'UPDATE '|| V_ESQUEMA ||'.DD_SCR_SUBCARTERA '||
-                    'SET DD_SCR_DESCRIPCION = '''||TRIM(V_TMP_TIPO_DATA(3))||'''
-                            , DD_SCR_DESCRIPCION_LARGA = '''||TRIM(V_TMP_TIPO_DATA(4))||'''
-                            , USUARIOMODIFICAR = ''HREOS-7494'' 
-                            , FECHAMODIFICAR = SYSDATE 
-                            , DD_CRA_ID = (SELECT DD_CRA_ID FROM DD_CRA_CARTERA WHERE DD_CRA_CODIGO = '''||TRIM(V_TMP_TIPO_DATA(1))||''') 
-                            WHERE DD_SCR_CODIGO = '''||TRIM(V_TMP_TIPO_DATA(2))||'''';
+          DBMS_OUTPUT.PUT_LINE('[INFO]: MODIFICAMOS EL REGISTRO '''|| TRIM(V_TMP_TIPO_DATA(1)) ||'''');
+          V_MSQL := 'UPDATE '|| V_ESQUEMA ||'.DD_CRA_CARTERA '||
+                    'SET DD_CRA_DESCRIPCION = '''||TRIM(V_TMP_TIPO_DATA(2))||'''
+                            , DD_CRA_DESCRIPCION_LARGA = '''||TRIM(V_TMP_TIPO_DATA(3))||'''
+                            , USUARIOMODIFICAR = ''HREOS-7494'' , FECHAMODIFICAR = SYSDATE 
+                            WHERE DD_CRA_CODIGO = '''||TRIM(V_TMP_TIPO_DATA(1))||'''';
           EXECUTE IMMEDIATE V_MSQL;
           DBMS_OUTPUT.PUT_LINE('[INFO]: REGISTRO MODIFICADO CORRECTAMENTE');
           
@@ -94,17 +76,16 @@ BEGIN
        ELSE
        
           DBMS_OUTPUT.PUT_LINE('[INFO]: INSERTAMOS EL REGISTRO '''|| TRIM(V_TMP_TIPO_DATA(1)) ||'''');     
-          V_MSQL := 'INSERT INTO '|| V_ESQUEMA ||'.DD_SCR_SUBCARTERA (' ||
-                      'DD_SCR_ID, DD_CRA_ID, DD_SCR_CODIGO, DD_SCR_DESCRIPCION, DD_SCR_DESCRIPCION_LARGA, VERSION, USUARIOCREAR, FECHACREAR, BORRADO) ' ||
-                      'SELECT '|| V_ESQUEMA ||'.S_DD_SCR_SUBCARTERA.NEXTVAL, (SELECT DD_CRA_ID FROM DD_CRA_CARTERA WHERE DD_CRA_CODIGO = '''||TRIM(V_TMP_TIPO_DATA(1))||'''), '''||V_TMP_TIPO_DATA(2)||''', '''||TRIM(V_TMP_TIPO_DATA(3))||''', '''||TRIM(V_TMP_TIPO_DATA(4))||''', 0, ''HREOS-7494'', SYSDATE, 0 FROM DUAL';
-          DBMS_OUTPUT.PUT_LINE(V_MSQL);
+          V_MSQL := 'INSERT INTO '|| V_ESQUEMA ||'.DD_CRA_CARTERA (' ||
+                      'DD_CRA_ID, DD_CRA_CODIGO, DD_CRA_DESCRIPCION, DD_CRA_DESCRIPCION_LARGA, VERSION, USUARIOCREAR, FECHACREAR, BORRADO) ' ||
+                      'SELECT '|| V_ESQUEMA ||'.S_DD_CRA_CARTERA.NEXTVAL DD_CRA_ID, '''||V_TMP_TIPO_DATA(1)||''', '''||TRIM(V_TMP_TIPO_DATA(2))||''', '''||TRIM(V_TMP_TIPO_DATA(3))||''', 0, ''HREOS-7494'', SYSDATE, 0 FROM DUAL';
           EXECUTE IMMEDIATE V_MSQL;
           DBMS_OUTPUT.PUT_LINE('[INFO]: REGISTRO INSERTADO CORRECTAMENTE');
         
        END IF;
       END LOOP;
     COMMIT;
-    DBMS_OUTPUT.PUT_LINE('[FIN]: DICCIONARIO DD_SCR_SUBCARTERA ACTUALIZADO CORRECTAMENTE ');
+    DBMS_OUTPUT.PUT_LINE('[FIN]: DICCIONARIO DD_CRA_CARTERA ACTUALIZADO CORRECTAMENTE ');
    
 
 EXCEPTION

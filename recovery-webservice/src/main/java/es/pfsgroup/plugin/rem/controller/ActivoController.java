8129208248c432e8f9cc5dad0a2c92561f847103
Alejandro Valverde <alejandro.valverde@pfsgroup.es>
2019-10-02 18:24:33 +0200

    Revert "Revert "Correción error del REMVIP-5182""
    This reverts commit 191047abed6525453f9bb8d49ed4afb9362fc337.

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/controller/ActivoController.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/controller/ActivoController.java
@@ -2939,21 +2939,6 @@ public class ActivoController extends ParadiseJsonController {
 	
 	@SuppressWarnings("unchecked")
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView bloquearChecksComercializacion(String idActivo, Integer action, ModelMap model) {
-		Activo activo = activoDao.getActivoById(Long.parseLong(idActivo));
-		try{
-			model.put(RESPONSE_DATA_KEY, activoApi.bloquearChecksComercializacionActivo(activo, action));
-		} catch (Exception e) {
-			logger.error("error en activoController", e);
-			model.put(RESPONSE_SUCCESS_KEY, false);
-			model.put(RESPONSE_ERROR_KEY, e.getMessage());
-		}
-		
-		return createModelAndViewJson(model);
-	}
-	
-	@SuppressWarnings("unchecked")
-	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView getHistoricoTramitacionTitulo(Long id, ModelMap model) {
 		try {
 			model.put(RESPONSE_DATA_KEY, activoApi.getHistoricoTramitacionTitulo(id));

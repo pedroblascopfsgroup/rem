9b2f39b6814966453c28daaecfd6a268775d9bed
JinLi Hu <jin.li@pfsgroup.es>
2019-10-22 10:57:18 +0200

    HREOS-7997, cambios sobre la plantilla según la petición del cliente - BD

--- sql/9.3/rem/DML_062_REM01_DD_OPM_OPERACION_MASIVA.sql
+++ sql/9.3/rem/DML_062_REM01_DD_OPM_OPERACION_MASIVA.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=JIN LI HU
---## FECHA_CREACION=20191014
+--## FECHA_CREACION=20191016
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.5
 --## INCIDENCIA_LINK=HREOS-7997
@@ -35,7 +35,7 @@ DECLARE
     TYPE T_FUNCION IS TABLE OF VARCHAR2(150);
     TYPE T_ARRAY_FUNCION IS TABLE OF T_FUNCION;
     V_FUNCION T_ARRAY_FUNCION := T_ARRAY_FUNCION(
-	  T_FUNCION('MSDOCADM', 'Carga masiva documentación administrativa', 'Carga masiva documentación administrativa','MASIVO_DOCUMENTACION_ADMINISTRATIVA','s*, n*, n*, s, f, f, f, f, f, s, n')
+	  T_FUNCION('MSDOCADM', 'Carga masiva documentación administrativa', 'Carga masiva documentación administrativa','MASIVO_DOCUMENTACION_ADMINISTRATIVA','s*, n*, n*, s, f, f, f, f, f, s, n, s, n, n, n')
     ); 
     V_TMP_FUNCION T_FUNCION;
     V_MSQL_1 VARCHAR2(4000 CHAR);
@@ -54,9 +54,18 @@ BEGIN
 			EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
 			
 			-- Si existe la FUNCION
-			IF V_NUM_TABLAS > 0 THEN	  
-				DBMS_OUTPUT.PUT_LINE('[INFO] Ya existen los datos en la tabla '||V_ESQUEMA||'.DD_OPM_OPERACION_MASIVA...no se modifica nada.');
+			IF V_NUM_TABLAS > 0 THEN
+				V_MSQL_1 := 'UPDATE DD_OPM_OPERACION_MASIVA
+								SET DD_OPM_DESCRIPCION = '''||V_TMP_FUNCION(2)||''',
+								DD_OPM_DESCRIPCION_LARGA = '''||V_TMP_FUNCION(3)||''',
+								FUN_ID = (SELECT FUN_ID FROM '||V_ESQUEMA_M||'.FUN_FUNCIONES WHERE FUN_DESCRIPCION = '''||V_TMP_FUNCION(4)||'''),
+								DD_OPM_VALIDACION_FORMATO = '''||V_TMP_FUNCION(5)||''',
+								USUARIOMODIFICAR = ''HREOS-7997'',
+								FECHAMODIFICAR = SYSDATE
+							WHERE DD_OPM_CODIGO = '''||V_TMP_FUNCION(1)||'''';
 				
+				EXECUTE IMMEDIATE V_MSQL_1;
+				DBMS_OUTPUT.PUT_LINE('[INFO] Datos de la tabla '||V_ESQUEMA||'.DD_OPM_OPERACION_MASIVA actualizado correctamente.');
 			ELSE
 				V_MSQL_1 := 'INSERT INTO '||V_ESQUEMA||'.DD_OPM_OPERACION_MASIVA' ||
 							' (DD_OPM_ID, DD_OPM_CODIGO, DD_OPM_DESCRIPCION, DD_OPM_DESCRIPCION_LARGA, FUN_ID, VERSION, USUARIOCREAR, FECHACREAR, BORRADO, DD_OPM_VALIDACION_FORMATO)' || 

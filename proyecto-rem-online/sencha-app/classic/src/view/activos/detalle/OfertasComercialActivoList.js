2a3985cbfafd92715538b27acf3b975014223bb4
adrian molina <adrian.molina@pfsgroup.es>
2019-10-29 15:26:52 +0100

    Revert "REMVIP-5576 - Problema Periodos de transparencia de Bankia"
    This reverts commit 7a376c6e353b55486c7eec49ab473737e1c09997.

--- proyecto-rem-online/sencha-app/classic/src/view/activos/detalle/OfertasComercialActivoList.js
+++ proyecto-rem-online/sencha-app/classic/src/view/activos/detalle/OfertasComercialActivoList.js
@@ -310,7 +310,13 @@ Ext.define('HreRem.view.activos.detalle.OfertasComercialActivoList', {
 					}
 				}
 				if(activo.get('cambioEstadoPublicacion')){
-					me.fireEvent("warnToast", HreRem.i18n("msg.cambio.estado.publicacion"));
+					if($AU.userHasFunction(['CAMBIAR_ESTADO_OFERTA_BANKIA'])){
+						me.fireEvent("warnToast", HreRem.i18n("msg.cambio.estado.publicacion"));
+					}else{
+						me.fireEvent("errorToast", HreRem.i18n("msg.cambio.estado.publicacion"));
+						me.up('activosdetalle').lookupController().refrescarActivo(true);
+						return false;
+					}
 				}
 				
 			} 

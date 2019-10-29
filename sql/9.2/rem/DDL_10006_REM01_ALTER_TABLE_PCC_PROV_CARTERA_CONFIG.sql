cfd95777dffddb56088a805e5b7a98fa1c0c6cf3
Alejandro Valverde <alejandro.valverde@pfsgroup.es>
2019-10-29 14:56:04 +0100

    HREOS-7619 - GD REM Bloque Proveedores Modificacion PCC_PROV_CARTERA_CONFIG

--- sql/9.2/rem/DDL_10006_REM01_ALTER_TABLE_PCC_PROV_CARTERA_CONFIG.sql
+++ sql/9.2/rem/DDL_10006_REM01_ALTER_TABLE_PCC_PROV_CARTERA_CONFIG.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=Ivan Rubio
---## FECHA_CREACION=20190801
+--## FECHA_CREACION=20191029
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.2
 --## INCIDENCIA_LINK=HREOS-7149

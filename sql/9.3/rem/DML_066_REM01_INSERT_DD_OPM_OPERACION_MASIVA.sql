531a2997b1c8a4531eb8d62b88aad178a8f546f2
Julián Dolz <julian.dolz@pfsgroup.es>
2019-11-04 15:37:16 +0100

    HREOS-8177 Modificado DML validación carga masiva

--- sql/9.3/rem/DML_066_REM01_INSERT_DD_OPM_OPERACION_MASIVA.sql
+++ sql/9.3/rem/DML_066_REM01_INSERT_DD_OPM_OPERACION_MASIVA.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=JULIAN DOLZ
---## FECHA_CREACION=20191029
+--## FECHA_CREACION=20191104
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.3
 --## INCIDENCIA_LINK=HREOS-8172
@@ -37,7 +37,7 @@ DECLARE
     TYPE T_FUNCION IS TABLE OF VARCHAR2(150);
     TYPE T_ARRAY_FUNCION IS TABLE OF T_FUNCION;
     V_FUNCION T_ARRAY_FUNCION := T_ARRAY_FUNCION(
-	     T_FUNCION('CMIN', 'Carga masiva de inscripciones', 'Carga masiva de inscripciones','CARGA_MASIVA_INSCRIPCIONES','n*,s,f,f,f,f,f,s*')
+	     T_FUNCION('CMIN', 'Carga masiva de inscripciones', 'Carga masiva de inscripciones','CARGA_MASIVA_INSCRIPCIONES','n*,s,s,s,s,s,s,s*')
     ); 
     V_TMP_FUNCION T_FUNCION;
     V_PERFILES VARCHAR2(100 CHAR) := '%';  -- Cambiar por ALGÚN PERFIL para otorgar permisos a ese perfil.
@@ -59,7 +59,19 @@ BEGIN
 			
 			-- Si existe la FUNCION
 			IF V_NUM_TABLAS > 0 THEN	  
-				DBMS_OUTPUT.PUT_LINE('[INFO] Ya existen los datos en la tabla '||V_ESQUEMA||'.'||V_TABLA||'...no se modifica nada.');
+				DBMS_OUTPUT.PUT_LINE('[INFO] Ya existen los datos en la tabla '||V_ESQUEMA||'.'||V_TABLA||'.');
+        V_MSQL_1 := 'UPDATE '||V_ESQUEMA||'.'||V_TABLA||' SET ' ||
+        'DD_OPM_DESCRIPCION = '''||V_TMP_FUNCION(2)||''', ' ||
+        'DD_OPM_DESCRIPCION_LARGA = '''||V_TMP_FUNCION(3)||''', ' ||
+        'FUN_ID = (SELECT FUN_ID FROM '||V_ESQUEMA_M||'.FUN_FUNCIONES WHERE FUN_DESCRIPCION = '''||V_TMP_FUNCION(4)||'''), ' ||
+        'USUARIOMODIFICAR = '''||V_USUARIOCREAR||''', ' ||
+        'FECHAMODIFICAR = SYSDATE, ' ||
+        'DD_OPM_VALIDACION_FORMATO = '''||V_TMP_FUNCION(5)||''' ' ||
+        'WHERE DD_OPM_CODIGO = '''||V_TMP_FUNCION(1)||''' ';
+        DBMS_OUTPUT.PUT_LINE(V_MSQL_1);
+
+        EXECUTE IMMEDIATE V_MSQL_1;
+				DBMS_OUTPUT.PUT_LINE('[INFO] Datos de la tabla '||V_ESQUEMA||'.'||V_TABLA||' actualizados correctamente.');
 			ELSE
 				V_MSQL_1 := 'INSERT INTO '||V_ESQUEMA||'.'||V_TABLA||
 							' (DD_OPM_ID, DD_OPM_CODIGO, DD_OPM_DESCRIPCION, DD_OPM_DESCRIPCION_LARGA, FUN_ID, VERSION, USUARIOCREAR, FECHACREAR, BORRADO, DD_OPM_VALIDACION_FORMATO)' || 

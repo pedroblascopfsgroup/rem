2b7def5a91b153e2014a42bd96e697444adcced8
Ramon Llinares <ramon.llinares@pfsgroup.es>
2019-11-19 16:11:51 +0100

    HREOS-0000

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/listener/RemContextListener.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/listener/RemContextListener.java
@@ -1,9 +1,9 @@
 package es.pfsgroup.plugin.rem.listener;
 
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.Properties;
 
+import javax.naming.ConfigurationException;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -13,8 +13,8 @@ import org.apache.commons.logging.LogFactory;
 public class RemContextListener implements ServletContextListener {
 
 	private final Log logger = LogFactory.getLog(getClass());
-
 	
+	private static final String DEVON_PROPERTIE = "devonProperties";
 
 	@Override
 	public void contextDestroyed(ServletContextEvent arg0) {
@@ -25,18 +25,33 @@ public class RemContextListener implements ServletContextListener {
 	public void contextInitialized(ServletContextEvent servletContextEvent) {
 		Properties appProperties = new Properties();
 		try {
-			if(System.getenv("DEVON_HOME") != null && !System.getenv("DEVON_HOME").isEmpty()) {
-				appProperties
-				.load(new FileInputStream(System.getenv("DEVON_HOME").concat("/devon.properties")));
-				logger.error("################# INICIADOR DE PROPIEDADES PARA EL FRONTEND ############################ ${DEVON_HOME}");
-				servletContextEvent.getServletContext().setAttribute("devonProperties", appProperties);
-			}else {
-				logger.error("################# no se encuenta DEVON_HOME ############################");
+			if (servletContextEvent.getServletContext().getAttribute(RemContextListener.DEVON_PROPERTIE) == null) {
+				String path = getDevonPath();
+				logger.info("################# INICIADOR DE PROPIEDADES PARA EL FRONTEND ############################"
+						.concat(path));
+				appProperties.load(new FileInputStream(path));
+				servletContextEvent.getServletContext().setAttribute(RemContextListener.DEVON_PROPERTIE, appProperties);
 			}
-			
-		} catch (IOException e) {
-			logger.error(e.getMessage(),e);
+
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
 		}
+	}
+
+	private String getDevonPath() throws ConfigurationException {
+		String path = null;
+		if (System.getenv("DEVON_HOME") != null && !System.getenv("DEVON_HOME").isEmpty()) {
+
+			path = System.getenv("DEVON_HOME").concat("/devon.properties");
+
+			if (path.charAt(0) != '/') {
+				path = "/".concat(path);
+			}
+		} else {
+			throw new ConfigurationException("#################  DEVON_HOME no está configurada correctamente");
+		}
+
+		return path;
 	}
 
 }

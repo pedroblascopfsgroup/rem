889d9f8b22d57986826844250281fe93fc5c8117
Ramon Llinares <ramon.llinares@pfsgroup.es>
2019-11-19 09:57:03 +0100

    REMVIP-5788

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/expedienteComercial/ExpedienteComercialManager.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/expedienteComercial/ExpedienteComercialManager.java
@@ -10093,8 +10093,8 @@ public class ExpedienteComercialManager extends BusinessOperationOverrider<Exped
 			List<TareaExterna> tareasActivas = activoTramiteApi.getListaTareaExternaActivasByIdTramite(tramite.getId());
 			if (tareasActivas != null && !tareasActivas.isEmpty()) {
 				for (TareaExterna tarea : tareasActivas) {
-					if (tarea.getTareaProcedimiento().getCodigo()
-							.equals(ComercialUserAssigantionService.CODIGO_T013_VALIDACION_CLIENTES)) {
+					if (ComercialUserAssigantionService.CODIGO_T013_VALIDACION_CLIENTES
+							.equals(tarea.getTareaProcedimiento().getCodigo())) {
 						tarNot = tarea.getTareaPadre();
 						if (!Checks.esNulo(tarNot)) {
 							tarNot.setFechaFin(new Date());

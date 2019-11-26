f2636a0f36a8fb852e2567cb08044e1f24c6886b
Remus Ovidiu Viorel <ovidiu.remus@pfsgroup.es>
2019-11-26 16:01:23 +0100

    REMVIP-5853 - Insertar y actualizar mediador

--- sql/9.3/rem/solo_produccion/DML_00039_REM01_UPDATE_ACT_ICO_INFO_COMERCIAL.sql
+++ sql/9.3/rem/solo_produccion/DML_00039_REM01_UPDATE_ACT_ICO_INFO_COMERCIAL.sql
@@ -1,7 +1,7 @@
 --/*
 --###########################################
 --## AUTOR=Viorel Remus Ovidiu
---## FECHA_CREACION=20191126
+--## FECHA_CREACION=20191127
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.3
 --## INCIDENCIA_LINK=REMVIP-5853
@@ -26,6 +26,7 @@ DECLARE
     V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.
     V_NUM_FILAS NUMBER(16); -- Vble. para validar la existencia de un registro.
     V_NUM_FILAS_2 NUMBER(16); -- Vble. para validar la existencia de un registro.
+    V_NUM_FILAS_3 NUMBER(16); -- Vble. para validar la existencia de un registro.
     ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
     ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
     V_USR VARCHAR2(30 CHAR) := 'REMVIP-5853'; -- USUARIOCREAR/USUARIOMODIFICAR.
@@ -33,6 +34,7 @@ DECLARE
     ACT_ID NUMBER(16);
     MEDIADOR_ID NUMBER(16);
     V_COUNT_UPDATE NUMBER(16):= 0; -- Vble. para contar updates
+    V_COUNT_INSERT NUMBER(16):= 0; -- Vble. para contar updates
     
     TYPE T_JBV IS TABLE OF VARCHAR2(32000);
     TYPE T_ARRAY_JBV IS TABLE OF T_JBV; 
@@ -483,8 +485,16 @@ BEGIN
 	V_SQL := 'SELECT COUNT(*) FROM '||V_ESQUEMA||'.ACT_ICO_INFO_COMERCIAL WHERE ACT_ID = '||ACT_ID;
 	
 	EXECUTE IMMEDIATE V_SQL INTO V_NUM_FILAS;
+
+	V_SQL := 'SELECT COUNT(*) FROM '||V_ESQUEMA||'.ACT_ACTIVO WHERE ACT_ID = '||ACT_ID;
 	
-	IF V_NUM_FILAS > 0 THEN
+	EXECUTE IMMEDIATE V_SQL INTO V_NUM_FILAS_2;
+
+	V_SQL := 'SELECT COUNT(*) FROM '||V_ESQUEMA||'.ACT_PVE_PROVEEDOR WHERE PVE_ID = '||MEDIADOR_ID;
+	
+	EXECUTE IMMEDIATE V_SQL INTO V_NUM_FILAS_3;	
+	
+	IF V_NUM_FILAS > 0 AND V_NUM_FILAS_2 > 0 AND V_NUM_FILAS_3 > 0 THEN
 	
 		V_MSQL := 'UPDATE '||V_ESQUEMA||'.ACT_ICO_INFO_COMERCIAL 
 				   SET ICO_MEDIADOR_ID = '||MEDIADOR_ID||',
@@ -498,9 +508,31 @@ BEGIN
 		
 		V_COUNT_UPDATE := V_COUNT_UPDATE + 1;
 		
-	ELSE
+	ELSIF V_NUM_FILAS = 0 AND V_NUM_FILAS_2 > 0 AND V_NUM_FILAS_3 > 0 THEN
 		
-		DBMS_OUTPUT.PUT_LINE('[INFO] REGISTRO NO EXISTE');
+		V_MSQL := 'INSERT INTO '||V_ESQUEMA||'.ACT_ICO_INFO_COMERCIAL (ICO_ID,
+										ACT_ID,
+										BORRADO,
+										USUARIOCREAR,
+										FECHACREAR,
+										VERSION)
+					   VALUES (
+						   REM01.S_ACT_ICO_INFO_COMERCIAL.NEXTVAL,
+						   '||ACT_ID||',
+						   0,
+						   '''||V_USR||''',
+						   SYSDATE, 
+						   0)';
+	
+		EXECUTE IMMEDIATE V_MSQL;
+    
+		DBMS_OUTPUT.PUT_LINE('[INFO] REGISTRO CON ACT_ID: '||ACT_ID||' INSERTADO');
+
+		V_COUNT_INSERT := V_COUNT_INSERT + 1;
+
+	ELSE
+
+		DBMS_OUTPUT.PUT_LINE('[INFO] NO EXISTE REGISTRO');
 		
 	END IF;
 	
@@ -508,7 +540,8 @@ BEGIN
 		
 	COMMIT;
 	
-	DBMS_OUTPUT.PUT_LINE('[FIN] Se han updateado en total '||V_COUNT_UPDATE||' registros');
+	DBMS_OUTPUT.PUT_LINE('[FIN] Se han UPDATEADO en total '||V_COUNT_UPDATE||' registros');
+	DBMS_OUTPUT.PUT_LINE('[FIN] Se han INSERTADO en total '||V_COUNT_INSERT||' registros');
  
 EXCEPTION
      WHEN OTHERS THEN

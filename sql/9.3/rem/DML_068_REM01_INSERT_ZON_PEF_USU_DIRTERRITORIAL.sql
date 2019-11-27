9d6de6c11229071c746173c1b11dfa65a1f09ed2
JinLi Hu <jin.li@pfsgroup.es>
2019-11-27 15:01:21 +0100

    HREOS-8604, Cambios en el flujo Comercial - Direcciones comerciales: perfil y función

---
+++ sql/9.3/rem/DML_068_REM01_INSERT_ZON_PEF_USU_DIRTERRITORIAL.sql
@@ -0,0 +1,99 @@
+--/*
+--##########################################
+--## AUTOR=JIN LI HU
+--## FECHA_CREACION=20191125
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=HREOS-8604
+--## PRODUCTO=NO
+--##
+--## Finalidad: Actualizar instrucciones
+--## INSTRUCCIONES: 
+--## VERSIONES:
+--##        0.1 Version inicial
+--##########################################
+--*/
+
+--Para permitir la visualización de texto en un bloque PL/SQL utilizando DBMS_OUTPUT.PUT_LINE
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
+SET DEFINE OFF;
+
+
+DECLARE
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar     
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.   
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+	V_ID NUMBER(16); -- Vble. auxiliar para almacenar temporalmente el numero de la sequencia.
+	V_TEXT_TABLA VARCHAR2(2400 CHAR) := 'ZON_PEF_USU'; -- Vble. auxiliar para almacenar el nombre de la tabla de ref.
+
+    TYPE T_TIPO_DATA IS TABLE OF VARCHAR2(32000 CHAR);
+    TYPE T_ARRAY_DATA IS TABLE OF T_TIPO_DATA;
+    V_TIPO_DATA T_ARRAY_DATA := T_ARRAY_DATA(
+    	T_TIPO_DATA('ecobian'),
+    	T_TIPO_DATA('imartin')
+    ); 
+    V_TMP_TIPO_DATA T_TIPO_DATA;
+BEGIN
+DBMS_OUTPUT.PUT_LINE('[INICIO]');
+
+
+    -- LOOP para insertar los valores --
+    DBMS_OUTPUT.PUT_LINE('[INFO]: INSERCION EN '||V_TEXT_TABLA);
+    FOR I IN V_TIPO_DATA.FIRST .. V_TIPO_DATA.LAST
+      LOOP
+        V_TMP_TIPO_DATA := V_TIPO_DATA(I);
+        --Comprobar el dato a insertar.
+        V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.'||V_TEXT_TABLA||' 
+					WHERE USU_ID = (SELECT USU_ID FROM '||V_ESQUEMA_M||'.USU_USUARIOS WHERE USU_USERNAME = '''||TRIM(V_TMP_TIPO_DATA(1))||''')  
+					AND PEF_ID = (SELECT PEF_ID FROM '||V_ESQUEMA||'.PEF_PERFILES WHERE PEF_CODIGO = ''DIRTERRITORIAL'')';
+        EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
+        IF V_NUM_TABLAS = 1 THEN
+        DBMS_OUTPUT.PUT_LINE('[INFO]: El usuario '''||TRIM(V_TMP_TIPO_DATA(1))||''' ya tiene el perfil DIRTERRITORIAL asignado');
+       ELSE
+       	-- Si no existe se inserta.
+          V_MSQL := 'SELECT '||V_ESQUEMA||'.S_'||V_TEXT_TABLA||'.NEXTVAL FROM DUAL';
+          EXECUTE IMMEDIATE V_MSQL INTO V_ID;
+          V_MSQL := 'INSERT INTO '||V_ESQUEMA||'.'||V_TEXT_TABLA||' (
+					  ZON_ID,
+					  PEF_ID,
+					  USU_ID,
+					  ZPU_ID,
+					  VERSION,
+					  USUARIOCREAR,
+					  FECHACREAR,
+					  BORRADO) VALUES (
+                      19504,
+                      (SELECT PEF_ID FROM '||V_ESQUEMA||'.PEF_PERFILES WHERE PEF_CODIGO = ''DIRTERRITORIAL''),
+                      (SELECT USU_ID FROM '||V_ESQUEMA_M||'.USU_USUARIOS WHERE USU_USERNAME = '''||TRIM(V_TMP_TIPO_DATA(1))||'''),
+                      '||V_ID||',
+                      0,
+                      ''HREOS-8604'',
+                      SYSDATE,
+                      0
+                      )';
+          EXECUTE IMMEDIATE V_MSQL;
+          DBMS_OUTPUT.PUT_LINE('[INFO]: Se le ha asignado el perfil DIRTERRITORIAL al usuario '''||TRIM(V_TMP_TIPO_DATA(1))||'''');
+
+       END IF;
+      END LOOP;
+    COMMIT;
+    DBMS_OUTPUT.PUT_LINE('[FIN]: TABLA '||V_TEXT_TABLA||' ACTUALIZADO CORRECTAMENTE ');
+
+EXCEPTION
+     WHEN OTHERS THEN
+          ERR_NUM := SQLCODE;
+          ERR_MSG := SQLERRM;
+          DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(ERR_NUM));
+          DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+          DBMS_OUTPUT.put_line(ERR_MSG);
+          ROLLBACK;
+          RAISE;   
+END;
+/
+EXIT;

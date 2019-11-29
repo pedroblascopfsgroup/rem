d6453b110f613a3010578cecfaebe5d79d6f2544
Juan Beltrán <juan.beltran@pfsgroup.es>
2019-11-29 13:36:33 +0100

    REMVIP-5757 GC

--- sql/9.2/rem/DML_1049_REM01_INSERT_APR_AUX_MAPEO_DD_SAC.sql
+++ sql/9.2/rem/DML_1049_REM01_INSERT_APR_AUX_MAPEO_DD_SAC.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=Juan Beltrán
---## FECHA_CREACION=20191129
+--## FECHA_CREACION=20191130
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.2
 --## INCIDENCIA_LINK=REMVIP-5757

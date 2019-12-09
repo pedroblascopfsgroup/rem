a7faa7d6a5c05a15bff651feab0a7f2824145b01
vicente.oron <vicente.oron@pfsgroup.es>
2019-12-09 15:20:42 +0100

    GC-16847 arreglo descarga de repo para inte2

--- proyecto-rem-online/dev-ops/pipeline/despliegue-continuo-inte2.groovy
+++ proyecto-rem-online/dev-ops/pipeline/despliegue-continuo-inte2.groovy
@@ -128,7 +128,6 @@ pipeline {
 
                 // Esto es necesario porque sino no descarga bien los módulos
                 // no se el porqué.
-                sh script: "git rm fwk"
                 
                 echo "Git init Submodules"
                 sh script: "bash ./proyecto-rem-online/dev-ops/common-git-submodule-init.sh ${GIT_USER}"

cf2f94177fdd81dfa9d225182b649615936b66c5
Vicente Martinez <vicente.martinez@pfsgroup.es>
2019-12-19 13:34:39 +0100

    HREOS-8609 - Suprimir imports + arreglo error sencha

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/controller/TramitacionOfertasController.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/controller/TramitacionOfertasController.java
@@ -11,10 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import es.pfsgroup.commons.utils.Checks;
 import es.pfsgroup.framework.paradise.controller.ParadiseJsonController;
 import es.pfsgroup.framework.paradise.utils.JsonViewerException;
-import es.pfsgroup.plugin.rem.adapter.AgrupacionAdapter;
 import es.pfsgroup.plugin.rem.api.TramitacionOfertasApi;
 import es.pfsgroup.plugin.rem.model.DtoOfertaActivo;
 

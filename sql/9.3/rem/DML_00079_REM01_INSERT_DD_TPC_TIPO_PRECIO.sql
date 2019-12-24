f160216221d6fff2f529a47ea74896b41254cdf9
Joaquin Bahamonde <joaquin.bahamonde@pfsgroup.es>
2019-12-24 11:48:30 +0100

    HREOS-8900 AÑADIDOS NUEVOS TIPOS DE PRECIO

---
+++ sql/9.3/rem/DML_00079_REM01_INSERT_DD_TPC_TIPO_PRECIO.sql
@@ -0,0 +1,117 @@
+--/*
+--##########################################
+--## AUTOR=Joaquin Bahamonde
+--## FECHA_CREACION=20191224
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=HREOS-8900
+--## PRODUCTO=NO
+--##
+--## Finalidad: Script que añade en DD_TPC_TIPO_PRECIO los datos añadidos en T_ARRAY_DATA
+--## INSTRUCCIONES:
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF;
+
+
+DECLARE
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar     
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.   
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+    V_ID NUMBER(16);
+    V_TEXT_TABLA VARCHAR2(2400 CHAR) := 'DD_TPC_TIPO_PRECIO'; -- Vble. auxiliar para almacenar el nombre de la tabla de ref.
+    V_TEXT_CHARS VARCHAR2(2400 CHAR) := 'TPC'; -- Vble. auxiliar para almacenar las 3 letras orientativas de la tabla de ref.
+    V_INCIDENCIA VARCHAR2(25 CHAR) := 'HREOS-8900';
+    V_ID_TIPO_PRECIO NUMBER(16); --Vble para extraer el ID del registro a modificar, si procede.
+
+    
+    
+    TYPE T_TIPO_DATA IS TABLE OF VARCHAR2(150);
+    TYPE T_ARRAY_DATA IS TABLE OF T_TIPO_DATA;
+    V_TIPO_DATA T_ARRAY_DATA := T_ARRAY_DATA(
+        T_TIPO_DATA('26', 'Final Sale Value venta', 'Final Sale Value (FSV) venta'),
+        T_TIPO_DATA('27', '	Final Sale Value renta', 'Final Sale Value (FSV) renta')
+		); 
+    V_TMP_TIPO_DATA T_TIPO_DATA;
+    
+BEGIN	
+	
+	DBMS_OUTPUT.PUT_LINE('[INICIO] ');
+
+	 
+    -- LOOP para insertar los valores en DD_TPC_TIPO_PRECIO -----------------------------------------------------------------
+    DBMS_OUTPUT.PUT_LINE('[INFO]: INSERCION EN DD_TPC_TIPO_PRECIO ');
+    FOR I IN V_TIPO_DATA.FIRST .. V_TIPO_DATA.LAST
+      LOOP
+      
+        V_TMP_TIPO_DATA := V_TIPO_DATA(I);
+    
+        
+        V_MSQL := 'SELECT DD_TPC_ID FROM '||V_ESQUEMA||'.'||V_TEXT_TABLA||' WHERE DD_TPC_CODIGO = '''||TRIM(V_TMP_TIPO_DATA(1))||'''';
+		    EXECUTE IMMEDIATE V_MSQL INTO V_ID_TIPO_PRECIO;
+        
+        --Comprobamos el dato a insertar
+        V_MSQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.'||V_TEXT_TABLA||' WHERE DD_TPC_CODIGO = '''||TRIM(V_TMP_TIPO_DATA(1))||'''';
+        EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;
+        
+             
+
+
+        DBMS_OUTPUT.PUT_LINE('[INFO NUMERO]: '||V_NUM_TABLAS||'');
+      DBMS_OUTPUT.PUT_LINE('[INFO]: LLEGAMOS AQUI');
+        --Si existe modificamos los valores
+        IF V_NUM_TABLAS > 0 THEN
+			    V_MSQL := 'UPDATE '|| V_ESQUEMA ||'.'||V_TEXT_TABLA||' '||
+          'SET DD_TPC_CODIGO = '''||TRIM(V_TMP_TIPO_DATA(1))||''''||
+		      ', DD_'||V_TEXT_CHARS||'_DESCRIPCION = '''||TRIM(V_TMP_TIPO_DATA(2))||''''|| 
+          ', DD_'||V_TEXT_CHARS||'_DESCRIPCION_LARGA = '''||TRIM(V_TMP_TIPO_DATA(3))||''''||
+          ', USUARIOMODIFICAR = '''||V_INCIDENCIA||''' , FECHAMODIFICAR = SYSDATE '||
+          'WHERE DD_'||V_TEXT_CHARS||'_ID = '''||V_ID_TIPO_PRECIO||'''';
+          EXECUTE IMMEDIATE V_MSQL;
+
+        ELSE
+          DBMS_OUTPUT.PUT_LINE('[INFO]: INSERTAMOS EL REGISTRO '''|| TRIM(V_TMP_TIPO_DATA(1)) ||'''');   
+          V_MSQL := 'SELECT '||V_ESQUEMA||'.S_DD_TPC_TIPO_PRECIO.NEXTVAL FROM DUAL';
+          EXECUTE IMMEDIATE V_MSQL INTO V_ID;	
+          
+          V_MSQL := 'INSERT INTO '||V_ESQUEMA||'.DD_TPC_TIPO_PRECIO (' ||
+                      'DD_TPC_ID, DD_TPC_CODIGO, DD_TPC_DESCRIPCION, DD_TPC_DESCRIPCION_LARGA, VERSION, USUARIOCREAR, FECHACREAR, BORRADO) ' ||
+                      'SELECT '||V_ID|| ','''||TRIM(V_TMP_TIPO_DATA(1))||''','''||TRIM(V_TMP_TIPO_DATA(2))||''','''||TRIM(V_TMP_TIPO_DATA(3))||''','||
+                      '0, '||V_INCIDENCIA||', SYSDATE, 0 FROM DUAL';
+          EXECUTE IMMEDIATE V_MSQL;
+          DBMS_OUTPUT.PUT_LINE('[INFO]: REGISTROS INSERTADO CORRECTAMENTE');
+        
+       END IF;
+      END LOOP;
+    COMMIT;
+
+    DBMS_OUTPUT.PUT_LINE('[FIN]: DICCIONARIO DD_TPC_TIPO_PRECIO MODIFICADO CORRECTAMENTE ');
+   
+
+EXCEPTION
+     WHEN OTHERS THEN
+          ERR_NUM := SQLCODE;
+          ERR_MSG := SQLERRM;
+
+          DBMS_OUTPUT.PUT_LINE('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(ERR_NUM));
+          DBMS_OUTPUT.PUT_LINE('-----------------------------------------------------------'); 
+          DBMS_OUTPUT.PUT_LINE(ERR_MSG);
+
+          ROLLBACK;
+          RAISE;          
+
+END;
+
+/
+
+EXIT

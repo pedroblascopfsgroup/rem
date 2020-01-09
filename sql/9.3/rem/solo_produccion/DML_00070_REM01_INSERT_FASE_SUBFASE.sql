178a94f862278f5d8aaa3001bc7e7b2e8a421a37
Remus Ovidiu Viorel <ovidiu.remus@pfsgroup.es>
2020-01-09 17:10:00 +0100

    REMVIP-6152 - Insertar regstros fases publicacion

---
+++ sql/9.3/rem/solo_produccion/DML_00070_REM01_INSERT_FASE_SUBFASE.sql
@@ -0,0 +1,85 @@
+--/*
+--#########################################
+--## AUTOR=Viorel Remus Ovidiu
+--## FECHA_CREACION=20200109
+--## ARTEFACTO=batch
+--## VERSION_ARTEFACTO=9.2
+--## INCIDENCIA_LINK=REMVIP-6999
+--## PRODUCTO=NO
+--## 
+--## Finalidad: Carga masiva de aprobación del informe comercial
+--##			
+--## INSTRUCCIONES:  
+--## VERSIONES:
+--##        0.1 Versión inicial
+--#########################################
+--*/
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF; 
+DECLARE
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar    
+    V_ESQUEMA VARCHAR2(25 CHAR):= 'REM01'; -- Configuracion Esquemas
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= 'REMMASTER'; -- Configuracion Esquema Master
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.
+    V_NUM_FILAS NUMBER(16); -- Vble. para validar la existencia de un registro.
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+    V_USR VARCHAR2(30 CHAR) := 'REMVIP-6152'; -- USUARIOCREAR/USUARIOMODIFICAR.
+    V_TABLA_AUX VARCHAR( 100 CHAR ) := 'AUX_REMVIP_6152';
+    V_COUNT NUMBER(16);		
+
+
+BEGIN		
+
+	DBMS_OUTPUT.PUT_LINE('[INICIO] INSERT FASE Y SUBFAS EPUBLICACION ');	
+
+	V_MSQL := ' MERGE INTO '||V_ESQUEMA||'.ACT_HFP_HIST_FASES_PUB T1
+		    USING( 
+			   SELECT ACT.ACT_ID, AUX.COD_FASE, AUX.COD_SUBFASE
+			   FROM '||V_ESQUEMA||'.ACT_ACTIVO ACT 
+               		   INNER JOIN '||V_ESQUEMA||'.AUX_REMVIP_6152 AUX ON ACT.ACT_NUM_ACTIVO = AUX.ACT_NUM_ACTIVO 
+			) T2
+		    ON ( T1.ACT_ID = T2.ACT_ID )
+		   WHEN NOT MATCHED THEN INSERT (T1.ACT_ID,
+		                                T1.DD_FSP_ID,
+		                                T1.DD_SFP_ID,
+		                                T1.USU_ID,
+		                                T1.HFP_FECHA_INI,
+		                                T1.VERSION,
+		                                T1.USUARIOCREAR,
+		                                T1.FECHACREAR,
+		                                T1.BORRADO)
+		                        VALUES (
+		                                T2.ACT_ID,
+		                                T2.COD_FASE,
+		                                T2.COD_SUBFASE,
+		                                29468,
+		                                SYSDATE,
+		                                1,
+		                                ''REMVIP-6999'',
+		                                SYSDATE,
+		                                0 
+		                                )';	
+	
+	EXECUTE IMMEDIATE V_MSQL;
+
+	DBMS_OUTPUT.PUT_LINE('[INFO] Se INSERTAN '||SQL%ROWCOUNT||' registros de ACT_HFP_HIST_FASES_PUB'); 	
+
+	COMMIT;
+	
+	DBMS_OUTPUT.PUT_LINE('[FIN]');
+ 
+EXCEPTION
+     WHEN OTHERS THEN
+          ERR_NUM := SQLCODE;
+          ERR_MSG := SQLERRM;
+          DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(ERR_NUM));
+          DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+          DBMS_OUTPUT.put_line(ERR_MSG);
+          ROLLBACK;
+          RAISE;   
+END;
+/
+EXIT;

5553b84c1e222602da8991e4ffbbad295aa78e17
Daniel Algaba <daniel.algaba@pfsgroup.es>
2020-01-31 15:45:10 +0100

    HREOS-9322 Nuevo modelo APIs - Controller - Parte Datos

---
+++ sql/9.3/rem/DDL_027_REM01_TMP_GCO_GCH.sql
@@ -0,0 +1,76 @@
+--/*
+--#########################################
+--## AUTOR=Daniel Algaba
+--## FECHA_CREACION=20190131
+--## ARTEFACTO=batch
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=HREOS-9322
+--## PRODUCTO=NO
+--## 
+--## Finalidad: Creación de la tabla 'TMP_GCO_GCH'
+--##			
+--## INSTRUCCIONES:  
+--## VERSIONES:
+--##        0.1 Versión inicial
+--#########################################
+--*/
+
+--Para permitir la visualización de texto en un bloque PL/SQL utilizando DBMS_OUTPUT.PUT_LINE
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
+SET DEFINE OFF;
+
+
+DECLARE
+
+TABLE_COUNT NUMBER(1,0) := 0;
+V_ESQUEMA_1 VARCHAR2(20 CHAR) := 'REM01';
+V_ESQUEMA_2 VARCHAR2(20 CHAR) := 'REMMASTER';
+V_TABLA VARCHAR2(40 CHAR) := 'TMP_GCO_GCH';
+V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar     
+
+BEGIN
+
+ V_MSQL := 'SELECT COUNT(1) FROM ALL_TABLES WHERE TABLE_NAME = '''||V_TABLA||''' AND OWNER = '''||V_ESQUEMA_1||'''';
+ EXECUTE IMMEDIATE V_MSQL INTO TABLE_COUNT;
+
+IF TABLE_COUNT > 0 THEN
+
+    DBMS_OUTPUT.PUT_LINE('[INFO] TABLA '||V_ESQUEMA_1||'.'||V_TABLA||' YA EXISTENTE. SE PROCEDE A BORRAR Y CREAR DE NUEVO.');
+    EXECUTE IMMEDIATE 'DROP TABLE '||V_ESQUEMA_1||'.'||V_TABLA||'';
+    
+END IF;
+
+EXECUTE IMMEDIATE 
+'CREATE TABLE '||V_ESQUEMA_1||'.'||V_TABLA||
+'(
+    ID NUMBER(16,2),
+	ECO_ID NUMBER(16,2),
+	GEE_ID NUMBER(16,2),
+    GEH_ID NUMBER(16,2)
+)'
+;
+
+DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA_1||'.'||V_TABLA||' CREADA');  
+
+IF V_ESQUEMA_2 != V_ESQUEMA_1 THEN
+
+	EXECUTE IMMEDIATE 'GRANT ALL ON "'||V_ESQUEMA_1||'"."'||V_TABLA||'" TO "'||V_ESQUEMA_2||'" WITH GRANT OPTION';
+	DBMS_OUTPUT.PUT_LINE('[INFO] PERMISOS SOBRE LA TABLA '||V_ESQUEMA_1||'.'||V_TABLA||' OTORGADOS A '||V_ESQUEMA_2||''); 
+
+END IF;
+
+EXCEPTION
+
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecucion:'||TO_CHAR(SQLCODE));
+        DBMS_OUTPUT.put_line('-----------------------------------------------------------');
+        DBMS_OUTPUT.put_line(SQLERRM);
+        ROLLBACK;
+        RAISE;
+END;
+/
+
+EXIT;
+
---
+++ sql/9.3/rem/DML_099_REM01_INSERT_GCO.sql
@@ -0,0 +1,75 @@
+--/*
+--#########################################
+--## AUTOR=Daniel Algaba
+--## FECHA_CREACION=20190131
+--## ARTEFACTO=batch
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=HREOS-9322
+--## PRODUCTO=NO
+--## 
+--## Finalidad: Aprovisionar la tabla 'TMP_GCO_GCH' con ECO_ID
+--##			
+--## INSTRUCCIONES:  
+--## VERSIONES:
+--##        0.1 Versión inicial
+--#########################################
+--*/
+
+--Para permitir la visualización de texto en un bloque PL/SQL utilizando DBMS_OUTPUT.PUT_LINE
+
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF;
+
+
+DECLARE
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar     
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.   
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+    V_ID NUMBER(16);
+    V_TEXT_TABLA VARCHAR2(2400 CHAR) := 'TMP_GCO_GCH'; -- Vble. auxiliar para almacenar el nombre de la tabla de ref.
+    V_INCIDENCIA VARCHAR2(25 CHAR) := 'HREOS-9322';
+    
+BEGIN	
+	
+	DBMS_OUTPUT.PUT_LINE('[INICIO] ');
+    
+          
+  V_MSQL := 'INSERT INTO REM01.GCO_GESTOR_ADD_ECO (ECO_ID, GEE_ID)
+  SELECT ECO_ID, GEE_ID FROM REM01.'||V_TEXT_TABLA||'';
+  EXECUTE IMMEDIATE V_MSQL;
+  DBMS_OUTPUT.PUT_LINE('[INFO] REGISTROS INSERTADOS CORRECTAMENTE EN GCO_GESTOR_ADD_ECO: ' || sql%rowcount);
+
+  V_MSQL := 'INSERT INTO REM01.GCH_GESTOR_ECO_HISTORICO (ECO_ID, GEH_ID)
+  SELECT ECO_ID, GEH_ID FROM REM01.'||V_TEXT_TABLA||'';
+  EXECUTE IMMEDIATE V_MSQL;
+  DBMS_OUTPUT.PUT_LINE('[INFO]: REGISTROS INSERTADO CORRECTAMENTE');
+  DBMS_OUTPUT.PUT_LINE('[INFO] REGISTROS INSERTADOS CORRECTAMENTE EN GCH_GESTOR_ECO_HISTORICO: ' || sql%rowcount);
+
+  V_MSQL := 'DROP TABLE REM01.TMP_GCO_GCH';
+  EXECUTE IMMEDIATE V_MSQL;
+   
+  COMMIT;   
+
+EXCEPTION
+     WHEN OTHERS THEN
+          ERR_NUM := SQLCODE;
+          ERR_MSG := SQLERRM;
+
+          DBMS_OUTPUT.PUT_LINE('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(ERR_NUM));
+          DBMS_OUTPUT.PUT_LINE('-----------------------------------------------------------'); 
+          DBMS_OUTPUT.PUT_LINE(ERR_MSG);
+
+          ROLLBACK;
+          RAISE;          
+
+END;
+
+/
+
+EXIT

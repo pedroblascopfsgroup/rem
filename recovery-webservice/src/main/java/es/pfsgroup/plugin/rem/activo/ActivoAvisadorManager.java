23e2ce985a7bf520eb5a50e7b96753ac661c4979
Ramon <ramon.llinares@pfsgroup.es>
2020-02-06 12:23:08 +0100

    HREOS-8223, Cambio literal aviso dnd

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/activo/ActivoAvisadorManager.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/activo/ActivoAvisadorManager.java
@@ -339,7 +339,7 @@ public class ActivoAvisadorManager implements ActivoAvisadorApi {
 		// Aviso 19: Activo perteneciente a agrupacion DND
 		if(!Checks.esNulo(activo) && !Checks.esNulo(activoApi.activoPerteneceDND(activo))) {
 			DtoAviso dtoAviso = new DtoAviso();
-			dtoAviso.setDescripcion("Activo incluido en un DND");
+			dtoAviso.setDescripcion("Activo En Curso de Obra por parte de Edificación");
 			dtoAviso.setId(String.valueOf(id));
 			listaAvisos.add(dtoAviso);
 		}

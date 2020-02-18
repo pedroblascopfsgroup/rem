339beb9214581caf5394650583ea838613f874d7
Lara Pablo <lara.pablo@pfsgroup.es>
2020-02-18 08:59:37 +0100

    HREOS-9459  version-3.1.0-rem  Arreglo a la hora de ver los trabajos en la pestaña gestión

--- sql/9.2/rem/procs_y_vistas/DDL_009_REM01_VI_BUSQUEDA_TRABAJOS.sql
+++ sql/9.2/rem/procs_y_vistas/DDL_009_REM01_VI_BUSQUEDA_TRABAJOS.sql
@@ -1,10 +1,10 @@
 --/*
 --##########################################
---## AUTOR=RLB
---## FECHA_CREACION=20191031
+--## AUTOR= Lara Pablo Flores
+--## FECHA_CREACION=20200218
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.2
---## INCIDENCIA_LINK=REMVIP-4784
+--## INCIDENCIA_LINK=HREOS-9459
 --## PRODUCTO=SI
 --## Finalidad: DDL
 --##           
@@ -16,6 +16,7 @@
 --##        0.4 Deshacemos cambios de 0.2 y 0.3
 --##		0.5 Añadimos la subcartera
 --##		0.6 Añadimos columna para saber si un trabajo está en otro gasto
+--##		0.7 Añadimos idActivo, numActivo, numAgrupacion
 --##########################################
 --*/
 
@@ -60,6 +61,7 @@ BEGIN
 		SELECT /*+ leading(rn act agr) use_hash(act) use_hash(agr) */
 			DISTINCT
 			tbj.tbj_id, 
+			act.act_id AS idactivo,
 			1 as rango,
 			tbj.tbj_num_trabajo, 
 			tbj.tbj_webcom_id, 
@@ -85,6 +87,7 @@ BEGIN
 			ddprv.dd_prv_codigo, 
 			ddprv.dd_prv_descripcion AS provincia, 
 			bieloc.bie_loc_cod_post AS codpostal, 
+			act.act_num_activo AS numactivo,
 			agr.agr_num_agrup_rem AS numagrupacion, 
 			cra.dd_cra_codigo AS cartera,
 			scr.dd_scr_codigo AS subcartera,

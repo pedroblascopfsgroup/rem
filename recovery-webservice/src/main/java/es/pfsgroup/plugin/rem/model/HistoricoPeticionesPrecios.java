8c4cad4547b199031fffbf6d8fc6c48a7bdeb776
Joaquin Bahamonde <joaquin.bahamonde@pfsgroup.es>
2020-02-24 13:41:10 +0100

    HREOS-9486 REM-PRECIOS: Nuevo grid de histórico de solicitudes de precios (Modelo de datos)

---
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/model/HistoricoPeticionesPrecios.java
@@ -0,0 +1,130 @@
+package es.pfsgroup.plugin.rem.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.Where;
+
+import es.capgemini.pfs.auditoria.Auditable;
+import es.capgemini.pfs.auditoria.model.Auditoria;
+import es.pfsgroup.plugin.rem.model.dd.DDResultadoCampo;
+import es.pfsgroup.plugin.rem.model.dd.DDTipoPeticionPrecio;
+
+
+
+/**
+ * Modelo que gestiona el historico de peticiones de precios
+ * 
+ * @author Joaquin Bahamonde
+ *
+ */
+@Entity
+@Table(name = "HPP_HISTORICO_PETICIONES_PRECIOS", schema = "${entity.schema}")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Where(clause = Auditoria.UNDELETED_RESTICTION)
+public class HistoricoPeticionesPrecios implements Serializable,Auditable  {
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+    @Column(name = "HPP_ID")
+    private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "DD_TPP_ID")
+	private DDTipoPeticionPrecio tipoPeticionPrecio;
+	
+	@Column(name = "HPP_FECHA_SOLICITUD")
+	private Date fechaSolicitud;
+	
+	@Column(name = "HPP_FECHA_SANCION")
+	private Date fechaSancion;
+	
+	@Column(name = "HPP_OBSERVACIONES")
+	private String observaciones;
+	
+	@Version   
+	private Long version;
+	
+	@Embedded
+	private Auditoria auditoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public DDTipoPeticionPrecio getTipoPeticionPrecio() {
+		return tipoPeticionPrecio;
+	}
+
+	public void setTipoPeticionPrecio(DDTipoPeticionPrecio tipoPeticionPrecio) {
+		this.tipoPeticionPrecio = tipoPeticionPrecio;
+	}
+
+	public Date getFechaSolicitud() {
+		return fechaSolicitud;
+	}
+
+	public void setFechaSolicitud(Date fechaSolicitud) {
+		this.fechaSolicitud = fechaSolicitud;
+	}
+
+	public Date getFechaSancion() {
+		return fechaSancion;
+	}
+
+	public void setFechaSancion(Date fechaSancion) {
+		this.fechaSancion = fechaSancion;
+	}
+
+	public String getObservaciones() {
+		return observaciones;
+	}
+
+	public void setObservaciones(String observaciones) {
+		this.observaciones = observaciones;
+	}
+
+	public Long getVersion() {
+		return version;
+	}
+
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+
+	public Auditoria getAuditoria() {
+		return auditoria;
+	}
+
+	public void setAuditoria(Auditoria auditoria) {
+		this.auditoria = auditoria;
+	}
+	
+
+	
+
+}

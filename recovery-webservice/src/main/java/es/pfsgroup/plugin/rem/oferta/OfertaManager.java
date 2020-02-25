c39a5e485605eb31ef4b659a12f55cac5b8a1e96
Cristian Montoya <cristian.montoya@pfsgroup.es>
2020-02-25 09:42:15 +0100

    REMVIP-6408 Controlado nullpointer de TareaExterna de Autoprorroga.

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/oferta/OfertaManager.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/oferta/OfertaManager.java
@@ -4638,10 +4638,12 @@ public class OfertaManager extends BusinessOperationOverrider<OfertaApi> impleme
 
 			if(!Checks.estaVacio(listaTareas)) {
 				for(TareaActivo tarea : listaTareas) {
-					if(codigoTarea.equals(tarea.getTareaExterna().getTareaProcedimiento().getCodigo())) {
-						resultado = !Checks.esNulo(tarea.getTareaFinalizada()) ? tarea.getTareaFinalizada() : false;
-
-						if(!resultado || (resultado && T017.equals(tramite.getTipoTramite().getCodigo()))) break;
+					if(tarea.getTareaExterna() != null 
+						&& tarea.getTareaExterna().getTareaProcedimiento() != null 
+						&& codigoTarea.equals(tarea.getTareaExterna().getTareaProcedimiento().getCodigo())) {
+							resultado = !Checks.esNulo(tarea.getTareaFinalizada()) ? tarea.getTareaFinalizada() : false;
+	
+							if(!resultado || (resultado && T017.equals(tramite.getTipoTramite().getCodigo()))) break;
 					}
 				}
 			}

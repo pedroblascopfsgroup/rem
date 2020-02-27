1a124a301b7d21524b095cd8c01a6df9717258c1
Daniel Algaba <daniel.algaba@pfsgroup.es>
2020-02-27 18:15:00 +0100

    HREOS-9612 DIVARIAN - Cuentas Contables y Partidas Presupuestarias: Cambio de configuración

---
+++ sql/9.3/rem/DML_130_REM01_INSERT_CCC.sql
@@ -0,0 +1,142 @@
+--/*
+--##########################################
+--## AUTOR=Daniel Algaba
+--## FECHA_CREACION=20200227
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=HREOS-9612
+--## PRODUCTO=NO
+--##
+--## Finalidad: Script que añade en proveedores los datos añadidos en T_ARRAY_DATA
+--## INSTRUCCIONES:
+--## VERSIONES:
+--##        0.1 Ver1ón inicial
+--##########################################
+--*/
+
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF;
+
+
+DECLARE
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar     
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.   
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+	
+    V_TEXT1 VARCHAR2(2400 CHAR); -- Vble. auxiliar
+    V_ENTIDAD_ID NUMBER(16);
+    V_ID NUMBER(16);
+    V_ID_COD_REM NUMBER(16);
+    V_ID_PVC NUMBER(16);
+    V_ID_ETP NUMBER(16);
+    V_BOOLEAN NUMBER(16);
+    V_ITEM VARCHAR2(25 CHAR):= 'HREOS-9612';
+    
+    
+    TYPE T_TIPO_DATA IS TABLE OF VARCHAR2(150);
+    TYPE T_ARRAY_DATA IS TABLE OF T_TIPO_DATA;
+
+    V_TIPO_DATA T_ARRAY_DATA := T_ARRAY_DATA(
+      T_TIPO_DATA('01','2016', '151','6311003'),
+      T_TIPO_DATA('01','2017', '151','6311003'),
+      T_TIPO_DATA('02','2016', '151','6311003'),
+      T_TIPO_DATA('02','2017', '151','6311003'),
+      T_TIPO_DATA('01','2016', '152','6311003'),
+      T_TIPO_DATA('01','2017', '152','6311003'),
+      T_TIPO_DATA('02','2016', '152','6311003'),
+      T_TIPO_DATA('02','2017', '152','6311003')
+		); 
+    V_TMP_TIPO_DATA T_TIPO_DATA;
+    
+BEGIN	
+	
+	DBMS_OUTPUT.PUT_LINE('[INICIO] ');
+
+    DBMS_OUTPUT.PUT_LINE('[INFO]: COMENZAMOS ');
+    FOR I IN V_TIPO_DATA.FIRST .. V_TIPO_DATA.LAST
+      LOOP
+      
+        V_TMP_TIPO_DATA := V_TIPO_DATA(I);
+    
+        --Comprobamos el dato a insertar
+        V_SQL := 'SELECT COUNT(1) 
+        FROM '||V_ESQUEMA||'.CCC_CONFIG_CTAS_CONTABLES CCC
+        JOIN '||V_ESQUEMA||'.ACT_EJE_EJERCICIO EJE ON CCC.EJE_ID = EJE.EJE_ID AND EJE.BORRADO = 0
+        LEFT JOIN '||V_ESQUEMA||'.DD_SCR_SUBCARTERA SCR ON CCC.DD_SCR_ID = SCR.DD_SCR_ID AND SCR.BORRADO = 0
+        LEFT JOIN '||V_ESQUEMA||'.DD_STG_SUBTIPOS_GASTO STG ON CCC.DD_STG_ID = STG.DD_STG_ID
+        WHERE SCR.DD_SCR_CODIGO = '''||TRIM(V_TMP_TIPO_DATA(3))||'''
+        AND STG.DD_STG_CODIGO = '''||TRIM(V_TMP_TIPO_DATA(1))||'''
+        AND EJE.EJE_ANYO = '''||TRIM(V_TMP_TIPO_DATA(2))||'''
+        AND CCC.CCC_CUENTA_CONTABLE = '''||TRIM(V_TMP_TIPO_DATA(4))||'''
+        AND CCC.BORRADO = 0
+        ';
+        EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
+        
+        --1 existe lo modificamos
+        IF V_NUM_TABLAS > 0 THEN	
+
+          DBMS_OUTPUT.PUT_LINE('[INFO]: YA EXISTE LA CUENTA: '''|| TRIM(V_TMP_TIPO_DATA(4)) ||'''');  
+
+        ELSE
+       
+          DBMS_OUTPUT.PUT_LINE('[INFO]: INSERTAMOS EL REGISTRO '''|| TRIM(V_TMP_TIPO_DATA(1)) ||''', '''|| TRIM(V_TMP_TIPO_DATA(2)) ||''', '''|| TRIM(V_TMP_TIPO_DATA(3)) ||''' y '''|| TRIM(V_TMP_TIPO_DATA(4)) ||'''');  
+
+          V_MSQL := '
+
+            INSERT INTO '|| V_ESQUEMA ||'.CCC_CONFIG_CTAS_CONTABLES CCC 
+            (
+              CCC_ID
+            , DD_STG_ID
+            , DD_CRA_ID
+            , USUARIOCREAR 
+            , FECHACREAR
+            , CCC_CUENTA_CONTABLE
+            , EJE_ID
+            , CCC_ARRENDAMIENTO
+            , DD_SCR_ID
+            )
+            SELECT
+            '|| V_ESQUEMA ||'.S_CCC_CONFIG_CTAS_CONTABLES.NEXTVAL
+            , (SELECT STG.DD_STG_ID FROM '||V_ESQUEMA||'.DD_STG_SUBTIPOS_GASTO STG WHERE STG.DD_STG_CODIGO = '''|| TRIM(V_TMP_TIPO_DATA(1)) ||''')
+            , (SELECT CRA.DD_CRA_ID FROM '||V_ESQUEMA||'.DD_CRA_CARTERA CRA WHERE CRA.DD_CRA_CODIGO = ''07'')
+            , '''||TRIM(V_ITEM)||'''
+            , SYSDATE
+            , '''|| TRIM(V_TMP_TIPO_DATA(4)) ||'''
+            , (SELECT EJE.EJE_ID FROM '|| V_ESQUEMA ||'.ACT_EJE_EJERCICIO EJE WHERE EJE.EJE_ANYO = '''|| TRIM(V_TMP_TIPO_DATA(2)) ||''')
+            , 0
+            , (SELECT SCR.DD_SCR_ID FROM '||V_ESQUEMA||'.DD_SCR_SUBCARTERA SCR WHERE SCR.DD_SCR_CODIGO = '''|| TRIM(V_TMP_TIPO_DATA(3)) ||''')
+            FROM DUAL CCC
+          ';
+          EXECUTE IMMEDIATE V_MSQL;
+          DBMS_OUTPUT.PUT_LINE('[INFO]: REGISTRO INSERTADO CORRECTAMENTE');
+        
+       END IF;
+      END LOOP;
+    COMMIT;
+
+    DBMS_OUTPUT.PUT_LINE('[FIN]:  MODIFICADO CORRECTAMENTE ');
+   
+
+EXCEPTION
+     WHEN OTHERS THEN
+          err_num := SQLCODE;
+          err_msg := SQLERRM;
+
+          DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(err_num));
+          DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+          DBMS_OUTPUT.put_line(err_msg);
+
+          ROLLBACK;
+          RAISE;          
+
+END;
+
+/
+
+EXIT

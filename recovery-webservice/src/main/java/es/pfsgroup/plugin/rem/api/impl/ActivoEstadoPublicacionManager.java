adc37ba2750c9cf7bdbb623508d90649f239da1c
Carles Molins <carles.molins@pfsgroup.es>
2020-03-02 12:48:58 +0100

    [HREOS-8914] Nullpointer en getEstadoIndicadorPublicacionAlquiler

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/api/impl/ActivoEstadoPublicacionManager.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/api/impl/ActivoEstadoPublicacionManager.java
@@ -1088,7 +1088,11 @@ public class ActivoEstadoPublicacionManager implements ActivoEstadoPublicacionAp
 		ActivoPatrimonio activoPatrimonio = activoPatrimonioDao.getActivoPatrimonioByActivo(activo.getId());
 		List<DtoAdmisionDocumento> listDtoAdmisionDocumento = activoAdapter.getListDocumentacionAdministrativaById(activo.getId());
 		boolean conCee = false;
-		boolean esVivienda = DDTipoActivo.COD_VIVIENDA.equals(activo.getTipoActivo().getCodigo());
+		boolean esVivienda = false;
+		
+		if (activo.getTipoActivo() != null) {
+			esVivienda = DDTipoActivo.COD_VIVIENDA.equals(activo.getTipoActivo().getCodigo());
+		}
 
 		for(DtoAdmisionDocumento aListDtoAdmisionDocumento : listDtoAdmisionDocumento) {
 			if (DDTipoDocumentoActivo.CODIGO_CEE_ACTIVO.equals(aListDtoAdmisionDocumento.getCodigoTipoDocumentoActivo())) {

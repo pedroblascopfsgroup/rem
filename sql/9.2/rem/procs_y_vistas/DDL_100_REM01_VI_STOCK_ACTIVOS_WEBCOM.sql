494eac6da2c7e0091e686c943849f7ec92d088db
Carles Molins <carles.molins@pfsgroup.es>
2020-03-10 14:15:42 +0100

    [REMVIP-0000] Recompilar vista stock

--- sql/9.2/rem/procs_y_vistas/DDL_100_REM01_VI_STOCK_ACTIVOS_WEBCOM.sql
+++ sql/9.2/rem/procs_y_vistas/DDL_100_REM01_VI_STOCK_ACTIVOS_WEBCOM.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=Carles Molins
---## FECHA_CREACION=20200211
+--## FECHA_CREACION=20200310
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.2
 --## INCIDENCIA_LINK=HREOS-9066

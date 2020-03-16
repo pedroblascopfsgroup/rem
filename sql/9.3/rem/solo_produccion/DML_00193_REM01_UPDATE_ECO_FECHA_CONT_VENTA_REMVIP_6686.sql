0d50f6da431d45622a1fdc6d6b8ad43dac1c5d5c
Cristian Montoya <cristian.montoya@pfsgroup.es>
2020-03-16 16:23:32 +0100

    REMVIP-6686 Minimigracion eco_fecha_cont_propietario.

---
+++ sql/9.3/rem/solo_produccion/DML_00193_REM01_UPDATE_ECO_FECHA_CONT_VENTA_REMVIP_6686.sql
@@ -0,0 +1,69 @@
+--/*
+--#########################################
+--## AUTOR=Cristian Montoya
+--## FECHA_CREACION=20200316
+--## ARTEFACTO=batch
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=REMVIP-6686
+--## PRODUCTO=NO
+--## 
+--## Finalidad: Actualizacion registros 
+--## INSTRUCCIONES:  
+--## VERSIONES:
+--##        0.1 Versión inicial
+--#########################################
+--*/
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
+SET DEFINE OFF;
+
+DECLARE
+
+	V_TABLA_TMP VARCHAR2(100 CHAR) := 'TMP_ECO_FECHA_REMVIP_6686'; -- Variable para tabla de salida para el borrado
+	V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#';-- '#ESQUEMA#'; -- Configuracion Esquema
+	V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#';-- '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+	ERR_NUM NUMBER;-- Numero de errores
+	ERR_MSG VARCHAR2(2048);-- Mensaje de error
+	V_SQL VARCHAR2(4000 CHAR);
+	PL_OUTPUT VARCHAR2(32000 CHAR);
+	V_USUARIO VARCHAR2(50 CHAR) := 'REMVIP-6686';
+	
+
+BEGIN
+	
+	DBMS_OUTPUT.PUT_LINE('[INICIO]'||CHR(10));
+	V_SQL := 'MERGE INTO '||V_ESQUEMA||'.ECO_EXPEDIENTE_COMERCIAL ECO
+		USING 
+		(   
+			SELECT OFR_ID, ECO_FECHA_CONT_PROPIETARIO
+			FROM '||V_ESQUEMA||'.'||V_TABLA_TMP||'
+		) AUX
+    	ON (ECO.OFR_ID = AUX.OFR_ID)
+    	WHEN MATCHED THEN
+        	UPDATE SET
+            	ECO.ECO_FECHA_CONT_PROPIETARIO = CAST(TO_TIMESTAMP(AUX.ECO_FECHA_CONT_PROPIETARIO, ''YYYY-MM-DD HH24:MI:SS,FF9'') AS DATE),
+				ECO.USUARIOMODIFICAR = '''||V_USUARIO||''',
+				ECO.FECHAMODIFICAR = SYSDATE';
+				
+
+		EXECUTE IMMEDIATE V_SQL;
+		DBMS_OUTPUT.PUT_LINE('[INFO] Se han mergeado en total '||SQL%ROWCOUNT||' registros en la tabla '||V_ESQUEMA||'.'||V_TABLA_TMP||'');
+
+	COMMIT;
+
+	PL_OUTPUT := PL_OUTPUT || '[FIN]'||CHR(10);
+	DBMS_OUTPUT.PUT_LINE(PL_OUTPUT);
+
+EXCEPTION
+    WHEN OTHERS THEN
+      PL_OUTPUT := PL_OUTPUT ||'[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(SQLCODE)||CHR(10);
+      PL_OUTPUT := PL_OUTPUT ||'-----------------------------------------------------------'||CHR(10);
+      PL_OUTPUT := PL_OUTPUT ||SQLERRM||CHR(10);
+      PL_OUTPUT := PL_OUTPUT ||V_SQL||CHR(10);
+      DBMS_OUTPUT.PUT_LINE(PL_OUTPUT);
+      ROLLBACK;
+      RAISE;
+END;
+/
+EXIT;

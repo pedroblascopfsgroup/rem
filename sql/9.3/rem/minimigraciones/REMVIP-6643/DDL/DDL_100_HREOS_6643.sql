09fe858510a1e898ec1e14f6f2dda3169b44df83
Jose Antonio Gigante 2 <jose.gigante@pfsgroup.es>
2020-03-23 09:29:04 +0100

    REMVIP-6643 - [ITREM-15830] PETICIÓN_Carga tasaciones REM, prioritario para BARBERINA - DDL creación tabla temporal y relleno de la misma

---
+++ sql/9.3/rem/minimigraciones/REMVIP-6643/DDL/DDL_100_HREOS_6643.sql
@@ -0,0 +1,74 @@
+--/*
+--#########################################
+--## AUTOR=José Antonio Gigante
+--## FECHA_CREACION=20200323
+--## ARTEFACTO=batch
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=HREOS-6643
+--## PRODUCTO=NO
+--## 
+--## Finalidad: Creación de tabla 'TMP_ACTUALIZA_ACT_CAT_CATASTRO'
+--##			
+--## INSTRUCCIONES:  
+--## VERSIONES:
+--##        0.1 Versión inicial
+--#########################################
+--*/
+
+--Para permitir la visualización de texto en un bloque PL/SQL utilizando DBMS_OUTPUT.PUT_LINE
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
+SET DEFINE OFF;
+
+
+DECLARE
+
+TABLE_COUNT NUMBER(1,0) := 0;
+V_ESQUEMA_1 VARCHAR2(20 CHAR) := 'REM01';
+V_ESQUEMA_2 VARCHAR2(20 CHAR) := 'REMMASTER';
+V_TABLA VARCHAR2(40 CHAR) := 'TMP_ACTUALIZAR_TASACION_REM';
+
+BEGIN
+
+SELECT COUNT(1) INTO TABLE_COUNT FROM ALL_TABLES WHERE TABLE_NAME = ''||V_TABLA||'' AND OWNER= ''||V_ESQUEMA_1||'';
+
+IF TABLE_COUNT > 0 THEN
+
+    DBMS_OUTPUT.PUT_LINE('[INFO] TABLA '||V_ESQUEMA_1||'.'||V_TABLA||' YA EXISTENTE. SE PROCEDE A BORRAR Y CREAR DE NUEVO.');
+
+    EXECUTE IMMEDIATE 'DROP TABLE '||V_ESQUEMA_1||'.'||V_TABLA||'';
+    
+END IF;
+
+EXECUTE IMMEDIATE 
+'CREATE TABLE '||V_ESQUEMA_1||'.'||V_TABLA||
+'(
+	ACT_NUM_ACTIVO                  VARCHAR(50),
+    IMPORTE_TASACION_REM            VARCHAR(50),
+    IMPORTE_TASACION_PRINEX         VARCHAR(50),
+    TAS_FECHA_INI_TASACION_PRINEX   VARCHAR(50)
+    
+)'
+;
+
+DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA_1||'.'||V_TABLA||' CREADA');  
+
+IF V_ESQUEMA_2 != V_ESQUEMA_1 THEN
+    EXECUTE IMMEDIATE 'GRANT ALL ON "'||V_ESQUEMA_1||'"."'||V_TABLA||'" TO "'||V_ESQUEMA_2||'" WITH GRANT OPTION';
+    DBMS_OUTPUT.PUT_LINE('[INFO] PERMISOS SOBRE LA TABLA '||V_ESQUEMA_1||'.'||V_TABLA||' OTORGADOS A '||V_ESQUEMA_2||''); 
+END IF;
+
+EXCEPTION
+
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecucion:'||TO_CHAR(SQLCODE));
+        DBMS_OUTPUT.put_line('-----------------------------------------------------------');
+        DBMS_OUTPUT.put_line(SQLERRM);
+        ROLLBACK;
+        RAISE;
+END;
+/
+
+EXIT;
+

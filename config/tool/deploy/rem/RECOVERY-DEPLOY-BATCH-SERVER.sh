8fea529925598de8de894c241fe0a9e6d9463142
Guillermo Llidó <guillermo.llido@pfsgroup.es>
2020-03-26 19:32:10 +0100

    HREOS-9884 -> DIVARIAN - COMERCIAL: Bulk Advisory Notes - configurar templates en resto de entornos

--- config/tool/deploy/rem/RECOVERY-DEPLOY-BATCH-SERVER.sh
+++ config/tool/deploy/rem/RECOVERY-DEPLOY-BATCH-SERVER.sh
@@ -23,7 +23,7 @@ if [[ "$1" == "val03" ]] ; then
 fi
 
 if [[ "$1" == "val04" ]] ; then
-    BASE_DIR=/recovery/map027/batch-server
+    BASE_DIR=/recovery/mba027/batch-server
 fi
 
 if [[ "$1" == "val05" ]] ; then
@@ -50,13 +50,31 @@ cp config/$1/config.ini $BASE_DIR/programas/etl/config/
 cd $LOCAL_PATH
 rm -f $BASE_DIR/shells/*.sh
 rm -rf $BASE_DIR/shells/ftp
+
+if [[ ! -d $BASE_DIR/control/etl/output/templates/ ]]; then
+    echo "Se crea el directorio shells"
+    mkdir $BASE_DIR/shells/
+fi
+
 cp config/$1/setBatchEnv.sh $BASE_DIR/shells/
 cp -r scripts/shells/* $BASE_DIR/shells/
 chmod a+rx $BASE_DIR/shells/*.sh
 chmod -R 755 $BASE_DIR/shells/ftp
 rm -rf $BASE_DIR/programas/etl/apr_*
+
+if [[ ! -d $BASE_DIR/programas/etl/ ]]; then
+    echo "Se crea el directorio etl"
+    mkdir $BASE_DIR/programas/etl/
+fi
+
 cp etl/* $BASE_DIR/programas/etl/
+
+if [[ ! -d $BASE_DIR/control/etl/output/templates/ ]]; then
+    echo "Se crea el directorio templates"
+    mkdir $BASE_DIR/control/etl/output/templates
+fi
 cp -r templates/* $BASE_DIR/control/etl/output/templates/
+
 cd $BASE_DIR/programas/etl/
 for etl in `ls *.zip`
 do

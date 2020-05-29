36c0aa500b2cf1c789690bbf486a22b1b83ced9c
Daniel Algaba <daniel.algaba@pfsgroup.es>
2020-05-29 11:58:07 +0200

    HREOS-10291 DND - Fase III - Fichero Stock - Cambio de shell

--- batch-shell/dist/shells/rem/apr_gen_rd_stock_activos.sh
+++ batch-shell/dist/shells/rem/apr_gen_rd_stock_activos.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Generado automaticamente a las mié jul 23 13:32:51 CEST 2014
+ 
+
 
 filename=$(basename $0)
 nameETL="${filename%.*}"
@@ -9,23 +11,20 @@ export DIR_CONFIG=$DIR_BASE_ETL/config/
 export CFG_FILE=config.ini
 export MAINSH="$nameETL"_run.sh
 
-echo "Nombre del directorio= $DIR_ETL"
-echo "La ruta de los directorios es $1"
-
-if [ ! -d $DIR_ETL ] ; then
-	echo "$(basename $0) Error en $filename: directorio inexistente $DIR_ETL"
-	exit 1
+cd "$DIR_ETL" &> /dev/null
+if [ $? -ne 0 ] ; then
+   echo "$(basename $0) Error en $filename: directorio inexistente $DIR_ETL"
+   exit 1
 fi
 
-cd $DIR_ETL
-
 if [ -f $MAINSH ]; then
-    CLASS="$(cat $MAINSH | grep "^ java" | cut -f11 -d" ")"
+    CLASS="$(cat $MAINSH | grep "^ java" | cut -f10 -d" ")"
     CLASS2=`echo $CLASS | sed -e 's/$ROOT_PATH/./g'`
-    CLASEINICIO="$(cat $MAINSH | grep "^ java" | cut -f12 -d" ")"
-    java -Xms512M -Xmx1536M -Dconfig.dir=$DIR_CONFIG -Dconfig.file.mask=$CFG_FILE -Duser.country=ES -Duser.language=es -Dconfig.rutaSalida=$1 -cp $CLASS2 $CLASEINICIO --context=Default "$@" 
+    CLASEINICIO="$(cat $MAINSH | grep "^ java" | cut -f11 -d" ")"
+    java -Xms512M -Xmx1536M -Dconfig.dir=$DIR_CONFIG -Dconfig.file.mask=$CFG_FILE -Duser.country=ES -Duser.language=es -cp $CLASS2 $CLASEINICIO --context=Default "$@"
     exit $?
 else
     echo "$(basename $0) Error en $filename: no se ha encontrado  $MAINSH"
     exit 1
 fi
+

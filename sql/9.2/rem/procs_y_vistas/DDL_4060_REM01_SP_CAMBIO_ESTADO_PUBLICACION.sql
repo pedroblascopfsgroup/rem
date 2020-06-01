bac8f38658d7e904be6b0c97170eeb1eaca6aaf8
Adrián Molina Garrido <adrian.molina@pfsgroup.es>
2020-06-01 18:00:14 +0200

    REMVIP-7233 - [ITREM-16972] ERROR_No se modifica el Canal de publicación

--- sql/9.2/rem/procs_y_vistas/DDL_4060_REM01_SP_CAMBIO_ESTADO_PUBLICACION.sql
+++ sql/9.2/rem/procs_y_vistas/DDL_4060_REM01_SP_CAMBIO_ESTADO_PUBLICACION.sql
@@ -1085,28 +1085,16 @@ END IF;
         /**************/
         /*CANAL DE PUBLICACION*/
         /**************/
-        V_MSQL := ' SELECT COUNT(*)
-                FROM '||V_ESQUEMA||'.ACT_AGA_AGRUPACION_ACTIVO AGA
-                JOIN '||V_ESQUEMA||'.ACT_ACTIVO ACT ON ACT.ACT_ID = AGA.ACT_ID
-                JOIN '||V_ESQUEMA||'.ACT_AGR_AGRUPACION AGR ON AGR.AGR_ID = AGA.AGR_ID
-                JOIN '||V_ESQUEMA||'.DD_TAG_TIPO_AGRUPACION TAG ON TAG.DD_TAG_ID = AGR.DD_TAG_ID
-                WHERE
-                TAG.DD_TAG_CODIGO = ''02'' -- RESTRINGIDA
-                AND AGR.AGR_FIN_VIGENCIA IS NULL
-                AND ACT.ACT_ID = '||nACT_ID;
-        EXECUTE IMMEDIATE V_MSQL INTO vRESTRINGIDA;
-        IF vRESTRINGIDA = 0 THEN
-	        V_MSQL := 'SELECT EPV.DD_EPV_CODIGO, POR.DD_POR_CODIGO
-	                  FROM '||V_ESQUEMA||'.ACT_APU_ACTIVO_PUBLICACION APU 
-	                  JOIN ' ||V_ESQUEMA||'.DD_EPV_ESTADO_PUB_VENTA EPV ON EPV.DD_EPV_ID = APU.DD_EPV_ID 
-	                  LEFT JOIN '||V_ESQUEMA||'.DD_POR_PORTAL POR ON POR.DD_POR_ID = APU.DD_POR_ID
-	                  WHERE APU.ACT_ID = '||nACT_ID||'
-	                      AND APU.BORRADO = 0';
-	        EXECUTE IMMEDIATE V_MSQL INTO cCODIGO_ESTADO_V, cDD_POR_CODIGO;
-	        
-	        IF cCODIGO_ESTADO_V IN ('03', '04') THEN
-	        	#ESQUEMA#.SP_PORTALES_ACTIVO(nACT_ID, null, pUSUARIOMODIFICAR, OutCANAL);
-	        END IF;
+        V_MSQL := 'SELECT EPV.DD_EPV_CODIGO, POR.DD_POR_CODIGO
+                  FROM '||V_ESQUEMA||'.ACT_APU_ACTIVO_PUBLICACION APU
+                  JOIN ' ||V_ESQUEMA||'.DD_EPV_ESTADO_PUB_VENTA EPV ON EPV.DD_EPV_ID = APU.DD_EPV_ID
+                  LEFT JOIN '||V_ESQUEMA||'.DD_POR_PORTAL POR ON POR.DD_POR_ID = APU.DD_POR_ID
+                  WHERE APU.ACT_ID = '||nACT_ID||'
+                      AND APU.BORRADO = 0';
+        EXECUTE IMMEDIATE V_MSQL INTO cCODIGO_ESTADO_V, cDD_POR_CODIGO;
+
+        IF cCODIGO_ESTADO_V IN ('03', '04') THEN
+            #ESQUEMA#.SP_PORTALES_ACTIVO(nACT_ID, null, pUSUARIOMODIFICAR, OutCANAL);
         END IF;
         /**************/
         /*HISTORIFICAR*/

b701e39f50c5ff8fff1e7463272882fdb0cd8afe
Carles Molins <carles.molins@pfsgroup.es>
2020-06-23 16:44:24 +0200

    [REMVIP-7637] Trabajos con PVC borrado

---
+++ sql/9.3/rem/solo_produccion/DML_00355_REM01_UPDATE_ACT_PVC_PROVEEDOR_CONTACTO.sql
@@ -0,0 +1,57 @@
+--/*
+--##########################################
+--## AUTOR=Carles Molins
+--## FECHA_CREACION=20200623
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=REMVIP-7637
+--## PRODUCTO=NO
+--##
+--## Finalidad: 
+--## INSTRUCCIONES:
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
+SET DEFINE OFF;
+
+DECLARE
+    V_ESQUEMA VARCHAR2(25 CHAR) := '#ESQUEMA#'; -- Configuracion Esquema.
+    V_ESQUEMA_MASTER VARCHAR2(25 CHAR) := '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master.
+    ERR_NUM NUMBER(25); -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(10024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+   
+    V_USUARIOMODIFICAR VARCHAR(100 CHAR):= 'REMVIP-7637'; -- Vble. para el usuario modificar.
+    V_MSQL VARCHAR2(32000 CHAR); -- Vble. auxiliar para almacenar la sentencia a ejecutar.
+   
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('[INICIO]');
+
+    V_MSQL := 'UPDATE '||V_ESQUEMA||'.ACT_PVC_PROVEEDOR_CONTACTO 
+				SET USU_ID = NULL, 
+				    USUARIOMODIFICAR = '''||V_USUARIOMODIFICAR||''', 
+				    FECHAMODIFICAR = SYSDATE, 
+				    USUARIOBORRAR = NULL,
+				    FECHABORRAR = NULL
+				WHERE BORRADO = 1 AND USUARIOBORRAR = ''REMVIP-7289_2'' ';
+    EXECUTE IMMEDIATE V_MSQL;
+
+    DBMS_OUTPUT.PUT_LINE('[INFO] Se han actualizado '||SQL%ROWCOUNT||' registros.');
+
+    DBMS_OUTPUT.PUT_LINE('[FIN]');
+    COMMIT;
+ 
+EXCEPTION
+    WHEN OTHERS THEN
+         ERR_NUM := SQLCODE;
+         ERR_MSG := SQLERRM;
+         DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(ERR_NUM));
+         DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+         DBMS_OUTPUT.put_line(ERR_MSG);
+         ROLLBACK;
+         RAISE;   
+END;
+/
+EXIT;

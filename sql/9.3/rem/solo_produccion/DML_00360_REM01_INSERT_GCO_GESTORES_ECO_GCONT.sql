6384be565d0d0b853a285cc55d49819051c34083
Guillem Rey <guillem.rey@pfsgroup.es>
2020-06-30 18:04:00 +0200

    [REMVIP-7588] ERROR distinct

--- sql/9.3/rem/solo_produccion/DML_00360_REM01_INSERT_GCO_GESTORES_ECO_GCONT.sql
+++ sql/9.3/rem/solo_produccion/DML_00360_REM01_INSERT_GCO_GESTORES_ECO_GCONT.sql
@@ -40,23 +40,24 @@ BEGIN
 	DBMS_OUTPUT.PUT_LINE('[INICIO] ');
       
     V_MSQL := 'INSERT INTO '||V_ESQUEMA||'.TMP_GCO_GCH (ID, ECO_ID) 
-    SELECT ROWNUM, eco.eco_id
-    FROM REM01.ACT_ACTIVO ACT
-	inner join rem01.gah_gestor_activo_historico gah on gah.act_id = act.act_id
-	inner join rem01.geh_gestor_entidad_hist geh on geh.geh_id = gah.geh_id and geh.borrado = 0 and geh.geh_fecha_hasta is null
-	inner join rem01.act_ofr aof on aof.act_id = act.act_id
-	inner join rem01.ofr_ofertas ofr on ofr.ofr_id = aof.ofr_id and ofr.borrado = 0
-	inner join (SELECT distinct eco.* FROM rem01.eco_expediente_comercial eco
+    SELECT ROWNUM, ECO_ID FROM (
+	SELECT DISTINCT eco.eco_id
+    FROM '||V_ESQUEMA||'.ACT_ACTIVO ACT
+	inner join '||V_ESQUEMA||'.gah_gestor_activo_historico gah on gah.act_id = act.act_id
+	inner join '||V_ESQUEMA||'.geh_gestor_entidad_hist geh on geh.geh_id = gah.geh_id and geh.borrado = 0 and geh.geh_fecha_hasta is null
+	inner join '||V_ESQUEMA||'.act_ofr aof on aof.act_id = act.act_id
+	inner join '||V_ESQUEMA||'.ofr_ofertas ofr on ofr.ofr_id = aof.ofr_id and ofr.borrado = 0
+	inner join (SELECT distinct eco.* FROM '||V_ESQUEMA||'.eco_expediente_comercial eco
 	    left join (
-	    select distinct eco.eco_id from rem01.eco_expediente_comercial eco     
-	    inner join rem01.gch_gestor_eco_historico gch on gch.eco_id = eco.eco_id 
-	    inner join rem01.geh_gestor_entidad_hist geh1 on geh1.geh_id = gch.geh_id and geh1.borrado = 0 and geh1.geh_fecha_hasta is null
-	    inner join remmaster.dd_tge_tipo_gestor tge1 on tge1.dd_tge_id = geh1.dd_tge_id
+	    select distinct eco.eco_id from '||V_ESQUEMA||'.eco_expediente_comercial eco     
+	    inner join '||V_ESQUEMA||'.gch_gestor_eco_historico gch on gch.eco_id = eco.eco_id 
+	    inner join '||V_ESQUEMA||'.geh_gestor_entidad_hist geh1 on geh1.geh_id = gch.geh_id and geh1.borrado = 0 and geh1.geh_fecha_hasta is null
+	    inner join '||V_ESQUEMA_M||'.dd_tge_tipo_gestor tge1 on tge1.dd_tge_id = geh1.dd_tge_id
 	    where eco.borrado = 0 and tge1.dd_tge_codigo = ''GCONT'') aux on aux.eco_id = eco.eco_id
-	    inner join rem01.act_tra_tramite tra on tra.tbj_id = eco.tbj_id and tra.borrado = 0 and tra.dd_epr_id != 5
+	    inner join '||V_ESQUEMA||'.act_tra_tramite tra on tra.tbj_id = eco.tbj_id and tra.borrado = 0 and tra.dd_epr_id != 5
 	    where eco.borrado = 0 and aux.eco_id is null) eco on eco.ofr_id = ofr.ofr_id and eco.borrado = 0
-	inner join remmaster.dd_tge_tipo_gestor tge on tge.dd_tge_id = geh.dd_tge_id
-	where tge.dd_tge_codigo = ''GCONT'' and act.borrado = 0';
+	inner join '||V_ESQUEMA_M||'.dd_tge_tipo_gestor tge on tge.dd_tge_id = geh.dd_tge_id
+	where tge.dd_tge_codigo = ''GCONT'' and act.borrado = 0)';
 
     EXECUTE IMMEDIATE V_MSQL;
 

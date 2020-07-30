3a6835c59047058584f65c95b49e67c615aa4e93
Ivan Repiso <ivan.repiso@pfsgroup.es>
2020-07-30 12:21:24 +0200

    REMVIP-7859 Poner a 0 campos null y ALTER TABLE NOT NULL

--- sql/9.3/rem/DDL_00051_REM01_ALTER_ACT_SPS_SIT_POSESORIA.sql
+++ sql/9.3/rem/DDL_00051_REM01_ALTER_ACT_SPS_SIT_POSESORIA.sql
@@ -1,16 +1,17 @@
 --/*
 --##########################################
---## AUTOR=Violre Remus Ovidiu
---## FECHA_CREACION=20200724
+--## AUTOR=IVAN REPISO 
+--## FECHA_CREACION=20200730
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.3
 --## INCIDENCIA_LINK=REMVIP-7859
 --## PRODUCTO=NO
---## Finalidad: AÑADIR DEFAULT A COMLUMNA
+--## Finalidad: AÑADIR DEFAULT A COLUMNA SPS_ACC_TAPIADO, SPS_ACC_ANTIOCUPA Y SPS_OCUPADO
 --##           
 --## INSTRUCCIONES: Configurar las variables necesarias en el principio del DECLARE
 --## VERSIONES:
 --##        0.1 Versión inicial
+--##	    0.2 REMVIP-7859 Modificar tapiado y antiocupa
 --##########################################
 --*/
 
@@ -27,18 +28,19 @@ DECLARE
     V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
     V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
     V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
-    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.  
-    V_NUM_SEQ NUMBER(16); -- Vble. para validar la existencia de una secuencia.  
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla. 
+    V_IS_NULL NUMBER(16); -- Vble. para validar si el campo is null. 
     ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
     ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
 
-    V_TEXT1 VARCHAR2(2400 CHAR); -- Vble. auxiliar
     V_TEXT_TABLA VARCHAR2(2400 CHAR) := 'ACT_SPS_SIT_POSESORIA'; -- Vble. auxiliar para almacenar el nombre de la tabla de ref.
 
     TYPE T_TIPO_DATA IS TABLE OF VARCHAR2(256);
     TYPE T_ARRAY_DATA IS TABLE OF T_TIPO_DATA;
     V_TIPO_DATA T_ARRAY_DATA := T_ARRAY_DATA(
-        T_TIPO_DATA('SPS_OCUPADO', 'NUMBER','DEFAULT 0 NOT NULL')
+        T_TIPO_DATA('SPS_ACC_TAPIADO', 'NUMBER','DEFAULT 0 NOT NULL'),
+	T_TIPO_DATA('SPS_ACC_ANTIOCUPA', 'NUMBER','DEFAULT 0 NOT NULL'),
+	T_TIPO_DATA('SPS_OCUPADO', 'NUMBER','DEFAULT 0 NOT NULL')
     ); 
     V_TMP_TIPO_DATA T_TIPO_DATA;
     
@@ -54,11 +56,18 @@ BEGIN
 		V_MSQL := 'SELECT COUNT(1) FROM ALL_TAB_COLUMNS WHERE COLUMN_NAME= '''||TRIM(V_TMP_TIPO_DATA(1))||''' and DATA_TYPE = '''||TRIM(V_TMP_TIPO_DATA(2))||''' and TABLE_NAME = '''||V_TEXT_TABLA||''' and owner = '''||V_ESQUEMA||'''';
 		EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;
 		
-		IF V_NUM_TABLAS = 1 THEN
-			EXECUTE IMMEDIATE 'ALTER TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' MODIFY '||V_TMP_TIPO_DATA(1)||' DEFAULT 0 NOT NULL';
-			DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.'||V_TMP_TIPO_DATA(1)||'... MODIFICADA');   
-		ELSE  
-			DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.'||V_TEXT_TABLA||' '||TRIM(V_TMP_TIPO_DATA(1))||'''... NO existe');    
+	IF V_NUM_TABLAS = 1 THEN
+		-- Comprobamos si campo is null
+		V_MSQL := 'SELECT COUNT(1) FROM ALL_TAB_COLUMNS WHERE COLUMN_NAME= '''||TRIM(V_TMP_TIPO_DATA(1))||''' and NULLABLE = ''Y'' and 			TABLE_NAME = '''||V_TEXT_TABLA||''' and owner = '''||V_ESQUEMA||'''';
+		EXECUTE IMMEDIATE V_MSQL INTO V_IS_NULL;
+		    IF V_IS_NULL = 1 THEN
+			EXECUTE IMMEDIATE 'ALTER TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' MODIFY '||TRIM(V_TMP_TIPO_DATA(1))||' '||TRIM(V_TMP_TIPO_DATA(3))||'';
+			DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.'||TRIM(V_TMP_TIPO_DATA(1))||'... MODIFICADA');  
+		    ELSE
+ 			DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.'||TRIM(V_TMP_TIPO_DATA(1))||'''... IS NOT NULL');
+		   END IF;
+	ELSE  
+			DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.'||TRIM(V_TMP_TIPO_DATA(1))||'''... NO existe');    
        	END IF;
       END LOOP;
 EXCEPTION

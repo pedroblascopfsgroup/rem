95bed3405c751bb33fd1b5dc0144e61d759bca92
Daniel Algaba <daniel.algaba@pfsgroup.es>
2020-08-10 12:36:11 +0200

    HREOS-10704 REM-GESTIÓN DE ACTIVOS - M3 - [RUFU-017] - Proceso de generación de albaranes y prefacturas - Desarrollo

---
+++ sql/9.3/rem/DDL_00071_REM01_AUX_PROC_GEN_ALB_PFA.sql
@@ -0,0 +1,84 @@
+--/*
+--#########################################
+--## AUTOR=Daniel Algaba
+--## FECHA_CREACION=20200807
+--## ARTEFACTO=batch
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=HREOS-10704
+--## PRODUCTO=NO
+--## 
+--## Finalidad: Creación de tabla auxiliar para AUX_PROC_GEN_ALB_PFA
+--##			
+--## INSTRUCCIONES:  
+--## VERSIONES:
+--##        0.1 Versión inicial
+--#########################################
+----*/
+
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF;
+
+
+DECLARE
+
+	V_MSQL VARCHAR2(32000 CHAR); 
+	V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; 
+	V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#';
+	V_SQL VARCHAR2(4000 CHAR);
+	V_NUM NUMBER(16);
+	ERR_NUM NUMBER(25);  
+	ERR_MSG VARCHAR2(1024 CHAR); 
+	V_TABLA VARCHAR2(50 CHAR):= 'AUX_PROC_GEN_ALB_PFA';
+
+
+BEGIN	
+
+	DBMS_OUTPUT.PUT_LINE('[INFO] INICIO DEL PROCESO.');
+	
+	V_MSQL := 'SELECT COUNT(1) FROM ALL_TABLES WHERE TABLE_NAME = '''||V_TABLA||''' AND OWNER = '''||V_ESQUEMA||'''';
+	EXECUTE IMMEDIATE V_MSQL INTO V_NUM;
+	
+	IF V_NUM != 0 THEN
+	
+		DBMS_OUTPUT.PUT_LINE('[INFO] LA TABLA '''||V_TABLA||''' YA EXISTE.');
+		V_MSQL := 'DROP TABLE '||V_ESQUEMA||'.'||V_TABLA||'';
+		EXECUTE IMMEDIATE V_MSQL;
+		
+	END IF;	
+
+	EXECUTE IMMEDIATE '
+	CREATE TABLE '||V_ESQUEMA||'.'||V_TABLA||' (
+		TBJ_ID		NUMBER(16,0)
+		, PVE_ID	NUMBER(16,0)
+		, PRO_ID	NUMBER(16,0)
+		, ANYO		NUMBER(4,0)
+		, GRUPO		NUMBER(16,0)
+		, ALB_ID	NUMBER(16,0)
+	)';
+
+	DBMS_OUTPUT.PUT_LINE('[INFO] LA TABLA '''||V_TABLA||''' HA SIDO CREADA CON EXITO.');
+  COMMIT;
+ 
+  
+  DBMS_OUTPUT.PUT_LINE('[INFO] PROCESO FINALIZADO.');
+
+         
+EXCEPTION
+
+   WHEN OTHERS THEN
+   
+        err_num := SQLCODE;
+        err_msg := SQLERRM;
+
+        DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(err_num));
+        DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+        DBMS_OUTPUT.put_line(err_msg);
+
+        ROLLBACK;
+        RAISE;          
+
+END;
+/
+EXIT

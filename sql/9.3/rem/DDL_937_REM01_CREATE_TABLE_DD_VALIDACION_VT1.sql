d87d468161c30da14b1346b5006992ad9da3f111
Dean Ibañez <dean.ibanez@pfsgroup.es>
2020-09-08 13:13:50 +0200

    HREOS-11061 Create table DD_VALIDACION_VT1

---
+++ sql/9.3/rem/DDL_937_REM01_CREATE_TABLE_DD_VALIDACION_VT1.sql
@@ -0,0 +1,105 @@
+--/*
+--##########################################
+--## AUTOR=Dean Ibañez Viño
+--## FECHA_CREACION=20200910
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=HREOS-11061
+--## PRODUCTO=NO
+--## Finalidad: Creación diccionario DD_VALIDACION_VT1
+--##           
+--## INSTRUCCIONES: Configurar las variables necesarias en el principio del DECLARE
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+
+
+--Para permitir la visualización de texto en un bloque PL/SQL utilizando DBMS_OUTPUT.PUT_LINE
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF;
+
+
+DECLARE
+
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar    
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+    V_TABLESPACE_IDX VARCHAR2(25 CHAR):= '#TABLESPACE_INDEX#'; -- Configuracion Tablespace de Indices
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.  
+    V_NUM_SEQ NUMBER(16); -- Vble. para validar la existencia de una secuencia.  
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+
+    V_TEXT1 VARCHAR2(2400 CHAR); -- Vble. auxiliar
+    V_TEXT_TABLA VARCHAR2(2400 CHAR) := 'DD_VALIDACION_VT1'; -- Vble. auxiliar para almacenar el nombre de la tabla de ref.
+    V_COMMENT_TABLE VARCHAR2(500 CHAR):= 'Diccionario con los con los tipo estado'; -- Vble. para los comentarios de las tablas
+
+BEGIN
+
+
+	DBMS_OUTPUT.PUT_LINE('********' ||V_TEXT_TABLA|| '********'); 	
+	
+	-- Verificar si la tabla ya existe
+	V_MSQL := 'SELECT COUNT(1) FROM ALL_TABLES WHERE TABLE_NAME = '''||V_TEXT_TABLA||''' and owner = '''||V_ESQUEMA||'''';
+	EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;	
+	IF V_NUM_TABLAS = 1 THEN
+		DBMS_OUTPUT.PUT_LINE('[INFO] ' || V_ESQUEMA || '.'||V_TEXT_TABLA||'... Ya existe.');
+    ELSE
+            
+            -- Creamos la tabla
+            DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA|| '.'||V_TEXT_TABLA||'...');
+            V_MSQL := 'CREATE TABLE ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'
+            (
+                DD_VAL_VT1_ID           	NUMBER(16)                  NOT NULL,
+                DD_VAL_VT1_CODIGO        	VARCHAR2(20 CHAR)           NOT NULL,
+                DD_VAL_VT1_DESCRIPCION		VARCHAR2(250 CHAR)          NOT NULL,
+                DD_VAL_VT1_QUERY            VARCHAR2(4000 CHAR)         NOT NULL,
+                BORRADO 					NUMBER(1,0) 		    	DEFAULT 0 NOT NULL ENABLE
+            )
+            LOGGING 
+            NOCOMPRESS 
+            NOCACHE
+            NOPARALLEL
+            NOMONITORING
+            ';
+            EXECUTE IMMEDIATE V_MSQL;
+            DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'... Tabla creada.');
+
+            -- Comprobamos si existe la secuencia
+            V_SQL := 'SELECT COUNT(1) FROM ALL_SEQUENCES WHERE SEQUENCE_NAME = ''S_'||V_TEXT_TABLA||''' and SEQUENCE_OWNER = '''||V_ESQUEMA||'''';
+            EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS; 
+            IF V_NUM_TABLAS = 1 THEN
+                DBMS_OUTPUT.PUT_LINE('[INFO] '|| V_ESQUEMA ||'.S_'||V_TEXT_TABLA||'... Ya existe.');  
+            ELSE
+                V_MSQL := 'CREATE SEQUENCE '||V_ESQUEMA||'.S_'||V_TEXT_TABLA||'';		
+                EXECUTE IMMEDIATE V_MSQL;		
+                DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.S_'||V_TEXT_TABLA||'... Secuencia creada');
+            END IF; 
+
+    END IF;
+	
+	COMMIT;
+
+
+EXCEPTION
+     WHEN OTHERS THEN 
+         DBMS_OUTPUT.PUT_LINE('KO!');
+          err_num := SQLCODE;
+          err_msg := SQLERRM;
+
+          DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(err_num));
+          DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+          DBMS_OUTPUT.put_line(err_msg);
+
+          ROLLBACK;
+          RAISE;          
+
+END;
+
+/
+
+EXIT;

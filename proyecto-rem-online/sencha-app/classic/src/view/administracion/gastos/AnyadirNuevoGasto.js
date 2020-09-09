0a35859d3c769fec3321c64e44734973996e6dee
javier esbri <javier.esbri@pfsgroup.es>
2020-09-09 11:59:52 +0200

    HREOS-11126 REM-NUEVO MODELO FACTURACIÓN: Pantalla creación gasto

--- proyecto-rem-online/sencha-app/classic/src/view/administracion/gastos/AnyadirNuevoGasto.js
+++ proyecto-rem-online/sencha-app/classic/src/view/administracion/gastos/AnyadirNuevoGasto.js
@@ -245,19 +245,6 @@ Ext.define('HreRem.view.administracion.gastos.AnyadirNuevoGasto', {
 													reference: 'gastoRefacturadoGrid',
 													name: 'gastoRefacturadoGrid'									
 												},
-												////
-											    { 
-													xtype: 'comboboxfieldbase',
-									               	fieldLabel:  HreRem.i18n('fieldlabel.subtipo'),
-									               	colspan:1,
-									               	reference: 'subtipoGastoCombo',
-											      	bind: {
-										           		store: '{comboSubtiposNuevoGasto}',
-										           		value: '{gastoNuevo.subtipoGastoCodigo}',
-										           		disabled: '{!gastoNuevo.tipoGastoCodigo}'
-										         	},
-										         	allowBlank: false
-											    },
 							            	    {
 							            	    	xtype: 'datefieldbase',
 							            	    	fieldLabel: HreRem.i18n('fieldlabel.fecha.emision'),

e76ced53446797adabea854f940346b0fd5a75ac
Carlos Augusto <carlos.augusto@pfsgroup.es>
2020-09-13 11:11:51 +0200

    HREOS-11058 DDL/DML DD_TGA_TPO_GASTO_ASOCIADO

---
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/model/dd/DDTipoGastoAsociado.java
@@ -0,0 +1,94 @@
+package es.pfsgroup.plugin.rem.model.dd;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Version;
+
+import es.capgemini.pfs.auditoria.Auditable;
+import es.capgemini.pfs.auditoria.model.Auditoria;
+import es.capgemini.pfs.diccionarios.Dictionary;
+
+public class DDTipoGastoAsociado implements Auditable, Dictionary{
+
+	private static final long serialVersionUID = 2307957295534774606L;
+	public static final String CODIGO_ITP = "01";
+    public static final String CODIGO_PLUSVALIA_DE_ADQUISICION = "02";
+    public static final String CODIGO_NOTARIA = "03";
+    public static final String CODIGO_REGISTRO = "04";
+    public static final String CODIGO_OTROS_GASTOS = "05";
+    
+    @Id
+	@Column(name = "DD_TGA_ID")
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "DDTipoGastoAsociado")
+	@SequenceGenerator(name = "DDTipoGastoAsociado", sequenceName = "S_DD_TGA_TPO_GASTO_ASOCIADO")
+	private Long id;
+    
+    @Column(name = "DD_TGA_CODIGO")   
+	private String codigo;
+	 
+	@Column(name = "DD_TGA_DESCRIPCION")   
+	private String descripcion;
+	    
+	@Column(name = "DD_TGA_DESCRIPCION_LARGA")   
+	private String descripcionLarga;
+	
+	@Version   
+	private Long version;
+	
+	@Embedded
+	private Auditoria auditoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getDescripcionLarga() {
+		return descripcionLarga;
+	}
+
+	public void setDescripcionLarga(String descripcionLarga) {
+		this.descripcionLarga = descripcionLarga;
+	}
+
+	public Long getVersion() {
+		return version;
+	}
+
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+
+	public Auditoria getAuditoria() {
+		return auditoria;
+	}
+
+	public void setAuditoria(Auditoria auditoria) {
+		this.auditoria = auditoria;
+	}
+	
+	
+}

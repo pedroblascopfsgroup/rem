3f84d979028e2ce52301fefaadc42cd8de8fec43
Josep Ros <josep.ros@pfsgroup.es>
2020-09-14 14:01:11 +0200

    HREOS-11054 Se actualiza la fecha de creacion del script para la RSR.

--- sql/9.3/rem/DDL_00051_REM01_CREATE_TABLE_TMP_CONV_SAREB.sql
+++ sql/9.3/rem/DDL_00051_REM01_CREATE_TABLE_TMP_CONV_SAREB.sql
@@ -1,7 +1,7 @@
 --/*
 --#########################################
 --## AUTOR=Daniel Algaba
---## FECHA_CREACION=20200709
+--## FECHA_CREACION=20200914
 --## ARTEFACTO=batch
 --## VERSION_ARTEFACTO=9.3
 --## INCIDENCIA_LINK=HREOS-10500

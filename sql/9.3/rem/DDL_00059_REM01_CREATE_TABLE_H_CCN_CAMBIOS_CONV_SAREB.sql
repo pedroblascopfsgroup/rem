656906531cd76951addabfc9967aefc2230d044f
Josep Ros <josep.ros@pfsgroup.es>
2020-09-15 12:35:41 +0200

    HREOS-11054 Se ALTER TABLE Se reemplazan columnas por otras. Se cambia Fecha Crear

--- sql/9.3/rem/DDL_00059_REM01_CREATE_TABLE_H_CCN_CAMBIOS_CONV_SAREB.sql
+++ sql/9.3/rem/DDL_00059_REM01_CREATE_TABLE_H_CCN_CAMBIOS_CONV_SAREB.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=Daniel Algaba
---## FECHA_CREACION=20200709
+--## FECHA_CREACION=20200915
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.3
 --## INCIDENCIA_LINK=HREOS-10459

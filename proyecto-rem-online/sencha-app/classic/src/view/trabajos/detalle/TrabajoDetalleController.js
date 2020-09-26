2511b9f1d017c3dd82c311a7c555750c7847c9ee
Alejandro Valverde <alejandro.valverde@pfsgroup.es>
2020-09-26 23:06:33 +0200

    HREOS-11373 - Error despliegue

--- proyecto-rem-online/sencha-app/classic/src/view/trabajos/detalle/TrabajoDetalleController.js
+++ proyecto-rem-online/sencha-app/classic/src/view/trabajos/detalle/TrabajoDetalleController.js
@@ -1814,6 +1814,7 @@ Ext.define('HreRem.view.trabajos.detalle.TrabajoDetalleController', {
     	if (comboProveedor.getSelection() != null && comboProveedor.getSelection().getData().idProveedor != undefined) {
         	me.loadComboProveedorContacto(comboProveedor.getSelection().getData().idProveedor);
     	}
+    },
     selectFechaTope: function(fecha){
     	var me = this;
     	me.lookupReference('fechaConcretaTrabajo').setValue(null);

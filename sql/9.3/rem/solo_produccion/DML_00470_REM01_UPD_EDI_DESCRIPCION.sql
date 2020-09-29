eb24a370619ee85dd18c0f74c8dfa61437e0fa40
Ivan Repiso <ivan.repiso@pfsgroup.es>
2020-09-29 12:31:51 +0200

    REMVIP-8121 Retorno de carro en descripcion

--- sql/9.3/rem/solo_produccion/DML_00470_REM01_UPD_EDI_DESCRIPCION.sql
+++ sql/9.3/rem/solo_produccion/DML_00470_REM01_UPD_EDI_DESCRIPCION.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=IVAN REPISO
---## FECHA_CREACION=20200929
+--## FECHA_CREACION=20200928
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.3
 --## INCIDENCIA_LINK=REMVIP-8121

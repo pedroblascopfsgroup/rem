b4b19da2e207e5b876f463c82f6ee7072b587445
Carles Molins <carles.molins@pfsgroup.es>
2020-10-21 15:18:45 +0200

    [REMVIP-8239] Flexibilización: PBC

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/jbpm/handler/user/impl/ComercialUserAssigantionService.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/jbpm/handler/user/impl/ComercialUserAssigantionService.java
@@ -83,6 +83,8 @@ public class ComercialUserAssigantionService implements UserAssigantionService  
 	public static final String CODIGO_T017_RESOLUCION_DIVARIAN = "T017_ResolucionDivarian";
 	public static final String CODIGO_T017_RESOLUCION_ARROW = "T017_ResolucionArrow";
 	
+	public static final String USUARIO_GESTOR_FORMALIZACION = "gestform";
+	
 	@Autowired
 	private ActivoApi activoApi;
 	
@@ -131,7 +133,6 @@ public class ComercialUserAssigantionService implements UserAssigantionService  
 		TareaActivo tareaActivo = (TareaActivo)tareaExterna.getTareaPadre();
 		String codigoCartera =tareaActivo.getActivo().getCartera().getCodigo();
 		Usuario usuarioDevolver;
-		String usuarioGestorFormalizacion="gestform";
 		
 		boolean isFdv = this.isFuerzaVentaDirecta(tareaExterna);
 		boolean isConFormalizacion = this.isConFormalizacion(tareaExterna);
@@ -240,27 +241,18 @@ public class ComercialUserAssigantionService implements UserAssigantionService  
 		}
 			
 		
-		if(DDCartera.CODIGO_CARTERA_CERBERUS.equals(codigoCartera)&& (CODIGO_T017_PBC_VENTA.equals(codigoTarea)
-				|| CODIGO_T017_PBC_RESERVA.equals(codigoTarea)) ) {
-				Filter filtroUsuario = genericDao.createFilter(FilterType.EQUALS, "username",usuarioGestorFormalizacion);
-				usuarioDevolver = genericDao.get(Usuario.class, filtroUsuario);
-				
-				if(usuarioDevolver!=null) {
-				return usuarioDevolver;
-				}
-				
-		}else if(DDCartera.CODIGO_CARTERA_BANKIA.equals(codigoCartera)
-				|| DDCartera.CODIGO_CARTERA_LIBERBANK.equals(codigoCartera)
-				|| DDCartera.CODIGO_CARTERA_SAREB.equals(codigoCartera)
-				|| DDCartera.CODIGO_CARTERA_CAJAMAR.equals(codigoCartera)
-				&& CODIGO_T013_RESULTADO_PBC.equals(codigoTarea) ) {
+		if((DDCartera.CODIGO_CARTERA_CERBERUS.equals(codigoCartera) && (CODIGO_T017_PBC_VENTA.equals(codigoTarea) || CODIGO_T017_PBC_RESERVA.equals(codigoTarea))) 
+				|| ((DDCartera.CODIGO_CARTERA_BANKIA.equals(codigoCartera)
+						|| DDCartera.CODIGO_CARTERA_LIBERBANK.equals(codigoCartera)
+						|| DDCartera.CODIGO_CARTERA_SAREB.equals(codigoCartera)
+						|| DDCartera.CODIGO_CARTERA_CAJAMAR.equals(codigoCartera))
+						&& CODIGO_T013_RESULTADO_PBC.equals(codigoTarea))) {		
+			Filter filtroUsuario = genericDao.createFilter(FilterType.EQUALS, "username", USUARIO_GESTOR_FORMALIZACION);
+			usuarioDevolver = genericDao.get(Usuario.class, filtroUsuario);
 			
-				Filter filtroUsuario = genericDao.createFilter(FilterType.EQUALS, "username",usuarioGestorFormalizacion);
-				usuarioDevolver = genericDao.get(Usuario.class, filtroUsuario);
-				
-				if(usuarioDevolver!=null) {
+			if(usuarioDevolver != null) {
 				return usuarioDevolver;
-				}
+			}	
 		}
 		
 		if(GestorActivoApi.CODIGO_GESTOR_FORMALIZACION.equals(codigoGestor) || GestorActivoApi.CODIGO_GESTORIA_FORMALIZACION.equals(codigoGestor) 

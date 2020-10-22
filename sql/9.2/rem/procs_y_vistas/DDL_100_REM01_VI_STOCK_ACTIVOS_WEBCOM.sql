acb77c0e5b4c6d3dd427fade2732878c456f6ac4
Carles Molins <carles.molins@pfsgroup.es>
2020-10-22 11:59:20 +0200

    REMVIP-0000 detector

--- sql/9.2/rem/procs_y_vistas/DDL_100_REM01_VI_STOCK_ACTIVOS_WEBCOM.sql
+++ sql/9.2/rem/procs_y_vistas/DDL_100_REM01_VI_STOCK_ACTIVOS_WEBCOM.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=Juan Beltrán
---## FECHA_CREACION=20201013
+--## FECHA_CREACION=20201022
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.2
 --## INCIDENCIA_LINK=REMVIP-8168

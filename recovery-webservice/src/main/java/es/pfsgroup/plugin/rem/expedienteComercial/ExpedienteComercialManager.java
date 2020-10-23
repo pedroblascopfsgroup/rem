7b84941c449859bc6b384c413c4d1a3f57fc413a
Carles Molins <carles.molins@pfsgroup.es>
2020-10-23 15:26:57 +0200

    [REMVIP-8282]

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/expedienteComercial/ExpedienteComercialManager.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/expedienteComercial/ExpedienteComercialManager.java
@@ -2396,15 +2396,15 @@ public class ExpedienteComercialManager extends BusinessOperationOverrider<Exped
 	@Override
 	public Boolean comprobarExisteAdjuntoExpedienteComercial(Long idTrabajo, String codigoDocumento) {
 		List<AdjuntoExpedienteComercial> adjuntos = new ArrayList<AdjuntoExpedienteComercial>();
-		//List<DtoAdjunto> listaAdjuntos = new ArrayList<DtoAdjunto>();
+		List<DtoAdjunto> listaAdjuntos = new ArrayList<DtoAdjunto>();
 		
-		/*if (gestorDocumentalAdapterApi.modoRestClientActivado()) {
+		if (gestorDocumentalAdapterApi.modoRestClientActivado()) {
 			ExpedienteComercial expedienteComercial = this.findOneByTrabajo(trabajoApi.findOne(idTrabajo));
 			try {
 				listaAdjuntos = gestorDocumentalAdapterApi.getAdjuntosExpedienteComercial(expedienteComercial);
 				for (DtoAdjunto adj : listaAdjuntos) {
 					DDSubtipoDocumentoExpediente subtipoDocumento = genericDao.get(DDSubtipoDocumentoExpediente.class, 
-							genericDao.createFilter(FilterType.EQUALS, "descripcion", adj.getTdn2_desc()));
+							genericDao.createFilter(FilterType.EQUALS, "matricula", adj.getMatricula()));
 					if (subtipoDocumento != null && codigoDocumento.equals(subtipoDocumento.getCodigo())) {
 						return true;
 					}
@@ -2412,14 +2412,14 @@ public class ExpedienteComercialManager extends BusinessOperationOverrider<Exped
 			} catch (GestorDocumentalException gex) {
 				logger.error(gex.getMessage(), gex);
 			}
-		} else {*/
+		} else {
 			Filter filtroTrabajoEC = genericDao.createFilter(FilterType.EQUALS, "expediente.trabajo.id", idTrabajo);
 			Filter filtroAdjuntoSubtipoCodigo = genericDao.createFilter(FilterType.EQUALS,
 					"subtipoDocumentoExpediente.codigo", codigoDocumento);
 	
 			adjuntos = genericDao.getList(AdjuntoExpedienteComercial.class,
 					filtroTrabajoEC, filtroAdjuntoSubtipoCodigo);
-		//}
+		}
 		return !Checks.estaVacio(adjuntos);
 	}
 

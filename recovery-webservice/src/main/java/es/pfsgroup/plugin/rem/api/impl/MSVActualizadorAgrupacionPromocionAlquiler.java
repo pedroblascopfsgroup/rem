f44aefc45b515e040a0aa3f7fdd71ad71f4f77cd
Guillem Rey <guillem.rey@pfsgroup.es>
2020-10-29 17:16:58 +0100

    [HREOS-11924] control nulo maestro activos alta y campos obligatorios

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/api/impl/MSVActualizadorAgrupacionPromocionAlquiler.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/api/impl/MSVActualizadorAgrupacionPromocionAlquiler.java
@@ -865,9 +865,10 @@ public class MSVActualizadorAgrupacionPromocionAlquiler extends AbstractMSVActua
 			if (!Checks.esNulo(actSitPosAM.getSitaucionJuridica())) {
 				actSitPosUA.setSitaucionJuridica(actSitPosAM.getSitaucionJuridica());
 			}
-			if (!Checks.esNulo(actSitPosAM.getOcupado())) {
-				actSitPosUA.setOcupado(0);
-			}
+			//campos obligatorios
+			actSitPosUA.setOcupado(0);
+			actSitPosUA.setAccesoAntiocupa(0);
+			actSitPosUA.setAccesoTapiado(0);
 			actSitPosUA.setAuditoria(auditoria);
 			actSitPosUA.setActivo(unidadAlquilable);
 			

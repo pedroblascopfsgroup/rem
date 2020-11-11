03f62aeb18833f37e93271b33ed804b026ccb174
Ivan Repiso <ivan.repiso@pfsgroup.es>
2020-11-11 17:07:42 +0100

    REMVIP-8351 Error en activo

--- sql/9.3/rem/solo_produccion/DML_00528_REM01_DELETE_ACT_TBJ.sql
+++ sql/9.3/rem/solo_produccion/DML_00528_REM01_DELETE_ACT_TBJ.sql
@@ -1,7 +1,7 @@
 --/*
 --#########################################
 --## AUTOR=IVAN REPISO
---## FECHA_CREACION=20201111
+--## FECHA_CREACION=20201112
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.3
 --## INCIDENCIA_LINK=REMVIP-8351

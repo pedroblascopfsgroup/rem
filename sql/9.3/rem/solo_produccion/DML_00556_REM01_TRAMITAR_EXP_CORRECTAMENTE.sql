018377adec6b3713572aff8b7d40e226f1e4e72b
Carles Molins <carles.molins@pfsgroup.es>
2020-11-25 16:51:06 +0100

    [REMVIP-8387] Script tramitación expediente: Formalización y trámite

---
+++ sql/9.3/rem/solo_produccion/DML_00556_REM01_TRAMITAR_EXP_CORRECTAMENTE.sql
@@ -0,0 +1,99 @@
+--/*
+--##########################################
+--## AUTOR=Carles Molins
+--## FECHA_CREACION=20201125
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=REMVIP-8387
+--## PRODUCTO=NO
+--##
+--## Finalidad: Expediente mal tramitado
+--## INSTRUCCIONES:
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
+SET DEFINE OFF;
+
+DECLARE
+
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar
+    V_ESQUEMA VARCHAR2(25 CHAR):= 'REM01'; -- Configuracion Esquema
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= 'REMMASTER'; -- Configuracion Esquema Master    
+    V_USUARIO VARCHAR2(30 CHAR) := 'REMVIP-8387'; -- USUARIOCREAR/USUARIOMODIFICAR,
+    
+    V_COUNT VARCHAR2(25 CHAR);
+    V_ECO_ID VARCHAR2(25 CHAR);
+    PL_OUTPUT VARCHAR2(20000 CHAR);
+    
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+
+    TYPE T_TIPO_DATA IS TABLE OF VARCHAR2(32000 CHAR);
+    TYPE T_ARRAY_DATA IS TABLE OF T_TIPO_DATA;
+    V_TIPO_DATA T_ARRAY_DATA := T_ARRAY_DATA(
+        T_TIPO_DATA('227638')
+    ); 
+    V_TMP_TIPO_DATA T_TIPO_DATA;
+
+BEGIN
+
+    DBMS_OUTPUT.PUT_LINE('[INICIO]');
+
+    FOR I IN V_TIPO_DATA.FIRST .. V_TIPO_DATA.LAST 
+    LOOP
+    
+        V_TMP_TIPO_DATA := V_TIPO_DATA(I);
+
+        DBMS_OUTPUT.PUT_LINE('  [INFO] EXPEDIENTE NÚMERO '||V_TMP_TIPO_DATA(1)||'');
+
+        V_MSQL := 'SELECT ECO_ID FROM '||V_ESQUEMA||'.ECO_EXPEDIENTE_COMERCIAL WHERE ECO_NUM_EXPEDIENTE = '''||V_TMP_TIPO_DATA(1)||'''';
+        EXECUTE IMMEDIATE V_MSQL INTO V_ECO_ID;
+
+        V_MSQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.FOR_FORMALIZACION WHERE ECO_ID = '||V_ECO_ID;
+        EXECUTE IMMEDIATE V_MSQL INTO V_COUNT;
+        
+        IF V_COUNT = 0 THEN
+            DBMS_OUTPUT.PUT_LINE('  [INFO] INSERTAMOS FORMALIZACION');
+            V_MSQL := 'INSERT INTO '||V_ESQUEMA||'.FOR_FORMALIZACION (FOR_ID, ECO_ID, USUARIOCREAR, FECHACREAR, BORRADO) 
+                            VALUES ('||V_ESQUEMA||'.S_FOR_FORMALIZACION.NEXTVAL, '||V_ECO_ID||', '''||V_USUARIO||''', SYSDATE, 0)';
+            EXECUTE IMMEDIATE V_MSQL;
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('  [INFO] EL EXPEDIENTE YA TIENE FORMALIZACION');
+        END IF;
+        
+        V_MSQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.ECO_EXPEDIENTE_COMERCIAL ECO
+                    JOIN '||V_ESQUEMA||'.ACT_TRA_TRAMITE TRA ON TRA.TBJ_ID = ECO.TBJ_ID
+                    WHERE ECO_ID = '||V_ECO_ID;
+        EXECUTE IMMEDIATE V_MSQL INTO V_COUNT;
+
+        IF V_COUNT = 0 THEN
+            DBMS_OUTPUT.PUT_LINE('  [INFO] CREAMOS EL TRÁMITE EN LA TAREA DEFINICIÓN DE OFERTA');
+            REM01.REPOSICIONAMIENTO_TRAMITE(V_USUARIO, V_TMP_TIPO_DATA(1), 'T013_DefinicionOferta', NULL, NULL, PL_OUTPUT);
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('  [INFO] EL EXPEDIENTE YA TIENE TRÁMITE');
+        END IF;
+        
+    END LOOP;
+    
+    COMMIT;
+    DBMS_OUTPUT.PUT_LINE('[FIN]');
+
+EXCEPTION
+    WHEN OTHERS THEN
+    ERR_NUM := SQLCODE;
+    ERR_MSG := SQLERRM;
+
+    DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(ERR_NUM));
+    DBMS_OUTPUT.put_line('-----------------------------------------------------------');
+    DBMS_OUTPUT.put_line(ERR_MSG);
+    DBMS_OUTPUT.put_line(V_MSQL);
+    ROLLBACK;
+    RAISE;
+
+END;
+/
+EXIT

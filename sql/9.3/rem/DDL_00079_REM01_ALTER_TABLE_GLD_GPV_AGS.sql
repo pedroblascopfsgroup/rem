7d1450eea555138d13da185d89a07779bafe123e
Dani Albert <dani.albert@pfsgroup.es>
2020-12-04 13:53:36 +0100

    HREOS-12373 Inserción activos genéricos BBVA

--- sql/9.3/rem/DDL_00079_REM01_ALTER_TABLE_GLD_GPV_AGS.sql
+++ sql/9.3/rem/DDL_00079_REM01_ALTER_TABLE_GLD_GPV_AGS.sql
@@ -1,10 +1,10 @@
 --/*
 --######################################### 
 --## AUTOR=DAP
---## FECHA_CREACION=20200910
+--## FECHA_CREACION=20201203
 --## ARTEFACTO=batch
 --## VERSION_ARTEFACTO=9.3
---## INCIDENCIA_LINK=HREOS-11091
+--## INCIDENCIA_LINK=HREOS-12373
 --## PRODUCTO=NO
 --## 
 --## Finalidad: Añadir constraints a tablas de gastos.
@@ -37,11 +37,13 @@ DECLARE
   TYPE T_ARRAY_COL IS TABLE OF T_COL;
   V_COL T_ARRAY_COL := T_ARRAY_COL(
     T_COL('MOVE_COLUMN', 'GPV_GASTOS_PROVEEDOR', 'GPV_GASTO_SIN_ACTIVOS', 'GLD_GASTOS_LINEA_DETALLE', 'GLD_LINEA_SIN_ACTIVOS','NUMBER(1) DEFAULT 0'),
-    T_COL('DROP_COLUMN', 'ACT_AGS_ACTIVO_GENERICO_STG', 'DD_CRA_ID'),
-    T_COL('DROP_COLUMN', 'ACT_AGS_ACTIVO_GENERICO_STG', 'DD_SCR_ID'),
-    T_COL('DROP_COLUMN', 'ACT_AGS_ACTIVO_GENERICO_STG', 'AGS_ACTIVO_GENERICO'),
-    T_COL('ADD_COLUMN', 'ACT_AGS_ACTIVO_GENERICO_STG', 'PRO_ID', 'NUMBER(16) NOT NULL'),
-    T_COL('ADD_COLUMN', 'ACT_AGS_ACTIVO_GENERICO_STG', 'AGS_ACTIVO_GENERICO', 'NUMBER(16) NOT NULL'),
+    T_COL('DROP_CONSTRAINT', 'ACT_AGS_ACTIVO_GENERICO_STG', 'UK_AGS_ACTIVO_GENERICO'),
+    T_COL('DROP_CONSTRAINT', 'ACT_AGS_ACTIVO_GENERICO_STG', 'FK_AGS_PRO_ID'),
+    T_COL('DROP_COLUMN', 'ACT_AGS_ACTIVO_GENERICO_STG', 'DD_CRA_ID', '0'),
+    T_COL('DROP_COLUMN', 'ACT_AGS_ACTIVO_GENERICO_STG', 'DD_SCR_ID', '0'),
+    T_COL('DROP_COLUMN', 'ACT_AGS_ACTIVO_GENERICO_STG', 'AGS_ACTIVO_GENERICO', '1'),
+    T_COL('ADD_COLUMN', 'ACT_AGS_ACTIVO_GENERICO_STG', 'PRO_ID', 'NUMBER(16) NOT NULL', '0'),
+    T_COL('ADD_COLUMN', 'ACT_AGS_ACTIVO_GENERICO_STG', 'AGS_ACTIVO_GENERICO', 'VARCHAR2(50 CHAR)', '1'),
     T_COL('ADD_UK', 'ACT_AGS_ACTIVO_GENERICO_STG', 'UK_AGS_ACTIVO_GENERICO', 'DD_STG_ID, PRO_ID, AGS_ANYO, AGS_ACTIVO_GENERICO'),
     T_COL('ADD_FK', 'ACT_AGS_ACTIVO_GENERICO_STG', 'FK_AGS_PRO_ID', 'ACT_PRO_PROPIETARIO','PRO_ID')
   );  
@@ -96,12 +98,40 @@ BEGIN
                 EXECUTE IMMEDIATE 'ALTER TABLE '||V_ESQUEMA||'.'||V_TMP_COL(2)||' DROP COLUMN '||V_TMP_COL(3);
 
             ELSE
+
                 DBMS_OUTPUT.PUT_LINE('  [INFO] El campo '||V_TMP_COL(3)||' no existe.');
+            
             END IF;
 
         END IF;
 
-        IF 'DROP_COLUMN' = ''||V_TMP_COL(1)||'' THEN
+        IF 'DROP_CONSTRAINT' = ''||V_TMP_COL(1)||'' THEN
+
+            --Comprobacion de la tabla
+            V_SQL := 'SELECT COUNT(1) FROM ALL_TABLES WHERE OWNER = '''||V_ESQUEMA||''' AND TABLE_NAME = '''||V_TMP_COL(2)||'''';
+            EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
+
+            IF V_NUM_TABLAS = 1 THEN
+
+                V_SQL := 'SELECT COUNT(*) FROM ALL_CONSTRAINTS WHERE CONSTRAINT_NAME = '''||V_TMP_COL(3)||'''';
+                EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
+            
+                IF V_NUM_TABLAS = 1 THEN
+
+                    DBMS_OUTPUT.PUT_LINE('  [INFO] Borrando restricción única '||V_TMP_COL(3)||'');         
+                    EXECUTE IMMEDIATE 'ALTER TABLE '||V_ESQUEMA|| '.'||V_TMP_COL(2)||' DROP CONSTRAINT '||V_TMP_COL(3);     
+
+                END IF;    
+
+            ELSE
+                
+                DBMS_OUTPUT.PUT_LINE('  [INFO] La tabla '||V_ESQUEMA||'.'||V_TMP_COL(2)||' no existe.');
+            
+            END IF; 
+
+        END IF;
+
+        IF 'DROP_COLUMN' = ''||V_TMP_COL(1)||'' AND ''||V_TMP_COL(4)||'' = '0' THEN
             --Comprobacion de la tabla
             V_SQL := 'SELECT COUNT(1) FROM ALL_TAB_COLUMNS WHERE OWNER = '''||V_ESQUEMA||''' AND TABLE_NAME = '''||V_TMP_COL(2)||''' AND COLUMN_NAME = '''||V_TMP_COL(3)||'''';
             EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
@@ -112,9 +142,40 @@ BEGIN
             ELSE
                 DBMS_OUTPUT.PUT_LINE('  [INFO] No existe el campo '||V_TMP_COL(3)||'.');
             END IF; 
+
+        ELSIF 'DROP_COLUMN' = ''||V_TMP_COL(1)||'' AND ''||V_TMP_COL(4)||'' = '1' THEN
+            --Comprobacion de la tabla
+            V_SQL := 'SELECT COUNT(1) FROM ALL_TAB_COLUMNS WHERE OWNER = '''||V_ESQUEMA||''' AND TABLE_NAME = '''||V_TMP_COL(2)||''' AND COLUMN_NAME = '''||V_TMP_COL(3)||'''';
+            EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
+
+            IF V_NUM_TABLAS = 1 THEN
+
+                --Comprobacion de la tabla
+                V_SQL := 'SELECT COUNT(1) FROM ALL_TAB_COLUMNS WHERE OWNER = '''||V_ESQUEMA||''' AND TABLE_NAME = '''||V_TMP_COL(2)||''' AND COLUMN_NAME = '''||V_TMP_COL(3)||'_BACKUP''';
+                EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
+
+                IF V_NUM_TABLAS = 0 THEN
+
+                    EXECUTE IMMEDIATE 'ALTER TABLE '||V_ESQUEMA||'.'||V_TMP_COL(2)||' ADD '||V_TMP_COL(3)||'_BACKUP VARCHAR2(50 CHAR)';
+                    DBMS_OUTPUT.PUT_LINE('  [INFO] Se ha creado el campo '||V_TMP_COL(3)||'_BACKUP.'); 
+
+                END IF;
+
+                V_MSQL := 'UPDATE '||V_ESQUEMA||'.'||V_TMP_COL(2)||' SET '||V_TMP_COL(3)||'_BACKUP = '||V_TMP_COL(3);
+                EXECUTE IMMEDIATE V_MSQL;
+
+                EXECUTE IMMEDIATE 'ALTER TABLE '||V_ESQUEMA||'.'||V_TMP_COL(2)||' DROP COLUMN '||V_TMP_COL(3);
+                DBMS_OUTPUT.PUT_LINE('  [INFO] Se ha borrado el campo '||V_TMP_COL(3)||'.');      
+
+            ELSE
+
+                DBMS_OUTPUT.PUT_LINE('  [INFO] No existe el campo '||V_TMP_COL(3)||'.');
+            
+            END IF;         
+
         END IF;  
     
-        IF 'ADD_COLUMN' = ''||V_TMP_COL(1)||'' THEN
+        IF 'ADD_COLUMN' = ''||V_TMP_COL(1)||'' AND ''||V_TMP_COL(5)||'' = '0' THEN
             --Comprobacion de la tabla
  			V_SQL := 'SELECT COUNT(1) FROM ALL_TAB_COLUMNS WHERE OWNER = '''||V_ESQUEMA||''' AND TABLE_NAME = '''||V_TMP_COL(2)||''' AND COLUMN_NAME = '''||V_TMP_COL(3)||'''';
             EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
@@ -125,6 +186,29 @@ BEGIN
             ELSE
                 DBMS_OUTPUT.PUT_LINE('  [INFO] El campo '||V_TMP_COL(3)||' ya existe.');
             END IF; 
+
+        ELSIF 'ADD_COLUMN' = ''||V_TMP_COL(1)||'' AND ''||V_TMP_COL(5)||'' = '1' THEN
+            --Comprobacion de la tabla
+            V_SQL := 'SELECT COUNT(1) FROM ALL_TAB_COLUMNS WHERE OWNER = '''||V_ESQUEMA||''' AND TABLE_NAME = '''||V_TMP_COL(2)||''' AND COLUMN_NAME = '''||V_TMP_COL(3)||'''';
+            EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
+
+            IF V_NUM_TABLAS = 0 THEN
+
+                EXECUTE IMMEDIATE 'ALTER TABLE '||V_ESQUEMA||'.'||V_TMP_COL(2)||' ADD '||V_TMP_COL(3)||' '||V_TMP_COL(4);
+                DBMS_OUTPUT.PUT_LINE('  [INFO] Se ha creado el campo '||V_TMP_COL(3)||'.');     
+
+                V_MSQL := 'UPDATE '||V_ESQUEMA||'.'||V_TMP_COL(2)||' SET '||V_TMP_COL(3)||' = '||V_TMP_COL(3)||'_BACKUP';
+                EXECUTE IMMEDIATE V_MSQL;
+
+                EXECUTE IMMEDIATE 'ALTER TABLE '||V_ESQUEMA||'.'||V_TMP_COL(2)||' DROP COLUMN '||V_TMP_COL(3)||'_BACKUP';
+                DBMS_OUTPUT.PUT_LINE('  [INFO] Se ha borrado el campo '||V_TMP_COL(3)||'_BACKUP.');   
+
+            ELSE
+                
+                DBMS_OUTPUT.PUT_LINE('  [INFO] El campo '||V_TMP_COL(3)||' ya existe.');
+            
+            END IF; 
+
         END IF;
 
         IF 'ADD_UK' = ''||V_TMP_COL(1)||'' THEN
@@ -133,18 +217,23 @@ BEGIN
             EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
 
             IF V_NUM_TABLAS = 1 THEN
-                V_SQL := 'SELECT COUNT(*) FROM ALL_INDEXES WHERE INDEX_NAME = '''||V_TMP_COL(3)||'''';
+
+                V_SQL := 'SELECT COUNT(*) FROM ALL_CONSTRAINTS WHERE CONSTRAINT_NAME = '''||V_TMP_COL(3)||'''';
                 EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
             
                 IF V_NUM_TABLAS = 0 THEN
-                    DBMS_OUTPUT.PUT_LINE('  [INFO] Añadiendo índice '||V_TMP_COL(3)||'');		 
-                    EXECUTE IMMEDIATE 'CREATE UNIQUE INDEX '||V_ESQUEMA||'.'||V_TMP_COL(3)||' ON '||V_ESQUEMA|| '.'||V_TMP_COL(2)||'('||V_TMP_COL(4)||')';
-                ELSE
-                    DBMS_OUTPUT.PUT_LINE('  [INFO] La restricción '||V_TMP_COL(3)||' ya existe.');
-                END IF;           
+
+                    DBMS_OUTPUT.PUT_LINE('  [INFO] Añadiendo restricción única '||V_TMP_COL(3)||'');         
+                    EXECUTE IMMEDIATE 'ALTER TABLE '||V_ESQUEMA|| '.'||V_TMP_COL(2)||' ADD CONSTRAINT '||V_TMP_COL(3)||' UNIQUE ('||V_TMP_COL(4)||')';       
+
+                END IF;    
+
             ELSE
+                
                 DBMS_OUTPUT.PUT_LINE('  [INFO] La tabla '||V_ESQUEMA||'.'||V_TMP_COL(2)||' no existe.');
+            
             END IF; 
+        
         END IF;
 
         IF 'ADD_FK' = ''||V_TMP_COL(1)||'' THEN
@@ -153,18 +242,27 @@ BEGIN
             EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
 
             IF V_NUM_TABLAS = 1 THEN
-                V_SQL := 'SELECT COUNT(*) FROM ALL_INDEXES WHERE INDEX_NAME = '''||V_TMP_COL(3)||'''';
+
+                V_SQL := 'SELECT COUNT(*) FROM ALL_CONSTRAINTS WHERE CONSTRAINT_NAME = '''||V_TMP_COL(3)||'''';
                 EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
             
                 IF V_NUM_TABLAS = 0 THEN
+
                     DBMS_OUTPUT.PUT_LINE('  [INFO] Añadiendo clave foránea '||V_TMP_COL(3)||'');		 
                     EXECUTE IMMEDIATE 'ALTER TABLE '||V_ESQUEMA||'.'||V_TMP_COL(2)||' ADD CONSTRAINT '||V_TMP_COL(3)||' FOREIGN KEY ('||V_TMP_COL(5)||') REFERENCES '||V_ESQUEMA||'.'||V_TMP_COL(4)||' ('||V_TMP_COL(5)||')';
+                
                 ELSE
+
                     DBMS_OUTPUT.PUT_LINE('  [INFO] La restricción '||V_TMP_COL(3)||' ya existe.');
-                END IF;           
+                
+                END IF;    
+
             ELSE
+
                 DBMS_OUTPUT.PUT_LINE('  [INFO] La tabla '||V_ESQUEMA||'.'||V_TMP_COL(2)||' no existe.');
+
             END IF; 
+
         END IF;
 
     END LOOP;

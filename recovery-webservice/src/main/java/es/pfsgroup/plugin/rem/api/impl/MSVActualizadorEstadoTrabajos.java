f3cbe1a043a2101d8852ab99f0087c9481b6a06e
Vicente Martinez <vicente.martinez@pfsgroup.es>
2020-12-23 17:03:27 +0100

    HREOS-12553 - Entrefina: Error en carga masiva estados de trabajos

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/api/impl/MSVActualizadorEstadoTrabajos.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/api/impl/MSVActualizadorEstadoTrabajos.java
@@ -75,7 +75,7 @@ public class MSVActualizadorEstadoTrabajos extends AbstractMSVActualizador imple
 			trabajo.setEstado(estadoTrabajo);
 			genericDao.update(Trabajo.class, trabajo);
 			
-			if (tmpAccion.equals("REJ")) {
+			if (tmpComentario != null) {
 				agenda = new AgendaTrabajo();
 				agenda.setTrabajo(trabajo);
 				agenda.setFecha(new Date());

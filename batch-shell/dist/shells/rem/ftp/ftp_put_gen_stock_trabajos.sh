ba251c88701fdcebf43280b9742f87ea846e5053
Jorge Cañizares <jorge.canizares@pfsgroup.es>
2021-01-14 15:56:52 +0100

    GC-27737 Incluir subida de ficheros stock_activos_rem

--- batch-shell/dist/shells/rem/ftp/ftp_put_gen_stock_trabajos.sh
+++ batch-shell/dist/shells/rem/ftp/ftp_put_gen_stock_trabajos.sh
@@ -2,7 +2,6 @@ cd $DIR_SALIDA
 
 lftp -p22 -u pfs_gestion_activos,0227c8684e sftp://intercambio.haya.es <<EOF
 cd /Archivos/
-mput stock_trabajos$1.csv 
+mput stock_trabajos$1.csv stock_activos_rem$1.csv
 bye
 EOF
-

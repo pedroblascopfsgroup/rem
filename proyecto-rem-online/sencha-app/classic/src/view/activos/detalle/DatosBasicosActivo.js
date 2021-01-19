706078afd0fc8ed12d38810f9f43fc43338003b7
Lara Pablo <lara.pablo@pfsgroup.es>
2021-01-19 12:47:17 +0100

    merge

--- proyecto-rem-online/sencha-app/classic/src/view/activos/detalle/DatosBasicosActivo.js
+++ proyecto-rem-online/sencha-app/classic/src/view/activos/detalle/DatosBasicosActivo.js
@@ -218,18 +218,6 @@ Ext.define('HreRem.view.activos.detalle.DatosBasicosActivo', {
 					            	style:'margin-left:10px'
 						        },
 						        {
-									xtype: 'comboboxfieldbase',
-						        	fieldLabel:  HreRem.i18n('fieldlabel.subtipo.activo'),
-						        	reference: 'subtipoActivoCombo',
-						        	bind: {
-					            		store: '{comboSubtipoActivo}',
-					            		value: '{activo.subtipoActivoCodigo}',
-					            		disabled: '{!activo.tipoActivoCodigo}'
-					            	},
-		    						allowBlank: false,
-					            	style:'margin-left:10px'
-						        },  
-						        {
 						        	xtype: 'comboboxfieldbase',
 						        	fieldLabel: HreRem.i18n('fieldlabel.subtipo.activo.bde'),
 									reference: 'subtipoActivoComboBde',

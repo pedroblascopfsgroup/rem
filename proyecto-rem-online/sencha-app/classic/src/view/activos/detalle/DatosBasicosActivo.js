039afdcedb1840e286e425da4f69d0c701b81e5b
Daniel Algaba <daniel.algaba@pfsgroup.es>
2021-01-26 17:35:16 +0100

    HREOS-0000 Corrección merge

--- proyecto-rem-online/sencha-app/classic/src/view/activos/detalle/DatosBasicosActivo.js
+++ proyecto-rem-online/sencha-app/classic/src/view/activos/detalle/DatosBasicosActivo.js
@@ -822,6 +822,52 @@ Ext.define('HreRem.view.activos.detalle.DatosBasicosActivo', {
 									value: '{activo.motivoAplicaFormalizar}'
 								}
 							},
+							//Fila Condiciones de inclusión en perímetro Haya						
+							
+							//Fila gestión comercial
+							{
+								xtype:'checkboxfieldbase',
+								fieldLabel: HreRem.i18n('fieldlabel.perimetro.check.gestion.comercial'),
+								reference: 'checkGestionComercial',
+								bind: {
+									value: '{activo.checkGestorComercial}'
+									,readOnly: '{activo.restringido}'
+								},
+								listeners: {
+									change: 'onChkbxGestionComercialChange'
+								}
+							},
+							{
+								xtype: 'datefieldbase',
+								bind:		'{activo.fechaGestionComercial}',
+								reference: 'datefieldFechaGestionComercial',
+								readOnly: '{activo.restringido}'
+							},
+							{
+								xtype:'checkboxfieldbase',
+								fieldLabel: HreRem.i18n('fieldlabel.perimetro.check.excluir.validaciones'),
+								reference: 'checkExclusionGestionComercial',
+								bind: {
+									value: '{activo.checkExcluirValidacionesGestionComercial}', 
+									readOnly: '{activo.restringido}'
+								},
+								listeners: {
+									change: 'onChkbxExclValPerimetroChange'
+								}
+							},
+							{
+								xtype: 'comboboxfieldbase',
+								fieldLabel: HreRem.i18n('fieldlabel.perimetros.motivoDeExcluido'),
+								colspan: 3,
+								reference: 'comboMotivoGestionComercial',
+								bind: {
+									store: '{comboMotivoGestionComercialActivo}',
+									value: '{activo.motivoGestionComercialCodigo}', 
+									readOnly: '{!esEditableComboMotivoExclusionGestorComercial}',
+									allowBlank: '{!esEditableComboMotivoExclusionGestorComercial}'
+
+								}
+							},
 							
 							//Bloque Comercialización
 							{    

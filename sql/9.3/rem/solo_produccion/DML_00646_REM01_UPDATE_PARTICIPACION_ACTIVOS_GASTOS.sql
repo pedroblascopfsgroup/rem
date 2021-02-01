b92d4156e72d44bd83f4f4a4e45b7b7f0b7606e7
Dani Albert <dani.albert@pfsgroup.es>
2021-02-01 18:13:43 +0100

    REMVIP-8708 Update

--- sql/9.3/rem/solo_produccion/DML_00646_REM01_UPDATE_PARTICIPACION_ACTIVOS_GASTOS.sql
+++ sql/9.3/rem/solo_produccion/DML_00646_REM01_UPDATE_PARTICIPACION_ACTIVOS_GASTOS.sql
@@ -38,114 +38,25 @@ BEGIN
 
     V_MSQL := 'MERGE INTO '||V_ESQUEMA||'.GLD_ENT T1
         USING (
-        SELECT AUX.GLD_ENT_ID, AUX.PARTICIPACION_CLI
-        FROM (
-            WITH SUPLIDOS AS (
-                SELECT PSU.TBJ_ID
-                        , NVL(
-                        SUM(
-                            CASE
-                                WHEN TAD.DD_TAD_CODIGO = ''01''
-                                    THEN -NVL(PSU.PSU_IMPORTE, 0)
-                                WHEN TAD.DD_TAD_CODIGO = ''02''
-                                    THEN NVL(PSU.PSU_IMPORTE, 0)
-                                ELSE NVL(PSU.PSU_IMPORTE, 0)
-                            END
-                        )
-                    , 0) IMPORTE_PROV_SUPL
-                FROM '||V_ESQUEMA||'.ACT_PSU_PROVISION_SUPLIDO PSU 
-                JOIN '||V_ESQUEMA||'.ACT_TBJ_TRABAJO TBJ ON TBJ.TBJ_ID = PSU.TBJ_ID
-                    AND TBJ.BORRADO = 0
-                LEFT JOIN '||V_ESQUEMA||'.DD_TAD_TIPO_ADELANTO TAD ON TAD.DD_TAD_ID = PSU.DD_TAD_ID
-                    AND TAD.BORRADO = 0
-                WHERE PSU.BORRADO = 0
-                GROUP BY PSU.TBJ_ID
-            )
-            , PART_TBJ_LIN AS ( 
-                SELECT
-                       GLD.GPV_ID
-                     , GLD.GLD_ID
-                     , TBJ.TBJ_ID
-                     , (TBJ.TBJ_IMPORTE_PRESUPUESTO + NVL(SUP.IMPORTE_PROV_SUPL, 0)) / SUM(TBJ.TBJ_IMPORTE_PRESUPUESTO + NVL(SUP.IMPORTE_PROV_SUPL, 0))
-                                                       OVER(PARTITION BY GLD.GLD_ID)       PART_TBJ_LIN_PVE
-                     , (TBJ.TBJ_IMPORTE_TOTAL + NVL(SUP.IMPORTE_PROV_SUPL, 0)) / SUM(TBJ.TBJ_IMPORTE_TOTAL + NVL(SUP.IMPORTE_PROV_SUPL, 0))
-                                                 OVER(PARTITION BY GLD.GLD_ID)       PART_TBJ_LIN_CLI
-                 FROM
-                        '||V_ESQUEMA||'.GLD_TBJ GTB
-                     JOIN '||V_ESQUEMA||'.GLD_GASTOS_LINEA_DETALLE    GLD ON GLD.GLD_ID = GTB.GLD_ID
-                      AND GLD.BORRADO = 0
-                     JOIN '||V_ESQUEMA||'.ACT_TBJ_TRABAJO             TBJ ON TBJ.TBJ_ID = GTB.TBJ_ID
-                      AND TBJ.BORRADO = 0
-                    LEFT JOIN SUPLIDOS SUP ON SUP.TBJ_ID = TBJ.TBJ_ID
-                WHERE
-                      (
-                        (
-                            NVL(TBJ.TBJ_IMPORTE_PRESUPUESTO, 0) <> 0
-                            AND NVL(TBJ.TBJ_IMPORTE_TOTAL, 0) <> 0
-                        )
-                        OR (
-                            NVL(SUP.IMPORTE_PROV_SUPL, 0) <> 0
-                        )
-                     )
-                      AND GTB.BORRADO = 0
-            ), PRINCIPAL AS (
-                SELECT PTL.GPV_ID, PTL.GLD_ID, GEN.GLD_ENT_ID
-                    , CASE WHEN ROUND(ATB.ACT_TBJ_PARTICIPACION * PTL.PART_TBJ_LIN_PVE, 4) < 0.01 THEN 0.01 ELSE ROUND(ATB.ACT_TBJ_PARTICIPACION * PTL.PART_TBJ_LIN_PVE, 4) END PARTICIPACION_PVE
-                    , CASE WHEN ROUND(ATB.ACT_TBJ_PARTICIPACION * PTL.PART_TBJ_LIN_CLI, 4) < 0.01 THEN 0.01 ELSE ROUND(ATB.ACT_TBJ_PARTICIPACION * PTL.PART_TBJ_LIN_CLI, 4) END PARTICIPACION_CLI
-                    , CASE WHEN ROUND(ATB.ACT_TBJ_PARTICIPACION * PTL.PART_TBJ_LIN_PVE, 4) < 0.01 THEN 0.01 ELSE ROUND(ATB.ACT_TBJ_PARTICIPACION * PTL.PART_TBJ_LIN_PVE, 4) END + 100 - SUM(CASE WHEN ROUND(ATB.ACT_TBJ_PARTICIPACION * PTL.PART_TBJ_LIN_PVE, 4) < 0.01 THEN 0.01 ELSE ROUND(ATB.ACT_TBJ_PARTICIPACION * PTL.PART_TBJ_LIN_PVE, 4) END) OVER(PARTITION BY PTL.GLD_ID) AJUSTE_PVE
-                    , CASE WHEN ROUND(ATB.ACT_TBJ_PARTICIPACION * PTL.PART_TBJ_LIN_CLI, 4) < 0.01 THEN 0.01 ELSE ROUND(ATB.ACT_TBJ_PARTICIPACION * PTL.PART_TBJ_LIN_CLI, 4) END + 100 - SUM(CASE WHEN ROUND(ATB.ACT_TBJ_PARTICIPACION * PTL.PART_TBJ_LIN_CLI, 4) < 0.01 THEN 0.01 ELSE ROUND(ATB.ACT_TBJ_PARTICIPACION * PTL.PART_TBJ_LIN_CLI, 4) END) OVER(PARTITION BY PTL.GLD_ID) AJUSTE_CLI
-                    , ROW_NUMBER() OVER(PARTITION BY PTL.GLD_ID ORDER BY PTL.PART_TBJ_LIN_CLI DESC) RN_CLI
-                FROM PART_TBJ_LIN PTL
-                JOIN '||V_ESQUEMA||'.GLD_ENT GEN ON GEN.GLD_ID = PTL.GLD_ID
-                    AND GEN.BORRADO = 0
-                JOIN '||V_ESQUEMA||'.DD_ENT_ENTIDAD_GASTO ENT ON ENT.DD_ENT_ID = GEN.DD_ENT_ID
-                    AND ENT.BORRADO = 0
-                JOIN '||V_ESQUEMA||'.ACT_TBJ ATB ON ATB.ACT_ID = GEN.ENT_ID
-                    AND ATB.TBJ_ID = PTL.TBJ_ID
-                WHERE ENT.DD_ENT_CODIGO = ''ACT''
-            ), RN_1 AS (
-                SELECT GPV_ID, GLD_ID, GLD_ENT_ID
-                    , SUM(AJUSTE_PVE) PARTICIPACION_PVE, SUM(AJUSTE_CLI) PARTICIPACION_CLI
-                FROM PRINCIPAL
-                WHERE RN_CLI = 1
-                GROUP BY GPV_ID, GLD_ID, GLD_ENT_ID
-            ), RN_RESTO AS (
-                SELECT GPV_ID, GLD_ID, GLD_ENT_ID, SUM(PARTICIPACION_PVE) PARTICIPACION_PVE, SUM(PARTICIPACION_CLI) PARTICIPACION_CLI
-                FROM PRINCIPAL
-                WHERE RN_CLI > 1
-                GROUP BY GPV_ID, GLD_ID, GLD_ENT_ID
-            )
-            SELECT GPV_ID, GLD_ID, GLD_ENT_ID, PARTICIPACION_PVE, PARTICIPACION_CLI
-            FROM RN_1
-            UNION
-            SELECT GPV_ID, GLD_ID, GLD_ENT_ID, PARTICIPACION_PVE, PARTICIPACION_CLI
-            FROM RN_RESTO
-        ) AUX
-        WHERE EXISTS (
-                WITH GASTOS_MAS_1_TRABAJO AS (
-                    SELECT GLD.GPV_ID, COUNT(1) 
-                    FROM '||V_ESQUEMA||'.GLD_GASTOS_LINEA_DETALLE GLD 
-                    INNER JOIN '||V_ESQUEMA||'.GLD_TBJ GTBJ ON GTBJ.GLD_ID = GLD.GLD_ID 
+            SELECT AUX.GLD_ENT_ID, SUM(AUX.PARTICIPACION_CLI) PARTICIPACION_CLI
+            FROM '||V_ESQUEMA||'.V_REPARTO_IMP_TBJ AUX
+            WHERE EXISTS (
+                    SELECT GPV.GPV_ID, GPV.GPV_NUM_GASTO_HAYA, GEN.GLD_ID, COUNT(1) 
+                    FROM REM01.GLD_GASTOS_LINEA_DETALLE GLD 
+                    INNER JOIN REM01.GLD_TBJ GTBJ ON GTBJ.GLD_ID = GLD.GLD_ID 
                         AND GTBJ.BORRADO = 0
-                    INNER JOIN '||V_ESQUEMA||'.GPV_GASTOS_PROVEEDOR GPV ON GPV.GPV_ID = GLD.GPV_ID 
+                    INNER JOIN REM01.GPV_GASTOS_PROVEEDOR GPV ON GPV.GPV_ID = GLD.GPV_ID 
                         AND GPV.BORRADO = 0
+                    INNER JOIN REM01.GLD_ENT GEN ON GEN.GLD_ID = GLD.GLD_ID
+                        AND GEN.BORRADO = 0
                     WHERE GLD.USUARIOCREAR = ''HREOS-10574''
                         AND GLD.BORRADO = 0
                         AND GPV.DD_EGA_ID IN (1,2,8,10,12)
-                    GROUP BY GLD.GPV_ID
+                        AND GPV.GPV_ID = AUX.GPV_ID
+                    GROUP BY GPV.GPV_ID, GPV.GPV_NUM_GASTO_HAYA, GEN.GLD_ID
+                    HAVING SUM(GEN.GLD_PARTICIPACION_GASTO) <> 100
                 )
-                SELECT 1
-                FROM GASTOS_MAS_1_TRABAJO MAS
-                JOIN '||V_ESQUEMA||'.GPV_GASTOS_PROVEEDOR GPV ON GPV.GPV_ID = MAS.GPV_ID
-                    AND GPV.BORRADO = 0
-                JOIN '||V_ESQUEMA||'.GLD_GASTOS_LINEA_DETALLE GLD ON GLD.GPV_ID = GPV.GPV_ID
-                    AND GLD.BORRADO = 0
-                JOIN '||V_ESQUEMA||'.GLD_ENT GEN ON GEN.GLD_ID = GLD.GLD_ID
-                    AND GEN.BORRADO = 0
-                WHERE GPV.GPV_ID = AUX.GPV_ID
-                GROUP BY GPV.GPV_ID, GPV.GPV_NUM_GASTO_HAYA, GEN.GLD_ID
-                HAVING SUM(GEN.GLD_PARTICIPACION_GASTO) <> 100
-            )
+            GROUP BY AUX.GLD_ENT_ID
         ) T2
         ON (T1.GLD_ENT_ID = T2.GLD_ENT_ID)
         WHEN MATCHED THEN

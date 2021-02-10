1612701e6c4d37cbcaae6bb66294a330fa2a1788
Sento Visiedo <vicente.rubio@pfsgroup.es>
2021-02-10 11:06:32 +0100

    HREOS-13072  BBVA Visibilidad usuarios BBVA CR visibilidad de documentación

---
+++ sql/9.3/rem/DML_00549_REM01_INSERT_PERFIL_CARTERA_BBVA.sql
@@ -0,0 +1,100 @@
+--/*
+--##########################################
+--## AUTOR=Sento Visiedo
+--## FECHA_CREACION=20210213
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=HREOS-13072
+--## PRODUCTO=NO
+--##
+--## Finalidad: Script que añade los usuarios de la tabla 
+--## UCA_USUARIO_CARTERA a la nueva siempre que sean de BBVA
+--## INSTRUCCIONES:
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF; 
+DECLARE
+  V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejECVtar    
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquemas
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+	V_NUM_FILAS NUMBER(16);
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+	V_USU VARCHAR2(30 CHAR) := 'HREOS-13072'; -- USUARIOCREAR/USUARIOMODIFICAR.
+	V_PERFIL VARCHAR2(30 CHAR) := 'CARTERA_BBVA';
+	V_PERFIL_DESC VARCHAR2(100 CHAR) := 'Perfil Cartera BBVA';
+
+
+BEGIN	
+
+
+	DBMS_OUTPUT.PUT_LINE('[INICIO] TABLA PEF_PERFILES');
+	
+	V_MSQL := 'SELECT COUNT(*) FROM '||V_ESQUEMA||'.PEF_PERFILES WHERE PEF_CODIGO = '''||V_PERFIL||''' AND BORRADO = 0';
+	EXECUTE IMMEDIATE V_MSQL INTO V_NUM_FILAS;
+	
+	IF V_NUM_FILAS = 0 THEN
+	
+		V_MSQL := 'INSERT INTO '||V_ESQUEMA||'.PEF_PERFILES (PEF_ID, PEF_CODIGO, PEF_DESCRIPCION, PEF_DESCRIPCION_LARGA,
+					USUARIOCREAR, FECHACREAR)
+					SELECT '||V_ESQUEMA||'.S_PEF_PERFILES.NEXTVAL,'''||V_PERFIL||''','''||V_PERFIL_DESC||''','''||V_PERFIL_DESC||''',
+					'''||V_USU||''',SYSDATE FROM DUAL';
+	
+		EXECUTE IMMEDIATE V_MSQL;
+    
+		DBMS_OUTPUT.PUT_LINE('[FIN] PERFIL INSERTADO');
+		
+	ELSE
+		
+		DBMS_OUTPUT.PUT_LINE('[FIN] PERFIL YA EXISTE');
+		
+	END IF;
+
+	DBMS_OUTPUT.PUT_LINE('[INICIO] TABLA ZON_PEF_USU');
+
+	V_MSQL := 'SELECT COUNT(*) FROM '||V_ESQUEMA||'.PEF_PERFILES WHERE PEF_CODIGO = '''||V_PERFIL||''' AND BORRADO = 0';
+	EXECUTE IMMEDIATE V_MSQL INTO V_NUM_FILAS;
+
+	IF V_NUM_FILAS = 0 THEN
+
+		DBMS_OUTPUT.PUT_LINE('[FIN] PERFIL NO EXISTE');
+
+	ELSE
+
+		DBMS_OUTPUT.PUT_LINE('[INFO] INSERTAMOS USUARIOS EN ZON_PEF_USU ASOCIADO AL PERFIL '||V_PERFIL||'.');
+   
+      V_MSQL := 'INSERT INTO '||V_ESQUEMA||'.ZON_PEF_USU (ZPU_ID, USUARIOCREAR, FECHACREAR, ZON_ID, PEF_ID, USU_ID )  
+          SELECT S_ZON_PEF_USU.NEXTVAL,
+          '''||V_USU||''', SYSDATE,
+					(SELECT ZON_ID FROM '||V_ESQUEMA||'.ZON_ZONIFICACION WHERE ZON_DESCRIPCION = ''REM'' AND BORRADO = 0),
+					(SELECT PEF_ID FROM '||V_ESQUEMA||'.PEF_PERFILES WHERE PEF_CODIGO = '''||V_PERFIL||''' AND BORRADO = 0),
+          USU_ID
+          FROM '||V_ESQUEMA_M||'.USU_USUARIOS USU WHERE EXISTS
+          (SELECT UCA.USU_ID FROM '||V_ESQUEMA||'.UCA_USUARIO_CARTERA UCA WHERE USU.usu_id = UCA.usu_id and DD_CRA_ID = 
+          (SELECT DD_CRA_ID FROM DD_CRA_CARTERA WHERE DD_CRA_CODIGO = 16))';
+      EXECUTE IMMEDIATE V_MSQL;
+
+		DBMS_OUTPUT.PUT_LINE('[INFO] Datos de la tabla '||V_ESQUEMA||'.ZON_PEF_USU insertados correctamente.');
+
+	END IF;
+
+	COMMIT;
+ 
+EXCEPTION
+     WHEN OTHERS THEN
+          ERR_NUM := SQLCODE;
+          ERR_MSG := SQLERRM;
+          DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(ERR_NUM));
+          DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+          DBMS_OUTPUT.put_line(ERR_MSG);
+          ROLLBACK;
+          RAISE;   
+END;
+/
+EXIT;

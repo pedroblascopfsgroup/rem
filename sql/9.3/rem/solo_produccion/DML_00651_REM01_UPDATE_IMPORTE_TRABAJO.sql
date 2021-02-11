3ddb678927cfba01774ca7291baf5143e9ae48cf
Juan Bautista <juan.bautista@pfsgroup.es>
2021-02-11 09:26:18 +0100

    REMVIP-8841 DND Volcar importe cliente

--- sql/9.3/rem/solo_produccion/DML_00651_REM01_UPDATE_IMPORTE_TRABAJO.sql
+++ sql/9.3/rem/solo_produccion/DML_00651_REM01_UPDATE_IMPORTE_TRABAJO.sql
@@ -1,7 +1,7 @@
 --/*
 --#########################################
 --## AUTOR=Juan Bautista Alfonso
---## FECHA_CREACION=20210209
+--## FECHA_CREACION=20210211
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.3
 --## INCIDENCIA_LINK=REMVIP-8841

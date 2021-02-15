4008e446145fbeba32ea250450578494cd1e6c68
Guillem Rey <guillem.rey@pfsgroup.es>
2021-02-15 11:56:54 +0100

    [REMVIP-8943] crear trabajo proveedor paci bbva

--- proyecto-rem-online/sencha-app/classic/src/view/trabajos/detalle/TrabajoDetalleController.js
+++ proyecto-rem-online/sencha-app/classic/src/view/trabajos/detalle/TrabajoDetalleController.js
@@ -2041,8 +2041,11 @@ Ext.define('HreRem.view.trabajos.detalle.TrabajoDetalleController', {
     			if(data != null && data.data != null && data.data.id != null){
     				var idProveedor = data.data.id;
 					var nombreProveedor = data.data.nombre;
-    				me.getView().lookupReference('comboProveedorGestionEconomica2').setValue(idProveedor);
-					me.getView().lookupReference('comboProveedorGestionEconomica2').setRawValue(nombreProveedor);
+					var record = new Ext.data.Model({idProveedor: idProveedor, nombreComercial: nombreProveedor});
+					me.getView().lookupReference('comboProveedorGestionEconomica2').getStore().add(record);
+    				me.getView().lookupReference('comboProveedorGestionEconomica2').setValue(record);
+
+					me.onChangeProveedorCombo();
     			}
     		},
     		failure: function () {

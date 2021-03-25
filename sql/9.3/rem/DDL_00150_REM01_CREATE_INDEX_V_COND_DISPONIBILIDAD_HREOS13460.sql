1a0e32a65aee3e80c27f030fe4d65ca1c7ca477c
Sergio Gomez <sergio.gomez@pfsgroup.es>
2021-03-25 16:20:56 +0100

    HREOS-13460 Optimizacion vista condicionantes disponibilidad

---
+++ sql/9.3/rem/DDL_00150_REM01_CREATE_INDEX_V_COND_DISPONIBILIDAD_HREOS13460.sql
@@ -0,0 +1,97 @@
+--/*
+--##########################################
+--## AUTOR=Sergio Gomez
+--## FECHA_CREACION=20210325
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=HREOS-13460
+--## PRODUCTO=NO
+--## Finalidad: Creación índices para V_COND_DISPONIBILIDAD
+--##           
+--## INSTRUCCIONES: Configurar las variables necesarias en el principio del DECLARE
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+
+--Para permitir la visualización de texto en un bloque PL/SQL utilizando DBMS_OUTPUT.PUT_LINE
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF;
+
+
+DECLARE
+
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar    
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+    
+    V_TABLESPACE_IDX VARCHAR2(25 CHAR):= '#TABLESPACE_INDEX#'; -- Configuracion Tablespace de Indices
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.  
+    V_NUM_SEQ NUMBER(16); -- Vble. para validar la existencia de una secuencia.  
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+    V_NUM_INDEX NUMBER(3); -- Vble. auxiliar para la numeración de índices
+    V_TABLA VARCHAR2(50 CHAR); -- Vble. auxiliar para almacenar el nombres de tablas
+    V_INDICE VARCHAR2(50 CHAR); -- Vble. auxiliar para almacenar el nombre del índice
+    V_NUM_COLS_INDEX NUMBER(2);--  Vble. auxiliar que indica el numero de columnas a los que apunta el ínidce
+    V_TEXT1 VARCHAR2(2400 CHAR); -- Vble. auxiliar 
+
+    --ARRAY PARA CREAR INDEX
+    TYPE T_TIPO_DATA IS TABLE OF VARCHAR2(150);
+    TYPE T_ARRAY_DATA IS TABLE OF T_TIPO_DATA;
+    V_TIPO_DATA T_ARRAY_DATA := T_ARRAY_DATA(
+                -- Nombre INDEX                     TABLA                   COLUMNAS
+        T_TIPO_DATA('IDX_TAG_DD_TAG_CODIGO', 'DD_TAG_TIPO_AGRUPACION', 'BORRADO,DD_TAG_CODIGO'),
+        T_TIPO_DATA('IDX_HOR_INSC_ACT_ID_BORR', 'ACT_REG_INFO_REGISTRAL', 'BORRADO,ACT_ID,REG_DIV_HOR_INSCRITO'),
+        T_TIPO_DATA('IDX_AGA_PPAL_ACT_ID', 'ACT_AGA_AGRUPACION_ACTIVO', 'BORRADO,AGA_PRINCIPAL,ACT_ID'),
+        T_TIPO_DATA('IDX_SPS_OCUPADO_BORR', 'ACT_SPS_SIT_POSESORIA', 'BORRADO,SPS_OCUPADO'),
+        T_TIPO_DATA('IDX_TPA_COD_BORR', 'DD_TPA_TIPO_TITULO_ACT', 'BORRADO,DD_TPA_CODIGO'),
+        T_TIPO_DATA('IDX_EON_COD_BORR', 'DD_EON_ESTADO_OBRA_NUEVA', 'BORRADO,DD_EON_CODIGO'),
+        T_TIPO_DATA('IDX_EAC_COD_BORR', 'DD_EAC_ESTADO_ACTIVO', 'BORRADO,DD_EAC_CODIGO'),
+        T_TIPO_DATA('IDX_CRA_COD_BORR', 'DD_CRA_CARTERA', 'BORRADO,DD_CRA_CODIGO'),
+        T_TIPO_DATA('IDX_ICO_POSIBLE_ACT_ID_ICO_ID_BORR', 'ACT_ICO_INFO_COMERCIAL', 'BORRADO,ICO_POSIBLE_HACER_INF,ICO_ID,ACT_ID')
+
+    ); 
+    V_TMP_TIPO_DATA T_TIPO_DATA;
+
+    BEGIN
+
+        FOR I IN V_TIPO_DATA.FIRST .. V_TIPO_DATA.LAST
+        LOOP
+            V_TMP_TIPO_DATA := V_TIPO_DATA(I);    	
+
+            V_MSQL := 'SELECT COUNT(1) FROM ALL_INDEXES WHERE INDEX_NAME = '||V_TMP_TIPO_DATA(1)||' ';        
+            EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;
+
+            IF V_NUM_TABLAS = 0 THEN
+                V_MSQL := 'CREATE INDEX '||V_ESQUEMA||'.'||V_TMP_TIPO_DATA(1)||' ON '||V_ESQUEMA||'.'||V_TMP_TIPO_DATA(2)||' ('||V_TMP_TIPO_DATA(3)||') TABLESPACE '||V_TABLESPACE_IDX;		
+                EXECUTE IMMEDIATE V_MSQL;
+                DBMS_OUTPUT.PUT_LINE('[INFO] Indice '||V_TMP_TIPO_DATA(1)||' - creado.');
+            ELSE
+                DBMS_OUTPUT.PUT_LINE('[INFO] Ya existe el índice '||V_TMP_TIPO_DATA(1)||'');
+            END IF;
+        END LOOP;
+	COMMIT;
+
+
+
+EXCEPTION
+     WHEN OTHERS THEN 
+         DBMS_OUTPUT.PUT_LINE('KO!');
+          err_num := SQLCODE;
+          err_msg := SQLERRM;
+
+          DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(err_num));
+          DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+          DBMS_OUTPUT.put_line(err_msg);
+
+          ROLLBACK;
+          RAISE;          
+
+END;
+
+/
+
+EXIT
--- sql/9.3/rem/procs_y_vistas/DDL_010_REM01_VI_CONDICIONANTES_DISPONIBILIDAD.sql
+++ sql/9.3/rem/procs_y_vistas/DDL_010_REM01_VI_CONDICIONANTES_DISPONIBILIDAD.sql
@@ -1,10 +1,10 @@
 --/*
 --##########################################
---## AUTOR=Juan Bautista Alfonso
---## FECHA_CREACION=20200826
+--## AUTOR=Sergio Gomez
+--## FECHA_CREACION=20210325
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.2
---## INCIDENCIA_LINK=REMVIP-7935
+--## INCIDENCIA_LINK=HREOS-13460
 --## PRODUCTO=NO
 --## Finalidad: DDL
 --##           
@@ -33,6 +33,7 @@
 --##        0.20 GUILLEM REY - REMVIP-4606 - Discleimer "Ocupado con título" para Activos Matrices
 --##        0.21 Remus Ovidiu - REMVIP-5203 - Quitamos activos Bankia del calculo de posesion por fecha de posesion, ya que estos se calculan por situacion juridica
 --##        0.22 Juan Bautista Alfonso - - REMVIP-7935 - Modificado fecha posesion para que cargue de la vista V_FECHA_POSESION_ACTIVO
+--#         0.23 Sergio Gomez - - HREOS-13460 - Ajustes joins 
 --##########################################
 --*/
 
@@ -91,43 +92,40 @@ BEGIN
                                                          )
 AS
    SELECT act_id, sin_toma_posesion_inicial, ocupado_contitulo, pendiente_inscripcion, proindiviso, tapiado, obranueva_sindeclarar, obranueva_enconstruccion, divhorizontal_noinscrita, ruina, vandalizado, otro, combo_otro,
-          sin_informe_aprobado, sin_informe_aprobado_REM, revision, procedimiento_judicial, con_cargas, sin_acceso, ocupado_sintitulo, estado_portal_externo, DECODE (est_disp_com_codigo1, ''01'', 1, 0) AS es_condicionado,
+          sin_informe_aprobado, sin_informe_aprobado_REM, revision, procedimiento_judicial, con_cargas, sin_acceso, ocupado_sintitulo, estado_portal_externo, DECODE (est_disp_com_codigo1, '01', 1, 0) AS es_condicionado,
           est_disp_com_codigo2,borrado
 
-     FROM (SELECT DISTINCT act.act_id, 
-				CASE WHEN (sps1.dd_sij_id is not null and sij.DD_SIJ_INDICA_POSESION = 0) 
-                    THEN 1 
-                    ELSE 
-						CASE WHEN (FPA.FECHA_POSESION IS NULL AND aba2.dd_cla_id = 2 and act.dd_cra_id <> 21) 
-							THEN 1 
-							ELSE 0 
-						END 
-                END AS sin_toma_posesion_inicial,        
-                CASE WHEN ((sps1.sps_ocupado = 1 AND TPA.DD_TPA_CODIGO = ''01'') OR ua.act_id is not null) THEN 1 ELSE 0 END AS ocupado_contitulo,
+     FROM (SELECT act.act_id,
+				CASE WHEN (sps1.dd_sij_id is not null and sij.DD_SIJ_INDICA_POSESION = 0) THEN 1
+                ELSE
+                    CASE WHEN (sps1.sps_fecha_toma_posesion IS NULL AND aba2.dd_cla_id = 2 and act.dd_cra_id <> 21) THEN 1
+                    ELSE 0
+				END
+                END AS sin_toma_posesion_inicial,
+                CASE WHEN (sps1.sps_ocupado = 1 AND TPA.DD_TPA_CODIGO = ''01'' OR ua.act_id is not null) THEN 1 ELSE 0 END AS ocupado_contitulo,
                 NVL2 (tit.act_id, 0, 1) AS pendiente_inscripcion,
                 NVL2 (npa.act_id, 1, 0) AS proindiviso,
-                CASE WHEN sps1.sps_acc_tapiado = 1 THEN 1 ELSE 0 END AS tapiado, 
+                CASE WHEN sps1.sps_acc_tapiado = 1 THEN 1 ELSE 0 END AS tapiado,
                 NVL2 (eon.dd_eon_id, 1, 0) AS obranueva_sindeclarar,
-                CASE WHEN (eac1.dd_eac_codigo = ''02''  OR   eac1.dd_eac_codigo = ''06'') THEN 1 ELSE 0 END as obranueva_enconstruccion, 
-                NVL2 (reg2.reg_id, 1, 0) AS divhorizontal_noinscrita, 
+                CASE WHEN (eac1.dd_eac_codigo = ''02''  OR   eac1.dd_eac_codigo = ''06'') THEN 1 ELSE 0 END as obranueva_enconstruccion,
+                NVL2 (reg2.reg_id, 1, 0) AS divhorizontal_noinscrita,
                 CASE WHEN eac1.dd_eac_codigo = ''05'' THEN 1 ELSE 0 END as ruina,
-				CASE WHEN (eac1.dd_eac_codigo IN (''08'',''07'')) THEN 1 ELSE 0 END as VANDALIZADO,
+				CASE WHEN (eac1.dd_eac_codigo = ''08''  OR   eac1.dd_eac_codigo = ''07'') THEN 1 ELSE 0 END as VANDALIZADO,
                 sps1.sps_otro AS otro,
 				sps1.sps_combo_otro AS combo_otro,
-				CASE WHEN (cra.dd_cra_codigo in (''01'', ''08'', ''02'' )) 
+				CASE WHEN (cra.dd_cra_codigo = ''01'' OR cra.dd_cra_codigo = ''02'' OR cra.dd_cra_codigo = ''08'')
                     THEN DECODE (vei.dd_aic_codigo, ''02'', 0, 1)
-                    ELSE 0	
+                    ELSE 0
 				END AS sin_informe_aprobado,
 				DECODE (vei.dd_aic_codigo, ''02'', 0, 1) AS sin_informe_aprobado_REM,
                 0 AS revision,                                                                                      --NO EXISTE EN REM
 				0 AS procedimiento_judicial,                                                          --NO EXISTE EN REM
 				NVL2 (vcg.con_cargas, vcg.con_cargas, 0) AS con_cargas,
-                DECODE (ico.ico_posible_hacer_inf, 1, 0, 0, 1, 0) AS sin_acceso,                                                                                                            
-                CASE WHEN (sps1.sps_ocupado = 1 AND (TPA.DD_TPA_CODIGO = ''02'' OR TPA.DD_TPA_CODIGO = ''03'')) THEN 1 ELSE 0 END AS ocupado_sintitulo, 
+                DECODE (ico.ico_posible_hacer_inf, 1, 0, 0, 1, 0) AS sin_acceso,
+                CASE WHEN (sps1.sps_ocupado = 1 AND (TPA.DD_TPA_CODIGO = ''02'' OR TPA.DD_TPA_CODIGO = ''03'')) THEN 1 ELSE 0 END AS ocupado_sintitulo,
                 CASE WHEN (sps1.sps_estado_portal_externo = 1) THEN 1 ELSE 0 END AS estado_portal_externo,  -- ESTADO PUBLICACION PORTALES EXTERNOS
-                CASE WHEN ( (FPA.FECHA_POSESION IS NULL AND aba2.dd_cla_id = 2)               -- SIN TOMA POSESION INICIAL
+                CASE WHEN ( (sps1.sps_fecha_toma_posesion IS NULL AND aba2.dd_cla_id = 2)               -- SIN TOMA POSESION INICIAL
                            OR eac1.dd_eac_codigo=''05''                                                   -- RUINA
-
                            OR NVL2 (tit.act_id, 0, 1) = 1
                            OR NVL2 (eon.dd_eon_id, 1, 0) = 1
                            OR NVL2 (npa.act_id, 1, 0) = 1
@@ -136,56 +134,48 @@ AS
                            OR sps1.sps_acc_tapiado = 1                                                  -- TAPIADO
                            OR (sps1.sps_ocupado = 1 AND (TPA.DD_TPA_CODIGO = ''02'' OR TPA.DD_TPA_CODIGO = ''03''))                        -- OCUPADO SIN TITULO
                            OR NVL2 (reg2.reg_id, 1, 0) = 1
-
-                           OR NVL2(sps1.sps_otro,1,0) = 1                                               -- OTROS MOTIVOS                           
-						   --OR DECODE(VEI.DD_AIC_CODIGO ,''02'' ,0 , 1) = 1                              -- sin_informe_aprobado         	 
-                          ) 
+                           OR NVL2(sps1.sps_otro,1,0) = 1                                               -- OTROS MOTIVOS
+                          )
 						   OR NVL2 (vcg.con_cargas, vcg.con_cargas, 0) = 1
 					THEN ''01''
                     ELSE ''02''
-                  END AS est_disp_com_codigo1,	
+                  END AS est_disp_com_codigo1,
                   vact.est_disp_com_codigo as est_disp_com_codigo2,
                   0 AS borrado
 
 
-             FROM '||V_ESQUEMA||'.act_activo act LEFT JOIN '||V_ESQUEMA||'.act_aba_activo_bancario aba2 ON aba2.act_id = act.act_id 
-				  
-				  LEFT JOIN '||V_ESQUEMA||'.DD_CRA_CARTERA cra ON cra.dd_cra_id = act.dd_cra_id 	
-				  LEFT JOIN '||V_ESQUEMA||'.dd_eac_estado_activo eac1 ON eac1.dd_eac_id = act.dd_eac_id                  
-                  LEFT JOIN '||V_ESQUEMA||'.act_sps_sit_posesoria sps1 ON sps1.act_id = act.act_id   
-				  LEFT JOIN '||V_ESQUEMA||'.V_FECHA_POSESION_ACTIVO FPA ON FPA.ACT_ID = ACT.ACT_ID             
-				  LEFT JOIN '||V_ESQUEMA||'.DD_TPA_TIPO_TITULO_ACT TPA ON TPA.DD_TPA_ID = SPS1.DD_TPA_ID
-				  LEFT JOIN '||V_ESQUEMA||'.DD_SIJ_SITUACION_JURIDICA sij on  sij.dd_sij_id =sps1.dd_sij_id                   
-				  LEFT JOIN (
-					    SELECT DISTINCT ACT.ACT_ID
-					    FROM '||V_ESQUEMA||'.ACT_ACTIVO ACT
-					    JOIN '||V_ESQUEMA||'.ACT_AGA_AGRUPACION_ACTIVO AGA ON ACT.ACT_ID = AGA.ACT_ID
-					    WHERE AGA.AGA_PRINCIPAL = 1 AND ACT.BORRADO = 0
-					    AND EXISTS (
-					        SELECT DISTINCT AGA2.AGR_ID, AGR.AGR_NUM_AGRUP_REM
-					        FROM '||V_ESQUEMA||'.ACT_AGA_AGRUPACION_ACTIVO AGA2
-					        JOIN '||V_ESQUEMA||'.ACT_AGR_AGRUPACION AGR ON AGA2.AGR_ID = AGR.AGR_ID AND (AGR.AGR_FECHA_BAJA IS NULL OR AGR.AGR_FECHA_BAJA > SYSDATE)
-					        JOIN '||V_ESQUEMA||'.DD_TAG_TIPO_AGRUPACION TAG ON TAG.DD_TAG_ID = AGR.DD_TAG_ID AND TAG.DD_TAG_CODIGO = ''16''
-					        JOIN '||V_ESQUEMA||'.ACT_SPS_SIT_POSESORIA SPS ON SPS.ACT_ID = AGA2.ACT_ID AND SPS.SPS_OCUPADO = 1
-					        JOIN '||V_ESQUEMA||'.DD_TPA_TIPO_TITULO_ACT TPA ON TPA.DD_TPA_ID = SPS.DD_TPA_ID AND TPA.DD_TPA_CODIGO = ''01''
-					        WHERE AGA.AGR_ID = AGA2.AGR_ID
-					    )
-				  ) UA ON UA.ACT_ID = ACT.ACT_ID
+             FROM REM01.act_activo act LEFT JOIN REM01.act_aba_activo_bancario aba2 ON aba2.act_id = act.act_id
+
+				  LEFT JOIN REM01.DD_CRA_CARTERA cra ON cra.dd_cra_id = act.dd_cra_id
+				  LEFT JOIN REM01.dd_eac_estado_activo eac1 ON eac1.dd_eac_id = act.dd_eac_id
+                  LEFT JOIN REM01.act_sps_sit_posesoria sps1 ON sps1.act_id = act.act_id
+				  LEFT JOIN REM01.DD_TPA_TIPO_TITULO_ACT TPA ON TPA.DD_TPA_ID = SPS1.DD_TPA_ID
+				  LEFT JOIN REM01.DD_SIJ_SITUACION_JURIDICA sij on  sij.dd_sij_id =sps1.dd_sij_id
+				  LEFT JOIN (select  aga.act_id
+                      from REM01.act_aga_agrupacion_activo aga
+                      where AGA.AGA_PRINCIPAL = 1 AND EXISTS
+                       (
+                         select aga2.agr_id from REM01.ACT_AGA_AGRUPACION_ACTIVO aga2
+                         inner join REM01.ACT_SPS_SIT_POSESORIA sps on sps.act_id = aga2.act_id
+                         inner join REM01.ACT_AGR_AGRUPACION agr on aga2.agr_id = agr.agr_id
+                         inner join REM01.DD_TAG_TIPO_AGRUPACION tag1 on tag1.dd_tag_id = agr.dd_tag_id
+                         where TAG1.DD_TAG_CODIGO = ''16'' and SPS.DD_TPA_ID = 1 AND aga.AGA_ID = aga2.AGA_ID
+                        )) ua on ua.act_id = act.act_id
 				  LEFT JOIN
                   (SELECT act_tit.act_id
-                     FROM '||V_ESQUEMA||'.act_reg_info_registral act_reg 
-                     LEFT JOIN '||V_ESQUEMA||'.act_aba_activo_bancario aba ON aba.act_id = act_reg.act_id
-                     JOIN '||V_ESQUEMA||'.ACT_TIT_TITULO act_tit ON act_tit.act_id = act_reg.act_id 
-                     JOIN '||V_ESQUEMA||'.BIE_DATOS_REGISTRALES BDR ON BDR.BIE_DREG_ID = act_REG.BIE_DREG_ID
+                     FROM REM01.act_reg_info_registral act_reg
+                     LEFT JOIN REM01.act_aba_activo_bancario aba ON aba.act_id = act_reg.act_id
+                     JOIN REM01.ACT_TIT_TITULO act_tit ON act_tit.act_id = act_reg.act_id
+                     JOIN REM01.BIE_DATOS_REGISTRALES BDR ON BDR.BIE_DREG_ID = act_REG.BIE_DREG_ID
                     WHERE aba.dd_cla_id = 1 OR act_tit.TIT_FECHA_INSC_REG IS NOT NULL) tit ON tit.act_id = act.act_id                                                            -- PENDIENTE DE INSCRIPCIÓN
-                  LEFT JOIN '||V_ESQUEMA||'.act_reg_info_registral reg1 ON reg1.act_id = act.act_id
-                  LEFT JOIN '||V_ESQUEMA||'.dd_eon_estado_obra_nueva eon ON eon.dd_eon_id = reg1.dd_eon_id AND eon.dd_eon_codigo IN (''02'')                                              -- OBRA NUEVA SIN DECLARAR
-                  LEFT JOIN '||V_ESQUEMA||'.act_reg_info_registral reg2 ON reg2.act_id = act.act_id AND reg2.reg_div_hor_inscrito = 0                                           -- DIVISIÓN HORIZONTAL NO INSCRITA
-                  LEFT JOIN '||V_ESQUEMA||'.v_num_propietariosactivo npa ON npa.act_id = act.act_id                                            --PROINDIVISO (VARIOS PROPIETARIOS O 1 PROPIETARIO CON %PROP < 100)
-                  LEFT JOIN '||V_ESQUEMA||'.vi_activos_con_cargas vcg ON vcg.act_id = act.act_id
-                  LEFT JOIN '||V_ESQUEMA||'.act_ico_info_comercial ico ON ico.act_id = act.act_id and ico.borrado = 0
-                  LEFT JOIN '||V_ESQUEMA||'.vi_estado_actual_infmed vei ON vei.ico_id = ico.ico_id                                                                                          --SIN_INFORME_APROBADO
-            	  LEFT JOIN '||V_ESQUEMA||'.V_ACT_ESTADO_DISP vact on vact.act_id = act.act_id
+                  LEFT JOIN REM01.act_reg_info_registral reg1 ON reg1.act_id = act.act_id
+                  LEFT JOIN REM01.dd_eon_estado_obra_nueva eon ON eon.dd_eon_id = reg1.dd_eon_id AND eon.dd_eon_codigo = ''02''                                              -- OBRA NUEVA SIN DECLARAR
+                  LEFT JOIN REM01.act_reg_info_registral reg2 ON reg2.act_id = act.act_id AND reg2.reg_div_hor_inscrito = 0                                           -- DIVISIÓN HORIZONTAL NO INSCRITA
+                  LEFT JOIN REM01.v_num_propietariosactivo npa ON npa.act_id = act.act_id                                            --PROINDIVISO (VARIOS PROPIETARIOS O 1 PROPIETARIO CON %PROP < 100)
+                  LEFT JOIN REM01.vi_activos_con_cargas vcg ON vcg.act_id = act.act_id
+                  LEFT JOIN REM01.act_ico_info_comercial ico ON ico.act_id = act.act_id
+                  LEFT JOIN REM01.vi_estado_actual_infmed vei ON vei.ico_id = ico.ico_id                                                                                          --SIN_INFORME_APROBADO
+            	  LEFT JOIN REM01.V_ACT_ESTADO_DISP vact on vact.act_id = act.act_id
             WHERE act.borrado = 0)
           ';
 

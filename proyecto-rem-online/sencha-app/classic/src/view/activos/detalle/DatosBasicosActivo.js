eaa29e1a31fc430daa3eece839110b219d5e6024
Sergio Gomez <sergio.gomez@pfsgroup.es>
2021-04-19 09:44:28 +0200

    HREOS-13753 Incidencia en la nueva Convivencia Sareb - Error al guardar la ficha del Activo

--- proyecto-rem-online/sencha-app/classic/src/view/activos/detalle/DatosBasicosActivo.js
+++ proyecto-rem-online/sencha-app/classic/src/view/activos/detalle/DatosBasicosActivo.js
@@ -203,19 +203,7 @@ Ext.define('HreRem.view.activos.detalle.DatosBasicosActivo', {
 					                	select: 'onChangeChainedCombo'
 					            	},
 					            	style:'margin-left:10px'
-						        },
-						        {
-									xtype: 'comboboxfieldbase',
-						        	fieldLabel:  HreRem.i18n('fieldlabel.subtipo.activo'),
-						        	reference: 'subtipoActivoCombo',
-						        	bind: {
-					            		store: '{comboSubtipoActivo}',
-					            		value: '{activo.subtipoActivoCodigo}',
-					            		disabled: '{!activo.tipoActivoCodigo}'
-					            	},
-		    						allowBlank: false,
-					            	style:'margin-left:10px'
-						        },  
+						        }, 
 						        {
 						        	xtype: 'comboboxfieldbase',
 						        	xtype: 'comboboxfieldbasedd',

df787f512bf7569b7ea709fa0bb2e2c382890ba3
Ivan Repiso <ivan.repiso@pfsgroup.es>
2021-04-19 18:19:22 +0200

    REMVIP-9499 Modificar combo de proveedores en trabajos

--- proyecto-rem-online/sencha-app/classic/src/view/trabajos/detalle/GestionEconomicaTrabajo.js
+++ proyecto-rem-online/sencha-app/classic/src/view/trabajos/detalle/GestionEconomicaTrabajo.js
@@ -63,7 +63,17 @@ Ext.define('HreRem.view.trabajos.detalle.GestionEconomicaTrabajo', {
 						filtradoEspecial: true,
 						listeners: {
 		                	select: 'onChangeComboProveedorGE'
-		            	}
+		            	},
+						tpl: Ext.create('Ext.XTemplate',
+								'<tpl for=".">',
+									'<div class="x-boundlist-item">{codigo} - {nombre} - {descripcionTipoProveedor} - {estadoProveedorDescripcion}</div>',
+								'</tpl>'
+						),
+						displayTpl:  Ext.create('Ext.XTemplate',
+								'<tpl for=".">',
+									'{codigo} - {nombre} - {descripcionTipoProveedor} - {estadoProveedorDescripcion}',
+								'</tpl>'
+						)
 					},
 					{
 						fieldLabel: HreRem.i18n('fieldlabel.email.contacto'),

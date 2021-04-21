0d5fcdc6bf405d0011b2d2b9f7ee71f5071ee236
Ivan Repiso <ivan.repiso@pfsgroup.es>
2021-04-21 15:56:16 +0200

    REMVIP-9530 Modificar trabajo

---
+++ sql/9.3/rem/solo_produccion/DML_00820_REM01_UPDATE_ACT_TBJ_TRABAJO.sql
@@ -0,0 +1,97 @@
+--/*
+--##########################################
+--## AUTOR=IVAN REPISO
+--## FECHA_CREACION=20210420
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=REMVIP-9530
+--## PRODUCTO=NO
+--##
+--## Finalidad: Script modifica trabajo
+--## INSTRUCCIONES:
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF;
+
+
+DECLARE
+
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar.
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema.
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master.
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+    V_TEXT_TABLA VARCHAR2(27 CHAR) := 'ACT_TBJ_TRABAJO'; -- Vble. auxiliar para almacenar el nombre de la tabla de ref.
+    V_USU VARCHAR2(30 CHAR) := 'REMVIP-9530'; -- Vble. auxiliar para almacenar el nombre de usuario que modifica los registros.
+	V_COUNT NUMBER(16);
+    
+BEGIN		
+
+	DBMS_OUTPUT.PUT_LINE('[INICIO]');
+
+	V_MSQL:= 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.'||V_TEXT_TABLA||' WHERE TBJ_NUM_TRABAJO = 924567824803 AND BORRADO = 0';
+	EXECUTE IMMEDIATE V_MSQL INTO V_COUNT;
+
+	IF V_COUNT = 1 THEN
+
+		V_MSQL:= 'MERGE INTO '||V_ESQUEMA||'.ACT_PRT_PRESUPUESTO_TRABAJO T1 USING (
+						SELECT DISTINCT PRT.PRT_ID, PRT.PRT_IMPORTE FROM '||V_ESQUEMA||'.ACT_PRT_PRESUPUESTO_TRABAJO PRT
+						JOIN '||V_ESQUEMA||'.'||V_TEXT_TABLA||' TBJ ON TBJ.TBJ_ID = PRT.TBJ_ID AND TBJ.BORRADO = 0
+						WHERE TBJ.TBJ_NUM_TRABAJO = 924567824803 AND PRT.BORRADO = 0
+					) T2
+					ON (T1.PRT_ID = T2.PRT_ID)
+					WHEN MATCHED THEN UPDATE SET
+					T1.PRT_IMPORTE_CLIENTE = T2.PRT_IMPORTE,
+					USUARIOMODIFICAR = '''||V_USU||''',
+					FECHAMODIFICAR = SYSDATE';
+		EXECUTE IMMEDIATE V_MSQL;
+		
+		DBMS_OUTPUT.PUT_LINE('[INFO] MODIFICADO IMPORTE CLIENTE DE PRESUPUESTO');
+
+		V_MSQL:= 'MERGE INTO '||V_ESQUEMA||'.'||V_TEXT_TABLA||' T1 USING (
+						SELECT DISTINCT TBJ_ID, TBJ_IMPORTE_PRESUPUESTO FROM '||V_ESQUEMA||'.'||V_TEXT_TABLA||'
+						WHERE TBJ_NUM_TRABAJO = 924567824803 AND BORRADO = 0
+					) T2
+					ON (T1.TBJ_ID = T2.TBJ_ID)
+					WHEN MATCHED THEN UPDATE SET
+					T1.DD_IRE_ID = NULL,
+					T1.TBJ_IMPORTE_TOTAL = T2.TBJ_IMPORTE_PRESUPUESTO
+					USUARIOMODIFICAR = '''||V_USU||''',
+					FECHAMODIFICAR = SYSDATE';
+		EXECUTE IMMEDIATE V_MSQL;
+		
+		DBMS_OUTPUT.PUT_LINE('[INFO] MODIFICADO IMPORTE CLIENTE Y ELIMINANDO AREA PETICIONARIA DE TRABAJO');
+
+		COMMIT;
+
+	ELSE
+
+	DBMS_OUTPUT.PUT_LINE('[INFO] NO EXISTE EL TRABAJO 924567824803 O HA SIDO BORRADO');
+
+	END IF;
+
+	DBMS_OUTPUT.PUT_LINE('[FIN]');
+
+
+EXCEPTION
+		WHEN OTHERS THEN
+			err_num := SQLCODE;
+			err_msg := SQLERRM;
+
+			DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(err_num));
+			DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+			DBMS_OUTPUT.put_line(err_msg);
+
+			ROLLBACK;
+			RAISE;          
+
+END;
+
+/
+
+EXIT

42bfb51543234bfc3bfa103f9558ea179e7c7b59
Javier Esbri <javier.esbri@pfsgroup.es>
2021-05-18 15:52:48 +0200

    HREOS-13938 Scripts & Mapeos Hydra

---
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/model/ActivoCaixa.java
@@ -0,0 +1,160 @@
+package es.pfsgroup.plugin.rem.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.Where;
+
+import es.capgemini.pfs.auditoria.Auditable;
+import es.capgemini.pfs.auditoria.model.Auditoria;
+import es.pfsgroup.plugin.recovery.nuevoModeloBienes.model.NMBLocalizacionesBien;
+import es.pfsgroup.plugin.rem.model.dd.DDEscaleraEdificio;
+import es.pfsgroup.plugin.rem.model.dd.DDEstadoComercialAlquilerCaixa;
+import es.pfsgroup.plugin.rem.model.dd.DDEstadoComercialVentaCaixa;
+import es.pfsgroup.plugin.rem.model.dd.DDEstadoTecnicoActivo;
+import es.pfsgroup.plugin.rem.model.dd.DDPlantaEdificio;
+import es.pfsgroup.plugin.rem.model.dd.DDTipoUbicacion;
+
+@Entity
+@Table(name = "ACT_ACTIVO_CAIXA", schema = "${entity.schema}")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Where(clause = Auditoria.UNDELETED_RESTICTION)
+public class ActivoCaixa implements Serializable, Auditable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+    @Column(name = "CBX_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "ActivoCaixaGenerator")
+    @SequenceGenerator(name = "ActivoCaixaGenerator", sequenceName = "S_ACT_ACTIVO_CAIXA")
+    private Long id;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ACT_ID")
+    private Activo activo;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "DD_ECA_ID")
+	private DDEstadoComercialAlquilerCaixa estadoComercialAlquiler;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "DD_ECV_ID")
+	private DDEstadoComercialVentaCaixa estadoComercialVenta;	
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "DD_EAT_ID")
+	private DDEstadoTecnicoActivo estadoTecnico;
+	
+    @Column(name = "FECHA_ECA_EST_COM_ALQUILER")
+    private Date fechaEstadoComercialAlquiler;
+    
+    @Column(name = "FECHA_ECV_EST_COM_VENTA")
+    private Date fechaEstadoComercialVenta;
+    
+    @Column(name = "FECHA_EAT_EST_TECNICO")
+    private Date fechaEstadoTecnico;
+	
+	@Version   
+	private Long version;
+	
+	@Embedded
+	private Auditoria auditoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Activo getActivo() {
+		return activo;
+	}
+
+	public void setActivo(Activo activo) {
+		this.activo = activo;
+	}
+
+	public DDEstadoComercialAlquilerCaixa getEstadoComercialAlquiler() {
+		return estadoComercialAlquiler;
+	}
+
+	public void setEstadoComercialAlquiler(DDEstadoComercialAlquilerCaixa estadoComercialAlquiler) {
+		this.estadoComercialAlquiler = estadoComercialAlquiler;
+	}
+
+	public DDEstadoComercialVentaCaixa getEstadoComercialVenta() {
+		return estadoComercialVenta;
+	}
+
+	public void setEstadoComercialVenta(DDEstadoComercialVentaCaixa estadoComercialVenta) {
+		this.estadoComercialVenta = estadoComercialVenta;
+	}
+
+	public DDEstadoTecnicoActivo getEstadoTecnico() {
+		return estadoTecnico;
+	}
+
+	public void setEstadoTecnico(DDEstadoTecnicoActivo estadoTecnico) {
+		this.estadoTecnico = estadoTecnico;
+	}
+
+	public Date getFechaEstadoComercialAlquiler() {
+		return fechaEstadoComercialAlquiler;
+	}
+
+	public void setFechaEstadoComercialAlquiler(Date fechaEstadoComercialAlquiler) {
+		this.fechaEstadoComercialAlquiler = fechaEstadoComercialAlquiler;
+	}
+
+	public Date getFechaEstadoComercialVenta() {
+		return fechaEstadoComercialVenta;
+	}
+
+	public void setFechaEstadoComercialVenta(Date fechaEstadoComercialVenta) {
+		this.fechaEstadoComercialVenta = fechaEstadoComercialVenta;
+	}
+
+	public Date getFechaEstadoTecnico() {
+		return fechaEstadoTecnico;
+	}
+
+	public void setFechaEstadoTecnico(Date fechaEstadoTecnico) {
+		this.fechaEstadoTecnico = fechaEstadoTecnico;
+	}
+
+	public Long getVersion() {
+		return version;
+	}
+
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+
+	public Auditoria getAuditoria() {
+		return auditoria;
+	}
+
+	public void setAuditoria(Auditoria auditoria) {
+		this.auditoria = auditoria;
+	}
+	
+}

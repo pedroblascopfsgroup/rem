8512eece662b36dd05cf3b477fe2ba897784842b
Lara Pablo <lara.pablo@pfsgroup.es>
2021-05-20 14:40:05 +0200

    HREOS-14087  func-rem-reglasComercializacion Cambio fsv

--- paradise-framework/src/main/java/es/pfsgroup/framework/paradise/bulkUpload/utils/impl/MSVActualizarPerimetroActivo.java
+++ paradise-framework/src/main/java/es/pfsgroup/framework/paradise/bulkUpload/utils/impl/MSVActualizarPerimetroActivo.java
@@ -1306,7 +1306,7 @@ public class MSVActualizarPerimetroActivo extends MSVExcelValidatorAbstract {
 										listaErroresParaMarcar.add(messageServices.getMessage(VALID_ACTIVO_NO_COMERCIALIZABLE));
 									}
 									
-									if(particularValidator.isActivoAlquiladoSCM(activo) && particularValidator.isActivoEnPerimetroAlquilerSocial(activo)) {
+									if(particularValidator.isActivoEnPerimetroAlquilerSocial(activo)) {
 										listaErroresParaMarcar.add(messageServices.getMessage(VALID_ACTIVO_ALQUILER_SOCIAL));
 									}
 									

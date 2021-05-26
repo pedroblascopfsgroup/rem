4eba7ada2481b221f6c503236be741d10a07c6ce
Sento Visiedo <vicente.rubio@pfsgroup.es>
2021-05-26 16:22:37 +0200

    HREOS-14107 REMDIMIENTO 2021 Bind Variables Parte4

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/activo/ActivoManager.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/activo/ActivoManager.java
@@ -5571,8 +5571,9 @@ public class ActivoManager extends BusinessOperationOverrider<ActivoApi> impleme
 		if (Checks.esNulo(idActivo))
 			return null;
 
-		List<Object> listaObj = rawDao.getExecuteSQLList("SELECT AGR_ID FROM ACT_AGA_AGRUPACION_ACTIVO WHERE ACT_ID = "
-				+ idActivo.toString() + "AND BORRADO = 0");
+		rawDao.addParam(  "idActivo", idActivo.toString());
+
+		List<Object> listaObj = rawDao.getExecuteSQLList("SELECT AGR_ID FROM ACT_AGA_AGRUPACION_ACTIVO WHERE ACT_ID = : idActivo AND BORRADO = 0");
 
 		List<Long> listaAgr = new ArrayList<Long>();
 

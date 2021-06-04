4e447010f35ed0ede52603061ecc91877e92b451
Cristian Montoya <cristian.montoya@pfsgroup.es>
2021-06-04 14:44:01 +0200

    Revert "HREOS-14123 Cambios estados oferta caixa"
    This reverts commit a5ba4fe8b0d9102a7e9c5b622687b5ee651f23c5.

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/model/dd/DDCartera.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/model/dd/DDCartera.java
@@ -46,7 +46,6 @@ public class DDCartera implements Auditable, Dictionary {
 	public static final String CODIGO_CARTERA_JAIPUR = "09";
 	public static final String CODIGO_CARTERA_EGEO = "13";
 	public static final String CODIGO_CARTERA_BBVA = "16";
-	public static final String CODIGO_CAIXA = "03";
 		/**
 	 * 
 	 */

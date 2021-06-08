08c219c10d1b65c5ddeada7a689c8913e8e3c3bb
Pedro Blasco <pedro.blasco@pfsgroup.es>
2021-06-08 10:04:44 +0200

    HREOS-0000 (ENT-305) Actualizacion perfiles Jupiter - REM 11

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/security/jupiter/IntegracionJupiter.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/security/jupiter/IntegracionJupiter.java
@@ -151,8 +151,12 @@ public class IntegracionJupiter implements IntegracionJupiterApi {
 					integracionJupiterDao.actualizarGrupos(usuario, altasGrupos, bajasGrupos);
 					integracionJupiterDao.actualizarCarteras(usuario, altasCarteras, bajasCarteras);
 					//No puede haber subcarteras si hay alguna cartera
-					if (altasCarteras.size() == 0 || (altasSubcarteras.size() == 0 && bajasCarteras.size() > 0)) {
+					if (altaCarteras.size() > 0) {
+						integracionJupiterDao.eliminarSubcarteras(usuario);
+					} else if (altasCarteras.size() == 0 || (altasSubcarteras.size() == 0 && bajasCarteras.size() > 0)) {
 						integracionJupiterDao.actualizarSubcarteras(usuario, altasSubcarteras, bajasSubcarteras);
+					}
+						
 					}
 				}
 			} catch (Exception e) {

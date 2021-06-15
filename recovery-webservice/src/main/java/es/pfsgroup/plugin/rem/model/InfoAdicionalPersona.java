6da498b4be20118fec02645ed4678008c9e4072e
Jesus Jativa <jesus.jmartinez@pfsgroup.es>
2021-06-15 20:00:40 +0200

    HREOS-14308 Creacion de modelo, modificacion de tablas y mapeo

---
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/model/InfoAdicionalPersona.java
@@ -0,0 +1,165 @@
+package es.pfsgroup.plugin.rem.model;
+
+import es.capgemini.pfs.auditoria.Auditable;
+import es.capgemini.pfs.auditoria.model.Auditoria;
+import es.pfsgroup.plugin.rem.model.dd.*;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+
+/**
+ * Modelo que gestiona la informacion adicional de una persona asociaca a BC Caixa
+ *  
+ * @author Jesus Jativa
+ *
+ */
+@Entity
+@Table(name = "IAP_INFO_ADC_PERSONA", schema = "${entity.schema}")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Where(clause = Auditoria.UNDELETED_RESTICTION)
+public class InfoAdicionalPersona implements Serializable, Auditable {
+	
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+		
+	@Id
+    @Column(name = "IAP_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "InfoAdicionalPersona")
+    @SequenceGenerator(name = "InfoAdicionalPersona", sequenceName = "S_IAP_INFO_ADC_PERSONA")
+    private Long id;
+
+	@Column(name = "ID_PERSONA_HAYA")
+	private String idPersonaHaya;
+
+	@Column(name = "C4C_ID")
+	private String idC4C;
+
+	@ManyToOne
+	@JoinColumn(name = "DD_CNO_ID")
+	private DDCnOcupacional cnOcupacional;
+
+	@ManyToOne
+	@JoinColumn(name = "DD_ACE_ID")
+	private DDCnae Cnae;
+
+	@ManyToOne
+	@JoinColumn(name = "DD_TSC_ID")
+	private DDTipoSocioComercial tipoSocioComercial;
+
+	@ManyToOne
+	@JoinColumn(name = "DD_TRI_ID")
+	private DDRolInterlocutor rolInterlocutor;
+
+	@ManyToOne
+	@JoinColumn(name = "DD_FOJ_ID")
+	private DDFormaJuridica formaJuridica;
+
+	@ManyToOne
+	@JoinColumn(name = "DD_ECC_ID")
+	private DDEstadoComunicacionC4C estadoComunicacionC4C;
+
+	@Embedded
+	private Auditoria auditoria;
+
+
+	@Version   
+	private Long version;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getIdPersonaHaya() {
+		return idPersonaHaya;
+	}
+
+	public void setIdPersonaHaya(String idPersonaHaya) {
+		this.idPersonaHaya = idPersonaHaya;
+	}
+
+	public String getIdC4C() {
+		return idC4C;
+	}
+
+	public void setIdC4C(String idC4C) {
+		this.idC4C = idC4C;
+	}
+
+	public DDCnOcupacional getCnOcupacional() {
+		return cnOcupacional;
+	}
+
+	public void setCnOcupacional(DDCnOcupacional cnOcupacional) {
+		this.cnOcupacional = cnOcupacional;
+	}
+
+	public DDCnae getCnae() {
+		return Cnae;
+	}
+
+	public void setCnae(DDCnae cnae) {
+		Cnae = cnae;
+	}
+
+	public DDTipoSocioComercial getTipoSocioComercial() {
+		return tipoSocioComercial;
+	}
+
+	public void setTipoSocioComercial(DDTipoSocioComercial tipoSocioComercial) {
+		this.tipoSocioComercial = tipoSocioComercial;
+	}
+
+	public DDRolInterlocutor getRolInterlocutor() {
+		return rolInterlocutor;
+	}
+
+	public void setRolInterlocutor(DDRolInterlocutor rolInterlocutor) {
+		this.rolInterlocutor = rolInterlocutor;
+	}
+
+	public DDFormaJuridica getFormaJuridica() {
+		return formaJuridica;
+	}
+
+	public void setFormaJuridica(DDFormaJuridica formaJuridica) {
+		this.formaJuridica = formaJuridica;
+	}
+
+	public DDEstadoComunicacionC4C getEstadoComunicacionC4C() {
+		return estadoComunicacionC4C;
+	}
+
+	public void setEstadoComunicacionC4C(DDEstadoComunicacionC4C estadoComunicacionC4C) {
+		this.estadoComunicacionC4C = estadoComunicacionC4C;
+	}
+
+	public Long getVersion() {
+		return version;
+	}
+
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+
+	@Override
+	public Auditoria getAuditoria() {
+		return auditoria;
+	}
+
+	@Override
+	public void setAuditoria(Auditoria auditoria) {
+		this.auditoria = auditoria;
+	}
+
+}
---
+++ sql/9.3/rem/DDL_961_REM01_ALTER_TABLE_COM_COMPRADOR.sql
@@ -0,0 +1,114 @@
+--/*
+--##########################################
+--## AUTOR=Jesus Jativa
+--## FECHA_CREACION=20210615
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=HREOS-14308
+--## PRODUCTO=NO
+--##
+--## INSTRUCCIONES:
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+--Para permitir la visualización de texto en un bloque PL/SQL utilizando DBMS_OUTPUT.PUT_LINE
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
+SET DEFINE OFF;
+
+
+DECLARE
+
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar    
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+    V_TABLESPACE_IDX VARCHAR2(25 CHAR):= '#TABLESPACE_INDEX#'; -- Configuracion Tablespace de Indices
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.  
+    V_NUM_SEQ NUMBER(16); -- Vble. para validar la existencia de una secuencia.  
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+ 
+    V_TEXT1 VARCHAR2(2400 CHAR); -- Vble. auxiliar 
+    V_TEXT_TABLA VARCHAR2(2400 CHAR) := 'COM_COMPRADOR'; -- Vble. auxiliar para almacenar el nombre de la tabla de ref.
+    
+    /* -- ARRAY CON NUEVAS COLUMNAS */
+    TYPE T_ALTER IS TABLE OF VARCHAR2(4000);
+    TYPE T_ARRAY_ALTER IS TABLE OF T_ALTER;
+    V_ALTER T_ARRAY_ALTER := T_ARRAY_ALTER(
+    	-- NOMBRE CAMPO						TIPO CAMPO							DESCRIPCION
+    	T_ALTER(  'IAP_ID',		 			 'NUMBER(16,0)',						'clave ajena a la tabla IAP_INFO_ADC_PERSONA'),
+    	T_ALTER(  'COM_FECHA_NACIMIENTO',	 'DATE',						        'fecha de nacimiento'),
+    	T_ALTER(  'DD_LOC_NAC_ID',		 	 'NUMBER(16,0)',						'localidad de nacimiento')
+	);
+    V_T_ALTER T_ALTER;    
+
+BEGIN
+	
+	DBMS_OUTPUT.PUT_LINE('********' ||V_TEXT_TABLA|| '********'); 
+	DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.'||V_TEXT_TABLA||'... Comprobaciones previas ****************************');
+
+	
+	-- Bucle que CREA las nuevas columnas 
+	FOR I IN V_ALTER.FIRST .. V_ALTER.LAST
+	LOOP
+
+		V_T_ALTER := V_ALTER(I);
+
+		-- Verificar si la columna ya existe. Si ya existe la columna, no se hace nada con esta (no tiene en cuenta si al existir los tipos coinciden)
+		V_MSQL := 'SELECT COUNT(1) FROM ALL_TAB_COLS WHERE COLUMN_NAME = '''||V_T_ALTER(1)||''' and TABLE_NAME = '''||V_TEXT_TABLA||''' and owner = '''||V_ESQUEMA||'''';
+		EXECUTE IMMEDIATE V_MSQL INTO V_NUM_TABLAS;	
+		IF V_NUM_TABLAS = 0 THEN
+			--No existe la columna y la creamos
+			DBMS_OUTPUT.PUT_LINE('[INFO] Cambios en ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'['||V_T_ALTER(1)||'] -------------------------------------------');
+			V_MSQL := 'ALTER TABLE '||V_TEXT_TABLA|| ' 
+					   ADD ('||V_T_ALTER(1)||' '||V_T_ALTER(2)||' )
+			';
+
+			EXECUTE IMMEDIATE V_MSQL;
+			--DBMS_OUTPUT.PUT_LINE('[1] '||V_MSQL);
+			DBMS_OUTPUT.PUT_LINE('[INFO] ... '||V_T_ALTER(1)||' Columna INSERTADA en tabla, con tipo '||V_T_ALTER(2));
+
+			-- Creamos comentario	
+			V_MSQL := 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TEXT_TABLA||'.'||V_T_ALTER(1)||' IS '''||V_T_ALTER(3)||'''';		
+			EXECUTE IMMEDIATE V_MSQL;
+			--DBMS_OUTPUT.PUT_LINE('[2] '||V_MSQL);
+			DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'... Comentario en columna creado.');
+		END IF;
+
+	END LOOP;
+
+		-- Creamos FK
+    	V_MSQL := 'ALTER TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' ADD (CONSTRAINT FK_'||V_TEXT_TABLA||'_IAP_ID FOREIGN KEY (IAP_ID) REFERENCES '||V_ESQUEMA|| '.IAP_INFO_ADC_PERSONA (IAP_ID))';
+    	EXECUTE IMMEDIATE V_MSQL;
+    	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'_FK... FK creada.');
+
+		-- Creamos FK
+    	V_MSQL := 'ALTER TABLE '||V_ESQUEMA||'.'||V_TEXT_TABLA||' ADD (CONSTRAINT FK_'||V_TEXT_TABLA||'_DD_LOC_NAC_ID FOREIGN KEY (DD_LOC_NAC_ID) REFERENCES '||V_ESQUEMA_M|| '.DD_LOC_LOCALIDAD (DD_LOC_ID))';
+    	EXECUTE IMMEDIATE V_MSQL;
+    	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||'_FK... FK creada.');
+
+	
+	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TEXT_TABLA||' AMPLIADA CON COLUMNAS NUEVAS.');
+	COMMIT;
+
+EXCEPTION
+     WHEN OTHERS THEN 
+         DBMS_OUTPUT.PUT_LINE('[ERROR] ...KO!');
+          err_num := SQLCODE;
+          err_msg := SQLERRM;
+
+          DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(err_num));
+          DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+          DBMS_OUTPUT.put_line(err_msg);
+
+          ROLLBACK;
+          RAISE;          
+
+END;
+
+/
+
+EXIT

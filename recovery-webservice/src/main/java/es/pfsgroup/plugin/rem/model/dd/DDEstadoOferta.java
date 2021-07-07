86f9340b4893a21c11bcd2c049d5a90726cc221b
Santiago Monzo <santiago.monzo@pfsgroup.es>
2021-07-07 12:58:49 +0200

    HREOS-14526 Modificar descripcion del DD y mapeo

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/model/dd/DDEstadoOferta.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/model/dd/DDEstadoOferta.java
@@ -34,7 +34,9 @@ public class DDEstadoOferta implements Auditable, Dictionary {
 	public static final String CODIGO_RECHAZADA= "02";
 	public static final String CODIGO_CONGELADA= "03";
 	public static final String CODIGO_PENDIENTE= "04";
-	
+	public static final String CODIGO_PENDIENTE_TITULARES= "05";
+	public static final String CODIGO_CADUCADA= "06";
+	public static final String CODIGO_PENDIENTE_DEPOSITO= "07";
 	public static final String CODIGO_ANULADA_RC= "999";
 	
 	private static final long serialVersionUID = 1L;

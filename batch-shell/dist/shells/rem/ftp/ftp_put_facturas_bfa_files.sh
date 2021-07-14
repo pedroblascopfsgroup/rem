f28167a2c5993f1e03465fa05753bf853e33becf
Guillermo Llidó Parra <guillermo.llido@pfsgroup.es>
2021-07-14 13:08:23 +0200

    GC-32259 -> [BAU] Revisar ruta ficheros apr_gen_ru_salidas_facturas_bfa

--- batch-shell/dist/shells/rem/ftp/ftp_put_facturas_bfa_files.sh
+++ batch-shell/dist/shells/rem/ftp/ftp_put_facturas_bfa_files.sh
@@ -1,16 +1,16 @@
 cd ${DIR_SALIDA}
 
-echo "Inicio de transferencia ficheros RUFACTUCP_BFA.txt / RUFACTUSP_BFA.txt"
+echo "Inicio de transferencia ficheros RUFACTUCP_BFA_$1.txt / RUFACTUSP_BFA_$1.txt"
 
 lftp -u pfs,SwQdLRyFE8A5 sftp://192.168.126.7 <<EOF
 	
 	cd /Archivos/REM/PFSToHaya/BFA
-  	mput ${DIR_SALIDA}RUFACTUCP_BFA.txt
-  	mput ${DIR_SALIDA}RUFACTUSP_BFA.txt
-  	mput ${DIR_SALIDA}RUFACTUCP_BFA.sem
-  	mput ${DIR_SALIDA}RUFACTUSP_BFA.sem  
+  	mput ${DIR_SALIDA}RUFACTUCP_BFA_$1.txt
+  	mput ${DIR_SALIDA}RUFACTUSP_BFA_$1.txt
+	mput ${DIR_SALIDA}RUFACTUCP_BFA_$1.sem  	
+  	mput ${DIR_SALIDA}RUFACTUSP_BFA_$1.sem  
   	bye
 bye
 EOF
 
-echo "Ha finalizado la transferencia de los ficheros RUFACTUCP_BFA.txt / RUFACTUSP_BFA.txt"
+echo "Ha finalizado la transferencia de los ficheros RUFACTUCP_BFA_$1.txt / RUFACTUSP_BFA_$1.txt"

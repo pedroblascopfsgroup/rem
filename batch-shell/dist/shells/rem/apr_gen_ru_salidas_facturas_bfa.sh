e6b7eea3c54ef89547e2c5bdfeac146626069301
Guillermo Llidó Parra <guillermo.llido@pfsgroup.es>
2021-07-14 13:08:23 +0200

    GC-32259 -> [BAU] Revisar ruta ficheros apr_gen_ru_salidas_facturas_bfa

--- batch-shell/dist/shells/rem/apr_gen_ru_salidas_facturas_bfa.sh
+++ batch-shell/dist/shells/rem/apr_gen_ru_salidas_facturas_bfa.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# Generado por GUSTAVO MORA 20170810
- 
-
 
 filename=$(basename $0)
 nameETL="${filename%.*}"
@@ -24,28 +21,13 @@ if [ -f $MAINSH ]; then
     java -Xms512M -Xmx1536M -Dconfig.dir=$DIR_CONFIG -Dconfig.file.mask=$CFG_FILE -Duser.country=ES -Duser.language=es -cp $CLASS2 $CLASEINICIO --context=Default "$@"
     
     
-if [ $? = 0 ]; then
-echo "Inicio de transferencia ficheros RUFACTUCP_BFA.txt/RUFACTUSP_BFA.txt"
-   lftp -c "open -u ftpsocpart,tempo.99 -p 2153 sftp://192.168.126.66; ls /datos/usuarios/socpart/CISA/out"
-
-
-  lftp -u ftpsocpart,tempo.99 -p 2153 sftp://192.168.126.66 <<EOF
-  cd /datos/usuarios/socpart/CISA/out/
-  mput $DIR_SALIDA/RUFACTUCP_BFA.txt
-  mput $DIR_SALIDA/RUFACTUSP_BFA.txt 
-  cd /datos/usuarios/socpart/CISA/in/
-  mput $DIR_SALIDA/RUFACTUCP_BFA.txt
-  mput $DIR_SALIDA/RUFACTUSP_BFA.txt 
-  bye
-EOF
-
-  exit 0
-else
-	echo "$(basename $0) Error en la ejecución del proceso"
-    exit 1
-fi    
+    if [ $? = 0 ]; then
+        exit 0
+    else
+    	echo "$(basename $0) Error en la ejecución del proceso"
+        exit 1
+    fi    
 else
     echo "$(basename $0) Error en $filename: no se ha encontrado  $MAINSH"
     exit 1
 fi
-

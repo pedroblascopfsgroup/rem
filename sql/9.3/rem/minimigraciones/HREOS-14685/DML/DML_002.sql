96326f3c3ab1f748c970b42ea790e5a97da21494
Daniel Algaba <daniel.algaba@pfsgroup.es>
2021-07-20 09:53:48 +0200

    HREOS-14685 CR [Salida] Migración de proveedores

---
+++ sql/9.3/rem/minimigraciones/HREOS-14685/DML/DML_002.sql
@@ -0,0 +1,75 @@
+--/*
+--#########################################
+--## AUTOR=Daniel Algaba
+--## FECHA_CREACION=20210720
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=HREOS-14685
+--## PRODUCTO=NO
+--## 
+--## Finalidad: Actualización
+--## INSTRUCCIONES:  
+--## VERSIONES:
+--##        0.1 Versión inicial
+--#########################################
+--*/
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
+SET DEFINE OFF;
+
+DECLARE
+	
+	V_TABLA_TMP VARCHAR2(30 CHAR) := 'APR_AUX_HREOS_14685'; -- Variable para tabla de salida para el borrado	
+	V_TABLA VARCHAR2(30 CHAR) := 'ACT_PVE_PROVEEDOR'; -- Variable para tabla de salida para el borrado	
+	V_ESQUEMA VARCHAR2(25 CHAR):= 'REM01';-- '#ESQUEMA#'; -- Configuracion Esquema
+	V_ESQUEMA_M VARCHAR2(25 CHAR):= 'REMMASTER';-- '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+	ERR_NUM NUMBER;-- Numero de errores
+	ERR_MSG VARCHAR2(2048);-- Mensaje de error	
+	V_SQL VARCHAR2(4000 CHAR);--Sentencia a ejecutar	
+	PL_OUTPUT VARCHAR2(32000 CHAR);
+	V_USUARIO VARCHAR2(50 CHAR) := 'HREOS-14685';
+	
+BEGIN
+
+	DBMS_OUTPUT.PUT_LINE('[INICIO]'||CHR(10));
+	DBMS_OUTPUT.PUT_LINE('[INFO] FUSIONANDO LOS DATOS DE LA TABLA '||V_ESQUEMA||'.'||V_TABLA_TMP||' EN LA TABLA '||V_TABLA||'');
+
+	V_SQL := 'MERGE INTO '||V_ESQUEMA||'.'||V_TABLA||' PVE
+				USING(
+				SELECT
+					PVE.PVE_ID
+					, PVE.PVE_COD_UVEM
+					, PVE.PVE_DOCIDENTIF
+					FROM '||V_ESQUEMA||'.'||V_TABLA||' PVE
+					WHERE PVE.BORRADO = 0
+					AND PVE.PVE_COD_UVEM IS NOT NULL
+					AND (USUARIOMODIFICAR IS NULL OR USUARIOMODIFICAR != '''||V_USUARIO||''')
+				) AUX
+				ON (PVE.PVE_ID = AUX.PVE_ID)
+				WHEN MATCHED THEN
+				UPDATE SET
+					PVE.PVE_COD_UVEM = NULL
+					, PVE.USUARIOMODIFICAR = '''||V_USUARIO||'''
+					, PVE.FECHAMODIFICAR = SYSDATE';
+	DBMS_OUTPUT.PUT_LINE(V_SQL);
+	EXECUTE IMMEDIATE V_SQL;
+	DBMS_OUTPUT.PUT_LINE('[INFO] Se han fusionado un total de '||SQL%ROWCOUNT||' filas de la tabla '||V_TABLA);
+
+	COMMIT;
+	
+	PL_OUTPUT := PL_OUTPUT || '[FIN]'||CHR(10);
+	DBMS_OUTPUT.PUT_LINE(PL_OUTPUT);
+
+EXCEPTION
+    WHEN OTHERS THEN
+      PL_OUTPUT := PL_OUTPUT ||'[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(SQLCODE)||CHR(10);
+      PL_OUTPUT := PL_OUTPUT ||'-----------------------------------------------------------'||CHR(10);
+      PL_OUTPUT := PL_OUTPUT ||SQLERRM||CHR(10);
+      PL_OUTPUT := PL_OUTPUT ||V_SQL||CHR(10);
+      DBMS_OUTPUT.PUT_LINE(PL_OUTPUT);
+      ROLLBACK;
+      RAISE;
+END;
+/
+EXIT;

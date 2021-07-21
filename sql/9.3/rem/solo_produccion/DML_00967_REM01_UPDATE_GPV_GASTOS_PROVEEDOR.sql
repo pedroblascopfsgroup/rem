f74682305b5e7c3dcbe196cd3508e23aeaecf928
Santiago Monzo <santiago.monzo@pfsgroup.es>
2021-07-21 13:31:09 +0200

    REMVIP-10169 cambiar estado oferta

---
+++ sql/9.3/rem/solo_produccion/DML_00967_REM01_UPDATE_GPV_GASTOS_PROVEEDOR.sql
@@ -0,0 +1,66 @@
+--/*
+--##########################################
+--## AUTOR=Santi Monzó
+--## FECHA_CREACION=20210721
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=REMVIP-10169
+--## PRODUCTO=NO
+--## 
+--## Finalidad: Actualización DD_EGA_ID
+--##			
+--## INSTRUCCIONES:  
+--## VERSIONES:
+--##        0.1 Versión inicial
+--#########################################
+--*/
+
+--Para permitir la visualización de texto en un bloque PL/SQL utilizando DBMS_OUTPUT.PUT_LINE
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
+SET DEFINE OFF;
+
+
+DECLARE
+	V_MSQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+ 	V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+ 	V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+	V_USUARIO VARCHAR2(50 CHAR) := 'REMVIP-10169'; -- USUARIO CREAR/MODIFICAR
+	V_COUNT NUMBER(16); -- Vble. para comprobar
+	err_num NUMBER; -- Numero de errores
+	err_msg VARCHAR2(2048); -- Mensaje de error
+
+	
+BEGIN
+	DBMS_OUTPUT.PUT_LINE('[INICIO] ');
+
+	V_MSQL := 'MERGE INTO '||V_ESQUEMA||'.GPV_GASTOS_PROVEEDOR T1 USING (
+					SELECT DISTINCT GPV.GPV_NUM_GASTO_HAYA, GPV.GPV_ID FROM '||V_ESQUEMA||'.GPV_GASTOS_PROVEEDOR GPV
+                    WHERE GPV.BORRADO= 0 AND GPV.DD_EGA_ID=1
+                    AND (GPV.DD_TOG_ID IS NULL OR GPV.GPV_FECHA_EMISION IS NULL
+                    OR GPV.DD_TPE_ID IS NULL OR (GPV.PVE_ID_GESTORIA IS NULL AND GPV.GPV_CONCEPTO IS NULL )
+                    OR (GPV.GPV_EXISTE_DOCUMENTO = 0 OR GPV.GPV_EXISTE_DOCUMENTO IS NULL) OR GPV.GPV_ID NOT IN (SELECT GPV.GPV_ID FROM '||V_ESQUEMA||'.GLD_GASTOS_LINEA_DETALLE GLD WHERE GLD.BORRADO=0))) T2
+				ON (T1.GPV_ID = T2.GPV_ID)
+				WHEN MATCHED THEN UPDATE SET
+				DD_EGA_ID = (SELECT DD_EGA_ID FROM '||V_ESQUEMA||'.DD_EGA_ESTADOS_GASTO WHERE DD_EGA_CODIGO = ''12''),
+				USUARIOMODIFICAR = '''||V_USUARIO||''',
+				FECHAMODIFICAR = SYSDATE';
+	EXECUTE IMMEDIATE V_MSQL;
+
+	DBMS_OUTPUT.PUT_LINE('[INFO] MODIFICADOS  '|| SQL%ROWCOUNT ||' REGISTROS EN GPV_GASTOS_PROVEEDOR');
+
+	DBMS_OUTPUT.PUT_LINE('[FIN] ');
+	
+	EXCEPTION
+	
+	    WHEN OTHERS THEN
+		DBMS_OUTPUT.PUT_LINE('[ERROR] Se ha producido un error en la ejecucion:'||TO_CHAR(SQLCODE));
+		DBMS_OUTPUT.PUT_LINE('-----------------------------------------------------------');
+		DBMS_OUTPUT.PUT_LINE(SQLERRM);
+		DBMS_OUTPUT.PUT_LINE(V_MSQL);
+		ROLLBACK;
+		RAISE;
+END;
+/
+EXIT;

aec724ba39385f81e578ac0254186fd3dfaf312f
Santiago Monzo <santiago.monzo@pfsgroup.es>
2021-07-21 15:58:39 +0200

    REMVIP-10207 DML_00968_REM01_UPDATE_ACT_ICO_INFO_COMERCIAL.sql

---
+++ sql/9.3/rem/solo_produccion/DML_00968_REM01_UPDATE_ACT_ICO_INFO_COMERCIAL.sql
@@ -0,0 +1,99 @@
+--/*
+--##########################################
+--## AUTOR=Santi Monzó
+--## FECHA_CREACION=20210721
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=REMVIP-10207
+--## PRODUCTO=NO
+--##
+--## Finalidad: 
+--## INSTRUCCIONES:
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF;
+
+
+DECLARE
+
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar.
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema.
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master.
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+    V_USU VARCHAR2(30 CHAR) := 'REMVIP-10207'; -- Vble. auxiliar para almacenar el nombre de usuario que modifica los registros.
+	V_COUNT NUMBER(16);
+	V_ACT_ID NUMBER(16);
+	V_ICO_ID NUMBER(16);
+
+	TYPE T_TIPO_DATA IS TABLE OF VARCHAR2(150);
+    TYPE T_ARRAY_DATA IS TABLE OF T_TIPO_DATA;
+    V_TIPO_DATA T_ARRAY_DATA := T_ARRAY_DATA(
+		T_TIPO_DATA('7433773','872193'),
+		T_TIPO_DATA('7433775','872197'),
+		T_TIPO_DATA('7433779','886703'),
+		T_TIPO_DATA('7433759','870153')
+
+		); 
+    V_TMP_TIPO_DATA T_TIPO_DATA;
+    
+BEGIN		
+
+	DBMS_OUTPUT.PUT_LINE('[INICIO]');
+	
+	FOR I IN V_TIPO_DATA.FIRST .. V_TIPO_DATA.LAST
+	LOOP
+	V_TMP_TIPO_DATA := V_TIPO_DATA(I);
+
+		V_MSQL:= 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.ACT_ACTIVO WHERE ACT_NUM_ACTIVO = '||V_TMP_TIPO_DATA(1)||'';
+		EXECUTE IMMEDIATE V_MSQL INTO V_COUNT;
+
+		IF V_COUNT = 1 THEN
+
+			V_MSQL:= 'SELECT ACT_ID FROM '||V_ESQUEMA||'.ACT_ACTIVO WHERE ACT_NUM_ACTIVO = '||V_TMP_TIPO_DATA(1)||'';
+			EXECUTE IMMEDIATE V_MSQL INTO V_ACT_ID;
+
+			V_MSQL:= 'UPDATE '||V_ESQUEMA||'.ACT_ICO_INFO_COMERCIAL SET
+				ICO_WEBCOM_ID = '||V_TMP_TIPO_DATA(2)||',
+				USUARIOMODIFICAR = '''||V_USU||''',
+				FECHAMODIFICAR = SYSDATE
+				WHERE ACT_ID = '||V_ACT_ID||'';
+			EXECUTE IMMEDIATE V_MSQL;
+
+			DBMS_OUTPUT.PUT_LINE('[INFO]: WEBCOM ID ACTUALIZADO PARA '||V_TMP_TIPO_DATA(1)||'');
+
+		ELSE
+
+			DBMS_OUTPUT.PUT_LINE('[INFO]: ACTIVO '||V_TMP_TIPO_DATA(1)||' NO EXISTE O ESTÁ BORRADO');
+
+		END IF;
+
+	END LOOP;
+
+	COMMIT;
+
+	DBMS_OUTPUT.PUT_LINE('[FIN]');
+
+
+EXCEPTION
+		WHEN OTHERS THEN
+			err_num := SQLCODE;
+			err_msg := SQLERRM;
+
+			DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(err_num));
+			DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+			DBMS_OUTPUT.put_line(err_msg);
+
+			ROLLBACK;
+			RAISE;          
+
+END;
+
+/
+
+EXIT

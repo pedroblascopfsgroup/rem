218fbd2268d5ed9221f62e23886006f6021cda06
Ivan Repiso <ivan.repiso@pfsgroup.es>
2021-07-28 10:38:02 +0200

    HREOS-14760 Alquileres CFV

--- proyecto-rem-online/sencha-app/classic/src/view/activos/detalle/OfertasComercialActivoList.js
+++ proyecto-rem-online/sencha-app/classic/src/view/activos/detalle/OfertasComercialActivoList.js
@@ -356,13 +356,15 @@ Ext.define('HreRem.view.activos.detalle.OfertasComercialActivoList', {
 							   msg: msg,
 							   buttons: Ext.MessageBox.YESNO,
 							   fn: function(buttonId) {
-							        if (buttonId == 'yes') {
-							        	me.up('activosdetalle').lookupController().mostrarCrearOfertaTramitada(editor, me, context);
-							        	
-							        	// me.saveFn(editor, me, context);
+							        if (buttonId == 'yes' && context.record.get("descripcionTipoOferta") == CONST.TIPO_COMERCIALIZACION_ACTIVO['VENTA']) {
+										me.up('activosdetalle').lookupController().mostrarCrearOfertaTramitada(editor, me, context);
+										
+										// me.saveFn(editor, me, context);
+									} else if (buttonId == 'yes') {
+										me.saveFn(editor, me, context);
 									} else{
-							    		me.getStore().load(); 	
-							    	}
+										me.getStore().load(); 	
+									}
 								}
 							});		    			
 		    			

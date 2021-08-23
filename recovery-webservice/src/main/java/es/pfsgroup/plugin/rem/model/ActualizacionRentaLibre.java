0fc1ddc335a3aad8dd00b0e615a48d285953349c
Lara Pablo <lara.pablo@pfsgroup.es>
2021-08-23 14:46:56 +0200

    HREOS-14942  func-rem-modeloHydra nuevos campos para la actualización de la renta

---
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/model/ActualizacionRentaLibre.java
@@ -0,0 +1,141 @@
+package es.pfsgroup.plugin.rem.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.Where;
+
+import es.capgemini.pfs.auditoria.Auditable;
+import es.capgemini.pfs.auditoria.model.Auditoria;
+import es.pfsgroup.plugin.rem.model.dd.DDEntidadFinanciera;
+import es.pfsgroup.plugin.rem.model.dd.DDEntidadesAvalistas;
+import es.pfsgroup.plugin.rem.model.dd.DDEstadoFinanciacion;
+import es.pfsgroup.plugin.rem.model.dd.DDEstadoTitulo;
+import es.pfsgroup.plugin.rem.model.dd.DDMetodoActualizacionRenta;
+import es.pfsgroup.plugin.rem.model.dd.DDRegimenFianzaCCAA;
+import es.pfsgroup.plugin.rem.model.dd.DDRevisionRenta;
+import es.pfsgroup.plugin.rem.model.dd.DDSituacionesPosesoria;
+import es.pfsgroup.plugin.rem.model.dd.DDTipoCalculo;
+import es.pfsgroup.plugin.rem.model.dd.DDTiposImpuesto;
+import es.pfsgroup.plugin.rem.model.dd.DDTiposPorCuenta;
+
+
+/**
+ *  
+ * @author Lara Pablo
+ *
+ */
+@Entity
+@Table(name = "ARL_ACTU_RENTA_LIBRE", schema = "${entity.schema}")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Where(clause = Auditoria.UNDELETED_RESTICTION)
+@Inheritance(strategy=InheritanceType.JOINED)
+public class ActualizacionRentaLibre implements Serializable, Auditable {
+
+	private static final long serialVersionUID = -2333323398245528237L;
+
+
+	@Id
+    @Column(name = "ARL_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "ActualizacionRentaLibreGenerator")
+    @SequenceGenerator(name = "ActualizacionRentaLibreGenerator", sequenceName = "S_ARL_ACTU_RENTA_LIBRE")
+    private Long id;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "COE_ID")
+    private CondicionanteExpediente condicionanteExpediente;
+	
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "DD_MTA_ID")
+	private DDMetodoActualizacionRenta metodoActualizacionRenta;
+    
+    @Column(name="ARL_FECHA_ACTU")
+    private Date fechaActualizacion;
+    
+    @Column(name="ARL_IMPORTE_ACTU")
+    private Double importeActualizacion;
+    
+    @Version   
+	private Long version;
+
+	@Embedded
+	private Auditoria auditoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public CondicionanteExpediente getCondicionanteExpediente() {
+		return condicionanteExpediente;
+	}
+
+	public void setCondicionanteExpediente(CondicionanteExpediente condicionanteExpediente) {
+		this.condicionanteExpediente = condicionanteExpediente;
+	}
+
+	public DDMetodoActualizacionRenta getMetodoActualizacionRenta() {
+		return metodoActualizacionRenta;
+	}
+
+	public void setMetodoActualizacionRenta(DDMetodoActualizacionRenta metodoActualizacionRenta) {
+		this.metodoActualizacionRenta = metodoActualizacionRenta;
+	}
+
+	public Date getFechaActualizacion() {
+		return fechaActualizacion;
+	}
+
+	public void setFechaActualizacion(Date fechaActualizacion) {
+		this.fechaActualizacion = fechaActualizacion;
+	}
+
+	public Double getImporteActualizacion() {
+		return importeActualizacion;
+	}
+
+	public void setImporteActualizacion(Double importeActualizacion) {
+		this.importeActualizacion = importeActualizacion;
+	}
+
+	public Long getVersion() {
+		return version;
+	}
+
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+
+	public Auditoria getAuditoria() {
+		return auditoria;
+	}
+
+	public void setAuditoria(Auditoria auditoria) {
+		this.auditoria = auditoria;
+	}
+ 
+}

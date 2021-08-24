4707d19afa416dd85a2fec40ff170c1ad1d48349
Alejandra Petrovic <alejandra.petrovic@pfsgroup.es>
2021-08-24 11:55:41 +0200

    HREOS-14974 CReación tabla EQV_CAIXA_TUD y modificación SP_BCR_01_DATOS_BASICOS y SP_RBC_04_DOCUMENTOS_ADMISION

---
+++ sql/9.3/rem/DML_00880_REM01_INSERT_EQV_CAIXA_TUD.sql
@@ -0,0 +1,117 @@
+--/*
+--##########################################
+--## AUTOR=Alejandra García
+--## FECHA_CREACION=20210824
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=HREOS-14974
+--## PRODUCTO=NO
+--## Finalidad: Creación diccionario EQV_CAIXA_TUD
+--##           
+--## INSTRUCCIONES: Configurar las variables necesarias en el principio del DECLARE
+--## VERSIONES:
+--##        0.1 Versión inicial 
+--##########################################
+--*/
+
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF;
+
+
+DECLARE
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar     
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.   
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+	
+    V_TEXT1 VARCHAR2(2400 CHAR); -- Vble. auxiliar
+    V_ENTIDAD_ID NUMBER(16);
+    V_ID NUMBER(16);
+    V_ITEM VARCHAR2(25 CHAR):= 'HREOS-14344';
+    
+    
+    TYPE T_TIPO_DATA IS TABLE OF VARCHAR2(150);
+    TYPE T_ARRAY_DATA IS TABLE OF T_TIPO_DATA;
+                  --TIPO(TPA)  --SUBTIPO()  --USO_DOMINANTE(TUD)
+    V_TIPO_DATA T_ARRAY_DATA := T_ARRAY_DATA(
+        T_TIPO_DATA('01','02','05'),
+        T_TIPO_DATA('01','03','05'),
+        T_TIPO_DATA('01','04','05')
+		); 
+    V_TMP_TIPO_DATA T_TIPO_DATA;
+    
+BEGIN	
+	
+	DBMS_OUTPUT.PUT_LINE('[INICIO] ');
+
+	 
+    
+    DBMS_OUTPUT.PUT_LINE('[INFO]: INSERCION EN EQV_CAIXA_TUD ');
+    FOR I IN V_TIPO_DATA.FIRST .. V_TIPO_DATA.LAST
+      LOOP
+      
+        V_TMP_TIPO_DATA := V_TIPO_DATA(I);
+    
+        --Comprobamos el dato a insertar
+        V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.EQV_CAIXA_TUD WHERE SUBTIPO = '''||TRIM(V_TMP_TIPO_DATA(2))||'''';
+        EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
+        
+        --Si existe lo modificamos
+        IF V_NUM_TABLAS > 0 THEN				
+          
+          DBMS_OUTPUT.PUT_LINE('[INFO]: MODIFICAMOS EL REGISTRO '''|| TRIM(V_TMP_TIPO_DATA(2)) ||'''');
+
+       	  V_MSQL := 'UPDATE '|| V_ESQUEMA ||'.EQV_CAIXA_TUD '||
+                    'SET   TIPO = '''||TRIM(V_TMP_TIPO_DATA(1))||''''|| 
+                        ', USO_DOMINANTE = '''||TRIM(V_TMP_TIPO_DATA(3))||''''||
+                    'WHERE SUBTIPO = '''||TRIM(V_TMP_TIPO_DATA(2))||'''';
+          EXECUTE IMMEDIATE V_MSQL;
+          DBMS_OUTPUT.PUT_LINE('[INFO]: REGISTRO MODIFICADO CORRECTAMENTE');
+          
+       --Si no existe, lo insertamos   
+       ELSE
+       
+          DBMS_OUTPUT.PUT_LINE('[INFO]: INSERTAMOS EL REGISTRO '''|| TRIM(V_TMP_TIPO_DATA(1)) ||'''');   
+
+          V_MSQL := 'INSERT INTO '|| V_ESQUEMA ||'.EQV_CAIXA_TUD 
+                      (' ||'
+                            TIPO
+                          , SUBTIPO
+                          , USO_DOMINANTE) ' ||
+                      'SELECT 
+                           '''||TRIM(V_TMP_TIPO_DATA(1))||'''
+                          ,'''||TRIM(V_TMP_TIPO_DATA(2))||'''
+                          ,'''||TRIM(V_TMP_TIPO_DATA(3))||'''
+                      FROM DUAL';
+          EXECUTE IMMEDIATE V_MSQL;
+          DBMS_OUTPUT.PUT_LINE('[INFO]: REGISTRO INSERTADO CORRECTAMENTE');
+        
+       END IF;
+      END LOOP;
+    COMMIT;
+
+    DBMS_OUTPUT.PUT_LINE('[FIN]: DICCIONARIO EQV_CAIXA_TUD MODIFICADO CORRECTAMENTE ');
+   
+
+EXCEPTION
+     WHEN OTHERS THEN
+          err_num := SQLCODE;
+          err_msg := SQLERRM;
+
+          DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(err_num));
+          DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+          DBMS_OUTPUT.put_line(err_msg);
+
+          ROLLBACK;
+          RAISE;          
+
+END;
+
+/
+
+EXIT

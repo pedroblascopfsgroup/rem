7bcccf88260fc95a76194e77f59efd0a456fb8a8
Ivan Repiso <ivan.repiso@pfsgroup.es>
2021-08-26 09:25:11 +0200

    HREOS-15002 Eliminar diccionario fuente testigos

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/utils/DiccionarioTargetClassMap.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/utils/DiccionarioTargetClassMap.java
@@ -315,7 +315,6 @@ public class DiccionarioTargetClassMap{
 		mapa.put("tipoCorrectivoSareb", DDTipoCorrectivoSareb.class); 
 		mapa.put("tipoCuotaComunidad", DDTipoCuotaComunidad.class);
 		mapa.put("segmentacionSareb", DDSegmentoSareb.class);
-		mapa.put("fuenteTestigos", DDFuenteTestigos.class);
 		mapa.put("disponibleAdministrativo", DDDisponibleAdministracion.class);
 		mapa.put("disponibleTecnico", DDDisponibleTecnico.class);
 		mapa.put("motivoTecnico", DDMotivoTecnico.class);

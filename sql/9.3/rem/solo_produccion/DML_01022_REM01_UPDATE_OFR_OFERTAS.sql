f50b3159fdb8add7f58f66aee0b06a6180d676a0
Carlos Santos Vílchez <carlos.vilchez@pfsgroup.es>
2021-08-26 15:52:06 +0200

    REMVIP-10376 Actualizar lead de oferta

---
+++ sql/9.3/rem/solo_produccion/DML_01022_REM01_UPDATE_OFR_OFERTAS.sql
@@ -0,0 +1,95 @@
+--/*
+--##########################################
+--## AUTOR=Carlos Santos Vílchez
+--## FECHA_CREACION=20210826
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=REMVIP-10376
+--## PRODUCTO=NO
+--##
+--## Finalidad: 
+--## INSTRUCCIONES:
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF;
+
+
+DECLARE
+
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar.
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema.
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master.
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+    V_USUARIO VARCHAR2(30 CHAR) := 'REMVIP-10376'; -- Vble. auxiliar para almacenar el nombre de usuario que modifica los registros.
+	V_COUNT NUMBER(16);
+
+    -- DD_ORC_ID             OFR_ID_PRES_ORI_LEAD
+	TYPE T_TIPO_DATA IS TABLE OF VARCHAR2(32000 CHAR);
+    	TYPE T_ARRAY_DATA IS TABLE OF T_TIPO_DATA;
+    	V_TIPO_DATA T_ARRAY_DATA := T_ARRAY_DATA(
+            T_TIPO_DATA('90332544','110083389')
+
+    	); 
+    	V_TMP_TIPO_DATA T_TIPO_DATA;
+    
+BEGIN		
+
+	DBMS_OUTPUT.PUT_LINE('[INICIO]');
+
+	FOR I IN V_TIPO_DATA.FIRST .. V_TIPO_DATA.LAST
+        LOOP
+    V_TMP_TIPO_DATA := V_TIPO_DATA(I);
+
+        --Comprobamos la existencia de la oferta
+        V_MSQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.OFR_OFERTAS WHERE OFR_NUM_OFERTA = '||V_TMP_TIPO_DATA(1)||'';
+        EXECUTE IMMEDIATE V_MSQL INTO V_COUNT;	
+
+        IF V_COUNT = 1 THEN
+
+            V_COUNT := 0;
+
+            --Actualizamos el dato
+            V_MSQL:= 'UPDATE '||V_ESQUEMA||'.OFR_OFERTAS SET           
+            OFR_ID_PRES_ORI_LEAD = (SELECT PVE_ID FROM '||V_ESQUEMA||'.ACT_PVE_PROVEEDOR WHERE PVE_COD_REM = '||V_TMP_TIPO_DATA(2)||'),
+            USUARIOMODIFICAR = '''||V_USUARIO||''',
+            FECHAMODIFICAR = SYSDATE               
+            WHERE OFR_NUM_OFERTA = '||V_TMP_TIPO_DATA(1)||'';
+            EXECUTE IMMEDIATE V_MSQL;
+
+            DBMS_OUTPUT.PUT_LINE('[INFO] OFERTA '''||V_TMP_TIPO_DATA(1)||''' ACTUALIZADA');
+            
+        ELSE 
+
+            DBMS_OUTPUT.PUT_LINE('[INFO] NO EXISTE LA OFERTA '''||V_TMP_TIPO_DATA(1)||'''');
+
+        END IF;
+
+    END LOOP;
+
+	COMMIT;
+
+	DBMS_OUTPUT.PUT_LINE('[FIN]');
+
+EXCEPTION
+		WHEN OTHERS THEN
+			err_num := SQLCODE;
+			err_msg := SQLERRM;
+
+			DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(err_num));
+			DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+			DBMS_OUTPUT.put_line(err_msg);
+
+			ROLLBACK;
+			RAISE;          
+
+END;
+
+/
+
+EXIT

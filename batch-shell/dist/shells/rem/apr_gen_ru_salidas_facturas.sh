5fd03608efa3c94ecf683d284644a7e7ca4b8714
Alejandra Petrovic <alejandra.petrovic@pfsgroup.es>
2021-09-08 16:24:55 +0200

    HREOS-15079 Modificación ETL apr_gen_ru_salidas_facturas

--- batch-shell/dist/shells/rem/apr_gen_ru_salidas_facturas.sh
+++ batch-shell/dist/shells/rem/apr_gen_ru_salidas_facturas.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# Generado por GUSTAVO MORA 20170810
- 
-
+# Generado automaticamente a las mié jul 23 13:32:51 CEST 2014
 
 filename=$(basename $0)
 nameETL="${filename%.*}"
@@ -11,41 +9,23 @@ export DIR_CONFIG=$DIR_BASE_ETL/config/
 export CFG_FILE=config.ini
 export MAINSH="$nameETL"_run.sh
 
-cd "$DIR_ETL" &> /dev/null
-if [ $? -ne 0 ] ; then
-   echo "$(basename $0) Error en $filename: directorio inexistente $DIR_ETL"
-   exit 1
+echo "Nombre del directorio= $DIR_ETL"
+echo "Adjutos de $1 ha transferir"
+
+if [ ! -d $DIR_ETL ] ; then
+	echo "$(basename $0) Error en $filename: directorio inexistente $DIR_ETL"
+	exit 1
 fi
 
+cd $DIR_ETL
+
 if [ -f $MAINSH ]; then
-    CLASS="$(cat $MAINSH | grep "^ java" | cut -f10 -d" ")"
+    CLASS="$(cat $MAINSH | grep "^ java" | cut -f11 -d" ")"
     CLASS2=`echo $CLASS | sed -e 's/$ROOT_PATH/./g'`
-    CLASEINICIO="$(cat $MAINSH | grep "^ java" | cut -f11 -d" ")"
-    java -Xms512M -Xmx1536M -Dconfig.dir=$DIR_CONFIG -Dconfig.file.mask=$CFG_FILE -Duser.country=ES -Duser.language=es -cp $CLASS2 $CLASEINICIO --context=Default "$@"
-    
-    
-if [ $? = 0 ]; then
-echo "Inicio de transferencia ficheros RUFACTUCP.txt/RUFACTUSP.txt"
-   lftp -c "open -u ftpsocpart,tempo.99 -p 2153 sftp://192.168.126.66; ls /datos/usuarios/socpart/CISA/out"
-
-
-  lftp -u ftpsocpart,tempo.99 -p 2153 sftp://192.168.126.66 <<EOF
-  cd /datos/usuarios/socpart/CISA/out/
-  mput $DIR_SALIDA/RUFACTUCP.txt
-  mput $DIR_SALIDA/RUFACTUSP.txt 
-  cd /datos/usuarios/socpart/CISA/in/
-  mput $DIR_SALIDA/RUFACTUCP.txt
-  mput $DIR_SALIDA/RUFACTUSP.txt 
-  bye
-EOF
-
-  exit 0
-else
-	echo "$(basename $0) Error en la ejecución del proceso"
-    exit 1
-fi    
+    CLASEINICIO="$(cat $MAINSH | grep "^ java" | cut -f12 -d" ")"
+    java -Xms512M -Xmx1536M -Dconfig.dir=$DIR_CONFIG -Dconfig.file.mask=$CFG_FILE -Duser.country=ES -Duser.language=es -Dconfig.param=$1 -cp $CLASS2 $CLASEINICIO --context=Default "$@" 
+    exit $?
 else
     echo "$(basename $0) Error en $filename: no se ha encontrado  $MAINSH"
     exit 1
 fi
-

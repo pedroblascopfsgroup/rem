7559aa2cc6f874a4efc234886ae477bba294df16
Sergio Gomez <sergio.gomez@pfsgroup.es>
2021-09-15 15:46:19 +0200

    HREOS-15050 Crear funciones

---
+++ sql/9.3/rem/DML_00887_REM01_INSERT_FUN_FUNCIONES.sql
@@ -0,0 +1,83 @@
+--/*
+--##########################################
+--## AUTOR=Sergio Gomez
+--## FECHA_CREACION=20210915
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=HREOS-15050
+--## PRODUCTO=NO
+--##
+--## Finalidad: Inserción nuevas funciones declaradas en el array
+--## INSTRUCCIONES: 
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF;
+DECLARE
+    V_MSQL VARCHAR2(4000 CHAR);
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.   
+    seq_count number(3); -- Vble. para validar la existencia de las Secuencias.
+    table_count number(3); -- Vble. para validar la existencia de las Tablas.
+    v_column_count number(3); -- Vble. para validar la existencia de las Columnas.
+    v_constraint_count number(3); -- Vble. para validar la existencia de las Constraints.
+    err_num NUMBER; -- Numero de errores
+    err_msg VARCHAR2(2048); -- Mensaje de error
+  	
+    V_ENTIDAD_ID NUMBER(16);
+    --Insertando valores en FUN_FUNCIONES
+    TYPE T_FUNCION IS TABLE OF VARCHAR2(150);
+    TYPE T_ARRAY_FUNCION IS TABLE OF T_FUNCION;
+    V_FUNCION T_ARRAY_FUNCION := T_ARRAY_FUNCION(
+        T_FUNCION('Avance resolución comité BC','AV_RES_COMITE_BC'),
+        T_FUNCION('Avance Confirmación Fecha Firma Arras','AV_CONF_FF_ARRAS'),
+        T_FUNCION('Avance Confirmarción Fecha Escritura','AV_CONF_F_ESC'),
+        T_FUNCION('Avance Expediente Comercial Bloqueado','AV_ECO_BLOQ')
+    ); 
+    V_TMP_FUNCION T_FUNCION;
+
+BEGIN	
+    DBMS_OUTPUT.PUT_LINE('[INICIO] '||V_ESQUEMA_M||'.FUN_FUNCIONES... Empezando a insertar datos en el diccionario');
+    
+    FOR I IN V_FUNCION.FIRST .. V_FUNCION.LAST
+      LOOP
+        V_MSQL := 'SELECT '|| V_ESQUEMA_M ||'.S_FUN_FUNCIONES.NEXTVAL FROM DUAL';
+        EXECUTE IMMEDIATE V_MSQL INTO V_ENTIDAD_ID;
+            V_TMP_FUNCION := V_FUNCION(I);
+			
+			V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA_M||'.FUN_FUNCIONES WHERE FUN_DESCRIPCION = '''||TRIM(V_TMP_FUNCION(2))||'''';
+			EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
+			-- Si existe la FUNCION
+			IF V_NUM_TABLAS > 0 THEN				
+				DBMS_OUTPUT.PUT_LINE('[INFO] ' || V_ESQUEMA_M || '.FUN_FUNCIONES... Ya existe la funcion '''|| TRIM(V_TMP_FUNCION(2))||'''');
+			ELSE		
+				V_MSQL := 'INSERT INTO '|| V_ESQUEMA_M ||'.FUN_FUNCIONES (' ||
+						'FUN_ID, FUN_DESCRIPCION_LARGA, FUN_DESCRIPCION, VERSION, USUARIOCREAR, FECHACREAR, BORRADO)' ||
+						'SELECT '|| V_ENTIDAD_ID || ','''||V_TMP_FUNCION(1)||''','''||TRIM(V_TMP_FUNCION(2))||''','||
+						'0, ''HREOS-15050'',SYSDATE,0 FROM DUAL';
+				DBMS_OUTPUT.PUT_LINE('INSERTANDO: '''||V_TMP_FUNCION(1)||''','''||TRIM(V_TMP_FUNCION(2))||'''');
+				EXECUTE IMMEDIATE V_MSQL;
+			END IF;
+      END LOOP;
+    COMMIT;
+    DBMS_OUTPUT.PUT_LINE('[FIN] '||V_ESQUEMA_M||'.FUN_FUNCIONES... Datos del diccionario insertado');
+
+EXCEPTION
+     WHEN OTHERS THEN
+          ERR_NUM := SQLCODE;
+          ERR_MSG := SQLERRM;
+          DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(ERR_NUM));
+         DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+	 DBMS_OUTPUT.put_line(V_SQL);
+          DBMS_OUTPUT.put_line(ERR_MSG);
+          ROLLBACK;
+          RAISE;   
+END;
+/
+EXIT;
+  	

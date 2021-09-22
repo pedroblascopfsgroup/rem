947cf0ee6bfeb1ab04bd301965c647147555fda2
Cristian Montoya <cristian.montoya@pfsgroup.es>
2021-09-22 08:47:26 +0200

    HREOS-15245 Creados updater services de tareas tramite alquiler no comercial.

---
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/jbpm/handler/updater/impl/UpdaterServiceDefinicionOfertaAlquilerNoComercial.java
@@ -0,0 +1,88 @@
+package es.pfsgroup.plugin.rem.jbpm.handler.updater.impl;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import es.capgemini.pfs.procesosJudiciales.model.TareaExterna;
+import es.capgemini.pfs.procesosJudiciales.model.TareaExternaValor;
+import es.pfsgroup.commons.utils.Checks;
+import es.pfsgroup.commons.utils.dao.abm.GenericABMDao;
+import es.pfsgroup.commons.utils.dao.abm.GenericABMDao.FilterType;
+import es.pfsgroup.plugin.recovery.coreextension.utils.api.UtilDiccionarioApi;
+import es.pfsgroup.plugin.rem.api.ExpedienteComercialApi;
+import es.pfsgroup.plugin.rem.api.OfertaApi;
+import es.pfsgroup.plugin.rem.api.RecalculoVisibilidadComercialApi;
+import es.pfsgroup.plugin.rem.jbpm.handler.updater.UpdaterService;
+import es.pfsgroup.plugin.rem.model.ActivoOferta;
+import es.pfsgroup.plugin.rem.model.ActivoTramite;
+import es.pfsgroup.plugin.rem.model.CondicionanteExpediente;
+import es.pfsgroup.plugin.rem.model.ExpedienteComercial;
+import es.pfsgroup.plugin.rem.model.Oferta;
+import es.pfsgroup.plugin.rem.model.dd.DDEstadoExpedienteBc;
+import es.pfsgroup.plugin.rem.model.dd.DDEstadoOferta;
+import es.pfsgroup.plugin.rem.model.dd.DDEstadosExpedienteComercial;
+import es.pfsgroup.plugin.rem.model.dd.DDResultadoCampo;
+
+@Component
+public class UpdaterServiceDefinicionOfertaAlquilerNoComercial implements UpdaterService {
+	
+    @Autowired
+    private GenericABMDao genericDao;
+
+    @Autowired
+	private UtilDiccionarioApi utilDiccionarioApi;
+    
+    @Autowired
+    private ExpedienteComercialApi expedienteComercialApi;
+    
+	@Autowired
+	private OfertaApi ofertaApi;
+	
+	@Autowired
+	private RecalculoVisibilidadComercialApi recalculoVisibilidadComercialApi;
+
+    protected static final Log logger = LogFactory.getLog(UpdaterServiceDefinicionOfertaAlquilerNoComercial.class);
+    
+	private static final String CODIGO_T018_DEFINICION_OFERTA = "T018_DefinicionOferta";
+
+	SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd");
+	
+	public void saveValues(ActivoTramite tramite, TareaExterna tareaExternaActual, List<TareaExternaValor> valores) {
+
+		ExpedienteComercial expedienteComercial = expedienteComercialApi.findOneByTrabajo(tramite.getTrabajo());
+		Oferta oferta = expedienteComercial.getOferta();
+		
+		DDEstadosExpedienteComercial estadoExpedienteComercial = null;
+		DDEstadoOferta estadoOferta = null;
+		DDEstadoExpedienteBc estadoExpedienteBc = null;
+		
+		CondicionanteExpediente coe = expedienteComercial.getCondicionante();
+
+		estadoExpedienteComercial = genericDao.get(DDEstadosExpedienteComercial.class,genericDao.createFilter(FilterType.EQUALS,"codigo", DDEstadosExpedienteComercial.PTE_SCREENING_Y_ANALISIS_BC));
+		estadoExpedienteBc = genericDao.get(DDEstadoExpedienteBc.class,genericDao.createFilter(FilterType.EQUALS,"codigo", DDEstadoExpedienteBc.PTE_SCREENING_Y_ANALISIS_BC));
+		
+		expedienteComercial.setEstado(estadoExpedienteComercial);
+		expedienteComercial.setEstadoBc(estadoExpedienteBc);
+
+		recalculoVisibilidadComercialApi.recalcularVisibilidadComercial(expedienteComercial.getOferta(), estadoExpedienteComercial);				
+
+		expedienteComercialApi.update(expedienteComercial,false);	
+		
+		ofertaApi.replicateOfertaFlushDto(expedienteComercial.getOferta(),expedienteComercialApi.buildReplicarOfertaDtoFromExpediente(expedienteComercial));
+
+	}
+
+	public String[] getCodigoTarea() {
+		return new String[]{CODIGO_T018_DEFINICION_OFERTA};
+	}
+
+	public String[] getKeys() {
+		return this.getCodigoTarea();
+	}
+
+}
---
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/jbpm/handler/updater/impl/UpdaterServiceRevisionBcYCondicionesAlquilerNoComercial.java
@@ -0,0 +1,107 @@
+package es.pfsgroup.plugin.rem.jbpm.handler.updater.impl;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import es.capgemini.pfs.procesosJudiciales.model.DDSiNo;
+import es.capgemini.pfs.procesosJudiciales.model.TareaExterna;
+import es.capgemini.pfs.procesosJudiciales.model.TareaExternaValor;
+import es.pfsgroup.commons.utils.Checks;
+import es.pfsgroup.commons.utils.dao.abm.GenericABMDao;
+import es.pfsgroup.commons.utils.dao.abm.GenericABMDao.FilterType;
+import es.pfsgroup.plugin.recovery.coreextension.utils.api.UtilDiccionarioApi;
+import es.pfsgroup.plugin.rem.api.ExpedienteComercialApi;
+import es.pfsgroup.plugin.rem.api.OfertaApi;
+import es.pfsgroup.plugin.rem.api.RecalculoVisibilidadComercialApi;
+import es.pfsgroup.plugin.rem.jbpm.handler.updater.UpdaterService;
+import es.pfsgroup.plugin.rem.model.ActivoOferta;
+import es.pfsgroup.plugin.rem.model.ActivoTramite;
+import es.pfsgroup.plugin.rem.model.CondicionanteExpediente;
+import es.pfsgroup.plugin.rem.model.ExpedienteComercial;
+import es.pfsgroup.plugin.rem.model.Oferta;
+import es.pfsgroup.plugin.rem.model.dd.DDEstadoExpedienteBc;
+import es.pfsgroup.plugin.rem.model.dd.DDEstadoOferta;
+import es.pfsgroup.plugin.rem.model.dd.DDEstadosExpedienteComercial;
+import es.pfsgroup.plugin.rem.model.dd.DDResultadoCampo;
+
+@Component
+public class UpdaterServiceRevisionBcYCondicionesAlquilerNoComercial implements UpdaterService {
+	
+    @Autowired
+    private GenericABMDao genericDao;
+
+    @Autowired
+	private UtilDiccionarioApi utilDiccionarioApi;
+    
+    @Autowired
+    private ExpedienteComercialApi expedienteComercialApi;
+    
+	@Autowired
+	private OfertaApi ofertaApi;
+	
+	@Autowired
+	private RecalculoVisibilidadComercialApi recalculoVisibilidadComercialApi;
+
+    protected static final Log logger = LogFactory.getLog(UpdaterServiceRevisionBcYCondicionesAlquilerNoComercial.class);
+    
+	private static final String COMBO_RESULTADO= "comboResultado";
+
+	private static final String CODIGO_T018_REVISION_BC_Y_CONDICIONES = "T018_RevisionBcYCondiciones";
+
+	SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd");
+	
+	public void saveValues(ActivoTramite tramite, TareaExterna tareaExternaActual, List<TareaExternaValor> valores) {
+
+		ExpedienteComercial expedienteComercial = expedienteComercialApi.findOneByTrabajo(tramite.getTrabajo());
+		Oferta oferta = expedienteComercial.getOferta();
+		
+		DDEstadosExpedienteComercial estadoExpedienteComercial = null;
+		DDEstadoOferta estadoOferta = null;
+		DDEstadoExpedienteBc estadoExpedienteBc = null;
+		
+		CondicionanteExpediente coe = expedienteComercial.getCondicionante();
+
+		for(TareaExternaValor valor :  valores){
+			
+			if(COMBO_RESULTADO.equals(valor.getNombre()) && valor.getValor() != null) {
+				if(DDSiNo.SI.equals(valor.getValor())) {
+					
+					if(coe.getVulnerabilidadDetectada() != null && coe.getVulnerabilidadDetectada()) {
+						estadoExpedienteComercial = genericDao.get(DDEstadosExpedienteComercial.class,genericDao.createFilter(FilterType.EQUALS,"codigo", DDEstadosExpedienteComercial.PENDIENTE_GARANTIAS_ADICIONALES));
+						estadoExpedienteBc = genericDao.get(DDEstadoExpedienteBc.class,genericDao.createFilter(FilterType.EQUALS,"codigo", DDEstadoExpedienteBc.CODIGO_PTE_GARANTIAS_ADICIONALES));
+					}else {
+						estadoExpedienteComercial = genericDao.get(DDEstadosExpedienteComercial.class,genericDao.createFilter(FilterType.EQUALS,"codigo", DDEstadosExpedienteComercial.PTE_TRASLADAR_OFERTA_AL_CLIENTE));
+						estadoExpedienteBc = genericDao.get(DDEstadoExpedienteBc.class,genericDao.createFilter(FilterType.EQUALS,"codigo", DDEstadoExpedienteBc.PTE_TRASLADAR_OFERTA_AL_CLIENTE));
+					}
+				}else {
+					estadoExpedienteComercial = genericDao.get(DDEstadosExpedienteComercial.class,genericDao.createFilter(FilterType.EQUALS,"codigo", DDEstadosExpedienteComercial.ANULADO));
+					estadoExpedienteBc = genericDao.get(DDEstadoExpedienteBc.class,genericDao.createFilter(FilterType.EQUALS,"codigo", DDEstadoExpedienteBc.CODIGO_OFERTA_CANCELADA));
+				}
+				
+				expedienteComercial.setEstado(estadoExpedienteComercial);
+				expedienteComercial.setEstadoBc(estadoExpedienteBc);
+			}
+		}
+
+		recalculoVisibilidadComercialApi.recalcularVisibilidadComercial(expedienteComercial.getOferta(), estadoExpedienteComercial);				
+
+		expedienteComercialApi.update(expedienteComercial,false);	
+		
+		ofertaApi.replicateOfertaFlushDto(expedienteComercial.getOferta(),expedienteComercialApi.buildReplicarOfertaDtoFromExpediente(expedienteComercial));
+
+	}
+
+	public String[] getCodigoTarea() {
+		return new String[]{CODIGO_T018_REVISION_BC_Y_CONDICIONES};
+	}
+
+	public String[] getKeys() {
+		return this.getCodigoTarea();
+	}
+
+}

267ea93cc65273c305a880b53a3900fa50ea56f6
Javier Esbri <javier.esbri@pfsgroup.es>
2021-09-23 15:40:46 +0200

    HREOS-15273 [BPM alq - Fase II] Restricciones sobre el activo para la creación de ofertas

--- proyecto-rem-online/sencha-app/classic/src/view/activos/detalle/OfertasComercialActivoList.js
+++ proyecto-rem-online/sencha-app/classic/src/view/activos/detalle/OfertasComercialActivoList.js
@@ -273,25 +273,7 @@ Ext.define('HreRem.view.activos.detalle.OfertasComercialActivoList', {
 					me.fireEvent("errorToast", HreRem.i18n("msg.crear.oferta.estado.error"));
 					return;
 				}
-			}
-			
-			if(activo.get('incluyeDestinoComercialAlquiler') && !activo.get('incluyeDestinoComercialVenta')){
-				if(Ext.isEmpty(me.lookupController().getViewModel().get('canalAlquilerBC').selection)){
-					me.fireEvent("errorToast", HreRem.i18n("msg.cambio.canal.alquiler.bc"));
-					return;
-				}
-			}else if(!activo.get('incluyeDestinoComercialAlquiler') && activo.get('incluyeDestinoComercialVenta')){
-				if(Ext.isEmpty(me.lookupController().getViewModel().get('canalVentaBC').selection)){
-					me.fireEvent("errorToast", HreRem.i18n("msg.cambio.canal.venta.bc"));
-					return;
-				}
-			}else if(activo.get('incluyeDestinoComercialAlquiler') && activo.get('incluyeDestinoComercialVenta')){
-				if(Ext.isEmpty(me.lookupController().getViewModel().get('canalVentaBC').selection) && Ext.isEmpty(me.lookupController().getViewModel().get('canalAlquilerBC').selection)){
-					me.fireEvent("errorToast", HreRem.i18n("msg.cambio.canal.bc"));
-					return;
-				}
-			}			
-			
+			}		
 		}
 		
 		if (activo.get('incluyeDestinoComercialAlquiler')) {

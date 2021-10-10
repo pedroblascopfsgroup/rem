eb25c7d969ffdd43d5178542bfaae4406a9686e4
Pier José Gotta Barrera <pier.gotta@pfsgroup.es>
2021-10-10 18:35:31 +0200

    HREOS-15441 Migracion ofertas y tramites

---
+++ sql/9.3/rem/migracion_ofertas_caixa/FASE_A/BLOQUE1/DDL_00000_REM_MIG_VOLUMETRIA_TABLAS.sql
@@ -0,0 +1,80 @@
+--/*
+--#########################################
+--## AUTOR=PIER GOTTA
+--## FECHA_CREACION=20210820
+--## ARTEFACTO=batch
+--## VERSION_ARTEFACTO=0.1
+--## INCIDENCIA_LINK=HREOS-14936
+--## PRODUCTO=NO
+--##
+--## Finalidad: Creación de tabla de migración
+--##
+--## INSTRUCCIONES:
+--## VERSIONES:
+--##        0.1 Versión inicial
+--#########################################
+--*/
+
+--Para permitir la visualización de texto en un bloque PL/SQL utilizando DBMS_OUTPUT.PUT_LINE
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
+SET DEFINE OFF;
+
+
+DECLARE
+
+TABLE_COUNT NUMBER(1,0) := 0;
+V_ESQUEMA_1 VARCHAR2(20 CHAR) := 'REM01';
+V_ESQUEMA_2 VARCHAR2(20 CHAR) := 'REM01';       --SE CREA UNA SEGUNDA VARIABLE DE ESQUEMA POR SI EN ALGÚN MOMENTO QUEREMOS CREAR LA TABLA EN UN ESQUEMA DIFERENTE AL DEL USUARIO QUE LA ACCEDE O VICEVERSA
+V_TABLA VARCHAR2(40 CHAR) := 'MIG2_CAIXA_VOLUMETRIA_TABLAS';
+
+BEGIN
+
+        SELECT COUNT(1) INTO TABLE_COUNT FROM ALL_TABLES WHERE TABLE_NAME = ''||V_TABLA||'' AND OWNER= ''||V_ESQUEMA_1||'';
+
+        IF TABLE_COUNT > 0 THEN
+
+                DBMS_OUTPUT.PUT_LINE('[INFO] TABLA '||V_ESQUEMA_1||'.'||V_TABLA||' YA EXISTENTE. SE PROCEDE A BORRAR Y CREAR DE NUEVO.');
+
+                EXECUTE IMMEDIATE 'DROP TABLE '||V_ESQUEMA_1||'.'||V_TABLA||'';
+
+        END IF;
+
+        EXECUTE IMMEDIATE '
+        CREATE TABLE '||V_ESQUEMA_1||'.'||V_TABLA||'
+        (
+            MIG_NOMBRE_INTERFAZ                 VARCHAR2(50 CHAR),
+            MIG_CANTIDAD_REGISTROS              NUMBER(16),
+            FECHA                               DATE
+	)';
+
+	DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA_1||'.'||V_TABLA||' CREADA');
+            EXECUTE IMMEDIATE 'COMMENT ON COLUMN ' || V_ESQUEMA_1 || '.'||V_TABLA||'.MIG_NOMBRE_INTERFAZ IS ''Nombre de la interfaz''';
+            EXECUTE IMMEDIATE 'COMMENT ON COLUMN ' || V_ESQUEMA_1 || '.'||V_TABLA||'.MIG_CANTIDAD_REGISTROS IS ''Total de registros''';
+            EXECUTE IMMEDIATE 'COMMENT ON COLUMN ' || V_ESQUEMA_1 || '.'||V_TABLA||'.FECHA IS ''Fecha''';
+
+    IF V_ESQUEMA_2 != V_ESQUEMA_1 THEN
+
+		EXECUTE IMMEDIATE 'GRANT ALL ON "'||V_ESQUEMA_1||'"."'||V_TABLA||'" TO "'||V_ESQUEMA_2||'" WITH GRANT OPTION';
+
+		DBMS_OUTPUT.PUT_LINE('[INFO] PERMISOS SOBRE LA TABLA '||V_ESQUEMA_1||'.'||V_TABLA||' OTORGADOS A '||V_ESQUEMA_2||'');
+
+	END IF;
+	EXECUTE IMMEDIATE 'GRANT ALL ON "'||V_ESQUEMA_1||'"."'||V_TABLA||'" TO "REMMASTER" WITH GRANT OPTION';
+	DBMS_OUTPUT.PUT_LINE('[INFO] PERMISOS SOBRE LA TABLA '||V_ESQUEMA_1||'.'||V_TABLA||' OTORGADOS A REMMASTER');
+EXCEPTION
+
+    WHEN OTHERS THEN
+	DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecucion:'||TO_CHAR(SQLCODE));
+	DBMS_OUTPUT.put_line('-----------------------------------------------------------');
+	DBMS_OUTPUT.put_line(SQLERRM);
+	ROLLBACK;
+	RAISE;
+
+
+
+
+END; 
+ / 
+ EXIT;

9e8928f7247dc170525c3230c362fb1ee4fd1575
Daniel Algaba <daniel.algaba@pfsgroup.es>
2021-10-19 11:23:54 +0200

    HREOS-15634 Debemos implementar una equivalencia en titulizadas para el campo Fondo, según el propietario real y la sociedad gestora

--- sql/9.3/rem/procs_y_vistas/DDL_00177_REM01_SP_RBC_TIT_01_DATOS_GENERALES.sql
+++ sql/9.3/rem/procs_y_vistas/DDL_00177_REM01_SP_RBC_TIT_01_DATOS_GENERALES.sql
@@ -1,16 +1,17 @@
 --/*
 --##########################################
 --## AUTOR=Daniel Algaba
---## FECHA_CREACION=20211001
+--## FECHA_CREACION=20211019
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.3
---## INCIDENCIA_LINK=HREOS-15217
+--## INCIDENCIA_LINK=HREOS-15634
 --## PRODUCTO=NO
 --##
 --## Finalidad: 
 --## INSTRUCCIONES:
 --## VERSIONES:
 --##        0.1 Versión inicial
+--##        0.2 Se cambian los NIFs de titulizados - [HREOS-15634] - Daniel Algaba
 --##########################################
 --*/
 WHENEVER SQLERROR EXIT SQL.SQLCODE;
@@ -48,7 +49,8 @@ BEGIN
 				USING (				                 
                   SELECT       
                      ACT.ACT_NUM_ACTIVO_CAIXA as NUM_IDENTIFICATIVO 
-                     , BCR.SOCIEDAD_PATRIMONIAL
+                     , EQV1.DD_CODIGO_CAIXA SOCIEDAD_PATRIMONIAL
+                     , EQV2.DD_CODIGO_CAIXA FONDO
                      , BCR.ESTADO_POSESORIO
                      , BCR.FEC_ESTADO_POSESORIO
                      , TO_CHAR(COALESCE(ADJ.BIE_ADJ_F_DECRETO_FIRME, ADN.ADN_FECHA_TITULO),''YYYYMMDD'') FEC_TITULO_FIRME       
@@ -64,7 +66,7 @@ BEGIN
                      , BCR.CLASE_USO
                      , BCR.SUBTIPO_SUELO
                      FROM '|| V_ESQUEMA ||'.ACT_ACTIVO ACT
-                     JOIN '|| V_ESQUEMA ||'.AUX_APR_BCR_STOCK BCR ON ACT.ACT_NUM_ACTIVO_CAIXA = BCR.NUM_IDENTIFICATIVO
+                     LEFT JOIN '|| V_ESQUEMA ||'.AUX_APR_BCR_STOCK BCR ON ACT.ACT_NUM_ACTIVO_CAIXA = BCR.NUM_IDENTIFICATIVO
                      JOIN '|| V_ESQUEMA ||'.ACT_PAC_PERIMETRO_ACTIVO PAC ON PAC.ACT_ID = ACT.ACT_ID AND PAC.BORRADO = 0
                      JOIN '|| V_ESQUEMA ||'.ACT_PAC_PROPIETARIO_ACTIVO ACT_PRO ON ACT_PRO.ACT_ID = ACT.ACT_ID AND ACT_PRO.BORRADO = 0
                      JOIN '|| V_ESQUEMA ||'.ACT_PRO_PROPIETARIO PRO ON PRO.PRO_ID = ACT_PRO.PRO_ID AND PRO.BORRADO = 0
@@ -72,15 +74,18 @@ BEGIN
                      LEFT JOIN '|| V_ESQUEMA ||'.BIE_ADJ_ADJUDICACION ADJ ON ADJ.BIE_ID = ACT.BIE_ID AND ADJ.BORRADO = 0
                      LEFT JOIN '|| V_ESQUEMA ||'.ACT_ADN_ADJNOJUDICIAL ADN ON ADN.ACT_ID = ACT.ACT_ID AND ADN.BORRADO = 0
                      LEFT JOIN '|| V_ESQUEMA ||'.DD_TCO_TIPO_COMERCIALIZACION TCO ON TCO.DD_TCO_ID = APU.DD_TCO_ID AND TCO.BORRADO = 0
+                     LEFT JOIN '|| V_ESQUEMA ||'.DD_EQV_TIT_CAIXA_REM EQV1 ON EQV1.DD_NOMBRE_CAIXA = ''SOCIEDAD_PATRIMONIAL'' AND EQV1.DD_CODIGO_REM = PRO.PRO_DOCIDENTIF AND EQV1.BORRADO = 0 
+                     LEFT JOIN '|| V_ESQUEMA ||'.DD_EQV_TIT_CAIXA_REM EQV2 ON EQV2.DD_NOMBRE_CAIXA = ''FONDO'' AND EQV2.DD_CODIGO_REM = PRO.PRO_DOCIDENTIF AND EQV2.BORRADO = 0 
                      WHERE ACT.ACT_NUM_ACTIVO_CAIXA IS NOT NULL
                      AND ACT.DD_CRA_ID = (SELECT DD_CRA_ID FROM '|| V_ESQUEMA ||'.DD_CRA_CARTERA WHERE DD_CRA_CODIGO = ''03'')
                      AND ACT.BORRADO = 0
                      AND PAC.PAC_INCLUIDO = 1
                      AND ACT.ACT_EN_TRAMITE = 0
-                     AND PRO.PRO_DOCIDENTIF IN (''A80352750'', ''A80514466'')
+                     AND PRO.PRO_DOCIDENTIF IN (''V84966126'',''V85164648'',''V85587434'',''V84322205'',''V84593961'',''V84669332'',''V85082675'',''V85623668'',''V84856319'',''V85500866'',''V85143659'',''V85594927'',''V85981231'',''V84889229'',''V84916956'',''V85160935'',''V85295087'',''V84175744'',''V84925569'')
                   ) AUX ON (RBC_TIT.NUM_IDENTIFICATIVO = AUX.NUM_IDENTIFICATIVO)
                   WHEN MATCHED THEN UPDATE SET
                      RBC_TIT.SOCIEDAD_PATRIMONIAL = AUX.SOCIEDAD_PATRIMONIAL
+                     , RBC_TIT.FONDO = AUX.FONDO
                      , RBC_TIT.ESTADO_POSESORIO = AUX.ESTADO_POSESORIO
                      , RBC_TIT.FEC_ESTADO_POSESORIO = AUX.FEC_ESTADO_POSESORIO
                      , RBC_TIT.FEC_TITULO_FIRME = AUX.FEC_TITULO_FIRME
@@ -98,7 +103,8 @@ BEGIN
                   WHEN NOT MATCHED THEN
                   INSERT  (
                               NUM_IDENTIFICATIVO
-                              , SOCIEDAD_PATRIMONIAL                                                                        
+                              , SOCIEDAD_PATRIMONIAL  
+                              , FONDO                                                                      
                               , ESTADO_POSESORIO
                               , FEC_ESTADO_POSESORIO
                               , FEC_TITULO_FIRME   
@@ -116,7 +122,8 @@ BEGIN
                            )
                   VALUES   (
                               AUX.NUM_IDENTIFICATIVO
-                              , AUX.SOCIEDAD_PATRIMONIAL                                                                       
+                              , AUX.SOCIEDAD_PATRIMONIAL    
+                              , AUX.FONDO                                                                   
                               , AUX.ESTADO_POSESORIO
                               , AUX.FEC_ESTADO_POSESORIO
                               , AUX.FEC_TITULO_FIRME 

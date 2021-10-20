d93c0d10bb77960d296b783ec53df9fe15dd9be2
Daniel Algaba <daniel.algaba@pfsgroup.es>
2021-10-20 12:55:59 +0200

    HREOS-15634 Quitar de los SP de titulizados la tabla de stock BC y recuperar esa info de otra manera, haciendo uso de los mapeos propuesto por Ana Lobo en el siguiente documento

--- sql/9.3/rem/procs_y_vistas/DDL_00177_REM01_SP_RBC_TIT_01_DATOS_GENERALES.sql
+++ sql/9.3/rem/procs_y_vistas/DDL_00177_REM01_SP_RBC_TIT_01_DATOS_GENERALES.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=Daniel Algaba
---## FECHA_CREACION=20211019
+--## FECHA_CREACION=20211020
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.3
 --## INCIDENCIA_LINK=HREOS-15634
@@ -12,6 +12,7 @@
 --## VERSIONES:
 --##        0.1 Versión inicial
 --##        0.2 Se cambian los NIFs de titulizados - [HREOS-15634] - Daniel Algaba
+--##        0.3 Se añaden más mapeos - [HREOS-15634] - Daniel Algaba
 --##########################################
 --*/
 WHENEVER SQLERROR EXIT SQL.SQLCODE;
@@ -51,31 +52,50 @@ BEGIN
                      ACT.ACT_NUM_ACTIVO_CAIXA as NUM_IDENTIFICATIVO 
                      , EQV1.DD_CODIGO_CAIXA SOCIEDAD_PATRIMONIAL
                      , EQV2.DD_CODIGO_CAIXA FONDO
-                     , BCR.ESTADO_POSESORIO
-                     , BCR.FEC_ESTADO_POSESORIO
-                     , TO_CHAR(COALESCE(ADJ.BIE_ADJ_F_DECRETO_FIRME, ADN.ADN_FECHA_TITULO),''YYYYMMDD'') FEC_TITULO_FIRME       
-                     , ACT.ACT_PORCENTAJE_CONSTRUCCION PORC_OBRA_EJECUTADA
-                     , NULL FEC_COMPRA
-                     , BCR.FEC_VALIDO_DE
-                     , NULL SUBTIPO_VIVIENDA/*BCR.SUBTIPO_VIVIENDA*/
-                     , NULL /*BCR.*/IND_OCUPANTES_VIVIENDA
-                     , NULL /*BCR.*/PRODUCTO
+                     , CASE WHEN SPS.SPS_OCUPADO IS NULL AND TTT.DD_TPA_ID IS NULL AND EAL.DD_EAL_ID IS NULL THEN ''P01''
+                           WHEN SPS.SPS_OCUPADO = 1 AND TTT.DD_TPA_CODIGO = ''01'' AND EAL.DD_EAL_CODIGO = ''02'' THEN  ''P02''
+                           WHEN SPS.SPS_OCUPADO = 1 AND TTT.DD_TPA_CODIGO = ''02'' AND EAL.DD_EAL_ID IS NULL THEN ''P03''
+                           WHEN SPS.SPS_VERTICAL = 1 THEN ''P05'' 
+                           WHEN SPS.SPS_OCUPADO = 0 AND TTT.DD_TPA_ID IS NULL AND (EAL.DD_EAL_ID IS NULL OR EAL.DD_EAL_CODIGO = ''01'') THEN ''P06''
+                           ELSE NULL
+                     END ESTADO_POSESORIO
+                     , NULL FEC_ESTADO_POSESORIO   
+                     , ACT.ACT_PORCENTAJE_CONSTRUCCION*100 PORC_OBRA_EJECUTADA
+                     , EQV5.DD_CODIGO_CAIXA SUBTIPO_VIVIENDA
+                     , NULL IND_OCUPANTES_VIVIENDA
+                     , NULL PRODUCTO
                      , NULL /*DECODE(TCO.DD_TCO_CODIGO, ''03'', ''N'', ''S'')*/ VENTA
-                     , NULL /*BCR.*/SITUACION_VPO
+                     , CASE WHEN ACT.ACT_VPO = 0 THEN ''0001'' 
+                           WHEN ACT.ACT_VPO = 1 AND ADM.ADM_DESCALIFICADO = 1 THEN ''0002'' 
+                           WHEN ACT.ACT_VPO = 1 AND ADM.ADM_ACTUALIZA_PRECIO_MAX = 0 THEN ''0003'' 
+                           WHEN ACT.ACT_VPO = 1 AND ADM.ADM_ACTUALIZA_PRECIO_MAX = 1 THEN ''0004'' 
+                           WHEN ACT.ACT_VPO = 1 AND ADM.ADM_LIBERTAD_CESION = 0 THEN ''0005'' 
+                           ELSE NULL
+                     END SITUACION_VPO
                      , NULL LIC_PRI_OCUPACION
-                     , BCR.CLASE_USO
-                     , BCR.SUBTIPO_SUELO
+                     , EQV3.DD_CODIGO_CAIXA CLASE_USO
+                     , EQV4.DD_CODIGO_CAIXA SUBTIPO_SUELO
                      FROM '|| V_ESQUEMA ||'.ACT_ACTIVO ACT
-                     LEFT JOIN '|| V_ESQUEMA ||'.AUX_APR_BCR_STOCK BCR ON ACT.ACT_NUM_ACTIVO_CAIXA = BCR.NUM_IDENTIFICATIVO
                      JOIN '|| V_ESQUEMA ||'.ACT_PAC_PERIMETRO_ACTIVO PAC ON PAC.ACT_ID = ACT.ACT_ID AND PAC.BORRADO = 0
                      JOIN '|| V_ESQUEMA ||'.ACT_PAC_PROPIETARIO_ACTIVO ACT_PRO ON ACT_PRO.ACT_ID = ACT.ACT_ID AND ACT_PRO.BORRADO = 0
                      JOIN '|| V_ESQUEMA ||'.ACT_PRO_PROPIETARIO PRO ON PRO.PRO_ID = ACT_PRO.PRO_ID AND PRO.BORRADO = 0
+                     LEFT JOIN '|| V_ESQUEMA ||'.ACT_SPS_SIT_POSESORIA SPS ON SPS.ACT_ID = ACT.ACT_ID AND SPS.BORRADO = 0
+                     LEFT JOIN '|| V_ESQUEMA ||'.ACT_PTA_PATRIMONIO_ACTIVO PTA ON PTA.ACT_ID = ACT.ACT_ID AND PTA.BORRADO = 0
+                     LEFT JOIN '|| V_ESQUEMA ||'.DD_TPA_TIPO_TITULO_ACT TTT ON TTT.DD_TPA_ID = SPS.DD_TPA_ID AND TTT.BORRADO = 0
+                     LEFT JOIN '|| V_ESQUEMA ||'.DD_EAL_ESTADO_ALQUILER EAL ON EAL.DD_EAL_ID = PTA.DD_EAL_ID AND EAL.BORRADO = 0
+                     LEFT JOIN '|| V_ESQUEMA ||'.ACT_ADM_INF_ADMINISTRATIVA ADM ON ADM.ACT_ID = ACT.ACT_ID AND ADM.BORRADO = 0
                      LEFT JOIN '|| V_ESQUEMA ||'.ACT_APU_ACTIVO_PUBLICACION APU ON APU.ACT_ID = ACT.ACT_ID AND APU.BORRADO = 0
                      LEFT JOIN '|| V_ESQUEMA ||'.BIE_ADJ_ADJUDICACION ADJ ON ADJ.BIE_ID = ACT.BIE_ID AND ADJ.BORRADO = 0
                      LEFT JOIN '|| V_ESQUEMA ||'.ACT_ADN_ADJNOJUDICIAL ADN ON ADN.ACT_ID = ACT.ACT_ID AND ADN.BORRADO = 0
                      LEFT JOIN '|| V_ESQUEMA ||'.DD_TCO_TIPO_COMERCIALIZACION TCO ON TCO.DD_TCO_ID = APU.DD_TCO_ID AND TCO.BORRADO = 0
                      LEFT JOIN '|| V_ESQUEMA ||'.DD_EQV_TIT_CAIXA_REM EQV1 ON EQV1.DD_NOMBRE_CAIXA = ''SOCIEDAD_PATRIMONIAL'' AND EQV1.DD_CODIGO_REM = PRO.PRO_DOCIDENTIF AND EQV1.BORRADO = 0 
                      LEFT JOIN '|| V_ESQUEMA ||'.DD_EQV_TIT_CAIXA_REM EQV2 ON EQV2.DD_NOMBRE_CAIXA = ''FONDO'' AND EQV2.DD_CODIGO_REM = PRO.PRO_DOCIDENTIF AND EQV2.BORRADO = 0 
+                     LEFT JOIN '|| V_ESQUEMA ||'.DD_SAC_SUBTIPO_ACTIVO SAC_CLASE_USO ON SAC_CLASE_USO.DD_SAC_ID = ACT.DD_SAC_ID AND SAC_CLASE_USO.BORRADO = 0
+                     LEFT JOIN '|| V_ESQUEMA ||'.DD_EQV_TIT_CAIXA_REM EQV3 ON EQV3.DD_NOMBRE_CAIXA = ''CLASE_USO'' AND EQV3.DD_CODIGO_REM = SAC_CLASE_USO.DD_SAC_CODIGO AND EQV3.BORRADO = 0
+                     LEFT JOIN '|| V_ESQUEMA ||'.DD_SAC_SUBTIPO_ACTIVO SAC_SUELO ON SAC_SUELO.DD_SAC_ID = ACT.DD_SAC_ID AND SAC_SUELO.BORRADO = 0
+                     LEFT JOIN '|| V_ESQUEMA ||'.DD_EQV_TIT_CAIXA_REM EQV4 ON EQV4.DD_NOMBRE_CAIXA = ''SUBTIPO_SUELO'' AND EQV4.DD_CODIGO_REM = SAC_SUELO.DD_SAC_CODIGO AND EQV4.BORRADO = 0
+                     LEFT JOIN '|| V_ESQUEMA ||'.DD_SAC_SUBTIPO_ACTIVO SAC_VIVIENDA ON SAC_VIVIENDA.DD_SAC_ID = ACT.DD_SAC_ID AND SAC_VIVIENDA.BORRADO = 0
+                     LEFT JOIN '|| V_ESQUEMA ||'.DD_EQV_TIT_CAIXA_REM EQV5 ON EQV5.DD_NOMBRE_CAIXA = ''SUBTIPO_VIVIENDA'' AND EQV5.DD_CODIGO_REM = SAC_SUELO.DD_SAC_CODIGO AND EQV5.BORRADO = 0   
                      WHERE ACT.ACT_NUM_ACTIVO_CAIXA IS NOT NULL
                      AND ACT.DD_CRA_ID = (SELECT DD_CRA_ID FROM '|| V_ESQUEMA ||'.DD_CRA_CARTERA WHERE DD_CRA_CODIGO = ''03'')
                      AND ACT.BORRADO = 0
@@ -88,10 +108,7 @@ BEGIN
                      , RBC_TIT.FONDO = AUX.FONDO
                      , RBC_TIT.ESTADO_POSESORIO = AUX.ESTADO_POSESORIO
                      , RBC_TIT.FEC_ESTADO_POSESORIO = AUX.FEC_ESTADO_POSESORIO
-                     , RBC_TIT.FEC_TITULO_FIRME = AUX.FEC_TITULO_FIRME
                      , RBC_TIT.PORC_OBRA_EJECUTADA = AUX.PORC_OBRA_EJECUTADA
-                     , RBC_TIT.FEC_COMPRA = AUX.FEC_COMPRA
-                     , RBC_TIT.FEC_VALIDO_DE = AUX.FEC_VALIDO_DE
                      , RBC_TIT.SUBTIPO_VIVIENDA = AUX.SUBTIPO_VIVIENDA                             
                      , RBC_TIT.IND_OCUPANTES_VIVIENDA = AUX.IND_OCUPANTES_VIVIENDA
                      , RBC_TIT.PRODUCTO = AUX.PRODUCTO
@@ -107,10 +124,7 @@ BEGIN
                               , FONDO                                                                      
                               , ESTADO_POSESORIO
                               , FEC_ESTADO_POSESORIO
-                              , FEC_TITULO_FIRME   
                               , PORC_OBRA_EJECUTADA
-                              , FEC_COMPRA
-                              , FEC_VALIDO_DE
                               , SUBTIPO_VIVIENDA
                               , IND_OCUPANTES_VIVIENDA
                               , PRODUCTO
@@ -126,10 +140,7 @@ BEGIN
                               , AUX.FONDO                                                                   
                               , AUX.ESTADO_POSESORIO
                               , AUX.FEC_ESTADO_POSESORIO
-                              , AUX.FEC_TITULO_FIRME 
                               , AUX.PORC_OBRA_EJECUTADA
-                              , AUX.FEC_COMPRA
-                              , AUX.FEC_VALIDO_DE
                               , AUX.SUBTIPO_VIVIENDA
                               , AUX.IND_OCUPANTES_VIVIENDA
                               , AUX.PRODUCTO

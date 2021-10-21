8a6cefc4b77329c2279401df65c0b4d4798a63cd
Daniel Algaba <daniel.algaba@pfsgroup.es>
2021-10-21 12:49:49 +0200

    HREOS-15634 Crear SPs de ocultación y publicaciones, especiales para ETL de alta de Caixabank.

---
+++ sql/9.3/rem/procs_y_vistas/DDL_00150_REM01_SP_MOTIVO_OCULTACION_AGR_CONV_CAIXA.sql
@@ -0,0 +1,161 @@
+--/*
+--##########################################
+--## AUTOR=Daniel Algaba
+--## FECHA_CREACION=20211021
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=HREOS-15634
+--## PRODUCTO=NO
+--## Finalidad: DDL
+--##           
+--## INSTRUCCIONES: Configurar las variables necesarias en el principio del DECLARE
+--## VERSIONES:
+--##		0.1 Versión inicial
+--########################################## 
+--*/
+
+--Para permitir la visualización de texto en un bloque PL/SQL utilizando DBMS_OUTPUT.PUT_LINE
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+
+create or replace PROCEDURE SP_MOTIVO_OCULTACION_AGR_CONV_CAIXA (nAGR_ID IN NUMBER
+												    , pTIPO IN VARCHAR2 /*ALQUILER/VENTA*/
+                                                    , pOCULTAR OUT NUMBER
+                                                    , pMOTIVO OUT VARCHAR2) IS
+
+	ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+	ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquemas.
+    V_ESQUEMA_MASTER VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquemas.
+
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar
+    vWHERE VARCHAR2(4000 CHAR);
+    nORDEN NUMBER;
+    vQUERY            VARCHAR2(4000 CHAR);
+  BEGIN
+	    /*DBMS_OUTPUT.PUT_LINE('[INICIO]');*/
+
+      pOCULTAR := 0;
+      pMOTIVO  := 0;
+
+	  vQUERY := ' (SELECT 1
+				   FROM  '|| V_ESQUEMA ||'.ACT_AGA_AGRUPACION_ACTIVO AGA
+				   JOIN '|| V_ESQUEMA ||'.ACT_AGR_AGRUPACION AGR ON AGR.AGR_ID = AGA.AGR_ID AND AGR.BORRADO = 0
+				   JOIN '|| V_ESQUEMA ||'.DD_TAG_TIPO_AGRUPACION TAG ON TAG.DD_TAG_ID = AGR.DD_TAG_ID AND TAG.BORRADO = 0 AND TAG.DD_TAG_CODIGO IN (''02''/*Restringida*/ ,''17''/*Restringida alquiler*/,''18''/*Restringida OBREM*/)
+					   AND (AGR.AGR_FIN_VIGENCIA IS NULL OR TRUNC(AGR.AGR_FIN_VIGENCIA) >= TRUNC(SYSDATE))
+				  WHERE AGA.ACT_ID = ACT.ACT_ID
+					AND AGA.BORRADO = 0
+					AND AGR.AGR_ID = '||nAGR_ID||'
+			      )';
+
+      V_MSQL := '
+            SELECT DISTINCT OCULTO, DD_MTO_CODIGO
+              FROM (
+                  SELECT OCULTO, DD_MTO_CODIGO, ROW_NUMBER () OVER (PARTITION BY ACT_ID ORDER BY ORDEN ASC) ROWNUMBER
+                    FROM(
+                          SELECT ACT.ACT_ID
+                               , 1 OCULTO
+                               , MTO.DD_MTO_CODIGO
+                               , MTO.DD_MTO_ORDEN ORDEN
+                                    FROM '|| V_ESQUEMA ||'.ACT_PAC_PERIMETRO_ACTIVO ACT
+                                    LEFT JOIN '|| V_ESQUEMA ||'.DD_MTO_MOTIVOS_OCULTACION MTO ON MTO.DD_MTO_CODIGO = ''08'' AND MTO.BORRADO = 0 /*Salida Perímetro*/
+                                   WHERE ACT.BORRADO = 0
+                                     AND ACT.PAC_INCLUIDO = 0
+                                     AND EXISTS '||vQUERY||
+                         ' UNION
+                          SELECT ACT.ACT_ID
+                               , 1 OCULTO
+                               , MTO.DD_MTO_CODIGO
+                               , MTO.DD_MTO_ORDEN ORDEN
+                                    FROM '|| V_ESQUEMA ||'.ACT_PAC_PERIMETRO_ACTIVO ACT
+                                    LEFT JOIN '|| V_ESQUEMA ||'.ACT_AGA_AGRUPACION_ACTIVO AGA ON AGA.ACT_ID = ACT.ACT_ID
+                                    LEFT JOIN '|| V_ESQUEMA ||'.ACT_AGR_AGRUPACION AGR ON AGR.AGR_ID = AGA.AGR_ID
+                                    LEFT JOIN '|| V_ESQUEMA ||'.DD_TAG_TIPO_AGRUPACION TAG ON TAG.DD_TAG_ID = AGR.DD_TAG_ID
+                                    LEFT JOIN '|| V_ESQUEMA ||'.DD_MTO_MOTIVOS_OCULTACION MTO ON MTO.DD_MTO_CODIGO = ''01'' AND MTO.BORRADO = 0 /*No Publicable*/
+                                   WHERE ACT.BORRADO = 0
+										AND (ACT.PAC_CHECK_PUBLICAR = 0
+                                        	OR (TAG.DD_TAG_CODIGO = ''13'' AND AGR.AGR_FIN_VIGENCIA < SYSDATE AND AGR.AGR_FECHA_BAJA IS NULL))
+                                     AND EXISTS '||vQUERY||
+                         ' UNION
+                          SELECT ACT.ACT_ID
+                               /*, DECODE(SCM.DD_SCM_CODIGO,''01'',1,0)OCULTO*/ /*No comercializable*/
+                               , 1 OCULTO /*No comercializable*/
+                               , MTO.DD_MTO_CODIGO
+                               , MTO.DD_MTO_ORDEN ORDEN
+                                    FROM '|| V_ESQUEMA ||'.ACT_ACTIVO ACT
+                                    JOIN '||V_ESQUEMA||'.ACT_PAC_PERIMETRO_ACTIVO PAC ON PAC.ACT_ID = ACT.ACT_ID AND PAC.BORRADO = 0
+                                    LEFT JOIN '|| V_ESQUEMA ||'.DD_MTO_MOTIVOS_OCULTACION MTO ON MTO.DD_MTO_CODIGO = ''02'' AND MTO.BORRADO = 0 /*No Comercializable*/
+                                   WHERE ACT.BORRADO = 0
+									 AND PAC.PAC_CHECK_COMERCIALIZAR = 0
+                                     AND EXISTS '||vQUERY||
+                         ' UNION
+                          SELECT ACT.ACT_ID
+                               , 1 OCULTO
+                               , MTO.DD_MTO_CODIGO
+                               , MTO.DD_MTO_ORDEN ORDEN
+                                    FROM '|| V_ESQUEMA ||'.ACT_APU_ACTIVO_PUBLICACION ACT
+                                    JOIN '|| V_ESQUEMA ||'.DD_TCO_TIPO_COMERCIALIZACION DDTCO ON DDTCO.DD_TCO_ID = ACT.DD_TCO_ID
+                                          AND DDTCO.DD_TCO_CODIGO IN (''02'',''03'',''04'')
+                                          AND DDTCO.BORRADO = 0
+                                    LEFT JOIN '|| V_ESQUEMA ||'.DD_MTO_MOTIVOS_OCULTACION MTO ON MTO.DD_MTO_CODIGO = ''14'' AND MTO.BORRADO = 0 /*Precio*/
+                                   WHERE ACT.BORRADO = 0
+                                     AND ACT.APU_CHECK_PUB_SIN_PRECIO_A = 0
+                                     AND NOT EXISTS (SELECT 1
+                                                      FROM '|| V_ESQUEMA ||'.ACT_VAL_VALORACIONES VAL
+                                                      JOIN '|| V_ESQUEMA ||'.DD_TPC_TIPO_PRECIO TPC ON TPC.DD_TPC_ID = VAL.DD_TPC_ID AND TPC.BORRADO = 0 AND TPC.DD_TPC_CODIGO = ''03''/*Aprobado de renta (web)*/
+                                                     WHERE VAL.BORRADO = 0
+													   AND VAL.VAL_IMPORTE IS NOT NULL
+                                                       AND VAL.ACT_ID = ACT.ACT_ID)
+                                     AND ''A'' = '''||pTIPO||'''
+                                     AND EXISTS '||vQUERY||
+                         ' UNION
+                          SELECT ACT.ACT_ID
+                               , 1 OCULTO
+                               , MTO.DD_MTO_CODIGO
+                               , MTO.DD_MTO_ORDEN ORDEN
+                                    FROM '|| V_ESQUEMA ||'.ACT_APU_ACTIVO_PUBLICACION ACT
+                                    JOIN '|| V_ESQUEMA ||'.DD_TCO_TIPO_COMERCIALIZACION DDTCO ON DDTCO.DD_TCO_ID = ACT.DD_TCO_ID
+                                          AND DDTCO.DD_TCO_CODIGO IN (''01'',''02'')
+                                          AND DDTCO.BORRADO = 0
+                                    LEFT JOIN '|| V_ESQUEMA ||'.DD_MTO_MOTIVOS_OCULTACION MTO ON MTO.DD_MTO_CODIGO = ''14'' AND MTO.BORRADO = 0 /*Precio*/
+                                   WHERE ACT.BORRADO = 0
+                                     AND ACT.APU_CHECK_PUB_SIN_PRECIO_V = 0
+                                     AND NOT EXISTS (SELECT 1
+                                                      FROM '|| V_ESQUEMA ||'.ACT_VAL_VALORACIONES VAL
+                                                      JOIN '|| V_ESQUEMA ||'.DD_TPC_TIPO_PRECIO TPC ON TPC.DD_TPC_ID = VAL.DD_TPC_ID AND TPC.BORRADO = 0 AND TPC.DD_TPC_CODIGO = ''02''/*Aprobado de venta (web)*/
+                                                     WHERE VAL.BORRADO = 0
+													   AND VAL.VAL_IMPORTE IS NOT NULL
+                                                       AND VAL.ACT_ID = ACT.ACT_ID)
+                                     AND ''V'' = '''||pTIPO||'''
+                                     AND EXISTS '||vQUERY||
+                       ')
+                    )AUX WHERE AUX.ROWNUMBER = 1 AND rownum = 1
+                 '
+       ;
+
+      BEGIN
+        EXECUTE IMMEDIATE V_MSQL INTO pOCULTAR, pMOTIVO;
+      EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+          pOCULTAR := 0;
+          pMOTIVO  := 0;
+      END;
+
+	  /*DBMS_OUTPUT.PUT_LINE('[FIN]');*/
+
+	EXCEPTION
+	  WHEN OTHERS THEN
+	    ERR_NUM := SQLCODE;
+	    ERR_MSG := SQLERRM;
+	    DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(ERR_NUM));
+	    DBMS_OUTPUT.put_line('-----------------------------------------------------------');
+	    DBMS_OUTPUT.put_line(ERR_MSG);
+	    ROLLBACK;
+	    RAISE;
+
+	END SP_MOTIVO_OCULTACION_AGR_CONV_CAIXA;
+/
+
+EXIT;

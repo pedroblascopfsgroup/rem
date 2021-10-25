8ad9a2670e9fd69245b251f9b5209cf123a144f3
Daniel Algaba <daniel.algaba@pfsgroup.es>
2021-10-25 13:56:24 +0200

    HREOS-15969 Modelo datos

--- sql/9.3/rem/procs_y_vistas/DDL_00180_REM01_SP_RBC_TIT_04_TASACIONES.sql
+++ sql/9.3/rem/procs_y_vistas/DDL_00180_REM01_SP_RBC_TIT_04_TASACIONES.sql
@@ -1,10 +1,10 @@
 --/*
 --##########################################
 --## AUTOR=Daniel Algaba
---## FECHA_CREACION=20211022
+--## FECHA_CREACION=20211025
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.3
---## INCIDENCIA_LINK=HREOS-15894
+--## INCIDENCIA_LINK=HREOS-15969
 --## PRODUCTO=NO
 --##
 --## Finalidad: 
@@ -12,7 +12,8 @@
 --## VERSIONES:
 --##        0.1 Versión inicial
 --##        0.2 Se cambian los NIFs de titulizados - [HREOS-15634] - Daniel Algaba
---##        0.2 Se añaden los campos reción creados y los mapeos necesarios - [HREOS-15894] - Daniel Algaba
+--##        0.3 Se añaden los campos reción creados y los mapeos necesarios - [HREOS-15894] - Daniel Algaba
+--##        0.4 Se cambia la cartera por la nuevo Titulizada - [HREOS-15634] - Daniel Algaba
 --##########################################
 --*/
 WHENEVER SQLERROR EXIT SQL.SQLCODE;
@@ -177,10 +178,10 @@ BEGIN
                      LEFT JOIN '|| V_ESQUEMA ||'.DD_EQV_TIT_CAIXA_REM EQV9 ON EQV9.DD_NOMBRE_CAIXA = ''SUBTIPO_SUELO'' AND EQV9.DD_CODIGO_REM = SAC_SUELO.DD_SAC_CODIGO AND EQV9.BORRADO = 0
                      WHERE TAS.BORRADO = 0) TASACION ON RN = 1 AND TASACION.ACT_ID = ACT.ACT_ID
                      WHERE ACT.ACT_NUM_ACTIVO_CAIXA IS NOT NULL
-                     AND ACT.DD_CRA_ID = (SELECT DD_CRA_ID FROM '|| V_ESQUEMA ||'.DD_CRA_CARTERA WHERE DD_CRA_CODIGO = ''03'')
+                     --AND ACT.DD_CRA_ID = (SELECT DD_CRA_ID FROM '|| V_ESQUEMA ||'.DD_CRA_CARTERA WHERE DD_CRA_CODIGO = ''17'')
                      AND ACT.BORRADO = 0
                      AND PAC.PAC_INCLUIDO = 1
-                     AND ACT.ACT_EN_TRAMITE = 0
+                     --AND ACT.ACT_EN_TRAMITE = 0
                      AND PRO.PRO_DOCIDENTIF IN (''V84966126'',''V85164648'',''V85587434'',''V84322205'',''V84593961'',''V84669332'',''V85082675'',''V85623668'',''V84856319'',''V85500866'',''V85143659'',''V85594927'',''V85981231'',''V84889229'',''V84916956'',''V85160935'',''V85295087'',''V84175744'',''V84925569'')
                   ) AUX ON (RBC_TIT.NUM_IDENTIFICATIVO = AUX.NUM_IDENTIFICATIVO)
                   WHEN MATCHED THEN UPDATE SET

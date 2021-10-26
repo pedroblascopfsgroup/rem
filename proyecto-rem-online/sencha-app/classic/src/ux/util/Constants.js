0cb1811f4bb6b125cb71c187133bab8b9f26822e
Daniel Algaba <daniel.algaba@pfsgroup.es>
2021-10-26 09:38:25 +0200

    HREOS-15969 Se cambia el código de Cartera Titulizada por conflicto

--- proyecto-rem-online/sencha-app/classic/src/ux/util/Constants.js
+++ proyecto-rem-online/sencha-app/classic/src/ux/util/Constants.js
@@ -196,7 +196,7 @@ Ext.define('HreRem.ux.util.Constants', {
     	GALEON:'15',
     	ZEUS: '14',
     	BBVA: '16',
-    	TITULIZADA: '17'
+    	TITULIZADA: '18'
     },
 
     NOMBRE_CARTERA:	{
@@ -213,7 +213,7 @@ Ext.define('HreRem.ux.util.Constants', {
     	'15': 'GALEON',
     	'14': 'ZEUS',
     	'16': 'BBVA',
-    	'17': 'TITULIZADA'
+    	'18': 'TITULIZADA'
     },
 
     NOMBRE_CARTERA2: {

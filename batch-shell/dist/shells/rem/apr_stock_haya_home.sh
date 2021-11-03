527e51a91f2570471a7f715521633be18b0fbb35
Pedro Blasco Onsurbe <pedro.blasco@pfsgroup.es>
2021-11-03 16:58:24 +0100

    HREOS-15865 Preparación del shell de envío de stocks

--- batch-shell/dist/shells/rem/apr_stock_haya_home.sh
+++ batch-shell/dist/shells/rem/apr_stock_haya_home.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Generado por GUSTAVO MORA 20170810
- 
 
+source ./setBatchEnv.sh
 
 filename=$(basename $0)
 nameETL="${filename%.*}"
@@ -22,7 +21,25 @@ if [ -f $MAINSH ]; then
     CLASS2=`echo $CLASS | sed -e 's/$ROOT_PATH/./g'`
     CLASEINICIO="$(cat $MAINSH | grep "^ java" | cut -f11 -d" ")"
     java -Xms512M -Xmx1536M -Dconfig.dir=$DIR_CONFIG -Dconfig.file.mask=$CFG_FILE -Duser.country=ES -Duser.language=es -cp $CLASS2 $CLASEINICIO --context=Default "$@"
-    exit $?
+    SALIDA_SHELL=$?
+    if [[ "$SALIDA_SHELL" == "0" ]] ; then
+        DIR_SALIDA=$(cat ${DIR_CONFIG}${CFG_FILE} | grep 'output_dir;' | cut -d';' -f2)
+        FECHA=$(date +%Y%m%d)
+        fichero="STOCK_*_HH_${FECHA}_*.csv"
+        cd "$DIR_SALIDA"  &> /dev/null
+        if [[ `ls $fichero 2> /dev/null | wc -l` -gt 0  ]] ; then
+            echo "Subiendo ficheros $fichero al FTP..."
+lftp -u $SFTP_HH_USER,$SFTP_HH_PASS sftp://${SFTP_HH_HOST} <<EOF
+cd "$SFTP_HH_RUTA"
+mput $fichero
+bye
+EOF
+            exit 0
+        else
+            echo "##### ERROR: No existe el fichero $fichero #####"
+            exit 1
+        fi
+    fi
 else
     echo "$(basename $0) Error en $filename: no se ha encontrado  $MAINSH"
     exit 1

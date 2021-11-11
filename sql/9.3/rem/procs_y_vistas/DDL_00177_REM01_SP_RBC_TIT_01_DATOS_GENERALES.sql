b2b51c8c2c8d958dcb67da21a27fd744665ee9b8
Daniel Algaba <daniel.algaba@pfsgroup.es>
2021-11-11 10:04:44 +0100

    HREOS-16362 Se añade el cálculo de fecha estado posesorio y se parametriza el proceso y generar o no fichero

--- sql/9.3/rem/procs_y_vistas/DDL_00177_REM01_SP_RBC_TIT_01_DATOS_GENERALES.sql
+++ sql/9.3/rem/procs_y_vistas/DDL_00177_REM01_SP_RBC_TIT_01_DATOS_GENERALES.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=Daniel Algaba
---## FECHA_CREACION=20211108
+--## FECHA_CREACION=20211110
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.3
 --## INCIDENCIA_LINK=HREOS-16362
@@ -17,6 +17,7 @@
 --##        0.5 Se refactoriza la consulta para que solo mire si son de la cartera Titulizada y están en perímetro - [HREOS-15969] - Daniel Algaba
 --##        0.6 Si es diferente de vivienda en subtipo de vivienda se envía 0 (En el caso de no aplicar al tratarse de un bien inmueble no vivienda) - [HREOS-15969] - Daniel Algaba
 --##        0.7 Se añade el campo faltante FEC_ULT_REHAB y se muestra si está en venta cuando está publicado - [HREOS-16362] - Daniel Algaba
+--##        0.8 Se añade un paso previo para mirar si ha cambiado el estado posesorio, si es así se actualiza en la tabla de titulizadas y los valores saldrán de allí - [HREOS-16362] - Daniel Algaba
 --##########################################
 --*/
 WHENEVER SQLERROR EXIT SQL.SQLCODE;
@@ -48,6 +49,39 @@ BEGIN
 
       SALIDA := SALIDA || '[INFO] SE VA A PROCEDER A EXTRAER LOS DATOS GENERALES'|| CHR(10);
 
+      V_MSQL := 'MERGE INTO '|| V_ESQUEMA ||'.ACT_ACTIVO_TITULIZADA ACT_TIT
+                        USING (				                 
+                              SELECT       
+                                 ACT.ACT_ID
+                                 , CASE WHEN SPS.SPS_OCUPADO IS NULL AND TTT.DD_TPA_ID IS NULL AND EAL.DD_EAL_ID IS NULL THEN ''P01'' -- Sin posesión
+                                       WHEN SPS.SPS_OCUPADO = 1 AND TTT.DD_TPA_CODIGO = ''01'' AND EAL.DD_EAL_CODIGO = ''02'' THEN ''P02'' -- Alquilado
+                                       WHEN SPS.SPS_OCUPADO = 1 AND (TTT.DD_TPA_CODIGO = ''02'' OR TTT.DD_TPA_CODIGO = ''03'') AND NVL(EAL.DD_EAL_CODIGO,0) != ''02'' THEN ''P03'' -- Reocupado
+                                       WHEN SPS.SPS_VERTICAL = 1 THEN ''P05''  -- Vertical
+                                       WHEN SPS.SPS_OCUPADO = 0 AND TTT.DD_TPA_ID IS NULL AND NVL(EAL.DD_EAL_CODIGO,0) != ''02'' THEN ''P06'' -- Con posesión
+                                       WHEN EAL.DD_EAL_CODIGO = ''02'' THEN ''P02'' -- Alquilado
+                                       WHEN EAL.DD_EAL_CODIGO = ''01'' THEN ''P06'' -- Con posesión
+                                       WHEN SPS.SPS_FECHA_TOMA_POSESION IS NOT NULL THEN ''P06'' -- Con posesión
+                                       ELSE ''P01'' -- Sin posesión
+                                 END ESTADO_POSESORIO
+                                 FROM '|| V_ESQUEMA ||'.ACT_ACTIVO_TITULIZADA ACT_TIT
+                                 JOIN '|| V_ESQUEMA ||'.ACT_ACTIVO ACT ON ACT_TIT.ACT_ID = ACT.ACT_ID AND ACT.BORRADO = 0
+                                 LEFT JOIN '|| V_ESQUEMA ||'.ACT_SPS_SIT_POSESORIA SPS ON SPS.ACT_ID = ACT.ACT_ID AND SPS.BORRADO = 0
+                                 LEFT JOIN '|| V_ESQUEMA ||'.ACT_PTA_PATRIMONIO_ACTIVO PTA ON PTA.ACT_ID = ACT.ACT_ID AND PTA.BORRADO = 0
+                                 LEFT JOIN '|| V_ESQUEMA ||'.DD_TPA_TIPO_TITULO_ACT TTT ON TTT.DD_TPA_ID = SPS.DD_TPA_ID AND TTT.BORRADO = 0
+                                 LEFT JOIN '|| V_ESQUEMA ||'.DD_EAL_ESTADO_ALQUILER EAL ON EAL.DD_EAL_ID = PTA.DD_EAL_ID AND EAL.BORRADO = 0
+                                 WHERE ACT.BORRADO = 0
+                              ) AUX ON (ACT_TIT.ACT_ID = AUX.ACT_ID)
+                              WHEN MATCHED THEN UPDATE SET
+                                 ACT_TIT.FEC_EST_POSESORIO_ACTUAL = SYSDATE
+                                 , ACT_TIT.DD_ETP_ID = (SELECT DD_ETP_ID FROM '|| V_ESQUEMA ||'.DD_ETP_ESTADO_POSESORIO WHERE DD_ETP_CODIGO = AUX.ESTADO_POSESORIO)
+                                 , ACT_TIT.USUARIOMODIFICAR = ''STOCK_TITULIZADAS''
+                                 , ACT_TIT.FECHAMODIFICAR = SYSDATE
+                                 WHERE ACT_TIT.DD_ETP_ID <> (SELECT DD_ETP_ID FROM '|| V_ESQUEMA ||'.DD_ETP_ESTADO_POSESORIO WHERE DD_ETP_CODIGO = AUX.ESTADO_POSESORIO)';
+
+   EXECUTE IMMEDIATE V_MSQL;
+   
+   SALIDA := SALIDA || '   [INFO] ACTUALIZADOS '|| SQL%ROWCOUNT|| CHR(10);
+
       SALIDA := SALIDA || '   [INFO] 1 - EXTRACCIÓN DE CAMPOS DE DATOS GENERALES'||CHR(10);
 
        V_MSQL := ' MERGE INTO '|| V_ESQUEMA ||'.AUX_APR_RBC_TIT_STOCK RBC_TIT
@@ -56,17 +90,8 @@ BEGIN
                      ACT.ACT_NUM_ACTIVO_CAIXA as NUM_IDENTIFICATIVO 
                      , EQV1.DD_CODIGO_CAIXA SOCIEDAD_PATRIMONIAL
                      , EQV2.DD_CODIGO_CAIXA FONDO
-                     , CASE WHEN SPS.SPS_OCUPADO IS NULL AND TTT.DD_TPA_ID IS NULL AND EAL.DD_EAL_ID IS NULL THEN ''P01'' -- Sin posesión
-                           WHEN SPS.SPS_OCUPADO = 1 AND TTT.DD_TPA_CODIGO = ''01'' AND EAL.DD_EAL_CODIGO = ''02'' THEN ''P02'' -- Alquilado
-                           WHEN SPS.SPS_OCUPADO = 1 AND (TTT.DD_TPA_CODIGO = ''02'' OR TTT.DD_TPA_CODIGO = ''03'') AND NVL(EAL.DD_EAL_CODIGO,0) != ''02'' THEN ''P03'' -- Reocupado
-                           WHEN SPS.SPS_VERTICAL = 1 THEN ''P05''  -- Vertical
-                           WHEN SPS.SPS_OCUPADO = 0 AND TTT.DD_TPA_ID IS NULL AND NVL(EAL.DD_EAL_CODIGO,0) != ''02'' THEN ''P06'' -- Con posesión
-                           WHEN EAL.DD_EAL_CODIGO = ''02'' THEN ''P02'' -- Alquilado
-                           WHEN EAL.DD_EAL_CODIGO = ''01'' THEN ''P06'' -- Con posesión
-                           WHEN SPS.SPS_FECHA_TOMA_POSESION IS NOT NULL THEN ''P06'' -- Con posesión
-                           ELSE ''P01'' -- Sin posesión
-                     END ESTADO_POSESORIO
-                     , NULL /*SPS.SPS_FECHA_REVISION_ESTADO*/ FEC_ESTADO_POSESORIO   
+                     , ETP.DD_ETP_CODIGO ESTADO_POSESORIO
+                     , ACT_TIT.FEC_EST_POSESORIO_ACTUAL FEC_ESTADO_POSESORIO   
                      , ACT.ACT_PORCENTAJE_CONSTRUCCION PORC_OBRA_EJECUTADA
                      , NULL FEC_ULT_REHAB
                      , CASE WHEN EQV3.DD_CODIGO_CAIXA != ''0001'' THEN ''0''
@@ -89,6 +114,7 @@ BEGIN
                      JOIN '|| V_ESQUEMA ||'.ACT_PAC_PERIMETRO_ACTIVO PAC ON PAC.ACT_ID = ACT.ACT_ID AND PAC.BORRADO = 0
                      JOIN '|| V_ESQUEMA ||'.ACT_PAC_PROPIETARIO_ACTIVO ACT_PRO ON ACT_PRO.ACT_ID = ACT.ACT_ID AND ACT_PRO.BORRADO = 0
                      JOIN '|| V_ESQUEMA ||'.ACT_PRO_PROPIETARIO PRO ON PRO.PRO_ID = ACT_PRO.PRO_ID AND PRO.BORRADO = 0
+                     JOIN '|| V_ESQUEMA ||'.ACT_ACTIVO_TITULIZADA ACT_TIT ON ACT_TIT.ACT_ID = ACT.ACT_ID AND ACT_TIT.BORRADO = 0
                      LEFT JOIN '|| V_ESQUEMA ||'.ACT_SPS_SIT_POSESORIA SPS ON SPS.ACT_ID = ACT.ACT_ID AND SPS.BORRADO = 0
                      LEFT JOIN '|| V_ESQUEMA ||'.ACT_PTA_PATRIMONIO_ACTIVO PTA ON PTA.ACT_ID = ACT.ACT_ID AND PTA.BORRADO = 0
                      LEFT JOIN '|| V_ESQUEMA ||'.DD_TPA_TIPO_TITULO_ACT TTT ON TTT.DD_TPA_ID = SPS.DD_TPA_ID AND TTT.BORRADO = 0
@@ -106,7 +132,8 @@ BEGIN
                      LEFT JOIN '|| V_ESQUEMA ||'.DD_SAC_SUBTIPO_ACTIVO SAC_SUELO ON SAC_SUELO.DD_SAC_ID = ACT.DD_SAC_ID AND SAC_SUELO.BORRADO = 0
                      LEFT JOIN '|| V_ESQUEMA ||'.DD_EQV_TIT_CAIXA_REM EQV4 ON EQV4.DD_NOMBRE_CAIXA = ''SUBTIPO_SUELO'' AND EQV4.DD_CODIGO_REM = SAC_SUELO.DD_SAC_CODIGO AND EQV4.BORRADO = 0
                      LEFT JOIN '|| V_ESQUEMA ||'.DD_SAC_SUBTIPO_ACTIVO SAC_VIVIENDA ON SAC_VIVIENDA.DD_SAC_ID = ACT.DD_SAC_ID AND SAC_VIVIENDA.BORRADO = 0
-                     LEFT JOIN '|| V_ESQUEMA ||'.DD_EQV_TIT_CAIXA_REM EQV5 ON EQV5.DD_NOMBRE_CAIXA = ''SUBTIPO_VIVIENDA'' AND EQV5.DD_CODIGO_REM = SAC_SUELO.DD_SAC_CODIGO AND EQV5.BORRADO = 0   
+                     LEFT JOIN '|| V_ESQUEMA ||'.DD_EQV_TIT_CAIXA_REM EQV5 ON EQV5.DD_NOMBRE_CAIXA = ''SUBTIPO_VIVIENDA'' AND EQV5.DD_CODIGO_REM = SAC_SUELO.DD_SAC_CODIGO AND EQV5.BORRADO = 0  
+                     LEFT JOIN '|| V_ESQUEMA ||'.DD_ETP_ESTADO_POSESORIO ETP ON ACT_TIT.DD_ETP_ID = ETP.DD_ETP_ID AND ETP.BORRADO = 0
                      WHERE ACT.BORRADO = 0
                      AND PAC.PAC_INCLUIDO = 1
                      AND ACT.ACT_NUM_ACTIVO_CAIXA IS NOT NULL

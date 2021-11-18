cc32d5a704432b7329a19a0c710176d6073718fd
Ivan Rubio <ivan.rubio@pfsgroup.es>
2021-11-18 16:09:44 +0100

    HREOS-15868 script update api duplicado

---
+++ sql/9.3/rem/DML_06004_REM01_UPDATE_DD_FIO_FUN_INTERLOCUTOR_OFERTA_HREOS-15868.sql
@@ -0,0 +1,102 @@
+--/*
+--##########################################
+--## AUTOR=Ivan Rubio
+--## FECHA_CREACION=20211118
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.2
+--## INCIDENCIA_LINK=HREOS-15868
+--## PRODUCTO=NO
+--##
+--## Finalidad: Script que añade los datos del array en DD_FIO_FUN_INTERLOCUTOR_OFERTA
+--## INSTRUCCIONES:
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF;
+
+DECLARE
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar     
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.   
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+	
+    V_ID NUMBER(16);
+    V_TABLA VARCHAR2(50 CHAR):= 'DD_FIO_FUN_INTERLOCUTOR_OFERTA';
+    V_CHARS VARCHAR2(3 CHAR):= 'FIO';
+    V_USUARIO VARCHAR2(25 CHAR):= 'HREOS-15868';
+    TYPE T_TIPO_DATA IS TABLE OF VARCHAR2(150);
+    TYPE T_ARRAY_DATA IS TABLE OF T_TIPO_DATA;
+    V_TIPO_DATA T_ARRAY_DATA := T_ARRAY_DATA(
+            -- CODIGO  			                DESCRIPCION                               DESCRIPCION_LARGA   
+		T_TIPO_DATA('Z56', 'Z57', 'API Custodio (rem3)')
+    ); 
+    V_TMP_TIPO_DATA T_TIPO_DATA;
+   
+BEGIN	
+	
+    -- LOOP para insertar los valores -----------------------------------------------------------------
+
+    FOR I IN V_TIPO_DATA.FIRST .. V_TIPO_DATA.LAST
+      LOOP
+
+      V_TMP_TIPO_DATA := V_TIPO_DATA(I);
+    	
+      --Comprobamos el dato a insertar
+      V_SQL := 'SELECT COUNT(DD_'||V_CHARS||'_CODIGO) FROM '||V_ESQUEMA||'.'||V_TABLA||' WHERE DD_'||V_CHARS||'_CODIGO = '''||V_TMP_TIPO_DATA(1)||'''';
+      EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
+      
+      --Si existe mas de 1 se modifica el primero
+      IF V_NUM_TABLAS > 1 THEN	
+
+        DBMS_OUTPUT.PUT_LINE('[INFO]: MODIFICAMOS EL REGISTRO DUPLICADO '''|| TRIM(V_TMP_TIPO_DATA(1)) ||'''');
+        V_MSQL := '
+          UPDATE '|| V_ESQUEMA ||'.'||V_TABLA||' 
+          SET 
+            DD_'||V_CHARS||'_CODIGO = '''||TRIM(V_TMP_TIPO_DATA(2))||''',
+            DD_'||V_CHARS||'_CODIGO_C4C = '''||TRIM(V_TMP_TIPO_DATA(2))||''',
+            DD_'||V_CHARS||'_DESCRIPCION = '''||TRIM(V_TMP_TIPO_DATA(3))||''',
+            DD_'||V_CHARS||'_DESCRIPCION_LARGA = '''||TRIM(V_TMP_TIPO_DATA(3))||''',
+		    USUARIOMODIFICAR = '''||V_USUARIO||''',
+            FECHAMODIFICAR = SYSDATE
+			    WHERE DD_'||V_CHARS||'_ID IN (SELECT DD_'||V_CHARS||'_ID FROM '|| V_ESQUEMA ||'.'||V_TABLA||'  
+				WHERE DD_'||V_CHARS||'_CODIGO = '''||TRIM(V_TMP_TIPO_DATA(1))||'''  FETCH FIRST 1 ROWS ONLY) ';
+				
+        EXECUTE IMMEDIATE V_MSQL;
+        DBMS_OUTPUT.PUT_LINE('[INFO]: REGISTRO MODIFICADO CORRECTAMENTE');
+        
+      -- Si no existe, nada
+      ELSE
+          
+        DBMS_OUTPUT.PUT_LINE('[INFO]: REGISTRO NO DUPLICADO');
+      
+      END IF;
+
+    END LOOP;
+  COMMIT;
+   
+
+EXCEPTION
+     WHEN OTHERS THEN
+          err_num := SQLCODE;
+          err_msg := SQLERRM;
+
+          DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(err_num));
+          DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+          DBMS_OUTPUT.put_line(err_msg);
+
+          ROLLBACK;
+          RAISE;          
+
+END;
+
+/
+
+EXIT

4dbf2cb9c2b304f35967306b97d97dd86fc7919e
Julian Dolz <julian.dolz@pfsgroup.es>
2021-12-17 09:59:52 +0100

    HREOS-16464 subida fecha script

--- sql/9.3/rem/procs_y_vistas/DDL_019_REM01_VI_BUSQUEDA_DATOS_COMPRADOR_EXP.sql
+++ sql/9.3/rem/procs_y_vistas/DDL_019_REM01_VI_BUSQUEDA_DATOS_COMPRADOR_EXP.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=Ivan Rubio
---## FECHA_CREACION=20211123
+--## FECHA_CREACION=2021217
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.3
 --## INCIDENCIA_LINK=16464

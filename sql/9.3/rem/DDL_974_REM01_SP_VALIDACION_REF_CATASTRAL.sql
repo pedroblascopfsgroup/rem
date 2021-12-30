cfe698702c985856cd8213b4fead8941cf089e77
Santiago Monzo <santiago.monzo@pfsgroup.es>
2021-12-30 14:00:57 +0100

    HREOS-16737

---
+++ sql/9.3/rem/DDL_974_REM01_SP_VALIDACION_REF_CATASTRAL.sql
@@ -0,0 +1,204 @@
+--/*
+--##########################################
+--## AUTOR=Santi Monzó
+--## FECHA_CREACION=20211228
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=HREOS-16737
+--## PRODUCTO=NO
+--##
+--## Finalidad: 
+--## INSTRUCCIONES:
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
+SET DEFINE OFF;						
+
+CREATE OR REPLACE PROCEDURE SP_VALIDACION_REF_CATASTRAL IS
+	
+    
+   V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar
+   V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema
+   V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master
+   ERR_NUM NUMBER(25); -- Vble. auxiliar para registrar errores en el script.
+   ERR_MSG VARCHAR2(10024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+   V_AUX NUMBER(10); -- Variable auxiliar
+   letraDc VARCHAR2(2000) := 'MQWERTYUIOPASDFGHJKLBZX';
+    dcCalculado VARCHAR2(2000) := '';
+    type pesoPosicion IS VARRAY(11) OF INTEGER;
+    myArray pesoPosicion;
+    var_cat_id number;
+    var VARCHAR2(1000);
+    var2 VARCHAR2(1000);
+    valorCaracter number := NULL;
+    sumaDigitos number := 0;
+    var_LONGREF number;
+    CURSOR c_product
+  IS
+     SELECT AUX.CAT_ID,AUX.CAT_CATASTRO,AUX.CAD1,AUX.CAD2,AUX.CAD3,AUX.LONGREF
+        FROM(
+            SELECT ACT_CAT.CAT_ID,
+            ACT_CAT.CAT_REF_CATASTRAL,
+            ACT_CAT.CAT_CATASTRO,
+            
+
+            CASE
+                WHEN ACT_CAT.CAT_CATASTRO IS NOT NULL
+                THEN LENGTH(CAT.CAT_REF_CATASTRAL)
+                WHEN ACT_CAT.CAT_CATASTRO IS NULL
+                THEN LENGTH(ACT_CAT.CAT_REF_CATASTRAL)
+            END AS LONGREF,
+
+
+            CASE
+                WHEN ACT_CAT.CAT_CATASTRO IS NOT NULL
+                THEN CONCAT(SUBSTR(CAT.CAT_REF_CATASTRAL, 1, 7),SUBSTR(CAT.CAT_REF_CATASTRAL, 15, 4))
+                WHEN ACT_CAT.CAT_CATASTRO IS NULL
+                THEN CONCAT(SUBSTR(ACT_CAT.CAT_REF_CATASTRAL, 1, 7),SUBSTR(ACT_CAT.CAT_REF_CATASTRAL, 15, 4))
+            END AS CAD1,
+
+            CASE
+                WHEN ACT_CAT.CAT_CATASTRO IS NOT NULL
+                THEN CONCAT(SUBSTR(CAT.CAT_REF_CATASTRAL, 8, 7),SUBSTR(CAT.CAT_REF_CATASTRAL, 15, 4))
+                WHEN ACT_CAT.CAT_CATASTRO IS NULL
+                THEN CONCAT(SUBSTR(ACT_CAT.CAT_REF_CATASTRAL, 8, 7),SUBSTR(ACT_CAT.CAT_REF_CATASTRAL, 15, 4))
+            END AS CAD2,
+
+            CASE
+                WHEN ACT_CAT.CAT_CATASTRO IS NOT NULL
+                THEN SUBSTR(CAT.CAT_REF_CATASTRAL, 19, 2)
+                WHEN ACT_CAT.CAT_CATASTRO IS NULL
+                THEN SUBSTR(ACT_CAT.CAT_REF_CATASTRAL, 19, 2)
+            END AS CAD3
+
+            FROM REM01.ACT_CAT_CATASTRO ACT_CAT
+            LEFT JOIN REM01.CAT_CATASTRO CAT ON CAT.CAT_ID = ACT_CAT.CAT_CATASTRO
+            WHERE ACT_CAT.BORRADO=0
+                                   
+            ) AUX           
+            JOIN REM01.AUX_CAT_CATASTRO AUX_CAT ON AUX_CAT.CAT_REF_CATASTRAL = AUX.CAT_REF_CATASTRAL;
+
+
+BEGIN
+
+myArray := pesoPosicion(13,15,12,5,4,17,9,21,3,7,1);
+FOR r_product IN c_product
+
+
+LOOP
+sumaDigitos := 0;
+var2 := r_product.CAD3;
+var_cat_id := r_product.CAT_ID;
+var_LONGREF := r_product.LONGREF;
+dcCalculado := '';
+    IF (var_LONGREF != 20) THEN
+        dbms_output.put_line('La longitud de la REF distinta de 20 ');
+        V_MSQL := 'UPDATE '||V_ESQUEMA||'.ACT_CAT_CATASTRO
+                        SET CAT_CORRECTO = 0
+                        WHERE CAT_ID = '||var_cat_id;
+                       EXECUTE IMMEDIATE V_MSQL;
+        ELSE              
+    
+        FOR i IN 1..LENGTH(r_product.CAD1) LOOP
+            var := substr(r_product.CAD1,i,1);
+            valorCaracter  := NULL;
+            IF REGEXP_LIKE( var, '[0-9]') THEN
+            valorCaracter  := var;
+            
+            ELSIF REGEXP_LIKE( var, '[A-N]') THEN
+            valorCaracter  := ASCII(var)-64;
+            
+            ELSIF REGEXP_LIKE( var, '[Ñ]') THEN
+            valorCaracter  := 15;
+            
+            ELSIF REGEXP_LIKE( var, '[N-Z]') THEN
+            valorCaracter  := ASCII(var)-63;
+            
+    
+            end if;
+            
+            IF ( (valorCaracter IS NOT NULL)  AND i < 12) THEN
+                sumaDigitos:=MOD((sumaDigitos+(valorCaracter*myArray(i))),23);
+            end if;
+            
+        END LOOP;
+        dcCalculado := CONCAT(dcCalculado,(substr(letraDc,sumaDigitos,1)));
+        FOR i IN 1..LENGTH(r_product.CAD2) LOOP
+            var := substr(r_product.CAD2,i,1);
+            valorCaracter  := NULL;
+            IF REGEXP_LIKE( var, '[0-9]') THEN
+            valorCaracter  := var;
+            
+            ELSIF REGEXP_LIKE( var, '[A-N]') THEN
+            valorCaracter  := ASCII(var)-64;
+            
+            ELSIF REGEXP_LIKE( var, '[Ñ]') THEN
+            valorCaracter  := 15;
+            
+            ELSIF REGEXP_LIKE( var, '[N-Z]') THEN
+            valorCaracter  := ASCII(var)-63;
+            
+            end if;
+            
+            IF ( (valorCaracter IS NOT NULL)  AND i < 12) THEN
+                sumaDigitos:=MOD((sumaDigitos+(valorCaracter*myArray(i))),23);
+    
+            end if;
+            
+        END LOOP;
+     
+    dcCalculado := CONCAT(dcCalculado,(substr(letraDc,sumaDigitos,1)));
+    end if;
+
+    IF ( dcCalculado =  var2 ) THEN
+            dbms_output.put_line('CORRECTO ');
+            dbms_output.put_line('dcCalculado '||dcCalculado||' ');
+            dbms_output.put_line('SUBSTR '||var2||' ');
+            V_MSQL := 'UPDATE '||V_ESQUEMA||'.ACT_CAT_CATASTRO
+                SET CAT_CORRECTO = 1
+                WHERE CAT_ID = '||var_cat_id||' 
+                AND CAT_CATASTRO IS NOT NULL';
+                EXECUTE IMMEDIATE V_MSQL;
+                
+             V_MSQL := 'UPDATE '||V_ESQUEMA||'.ACT_CAT_CATASTRO
+                SET CAT_CORRECTO = 1,
+                CAT_CATASTRO = var_cat_id
+                WHERE CAT_ID = var_cat_id 
+                AND CAT_CATASTRO IS NULL
+                AND var_cat_id IN (SELECT CAT.CAT_ID FROM '||V_ESQUEMA||'.CAT_CATASTRO CAT WHERE CAT.CAT_ID = '||var_cat_id||')';
+                EXECUTE IMMEDIATE V_MSQL; 
+                
+                
+        
+         ELSE
+          dbms_output.put_line('DISTINTO ');
+            dbms_output.put_line('dcCalculado '||dcCalculado||' ');
+            dbms_output.put_line('SUBSTR '||var2||' ');
+           V_MSQL := ' UPDATE '||V_ESQUEMA||'.ACT_CAT_CATASTRO
+                SET CAT_CORRECTO = 0
+                WHERE CAT_ID = '||var_cat_id;
+                EXECUTE IMMEDIATE V_MSQL;
+                
+           
+        end if;
+
+
+END LOOP;
+
+
+
+
+EXCEPTION
+   WHEN OTHERS THEN
+      ERR_NUM := SQLCODE;
+      ERR_MSG := SQLERRM;
+      
+      ROLLBACK;
+      RAISE;
+END SP_VALIDACION_REF_CATASTRAL;
+/
+EXIT;
+
---
+++ sql/9.3/rem/DDL_975_REM01_AUX_CAT_CATASTRO.sql
@@ -0,0 +1,100 @@
+--/*
+--#########################################
+--## AUTOR=DAP
+--## FECHA_CREACION=20211224
+--## ARTEFACTO=batch
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=HREOS-16737
+--## PRODUCTO=NO
+--## 
+--## Finalidad: Creación de tabla auxiliar para AUX_CAT_CATASTRO
+--##			
+--## INSTRUCCIONES:  
+--## VERSIONES:
+--##        0.1 Versión inicial
+--#########################################
+----*/
+
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF;
+
+
+DECLARE
+
+	V_MSQL VARCHAR2(32000 CHAR); 
+	V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; 
+	V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#';
+	V_SQL VARCHAR2(4000 CHAR);
+	V_NUM NUMBER(16);
+	ERR_NUM NUMBER(25);  
+	ERR_MSG VARCHAR2(1024 CHAR); 
+	V_TABLA VARCHAR2(50 CHAR):= 'AUX_CAT_CATASTRO';
+
+
+BEGIN	
+
+	DBMS_OUTPUT.PUT_LINE('[INFO] INICIO DEL PROCESO.');
+	
+	V_MSQL := 'SELECT COUNT(1) FROM ALL_TABLES WHERE TABLE_NAME = '''||V_TABLA||''' AND OWNER = '''||V_ESQUEMA||'''';
+	EXECUTE IMMEDIATE V_MSQL INTO V_NUM;
+	
+	IF V_NUM != 0 THEN
+	
+		DBMS_OUTPUT.PUT_LINE('[INFO] LA TABLA '''||V_TABLA||''' YA EXISTE.');
+		V_MSQL := 'DROP TABLE '||V_ESQUEMA||'.'||V_TABLA||'';
+		EXECUTE IMMEDIATE V_MSQL;
+		
+	END IF;	
+
+	EXECUTE IMMEDIATE '
+	CREATE TABLE '||V_ESQUEMA||'.'||V_TABLA||' (
+		 CAT_ID	                        VARCHAR2(50 CHAR)
+        ,CAT_REF_CATASTRAL	            VARCHAR2(50 CHAR)
+        ,CAT_SUPERFICIE_PARCELA	        VARCHAR2(50 CHAR)
+        ,CAT_SUPERFICIE_CONSTRUIDA	    VARCHAR2(50 CHAR)
+        ,CAT_SUPERFICIE_ZONAS_COMUNES	VARCHAR2(50 CHAR)
+        ,CAT_ANYO_CONSTRUCCION	        VARCHAR2(50 CHAR)
+        ,CAT_COD_POSTAL	                VARCHAR2(50 CHAR)
+        ,DD_TVI_ID	                    VARCHAR2(50 CHAR)
+        ,CAT_DESCRIPCION_VIA	        VARCHAR2(50 CHAR)
+        ,CAT_NUM_VIA	                VARCHAR2(50 CHAR)
+        ,CAT_PISO	                    VARCHAR2(50 CHAR)
+        ,CAT_PLANTA	                    VARCHAR2(50 CHAR)
+        ,CAT_PUERTA	                    VARCHAR2(50 CHAR)
+        ,CAT_ESCALERA	                VARCHAR2(50 CHAR)
+        ,CAT_CLASE	                    VARCHAR2(50 CHAR)
+        ,CAT_USO_PRINCIPAL	            VARCHAR2(50 CHAR)
+        ,CAT_DIV_HORIZONTAL             VARCHAR2(50 CHAR)
+        ,DD_PRV_ID	                    VARCHAR2(50 CHAR)
+        ,DD_LOC_ID	                    VARCHAR2(50 CHAR)
+        ,CAT_LATITUD	                VARCHAR2(50 CHAR)
+        ,CAT_LONGITUD	                VARCHAR2(50 CHAR)
+        ,CAT_GEODISTANCIA	            VARCHAR2(50 CHAR)
+	)';
+
+	DBMS_OUTPUT.PUT_LINE('[INFO] LA TABLA '''||V_TABLA||''' HA SIDO CREADA CON EXITO.');
+  COMMIT;
+ 
+  
+  DBMS_OUTPUT.PUT_LINE('[INFO] PROCESO FINALIZADO.');
+
+         
+EXCEPTION
+
+   WHEN OTHERS THEN
+   
+        err_num := SQLCODE;
+        err_msg := SQLERRM;
+
+        DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(err_num));
+        DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+        DBMS_OUTPUT.put_line(err_msg);
+
+        ROLLBACK;
+        RAISE;          
+
+END;
+/
+EXIT

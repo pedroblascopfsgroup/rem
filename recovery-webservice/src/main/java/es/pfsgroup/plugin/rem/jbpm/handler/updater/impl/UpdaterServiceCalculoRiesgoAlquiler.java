c66f983a6b014caee4fe87620839fa4243dd4b77
Lara <lara.pablo@pfsgroup.es>
2022-01-31 12:01:51 +0100

    HREOS-17058 func-rem-hydraModificacionPBC cambio de estado

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/jbpm/handler/updater/impl/UpdaterServiceCalculoRiesgoAlquiler.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/jbpm/handler/updater/impl/UpdaterServiceCalculoRiesgoAlquiler.java
@@ -60,12 +60,12 @@ public class UpdaterServiceCalculoRiesgoAlquiler implements UpdaterService {
 				String estadoCodigo = null;
 				String estadoBcCodigo = null;
 				estadoCodigo = DDEstadosExpedienteComercial.PTE_PBC;
-				if(comboResultado.equals(DDRiesgoOperacion.CODIGO_ROP_NO_APLICA)) {
+				
+				if(DDRiesgoOperacion.CODIGO_ROP_NO_APLICA.equals(comboResultado)) {
 					estadoBcCodigo = DDEstadoExpedienteBc.CODIGO_OFERTA_APROBADA;
-				}else if(comboResultado.equals(DDRiesgoOperacion.CODIGO_ROP_ALTO)){
+				}else if(DDRiesgoOperacion.CODIGO_ROP_ALTO.equals(comboResultado)){
 					estadoBcCodigo = DDEstadoExpedienteBc.CODIGO_INGRESO_FINAL_PDTE_BC;
-				} else if(comboResultado.equals(DDRiesgoOperacion.CODIGO_ROP_MEDIO) 
-						||comboResultado.equals(DDRiesgoOperacion.CODIGO_ROP_BAJO)) {
+				} else if(DDRiesgoOperacion.CODIGO_ROP_MEDIO.equals(comboResultado) ||DDRiesgoOperacion.CODIGO_ROP_BAJO.equals(comboResultado)) {
 					estadoBcCodigo = DDEstadoExpedienteBc.CODIGO_INGRESO_FINAL_DOCUMENTACION_APORTADA_A_BC;
 				}
 				

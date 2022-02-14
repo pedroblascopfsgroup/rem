b539ff7f6e9602405fcd9a542fcec8abcf2d0367
Santiago Monzo <santiago.monzo@pfsgroup.es>
2022-02-14 12:02:27 +0100

    HREOS-17119

---
+++ sql/9.3/rem/minimigraciones/HREOS-17119/DML/DML_025_REM01_SP_ASC_ACT_SIT_COM_VACIOS_V2_.sql
@@ -0,0 +1,94 @@
+--/*
+--#########################################
+--## AUTOR=Santi Monzó
+--## FECHA_CREACION=20220214
+--## ARTEFACTO=batch
+--## VERSION_ARTEFACTO=9.3
+--## ARTEFACTO=batch
+--## INCIDENCIA_LINK=HREOS-15592
+--## PRODUCTO=NO
+--## 
+--## Finalidad:
+--##			
+--## INSTRUCCIONES:  
+--## VERSIONES:
+--##        0.1 Versión inicial
+--#########################################
+--*/
+--Para permitir la visualización de texto en un bloque PL/SQL utilizando DBMS_OUTPUT.PUT_LINE
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
+SET DEFINE OFF;
+
+
+DECLARE
+
+	V_ESQUEMA VARCHAR2(10 CHAR) := 'REM01';
+	V_ESQUEMA_MASTER VARCHAR2(15 CHAR) := 'REMMASTER';
+	V_USUARIO VARCHAR2(50 CHAR) := 'HREOS-16329';
+	V_SQL VARCHAR2(2500 CHAR) := '';
+	V_NUM NUMBER(25);
+	V_SENTENCIA VARCHAR2(4000 CHAR);
+	TABLE_COUNT NUMBER(10,0) := 0;
+	
+	V_TABLA_ACT VARCHAR2 (30 CHAR) := 'ACT_ACTIVO';
+	V_TABLA_AUX VARCHAR2 (30 CHAR) := 'AUX_ACT_TRASPASO_ACTIVO';
+	V_TABLA VARCHAR2(40 CHAR) := 'ACT_AOB_ACTIVO_OBS';
+  V_TABLA_AUX_MIG VARCHAR2 (30 CHAR) := 'AUX_SEGUIR_MIGRACION';
+
+
+BEGIN
+  
+  DBMS_OUTPUT.PUT_LINE('[INFO] COMIENZA EL PROCESO DE MIGRACION SOBRE LA TABLA '||V_ESQUEMA||'.'||V_TABLA||'.');
+ 
+     V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.'||V_TABLA_AUX_MIG||' WHERE SEGUIR_MIGRACION = 0';
+    EXECUTE IMMEDIATE V_SQL INTO V_NUM;
+    
+    IF V_NUM > 0  THEN
+        
+        DBMS_OUTPUT.PUT_LINE('[INFO] YA HAY ACTIVOS INSERTADOS EN ACT_ACTIVO DE LOS QUE SE QUIEREN INSERTAR');
+        
+    ELSE 
+ 
+  EXECUTE IMMEDIATE ('
+        UPDATE '||V_ESQUEMA||'.ACT_ACTIVO
+            SET DD_SCM_ID = NULL
+            WHERE ACT_ID IN (
+
+            SELECT ACT.ACT_ID
+            FROM '||V_ESQUEMA||'.AUX_ACT_TRASPASO_ACTIVO AUX
+            JOIN '||V_ESQUEMA||'.ACT_ACTIVO ACT ON ACT.ACT_NUM_ACTIVO = AUX.ACT_NUM_ACTIVO_NUV)
+            '
+  )
+  ;
+
+  DBMS_OUTPUT.PUT_LINE('[INFO] - '||to_char(sysdate,'HH24:MI:SS')||'  '||V_ESQUEMA||'.'||V_TABLA||' actualizada. '||SQL%ROWCOUNT||' Filas.');
+  
+  
+    REM01.SP_ASC_ACT_SIT_COM_VACIOS_V2 (0);
+
+
+
+
+
+  END IF;
+  
+  COMMIT;
+  
+  V_SENTENCIA := 'BEGIN '||V_ESQUEMA||'.OPERACION_DDL.DDL_TABLE(''ANALYZE'','''||V_TABLA||''',''10''); END;';
+  EXECUTE IMMEDIATE V_SENTENCIA;
+  DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.'||V_TABLA||' ANALIZADA.');
+  
+  EXCEPTION
+
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecucion:'||TO_CHAR(SQLCODE));
+        DBMS_OUTPUT.put_line('-----------------------------------------------------------');
+        DBMS_OUTPUT.put_line(SQLERRM);
+        ROLLBACK;
+        RAISE;
+END;
+/
+
+EXIT;

06ca44ba6d8433b4bca368b48be523befd5ad686
Javier Esbri <javier.esbri@pfsgroup.es>
2022-02-25 13:13:39 +0100

    Cambio fecha vistas materializadas para relanzamiento en entorno

--- sql/9.3/rem/procs_y_vistas/DDL_00171_REM01_VI_AUX_APR_RBC_STOCK_M.sql
+++ sql/9.3/rem/procs_y_vistas/DDL_00171_REM01_VI_AUX_APR_RBC_STOCK_M.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=Daniel Algaba
---## FECHA_CREACION=20210811
+--## FECHA_CREACION=20220225
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.2
 --## INCIDENCIA_LINK=HREOS-14838

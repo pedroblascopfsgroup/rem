700aecf04e89e80e6943398fed7eaef80bf3ff6c
Lara <lara.pablo@pfsgroup.es>
2022-02-28 14:21:37 +0100

    HREOS-17304 func-rem-catastro cambiar superficie construida

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/activo/catastro/CatastroManager.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/activo/catastro/CatastroManager.java
@@ -25,7 +25,6 @@ import es.pfsgroup.commons.utils.Checks;
 import es.pfsgroup.commons.utils.dao.abm.GenericABMDao;
 import es.pfsgroup.commons.utils.dao.abm.GenericABMDao.Filter;
 import es.pfsgroup.commons.utils.dao.abm.GenericABMDao.FilterType;
-import es.pfsgroup.plugin.recovery.nuevoModeloBienes.model.NMBBien;
 import es.pfsgroup.plugin.recovery.nuevoModeloBienes.model.NMBLocalizacionesBien;
 import es.pfsgroup.plugin.rem.activo.dao.ActivoDao;
 import es.pfsgroup.plugin.rem.api.ActivoApi;
@@ -148,9 +147,18 @@ public class CatastroManager implements CatastroApi {
 				}
 			}
 			
-			NMBBien bien = activo.getBien();
-			if(bien != null && bien.getBienEntidad() != null && bien.getBienEntidad().getSuperficieConstruida() != null) {
-				dto.setSuperficieConstruida((double) bien.getBienEntidad().getSuperficieConstruida());	
+			ActivoInfoRegistral air = activo.getInfoRegistral();
+			
+			if(air != null) {
+				if(air.getSuperficieElementosComunes() != null) {
+					dto.setSuperficieReperComun((double) air.getSuperficieElementosComunes()); 
+				}
+				if(air.getSuperficieParcela() != null) {
+					dto.setSuperficieReperComun((double) air.getSuperficieParcela()); 
+				}
+				if(air.getInfoRegistralBien() != null && air.getInfoRegistralBien().getSuperficieConstruida() != null) {
+					dto.setSuperficieConstruida(air.getInfoRegistralBien().getSuperficieConstruida().doubleValue());	
+				}
 			}
 			
 		}

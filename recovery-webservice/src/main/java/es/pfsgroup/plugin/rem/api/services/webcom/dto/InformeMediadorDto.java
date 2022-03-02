bcff77f7342f58dc6b69ec23f164e97b996ed75c
Carles Molins <carles.molins@pfsgroup.es>
2022-03-02 17:02:58 +0100

    Añadir campos dto webcom

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/api/services/webcom/dto/InformeMediadorDto.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/api/services/webcom/dto/InformeMediadorDto.java
@@ -1,14 +1,10 @@
 package es.pfsgroup.plugin.rem.api.services.webcom.dto;
 
-import java.util.List;
-
 import es.pfsgroup.plugin.rem.api.services.webcom.dto.datatype.BooleanDataType;
 import es.pfsgroup.plugin.rem.api.services.webcom.dto.datatype.DateDataType;
 import es.pfsgroup.plugin.rem.api.services.webcom.dto.datatype.DoubleDataType;
 import es.pfsgroup.plugin.rem.api.services.webcom.dto.datatype.LongDataType;
 import es.pfsgroup.plugin.rem.api.services.webcom.dto.datatype.StringDataType;
-import es.pfsgroup.plugin.rem.api.services.webcom.dto.datatype.annotations.MappedColumn;
-import es.pfsgroup.plugin.rem.api.services.webcom.dto.datatype.annotations.NestedDto;
 import es.pfsgroup.plugin.rem.api.services.webcom.dto.datatype.annotations.WebcomRequired;
 
 public class InformeMediadorDto implements WebcomRESTDto {
@@ -48,12 +44,8 @@ public class InformeMediadorDto implements WebcomRESTDto {
 	private StringDataType nombreGestorPublicaciones;
 	@WebcomRequired
 	private StringDataType emailGestorPublicaciones;
-	//@WebcomRequired
-	//private BooleanDataType publicado; //falta en vista
 	@WebcomRequired
 	private DateDataType envioLlavesAApi;
-	//@WebcomRequired
-	//private DateDataType fechaPosesion; //falta en vista
 	@WebcomRequired
 	private StringDataType codEstadoOcupacional;
 	@WebcomRequired
@@ -99,11 +91,15 @@ public class InformeMediadorDto implements WebcomRESTDto {
 	@WebcomRequired
 	private BooleanDataType ascensor;
 	@WebcomRequired
+	private DoubleDataType utilSuperficie;
+	@WebcomRequired
 	private BooleanDataType rehabilitado;
 	@WebcomRequired
 	private LongDataType anyoRehabilitacion;
 	@WebcomRequired
-	private StringDataType licenciaApertura;
+	private BooleanDataType licenciaApertura;
+	@WebcomRequired
+	private BooleanDataType licenciaObra;
 	@WebcomRequired
 	private DateDataType fechaRecepcionInforme;
 	@WebcomRequired
@@ -115,6 +111,127 @@ public class InformeMediadorDto implements WebcomRESTDto {
 	//@WebcomRequired
 	//private StringDataType primerEnvioInformeCompletadoPor; //falta en vista
 	
+	private BooleanDataType esVisitable;
+	
+	private DateDataType fechaUltimaVisita;
+	
+	private StringDataType descripcionComercial;
+	
+	private StringDataType codEstadoConservacion;
+	
+	private BooleanDataType anejoGaraje;
+	
+	private BooleanDataType anejoTrastero;
+	
+	private StringDataType cocinaRating;
+	
+	private BooleanDataType cocinaAmueblada;
+	
+	private StringDataType codCalefaccion;
+	
+	private StringDataType codTipoCalefaccion;
+	
+	private StringDataType codAireAcondicionado;
+	
+	private BooleanDataType existenArmariosEmpotrados;
+	
+	private DoubleDataType superficieTerraza;
+	
+	private DoubleDataType superficiePatio;
+	
+	private StringDataType exteriorInterior;
+	
+	private BooleanDataType existenZonasVerdes;
+	
+	private BooleanDataType existeConserjeVigilancia;
+	
+	private StringDataType jardin;
+	
+	private StringDataType piscina;
+	
+	private BooleanDataType existenInstalacionesDeportivas;
+	
+	private StringDataType gimnasio;
+	
+	private BooleanDataType accesoMinusvalidosOtrasCaracteristicas;
+	
+	private StringDataType codEstadoConservacionEdificio;
+	
+	private LongDataType numeroPlantasEdificio;
+	
+	private StringDataType codTipoPuertaAcceso;
+	
+	private StringDataType codEstadoPuertasInteriores;
+	
+	private StringDataType codEstadoVentanas;
+	
+	private StringDataType codEstadoPersianas;
+	
+	private StringDataType codEstadoPintura;
+	
+	private StringDataType codEstadoSolados;
+	
+	private StringDataType codAdmiteMascota;
+	
+	private StringDataType codEstadoBanyos;
+	
+	private StringDataType codValoracionUbicacion;
+	
+	private StringDataType codUbicacion;
+	
+	private BooleanDataType salidaHumosOtrasCaracteristicas;
+	
+	private BooleanDataType aptoUsoEnBruto;
+	
+	private StringDataType accesibilidad;
+	
+	private DoubleDataType edificabilidadSuperficieTecho;
+	
+	private DoubleDataType parcelaSuperficie;
+	
+	private DoubleDataType porcentajeUrbanizacionEjecutado;
+	
+	private StringDataType clasificacion;
+	
+	private StringDataType codUso;
+	
+	private DoubleDataType metrosLinealesFachadaPrincipal;
+	
+	private BooleanDataType almacen;
+	
+	private DoubleDataType almacenSuperficie;
+	
+	private BooleanDataType superficieVentaExposicion;
+	
+	private DoubleDataType superficieVentaExposicionConstruido;
+	
+	private BooleanDataType entreplanta;
+	
+	private DoubleDataType altura;
+	
+	private DoubleDataType porcentajeEdificacionEjecutada;
+	
+	private BooleanDataType ocupado;
+	
+	private DateDataType visitableFechaVisita;
+	
+	private DoubleDataType valorEstimadoMinVenta;
+	
+	private DoubleDataType valorEstimadoMaxVenta;
+	
+	private DoubleDataType valorEstimadoVenta;
+	
+	private DoubleDataType valorEstimadoMaxRenta;
+	
+	private DoubleDataType valorEstimadoMinRenta;
+	
+	private DoubleDataType valorEstimadoRenta;
+	
+	private LongDataType numeroSalones;
+	
+	private LongDataType numeroEstancias;
+	
+	private LongDataType numeroPlantas;
 	
 	public LongDataType getIdUsuarioRemAccion() {
 		return idUsuarioRemAccion;
@@ -386,11 +503,17 @@ public class InformeMediadorDto implements WebcomRESTDto {
 	public void setAnyoRehabilitacion(LongDataType anyoRehabilitacion) {
 		this.anyoRehabilitacion = anyoRehabilitacion;
 	}
-	public StringDataType getLicenciaApertura() {
+	public BooleanDataType getLicenciaApertura() {
 		return licenciaApertura;
 	}
-	public void setLicenciaApertura(StringDataType licenciaApertura) {
+	public void setLicenciaApertura(BooleanDataType licenciaApertura) {
 		this.licenciaApertura = licenciaApertura;
+	}
+	public BooleanDataType getLicenciaObra() {
+		return licenciaObra;
+	}
+	public void setLicenciaObra(BooleanDataType licenciaObra) {
+		this.licenciaObra = licenciaObra;
 	}
 	public DateDataType getFechaRecepcionInforme() {
 		return fechaRecepcionInforme;
@@ -421,5 +544,377 @@ public class InformeMediadorDto implements WebcomRESTDto {
 	}
 	public void setPrimerEnvioInformeCompletadoPor(StringDataType primerEnvioInformeCompletadoPor) {
 		this.primerEnvioInformeCompletadoPor = primerEnvioInformeCompletadoPor;
-	}*/	
+	}*/
+	public DoubleDataType getUtilSuperficie() {
+		return utilSuperficie;
+	}
+	public void setUtilSuperficie(DoubleDataType utilSuperficie) {
+		this.utilSuperficie = utilSuperficie;
+	}
+	public BooleanDataType getEsVisitable() {
+		return esVisitable;
+	}
+	public void setEsVisitable(BooleanDataType esVisitable) {
+		this.esVisitable = esVisitable;
+	}
+	public DateDataType getFechaUltimaVisita() {
+		return fechaUltimaVisita;
+	}
+	public void setFechaUltimaVisita(DateDataType fechaUltimaVisita) {
+		this.fechaUltimaVisita = fechaUltimaVisita;
+	}
+	public StringDataType getDescripcionComercial() {
+		return descripcionComercial;
+	}
+	public void setDescripcionComercial(StringDataType descripcionComercial) {
+		this.descripcionComercial = descripcionComercial;
+	}
+	public StringDataType getCodEstadoConservacion() {
+		return codEstadoConservacion;
+	}
+	public void setCodEstadoConservacion(StringDataType codEstadoConservacion) {
+		this.codEstadoConservacion = codEstadoConservacion;
+	}
+	public BooleanDataType getAnejoGaraje() {
+		return anejoGaraje;
+	}
+	public void setAnejoGaraje(BooleanDataType anejoGaraje) {
+		this.anejoGaraje = anejoGaraje;
+	}
+	public BooleanDataType getAnejoTrastero() {
+		return anejoTrastero;
+	}
+	public void setAnejoTrastero(BooleanDataType anejoTrastero) {
+		this.anejoTrastero = anejoTrastero;
+	}
+	public StringDataType getCocinaRating() {
+		return cocinaRating;
+	}
+	public void setCocinaRating(StringDataType cocinaRating) {
+		this.cocinaRating = cocinaRating;
+	}
+	public BooleanDataType getCocinaAmueblada() {
+		return cocinaAmueblada;
+	}
+	public void setCocinaAmueblada(BooleanDataType cocinaAmueblada) {
+		this.cocinaAmueblada = cocinaAmueblada;
+	}
+	public StringDataType getCodCalefaccion() {
+		return codCalefaccion;
+	}
+	public void setCodCalefaccion(StringDataType codCalefaccion) {
+		this.codCalefaccion = codCalefaccion;
+	}
+	public StringDataType getCodTipoCalefaccion() {
+		return codTipoCalefaccion;
+	}
+	public void setCodTipoCalefaccion(StringDataType codTipoCalefaccion) {
+		this.codTipoCalefaccion = codTipoCalefaccion;
+	}
+	public StringDataType getCodAireAcondicionado() {
+		return codAireAcondicionado;
+	}
+	public void setCodAireAcondicionado(StringDataType codAireAcondicionado) {
+		this.codAireAcondicionado = codAireAcondicionado;
+	}
+	public BooleanDataType getExistenArmariosEmpotrados() {
+		return existenArmariosEmpotrados;
+	}
+	public void setExistenArmariosEmpotrados(BooleanDataType existenArmariosEmpotrados) {
+		this.existenArmariosEmpotrados = existenArmariosEmpotrados;
+	}
+	public DoubleDataType getSuperficieTerraza() {
+		return superficieTerraza;
+	}
+	public void setSuperficieTerraza(DoubleDataType superficieTerraza) {
+		this.superficieTerraza = superficieTerraza;
+	}
+	public DoubleDataType getSuperficiePatio() {
+		return superficiePatio;
+	}
+	public void setSuperficiePatio(DoubleDataType superficiePatio) {
+		this.superficiePatio = superficiePatio;
+	}
+	public StringDataType getExteriorInterior() {
+		return exteriorInterior;
+	}
+	public void setExteriorInterior(StringDataType exteriorInterior) {
+		this.exteriorInterior = exteriorInterior;
+	}
+	public BooleanDataType getExistenZonasVerdes() {
+		return existenZonasVerdes;
+	}
+	public void setExistenZonasVerdes(BooleanDataType existenZonasVerdes) {
+		this.existenZonasVerdes = existenZonasVerdes;
+	}
+	public BooleanDataType getExisteConserjeVigilancia() {
+		return existeConserjeVigilancia;
+	}
+	public void setExisteConserjeVigilancia(BooleanDataType existeConserjeVigilancia) {
+		this.existeConserjeVigilancia = existeConserjeVigilancia;
+	}
+	public StringDataType getJardin() {
+		return jardin;
+	}
+	public void setJardin(StringDataType jardin) {
+		this.jardin = jardin;
+	}
+	public StringDataType getPiscina() {
+		return piscina;
+	}
+	public void setPiscina(StringDataType piscina) {
+		this.piscina = piscina;
+	}
+	public BooleanDataType getExistenInstalacionesDeportivas() {
+		return existenInstalacionesDeportivas;
+	}
+	public void setExistenInstalacionesDeportivas(BooleanDataType existenInstalacionesDeportivas) {
+		this.existenInstalacionesDeportivas = existenInstalacionesDeportivas;
+	}
+	public StringDataType getGimnasio() {
+		return gimnasio;
+	}
+	public void setGimnasio(StringDataType gimnasio) {
+		this.gimnasio = gimnasio;
+	}
+	public BooleanDataType getAccesoMinusvalidosOtrasCaracteristicas() {
+		return accesoMinusvalidosOtrasCaracteristicas;
+	}
+	public void setAccesoMinusvalidosOtrasCaracteristicas(BooleanDataType accesoMinusvalidosOtrasCaracteristicas) {
+		this.accesoMinusvalidosOtrasCaracteristicas = accesoMinusvalidosOtrasCaracteristicas;
+	}
+	public StringDataType getCodEstadoConservacionEdificio() {
+		return codEstadoConservacionEdificio;
+	}
+	public void setCodEstadoConservacionEdificio(StringDataType codEstadoConservacionEdificio) {
+		this.codEstadoConservacionEdificio = codEstadoConservacionEdificio;
+	}
+	public LongDataType getNumeroPlantasEdificio() {
+		return numeroPlantasEdificio;
+	}
+	public void setNumeroPlantasEdificio(LongDataType numeroPlantasEdificio) {
+		this.numeroPlantasEdificio = numeroPlantasEdificio;
+	}
+	public StringDataType getCodTipoPuertaAcceso() {
+		return codTipoPuertaAcceso;
+	}
+	public void setCodTipoPuertaAcceso(StringDataType codTipoPuertaAcceso) {
+		this.codTipoPuertaAcceso = codTipoPuertaAcceso;
+	}
+	public StringDataType getCodEstadoPuertasInteriores() {
+		return codEstadoPuertasInteriores;
+	}
+	public void setCodEstadoPuertasInteriores(StringDataType codEstadoPuertasInteriores) {
+		this.codEstadoPuertasInteriores = codEstadoPuertasInteriores;
+	}
+	public StringDataType getCodEstadoVentanas() {
+		return codEstadoVentanas;
+	}
+	public void setCodEstadoVentanas(StringDataType codEstadoVentanas) {
+		this.codEstadoVentanas = codEstadoVentanas;
+	}
+	public StringDataType getCodEstadoPersianas() {
+		return codEstadoPersianas;
+	}
+	public void setCodEstadoPersianas(StringDataType codEstadoPersianas) {
+		this.codEstadoPersianas = codEstadoPersianas;
+	}
+	public StringDataType getCodEstadoPintura() {
+		return codEstadoPintura;
+	}
+	public void setCodEstadoPintura(StringDataType codEstadoPintura) {
+		this.codEstadoPintura = codEstadoPintura;
+	}
+	public StringDataType getCodEstadoSolados() {
+		return codEstadoSolados;
+	}
+	public void setCodEstadoSolados(StringDataType codEstadoSolados) {
+		this.codEstadoSolados = codEstadoSolados;
+	}
+	public StringDataType getCodAdmiteMascota() {
+		return codAdmiteMascota;
+	}
+	public void setCodAdmiteMascota(StringDataType codAdmiteMascota) {
+		this.codAdmiteMascota = codAdmiteMascota;
+	}
+	public StringDataType getCodEstadoBanyos() {
+		return codEstadoBanyos;
+	}
+	public void setCodEstadoBanyos(StringDataType codEstadoBanyos) {
+		this.codEstadoBanyos = codEstadoBanyos;
+	}
+	public StringDataType getCodValoracionUbicacion() {
+		return codValoracionUbicacion;
+	}
+	public void setCodValoracionUbicacion(StringDataType codValoracionUbicacion) {
+		this.codValoracionUbicacion = codValoracionUbicacion;
+	}
+	public StringDataType getCodUbicacion() {
+		return codUbicacion;
+	}
+	public void setCodUbicacion(StringDataType codUbicacion) {
+		this.codUbicacion = codUbicacion;
+	}
+	public BooleanDataType getSalidaHumosOtrasCaracteristicas() {
+		return salidaHumosOtrasCaracteristicas;
+	}
+	public void setSalidaHumosOtrasCaracteristicas(BooleanDataType salidaHumosOtrasCaracteristicas) {
+		this.salidaHumosOtrasCaracteristicas = salidaHumosOtrasCaracteristicas;
+	}
+	public BooleanDataType getAptoUsoEnBruto() {
+		return aptoUsoEnBruto;
+	}
+	public void setAptoUsoEnBruto(BooleanDataType aptoUsoEnBruto) {
+		this.aptoUsoEnBruto = aptoUsoEnBruto;
+	}
+	public StringDataType getAccesibilidad() {
+		return accesibilidad;
+	}
+	public void setAccesibilidad(StringDataType accesibilidad) {
+		this.accesibilidad = accesibilidad;
+	}
+	public DoubleDataType getEdificabilidadSuperficieTecho() {
+		return edificabilidadSuperficieTecho;
+	}
+	public void setEdificabilidadSuperficieTecho(DoubleDataType edificabilidadSuperficieTecho) {
+		this.edificabilidadSuperficieTecho = edificabilidadSuperficieTecho;
+	}
+	public DoubleDataType getParcelaSuperficie() {
+		return parcelaSuperficie;
+	}
+	public void setParcelaSuperficie(DoubleDataType parcelaSuperficie) {
+		this.parcelaSuperficie = parcelaSuperficie;
+	}
+	public DoubleDataType getPorcentajeUrbanizacionEjecutado() {
+		return porcentajeUrbanizacionEjecutado;
+	}
+	public void setPorcentajeUrbanizacionEjecutado(DoubleDataType porcentajeUrbanizacionEjecutado) {
+		this.porcentajeUrbanizacionEjecutado = porcentajeUrbanizacionEjecutado;
+	}
+	public StringDataType getClasificacion() {
+		return clasificacion;
+	}
+	public void setClasificacion(StringDataType clasificacion) {
+		this.clasificacion = clasificacion;
+	}
+	public StringDataType getCodUso() {
+		return codUso;
+	}
+	public void setCodUso(StringDataType codUso) {
+		this.codUso = codUso;
+	}
+	public DoubleDataType getMetrosLinealesFachadaPrincipal() {
+		return metrosLinealesFachadaPrincipal;
+	}
+	public void setMetrosLinealesFachadaPrincipal(DoubleDataType metrosLinealesFachadaPrincipal) {
+		this.metrosLinealesFachadaPrincipal = metrosLinealesFachadaPrincipal;
+	}
+	public BooleanDataType getAlmacen() {
+		return almacen;
+	}
+	public void setAlmacen(BooleanDataType almacen) {
+		this.almacen = almacen;
+	}
+	public DoubleDataType getAlmacenSuperficie() {
+		return almacenSuperficie;
+	}
+	public void setAlmacenSuperficie(DoubleDataType almacenSuperficie) {
+		this.almacenSuperficie = almacenSuperficie;
+	}
+	public BooleanDataType getSuperficieVentaExposicion() {
+		return superficieVentaExposicion;
+	}
+	public void setSuperficieVentaExposicion(BooleanDataType superficieVentaExposicion) {
+		this.superficieVentaExposicion = superficieVentaExposicion;
+	}
+	public DoubleDataType getSuperficieVentaExposicionConstruido() {
+		return superficieVentaExposicionConstruido;
+	}
+	public void setSuperficieVentaExposicionConstruido(DoubleDataType superficieVentaExposicionConstruido) {
+		this.superficieVentaExposicionConstruido = superficieVentaExposicionConstruido;
+	}
+	public BooleanDataType getEntreplanta() {
+		return entreplanta;
+	}
+	public void setEntreplanta(BooleanDataType entreplanta) {
+		this.entreplanta = entreplanta;
+	}
+	public DoubleDataType getAltura() {
+		return altura;
+	}
+	public void setAltura(DoubleDataType altura) {
+		this.altura = altura;
+	}
+	public DoubleDataType getPorcentajeEdificacionEjecutada() {
+		return porcentajeEdificacionEjecutada;
+	}
+	public void setPorcentajeEdificacionEjecutada(DoubleDataType porcentajeEdificacionEjecutada) {
+		this.porcentajeEdificacionEjecutada = porcentajeEdificacionEjecutada;
+	}
+	public BooleanDataType getOcupado() {
+		return ocupado;
+	}
+	public void setOcupado(BooleanDataType ocupado) {
+		this.ocupado = ocupado;
+	}
+	public DateDataType getVisitableFechaVisita() {
+		return visitableFechaVisita;
+	}
+	public void setVisitableFechaVisita(DateDataType visitableFechaVisita) {
+		this.visitableFechaVisita = visitableFechaVisita;
+	}
+	public DoubleDataType getValorEstimadoMinVenta() {
+		return valorEstimadoMinVenta;
+	}
+	public void setValorEstimadoMinVenta(DoubleDataType valorEstimadoMinVenta) {
+		this.valorEstimadoMinVenta = valorEstimadoMinVenta;
+	}
+	public DoubleDataType getValorEstimadoMaxVenta() {
+		return valorEstimadoMaxVenta;
+	}
+	public void setValorEstimadoMaxVenta(DoubleDataType valorEstimadoMaxVenta) {
+		this.valorEstimadoMaxVenta = valorEstimadoMaxVenta;
+	}
+	public DoubleDataType getValorEstimadoVenta() {
+		return valorEstimadoVenta;
+	}
+	public void setValorEstimadoVenta(DoubleDataType valorEstimadoVenta) {
+		this.valorEstimadoVenta = valorEstimadoVenta;
+	}
+	public DoubleDataType getValorEstimadoMaxRenta() {
+		return valorEstimadoMaxRenta;
+	}
+	public void setValorEstimadoMaxRenta(DoubleDataType valorEstimadoMaxRenta) {
+		this.valorEstimadoMaxRenta = valorEstimadoMaxRenta;
+	}
+	public DoubleDataType getValorEstimadoMinRenta() {
+		return valorEstimadoMinRenta;
+	}
+	public void setValorEstimadoMinRenta(DoubleDataType valorEstimadoMinRenta) {
+		this.valorEstimadoMinRenta = valorEstimadoMinRenta;
+	}
+	public DoubleDataType getValorEstimadoRenta() {
+		return valorEstimadoRenta;
+	}
+	public void setValorEstimadoRenta(DoubleDataType valorEstimadoRenta) {
+		this.valorEstimadoRenta = valorEstimadoRenta;
+	}
+	public LongDataType getNumeroSalones() {
+		return numeroSalones;
+	}
+	public void setNumeroSalones(LongDataType numeroSalones) {
+		this.numeroSalones = numeroSalones;
+	}
+	public LongDataType getNumeroEstancias() {
+		return numeroEstancias;
+	}
+	public void setNumeroEstancias(LongDataType numeroEstancias) {
+		this.numeroEstancias = numeroEstancias;
+	}
+	public LongDataType getNumeroPlantas() {
+		return numeroPlantas;
+	}
+	public void setNumeroPlantas(LongDataType numeroPlantas) {
+		this.numeroPlantas = numeroPlantas;
+	}	
 }

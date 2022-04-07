ef72e28e15bbf31b80267007a6dd5d3dd29ddeff
Javier Esbri <javier.esbri@pfsgroup.es>
2022-04-07 14:28:11 +0200

    HREOS-17625  PTE [FRONT] ITHYDRA-180 Custodio - Depósito - Espacio enlazado - #24

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/deposito/DepositoManager.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/deposito/DepositoManager.java
@@ -1,5 +1,7 @@
 package es.pfsgroup.plugin.rem.deposito;
 
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,13 +9,17 @@ import org.springframework.stereotype.Service;
 
 import es.pfsgroup.commons.utils.bo.BusinessOperationOverrider;
 import es.pfsgroup.commons.utils.dao.abm.GenericABMDao;
+import es.pfsgroup.commons.utils.dao.abm.GenericABMDao.Filter;
 import es.pfsgroup.commons.utils.dao.abm.GenericABMDao.FilterType;
 import es.pfsgroup.framework.paradise.utils.BeanUtilNotNull;
+import es.pfsgroup.plugin.rem.api.ActivoApi;
 import es.pfsgroup.plugin.rem.api.DepositoApi;
 import es.pfsgroup.plugin.rem.model.ConfiguracionDeposito;
 import es.pfsgroup.plugin.rem.model.Deposito;
 import es.pfsgroup.plugin.rem.model.Oferta;
+import es.pfsgroup.plugin.rem.model.ParametrizacionDeposito;
 import es.pfsgroup.plugin.rem.model.dd.DDEstadoDeposito;
+import es.pfsgroup.plugin.rem.model.dd.DDTipoPrecio;
 
 @Service("depositoManager")
 public class DepositoManager extends BusinessOperationOverrider<DepositoApi> implements DepositoApi {
@@ -23,6 +29,9 @@ public class DepositoManager extends BusinessOperationOverrider<DepositoApi> imp
 	
 	@Autowired
 	private GenericABMDao genericDao;
+	
+	@Autowired
+	private ActivoApi activoApi;
 	
 	@Override
 	public String managerName() {
@@ -74,8 +83,18 @@ public class DepositoManager extends BusinessOperationOverrider<DepositoApi> imp
 	
 	@Override
 	public Double getImporteDeposito(Oferta oferta) {
-		Double importeDeposito = 1.1;
-		
+		Double importeDeposito = null;
+		Double precioVentaActivo = activoApi.getImporteValoracionActivoByCodigo(oferta.getActivoPrincipal(), DDTipoPrecio.CODIGO_TPC_APROBADO_VENTA);
+		Filter filterSubcartera = genericDao.createFilter(FilterType.EQUALS, "subcartera", oferta.getActivoPrincipal().getSubcartera());
+		Filter filterEquipoGestion = genericDao.createFilter(FilterType.EQUALS, "equipoGestion", oferta.getActivoPrincipal().getEquipoGestion().getCodigo());
+		List<ParametrizacionDeposito> parametrizacionDeposito = genericDao.getList(ParametrizacionDeposito.class, filterSubcartera, filterEquipoGestion);
+		if (parametrizacionDeposito != null) {
+			for (ParametrizacionDeposito paramDeposito: parametrizacionDeposito) {
+				if (paramDeposito.getPrecioVenta() >= precioVentaActivo) {
+					importeDeposito = paramDeposito.getImporteDeposito();
+				}
+			}
+		}
 		return importeDeposito;
 	}
 	
---
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/model/ParametrizacionDeposito.java
@@ -0,0 +1,123 @@
+package es.pfsgroup.plugin.rem.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.Where;
+
+import es.capgemini.pfs.auditoria.Auditable;
+import es.capgemini.pfs.auditoria.model.Auditoria;
+import es.pfsgroup.plugin.rem.model.dd.DDEquipoGestion;
+import es.pfsgroup.plugin.rem.model.dd.DDSubcartera;
+
+/**
+ * Modelo que gestiona la tabla de parametrizacion deposito
+ * 
+ * 
+ * @author Javier Esbri
+ */
+@Entity
+@Table(name = "PDE_PARAMETRIZACION_DEPOSITO", schema = "${entity.schema}")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Where(clause = Auditoria.UNDELETED_RESTICTION)
+public class ParametrizacionDeposito implements Serializable, Auditable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "PDE_ID")
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "ParametrizacionDepositoGenerator")
+	@SequenceGenerator(name = "ParametrizacionDepositoGenerator", sequenceName = "S_PDE_PARAMETRIZACION_DEPOSITO")
+	private Long id;
+	
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "DD_EQG_ID")
+    private DDEquipoGestion equipoGestion;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "DD_SCR_ID")
+	private DDSubcartera subcartera;
+	
+	@Column(name = "PDE_IMPORTE")
+	private Double importeDeposito;
+
+	@Column(name = "PDE_PRECIO")
+	private Double precioVenta;
+	
+	@Version
+	private Long version;
+
+	@Embedded
+	private Auditoria auditoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public DDEquipoGestion getEquipoGestion() {
+		return equipoGestion;
+	}
+
+	public void setEquipoGestion(DDEquipoGestion equipoGestion) {
+		this.equipoGestion = equipoGestion;
+	}
+
+	public DDSubcartera getSubcartera() {
+		return subcartera;
+	}
+
+	public void setSubcartera(DDSubcartera subcartera) {
+		this.subcartera = subcartera;
+	}
+
+	public Double getImporteDeposito() {
+		return importeDeposito;
+	}
+
+	public void setImporteDeposito(Double importeDeposito) {
+		this.importeDeposito = importeDeposito;
+	}
+
+	public Double getPrecioVenta() {
+		return precioVenta;
+	}
+
+	public void setPrecioVenta(Double precioVenta) {
+		this.precioVenta = precioVenta;
+	}
+
+	public Long getVersion() {
+		return version;
+	}
+
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+
+	public Auditoria getAuditoria() {
+		return auditoria;
+	}
+
+	public void setAuditoria(Auditoria auditoria) {
+		this.auditoria = auditoria;
+	}
+	
+}

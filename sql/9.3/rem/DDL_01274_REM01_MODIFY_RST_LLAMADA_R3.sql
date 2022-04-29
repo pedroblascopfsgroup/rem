f547701238cb2b2dfebab00c52eeec4e8a63c957
Alejandro Valverde <alejandro.valverde@pfsgroup.es>
2022-04-29 11:53:09 +0200

    HREOS-17740 - Modify varchar endpolint RST_LLAMADA_R3

---
+++ sql/9.3/rem/DDL_01274_REM01_MODIFY_RST_LLAMADA_R3.sql
@@ -0,0 +1,76 @@
+--/*
+--#########################################
+--## AUTOR=Alejandro Valverde
+--## FECHA_CREACION=20220429
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=HREOS-17740
+--## PRODUCTO=NO
+--## 
+--## Finalidad: Modificación tabla RST_LLAMADA_R3
+--##			
+--## INSTRUCCIONES:  
+--## VERSIONES:
+--##        0.1 Versión inicial
+--#########################################
+----*/
+
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF;
+
+
+DECLARE
+
+	V_MSQL VARCHAR2(32000 CHAR); 
+	V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; 
+	V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#';
+	V_SQL VARCHAR2(4000 CHAR);
+	V_NUM NUMBER(16);
+	ERR_NUM NUMBER(25);  
+	ERR_MSG VARCHAR2(1024 CHAR); 
+	V_TABLA VARCHAR2(50 CHAR):= 'RST_LLAMADA_R3';
+
+
+BEGIN	
+
+	DBMS_OUTPUT.PUT_LINE('[INFO] INICIO DEL PROCESO.');
+	
+	V_MSQL := 'SELECT COUNT(1) FROM ALL_TABLES WHERE TABLE_NAME = '''||V_TABLA||''' AND OWNER = '''||V_ESQUEMA||'''';
+	EXECUTE IMMEDIATE V_MSQL INTO V_NUM;
+	
+	IF V_NUM != 0 THEN
+	
+			V_MSQL := 'ALTER TABLE '||V_ESQUEMA||'.'||V_TABLA|| ' MODIFY (RST_LLAMADA_ENDPOINT VARCHAR2(250 CHAR) )';
+
+			EXECUTE IMMEDIATE V_MSQL;
+	
+
+		DBMS_OUTPUT.PUT_LINE('[INFO] LA TABLA '''||V_TABLA||''' MODIFICADA.');
+		
+	END IF;	
+
+  COMMIT;
+ 
+  
+  DBMS_OUTPUT.PUT_LINE('[INFO] PROCESO FINALIZADO.');
+
+         
+EXCEPTION
+
+   WHEN OTHERS THEN
+   
+        err_num := SQLCODE;
+        err_msg := SQLERRM;
+
+        DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(err_num));
+        DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+        DBMS_OUTPUT.put_line(err_msg);
+
+        ROLLBACK;
+        RAISE;          
+
+END;
+/
+EXIT

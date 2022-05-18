df4156e1cd694a2177201743f741fc60be7ed734
Ivan Repiso <ivan.repiso@pfsgroup.es>
2022-05-18 20:22:16 +0200

    HREOS-17904 Custodio - Depósito - Incidencia UAT #2

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/oferta/OfertaManager.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/oferta/OfertaManager.java
@@ -783,7 +783,10 @@ public class OfertaManager extends BusinessOperationOverrider<OfertaApi> impleme
 		}
 		if (!Checks.esNulo(ofertaDto.getCodEstadoOferta())) {
 			DDEstadoOferta estadoOferta = genericDao.get(DDEstadoOferta.class, genericDao.createFilter(FilterType.EQUALS, "codigo", ofertaDto.getCodEstadoOferta()));
-			if (Checks.esNulo(estadoOferta)) errorsList.put("codEstadoOferta", RestApi.REST_MSG_UNKNOWN_KEY);
+			if (Checks.esNulo(estadoOferta) || (DDEstadoOferta.CODIGO_PDTE_DEPOSITO.equals(ofertaDto.getCodEstadoOferta()) 
+					&& (!Checks.esNulo(oferta) ? DDTipoOferta.isTipoVenta(oferta.getTipoOferta()) : DDTipoOferta.CODIGO_VENTA.equals(ofertaDto.getCodTipoOferta())))) {
+				errorsList.put("codEstadoOferta", RestApi.REST_MSG_UNKNOWN_KEY);
+			} 
 		}
 		if (!Checks.esNulo(ofertaDto.getIdVisitaRem())) {
 			Visita visita = genericDao.get(Visita.class,

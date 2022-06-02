2a3fcf7916e7ba5324ce7586079294d49b7e38a5
Daniel Algaba <daniel.algaba@pfsgroup.es>
2022-06-02 10:21:30 +0200

    HREOS-16973 Cambios en ETL send_mail_registros_duplicados para recibir los nuevos campos. También se retrocede la versión del apr_main_actives_from_file

--- sql/9.3/rem/DDL_01267_REM01_ALTER_ADD_COLUMN_APR_AUX_ACT_ACTIVO.sql
+++ sql/9.3/rem/DDL_01267_REM01_ALTER_ADD_COLUMN_APR_AUX_ACT_ACTIVO.sql
@@ -1,12 +1,12 @@
 --/*
 --##########################################
---## AUTOR=PIER GOTTA
---## FECHA_CREACION=20220120
+--## AUTOR=Daniel Algaba
+--## FECHA_CREACION=20220527
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.3
---## INCIDENCIA_LINK=REMVIP-10039
+--## INCIDENCIA_LINK=HREOS-16973
 --## PRODUCTO=NO
---## Finalidad: Añadir columnas a la APR_AUX_ACT_ACTIVO
+--## Finalidad: Añadir columnas a la AUX_ACT_DCA_DTS_CNT_ALQ
 --##           
 --## INSTRUCCIONES: Configurar las variables necesarias en el principio del DECLARE
 --## VERSIONES:
@@ -35,27 +35,25 @@ DECLARE
 
  
     V_TEXT1 VARCHAR2(2400 CHAR); -- Vble. auxiliar 
-    V_TEXT_TABLA VARCHAR2(2400 CHAR) := 'APR_AUX_ACT_ACTIVO'; -- Vble. auxiliar para almacenar el nombre de la tabla de ref.
+    V_TEXT_TABLA VARCHAR2(2400 CHAR) := 'AUX_ACT_DCA_DTS_CNT_ALQ'; -- Vble. auxiliar para almacenar el nombre de la tabla de ref.
     
     /* -- ARRAY CON NUEVAS COLUMNAS */
     TYPE T_ALTER IS TABLE OF VARCHAR2(4000);
     TYPE T_ARRAY_ALTER IS TABLE OF T_ALTER;
     V_ALTER T_ARRAY_ALTER := T_ARRAY_ALTER(
     			-- NOMBRE CAMPO			    TIPO CAMPO		
-    	T_ALTER(  'TIPO_CONTRATO_BANKIA',   'VARCHAR2(20 CHAR)'),
-        T_ALTER(  'ESTADO_ACTIVO',   'VARCHAR2(20 CHAR)'),
+    	T_ALTER(  'TIPO_CONTRATO_BANKIA',   'VARCHAR2(100 CHAR)'),
         T_ALTER(  'TRAMITE_ALQ_SOCIAL',   'NUMBER(1,0)'),
-        T_ALTER(  'CESION_USO',   'VARCHAR2(20 CHAR)'),
+        T_ALTER(  'CESION_USO',   'NUMBER(1,0)'),
         T_ALTER(  'RENUNCIA_TANTEO',   'NUMBER(1,0)'),
         T_ALTER(  'PRECIO_COMPRA',   'NUMBER(16,2)'),
-        T_ALTER(  'ALTA_PRIMA_PRECIO',   'NUMBER(1,0)'),
-        T_ALTER(  'TIPO_ALTA_ARRENDAMIENTO',   'VARCHAR2(20 CHAR)'),
+        T_ALTER(  'ALTA_PRIMA_COMPRA',   'NUMBER(1,0)'),
+        T_ALTER(  'TIPO_ALTA_ARRENDAMIENTO',   'VARCHAR2(100 CHAR)'),
         T_ALTER(  'OBLIGADO_CUMPLIMIENTO',   'DATE'),
         T_ALTER(  'FIANZA_OBLIGATORIA',   'NUMBER(16,2)'),
         T_ALTER(  'FECHA_REGISTRO',   'DATE'),
-        T_ALTER(  'IMPORTE_GARANTIA_AVAL',   'NUMBER(16,2)'),
-        T_ALTER(  'TIPO_INQUILINO',   'VARCHAR2(20 CHAR)'),
-        T_ALTER(  'IMPORTE_GARANTIA_DEPOSITO',   'NUMBER(16,2)')
+        T_ALTER(  'IMPORTE_GARANTIA',   'NUMBER(16,2)'),
+        T_ALTER(  'TIPO_INQUILINO',   'VARCHAR2(100 CHAR)')
 
 	);
     V_T_ALTER T_ALTER;

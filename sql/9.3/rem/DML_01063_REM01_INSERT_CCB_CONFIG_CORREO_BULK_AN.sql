d0098f1af77ce001165036056a742f929c4775d3
Santiago Monzo <santiago.monzo@pfsgroup.es>
2022-06-17 14:10:58 +0200

    HREOS-18095 anonimizar correos

--- sql/9.3/rem/DML_01063_REM01_INSERT_CCB_CONFIG_CORREO_BULK_AN.sql
+++ sql/9.3/rem/DML_01063_REM01_INSERT_CCB_CONFIG_CORREO_BULK_AN.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=Santi Monzó
---## FECHA_CREACION=20220526
+--## FECHA_CREACION=20220617
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.3
 --## INCIDENCIA_LINK=HREOS-17983
@@ -28,7 +28,8 @@ DECLARE
     V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.   
     ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
     ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
-	
+    V_MAIL VARCHAR2(25 CHAR):= 'pruebashrem@gmail.com';
+    V_ENTORNO NUMBER(16);
     V_TEXT1 VARCHAR2(2400 CHAR); -- Vble. auxiliar
     V_ENTIDAD_ID NUMBER(16);
     V_ID NUMBER(16);
@@ -43,33 +44,44 @@ DECLARE
     TYPE T_ARRAY_DATA IS TABLE OF T_TIPO_DATA;
 
     V_TIPO_DATA T_ARRAY_DATA := T_ARRAY_DATA(
-      T_TIPO_DATA('DID','appleofertas@haya.es'),
-      T_TIPO_DATA('DIC','backup.rem@pfsgroup.es'),
-      T_TIPO_DATA('DIC','appleofertas@haya.es'),
+      T_TIPO_DATA('DID','divarian_gov@haya.es'),
+      T_TIPO_DATA('DID','scampillo@haya.es'),
       T_TIPO_DATA('DID','backup.rem@pfsgroup.es'),
 
+      T_TIPO_DATA('DIC','divarian_gov@haya.es'),
+      T_TIPO_DATA('DIC','scampillo@haya.es'),
+      T_TIPO_DATA('DIC','backup.rem@pfsgroup.es'),
+
       T_TIPO_DATA('APDB','appleofertas@haya.es'),
+      T_TIPO_DATA('APDB','lcarrillo@haya.es'),
       T_TIPO_DATA('APDB','backup.rem@pfsgroup.es'),
 
       T_TIPO_DATA('APDO','appleofertas@haya.es'),
+      T_TIPO_DATA('APDO','lcarrillo@haya.es'),
       T_TIPO_DATA('APDO','backup.rem@pfsgroup.es'),
 
-      T_TIPO_DATA('DRDB','appleofertas@haya.es'),
+      T_TIPO_DATA('DRDB','divarian_gov@haya.es'),
+      T_TIPO_DATA('DRDB','scampillo@haya.es'),
       T_TIPO_DATA('DRDB','backup.rem@pfsgroup.es'),
 
-      T_TIPO_DATA('DRDO','appleofertas@haya.es'),
+      T_TIPO_DATA('DRDO','divarian_gov@haya.es'),
+      T_TIPO_DATA('DRDO','scampillo@haya.es'),
       T_TIPO_DATA('DRDO','backup.rem@pfsgroup.es'),
 
-      T_TIPO_DATA('JRCC','appleofertas@haya.es'),
+      T_TIPO_DATA('JRCC','jaguar_offer_sales@haya.es'),
+      T_TIPO_DATA('JRCC','lcarrillo@haya.es'),
       T_TIPO_DATA('JRCC','backup.rem@pfsgroup.es'),
 
-      T_TIPO_DATA('JRDC','appleofertas@haya.es'),
+      T_TIPO_DATA('JRDC','jaguar_offer_sales@haya.es'),
+      T_TIPO_DATA('JRDC','lcarrillo@haya.es'),
       T_TIPO_DATA('JRDC','backup.rem@pfsgroup.es'),
 
-      T_TIPO_DATA('JRDB','appleofertas@haya.es'),
+      T_TIPO_DATA('JRDB','jaguar_offer_sales@haya.es'),
+      T_TIPO_DATA('JRDB','lcarrillo@haya.es'),
       T_TIPO_DATA('JRDB','backup.rem@pfsgroup.es'),
 
-      T_TIPO_DATA('JRDO','appleofertas@haya.es'),
+      T_TIPO_DATA('JRDO','jaguar_offer_sales@haya.es'),
+      T_TIPO_DATA('JRDO','lcarrillo@haya.es'),
       T_TIPO_DATA('JRDO','backup.rem@pfsgroup.es')
 		); 
     V_TMP_TIPO_DATA T_TIPO_DATA;
@@ -78,18 +90,36 @@ BEGIN
 	
 	DBMS_OUTPUT.PUT_LINE('[INICIO] ');
 
-    DBMS_OUTPUT.PUT_LINE('[INFO]: COMENZAMOS ');
-    FOR I IN V_TIPO_DATA.FIRST .. V_TIPO_DATA.LAST
+  FOR I IN V_TIPO_DATA.FIRST .. V_TIPO_DATA.LAST
       LOOP
       
         V_TMP_TIPO_DATA := V_TIPO_DATA(I);
+
+        V_MSQL := 'SELECT CASE
+                  WHEN SYS_CONTEXT ( ''USERENV'', ''DB_NAME'' ) = ''orarem'' THEN 1
+                  ELSE 0
+                  END AS ES_PRO
+              FROM DUAL';
+
+        EXECUTE IMMEDIATE V_MSQL INTO V_ENTORNO;
+
+        IF V_ENTORNO = 1 THEN 
+       
+          V_MAIL:= TRIM(V_TMP_TIPO_DATA(2));
+
+        END IF;
+
+
+
+    DBMS_OUTPUT.PUT_LINE('[INFO]: COMENZAMOS ');
     
+
         --Comprobamos el dato a insertar
         V_SQL := 'SELECT COUNT(1) 
         FROM '||V_ESQUEMA||'.CCB_CONFIG_CORREO_BULK_AN CCB
         LEFT JOIN '||V_ESQUEMA||'.DD_TBK_TIPO_BULK_AN TBK ON CCB.DD_TBK_ID = TBK.DD_TBK_ID AND TBK.BORRADO = 0
         WHERE TBK.DD_TBK_CODIGO = '''||TRIM(V_TMP_TIPO_DATA(1))||'''
-        AND CCB.CORREO = '''||TRIM(V_TMP_TIPO_DATA(2))||'''
+        AND CCB.CORREO = '''||V_MAIL||'''
         AND CCB.BORRADO = 0
         ';
         EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
@@ -97,11 +127,11 @@ BEGIN
         --1 existe lo modificamos
         IF V_NUM_TABLAS > 0 THEN	
 
-          DBMS_OUTPUT.PUT_LINE('[INFO]: YA EXISTE : '''|| TRIM(V_TMP_TIPO_DATA(1)) ||''', '''|| TRIM(V_TMP_TIPO_DATA(2)) ||'''');  
+          DBMS_OUTPUT.PUT_LINE('[INFO]: YA EXISTE : '''|| TRIM(V_TMP_TIPO_DATA(1)) ||''', '''||V_MAIL||'''');  
 
         ELSE
        
-          DBMS_OUTPUT.PUT_LINE('[INFO]: INSERTAMOS EL REGISTRO '''|| TRIM(V_TMP_TIPO_DATA(1)) ||''', '''|| TRIM(V_TMP_TIPO_DATA(2)) ||'''');  
+          DBMS_OUTPUT.PUT_LINE('[INFO]: INSERTAMOS EL REGISTRO '''|| TRIM(V_TMP_TIPO_DATA(1)) ||''', '''||V_MAIL||'''');  
 
           V_MSQL := '
 
@@ -116,7 +146,7 @@ BEGIN
             SELECT
             '|| V_ESQUEMA ||'.S_CCB_CONFIG_CORREO_BULK_AN.NEXTVAL
             , (SELECT TBK.DD_TBK_ID FROM '||V_ESQUEMA||'.DD_TBK_TIPO_BULK_AN TBK WHERE TBK.DD_TBK_CODIGO = '''||TRIM(V_TMP_TIPO_DATA(1))||''')
-            , '''||TRIM(V_TMP_TIPO_DATA(2))||'''
+            , '''||V_MAIL||'''
             , '''||TRIM(V_ITEM)||'''
             , SYSDATE
             FROM DUAL

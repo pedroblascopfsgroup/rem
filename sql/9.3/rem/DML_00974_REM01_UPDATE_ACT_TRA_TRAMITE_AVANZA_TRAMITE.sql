95ebd90648f52baa4d64850443e84fcd40517468
Lara <lara.pablo@pfsgroup.es>
2022-06-21 08:29:38 +0200

    HREOS-17749 func-rem-pbcSinArras cambio de script

--- sql/9.3/rem/DML_00974_REM01_UPDATE_ACT_TRA_TRAMITE_AVANZA_TRAMITE.sql
+++ sql/9.3/rem/DML_00974_REM01_UPDATE_ACT_TRA_TRAMITE_AVANZA_TRAMITE.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
---## AUTOR=Ivan Rubio
---## FECHA_CREACION=20220505
+--## AUTOR=Lara Pablo
+--## FECHA_CREACION=20220621
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.3
 --## INCIDENCIA_LINK=HREOS-17749
@@ -33,10 +33,15 @@ DECLARE
     V_ECO_NUM VARCHAR(50 CHAR); -- Vble. que almacena el num de expediente
     V_CONTADOR NUMBER(16,0) := 1;
     
-    CURSOR EXPEDIENTES IS select distinct eco.eco_num_expediente
+BEGIN
+	
+	BEGIN
+	  FOR r_product IN (
+	        select distinct eco.eco_num_expediente
 			from #ESQUEMA#.eco_expediente_comercial eco
 			inner join #ESQUEMA#.act_ofr actofr on actofr.ofr_id = eco.ofr_id
 			inner join #ESQUEMA#.ofr_ofertas ofr on ofr.ofr_id = actofr.ofr_id
+			inner join #ESQUEMA#.coe_condicionantes_expediente coe on eco.eco_id = coe.eco_id
 			inner join #ESQUEMA#.act_activo act on act.act_id = actofr.act_id
 			inner join #ESQUEMA#.act_tra_tramite atr on eco.tbj_id = atr.tbj_id
 			inner join #ESQUEMA#.tac_tareas_activos tac on atr.tra_id = tac.tra_id
@@ -45,27 +50,25 @@ DECLARE
 			inner join #ESQUEMA#.tap_tarea_procedimiento tap on txt.tap_id = tap.tap_id 
 			where tar.tar_tarea_finalizada in (0)
 			and tap.tap_codigo in ('T017_PBCReserva')
-			AND ofr.OFR_IMPORTE < 20000.00
+			AND coe.coe_importe_reserva < 20000.00
 			and act.dd_cra_id in (SELECT dd_cra_id FROM #ESQUEMA#.DD_CRA_CARTERA WHERE dd_cra_codigo = '07')
-			and act.dd_scr_id in (SELECT dd_scr_id FROM #ESQUEMA#.DD_SCR_SUBCARTERA WHERE dd_scr_codigo IN ('138','150','70')); 
-
-BEGIN
-
-	FOR ECO IN EXPEDIENTES LOOP
-	
-		IF V_CONTADOR = 10 THEN
+			and act.dd_scr_id in (SELECT dd_scr_id FROM #ESQUEMA#.DD_SCR_SUBCARTERA WHERE dd_scr_codigo IN ('138','150','70'))
+	    )
+	  LOOP
+	     IF V_CONTADOR = 10 THEN
 			COMMIT;
 			V_CONTADOR := 1;
 		ELSE
-			V_ECO_NUM:=ECO.eco_num_expediente;
-			DBMS_OUTPUT.PUT_LINE('[INFO]: AVANZAR TRAMITE DE EXPEDIENTE '||V_ECO_NUM);
-			#ESQUEMA#.AVANCE_TRAMITE(V_USUARIO,V_ECO_NUM,'T017_InstruccionesReserva',null,null,PL_OUTPUT);
+			DBMS_OUTPUT.PUT_LINE('[INFO]: AVANZAR TRAMITE DE EXPEDIENTE '||r_product);
+			#ESQUEMA#.AVANCE_TRAMITE(V_USUARIO,r_product,'T017_InstruccionesReserva',null,null,PL_OUTPUT);
 			DBMS_OUTPUT.PUT_LINE(PL_OUTPUT);
 			DBMS_OUTPUT.PUT_LINE('[INFO]: Nº EXPEDIENTES AVANZADOS '||V_CONTADOR);
 			V_CONTADOR := V_CONTADOR +1;
 		END IF;
-	END LOOP;
-		DBMS_OUTPUT.PUT_LINE('[INFO]: '||SQL%ROWCOUNT||' ACTUALIZADO');
+	  END LOOP;
+	END;
+
+
 
 	COMMIT;
 EXCEPTION

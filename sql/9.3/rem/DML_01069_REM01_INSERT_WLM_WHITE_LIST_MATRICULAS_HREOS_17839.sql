9f403febb1b245536525c628b965c4b8fc46be2e
Lara <lara.pablo@pfsgroup.es>
2022-06-30 12:33:48 +0200

    HREOS-17949 func-rem-verDocsIdentifEco cambio fecha script

--- sql/9.3/rem/DML_01069_REM01_INSERT_WLM_WHITE_LIST_MATRICULAS_HREOS_17839.sql
+++ sql/9.3/rem/DML_01069_REM01_INSERT_WLM_WHITE_LIST_MATRICULAS_HREOS_17839.sql
@@ -1,7 +1,7 @@
 --/*
 --##########################################
 --## AUTOR=Ivan Rubio
---## FECHA_CREACION=20220519
+--## FECHA_CREACION=20220520
 --## ARTEFACTO=online
 --## VERSION_ARTEFACTO=9.3
 --## INCIDENCIA_LINK=HREOS-17848

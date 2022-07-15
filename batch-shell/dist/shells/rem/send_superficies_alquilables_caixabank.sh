7e90ab44b1b818c5dd99993564a6af17bc6e029e
Pier José Gotta Barrera <pier.gotta@pfsgroup.es>
2022-07-15 12:46:03 +0200

    HREOS-18290 Modificacion sh

--- batch-shell/dist/shells/rem/send_superficies_alquilables_caixabank.sh
+++ batch-shell/dist/shells/rem/send_superficies_alquilables_caixabank.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Generado por GUILLERMO LLIDO 20180802
+# Generado automaticamente a las mar nov 16 11:17:51 CEST 2021
  
-
 filename=$(basename $0)
 nameETL="${filename%.*}"
 
@@ -10,20 +9,19 @@ export DIR_CONFIG=$DIR_BASE_ETL/config/
 export CFG_FILE=config.ini
 export MAINSH="$nameETL"_run.sh
 
-cd "$DIR_ETL" &> /dev/null
-if [ $? -ne 0 ] ; then
+if [ ! -d "$DIR_ETL" ] ; then
    echo "$(basename $0) Error en $filename: directorio inexistente $DIR_ETL"
    exit 1
 fi
 
+cd $DIR_ETL
+
 if [ -f $MAINSH ]; then
-    CLASS="$(cat $MAINSH | grep "^ java" | cut -f10 -d" ")"
-    CLASS2=`echo $CLASS | sed -e 's/$ROOT_PATH/./g'`
-    CLASEINICIO="$(cat $MAINSH | grep "^ java" | cut -f11 -d" ")"
-    java -Xms512M -Xmx1536M -Dconfig.dir=$DIR_CONFIG -Dconfig.file.mask=$CFG_FILE -Duser.country=ES -Duser.language=es -cp $CLASS2 $CLASEINICIO --context=Default "$@"
+    CLASS="$(cat $MAINSH | sed 's/ /\n/g' | grep '$ROOT_PATH' | sed -e 's/$ROOT_PATH/./g')"
+    CLASEINICIO="$(cat $MAINSH | sed 's/ /\n/g' | grep -v '$ROOT_PATH' | grep $nameETL)"
+    java -Xms512M -Xmx1536M -Dconfig.dir=$DIR_CONFIG -Dconfig.file.mask=$CFG_FILE -Duser.country=ES -Duser.language=es -cp "$CLASS" "$CLASEINICIO" --context=Default "$@"
     exit $?
 else
     echo "$(basename $0) Error en $filename: no se ha encontrado  $MAINSH"
     exit 1
 fi
-

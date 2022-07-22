e642f5cb3885c17cafbeae4f28154684ab20657a
Lara <lara.pablo@pfsgroup.es>
2022-07-22 11:40:32 +0200

    HREOS-18420 - Concurrencia - tramitación con tiempo

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/concurrencia/ConcurrenciaManager.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/concurrencia/ConcurrenciaManager.java
@@ -125,6 +125,8 @@ public class ConcurrenciaManager  implements ConcurrenciaApi {
 	@Override
 	public boolean isOfertaEnPlazoConcu(boolean bloquear, List<Oferta> listOfertas) {
 		if(listOfertas != null && !listOfertas.isEmpty()) {
+			Filter filtroPendientesDeposito = genericDao.createFilter(FilterType.NOT_EQUALS, "estadoOferta.codigo", DDEstadoOferta.CODIGO_PDTE_DEPOSITO);
+		    listOfertas.remove(genericDao.get(Oferta.class, filtroPendientesDeposito));
 			for (Oferta oferta : listOfertas) {
 				if(this.entraEnTiempoDeposito(oferta)) {
 					bloquear =  true;

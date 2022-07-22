fe19c7131a11fd4e6afe4a358c6ff0d726ac8668
Ivan Rubio <ivan.rubio@pfsgroup.es>
2022-07-22 12:12:07 +0200

    HREOS-18341 Solicitud de Nueva Carga Masiva - Estados de gastos - Espacio enlazado #3

--- paradise-framework/src/main/java/es/pfsgroup/framework/paradise/bulkUpload/model/MSVDDOperacionMasiva.java
+++ paradise-framework/src/main/java/es/pfsgroup/framework/paradise/bulkUpload/model/MSVDDOperacionMasiva.java
@@ -1,13 +1,26 @@
 package es.pfsgroup.framework.paradise.bulkUpload.model;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
 import es.capgemini.pfs.auditoria.Auditable;
 import es.capgemini.pfs.auditoria.model.Auditoria;
 import es.capgemini.pfs.diccionarios.Dictionary;
 import es.capgemini.pfs.users.domain.Funcion;
-import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
-
-import java.io.Serializable;
 
 
 @Entity

d33c9d55e47a971ac97ecb8c987cfcb13bc333ad
Ivan Repiso Camara <ivan.repiso@pfsgroup.es>
2022-09-02 17:43:55 +0200

    REMVIP-12309 REM NO PERMITE PUBLICAR ALQUILER CARTERA CIBE

--- paradise-framework/src/main/java/es/pfsgroup/framework/paradise/bulkUpload/api/impl/ParticularValidatorManager.java
+++ paradise-framework/src/main/java/es/pfsgroup/framework/paradise/bulkUpload/api/impl/ParticularValidatorManager.java
@@ -5884,7 +5884,7 @@ public class ParticularValidatorManager implements ParticularValidatorApi {
 			listaAgrupaciones.add(numAgrup.longValue());
 		}
 		
-		return listaAgrupaciones.isEmpty() ? null: listaAgrupaciones;
+		return listaAgrupaciones;
 	}
 	
 	@Override

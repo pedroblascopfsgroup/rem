2e349178c072738943154c2c7bd5ec9466e78a4e
Juan Bautista <juan.bautista@pfsgroup.es>
2022-10-25 16:44:15 +0200

    REMVIP-12631 shell val_gen_ru_salidas_facturas

--- batch-shell/dist/shells/rem/val_gen_ru_salidas_facturas.sh
+++ batch-shell/dist/shells/rem/val_gen_ru_salidas_facturas.sh
@@ -10,12 +10,37 @@ export DIR_ETL=$DIR_BASE_ETL/$nameETL
 export DIR_CONFIG=$DIR_BASE_ETL/config/
 export CFG_FILE=config.ini
 export MAINSH="$nameETL"_run.sh
+export OUTPUT_PATH=`cat $DIR_CONFIG$CFG_FILE | grep 'output_dir;' | cut -d';' -f2`
+export PREVALIDADO_PATH=$OUTPUT_PATH/prevalidado
 
 cd "$DIR_ETL" &> /dev/null
 if [ $? -ne 0 ] ; then
    echo "$(basename $0) Error en $filename: directorio inexistente $DIR_ETL"
    exit 1
 fi
+
+if test ! -f ${OUTPUT_PATH}/RUFACTUSP_??????????????.txt ; then
+   echo "$(basename $0) Error ${PREVALIDADO_PATH}/RUFACTUSP_??????????????.txt inexistente"
+   exit 1
+fi
+
+if test ! -f ${OUTPUT_PATH}/RUFACTUCP_??????????????.txt ; then
+   echo "$(basename $0) Error ${PREVALIDADO_PATH}/RUFACTUCP_??????????????.txt inexistente"
+   exit 1
+fi
+
+if test ! -f ${OUTPUT_PATH}/RUFACTUSP_??????????????.sem ; then
+   echo "$(basename $0) Error ${PREVALIDADO_PATH}/RUFACTUSP_??????????????.sem inexistente"
+   exit 1
+fi
+
+if test ! -f ${OUTPUT_PATH}/RUFACTUCP_??????????????.sem ; then
+   echo "$(basename $0) Error ${PREVALIDADO_PATH}/RUFACTUCP_??????????????.sem inexistente"
+   exit 1
+fi
+
+mv $OUTPUT_PATH/RUFACTU*.* $PREVALIDADO_PATH/.
+
 
 if [ -f $MAINSH ]; then
     CLASS="$(cat $MAINSH | grep "^ java" | cut -f10 -d" ")"

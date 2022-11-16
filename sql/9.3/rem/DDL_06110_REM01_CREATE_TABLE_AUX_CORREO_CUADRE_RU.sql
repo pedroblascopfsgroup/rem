990992f7158a08e543329b3deca1e33ef85b0d56
Juan Bautista <juan.bautista@pfsgroup.es>
2022-11-16 19:02:34 +0100

    REMVIP-12675 Tabla auxiliar para correos

---
+++ sql/9.3/rem/DDL_06110_REM01_CREATE_TABLE_AUX_CORREO_CUADRE_RU.sql
@@ -0,0 +1,213 @@
+--/*
+--##########################################
+--## AUTOR=Juan Bautista Alfonso
+--## FECHA_CREACION=20221115
+--## ARTEFACTO=batch
+--## VERSION_ARTEFACTO=9.3
+--## INCIDENCIA_LINK=REMVIP-12675
+--## PRODUCTO=NO
+--##
+--## Finalidad: Creación de tablas '||V_TABLA||'
+--##
+--## INSTRUCCIONES:
+--## VERSIONES:
+--##        0.1 Versión inicial - [REMVIP-12675] - Juan Bautista Alfonso
+--#########################################
+--*/
+
+--Para permitir la visualización de texto en un bloque PL/SQL utilizando DBMS_OUTPUT.PUT_LINE
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON;
+SET DEFINE OFF;
+
+DECLARE
+
+TABLE_COUNT NUMBER(1,0) := 0;
+V_ESQUEMA VARCHAR2(20 CHAR) := '#ESQUEMA#';
+V_TABLA VARCHAR2(40 CHAR) := 'AUX_CORREO_CUADRE_RU';
+V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+V_MSQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.
+V_TABLESPACE_IDX VARCHAR2(25 CHAR):= '#TABLESPACE_INDEX#'; -- Configuracion Tablespace de Indices
+V_USUARIO VARCHAR2(100 CHAR):='REMVIP-12675'; --Usuariomodificar
+
+ TYPE T_TIPO_DATA IS TABLE OF VARCHAR2(150);
+    TYPE T_ARRAY_DATA IS TABLE OF T_TIPO_DATA;
+
+    V_TIPO_DATA T_ARRAY_DATA := T_ARRAY_DATA(
+                --CORREO             --CORREO_EN_COPIA
+   /*   T_TIPO_DATA('adelgado@haya.es','0'),
+      T_TIPO_DATA('jrodriguezb@haya.es','0'),
+      T_TIPO_DATA('amolinam@haya.es','1'),
+      T_TIPO_DATA('dgutierrez@haya.es','1'),
+      T_TIPO_DATA('juanantonio.morenoortiz@zelenza.com','0'),*/
+      T_TIPO_DATA('pruebashrem@gmail.com','0'),
+      T_TIPO_DATA('juan.alfonso@pfsgroup.es','0')
+		); 
+    V_TMP_TIPO_DATA T_TIPO_DATA;
+
+BEGIN
+
+	SELECT COUNT(1) INTO TABLE_COUNT FROM ALL_TABLES WHERE TABLE_NAME = ''||V_TABLA||'' AND OWNER= ''||V_ESQUEMA||'';
+
+    -- Verificar si la tabla ya existe
+	IF TABLE_COUNT > 0 THEN
+		DBMS_OUTPUT.PUT_LINE('[INFO] TABLA '||V_ESQUEMA||'.'||V_TABLA||' YA EXISTENTE. SE PROCEDE A BORRAR Y CREAR DE NUEVO.');
+		EXECUTE IMMEDIATE 'DROP TABLE '||V_ESQUEMA||'.'||V_TABLA||'';
+	END IF;
+
+    --Comprobamos si existe la secuencia
+	V_SQL := 'SELECT COUNT(1) FROM ALL_SEQUENCES WHERE SEQUENCE_NAME = ''S_'||V_TABLA||''' and SEQUENCE_OWNER = '''||V_ESQUEMA||'''';
+	EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
+	IF V_NUM_TABLAS = 1 THEN
+		DBMS_OUTPUT.PUT_LINE('[INFO] '|| V_ESQUEMA ||'.S_'||V_TABLA||'... Ya existe. Se borrará.');
+		EXECUTE IMMEDIATE 'DROP SEQUENCE '||V_ESQUEMA||'.S_'||V_TABLA||'';
+
+	END IF;
+
+	EXECUTE IMMEDIATE 'CREATE TABLE '||V_ESQUEMA||'.'||V_TABLA||'
+                            (
+                                INF_ID	 	          		NUMBER(16)                  NOT NULL,
+                                CORREO						VARCHAR2(150 CHAR)			NOT NULL,
+                                CORREO_EN_COPIA				NUMBER(1,0)			        NOT NULL,
+                                VERSION 					NUMBER(38,0) 		    	DEFAULT 0 NOT NULL ENABLE,
+                                USUARIOCREAR 				VARCHAR2(50 CHAR) 	    	NOT NULL ENABLE,
+                                FECHACREAR 					TIMESTAMP (6) 		    	NOT NULL ENABLE,
+                                USUARIOMODIFICAR 			VARCHAR2(50 CHAR),
+                                FECHAMODIFICAR 				TIMESTAMP (6),
+                                USUARIOBORRAR 				VARCHAR2(50 CHAR),
+                                FECHABORRAR 				TIMESTAMP (6),
+                                BORRADO 					NUMBER(1,0) 		    	DEFAULT 0 NOT NULL ENABLE
+
+                           )';
+
+
+
+
+	DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.'||V_TABLA||' CREADA');
+
+    -- Creamos indice
+	V_SQL := 'CREATE UNIQUE INDEX '||V_ESQUEMA||'.'||V_TABLA||'_PK ON '||V_ESQUEMA|| '.'||V_TABLA||'(INF_ID) TABLESPACE '||V_TABLESPACE_IDX;
+	EXECUTE IMMEDIATE V_SQL;
+	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TABLA||'_PK... Indice creado.');
+
+
+	-- Creamos primary key
+	V_SQL := 'ALTER TABLE '||V_ESQUEMA||'.'||V_TABLA||' ADD (CONSTRAINT '||V_TABLA||'_PK PRIMARY KEY (INF_ID) USING INDEX)';
+	EXECUTE IMMEDIATE V_SQL;
+	DBMS_OUTPUT.PUT_LINE('[INFO] ' ||V_ESQUEMA||'.'||V_TABLA||'_PK... PK creada.');
+
+
+	-- Creamos sequence
+	V_SQL := 'CREATE SEQUENCE '||V_ESQUEMA||'.S_'||V_TABLA||'';
+	EXECUTE IMMEDIATE V_SQL;
+	DBMS_OUTPUT.PUT_LINE('[INFO] '||V_ESQUEMA||'.S_'||V_TABLA||'... Secuencia creada');
+
+    V_SQL := 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TABLA||'.INF_ID IS ''Identificador único ''';
+    EXECUTE IMMEDIATE V_SQL;
+    DBMS_OUTPUT.PUT_LINE('[INFO] Comentario de la columna INF_ID creado.');
+
+    V_SQL := 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TABLA||'.CORREO IS ''Correo al que se envia''';
+    EXECUTE IMMEDIATE V_SQL;
+    DBMS_OUTPUT.PUT_LINE('[INFO] Comentario de la columna CORREO creado.');
+
+    V_SQL := 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TABLA||'.CORREO_EN_COPIA IS ''Correo en copia al que se envia''';
+    EXECUTE IMMEDIATE V_SQL;
+    DBMS_OUTPUT.PUT_LINE('[INFO] Comentario de la columna CORREO_EN_COPIA creado.');
+
+    V_SQL := 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TABLA||'.VERSION IS ''Indica la versión del registro.''';
+    EXECUTE IMMEDIATE V_SQL;
+    DBMS_OUTPUT.PUT_LINE('[INFO] Comentario de la columna VERSION creado.');
+
+    V_SQL := 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TABLA||'.USUARIOCREAR IS ''Indica el usuario que creó el registro.''';
+    EXECUTE IMMEDIATE V_SQL;
+    DBMS_OUTPUT.PUT_LINE('[INFO] Comentario de la columna USUARIOCREAR creado.');
+
+    V_SQL := 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TABLA||'.FECHACREAR IS ''Indica la fecha en la que se creó el registro.''';
+    EXECUTE IMMEDIATE V_SQL;
+    DBMS_OUTPUT.PUT_LINE('[INFO] Comentario de la columna FECHACREAR creado.');
+
+    V_SQL := 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TABLA||'.USUARIOMODIFICAR IS ''Indica el usuario que modificó el registro.''';
+    EXECUTE IMMEDIATE V_SQL;
+    DBMS_OUTPUT.PUT_LINE('[INFO] Comentario de la columna USUARIOMODIFICAR creado.');
+
+    V_SQL := 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TABLA||'.FECHAMODIFICAR IS ''Indica la fecha en la que se modificó el registro.''';
+    EXECUTE IMMEDIATE V_SQL;
+    DBMS_OUTPUT.PUT_LINE('[INFO] Comentario de la columna FECHAMODIFICAR creado.');
+
+    V_SQL := 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TABLA||'.USUARIOBORRAR IS ''Indica el usuario que borró el registro.''';
+    EXECUTE IMMEDIATE V_SQL;
+    DBMS_OUTPUT.PUT_LINE('[INFO] Comentario de la columna USUARIOBORRAR creado.');
+
+    V_SQL := 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TABLA||'.FECHABORRAR IS ''Indica la fecha en la que se borró el registro.''';
+    EXECUTE IMMEDIATE V_SQL;
+    DBMS_OUTPUT.PUT_LINE('[INFO] Comentario de la columna FECHABORRAR creado.');
+
+    V_SQL := 'COMMENT ON COLUMN '||V_ESQUEMA||'.'||V_TABLA||'.BORRADO IS ''Indicador de borrado.''';
+    EXECUTE IMMEDIATE V_SQL;
+    DBMS_OUTPUT.PUT_LINE('[INFO] Comentario de la columna BORRADO creado.');
+
+    DBMS_OUTPUT.PUT_LINE('[INFO]: COMENZAMOS A INSERTAR REGISTROS...');
+    FOR I IN V_TIPO_DATA.FIRST .. V_TIPO_DATA.LAST
+      LOOP
+      
+        V_TMP_TIPO_DATA := V_TIPO_DATA(I);
+    
+        --Comprobamos el dato a insertar
+        V_SQL := 'SELECT COUNT(1) 
+        FROM '||V_ESQUEMA||'.'||V_TABLA||' INF
+        WHERE INF.CORREO = '''||TRIM(V_TMP_TIPO_DATA(1))||'''
+        AND INF.CORREO_EN_COPIA  = '''||TRIM(V_TMP_TIPO_DATA(2))||'''
+        AND INF.BORRADO = 0
+        ';
+        EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
+        
+        --1 existe lo modificamos
+        IF V_NUM_TABLAS > 0 THEN	
+
+          DBMS_OUTPUT.PUT_LINE('[INFO]: YA EXISTE : CORREO: '''|| TRIM(V_TMP_TIPO_DATA(1)) ||''' O CORREO EN COPIA: '''|| TRIM(V_TMP_TIPO_DATA(2)) ||'''');  
+
+        ELSE
+       
+          DBMS_OUTPUT.PUT_LINE('[INFO]: INSERTAMOS EL REGISTRO -> CORREO: '''|| TRIM(V_TMP_TIPO_DATA(1)) ||''' O CORREO EN COPIA: '''|| TRIM(V_TMP_TIPO_DATA(2)) ||'''');  
+
+          V_MSQL := '
+
+            INSERT INTO '|| V_ESQUEMA ||'.'||V_TABLA||' INF
+            (
+              INF_ID
+              , CORREO
+              , CORREO_EN_COPIA
+              , USUARIOCREAR
+              , FECHACREAR
+            )
+            SELECT
+            '|| V_ESQUEMA ||'.S_'||V_TABLA||'.NEXTVAL
+            , '''||TRIM(V_TMP_TIPO_DATA(1))||'''
+            , '''||TRIM(V_TMP_TIPO_DATA(2))||'''
+            , '''||TRIM(V_USUARIO)||'''
+            , SYSDATE
+            FROM DUAL
+          ';
+          EXECUTE IMMEDIATE V_MSQL;
+          DBMS_OUTPUT.PUT_LINE('[INFO]: REGISTRO INSERTADO CORRECTAMENTE');
+        
+       END IF;
+      END LOOP;
+
+    COMMIT;
+DBMS_OUTPUT.PUT_LINE('[FIN]:                            ');
+
+EXCEPTION
+
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecucion:'||TO_CHAR(SQLCODE));
+        DBMS_OUTPUT.put_line('-----------------------------------------------------------');
+        DBMS_OUTPUT.put_line(SQLERRM);
+        ROLLBACK;
+        RAISE;
+END;
+/
+
+EXIT;

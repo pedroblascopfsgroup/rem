7ebbd364f5a959f6eabce5e31116cab242695394
Juan Bautista <juan.bautista@pfsgroup.es>
2022-11-22 15:28:48 +0100

    REMVIP-12774 Añadir subcartera a los comites de jaguar

---
+++ sql/9.3/rem/DML_06186_REM01_UPDATE_DD_COS_COMITES_SANCION.sql
@@ -0,0 +1,101 @@
+--/*
+--##########################################
+--## AUTOR=Juan Bautista Alfonso
+--## FECHA_CREACION=20221122
+--## ARTEFACTO=online
+--## VERSION_ARTEFACTO=9.2
+--## INCIDENCIA_LINK=REMVIP-12774
+--## PRODUCTO=NO
+--##
+--## Finalidad: Script que añade en DD_COS_COMITES_SANCION los datos añadidos en T_ARRAY_DATA
+--## INSTRUCCIONES:
+--## VERSIONES:
+--##        0.1 Versión inicial
+--##########################################
+--*/
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+SET SERVEROUTPUT ON; 
+SET DEFINE OFF;
+
+
+DECLARE
+
+    V_MSQL VARCHAR2(32000 CHAR); -- Sentencia a ejecutar.
+    V_ESQUEMA VARCHAR2(25 CHAR):= '#ESQUEMA#'; -- Configuracion Esquema.
+    V_ESQUEMA_M VARCHAR2(25 CHAR):= '#ESQUEMA_MASTER#'; -- Configuracion Esquema Master.
+    V_SQL VARCHAR2(4000 CHAR); -- Vble. para consulta que valida la existencia de una tabla.
+    V_NUM_TABLAS NUMBER(16); -- Vble. para validar la existencia de una tabla.
+    ERR_NUM NUMBER(25);  -- Vble. auxiliar para registrar errores en el script.
+    ERR_MSG VARCHAR2(1024 CHAR); -- Vble. auxiliar para registrar errores en el script.
+    V_NUM_SEQUENCE NUMBER(16); -- Vble. auxiliar para almacenar el número de secuencia actual.
+    V_NUM_MAXID NUMBER(16); -- Vble. auxiliar para almacenar el número de secuencia máxima utilizada en los registros.
+
+    V_TEXT_TABLA VARCHAR2(27 CHAR) := 'DD_COS_COMITES_SANCION'; -- Vble. auxiliar para almacenar el nombre de la tabla de ref.
+    V_TEXT_CHARS VARCHAR2(3 CHAR) := 'COS'; -- Vble. auxiliar para almacenar las 3 letras orientativas de la tabla de ref.
+    V_USU_MODIFICAR VARCHAR2(30 CHAR) := '''REMVIP-12774'''; -- Vble. auxiliar para almacenar el nombre de usuario que modifica los registros.
+
+    TYPE T_TIPO_DATA IS TABLE OF VARCHAR2(150);
+    TYPE T_ARRAY_DATA IS TABLE OF T_TIPO_DATA;
+    V_TIPO_DATA T_ARRAY_DATA := T_ARRAY_DATA(
+    --        CODIGO    DESCRIPCION         DESCRIPCION_LARGA   CARTERA_CODIGO	SUBCARTERA_CODIGO
+        T_TIPO_DATA('51',   'Jaguar'  ,   'Jaguar',   '07',	'70'),
+        T_TIPO_DATA('52',   'Haya'  ,   'Haya',   '07',	'70')
+    ); 
+    V_TMP_TIPO_DATA T_TIPO_DATA;
+
+BEGIN 
+
+  DBMS_OUTPUT.PUT_LINE('[INICIO]');
+
+
+  -- LOOP para insertar los valores --
+  DBMS_OUTPUT.PUT_LINE('[INFO]: INSERCION EN '||V_TEXT_TABLA);
+  FOR I IN V_TIPO_DATA.FIRST .. V_TIPO_DATA.LAST
+  LOOP
+
+    V_TMP_TIPO_DATA := V_TIPO_DATA(I);
+
+    -- Comprobar el dato a insertar.
+    V_SQL := 'SELECT COUNT(1) FROM '||V_ESQUEMA||'.'||V_TEXT_TABLA||' WHERE DD_'||V_TEXT_CHARS||'_CODIGO = '''||TRIM(V_TMP_TIPO_DATA(1))||'''';
+    EXECUTE IMMEDIATE V_SQL INTO V_NUM_TABLAS;
+
+        IF V_NUM_TABLAS > 0 THEN        
+      -- Si existe se modifica.
+      DBMS_OUTPUT.PUT_LINE('[INFO]: MODIFICAR EL REGISTRO '''|| TRIM(V_TMP_TIPO_DATA(1)) ||'''');
+      V_MSQL := 'UPDATE '|| V_ESQUEMA ||'.'||V_TEXT_TABLA||' '||
+            'SET DD_'||V_TEXT_CHARS||'_DESCRIPCION = '''||TRIM(V_TMP_TIPO_DATA(2))||''''|| 
+            ', DD_'||V_TEXT_CHARS||'_DESCRIPCION_LARGA = '''||TRIM(V_TMP_TIPO_DATA(3))||''''||
+            ', DD_CRA_ID = (SELECT DD_CRA_ID FROM '||V_ESQUEMA||'.DD_CRA_CARTERA WHERE DD_CRA_CODIGO = '''||TRIM(V_TMP_TIPO_DATA(4))||''')'||
+            ', DD_SCR_ID = (SELECT DD_SCR_ID FROM '||V_ESQUEMA||'.DD_SCR_SUBCARTERA WHERE DD_SCR_CODIGO = '''||TRIM(V_TMP_TIPO_DATA(5))||''')'||
+            ', USUARIOMODIFICAR = '|| V_USU_MODIFICAR ||' , FECHAMODIFICAR = SYSDATE '||
+            'WHERE DD_'||V_TEXT_CHARS||'_CODIGO = '''||TRIM(V_TMP_TIPO_DATA(1))||'''';
+      EXECUTE IMMEDIATE V_MSQL;
+      DBMS_OUTPUT.PUT_LINE('[INFO]: REGISTRO '''||TRIM(V_TMP_TIPO_DATA(1))||''' MODIFICADO CORRECTAMENTE');
+
+       ELSE
+        DBMS_OUTPUT.PUT_LINE('[WARN]: YA EXISTE EL REGISTRO '''||TRIM(V_TMP_TIPO_DATA(1))||'''');
+
+    END IF;
+  END LOOP;
+
+  COMMIT;
+
+  DBMS_OUTPUT.PUT_LINE('[FIN]: DICCIONARIO '||V_TEXT_TABLA||' ACTUALIZADO CORRECTAMENTE ');
+
+
+EXCEPTION
+    WHEN OTHERS THEN
+      err_num := SQLCODE;
+      err_msg := SQLERRM;
+
+      DBMS_OUTPUT.put_line('[ERROR] Se ha producido un error en la ejecución:'||TO_CHAR(err_num));
+      DBMS_OUTPUT.put_line('-----------------------------------------------------------'); 
+      DBMS_OUTPUT.put_line(err_msg);
+
+      ROLLBACK;
+      RAISE;          
+
+END;
+/
+EXIT

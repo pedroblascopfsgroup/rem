753b9d2582d8126bdae4f57886cf7f64cb9d1cd2
Alejandra Petrovic <alejandra.petrovic@pfsgroup.es>
2022-12-21 12:28:08 +0100

    HREOS-19105 Eliminar de la segunda validación los estados anulados y denegados del expediente comercial

--- sql/9.3/rem/minimigraciones/HREOS-15592/DML/DML_002_REM01_INSERT_TABLE_BIENES_ACTIVOS.sql
+++ sql/9.3/rem/minimigraciones/HREOS-15592/DML/DML_002_REM01_INSERT_TABLE_BIENES_ACTIVOS.sql
@@ -1,18 +1,19 @@
 --/*
 --#########################################
---## AUTOR=Santi Monzó
---## FECHA_CREACION=20211015
+--## AUTOR=Alejandra García
+--## FECHA_CREACION=20221221
 --## ARTEFACTO=batch
 --## VERSION_ARTEFACTO=9.3
 --## ARTEFACTO=batch
---## INCIDENCIA_LINK=HREOS-15592
+--## INCIDENCIA_LINK=HREOS-19105
 --## PRODUCTO=NO
 --## 
 --## Finalidad:
 --##			
 --## INSTRUCCIONES:  
 --## VERSIONES:
---##        0.1 Versión inicial
+--##        0.1 Versión inicial - [HREOS-15592] - Santi Monzó (20211015)
+--##        0.2 Eliminar de la segunda validación los estados anulados y denegados del expediente comercial - [HREOS-19105] - Alejandra García (20221221)
 --#########################################
 --*/
 
@@ -125,7 +126,7 @@ BEGIN
 					LEFT JOIN '||V_ESQUEMA||'.ECO_EXPEDIENTE_COMERCIAL ECO ON ECO.OFR_ID = OFR.OFR_ID
 					
 					WHERE OFR.DD_EOF_ID IN (SELECT DD_EOF_ID FROM '||V_ESQUEMA||'.DD_EOF_ESTADOS_OFERTA WHERE DD_EOF_CODIGO = (''01''))
-					AND ECO.DD_EEC_ID NOT IN (SELECT DD_EEC_ID FROM '||V_ESQUEMA||'.DD_EEC_EST_EXP_COMERCIAL WHERE DD_EEC_CODIGO IN (''01'',''02'',''04'',''10'',''12'',''14'',''38'',''43'')))
+					AND ECO.DD_EEC_ID NOT IN (SELECT DD_EEC_ID FROM '||V_ESQUEMA||'.DD_EEC_EST_EXP_COMERCIAL WHERE DD_EEC_CODIGO IN (''01'',''04'',''10'',''14'',''38'',''43'')))
 					
 					us ON (us.ID_ACTIVO_CAJAMAR = AUX.ID_ACTIVO_CAJAMAR )
 										WHEN MATCHED THEN UPDATE SET

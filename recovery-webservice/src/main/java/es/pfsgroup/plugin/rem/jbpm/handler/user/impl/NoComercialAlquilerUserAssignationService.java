81ed1e185725bfc915823e1ffeeb41f51496f99a
Ivan Repiso Camara <ivan.repiso@pfsgroup.es>
2023-01-23 16:43:40 +0100

    REMVIP-12685 Reasignar tareas

--- recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/jbpm/handler/user/impl/NoComercialAlquilerUserAssignationService.java
+++ recovery-webservice/src/main/java/es/pfsgroup/plugin/rem/jbpm/handler/user/impl/NoComercialAlquilerUserAssignationService.java
@@ -13,7 +13,6 @@ import es.pfsgroup.plugin.rem.model.TareaActivo;
 @Component
 public class NoComercialAlquilerUserAssignationService implements UserAssigantionService {
 
-	private static final 	String	CODIGO_T018_AGENDARYFIRMAR = "T018_AgendarYFirmar	";
 	private static final 	String	CODIGO_T018_ANALISISBC	= 	"T018_AnalisisBc";
 	private static final 	String	CODIGO_T018_ANALISISTECNICO	 = "T018_AnalisisTecnico";
 	private static final 	String	CODIGO_T018_BLOQUEOSCREENING = "T018_BloqueoScreening";
@@ -21,23 +20,16 @@ public class NoComercialAlquilerUserAssignationService implements UserAssigantio
 	private static final 	String	CODIGO_T018_PBCALQUILER	 = "T018_PbcAlquiler";
 	private static final 	String	CODIGO_T018_PTECLROD= "T018_PteClRod";
 	private static final 	String	CODIGO_T018_RESOLUCIONCOMITE = "T018_ResolucionComite";
-	private static final 	String	CODIGO_T018_REVISIONBCYCONDICIONES = "T018_RevisionBcYCondiciones";
 	private static final 	String	CODIGO_T018_SCORING	= "T018_Scoring";
 	private static final 	String	CODIGO_T018_SCORINGBC= "T018_ScoringBc";
 	private static final 	String	CODIGO_T018_SOLICITARGARANTIASADICIONALES = "T018_SolicitarGarantiasAdicionales";
-	private static final 	String	CODIGO_T018_TRASLADAROFERTACLIENTE = "T018_TrasladarOfertaCliente";
 	private static final 	String	CODIGO_TO18_CIERRE = "T018_CierreContrato";
 	private static final 	String 	CODIGO_T018_BLOQUEOSCORING = "T018_BloqueoScoring";
-	private static final 	String 	CODIGO_T018_APROBACION_ALQUILER_SOCIAL = "T018_AprobacionAlquilerSocial";
 	private static final 	String 	CODIGO_T018_RESPUESTA_CONTRAOFERTA_BC = "T018_RespuestaContraofertaBC";
 	private static final 	String 	CODIGO_T018_ENTREGA_FIANZAS = "T018_EntregaFianzas";
 	private static final 	String 	CODIGO_T018_APROBACION_OFERTA = "T018_AprobacionOferta";
-	private static final 	String 	CODIGO_T018_COMUNICAR_SUBROGACION = "T018_ComunicarSubrogacion";
-	private static final 	String 	CODIGO_T018_ALTA_CONTRATO_ALQUILER = "T018_AltaContratoAlquiler";
-	private static final 	String 	CODIGO_T018_APROBACION_CONTRATO = "T018_AprobacionContrato";
 	private static final 	String 	CODIGO_T018_AGENDAR_FIRMA_ADENDA = "T018_AgendarFirmaAdenda";
 	private static final 	String 	CODIGO_T018_FIRMA_ADENDA = "T018_FirmaAdenda";
-	private static final 	String 	CODIGO_T018_ALTA_CONTRATO_ALQUILER_ADENDA = "T018_AltaContratoAlquilerAdenda";
 	private static final 	String 	CODIGO_T018_PROPONER_RECESION_CLIENTE = "T018_ProponerRescisionCliente";
 	private static final 	String 	CODIGO_T018_FIRMA_RECESION_CONTRATO = "T018_FirmaRescisionContrato";
 	private static final 	String 	CODIGO_T018_RESPUESTA_REAGENDACION_BC = "T018_RespuestaReagendacionBC";
@@ -46,8 +38,9 @@ public class NoComercialAlquilerUserAssignationService implements UserAssigantio
 	private static final 	String 	CODIGO_T018_RESPUESTA_OFERTA_BC = "T018_RespuestaOfertaBC";
 	private static final 	String 	CODIGO_T018_DECISION_COMITE = "T018_DecisionComite";
 	private static final 	String 	CODIGO_T018_DECISION_CONTINUIDAD_OFERTA = "T018_DecisionContinuidadOferta";
-
-	
+	private static final String CODIGO_T018_CALCULO_RIESGO = "T018_CalculoRiesgo";
+	public static final String CODIGO_DATOSPBC = "T018_DatosPBC";
+	private static final String CODIGO_T018_ESPERA_COMITE_POSESIONES = "T018_EsperaComitePosesiones";
 
 	@Autowired
 	private GestorActivoApi gestorActivoApi;
@@ -59,11 +52,10 @@ public class NoComercialAlquilerUserAssignationService implements UserAssigantio
 
 	@Override
 	public String[] getCodigoTarea() {
-		return new String[]{CODIGO_T018_AGENDARYFIRMAR, CODIGO_T018_DEFINICIONOFERTA, CODIGO_T018_PBCALQUILER, CODIGO_T018_BLOQUEOSCREENING,
-			CODIGO_T018_SCORING, CODIGO_T018_SOLICITARGARANTIASADICIONALES, CODIGO_T018_TRASLADAROFERTACLIENTE, CODIGO_T018_PTECLROD,
-			CODIGO_T018_ANALISISTECNICO,CODIGO_TO18_CIERRE,CODIGO_T018_BLOQUEOSCORING,CODIGO_T018_SCORINGBC,CODIGO_T018_REVISIONBCYCONDICIONES,
-			CODIGO_T018_RESOLUCIONCOMITE,CODIGO_T018_ANALISISBC,CODIGO_T018_ALTA_CONTRATO_ALQUILER,CODIGO_T018_ALTA_CONTRATO_ALQUILER_ADENDA,
-			CODIGO_T018_DECISION_COMITE, CODIGO_T018_DECISION_CONTINUIDAD_OFERTA};
+		return new String[]{CODIGO_T018_DEFINICIONOFERTA,CODIGO_T018_SCORING,CODIGO_T018_SOLICITARGARANTIASADICIONALES,CODIGO_T018_PTECLROD,CODIGO_T018_ANALISISTECNICO,
+				CODIGO_T018_RESOLUCIONCOMITE,CODIGO_DATOSPBC,CODIGO_T018_CALCULO_RIESGO,CODIGO_T018_PBCALQUILER,CODIGO_T018_APROBACION_OFERTA,CODIGO_T018_ANALISISBC,
+				CODIGO_T018_PROPONER_RECESION_CLIENTE,CODIGO_T018_ESPERA_COMITE_POSESIONES,CODIGO_T018_FIRMA_CONTRATO,CODIGO_T018_AGENDAR_FIRMA,CODIGO_T018_SCORINGBC,
+				CODIGO_T018_AGENDAR_FIRMA_ADENDA,CODIGO_TO18_CIERRE,CODIGO_T018_FIRMA_ADENDA,CODIGO_T018_DECISION_COMITE,CODIGO_T018_FIRMA_RECESION_CONTRATO};
 	}
 
 	@Override
